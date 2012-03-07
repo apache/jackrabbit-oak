94050ca7e922f8a38346816bf0a1d91ae571bfb5
Stefan Guggisberg <stefan@apache.org>
2012-03-07 16:41:29 +0000

    renaming persistence abstraction layer
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1298016 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/jackrabbit/mk/store/persistence/MongoPersistenceManager.java
+++ src/main/java/org/apache/jackrabbit/mk/store/persistence/MongoPersistenceManager.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.mk.store.pm;
+package org.apache.jackrabbit.mk.store.persistence;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
