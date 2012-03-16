077fccb7f711d1b80ff75928327e18c0a2af630e
Michael Dürig <mduerig@apache.org>
2012-03-16 13:30:44 +0000

    OAK-15: Clean up oak-jcr  - formatting
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1301494 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/state/PropertyStateImpl.java
+++ oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/state/PropertyStateImpl.java
@@ -4,13 +4,13 @@ import org.apache.jackrabbit.oak.jcr.json.JsonValue;
 import org.apache.jackrabbit.oak.model.AbstractPropertyState;
 
 public class PropertyStateImpl extends AbstractPropertyState {
-private final String name;
-private final JsonValue value;
+    private final String name;
+    private final JsonValue value;
 
-public PropertyStateImpl(String name, JsonValue value) {
-    this.name = name;
-    this.value = value;
-}
+    public PropertyStateImpl(String name, JsonValue value) {
+        this.name = name;
+        this.value = value;
+    }
 
     public JsonValue getValue() {
         return value;
