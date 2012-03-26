e8eee044f3cb9ec3d12743d8bd5634e2c0d606c1
Michael Dürig <mduerig@apache.org>
2012-03-26 10:31:01 +0000

    OAK-33: Values in oak-core move ScalarImpl from into kernel package
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1305296 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/jackrabbit/oak/kernel/ScalarImpl.java
+++ src/main/java/org/apache/jackrabbit/oak/kernel/ScalarImpl.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.oak;
+package org.apache.jackrabbit.oak.kernel;
 
 import org.apache.jackrabbit.mk.model.Scalar;
 
