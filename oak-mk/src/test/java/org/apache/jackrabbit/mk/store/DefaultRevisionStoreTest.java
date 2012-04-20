4288550733a92b3fee5bb7416eb93dd606bdd697
Dominique Pfister <dpfister@apache.org>
2012-04-20 15:21:14 +0000

    GC for revisions - add first smoke test
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1328403 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-mk/src/test/java/org/apache/jackrabbit/mk/store/DefaultRevisionStoreTest.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.mk.store;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.jackrabbit.mk.api.MicroKernel;
+import org.apache.jackrabbit.mk.blobs.MemoryBlobStore;
+import org.apache.jackrabbit.mk.core.MicroKernelImpl;
+import org.apache.jackrabbit.mk.core.Repository;
+import org.apache.jackrabbit.mk.persistence.InMemPersistence;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests verifying the inner workings of <code>DefaultRevisionStore</code>.
+ */
+public class DefaultRevisionStoreTest {
+
+    private DefaultRevisionStore rs;
+    private MicroKernel mk;
+    
+    @Before
+    public void setup() throws Exception {
+        rs = new DefaultRevisionStore(new InMemPersistence()) {
+            @Override
+            protected void doMark() throws Exception {
+                markCommit(getHeadCommit());
+            }
+        };
+        rs.initialize();
+        mk = new MicroKernelImpl(new Repository(rs, new MemoryBlobStore()));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (mk != null) {
+            mk.dispose();
+        }
+    }
+    
+    @Test
+    public void testGC() {
+        mk.commit("/", "+\"a\" : { \"c\":{}, \"d\":{} }", mk.getHeadRevision(), null);
+        mk.commit("/", "+\"b\" : {}", mk.getHeadRevision(), null);
+        mk.commit("/b", "+\"e\" : {}", mk.getHeadRevision(), null);
+        mk.commit("/a/c", "+\"f\" : {}", mk.getHeadRevision(), null);
+        
+        String headRevision = mk.getHeadRevision();
+        String contents = mk.getNodes("/", headRevision);
+        
+        rs.gc();
+        
+        assertEquals(headRevision, mk.getHeadRevision());
+        assertEquals(contents, mk.getNodes("/", headRevision));
+    }
+}
