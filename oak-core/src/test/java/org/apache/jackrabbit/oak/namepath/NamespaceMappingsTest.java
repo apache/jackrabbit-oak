ab48d9e64f4002b0ae0d9c72b43ceffc1c5700e8
Michael Dürig <mduerig@apache.org>
2012-04-22 10:34:48 +0000

    OAK-61: Implement JCR path handling - rename NameSpaceRegistryTest to NamespaceMappingsTest to match class under test - improve auth generated name
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1328838 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/namepath/NamespaceMappingsTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/namepath/NamespaceMappingsTest.java
@@ -22,7 +22,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
-public class NameSpaceRegistryTest {
+public class NamespaceMappingsTest {
 
     @Test
     public void nameSpaceRegistry() {
