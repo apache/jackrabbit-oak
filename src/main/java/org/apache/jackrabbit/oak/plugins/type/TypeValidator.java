e936b9ed847168bc967599ddbc1773c175c5a825
Jukka Zitting <jukka@apache.org>
2012-04-23 16:12:59 +0000

    OAK-68: Extension point for commit validation
    Add basic JCR name and type validators as example plugins
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1329310 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/jackrabbit/oak/plugins/type/TypeValidator.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.plugins.type;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.apache.jackrabbit.oak.api.CommitFailedException;
+import org.apache.jackrabbit.oak.api.PropertyState;
+import org.apache.jackrabbit.oak.api.Scalar;
+import org.apache.jackrabbit.oak.kernel.NodeState;
+import org.apache.jackrabbit.oak.kernel.NodeValidator;
+
+class TypeValidator implements NodeValidator {
+
+    private final Set<String> types;
+
+    public TypeValidator(Set<String> types) {
+        this.types = types;
+    }
+
+    private void checkTypeExists(PropertyState after)
+            throws CommitFailedException {
+        Iterable<Scalar> scalars = Collections.emptyList();
+        if ("jcr:primaryType".equals(after.getName())) {
+            scalars = Collections.singletonList(after.getScalar());
+        } else if ("jcr:mixinTypes".equals(after.getName())) {
+            scalars = after.getArray();
+        }
+        for (Scalar scalar : scalars) {
+            String value = scalar.getString();
+            if (!types.contains(value)) {
+                throw new CommitFailedException("Unknown node type: " + value);
+            }
+        }
+    }
+
+    //-------------------------------------------------------< NodeValidator >
+
+    @Override
+    public void propertyAdded(PropertyState after)
+            throws CommitFailedException {
+        checkTypeExists(after);
+        // TODO: validate added property
+    }
+
+    @Override
+    public void propertyChanged(PropertyState before, PropertyState after)
+            throws CommitFailedException {
+        checkTypeExists(after);
+        // TODO: validate changed property
+    }
+
+    @Override
+    public void propertyDeleted(PropertyState before)
+            throws CommitFailedException {
+        // TODO: validate removed property
+    }
+
+    @Override
+    public NodeValidator childNodeAdded(String name, NodeState after)
+            throws CommitFailedException {
+        // TODO: validate added child node
+        // TODO: get the type for validating the child contents
+        return this;
+    }
+
+    @Override
+    public NodeValidator childNodeChanged(
+            String name, NodeState before, NodeState after)
+            throws CommitFailedException {
+        // TODO: validate changed child node
+        // TODO: get the type to validating the child contents
+        return this;
+    }
+
+    @Override
+    public NodeValidator childNodeDeleted(String name, NodeState before) {
+        // TODO: validate removed child node
+        return null;
+    }
+
+}
