375890041e547b4bcd05ffbf82acd1f1a34790bc
Jukka Zitting <jukka@apache.org>
2012-04-23 16:12:59 +0000

    OAK-68: Extension point for commit validation
    Add basic JCR name and type validators as example plugins
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1329310 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/name/NameValidator.java
@@ -0,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.plugins.name;
+
+import java.util.Set;
+
+import org.apache.jackrabbit.oak.api.CommitFailedException;
+import org.apache.jackrabbit.oak.api.PropertyState;
+import org.apache.jackrabbit.oak.kernel.NodeState;
+import org.apache.jackrabbit.oak.kernel.NodeValidator;
+
+class NameValidator implements NodeValidator {
+
+    private final Set<String> prefixes;
+
+    public NameValidator(Set<String> prefixes) {
+        this.prefixes = prefixes;
+    }
+
+    protected void checkValidName(String name) throws CommitFailedException {
+        String prefix = null;
+        String local = name;
+
+        int colon = name.indexOf(':');
+        if (colon != -1) {
+            prefix = name.substring(0, colon);
+            local = name.substring(colon + 1);
+        }
+
+        if (!(prefix == null || prefixes.contains(prefix))
+                || !isValidLocalName(local)) {
+            throw new CommitFailedException(
+                    "Self or parent paths (. or ..) are not valid as names");
+        }
+    }
+
+    private boolean isValidLocalName(String local) {
+        if (".".equals(local) || "..".equals(local)) {
+            return false;
+        }
+
+        for (int i = 0; i < local.length(); i++) {
+            char ch = local.charAt(i);
+            if ("/:[]|*".indexOf(ch) != -1) { // TODO: XMLChar check
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    //-------------------------------------------------------< NodeValidator >
+
+    @Override
+    public void propertyAdded(PropertyState after)
+            throws CommitFailedException {
+        checkValidName(after.getName());
+    }
+
+    @Override
+    public void propertyChanged(PropertyState before, PropertyState after) {
+        // do nothing
+    }
+
+    @Override
+    public void propertyDeleted(PropertyState before) {
+        // do nothing
+    }
+
+    @Override
+    public NodeValidator childNodeAdded(String name, NodeState after)
+            throws CommitFailedException {
+        checkValidName(name);
+        return this;
+    }
+
+    @Override
+    public NodeValidator childNodeChanged(
+            String name, NodeState before, NodeState after)
+            throws CommitFailedException {
+        return this;
+    }
+
+    @Override
+    public NodeValidator childNodeDeleted(String name, NodeState before) {
+        // do nothing
+        return null;
+    }
+
+}
---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/name/NameValidatorProvider.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.plugins.name;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.jackrabbit.oak.api.PropertyState;
+import org.apache.jackrabbit.oak.kernel.NodeState;
+import org.apache.jackrabbit.oak.kernel.NodeValidator;
+import org.apache.jackrabbit.oak.kernel.NodeValidatorProvider;
+
+public class NameValidatorProvider implements NodeValidatorProvider {
+
+    @Override
+    public NodeValidator getRootValidator(NodeState before, NodeState after) {
+        Set<String> prefixes = new HashSet<String>();
+
+        // Default JCR prefixes are always available
+        prefixes.add("jcr");
+        prefixes.add("nt");
+        prefixes.add("mix");
+
+        // Jackrabbit 2.x prefixes are always available
+        prefixes.add("rep");
+
+        // Find any extra prefixes from /jcr:system/jcr:namespaceRegistry
+        NodeState system = after.getChildNode("jcr:system");
+        if (system != null) {
+            NodeState registry = system.getChildNode("jcr:namespaces");
+            if (registry != null) {
+                for (PropertyState property : registry.getProperties()) {
+                    prefixes.add(property.getName());
+                }
+            }
+        }
+
+        return new NameValidator(prefixes);
+    }
+
+}
