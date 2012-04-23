4bb5c1af58ea23a04a3322e8bc690b7bcec535e3
Jukka Zitting <jukka@apache.org>
2012-04-23 12:37:59 +0000

    OAK-68: Extension point for commit validation
    First draft of such an extension point
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1329213 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/kernel/NodeValidatorProvider.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.kernel;
+
+import org.apache.jackrabbit.oak.kernel.NodeState;
+
+/**
+ * Extension point for plugging in different kinds of validation rules
+ * for content changes.
+ */
+public interface NodeValidatorProvider {
+
+    /**
+     * Returns a validator for checking the changes between the given
+     * two root states.
+     *
+     * @param before original root state
+     * @param after  modified root state
+     * @return validator for checking the modifications
+     */
+    NodeValidator getRootValidator(NodeState before, NodeState after);
+
+}
