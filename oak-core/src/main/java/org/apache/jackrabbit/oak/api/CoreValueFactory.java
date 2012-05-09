02916bbc49752407597a440dab8598620d776aa6
Angela Schreiber <angela@apache.org>
2012-05-09 10:41:07 +0000

    javadoc
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1336065 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/api/CoreValueFactory.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/api/CoreValueFactory.java
@@ -21,21 +21,76 @@ import java.io.InputStream;
 import java.math.BigDecimal;
 
 /**
- * CoreValueFactory... TODO: add description and javadoc for methods
+ * {@code CoreValueFactory} defines methods to create new instances of
+ * {@code CoreValue}.
  */
 public interface CoreValueFactory {
 
+    /**
+     * Creates a new value of type {@link javax.jcr.PropertyType#STRING}.
+     *
+     * @param value A non-null {@code String} defining the new value.
+     * @return a new value instance.
+     * @throws IllegalArgumentException if the specified {@code String}
+     * is {@code null}.
+     */
     CoreValue createValue(String value);
 
+    /**
+     * Creates a new value of type {@link javax.jcr.PropertyType#DOUBLE}.
+     *
+     * @param value The {@code double} that defines the new value.
+     * @return a new value instance.
+     */
     CoreValue createValue(double value);
 
+    /**
+     * Creates a new value of type {@link javax.jcr.PropertyType#DOUBLE}.
+     *
+     * @param value The {@code double} that defines the new value.
+     * @return a new value instance.
+     */
     CoreValue createValue(long value);
 
+    /**
+     * Creates a new value of type {@link javax.jcr.PropertyType#BOOLEAN}.
+     *
+     * @param value The {@code boolean} that defines the new value.
+     * @return a new value instance.
+     */
     CoreValue createValue(boolean value);
 
+    /**
+     * Creates a new value of type {@link javax.jcr.PropertyType#DECIMAL}.
+     *
+     * @param value A non-null {@code BigDecimal} that defines the new value.
+     * @return a new value instance.
+     * @throws IllegalArgumentException if the specified {@code BigDecimal} is {@code null}.
+     */
     CoreValue createValue(BigDecimal value);
 
+    /**
+     * Creates a new value of type {@link javax.jcr.PropertyType#BINARY}.
+     *
+     * @param value A non-null {@code InputStream} that defines the new value.
+     * @return a new value instance.
+     * @throws IllegalArgumentException if the specified {@code InputStream} is {@code null}.
+     * @throws IOException If an error occurs while processing the stream.
+     * @throws //TODO define exceptions (currently impl. throws MikrokernelException)
+     */
     CoreValue createValue(InputStream value) throws IOException;
 
+    /**
+     * Creates a new value of the specified type.
+     *
+     * @param value A non-null {@code String} that defines the new value.
+     * @param type The desired target type of the new value.
+     * @return a new value instance.
+     * @throws IllegalArgumentException if the specified {@code value} is {@code null}
+     * or if the given type is not supported.
+     * @throws NumberFormatException If the specified {@code type} requires
+     * conversion to any of the number types and the conversion fails.
+     * @throws //TODO define and consolidate exceptions
+     */
     CoreValue createValue(String value, int type);
 }
