8b1215c685f0a89d22af801430c04f293ce961c6
Angela Schreiber <angela@apache.org>
2012-05-09 12:37:04 +0000

    OAK-91 - Implement Authentication Support (WIP)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1336142 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/jackrabbit/oak/security/authentication/LoginModuleImpl.java
+++ src/main/java/org/apache/jackrabbit/oak/security/authentication/LoginModuleImpl.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.oak.spi.security.authentication;
+package org.apache.jackrabbit.oak.security.authentication;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
