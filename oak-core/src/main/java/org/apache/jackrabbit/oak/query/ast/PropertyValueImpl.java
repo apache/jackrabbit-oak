7a7105c202c5abc02e0528ffff881ef983564891
Thomas Mueller <thomasm@apache.org>
2012-05-30 13:26:00 +0000

    OAK-77 Consolidate Utilities - use PathUtils.elements instead of PathUtils.split
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1344246 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/query/ast/PropertyValueImpl.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/query/ast/PropertyValueImpl.java
@@ -60,12 +60,10 @@ public class PropertyValueImpl extends DynamicOperandImpl {
         }
         // TODO really support relative properties?
         NodeImpl n = selector.currentNode();
-        String[] elements = PathUtils.split(propertyName);
-        for (int i = 0; i < elements.length - 1; i++) {
+        for (String p : PathUtils.elements(PathUtils.getParentPath(propertyName))) {
             if (n == null) {
                 return null;
             }
-            String p = elements[i];
             if (!n.exists(p)) {
                 return null;
             }
