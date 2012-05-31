d378e719a79bf96304d89c35f95bd60cc8e5c5f3
Angela Schreiber <angela@apache.org>
2012-05-31 12:17:02 +0000

    OAK-50 : Implement User Management (WIP)
    - tests + test setup that allows to extend from AbstractJcrTest
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1344670 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/OakRepositoryStub.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.jcr;
+
+import org.apache.jackrabbit.test.NotExecutableException;
+import org.apache.jackrabbit.test.RepositoryStub;
+
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.UnsupportedRepositoryOperationException;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Properties;
+
+public class OakRepositoryStub extends RepositoryStub {
+
+    private final Repository repository;
+
+    /**
+     * Constructor as required by the JCR TCK.
+     * 
+     * @param settings repository settings
+     * @throws javax.jcr.RepositoryException If an error occurs.
+     * @throws java.io.IOException
+     */
+    public OakRepositoryStub(Properties settings) throws RepositoryException, IOException {
+        super(settings);
+        repository = new RepositoryImpl();
+        Session session = repository.login(superuser);
+        try {
+            TestContentLoader loader = new TestContentLoader();
+            loader.loadTestContent(session);
+        } finally {
+            session.logout();
+        }
+    }
+
+    /**
+     * Returns the configured repository instance.
+     * 
+     * @return the configured repository instance.
+     */
+    @Override
+    public synchronized Repository getRepository() {
+        return repository;
+    }
+
+    @Override
+    public Principal getKnownPrincipal(Session session) throws RepositoryException {
+        throw new UnsupportedRepositoryOperationException();
+    }
+
+    private static Principal UNKNOWN_PRINCIPAL = new Principal() {
+        @Override
+        public String getName() {
+            return "an_unknown_user";
+        }
+    };
+
+    @Override
+    public Principal getUnknownPrincipal(Session session) throws RepositoryException, NotExecutableException {
+        return UNKNOWN_PRINCIPAL;
+    }
+
+}
