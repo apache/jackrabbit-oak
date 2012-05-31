3515036a28c6e45bc09e2c5eeae4279730275af1
Michael Dürig <mduerig@apache.org>
2012-05-31 08:18:26 +0000

    OAK-68 - Extension point for commit validation Add composite variants for CommitHook, Validator and ValidatorProvider
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1344592 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/jackrabbit/oak/spi/commit/CompositeCommitHook.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.spi.commit;
+
+import org.apache.jackrabbit.oak.api.CommitFailedException;
+import org.apache.jackrabbit.oak.spi.state.NodeState;
+import org.apache.jackrabbit.oak.spi.state.NodeStore;
+
+import java.util.List;
+
+public class CompositeCommitHook implements CommitHook {
+    private final List<CommitHook> hooks;
+
+    public CompositeCommitHook(List<CommitHook> hooks) {
+        this.hooks = hooks;
+    }
+
+    @Override
+    public NodeState beforeCommit(NodeStore store, NodeState before, NodeState after)
+            throws CommitFailedException {
+
+        NodeState oldState = before;
+        NodeState newState = after;
+        for (CommitHook hook : hooks) {
+            NodeState newOldState = newState;
+            newState = hook.beforeCommit(store, oldState, newState);
+            oldState = newOldState;
+        }
+
+        return newState;
+    }
+
+    @Override
+    public void afterCommit(NodeStore store, NodeState before, NodeState after) {
+        for (CommitHook hook : hooks) {
+            hook.afterCommit(store, before, after);
+        }
+    }
+}
---
+++ src/main/java/org/apache/jackrabbit/oak/spi/commit/CompositeValidator.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.spi.commit;
+
+import org.apache.jackrabbit.oak.api.CommitFailedException;
+import org.apache.jackrabbit.oak.api.PropertyState;
+import org.apache.jackrabbit.oak.spi.state.NodeState;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompositeValidator implements Validator {
+    private final List<Validator> validators;
+
+    public CompositeValidator(List<Validator> validators) {
+        this.validators = validators;
+    }
+
+    @Override
+    public void propertyAdded(PropertyState after) throws CommitFailedException {
+        for (Validator validator : validators) {
+            validator.propertyAdded(after);
+        }
+    }
+
+    @Override
+    public void propertyChanged(PropertyState before, PropertyState after)
+            throws CommitFailedException {
+
+        for (Validator validator : validators) {
+            validator.propertyChanged(before, after);
+        }
+    }
+
+    @Override
+    public void propertyDeleted(PropertyState before) throws CommitFailedException {
+        for (Validator validator : validators) {
+            validator.propertyDeleted(before);
+        }
+    }
+
+    @Override
+    public Validator childNodeAdded(String name, NodeState after) throws CommitFailedException {
+        List<Validator> childValidators = new ArrayList<Validator>(validators.size());
+        for (Validator validator : validators) {
+            childValidators.add(
+                    validator.childNodeAdded(name, after));
+        }
+        return new CompositeValidator(childValidators);
+    }
+
+    @Override
+    public Validator childNodeChanged(String name, NodeState before, NodeState after) throws CommitFailedException {
+        List<Validator> childValidators = new ArrayList<Validator>(validators.size());
+        for (Validator validator : validators) {
+            childValidators.add(
+                    validator.childNodeChanged(name, before, after));
+        }
+        return new CompositeValidator(childValidators);
+    }
+
+    @Override
+    public void childNodeDeleted(String name, NodeState before) throws CommitFailedException {
+        for (Validator validator : validators) {
+            validator.childNodeDeleted(name, before);
+        }
+    }
+}
