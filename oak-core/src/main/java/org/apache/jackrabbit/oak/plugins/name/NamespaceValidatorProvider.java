edab9b412b988977cc04fbd3387e31d60ee452ce
Jukka Zitting <jukka@apache.org>
2012-06-01 08:13:30 +0000

    OAK-125: Improved namespace registry
    Add a validator for the /jcr:system/jcr:namespaces tree.
    Restore the Validator return value in childNodeDeleted as it makes NamespaceValidator easier to write.
    Added some utility classes and tests for the validator infrastructure.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1345006 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/name/NamespaceValidatorProvider.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.plugins.name;
+
+import java.util.Map;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.jackrabbit.oak.spi.commit.SubtreeValidator;
+import org.apache.jackrabbit.oak.spi.commit.Validator;
+import org.apache.jackrabbit.oak.spi.commit.ValidatorProvider;
+import org.apache.jackrabbit.oak.spi.state.NodeState;
+
+/**
+ * Validator service that checks that all node and property names as well
+ * as any name values are syntactically valid and that any namespace prefixes
+ * are properly registered.
+ */
+@Component
+@Service(ValidatorProvider.class)
+public class NamespaceValidatorProvider implements ValidatorProvider {
+
+    @Override
+    public Validator getRootValidator(NodeState before, NodeState after) {
+        Map<String, String> map = Namespaces.getNamespaceMap(before);
+        Validator validator =
+                new NamespaceValidator(map);
+        return new SubtreeValidator(validator, "jcr:system", "jcr:namespaces");
+    }
+
+}
