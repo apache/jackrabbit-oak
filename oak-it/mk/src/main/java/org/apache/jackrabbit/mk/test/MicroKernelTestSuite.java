a2c0d856d8478293a2ad53a53fb70995c4d6033f
Stefan Guggisberg <stefan@apache.org>
2012-06-20 08:07:27 +0000

    fixed broken build
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1351977 13f79535-47bb-0310-9956-ffa450edef68

--- oak-it/mk/src/main/java/org/apache/jackrabbit/mk/test/MicroKernelTestSuite.java
+++ oak-it/mk/src/main/java/org/apache/jackrabbit/mk/test/MicroKernelTestSuite.java
@@ -21,7 +21,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    MicroKernelIT.class,
+    MicroKernelIT.class
     // TODO remove 
     // DataStoreIT.class
 })
