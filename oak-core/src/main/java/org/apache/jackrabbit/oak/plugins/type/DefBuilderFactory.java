ce49c94f8caaa0ca676ed425ff96b9401282e2a6
Jukka Zitting <jukka@apache.org>
2012-07-19 18:16:10 +0000

    OAK-66: JCR Node Type Management
    Add type templates based on the CND reader code from jcr-commons
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1363452 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/type/DefBuilderFactory.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.plugins.type;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Value;
+import javax.jcr.nodetype.NodeType;
+import javax.jcr.nodetype.NodeTypeTemplate;
+
+import org.apache.jackrabbit.commons.cnd.DefinitionBuilderFactory;
+
+class DefBuilderFactory extends
+        DefinitionBuilderFactory<NodeTypeTemplate, Map<String, String>> {
+
+    private Map<String, String> namespaces = new HashMap<String, String>();
+
+    @Override
+    public NodeTypeTemplateImpl newNodeTypeDefinitionBuilder()
+            throws RepositoryException {
+        return new NodeTypeTemplateImpl() {
+            @Override
+            protected NodeType getNodeType(String name) {
+                // TODO Auto-generated method stub
+                return null;
+            }
+            @Override
+            protected Value createValue(String value)
+                    throws RepositoryException {
+                // TODO Auto-generated method stub
+                return null;
+            }
+        };
+    }
+
+    @Override
+    public Map<String, String> getNamespaceMapping() {
+        return namespaces;
+    }
+
+    @Override
+    public void setNamespaceMapping(Map<String, String> namespaces) {
+        this.namespaces = namespaces;
+    }
+
+    @Override
+    public void setNamespace(String prefix, String uri) {
+        namespaces.put(prefix, uri);
+    }
+
+}
