93c453345e536e97ec593779e1fbe7f32c304e30
Jukka Zitting <jukka@apache.org>
2012-07-19 10:04:40 +0000

    OAK-193: TODO class for partially implemented features
    First draft of the proposed TODO class.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1363277 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/util/TODO.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.util;
+
+import javax.jcr.UnsupportedRepositoryOperationException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Helper class for identifying partially implemented features and
+ * controlling their runtime behavior.
+ *
+ * @see <a href="https://issues.apache.org/jira/browse/OAK-193">OAK-193</a>
+ */
+public class TODO {
+
+    private static final String mode = System.getProperty("todo", "strict");
+
+    private static final boolean strict = "strict".equals(mode);
+
+    private static final boolean log = "log".equals(mode);
+
+    public static TODO unimplemented() {
+        return new TODO("unimplemented");
+    }
+
+    private final UnsupportedOperationException exception;
+
+    private final Logger logger;
+
+    private final String message;
+
+    private TODO(String message) {
+        this.exception = new UnsupportedOperationException(message);
+        StackTraceElement[] trace = exception.getStackTrace();
+        if (trace != null && trace.length > 2) {
+            String className = trace[2].getClassName();
+            String methodName = trace[2].getMethodName();
+            this.logger = LoggerFactory.getLogger(className);
+            this.message =
+                    "TODO: " + className + "." + methodName + "() - " + message;
+        } else {
+            this.logger = LoggerFactory.getLogger(TODO.class);
+            this.message = "TODO: " + message;
+        }
+    }
+
+    public void doNothing() throws UnsupportedRepositoryOperationException {
+        if (strict) {
+            throw new UnsupportedRepositoryOperationException(
+                    message, exception);
+        } else if (log) {
+            logger.warn(message, exception);
+        }
+    }
+}
