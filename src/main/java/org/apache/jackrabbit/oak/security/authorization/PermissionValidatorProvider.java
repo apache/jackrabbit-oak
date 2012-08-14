e94069c236deaac7dbcacc879d2388c3a0039c7a
Angela Schreiber <angela@apache.org>
2012-08-14 08:46:18 +0000

    minor improvement
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1372782 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/jackrabbit/oak/security/authorization/PermissionValidatorProvider.java
+++ src/main/java/org/apache/jackrabbit/oak/security/authorization/PermissionValidatorProvider.java
@@ -19,9 +19,7 @@ package org.apache.jackrabbit.oak.security.authorization;
 import javax.annotation.Nonnull;
 
 import org.apache.jackrabbit.oak.api.ContentSession;
-import org.apache.jackrabbit.oak.api.CoreValueFactory;
 import org.apache.jackrabbit.oak.core.ReadOnlyTree;
-import org.apache.jackrabbit.oak.namepath.NamePathMapper;
 import org.apache.jackrabbit.oak.spi.commit.Validator;
 import org.apache.jackrabbit.oak.spi.commit.ValidatorProvider;
 import org.apache.jackrabbit.oak.spi.security.authorization.AccessControlContext;
@@ -40,11 +38,8 @@ public class PermissionValidatorProvider implements ValidatorProvider {
     @Nonnull
     @Override
     public Validator getRootValidator(NodeState before, NodeState after) {
-        NamePathMapper mapper = new NamePathMapper.Default();
-        CoreValueFactory vf = contentSession.getCoreValueFactory();
-
-        NodeUtil rootBefore = new NodeUtil(vf, mapper, new ReadOnlyTree(before));
-        NodeUtil rootAfter = new NodeUtil(vf, mapper, new ReadOnlyTree(after));
+        NodeUtil rootBefore = new NodeUtil(new ReadOnlyTree(before), contentSession);
+        NodeUtil rootAfter = new NodeUtil(new ReadOnlyTree(after), contentSession);
         return new PermissionValidator(acContext.getPermissions(), rootBefore, rootAfter);
     }
 }
