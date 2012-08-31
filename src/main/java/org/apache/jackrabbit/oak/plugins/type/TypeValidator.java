da454ea905494609bca8304e02cda88f4342a0db
Michael Dürig <mduerig@apache.org>
2012-08-31 10:04:58 +0000

    OAK-66: JCR Node Type Management node type validation
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1379392 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/jackrabbit/oak/plugins/type/TypeValidator.java
+++ src/main/java/org/apache/jackrabbit/oak/plugins/type/TypeValidator.java
@@ -17,15 +17,18 @@
 package org.apache.jackrabbit.oak.plugins.type;
 
 import javax.jcr.RepositoryException;
+import javax.jcr.nodetype.ConstraintViolationException;
 import javax.jcr.nodetype.NodeType;
 import javax.jcr.nodetype.NodeTypeManager;
 
-import org.apache.jackrabbit.JcrConstants;
 import org.apache.jackrabbit.oak.api.CommitFailedException;
 import org.apache.jackrabbit.oak.api.CoreValue;
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.spi.commit.Validator;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
+
+import static org.apache.jackrabbit.JcrConstants.JCR_MIXINTYPES;
+import static org.apache.jackrabbit.JcrConstants.JCR_PRIMARYTYPE;
 
 class TypeValidator implements Validator {
     private final NodeTypeManager ntm;
@@ -34,56 +37,33 @@ class TypeValidator implements Validator {
         this.ntm = ntm;
     }
 
-    private void checkTypeExists(PropertyState after) throws CommitFailedException {
-        if (JcrConstants.JCR_PRIMARYTYPE.equals(after.getName()) || JcrConstants.JCR_MIXINTYPES.equals(after.getName())) {
-            try {
-                for (CoreValue cv : after.getValues()) {
-                    String ntName = cv.getString();
-                    NodeType nt = ntm.getNodeType(ntName);
-                    if (nt.isAbstract()) {
-                        throw new CommitFailedException("Can't create node with abstract type: " + ntName);
-                    }
-                }
-            }
-            catch (RepositoryException e) {
-                throw new CommitFailedException(e);
-            }
-        }
-    }
-
     //-------------------------------------------------------< NodeValidator >
 
     @Override
-    public void propertyAdded(PropertyState after)
-            throws CommitFailedException {
-        checkTypeExists(after);
+    public void propertyAdded(PropertyState after) throws CommitFailedException {
+        validateType(after);
         // TODO: validate added property
     }
 
     @Override
-    public void propertyChanged(PropertyState before, PropertyState after)
-            throws CommitFailedException {
-        checkTypeExists(after);
+    public void propertyChanged(PropertyState before, PropertyState after) throws CommitFailedException {
+        validateType(after);
     }
 
     @Override
-    public void propertyDeleted(PropertyState before)
-            throws CommitFailedException {
+    public void propertyDeleted(PropertyState before) throws CommitFailedException {
         // TODO: validate removed property
     }
 
     @Override
-    public Validator childNodeAdded(String name, NodeState after)
-            throws CommitFailedException {
+    public Validator childNodeAdded(String name, NodeState after) throws CommitFailedException {
         // TODO: validate added child node
         // TODO: get the type for validating the child contents
         return this;
     }
 
     @Override
-    public Validator childNodeChanged(
-            String name, NodeState before, NodeState after)
-            throws CommitFailedException {
+    public Validator childNodeChanged(String name, NodeState before, NodeState after) throws CommitFailedException {
         // TODO: validate changed child node
         // TODO: get the type to validating the child contents
         return this;
@@ -93,6 +73,35 @@ class TypeValidator implements Validator {
     public Validator childNodeDeleted(String name, NodeState before) {
         // TODO: validate removed child node
         return null;
+    }
+
+    private void validateType(PropertyState after) throws CommitFailedException {
+        boolean primaryType = JCR_PRIMARYTYPE.equals(after.getName());
+        boolean mixinType = JCR_MIXINTYPES.equals(after.getName());
+        if (primaryType || mixinType) {
+            try {
+                for (CoreValue cv : after.getValues()) {
+                    String ntName = cv.getString();
+                    NodeType nt = ntm.getNodeType(ntName);
+                    if (nt.isAbstract()) {
+                        throwConstraintViolationException("Can't create node with abstract type: " + ntName);
+                    }
+                    if (primaryType && nt.isMixin()) {
+                        throwConstraintViolationException("Can't assign mixin for primary type: " + ntName);
+                    }
+                    if (mixinType && !nt.isMixin()) {
+                        throwConstraintViolationException("Can't assign primary type for mixin: " + ntName);
+                    }
+                }
+            }
+            catch (RepositoryException e) {
+                throw new CommitFailedException(e);
+            }
+        }
+    }
+
+    private static void throwConstraintViolationException(String message) throws CommitFailedException {
+        throw new CommitFailedException(new ConstraintViolationException(message));
     }
 
 }
