c447cd74dcc8299d56723fcbebdc05a0a6c07c9a
Thomas Mueller <thomasm@apache.org>
2012-09-12 09:29:38 +0000

    OAK-288 QueryTests should use the NodeStore apis - move JsopUtils to test folder
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1383866 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/jackrabbit/oak/query/JsopUtil.java
+++ src/test/java/org/apache/jackrabbit/oak/query/JsopUtil.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.oak.util;
+package org.apache.jackrabbit.oak.query;
 
 import java.util.ArrayList;
 import java.util.Iterator;
