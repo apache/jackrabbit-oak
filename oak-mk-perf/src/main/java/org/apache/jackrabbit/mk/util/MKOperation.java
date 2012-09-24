ee4e6528aafdbae59c24835c206e388e1b9797ba
Tudor Rogoz <rogoz@adobe.com>
2012-09-24 11:06:57 +0200

    Add new performance tests for microkernel

--- oak-mk-perf/src/main/java/org/apache/jackrabbit/mk/util/MKOperation.java
+++ oak-mk-perf/src/main/java/org/apache/jackrabbit/mk/util/MKOperation.java
@@ -1,9 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.jackrabbit.mk.util;
 
 import org.apache.jackrabbit.mk.api.MicroKernel;
 
 /**
- * Useful methods for building/commiting nodes.
+ * Useful methods for building/committing nodes.
  * @author rogoz
  *
  */
@@ -20,7 +36,7 @@ public class MKOperation {
 	 *            Name of the node.
 	 * @return
 	 */
-	public static String commitEmptyNode(MicroKernel mk, String parentNode,
+	private static String commitEmptyNode(MicroKernel mk, String parentNode,
 			String name) {
 
 		return mk.commit(parentNode, "+\"" + name + "\" : {} \n", null, "");
