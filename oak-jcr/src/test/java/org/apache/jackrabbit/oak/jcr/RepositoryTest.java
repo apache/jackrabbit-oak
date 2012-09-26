4efa9d30a50078c655d5eb548b25a7a2903e47ea
Angela Schreiber <angela@apache.org>
2012-09-26 10:31:31 +0000

    OAK-343 (adding test-case, currently ignored)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1390383 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/RepositoryTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/RepositoryTest.java
@@ -187,6 +187,22 @@ public class RepositoryTest extends AbstractRepositoryTest {
         assertTrue(node.isSame(node2));
     }
 
+    @Ignore //OAK-343
+    @Test
+    public void getNodeByUUID() throws RepositoryException {
+        Node node = getNode("/foo").addNode("boo");
+        node.addMixin(JcrConstants.MIX_REFERENCEABLE);
+
+        assertTrue(node.isNodeType(JcrConstants.MIX_REFERENCEABLE));
+        String uuid = node.getUUID();
+        assertNotNull(uuid);
+        assertEquals(uuid, node.getIdentifier());
+
+        Node nAgain = node.getSession().getNodeByUUID(uuid);
+        assertTrue(nAgain.isSame(node));
+        assertTrue(nAgain.isSame(node.getSession().getNodeByIdentifier(uuid)));
+    }
+
     @Test
     public void getNodeFromNode() throws RepositoryException {
         Node root = getNode("/");
