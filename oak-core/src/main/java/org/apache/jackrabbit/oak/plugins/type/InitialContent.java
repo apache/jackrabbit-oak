15cd199e69bcbbd79b08e4696b310e4eef8989bb
Angela Schreiber <angela@apache.org>
2012-10-02 08:00:03 +0000

    add FIXME again
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1392780 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/type/InitialContent.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/type/InitialContent.java
@@ -44,6 +44,8 @@ public class InitialContent extends DefaultMicroKernelTracker {
             mk.commit("/", "^\"jcr:primaryType\":\"nam:rep:root\"" +
                     "+\"jcr:system\":{" +
                     "\"jcr:primaryType\"    :\"nam:rep:system\"," +
+                    // FIXME: user-mgt related unique properties (rep:authorizableId, rep:principalName) are implementation detail and not generic for repo
+                    // FIXME: rep:principalName only needs to be unique if defined with user/group nodes -> add defining nt-info to uniqueness constraint otherwise ac-editing will fail.
                     "\":unique\"            :{\"jcr:uuid\":{},\"rep:authorizableId\":{},\"rep:principalName\":{}}," +
                     "\"jcr:versionStorage\" :{\"jcr:primaryType\":\"nam:rep:versionStorage\"}," +
                     "\"jcr:nodeTypes\"      :{\"jcr:primaryType\":\"nam:rep:nodeTypes\"}," +
