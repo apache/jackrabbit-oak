e392bec88cea19d050b1a239d9c3a07a4938938c
Michael Duerig <mduerig@apache.org>
2012-10-04 15:46:34 +0000

    Reverting changes from revisions 1394092, 1394101, 1394112 which cause QueryTest and NodeDefTests to fail
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1394119 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/query/QueryTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/query/QueryTest.java
@@ -39,7 +39,6 @@ import org.apache.jackrabbit.oak.plugins.index.old.PropertyIndexer;
 import org.apache.jackrabbit.oak.spi.commit.CompositeHook;
 import org.apache.jackrabbit.oak.spi.query.CompositeQueryIndexProvider;
 import org.apache.jackrabbit.oak.spi.query.QueryIndexProvider;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -84,7 +83,6 @@ public class QueryTest extends AbstractQueryTest {
     }
 
     @Test
-    @Ignore
     public void sql2_measure() throws Exception {
         test("sql2_measure.txt");
     }
