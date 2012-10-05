1132f73e11fafdd0f3fd31d0175f3ba943e89bb3
Michael Dürig <mduerig@apache.org>
2012-10-05 09:32:33 +0000

    OAK-350: Unify PropertyState and CoreValue - Replace CoreValue.getString with PropertyState.getValue(Type)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1394421 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/kernel/JsopDiff.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/kernel/JsopDiff.java
@@ -16,18 +16,29 @@
  */
 package org.apache.jackrabbit.oak.kernel;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import javax.jcr.PropertyType;
 
 import org.apache.jackrabbit.mk.api.MicroKernel;
 import org.apache.jackrabbit.mk.json.JsopBuilder;
-import org.apache.jackrabbit.oak.api.CoreValue;
+import org.apache.jackrabbit.oak.api.Blob;
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.commons.PathUtils;
 import org.apache.jackrabbit.oak.spi.state.ChildNodeEntry;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
 import org.apache.jackrabbit.oak.spi.state.NodeStateDiff;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.apache.jackrabbit.oak.api.Type.BINARIES;
+import static org.apache.jackrabbit.oak.api.Type.BOOLEANS;
+import static org.apache.jackrabbit.oak.api.Type.LONGS;
+import static org.apache.jackrabbit.oak.api.Type.STRINGS;
 
 class JsopDiff implements NodeStateDiff {
+    private static final Logger log = LoggerFactory.getLogger(JsopDiff.class);
 
     private final MicroKernel kernel;
 
@@ -120,34 +131,51 @@ class JsopDiff implements NodeStateDiff {
     private void toJson(PropertyState propertyState, JsopBuilder jsop) {
         if (propertyState.isArray()) {
             jsop.array();
-            for (CoreValue value : propertyState.getValues()) {
-                toJson(value, jsop);
-            }
+            toJsonValue(propertyState, jsop);
             jsop.endArray();
         } else {
-            toJson(propertyState.getValue(), jsop);
+            toJsonValue(propertyState, jsop);
         }
     }
 
-    private void toJson(CoreValue value, JsopBuilder jsop) {
-        int type = value.getType();
-        if (type == PropertyType.BOOLEAN) {
-            jsop.value(value.getBoolean());
-        } else if (type == PropertyType.LONG) {
-            jsop.value(value.getLong());
-        } else {
-            String string;
-            if (type == PropertyType.BINARY
-                    && !(value instanceof BinaryValue)) {
-                string = kernel.write(value.getNewStream());
-            } else {
-                string = value.getString();
-            }
-            if (type != PropertyType.STRING
-                    || CoreValueMapper.startsWithHint(string)) {
-                string = CoreValueMapper.getHintForType(type) + ':' + string;
-            }
-            jsop.value(string);
+    private void toJsonValue(PropertyState property, JsopBuilder jsop) {
+        int type = property.getType().tag();
+        switch (type) {
+            case PropertyType.BOOLEAN:
+                for (boolean value : property.getValue(BOOLEANS)) {
+                    jsop.value(value);
+                }
+                break;
+            case PropertyType.LONG:
+                for (long value : property.getValue(LONGS)) {
+                    jsop.value(value);
+                }
+                break;
+            case PropertyType.BINARY:
+                for (Blob value : property.getValue(BINARIES)) {
+                    InputStream is = value.getNewStream();
+                    String binId = CoreValueMapper.getHintForType(type) + ':' + kernel.write(is);
+                    close(is);
+                    jsop.value(binId);
+                }
+                break;
+            default:
+                for (String value : property.getValue(STRINGS)) {
+                    if (PropertyType.STRING != type || CoreValueMapper.startsWithHint(value)) {
+                        value = CoreValueMapper.getHintForType(type) + ':' + value;
+                    }
+                    jsop.value(value);
+                }
+                break;
+        }
+    }
+
+    private static void close(InputStream stream) {
+        try {
+            stream.close();
+        }
+        catch (IOException e) {
+            log.warn("Error closing stream", e);
         }
     }
 }
