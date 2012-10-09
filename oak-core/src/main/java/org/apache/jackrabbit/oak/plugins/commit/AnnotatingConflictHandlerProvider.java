d902defd93ba4ff1632d97207df5d8c23c94f6e2
Michael Duerig <mduerig@apache.org>
2012-10-09 13:43:59 +0000

    OAK-350: Unify PropertyState and CoreValue - Remove CoreValueFactory member from AnnotatingConflictHandler
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1396036 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/commit/AnnotatingConflictHandlerProvider.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/commit/AnnotatingConflictHandlerProvider.java
@@ -1,12 +1,11 @@
 package org.apache.jackrabbit.oak.plugins.commit;
 
-import org.apache.jackrabbit.oak.api.CoreValueFactory;
 import org.apache.jackrabbit.oak.spi.commit.ConflictHandler;
 import org.apache.jackrabbit.oak.spi.commit.ConflictHandlerProvider;
 
 public class AnnotatingConflictHandlerProvider implements ConflictHandlerProvider {
     @Override
-    public ConflictHandler getConflictHandler(CoreValueFactory valueFactory) {
-        return new AnnotatingConflictHandler(valueFactory);
+    public ConflictHandler getConflictHandler() {
+        return new AnnotatingConflictHandler();
     }
 }
--- oak-core/src/main/java/org/apache/jackrabbit/oak/spi/commit/ConflictHandlerProvider.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/commit/ConflictHandlerProvider.java
@@ -1,7 +1,5 @@
 package org.apache.jackrabbit.oak.spi.commit;
 
-import org.apache.jackrabbit.oak.api.CoreValueFactory;
-
 public interface ConflictHandlerProvider {
-    ConflictHandler getConflictHandler(CoreValueFactory valueFactory);
+    ConflictHandler getConflictHandler();
 }
