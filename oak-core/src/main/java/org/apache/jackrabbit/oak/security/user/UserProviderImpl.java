fbfed36125e956f33fbd5d2ade968d28cf7b0925
Mete Atamel <meteatamel@adobe.com>
2012-10-09 12:25:29 +0200

    Oak changes needed for Granite quickstart
    These are the Oak changes needed in order to get Granite quickstart in
    quickstart/oak branch work.

--- oak-core/src/main/java/org/apache/jackrabbit/oak/security/user/UserProviderImpl.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/security/user/UserProviderImpl.java
@@ -207,10 +207,10 @@ class UserProviderImpl extends AuthorizableBaseProvider implements UserProvider 
         // index as well.
         try {
             CoreValue bindValue = valueFactory.createValue(principal.getName());
-            StringBuilder stmt = new StringBuilder();
-            stmt.append("SELECT * FROM [").append(UserConstants.NT_REP_AUTHORIZABLE).append(']');
-            stmt.append("WHERE [").append(UserConstants.REP_PRINCIPAL_NAME).append("] = $principalName");
-
+//            StringBuilder stmt = new StringBuilder();
+//            stmt.append("SELECT * FROM [").append(UserConstants.NT_REP_AUTHORIZABLE).append(']');
+//            stmt.append("WHERE [").append(UserConstants.REP_PRINCIPAL_NAME).append("] = $principalName");
+            String stmt = "SELECT * FROM [nt:base] WHERE ([jcr:primaryType] = \"rep:Group\" OR [jcr:primaryType] = \"rep:User\") AND [rep:principalName] = $principalName";
             Result result = root.getQueryEngine().executeQuery(stmt.toString(),
                     Query.JCR_SQL2, 1, 0,
                     Collections.singletonMap("principalName", bindValue),
