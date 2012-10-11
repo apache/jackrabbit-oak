8eb03fed041426b1774d95c6b51ffd8d7d8b219d
Angela Schreiber <angela@apache.org>
2012-10-11 13:31:19 +0000

    OAK-50 : Implement User Management (WIP)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1397041 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/user/UserTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/user/UserTest.java
@@ -16,6 +16,8 @@
  */
 package org.apache.jackrabbit.oak.jcr.security.user;
 
+import java.security.Principal;
+import javax.jcr.Credentials;
 import javax.jcr.LoginException;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
@@ -25,6 +27,7 @@ import javax.jcr.UnsupportedRepositoryOperationException;
 import org.apache.jackrabbit.api.JackrabbitSession;
 import org.apache.jackrabbit.api.security.user.Authorizable;
 import org.apache.jackrabbit.api.security.user.User;
+import org.apache.jackrabbit.oak.spi.security.user.UserConstants;
 import org.apache.jackrabbit.test.NotExecutableException;
 import org.junit.Test;
 
@@ -73,122 +76,170 @@ public class UserTest extends AbstractUserTest {
     }
 
     @Test
-    public void testChangePassword() throws RepositoryException, NotExecutableException {
-        String oldPw = getHelper().getProperty("javax.jcr.tck.superuser.pwd");
-        if (oldPw == null) {
-            // missing property
-            throw new NotExecutableException();
-        }
-
-        User user = getTestUser(superuser);
+    public void testChangePasswordNull() throws RepositoryException, NotExecutableException {
+        // invalid 'null' pw string
         try {
-            user.changePassword("pw");
+            user.changePassword(null);
             superuser.save();
-
-            // make sure the user can login with the new pw
-            Session s = getHelper().getRepository().login(new SimpleCredentials(user.getID(), "pw".toCharArray()));
-            s.logout();
-        } finally {
-            user.changePassword(oldPw);
-            superuser.save();
+            fail("invalid pw null");
+        } catch (Exception e) {
+            // success
         }
     }
 
     @Test
-    public void testChangePassword2() throws RepositoryException, NotExecutableException {
-        String oldPw = getHelper().getProperty("javax.jcr.tck.superuser.pwd");
-        if (oldPw == null) {
-            // missing property
-            throw new NotExecutableException();
-        }
-
-        User user = getTestUser(superuser);
+    public void testChangePassword() throws RepositoryException, NotExecutableException {
         try {
-            user.changePassword("pw");
+            String hash = getNode(user, superuser).getProperty(UserConstants.REP_PASSWORD).getString();
+
+            user.changePassword("changed");
             superuser.save();
 
-            Session s = getHelper().getRepository().login(new SimpleCredentials(user.getID(), oldPw.toCharArray()));
-            s.logout();
-            fail("superuser pw has changed. login must fail.");
-        } catch (LoginException e) {
+            assertFalse(hash.equals(getNode(user, superuser).getProperty(UserConstants.REP_PASSWORD).getString()));
+        } catch (Exception e) {
             // success
-        } finally {
-            user.changePassword(oldPw);
+        }
+    }
+
+    @Test
+    public void testChangePasswordWithInvalidOldPassword() throws RepositoryException, NotExecutableException {
+        try {
+            user.changePassword("changed", "wrongOldPw");
             superuser.save();
+            fail("old password didn't match -> changePassword(String,String) should fail.");
+        } catch (RepositoryException e) {
+            // success.
         }
     }
 
     @Test
     public void testChangePasswordWithOldPassword() throws RepositoryException, NotExecutableException {
-        String oldPw = getHelper().getProperty("javax.jcr.tck.superuser.pwd");
-        if (oldPw == null) {
-            // missing property
-            throw new NotExecutableException();
-        }
-
-        User user = getTestUser(superuser);
         try {
-            try {
-                user.changePassword("pw", "wrongOldPw");
-                superuser.save();
-                fail("old password didn't match -> changePassword(String,String) should fail.");
-            } catch (RepositoryException e) {
-                // success.
-            }
+            String hash = getNode(user, superuser).getProperty(UserConstants.REP_PASSWORD).getString();
 
-            user.changePassword("pw", oldPw);
+            user.changePassword("changed", testPw);
+            superuser.save();
+
+            assertFalse(hash.equals(getNode(user, superuser).getProperty(UserConstants.REP_PASSWORD).getString()));
+        } finally {
+            user.changePassword(testPw);
+            superuser.save();
+        }
+    }
+
+      @Test
+    public void testLoginAfterChangePassword() throws RepositoryException {
+        user.changePassword("changed");
+        superuser.save();
+
+        // make sure the user can login with the new pw
+        Session s = getHelper().getRepository().login(new SimpleCredentials(user.getID(), "changed".toCharArray()));
+        s.logout();
+    }
+
+    @Test
+    public void testLoginAfterChangePassword2() throws RepositoryException, NotExecutableException {
+        try {
+
+            user.changePassword("changed", testPw);
             superuser.save();
 
             // make sure the user can login with the new pw
-            Session s = getHelper().getRepository().login(new SimpleCredentials(user.getID(), "pw".toCharArray()));
+            Session s = getHelper().getRepository().login(new SimpleCredentials(user.getID(), "changed".toCharArray()));
             s.logout();
         } finally {
-            user.changePassword(oldPw);
+            user.changePassword(testPw);
             superuser.save();
         }
     }
 
     @Test
-    public void testChangePasswordWithOldPassword2() throws RepositoryException, NotExecutableException {
-        String oldPw = getHelper().getProperty("javax.jcr.tck.superuser.pwd");
-        if (oldPw == null) {
-            // missing property
-            throw new NotExecutableException();
-        }
-
-        User user = getTestUser(superuser);
+    public void testLoginWithOldPassword() throws RepositoryException, NotExecutableException {
         try {
-            user.changePassword("pw", oldPw);
+            user.changePassword("changed");
             superuser.save();
 
-            Session s = getHelper().getRepository().login(new SimpleCredentials(user.getID(), oldPw.toCharArray()));
+            Session s = getHelper().getRepository().login(new SimpleCredentials(user.getID(), testPw.toCharArray()));
+            s.logout();
+            fail("user pw has changed. login must fail.");
+        } catch (LoginException e) {
+            // success
+        }
+    }
+
+    @Test
+    public void testLoginWithOldPassword2() throws RepositoryException, NotExecutableException {
+        try {
+            user.changePassword("changed", testPw);
+            superuser.save();
+
+            Session s = getHelper().getRepository().login(new SimpleCredentials(user.getID(), testPw.toCharArray()));
             s.logout();
             fail("superuser pw has changed. login must fail.");
         } catch (LoginException e) {
             // success
         } finally {
-            user.changePassword(oldPw);
+            user.changePassword(testPw);
             superuser.save();
         }
     }
 
     @Test
-    public void testDisable() throws Exception {
+    public void testEnabledByDefault() throws Exception {
         // by default a user isn't disabled
         assertFalse(user.isDisabled());
         assertNull(user.getDisabledReason());
+    }
 
-        // disable user
+    @Test
+    public void testDisable() throws Exception {
         String reason = "readonly user is disabled!";
         user.disable(reason);
         superuser.save();
         assertTrue(user.isDisabled());
         assertEquals(reason, user.getDisabledReason());
+    }
+
+    @Test
+    public void testAccessDisabledUser() throws Exception {
+        user.disable("readonly user is disabled!");
+        superuser.save();
 
         // user must still be retrievable from user manager
         assertNotNull(getUserManager(superuser).getAuthorizable(user.getID()));
         // ... and from principal manager as well
         assertTrue(((JackrabbitSession) superuser).getPrincipalManager().hasPrincipal(user.getPrincipal().getName()));
+    }
+
+    @Test
+    public void testAccessPrincipalOfDisabledUser()  throws Exception {
+        user.disable("readonly user is disabled!");
+        superuser.save();
+
+        Principal principal = user.getPrincipal();
+        assertTrue(((JackrabbitSession) superuser).getPrincipalManager().hasPrincipal(principal.getName()));
+        assertEquals(principal, ((JackrabbitSession) superuser).getPrincipalManager().getPrincipal(principal.getName()));
+    }
+
+    @Test
+    public void testEnableUser() throws Exception {
+        user.disable("readonly user is disabled!");
+        superuser.save();
+
+        // enable user again
+        user.disable(null);
+        superuser.save();
+        assertFalse(user.isDisabled());
+        assertNull(user.getDisabledReason());
+
+        // -> login must succeed again
+        getHelper().getRepository().login(new SimpleCredentials(user.getID(), "pw".toCharArray())).logout();
+    }
+
+    @Test
+    public void testLoginDisabledUser() throws Exception {
+        user.disable("readonly user is disabled!");
+        superuser.save();
 
         // -> login must fail
         try {
@@ -198,6 +249,12 @@ public class UserTest extends AbstractUserTest {
         } catch (LoginException e) {
             // success
         }
+    }
+
+    @Test
+    public void testImpersonateDisabledUser() throws Exception {
+        user.disable("readonly user is disabled!");
+        superuser.save();
 
         // -> impersonating this user must fail
         try {
@@ -207,13 +264,27 @@ public class UserTest extends AbstractUserTest {
         } catch (LoginException e) {
             // success
         }
+    }
 
-        // enable user again
-        user.disable(null);
-        superuser.save();
-        assertFalse(user.isDisabled());
+    public void testUserGetCredentials() throws RepositoryException, NotExecutableException {
+        try {
+            Credentials creds = user.getCredentials();
+            fail("getCredentials is not yet implemented");
+        } catch (UnsupportedRepositoryOperationException e) {
+            // expected
+        }
+    }
 
-        // -> login must succeed again
-        getHelper().getRepository().login(new SimpleCredentials(user.getID(), "pw".toCharArray())).logout();
+    public void testLoginWithGetCredentials() throws RepositoryException, NotExecutableException {
+        try {
+            Credentials creds = user.getCredentials();
+            Session s = getHelper().getRepository().login(creds);
+            s.logout();
+            fail("Login using credentials exposed on user must fail.");
+        } catch (UnsupportedRepositoryOperationException e) {
+            throw new NotExecutableException(e.getMessage());
+        } catch (LoginException e) {
+            // success
+        }
     }
 }
