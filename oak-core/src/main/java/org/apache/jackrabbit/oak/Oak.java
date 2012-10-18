fbe416a62e730ea70b6942278b894288d9bb1476
Jukka Zitting <jukka@apache.org>
2012-10-18 16:12:52 +0000

    OAK-352: Oak builder for simplified repository construction
    Add methods for simplifying test cases that need only a single ContentSession or Root
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1399718 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/Oak.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/Oak.java
@@ -18,11 +18,15 @@ package org.apache.jackrabbit.oak;
 
 import java.util.List;
 import javax.annotation.Nonnull;
+import javax.jcr.NoSuchWorkspaceException;
+import javax.security.auth.login.LoginException;
 
 import com.google.common.collect.Lists;
 import org.apache.jackrabbit.mk.api.MicroKernel;
 import org.apache.jackrabbit.mk.core.MicroKernelImpl;
 import org.apache.jackrabbit.oak.api.ContentRepository;
+import org.apache.jackrabbit.oak.api.ContentSession;
+import org.apache.jackrabbit.oak.api.Root;
 import org.apache.jackrabbit.oak.core.ContentRepositoryImpl;
 import org.apache.jackrabbit.oak.kernel.KernelNodeStore;
 import org.apache.jackrabbit.oak.plugins.memory.MemoryNodeStore;
@@ -35,6 +39,7 @@ import org.apache.jackrabbit.oak.spi.commit.Validator;
 import org.apache.jackrabbit.oak.spi.commit.ValidatorProvider;
 import org.apache.jackrabbit.oak.spi.query.CompositeQueryIndexProvider;
 import org.apache.jackrabbit.oak.spi.query.QueryIndexProvider;
+import org.apache.jackrabbit.oak.spi.security.OpenSecurityProvider;
 import org.apache.jackrabbit.oak.spi.security.SecurityProvider;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
 import org.apache.jackrabbit.oak.spi.state.NodeStore;
@@ -61,7 +66,7 @@ public class Oak {
 
     private final List<ValidatorProvider> validatorProviders = Lists.newArrayList();
 
-    private SecurityProvider securityProvider;
+    private SecurityProvider securityProvider = new OpenSecurityProvider();
 
     private ConflictHandler conflictHandler;
 
@@ -178,6 +183,54 @@ public class Oak {
                 createCommitHook(),
                 conflictHandler,
                 securityProvider);
+    }
+
+    /**
+     * Creates a content repository with the given configuration
+     * and logs in to the default workspace with no credentials,
+     * returning the resulting content session.
+     * <p>
+     * This method exists mostly as a convenience for one-off tests,
+     * as there's no way to create other sessions for accessing the
+     * same repository.
+     * <p>
+     * There is typically no need to explicitly close the returned
+     * session unless the repository has explicitly been configured
+     * to reserve some resources until all sessions have been closed.
+     * The repository will be garbage collected once the session is no
+     * longer used.
+     *
+     * @return content session
+     */
+    public ContentSession createContentSession() {
+        try {
+            return createContentRepository().login(null, null);
+        } catch (NoSuchWorkspaceException e) {
+            throw new IllegalStateException("Default workspace not found", e);
+        } catch (LoginException e) {
+            throw new IllegalStateException("Anonymous login not allowed", e);
+        }
+    }
+
+    /**
+     * Creates a content repository with the given configuration
+     * and returns a {@link Root} instance after logging in to the
+     * default workspace with no credentials.
+     * <p>
+     * This method exists mostly as a convenience for one-off tests, as
+     * the returned root is the only way to access the session or the
+     * repository.
+     * <p>
+     * Note that since there is no way to close the underlying content
+     * session, this method should only be used when no components that
+     * require sessions to be closed have been configured. The repository
+     * and the session will be garbage collected once the root is no longer
+     * used.
+     *
+     * @return root instance
+     */
+    public Root createRoot() {
+        return createContentSession().getLatestRoot();
     }
 
     private CommitHook createCommitHook() {
