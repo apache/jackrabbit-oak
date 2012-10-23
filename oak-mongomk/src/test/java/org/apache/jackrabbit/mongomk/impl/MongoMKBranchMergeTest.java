4c8af02ce4b344064cf05fee3ad0d64a0614026c
Mete Atamel <meteatamel@adobe.com>
2012-10-23 17:30:46 +0200

    Branch/merge tests

--- oak-mongomk/src/test/java/org/apache/jackrabbit/mongomk/impl/MongoMKBranchMergeTest.java
+++ oak-mongomk/src/test/java/org/apache/jackrabbit/mongomk/impl/MongoMKBranchMergeTest.java
@@ -15,17 +15,17 @@ public class MongoMKBranchMergeTest extends BaseMongoMicroKernelTest {
 
     @Test
     public void oneBranchOneChild() {
-        // Initial trunk.
-        mk.commit("", "+\"/trunk\":{}", null, "");
-        mk.commit("", "+\"/trunk/child1\":{}", null, "");
+        // Commit to initial trunk.
+        commitNodes(null, "/trunk", "/trunk/child1");
 
         // Check initial trunk children exist on trunk.
         assertNodesExist(null, "/trunk", "/trunk/child1");
 
         // Branch.
         String branchRev = mk.branch(null);
-        branchRev = mk.commit("", "+\"/branch1\":{}", branchRev, "");
-        branchRev = mk.commit("", "+\"/branch1/child1\":{}", branchRev, "");
+
+        // Commit to branch.
+        branchRev = commitNodes(branchRev, "/branch1", "/branch1/child1");
 
         // Check initial trunk children still exist in branch.
         assertNodesExist(branchRev, "/trunk", "/trunk/child1");
@@ -37,65 +37,91 @@ public class MongoMKBranchMergeTest extends BaseMongoMicroKernelTest {
         assertNodesNotExist(null, "/branch1", "/branch1/child1");
 
         // Add another child on trunk.
-        mk.commit("", "+\"/trunk/child2\":{}", null, "");
+        commitNodes(null, "/trunk/child2");
 
-        // Check that the new child exists in trunk but not on branch.
+        // Check that the new child exists in trunk but not on branch still.
         assertNodesExist(null, "/trunk/child2");
         assertNodesNotExist(branchRev, "/trunk/child2");
     }
 
     @Test
     public void twoBranchesOneChild() {
+        // Commit to initial trunk.
+        commitNodes(null, "/trunk", "/trunk/child1");
+
+        // Check initial trunk children exist on trunk.
+        assertNodesExist(null, "/trunk", "/trunk/child1");
+
+        // Branches.
         String branchRev1 = mk.branch(null);
         String branchRev2 = mk.branch(null);
 
-        branchRev1 = mk.commit("", "+\"/branch1\":{}", branchRev1, "");
-        branchRev1 = mk.commit("", "+\"/branch1/child1\":{}", branchRev1, "");
+        // Commit to branches.
+        branchRev1 = commitNodes(branchRev1, "/branch1", "/branch1/child1");
+        branchRev2 = commitNodes(branchRev2, "/branch2", "/branch2/child2");
 
-        branchRev2 = mk.commit("", "+\"/branch2\":{}", branchRev2, "");
-        branchRev2 = mk.commit("", "+\"/branch2/child2\":{}", branchRev2, "");
+        // Check initial trunk children still exist in branches.
+        assertNodesExist(branchRev1, "/trunk", "/trunk/child1");
+        assertNodesExist(branchRev2, "/trunk", "/trunk/child1");
 
+        // Check that branch children also exist in branches.
+        assertTrue(mk.nodeExists("/branch1/child1", branchRev1));
+        assertFalse(mk.nodeExists("/branch2/child2", branchRev1));
+        assertTrue(mk.nodeExists("/branch2/child2", branchRev2));
+        assertFalse(mk.nodeExists("/branch1/child1", branchRev2));
+
+        // But branch children does not exist in trunk.
         assertFalse(mk.nodeExists("/branch1/child1", null));
         assertFalse(mk.nodeExists("/branch2/child2", null));
 
-        assertTrue(mk.nodeExists("/branch1/child1", branchRev1));
-        assertFalse(mk.nodeExists("/branch2/child2", branchRev1));
+        // Add another child on trunk.
+        commitNodes(null, "/trunk/child2");
 
-        assertFalse(mk.nodeExists("/branch1/child1", branchRev2));
-        assertTrue(mk.nodeExists("/branch2/child2", branchRev2));
-
-        mk.commit("", "+\"/trunk\":{}", null, "");
-
-        assertTrue(mk.nodeExists("/trunk", null));
-        assertFalse(mk.nodeExists("/trunk", branchRev1));
-        assertFalse(mk.nodeExists("/trunk", branchRev2));
+        // Check that the new child exists in trunk but not on branches still.
+        assertNodesExist(null, "/trunk/child2");
+        assertNodesNotExist(branchRev1, "/trunk/child2");
+        assertNodesNotExist(branchRev2, "/trunk/child2");
     }
 
     @Test
     public void twoBranchesAndMerge() {
+        // Commit to initial trunk.
+        commitNodes(null, "/trunk", "/trunk/child1");
+
+        // Check initial trunk children exist on trunk.
+        assertNodesExist(null, "/trunk", "/trunk/child1");
+
+        // Branches.
         String branchRev1 = mk.branch(null);
         String branchRev2 = mk.branch(null);
 
-        branchRev1 = mk.commit("", "+\"/branch1\":{}", branchRev1, "");
-        branchRev2 = mk.commit("", "+\"/branch2\":{}", branchRev2, "");
-        branchRev1 = mk.commit("", "+\"/branch1/child1\":{}", branchRev1, "");
-        branchRev2 = mk.commit("", "+\"/branch2/child2\":{}", branchRev2, "");
+        // Commit to branches.
+        branchRev1 = commitNodes(branchRev1, "/branch1", "/branch1/child1");
+        branchRev2 = commitNodes(branchRev2, "/branch2", "/branch2/child2");
 
+        // Check initial trunk children still exist in branches.
+        assertNodesExist(branchRev1, "/trunk", "/trunk/child1");
+        assertNodesExist(branchRev2, "/trunk", "/trunk/child1");
+
+        // Check that branch children also exist in branches.
+        assertTrue(mk.nodeExists("/branch1/child1", branchRev1));
+        assertFalse(mk.nodeExists("/branch2/child2", branchRev1));
+        assertTrue(mk.nodeExists("/branch2/child2", branchRev2));
+        assertFalse(mk.nodeExists("/branch1/child1", branchRev2));
+
+        // But branch children does not exist in trunk.
         assertFalse(mk.nodeExists("/branch1/child1", null));
         assertFalse(mk.nodeExists("/branch2/child2", null));
 
-        assertTrue(mk.nodeExists("/branch1/child1", branchRev1));
-        assertFalse(mk.nodeExists("/branch2/child2", branchRev1));
+        // Add another child on trunk.
+        commitNodes(null, "/trunk/child2");
 
-        assertFalse(mk.nodeExists("/branch1/child1", branchRev2));
-        assertTrue(mk.nodeExists("/branch2/child2", branchRev2));
+        // Check that the new child exists in trunk but not on branches still.
+        assertNodesExist(null, "/trunk/child2");
+        assertNodesNotExist(branchRev1, "/trunk/child2");
+        assertNodesNotExist(branchRev2, "/trunk/child2");
 
-        mk.commit("", "+\"/trunk\":{}", null, "");
-
-        assertTrue(mk.nodeExists("/trunk", null));
-        assertFalse(mk.nodeExists("/trunk", branchRev1));
-        assertFalse(mk.nodeExists("/trunk", branchRev2));
-
+        // Merge branch1 and do the checks.
         mk.merge(branchRev1, "");
         assertTrue(mk.nodeExists("/trunk", null));
         assertTrue(mk.nodeExists("/branch1", null));
@@ -103,6 +129,7 @@ public class MongoMKBranchMergeTest extends BaseMongoMicroKernelTest {
         assertFalse(mk.nodeExists("/branch2", null));
         assertFalse(mk.nodeExists("/branch2/child2", null));
 
+        // Merge branch2 and do the checks.
         mk.merge(branchRev2, "");
         assertTrue(mk.nodeExists("/trunk", null));
         assertTrue(mk.nodeExists("/branch1", null));
@@ -136,6 +163,14 @@ public class MongoMKBranchMergeTest extends BaseMongoMicroKernelTest {
     }
 
     // FIXME - Add more complicated branch/merge tests with properties, conflicts etc.
+
+    private String commitNodes(String rev, String...nodes) {
+        String newRev = rev;
+        for (String node : nodes) {
+            newRev = mk.commit("", "+\"" + node + "\":{}", rev, "");
+        }
+        return newRev;
+    }
 
     private void assertNodesExist(String revision, String...paths) {
         doAssertNodes(true, revision, paths);
