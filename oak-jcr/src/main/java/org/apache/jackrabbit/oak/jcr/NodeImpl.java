e4eecd5e0688425348b4c720e73f79b062c8d900
Angela Schreiber <angela@apache.org>
2012-10-25 10:36:21 +0000

    OAK-395 : Inconsistency in Node#setProperty in case of null value
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1402087 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/NodeImpl.java
+++ oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/NodeImpl.java
@@ -302,19 +302,20 @@ public class NodeImpl extends ItemImpl<NodeDelegate> implements Node {
      * @see Node#setProperty(String, javax.jcr.Value, int)
      */
     @Override
-    @CheckForNull
+    @Nonnull
     public Property setProperty(final String jcrName, final Value value, final int type)
             throws RepositoryException {
         checkStatus();
 
-        return sessionDelegate.perform(new SessionOperation<PropertyImpl>() {
+        return sessionDelegate.perform(new SessionOperation<Property>() {
             @Override
-            public PropertyImpl perform() throws RepositoryException {
-                String oakName = sessionDelegate.getOakPathOrThrow(jcrName);
+            public Property perform() throws RepositoryException {
                 if (value == null) {
-                    dlg.removeProperty(oakName);
-                    return null;
+                    Property property = getProperty(jcrName);
+                    property.remove();
+                    return property;
                 } else {
+                    String oakName = sessionDelegate.getOakPathOrThrow(jcrName);
                     int targetType = getTargetType(value, type);
                     Value targetValue =
                             ValueHelper.convert(value, targetType, getValueFactory());
