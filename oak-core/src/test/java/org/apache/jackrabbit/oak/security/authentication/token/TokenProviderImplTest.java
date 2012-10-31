fdc7e8efdbf9e04955edbe9abf80cadb273b448f
Angela Schreiber <angela@apache.org>
2012-10-31 18:38:26 +0000

    OAK-91 - Implement Authentication Support (WIP)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1404287 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/security/authentication/token/TokenProviderImplTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/security/authentication/token/TokenProviderImplTest.java
@@ -90,7 +90,6 @@ public class TokenProviderImplTest extends AbstractSecurityTest {
         }
     }
 
-
     @Test
     public void testDoCreateToken() throws Exception {
         assertFalse(tokenProvider.doCreateToken(new GuestCredentials()));
@@ -149,50 +148,6 @@ public class TokenProviderImplTest extends AbstractSecurityTest {
     public void testCreateTokenFromUserId() throws Exception {
         TokenInfo info = tokenProvider.createToken(userId, Collections.<String, Object>emptyMap());
         assertTokenInfo(info, userId);
-    }
-
-    @Test
-    public void testCreateTokenWithAttributes() throws Exception {
-        Map<String, String> reserved = new HashMap<String, String>();
-        reserved.put(".token", "value");
-        reserved.put(".token.key", "value");
-        reserved.put(".token.exp", "value");
-
-        Map<String, String> privateAttributes = new HashMap<String, String>();
-        privateAttributes.put(".token_exp", "value");
-        privateAttributes.put(".tokenTest", "value");
-        privateAttributes.put(".token_something", "value");
-
-        Map<String, String> publicAttributes = new HashMap<String, String>();
-        publicAttributes.put("any", "value");
-        publicAttributes.put("another", "value");
-
-        Map<String, String> attributes = new HashMap<String, String>();
-        attributes.putAll(reserved);
-        attributes.putAll(publicAttributes);
-        attributes.putAll(privateAttributes);
-
-        TokenInfo info = tokenProvider.createToken(userId, attributes);
-        assertTokenInfo(info, userId);
-
-        Map<String,String> pubAttr = info.getPublicAttributes();
-        assertEquals(publicAttributes.size(), pubAttr.size());
-        for (String key : publicAttributes.keySet()) {
-            assertTrue(pubAttr.containsKey(key));
-            assertEquals(publicAttributes.get(key), pubAttr.get(key));
-        }
-
-        Map<String,String> privAttr = info.getPrivateAttributes();
-        assertEquals(privateAttributes.size(), privAttr.size());
-        for (String key : privateAttributes.keySet()) {
-            assertTrue(privAttr.containsKey(key));
-            assertEquals(privateAttributes.get(key), privAttr.get(key));
-        }
-
-        for (String key : reserved.keySet()) {
-            assertFalse(privAttr.containsKey(key));
-            assertFalse(pubAttr.containsKey(key));
-        }
     }
 
     @Test
@@ -312,6 +267,7 @@ public class TokenProviderImplTest extends AbstractSecurityTest {
         assertFalse(info.isExpired(loginTime));
         assertTrue(tokenProvider.resetTokenExpiration(info, loginTime));
 
+        // TODO: check again if expired tokens should be resettable
         long expiredTime = new Date().getTime() + 7200000;
         assertTrue(info.isExpired(expiredTime));
         assertTrue(tokenProvider.resetTokenExpiration(info, expiredTime));
