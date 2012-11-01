97581344f8724795d29d8376f919c04cbdd16b18
Mete Atamel <meteatamel@adobe.com>
2012-11-01 13:58:21 +0100

    Converted NodeExistsCommandMongoTest into MongoMKNodeExistsTest
    This way the whole call chain can be tested

---
+++ oak-mongomk/src/test/java/org/apache/jackrabbit/mongomk/impl/MongoMKGetJournalTest.java
@@ -0,0 +1,10 @@
+package org.apache.jackrabbit.mongomk.impl;
+
+import org.apache.jackrabbit.mongomk.BaseMongoMicroKernelTest;
+
+/**
+ * Tests for {@code MongoMicroKernel#getJournal(String, String, String)}
+ */
+public class MongoMKGetJournalTest extends BaseMongoMicroKernelTest {
+
+}
