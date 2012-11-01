f39c468b19aa1aee9697ce8c251b39a34bdf4469
Angela Schreiber <angela@apache.org>
2012-11-01 14:58:21 +0000

    OAK-127 : Support for XML imports (work in progress)
    - initial draft for pluggable interfaces and classes used for the import of protected items
    - code is a slightly modified oak-adaption of the code we used in jackrabbit and which turned
       to be feasible to cover all cases we had so far. the distinction between methods for session
       and workspace import however looks to be redundant in the light of the oak api (-> simplified).
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1404623 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/xml/PropInfo.java
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.spi.xml;
+
+import javax.jcr.PropertyType;
+import javax.jcr.RepositoryException;
+import javax.jcr.Value;
+import javax.jcr.nodetype.PropertyDefinition;
+
+import org.apache.jackrabbit.oak.api.Type;
+import org.apache.jackrabbit.oak.namepath.NamePathMapper;
+
+/**
+ * Information about a property being imported. This class is used
+ * by the XML import handlers to pass the parsed property information
+ * to the import process.
+ * <p>
+ * In addition to carrying the actual property data, instances of this
+ * class also know how to apply that data when imported either to a
+ * {@link javax.jcr.Node} instance through a session or directly to a
+ * {@link org.apache.jackrabbit.oak.api.Tree} instance on the oak level.
+ */
+public class PropInfo {
+
+    /**
+     * String of the property being imported.
+     */
+    private final String name;
+
+    /**
+     * Type of the property being imported.
+     */
+    private final Type type;
+
+    /**
+     * Value(s) of the property being imported.
+     */
+    private final TextValue[] values;
+
+    /**
+     * Hint indicating whether the property is multi- or single-value
+     */
+    public enum MultipleStatus { UNKNOWN, SINGLE, MULTIPLE }
+    private MultipleStatus multipleStatus;
+
+    /**
+     * Creates a property information instance.
+     *
+     * @param name name of the property being imported
+     * @param type type of the property being imported
+     * @param values value(s) of the property being imported
+     */
+    public PropInfo(String name, Type type, TextValue[] values) {
+        this.name = name;
+        this.type = type;
+        this.values = values;
+        multipleStatus = (values.length == 1) ? MultipleStatus.UNKNOWN : MultipleStatus.MULTIPLE;
+    }
+
+    /**
+     * Creates a property information instance.
+     *
+     * @param name name of the property being imported
+     * @param type type of the property being imported
+     * @param values value(s) of the property being imported
+     * @param multipleStatus Hint indicating whether the property is
+     *                       multi- or single-value
+     */
+    public PropInfo(String name, Type type, TextValue[] values,
+                    MultipleStatus multipleStatus) {
+        this.name = name;
+        this.type = type;
+        this.values = values;
+        this.multipleStatus = multipleStatus;
+    }
+
+    /**
+     * Disposes all values contained in this property.
+     */
+    public void dispose() {
+        for (TextValue value : values) {
+            value.dispose();
+        }
+    }
+
+    public Type getTargetType(PropertyDefinition def) {
+        int target = def.getRequiredType();
+        if (target != PropertyType.UNDEFINED) {
+            return Type.fromTag(target, def.isMultiple());
+        } else if (type.tag() != PropertyType.UNDEFINED) {
+            return type;
+        } else {
+            return Type.STRING;
+        }
+    }
+
+    public String getString() {
+        return name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public TextValue[] getTextValues() {
+        return values;
+    }
+
+    public Value[] getValues(Type targetType, NamePathMapper namePathMapper) throws RepositoryException {
+        Value[] va = new Value[values.length];
+        for (int i = 0; i < values.length; i++) {
+            va[i] = values[i].getValue(targetType, namePathMapper);
+        }
+        return va;
+    }
+}
