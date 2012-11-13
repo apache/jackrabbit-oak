71c6ef57de3cc76600e5276513551c5e3f478ac8
Michael Dürig <mduerig@apache.org>
2012-11-13 12:20:07 +0000

    OAK-428: Binaries might get removed by garbage collection while still referenced change prefix for binaries from bin: to :blobId:
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1408693 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/jackrabbit/oak/kernel/TypeCodesTest.java
+++ src/test/java/org/apache/jackrabbit/oak/kernel/TypeCodesTest.java
@@ -28,7 +28,7 @@ public class TypeCodesTest {
     @Test
     public void testEncode() {
         assertEquals("str:foo", TypeCodes.encode(PropertyType.STRING, "foo"));
-        assertEquals("bin:", TypeCodes.encode(PropertyType.BINARY, ""));
+        assertEquals(":blobId:", TypeCodes.encode(PropertyType.BINARY, ""));
         assertEquals("und:foo", TypeCodes.encode(PropertyType.UNDEFINED, "foo"));
     }
 
