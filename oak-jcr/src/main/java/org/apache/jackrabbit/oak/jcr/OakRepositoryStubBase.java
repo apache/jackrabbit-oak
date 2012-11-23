fcf2ce304bf5074b4c75c11c2e56466dc79b48fd
Alexandru Parvulescu <alexparvulescu@apache.org>
2012-11-23 15:13:40 +0000

    OAK-456 RepositoryInitializer for the lucene global index used in tests  - moved OakRepositoryStub to main so that it can be used from other projects as well
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1412909 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/OakRepositoryStubBase.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.jcr;
+
+import java.security.Principal;
+import java.util.Properties;
+import java.util.concurrent.Executors;
+import javax.jcr.Credentials;
+import javax.jcr.GuestCredentials;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.UnsupportedRepositoryOperationException;
+import javax.security.auth.login.Configuration;
+
+import org.apache.jackrabbit.mk.core.MicroKernelImpl;
+import org.apache.jackrabbit.oak.security.OakConfiguration;
+import org.apache.jackrabbit.test.NotExecutableException;
+import org.apache.jackrabbit.test.RepositoryStub;
+
+public class OakRepositoryStubBase extends RepositoryStub {
+
+    private final Repository repository;
+
+    /**
+     * Constructor as required by the JCR TCK.
+     * 
+     * @param settings repository settings
+     * @throws javax.jcr.RepositoryException If an error occurs.
+     */
+    public OakRepositoryStubBase(Properties settings) throws RepositoryException {
+        super(settings);
+
+        // TODO: OAK-17. workaround for missing test configuration
+        Configuration.setConfiguration(new OakConfiguration());
+
+        String dir = "target/mk-tck-" + System.currentTimeMillis();
+        Jcr jcr = new Jcr(new MicroKernelImpl(dir));
+        jcr.with(Executors.newScheduledThreadPool(1));
+        preCreateRepository(jcr);
+        repository = jcr.createRepository();
+    }
+
+    protected void preCreateRepository(Jcr jcr) {
+    }
+
+    /**
+     * Returns the configured repository instance.
+     * 
+     * @return the configured repository instance.
+     */
+    @Override
+    public synchronized Repository getRepository() {
+        return repository;
+    }
+
+    @Override
+    public Credentials getReadOnlyCredentials() {
+        return new GuestCredentials();
+    }
+
+    @Override
+    public Principal getKnownPrincipal(Session session) throws RepositoryException {
+        throw new UnsupportedRepositoryOperationException();
+    }
+
+    private static final Principal UNKNOWN_PRINCIPAL = new Principal() {
+        @Override
+        public String getName() {
+            return "an_unknown_user";
+        }
+    };
+
+    @Override
+    public Principal getUnknownPrincipal(Session session) throws RepositoryException, NotExecutableException {
+        return UNKNOWN_PRINCIPAL;
+    }
+
+}
