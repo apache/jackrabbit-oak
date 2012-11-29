d680d18accfd30c030701003393ce8726b597b51
Alexandru Parvulescu <alexparvulescu@apache.org>
2012-11-29 10:19:53 +0000

    OAK-237 Run Jackrabbit query tests  - enabled tests & updated references to oak jira issues for the lucene index
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1415115 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/jcr/query/QueryJcrTest.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/jcr/query/QueryJcrTest.java
@@ -20,7 +20,18 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import org.apache.jackrabbit.core.query.FulltextQueryTest;
+import org.apache.jackrabbit.core.query.FulltextSQL2QueryTest;
+import org.apache.jackrabbit.core.query.JoinTest;
+import org.apache.jackrabbit.core.query.LimitAndOffsetTest;
+import org.apache.jackrabbit.core.query.MixinTest;
+import org.apache.jackrabbit.core.query.PathQueryNodeTest;
+import org.apache.jackrabbit.core.query.SQL2NodeLocalNameTest;
+import org.apache.jackrabbit.core.query.SQL2OffsetLimitTest;
+import org.apache.jackrabbit.core.query.SQL2OrderByTest;
+import org.apache.jackrabbit.core.query.SQL2OuterJoinTest;
 import org.apache.jackrabbit.core.query.SQLTest;
+import org.apache.jackrabbit.core.query.SkipDeletedNodesTest;
 import org.apache.jackrabbit.test.ConcurrentTestSuite;
 
 public class QueryJcrTest extends TestCase {
@@ -28,63 +39,41 @@ public class QueryJcrTest extends TestCase {
     public static Test suite() {
         TestSuite suite = new ConcurrentTestSuite(
                 "Jackrabbit query tests using a Lucene based index");
-        // suite.addTestSuite(FulltextQueryTest.class);
-        // ok but I'm not too sure about
-        // OAK-348 - fulltext tokenization
+        suite.addTestSuite(FulltextQueryTest.class);
+        suite.addTestSuite(SQLTest.class);
+        suite.addTestSuite(JoinTest.class);
+        suite.addTestSuite(SkipDeletedNodesTest.class);
+        suite.addTestSuite(PathQueryNodeTest.class);
+        suite.addTestSuite(FulltextSQL2QueryTest.class);
+        suite.addTestSuite(SQL2NodeLocalNameTest.class);
+        suite.addTestSuite(SQL2OrderByTest.class);
+        suite.addTestSuite(MixinTest.class);
+        suite.addTestSuite(SQL2OuterJoinTest.class);
+        suite.addTestSuite(SQL2OffsetLimitTest.class);
+        suite.addTestSuite(LimitAndOffsetTest.class);
 
-        suite.addTestSuite(SQLTest.class); // ok
-
-        // suite.addTestSuite(SimpleQueryTest.class);
-        // OAK-327 eq support
-        // like pattern escaped
-        // type support
-
-        // suite.addTestSuite(UpperLowerCaseQueryTest.class);
-        // OAK-329 type check should fail but doesn't
-
-        // suite.addTestSuite(QueryResultTest.class);
-        // OAK-308
-        // type checks *probably* fail some of the tests
-        // some orderby conditions
-
-        // suite.addTestSuite(SQL2QueryResultTest.class);
-        // OAK-323 - column names
-
-        // suite.addTestSuite(SQL2OuterJoinTest.class); // ok
-        // suite.addTestSuite(SkipDeletedNodesTest.class); // ok
-        // suite.addTestSuite(PathQueryNodeTest.class); // ok
-        // suite.addTestSuite(FulltextSQL2QueryTest.class); // ok
-        // suite.addTestSuite(SQL2NodeLocalNameTest.class); // ok
-        // suite.addTestSuite(SQL2OffsetLimitTest.class); // ok
-        // suite.addTestSuite(SQL2OrderByTest.class); // ok
-        // suite.addTestSuite(MixinTest.class);// ok
-        // suite.addTestSuite(JoinTest.class); // ok
-
+        // FAILURES
         //
-        // NOT OK
-        //
-        // suite.addTestSuite(OrderByTest.class); // OAK-347
-        // suite.addTestSuite(SQL2PathEscapingTest.class); // OAK-295
-
-        // suite.addTestSuite(LimitAndOffsetTest.class); // OAK-308
+        // suite.addTestSuite(QueryResultTest.class); // OAK-484
+        // suite.addTestSuite(OrderByTest.class); // OAK-466
         // suite.addTestSuite(ParentNodeTest.class); // OAK-309
-        // suite.addTestSuite(XPathAxisTest.class); // OAK-322
-
-        // suite.addTestSuite(FnNameQueryTest.class);
-        // OAK-328: "%:content" illegal name
-
-        //
-        // NOT IMPLEMENTED
-        //
-        // suite.addTestSuite(DerefTest.class); // OAK-321
         // suite.addTestSuite(ExcerptTest.class); // OAK-318
         // suite.addTestSuite(SimilarQueryTest.class); // OAK-319
+        // suite.addTestSuite(DerefTest.class); // OAK-321
+        // suite.addTestSuite(XPathAxisTest.class); // OAK-322
+        // suite.addTestSuite(SQL2QueryResultTest.class); // OAK-323
+        // suite.addTestSuite(SimpleQueryTest.class); // OAK-327
+        // suite.addTestSuite(FnNameQueryTest.class); // OAK-328
+        // suite.addTestSuite(UpperLowerCaseQueryTest.class); // OAK-329
+        // suite.addTestSuite(SQL2PathEscapingTest.class); // OAK-481
+
+        // NOT IMPLEMENTED
+        //
         // suite.addTestSuite(ChildAxisQueryTest.class); // sns
         // suite.addTestSuite(SelectClauseTest.class); // sns
         // suite.addTestSuite(ShareableNodeTest.class); // ws#clone
         // suite.addTestSuite(VersionStoreQueryTest.class); // versioning
 
-        //
         // TOO JR SPECIFIC
         //
         // suite.addTestSuite(LimitedAccessQueryTest.class); // acls
