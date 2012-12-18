23e00ec654433e6b861a56e930ea59e15d2a62c0
Michael Dürig <mduerig@apache.org>
2012-12-18 13:29:35 +0000

    OAK-532: Inconsistent journal with concurrent updates  test case
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1423428 13f79535-47bb-0310-9956-ffa450edef68

--- oak-mk/src/test/java/org/apache/jackrabbit/mk/concurrent/ConcurrentTest.java
+++ oak-mk/src/test/java/org/apache/jackrabbit/mk/concurrent/ConcurrentTest.java
@@ -13,12 +13,16 @@
  */
 package org.apache.jackrabbit.mk.concurrent;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import junit.framework.Assert;
 import org.apache.jackrabbit.mk.api.MicroKernel;
+import org.apache.jackrabbit.mk.api.MicroKernelException;
 import org.apache.jackrabbit.mk.core.MicroKernelImpl;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Test concurrent access to nodes, the journal, and revision.
@@ -50,6 +54,59 @@ public class ConcurrentTest {
                 Assert.assertFalse(mk.nodeExists("/" + i, newRev));
             }
         });
+    }
+
+    @Test
+    @Ignore("OAK-532")  // FIXME OAK-532
+    public void journalConsistency() throws Exception {
+        while (true) {
+            final MicroKernel mk1 = new MicroKernelImpl();
+            final String rev = mk1.commit("", "+\"/a\":{}", null, null);
+
+            Thread t1 = new Thread("t1") {
+                @Override
+                public void run() {
+                    try {
+                        String r2 = mk1.commit("", "-\"/a\"+\"/c\":{}", rev, null);
+                    }
+                    catch (MicroKernelException ignore) { }
+                }
+            };
+            Thread t2 = new Thread("t2") {
+                @Override
+                public void run() {
+                    try {
+                        String r2 = mk1.commit("", "-\"/a\"+\"/b\":{}", rev, null);
+                    }
+                    catch (MicroKernelException ignore) { }
+                }
+            };
+
+            t1.start();
+            t2.start();
+
+            t1.join();
+            t2.join();
+
+            String journal = mk1.getJournal(rev, null, null);
+            int c = count("-\\\"/a\\", journal);
+            assertEquals(1, c);
+        }
+    }
+
+    private static int count(String subStr, String str) {
+        int lastIndex = 0;
+        int count =0;
+
+        while(lastIndex != -1){
+            lastIndex = str.indexOf(subStr, lastIndex);
+
+            if( lastIndex != -1) {
+                count ++;
+                lastIndex += subStr.length();
+            }
+        }
+        return count;
     }
 
 }
