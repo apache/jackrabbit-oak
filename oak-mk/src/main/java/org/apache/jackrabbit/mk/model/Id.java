3cc1a9a4e566cd5a83ef1fad941a1b49953faf82
Stefan Guggisberg <stefan@apache.org>
2013-01-04 09:27:15 +0000

    OAK-539: Wrong compareTo in micro-kernel Id class
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1428764 13f79535-47bb-0310-9956-ffa450edef68

--- oak-mk/src/main/java/org/apache/jackrabbit/mk/model/Id.java
+++ oak-mk/src/main/java/org/apache/jackrabbit/mk/model/Id.java
@@ -113,7 +113,9 @@ public class Id implements Comparable<Id> {
         
         for (int i = 0; i < len; i++) {
             if (raw[i] != other[i]) {
-                return raw[i] - other[i];
+                final int rawValue = raw[i] & 0xFF; // unsigned value
+                final int otherValue = other[i] & 0xFF; // unsigned value
+                return rawValue - otherValue;
             }
         }
         return raw.length - other.length;
