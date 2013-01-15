717186d61dd2c80adb12a0fa6b260fe9085c7c75
Thomas Mueller <thomasm@apache.org>
2013-01-15 16:06:39 +0000

    OAK-548 Moving larger trees cause OutOfMemoryError (diff ignores depth for added nodes)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1433482 13f79535-47bb-0310-9956-ffa450edef68

--- oak-mk/src/test/java/org/apache/jackrabbit/mk/MicroKernelImplTest.java
+++ oak-mk/src/test/java/org/apache/jackrabbit/mk/MicroKernelImplTest.java
@@ -63,4 +63,15 @@ public class MicroKernelImplTest {
         headRev = mk.commit("/", "+\"b\" : {}", mk.getHeadRevision(), null);
         assertFalse("Commit must not have same id as branch", headRev.equals(branchRev));
     }
+    
+    @Test
+    public void diffWithDepth() {
+        String head = mk.getHeadRevision();
+        String r1 = mk.commit("/", "+\"a\" : { \"l\": 1, \"x\": { \"l\": 2, \"y\": {} } }", head, null);
+        String r2 = mk.commit("/", ">\"a\" : \"b\"", r1, null);
+        assertEquals("+\"/b\":{\"l\":1}", mk.diff(r1, r2, "/b", 0).trim());
+        assertEquals("+\"/b\":{\"l\":1,\"x\":{\"l\":2}}", mk.diff(r1, r2, "/b", 1).trim());
+        assertEquals("+\"/b\":{\"l\":1,\"x\":{\"l\":2,\"y\":{}}}", mk.diff(r1, r2, "/b", -1).trim());
+    }
+
 }
