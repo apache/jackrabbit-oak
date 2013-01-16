469e9418f690382b1ff33e5abfd555843a5298a5
Marcel Reutegger <mreutegg@apache.org>
2013-01-16 15:47:49 +0000

    OAK-535: MergeCommand reads complete tree into memory - additional test
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1433995 13f79535-47bb-0310-9956-ffa450edef68

--- oak-mongomk/src/test/java/org/apache/jackrabbit/mongomk/impl/MongoMKBranchMergeTest.java
+++ oak-mongomk/src/test/java/org/apache/jackrabbit/mongomk/impl/MongoMKBranchMergeTest.java
@@ -1,10 +1,15 @@
 package org.apache.jackrabbit.mongomk.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import org.apache.jackrabbit.mk.api.MicroKernelException;
 import org.apache.jackrabbit.mongomk.BaseMongoMicroKernelTest;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -336,6 +341,42 @@ public class MongoMKBranchMergeTest extends BaseMongoMicroKernelTest {
         rev = mk.merge(branchRev, null);
         assertNodesExist(rev, "/a", "/a/b");
         assertPropExists(rev, "/a/b", "p");
+    }
+
+    @Test
+    @Ignore("OAK-535")
+    public void concurrentNonConflictingMerges() throws Exception {
+        int numThreads = 5;
+        mk.commit("/", "+\"test\":{}", null, null);
+        List<Thread> workers = new ArrayList<Thread>();
+        final List<Exception> exceptions = Collections.synchronizedList(new ArrayList<Exception>());
+        for (int i = 0; i < numThreads; i++) {
+            final String path = "/test/t" + i;
+            mk.commit("", "+\"" + path + "\":{}", null, null);
+            workers.add(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        for (int i = 0; i < 100; i++) {
+                            String branchRev = mk.branch(null);
+                            branchRev = mk.commit(path, "+\"node" + i + "\":{}", branchRev, null);
+                            mk.merge(branchRev, null);
+                        }
+                    } catch (MicroKernelException e) {
+                        exceptions.add(e);
+                    }
+                }
+            }));
+        }
+        for (Thread t : workers) {
+            t.start();
+        }
+        for (Thread t : workers) {
+            t.join();
+        }
+        if (!exceptions.isEmpty()) {
+            throw exceptions.get(0);
+        }
     }
 
     private String addNodes(String rev, String...nodes) {
