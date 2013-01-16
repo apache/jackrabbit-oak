943cfe17659f27f4a2c223d78de22101228cdfe3
Marcel Reutegger <mreutegg@apache.org>
2013-01-16 10:38:17 +0000

    OAK-558: Compress affected paths and diff field in MongoCommit
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1433888 13f79535-47bb-0310-9956-ffa450edef68

--- oak-mongomk/src/main/java/org/apache/jackrabbit/mongomk/impl/model/MongoCommit.java
+++ oak-mongomk/src/main/java/org/apache/jackrabbit/mongomk/impl/model/MongoCommit.java
@@ -16,10 +16,20 @@
  */
 package org.apache.jackrabbit.mongomk.impl.model;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 import org.apache.jackrabbit.mongomk.api.instruction.Instruction;
 import org.apache.jackrabbit.mongomk.api.model.Commit;
@@ -42,6 +52,7 @@ public class MongoCommit extends BasicDBObject implements Commit {
     public static final String KEY_TIMESTAMP = "ts";
 
     private final List<Instruction> instructions;
+    private List<String> affectedPaths;
 
     private static final long serialVersionUID = 6656294757102309827L;
 
@@ -62,11 +73,15 @@ public class MongoCommit extends BasicDBObject implements Commit {
     @Override
     @SuppressWarnings("unchecked")
     public List<String> getAffectedPaths() {
-        return (List<String>) get(KEY_AFFECTED_PATH);
+        if (affectedPaths == null) {
+            affectedPaths = inflate((byte[]) get(KEY_AFFECTED_PATH));
+        }
+        return affectedPaths;
     }
 
     public void setAffectedPaths(List<String> affectedPaths) {
-        put(KEY_AFFECTED_PATH, affectedPaths);
+        this.affectedPaths = affectedPaths;
+        put(KEY_AFFECTED_PATH, compress(affectedPaths));
     }
 
     @Override
@@ -93,11 +108,16 @@ public class MongoCommit extends BasicDBObject implements Commit {
 
     @Override
     public String getDiff() {
-        return getString(KEY_DIFF);
+        List<String> data = inflate((byte[]) get(KEY_DIFF));
+        if (data == null || data.isEmpty()) {
+            return null;
+        } else {
+            return data.get(0);
+        }
     }
 
     public void setDiff(String diff) {
-        put(KEY_DIFF, diff);
+        put(KEY_DIFF, compress(Collections.singleton(diff)));
     }
 
     public boolean isFailed() {
@@ -163,5 +183,49 @@ public class MongoCommit extends BasicDBObject implements Commit {
     @Override
     public List<Instruction> getInstructions() {
         return Collections.unmodifiableList(instructions);
+    }
+
+    //-----------------------------< internal >---------------------------------
+
+    private List<String> inflate(byte[] bytes) {
+        if (bytes == null) {
+            return null;
+        }
+        List<String> data = new ArrayList<String>();
+        try {
+            DataInputStream in = new DataInputStream(new GZIPInputStream(
+                    new ByteArrayInputStream(bytes)));
+            int num = in.readInt();
+            for (int i = 0; i < num; i++) {
+                int len = in.readInt();
+                byte[] buffer = new byte[len];
+                in.readFully(buffer);
+                data.add(new String(buffer, "UTF-8"));
+            }
+        } catch (IOException e) {
+            // FIXME: shoulnd't actually happen with byte[] and UTF-8
+            throw new RuntimeException(e);
+        }
+        return data;
+    }
+
+    private byte[] compress(Collection<String> strings) {
+        try {
+            ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+            OutputStream out = new GZIPOutputStream(byteOut);
+            DataOutputStream dataOut = new DataOutputStream(out);
+            dataOut.writeInt(strings.size());
+            for (String s : strings) {
+                byte[] buffer = s.getBytes("UTF-8");
+                dataOut.writeInt(buffer.length);
+                dataOut.write(buffer);
+            }
+            dataOut.close();
+            out.close();
+            return byteOut.toByteArray();
+        } catch (IOException e) {
+            // FIXME: shoulnd't actually happen with byte[] and UTF-8
+            throw new RuntimeException(e);
+        }
     }
 }
