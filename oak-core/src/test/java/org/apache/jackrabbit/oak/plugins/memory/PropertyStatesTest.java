388c95cd4e35ae29266d14061824fc29747f6028
Michael Duerig <mduerig@apache.org>
2013-01-18 16:08:51 +0000

    OAK-554: PropertyStates#createProperty ignores namespace mappings when creating states of type NAME and PATH test case
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1435215 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/memory/PropertyStatesTest.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.jackrabbit.oak.plugins.memory;
+
+import javax.jcr.PropertyType;
+import javax.jcr.RepositoryException;
+import javax.jcr.Value;
+
+import org.apache.jackrabbit.oak.TestNameMapper;
+import org.apache.jackrabbit.oak.api.PropertyState;
+import org.apache.jackrabbit.oak.namepath.NamePathMapperImpl;
+import org.apache.jackrabbit.oak.plugins.value.ValueFactoryImpl;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static java.util.Collections.singletonMap;
+import static org.junit.Assert.assertEquals;
+
+public class PropertyStatesTest {
+
+    private final NamePathMapperImpl namePathMapper = new NamePathMapperImpl(new TestNameMapper(
+        singletonMap("oak-prefix", TestNameMapper.TEST_URI),
+        singletonMap("jcr-prefix", TestNameMapper.TEST_URI) ));
+
+    @Test
+    @Ignore  // FIXME OAK-554
+    public void namePropertyFromNameValue() throws RepositoryException {
+        PropertyState nameProperty = PropertyStates.createProperty("name", "oak-prefix:value", PropertyType.NAME);
+        Value nameValue = ValueFactoryImpl.createValue(nameProperty, namePathMapper);
+        PropertyState namePropertyFromValue = PropertyStates.createProperty("name", nameValue);
+        assertEquals(nameProperty, namePropertyFromValue);
+    }
+
+    @Test
+    @Ignore  // FIXME OAK-554
+    public void pathPropertyFromPathValue() throws RepositoryException {
+        PropertyState pathProperty = PropertyStates.createProperty("path", "oak-prefix:a/oak-prefix:b", PropertyType.PATH);
+        Value nameValue = ValueFactoryImpl.createValue(pathProperty, namePathMapper);
+        PropertyState namePropertyFromValue = PropertyStates.createProperty("path", nameValue);
+        assertEquals(pathProperty, namePropertyFromValue);
+    }
+}
