c2b3ac3296b14f4b7e91eb2d6f1fba39d9ecadc3
Mete Atamel <meteatamel@apache.org>
2013-01-23 09:51:21 +0000

    OAK-351 - Inconsistency issue when committing a new node under an old parent
    Added the test case for the issue
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1437314 13f79535-47bb-0310-9956-ffa450edef68

--- oak-mongomk/src/test/java/org/apache/jackrabbit/mongomk/impl/MongoMKCommitAddTest.java
+++ oak-mongomk/src/test/java/org/apache/jackrabbit/mongomk/impl/MongoMKCommitAddTest.java
@@ -52,6 +52,19 @@ public class MongoMKCommitAddTest extends BaseMongoMicroKernelTest {
     }
 
     @Test
+    public void addNodeWithNestedChildren2() throws Exception {
+        mk.commit("/", "+\"a\" : {}", null, null);
+        mk.commit("/a", "+\"b\" : {}", null, null);
+        mk.commit("/a/b", "+\"c\" : {}", null, null);
+        mk.commit("/a", "+\"d\" : {}", null, null);
+
+        assertTrue(mk.nodeExists("/a",null));
+        assertTrue(mk.nodeExists("/a/b",null));
+        assertTrue(mk.nodeExists("/a/b/c",null));
+        assertTrue(mk.nodeExists("/a/d", null));
+    }
+
+    @Test
     public void addNodeWithParanthesis() throws Exception {
         mk.commit("/", "+\"Test({0})\" : {}", null, null);
         String nodes = mk.getNodes("/Test({0})", null, 1, 0, -1, null);
