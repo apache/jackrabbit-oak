a385fbfc1b8063916319ce85407fbe13f906ddd0
Angela Schreiber <angela@apache.org>
2013-01-24 12:57:48 +0000

    simplify property index setup
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1437984 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/IndexUtils.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/IndexUtils.java
@@ -21,10 +21,15 @@ import java.util.Collections;
 import java.util.List;
 
 import org.apache.jackrabbit.oak.api.PropertyState;
+import org.apache.jackrabbit.oak.api.Type;
+import org.apache.jackrabbit.oak.plugins.memory.PropertyStates;
 import org.apache.jackrabbit.oak.spi.state.ChildNodeEntry;
+import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
 import org.apache.jackrabbit.oak.util.NodeUtil;
 
+import static org.apache.jackrabbit.JcrConstants.JCR_PRIMARYTYPE;
+import static org.apache.jackrabbit.JcrConstants.NT_UNSTRUCTURED;
 import static org.apache.jackrabbit.oak.api.Type.STRING;
 import static org.apache.jackrabbit.oak.commons.PathUtils.concat;
 
@@ -32,6 +37,48 @@ import static org.apache.jackrabbit.oak.commons.PathUtils.concat;
  * TODO document
  */
 public class IndexUtils implements IndexConstants {
+
+    public static NodeBuilder getOrCreateOakIndex(NodeBuilder root) {
+        NodeBuilder index;
+        if (!root.hasChildNode(IndexConstants.INDEX_DEFINITIONS_NAME)) {
+            index = root.child(IndexConstants.INDEX_DEFINITIONS_NAME);
+            // TODO: use property node type name
+            index.setProperty(JCR_PRIMARYTYPE, NT_UNSTRUCTURED, Type.NAME);
+        } else {
+            index = root.child(IndexConstants.INDEX_DEFINITIONS_NAME);
+        }
+        return index;
+    }
+
+    /**
+     * Create a new property2 index definition below the given {@code indexNode}.
+     *
+     * @param index The oak:index node builder
+     * @param indexDefName The name of the new property index.
+     * @param reindex {@code true} if the the reindex flag should be turned on.
+     * @param unique {@code true} if the index is expected the assert property
+     * uniqueness.
+     * @param propertyNames The property names that should be indexed.
+     */
+    public static void createIndexDefinition(NodeBuilder index,
+                                             String indexDefName,
+                                             boolean reindex,
+                                             boolean unique,
+                                             List<String> propertyNames) {
+        NodeBuilder entry = index.child(indexDefName)
+                .setProperty(JCR_PRIMARYTYPE, IndexConstants.INDEX_DEFINITIONS_NODE_TYPE, Type.NAME)
+                .setProperty(IndexConstants.TYPE_PROPERTY_NAME, "p2")
+                .setProperty(IndexConstants.REINDEX_PROPERTY_NAME, reindex);
+        if (unique) {
+            entry.setProperty(IndexConstants.UNIQUE_PROPERTY_NAME, unique);
+        }
+        // FIXME OAK-583
+        if (propertyNames.size() == 1) {
+            entry.setProperty(IndexConstants.PROPERTY_NAMES, propertyNames.get(0));
+        } else {
+            entry.setProperty(PropertyStates.createProperty(IndexConstants.PROPERTY_NAMES, propertyNames, Type.STRINGS));
+        }
+    }
 
     /**
      * Create a new property2 index definition below the given {@code indexNode}.
