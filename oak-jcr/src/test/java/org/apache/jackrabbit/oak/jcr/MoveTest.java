bac79c48508f48f70aa45e1bd049d488b092542f
Angela Schreiber <angela@apache.org>
2013-02-08 09:14:55 +0000

    OAK-606, OAK-607: moving referenceable nodes
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1443895 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/MoveTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/MoveTest.java
@@ -18,6 +18,7 @@ package org.apache.jackrabbit.oak.jcr;
 
 import javax.jcr.Node;
 
+import org.apache.jackrabbit.JcrConstants;
 import org.apache.jackrabbit.test.AbstractJCRTest;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -73,10 +74,40 @@ public class MoveTest extends AbstractJCRTest {
         assertEquals(destPath, node1.getPath());
     }
 
+    @Ignore("OAK-606")
+    public void testMoveReferenceable() throws Exception {
+        Node node1 = testRootNode.addNode(nodeName1);
+        node1.addMixin(JcrConstants.MIX_REFERENCEABLE);
+        Node node2 = testRootNode.addNode(nodeName2);
+        superuser.save();
+
+        String destPath = node2.getPath() + '/' + nodeName1;
+        move(node1.getPath(), destPath, true);
+
+        assertEquals(destPath, node1.getPath());
+    }
+
     @Ignore("OAK-607")
     @Test
     public void testMoveNewNode() throws Exception {
         Node node1 = testRootNode.addNode(nodeName1);
+        Node node2 = testRootNode.addNode(nodeName2);
+
+        String destPath = node2.getPath() + '/' + nodeName1;
+        move(node1.getPath(), destPath, false);
+
+        assertEquals(destPath, node1.getPath());
+
+        superuser.save();
+        assertEquals(destPath, node1.getPath());
+    }
+
+    @Ignore("OAK-607")
+    @Test
+    public void testMoveNewReferenceable() throws Exception {
+        Node node1 = testRootNode.addNode(nodeName1);
+        node1.addMixin(JcrConstants.MIX_REFERENCEABLE);
+        assertTrue(node1.isNodeType(JcrConstants.MIX_REFERENCEABLE));
         Node node2 = testRootNode.addNode(nodeName2);
 
         String destPath = node2.getPath() + '/' + nodeName1;
