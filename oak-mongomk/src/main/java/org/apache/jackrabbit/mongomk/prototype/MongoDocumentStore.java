25f1ef5d2fe9d146e143540e8a7dfba5b1ce1625
Mete Atamel <meteatamel@apache.org>
2013-02-13 13:46:27 +0000

    OAK-619 - Lock-free MongoMK implementation
    Added initial mongo based store along with the test case
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1445595 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-mongomk/src/main/java/org/apache/jackrabbit/mongomk/prototype/MongoDocumentStore.java
@@ -0,0 +1,133 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.mongomk.prototype;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.jackrabbit.mongomk.prototype.UpdateOp.Operation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.QueryBuilder;
+import com.mongodb.WriteConcern;
+import com.mongodb.WriteResult;
+
+public class MongoDocumentStore implements DocumentStore {
+
+    private static final String KEY_PATH = "_path";
+    private static final Logger LOG = LoggerFactory.getLogger(MongoDocumentStore.class);
+
+    private final DBCollection nodesCollection;
+
+    public MongoDocumentStore(DB db) {
+        nodesCollection = db.getCollection(Collection.NODES.toString());
+    }
+
+    @Override
+    public Map<String, Object> find(Collection collection, String path) {
+        DBObject n = getNode(collection, path);
+        if (n == null) {
+            return null;
+        }
+        return convertFromDBObject(n);
+    }
+
+    @Override
+    public void remove(Collection collection, String path) {
+        DBCollection dbCollection = getDBCollection(collection);
+        WriteResult writeResult = dbCollection.remove(getByPathQuery(path), WriteConcern.SAFE);
+        if (writeResult.getError() != null) {
+            LOG.error("Remove failed: {}", writeResult.getError());
+        }
+    }
+
+    @Override
+    public Map<String, Object> createOrUpdate(Collection collection, UpdateOp updateOp) {
+        DBCollection dbCollection = getDBCollection(collection);
+
+        BasicDBObject setUpdates = new BasicDBObject();
+        BasicDBObject incUpdates = new BasicDBObject();
+
+        for (Entry<String, Operation> entry : updateOp.changes.entrySet()) {
+            String k = entry.getKey();
+            Operation op = entry.getValue();
+            switch (op.type) {
+                case SET: {
+                    setUpdates.append(k, op.value);
+                    break;
+                }
+                case INCREMENT: {
+                    incUpdates.append(k, (Long)op.value);
+                    break;
+                }
+                case ADD_MAP_ENTRY: {
+                    setUpdates.append(k + "." + op.subKey.toString(), op.value.toString());
+                    break;
+                }
+                case REMOVE_MAP_ENTRY: {
+                    // TODO
+                    break;
+                }
+            }
+        }
+
+        DBObject query = getByPathQuery(updateOp.key);
+        BasicDBObject update = new BasicDBObject();
+        if (!setUpdates.isEmpty()) {
+            update.append("$set", setUpdates);
+        }
+        if (!incUpdates.isEmpty()) {
+            update.append("$inc", incUpdates);
+        }
+
+        DBObject oldNode = dbCollection.findAndModify(query, null /*fields*/, null /*sort*/, false /*remove*/, update, false /*returnNew*/, true /*upsert*/);
+        return convertFromDBObject(oldNode);
+    }
+
+    private Map<String, Object> convertFromDBObject(DBObject n) {
+        Map<String, Object> copy = Utils.newMap();
+        synchronized (n) {
+            for (String key : n.keySet()) {
+                copy.put(key, n.get(key));
+            }
+            return copy;
+        }
+    }
+
+    private DBCollection getDBCollection(Collection collection) {
+        switch (collection) {
+            case NODES:
+                return nodesCollection;
+            default:
+                throw new IllegalArgumentException(collection.name());
+        }
+    }
+
+    private DBObject getNode(Collection collection, String path) {
+        DBCollection dbCollection = getDBCollection(collection);
+        return dbCollection.findOne(getByPathQuery(path));
+    }
+
+    private DBObject getByPathQuery(String path) {
+        return QueryBuilder.start(KEY_PATH).is(path).get();
+    }
+}
