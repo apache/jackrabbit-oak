27368a57bcb5e9d337a4ee274fb50c2c01ac565c
Jukka Zitting <jukka@apache.org>
2013-02-14 21:05:56 +0000

    OAK-593: Segment-based MK
    Store large binaries entirely in separate bulk segments to make normal segments more useful for caching.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1446326 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/segment/RecordTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/segment/RecordTest.java
@@ -115,10 +115,12 @@ public class RecordTest {
     public void testStreamRecord() throws IOException {
         checkRandomStreamRecord(0);
         checkRandomStreamRecord(1);
+        checkRandomStreamRecord(0x79);
+        checkRandomStreamRecord(0x80);
+        checkRandomStreamRecord(0x4079);
+        checkRandomStreamRecord(0x4080);
         checkRandomStreamRecord(SegmentWriter.BLOCK_SIZE);
         checkRandomStreamRecord(SegmentWriter.BLOCK_SIZE + 1);
-        checkRandomStreamRecord(SegmentWriter.INLINE_SIZE);
-        checkRandomStreamRecord(SegmentWriter.INLINE_SIZE + 1);
         checkRandomStreamRecord(store.getMaxSegmentSize());
         checkRandomStreamRecord(store.getMaxSegmentSize() + 1);
         checkRandomStreamRecord(store.getMaxSegmentSize() * 2);
