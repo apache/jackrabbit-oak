50c12c33f7bc381d413f9354f67573a7b180a765
Michael Dürig <mduerig@apache.org>
2013-02-19 21:55:52 +0000

    OAK-642: NPE trying to add a node to an nt:folder node
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1447940 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/NodeImpl.java
+++ oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/NodeImpl.java
@@ -250,11 +250,10 @@ public class NodeImpl<T extends NodeDelegate> extends ItemImpl<T> implements Nod
                 String ntName = primaryNodeTypeName;
                 if (ntName == null) {
                     DefinitionProvider dp = sessionDelegate.getDefinitionProvider();
-                    try {
-                        String childName = sessionDelegate.getOakName(PathUtils.getName(relPath));
-                        NodeDefinition def = dp.getDefinition(new NodeImpl<NodeDelegate>(parent), childName);
-                        ntName = def.getDefaultPrimaryTypeName();
-                    } catch (RepositoryException e) {
+                    String childName = sessionDelegate.getOakName(PathUtils.getName(relPath));
+                    NodeDefinition def = dp.getDefinition(new NodeImpl<NodeDelegate>(parent), childName);
+                    ntName = def.getDefaultPrimaryTypeName();
+                    if (ntName == null) {
                         throw new ConstraintViolationException(
                                 "no matching child node definition found for " + relPath);
                     }
