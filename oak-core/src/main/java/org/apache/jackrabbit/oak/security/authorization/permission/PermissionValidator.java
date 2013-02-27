f55805b4ad074b67d2d228280716f1a954a2ca25
Angela Schreiber <angela@apache.org>
2013-02-27 17:44:22 +0000

    OAK-527: permissions (wip)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1450879 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/security/authorization/permission/PermissionValidator.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/security/authorization/permission/PermissionValidator.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.oak.security.authorization;
+package org.apache.jackrabbit.oak.security.authorization.permission;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
