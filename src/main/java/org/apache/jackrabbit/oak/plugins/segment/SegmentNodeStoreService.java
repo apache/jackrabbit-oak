1e0517acefb9188745e8d050e757614478186f1c
Jukka Zitting <jukka@apache.org>
2013-02-28 10:11:00 +0000

    OAK-593: Segment-based MK
    Make SegmentMK usable in OSGi environments
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1451165 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/jackrabbit/oak/plugins/segment/SegmentNodeStoreService.java
@@ -0,0 +1,121 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.plugins.segment;
+
+import java.net.UnknownHostException;
+import java.util.Dictionary;
+import java.util.UUID;
+
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.ConfigurationPolicy;
+import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.jackrabbit.oak.spi.state.NodeStore;
+import org.osgi.service.component.ComponentContext;
+
+import com.mongodb.Mongo;
+
+@Component(policy = ConfigurationPolicy.REQUIRE)
+@Service(NodeStore.class)
+public class SegmentNodeStoreService extends SegmentNodeStore {
+
+    @Property(description="The unique name of this instance")
+    public static final String NAME = "name";
+
+    @Property(description="MongoDB host", value="localhost")
+    public static final String HOST = "host";
+
+    @Property(description="MongoDB host", intValue=27017)
+    public static final String PORT = "port";
+
+    @Property(description="MongoDB database", value="Oak")
+    public static final String DB = "db";
+
+    @Property(description="Cache size (MB)", intValue=200)
+    public static final String CACHE = "cache";
+
+    private static final long MB = 1024 * 1024;
+
+    private String name;
+
+    private Mongo mongo;
+
+    private final SegmentStore[] store;
+
+    public SegmentNodeStoreService(final SegmentStore[] store) {
+        super(new SegmentStore() {
+            @Override
+            public RecordId getJournalHead(String name) {
+                return store[0].getJournalHead(name);
+            }
+            @Override
+            public boolean setJournalHead(
+                    String name, RecordId head, RecordId base) {
+                return store[0].setJournalHead(name, head, base);
+            }
+            @Override
+            public Segment readSegment(UUID segmentId) {
+                return store[0].readSegment(segmentId);
+            }            
+            @Override
+            public void createSegment(Segment segment) {
+                store[0].createSegment(segment);
+            }
+            @Override
+            public void createSegment(
+                    UUID segmentId, byte[] bytes, int offset, int len) {
+                store[0].createSegment(segmentId, bytes, offset, len);
+            }
+            @Override
+            public void deleteSegment(UUID segmentId) {
+                store[0].deleteSegment(segmentId);
+            }
+        });
+        this.store = store;
+    }
+
+    public SegmentNodeStoreService() {
+        this(new SegmentStore[0]);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Activate
+    public void activate(ComponentContext context) throws UnknownHostException {
+        Dictionary<?, ?> properties = context.getProperties();
+        name = "" + properties.get(NAME);
+
+        String host = String.valueOf(properties.get(HOST));
+        int port = Integer.parseInt(String.valueOf(properties.get(PORT)));
+        String db = String.valueOf(properties.get(DB));
+        int cache = Integer.parseInt(String.valueOf(properties.get(CACHE)));
+
+        mongo = new Mongo(host, port);
+        store[0] = new MongoStore(mongo.getDB(db), cache * MB);
+    }
+
+    @Deactivate
+    public void deactivate() {
+        mongo.close();
+    }
+
+}
