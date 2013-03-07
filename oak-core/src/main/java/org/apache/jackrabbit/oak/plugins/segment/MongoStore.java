5afcff1d8a7152306a605e64c6dc2769e4311351
Jukka Zitting <jukka@apache.org>
2013-03-07 11:59:21 +0000

    OAK-633: SegmentMK: Hierarchy of journals
    Extract MongoJournal to a separate file
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1453792 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/segment/MongoStore.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/segment/MongoStore.java
@@ -19,7 +19,6 @@ package org.apache.jackrabbit.oak.plugins.segment;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.Callable;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.jackrabbit.oak.plugins.memory.MemoryNodeState;
 
@@ -32,9 +31,6 @@ import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 
 public class MongoStore implements SegmentStore {
-
-    private static final long UPDATE_INTERVAL =
-            TimeUnit.NANOSECONDS.convert(10, TimeUnit.MILLISECONDS);
 
     private final DBCollection segments;
 
@@ -67,46 +63,8 @@ public class MongoStore implements SegmentStore {
     }
 
     @Override
-    public Journal getJournal(final String name) {
-        return new Journal() {
-
-            private long nextUpdate = System.nanoTime() - 2 * UPDATE_INTERVAL;
-
-            private RecordId head = getHead();
-
-            @Override
-            public synchronized RecordId getHead() {
-                long now = System.nanoTime();
-                if (now >= nextUpdate) {
-                    DBObject journal = journals.findOne(new BasicDBObject("_id", name));
-                    head = RecordId.fromString(journal.get("head").toString());
-                    nextUpdate = now + UPDATE_INTERVAL;
-                }
-                return head;
-            }
-
-            @Override
-            public boolean setHead(RecordId base, RecordId head) {
-                DBObject baseObject = new BasicDBObject(
-                        ImmutableMap.of("_id", name, "head", base.toString()));
-                DBObject headObject = new BasicDBObject(
-                        ImmutableMap.of("_id", name, "head", head.toString()));
-                if (journals.findAndModify(baseObject, headObject) != null) {
-                    this.head = head;
-                    nextUpdate = System.nanoTime() + UPDATE_INTERVAL;
-                    return true;
-                } else if (base.equals(this.head)) {
-                    // force an update at next getHead() call
-                    nextUpdate = System.nanoTime();
-                }
-                return false;
-            }
-
-            @Override
-            public void merge() {
-                throw new UnsupportedOperationException();
-            }
-        };
+    public Journal getJournal(String name) {
+        return new MongoJournal(journals, name);
     }
 
     @Override
