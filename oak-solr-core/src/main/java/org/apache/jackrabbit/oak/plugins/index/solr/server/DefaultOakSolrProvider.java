7bb71643b8d42c2c45af2427cfac062038676923
Tommaso Teofili <tommaso@apache.org>
2013-03-26 09:28:47 +0000

    OAK-718 - default configuration is copied in the configured paths if needed
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1461034 13f79535-47bb-0310-9956-ffa450edef68

--- oak-solr-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/solr/server/DefaultOakSolrProvider.java
+++ oak-solr-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/solr/server/DefaultOakSolrProvider.java
@@ -17,7 +17,12 @@
 package org.apache.jackrabbit.oak.plugins.index.solr.server;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
+import org.apache.jackrabbit.mk.util.IOUtils;
 import org.apache.jackrabbit.oak.plugins.index.solr.OakSolrConfiguration;
 import org.apache.jackrabbit.oak.plugins.index.solr.OakSolrConfigurationProvider;
 import org.apache.jackrabbit.oak.plugins.index.solr.SolrServerProvider;
@@ -42,9 +47,88 @@ public class DefaultOakSolrProvider implements SolrServerProvider, OakSolrConfig
     private SolrServer solrServer;
 
     private SolrServer createSolrServer() throws Exception {
-        CoreContainer coreContainer = new CoreContainer(oakSolrConfiguration.getSolrHomePath());
-        coreContainer.load(oakSolrConfiguration.getSolrHomePath(), new File(oakSolrConfiguration.getSolrConfigPath()));
-        return new EmbeddedSolrServer(coreContainer, oakSolrConfiguration.getCoreName());
+
+        String solrHomePath = oakSolrConfiguration.getSolrHomePath();
+        String coreName = oakSolrConfiguration.getCoreName();
+        String solrConfigPath = oakSolrConfiguration.getSolrConfigPath();
+
+        checkSolrConfiguration(solrHomePath, solrConfigPath, coreName);
+
+        CoreContainer coreContainer = new CoreContainer(solrHomePath);
+        coreContainer.load(solrHomePath, new File(solrConfigPath));
+        return new EmbeddedSolrServer(coreContainer, coreName);
+    }
+
+    private void checkSolrConfiguration(String solrHomePath, String solrConfigPath, String coreName) throws IOException {
+
+        // check if solrHomePath exists
+        File solrHomePathFile = new File(solrHomePath);
+        if (!solrHomePathFile.exists()) {
+            if (!solrHomePathFile.mkdir()) {
+                throw new IOException("could not create solrHomePath directory");
+            } else {
+                // copy all the needed files to the just created directory
+                copy("/solr/solr.xml", solrHomePath);
+                copy("/solr/zoo.cfg", solrHomePath);
+                if (!new File(solrHomePath + "/" + "oak").mkdir() || !new File(solrHomePath + "/" + "oak/conf/").mkdir()) {
+                    throw new IOException("could not create nested core directory in solrHomePath");
+                }
+                String coreDir = solrHomePath + "/oak/conf/";
+                copy("/solr/oak/conf/currency.xml", coreDir);
+                copy("/solr/oak/conf/schema.xml", coreDir);
+                copy("/solr/oak/conf/solrconfig.xml", coreDir);
+            }
+        } else if (!solrHomePathFile.isDirectory()) {
+            throw new IOException("a non directory file with the specified name already exists for the given solrHomePath");
+        }
+
+        File solrConfigPathFile = new File(solrConfigPath);
+        // check if solrConfigPath exists
+        if (!solrConfigPathFile.exists()) {
+            if (solrConfigPathFile.createNewFile()) {
+                copy("/solr/solr.xml", solrConfigPathFile.getAbsolutePath());
+            }
+        }
+
+        // check if the a core with the given coreName exists
+        // TODO : improve this check
+        String[] files = new File(solrHomePath).list();
+        Arrays.sort(files);
+        if (Arrays.binarySearch(files, coreName) < 0) {
+            throw new IOException("could not find a directory with the given coreName in the solrHomePath");
+        }
+
+
+    }
+
+    private void copy(String resource, String dir) throws IOException {
+        String fileName = dir + resource.substring(resource.lastIndexOf("/"));
+        File outputFile = new File(fileName);
+        if (outputFile.createNewFile()) {
+            InputStream inputStream = null;
+            FileOutputStream outputStream = null;
+            try {
+                inputStream = getClass().getResourceAsStream(resource);
+                outputStream = new FileOutputStream(outputFile);
+                IOUtils.copy(inputStream, outputStream);
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (Exception e){
+                        // do nothing
+                    }
+                }
+                if (outputStream != null) {
+                    try {
+                        outputStream.close();
+                    } catch (Exception e){
+                        // do nothing
+                    }
+                }
+            }
+        }
+
     }
 
     @Override
