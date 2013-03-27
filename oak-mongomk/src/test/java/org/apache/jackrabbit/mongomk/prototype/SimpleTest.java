b20a0433d70b0ed2bd054dbee7a1b68072ef5680
Thomas Mueller <thomasm@apache.org>
2013-03-27 14:48:52 +0000

    OAK-619 Lock-free MongoMK implementation (better error message, fix source merge error)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1461618 13f79535-47bb-0310-9956-ffa450edef68

--- oak-mongomk/src/test/java/org/apache/jackrabbit/mongomk/prototype/SimpleTest.java
+++ oak-mongomk/src/test/java/org/apache/jackrabbit/mongomk/prototype/SimpleTest.java
@@ -200,9 +200,11 @@ public class SimpleTest {
         MongoMK mk = createMK();
         String rev = mk.commit("/", "+\"test\":{\"x\":\"1\",\"child\": {}}", null, null);
         rev = mk.commit("/", "-\"test\"", rev, null);
-        rev = mk.commit("/", "+\"test\":{}", null, null);
+        rev = mk.commit("/", "+\"test\":{}  +\"test2\": {}", null, null);
         String test = mk.getNodes("/test", rev, 0, 0, Integer.MAX_VALUE, null);
         assertEquals("{\":childNodeCount\":0}", test);
+        String test2 = mk.getNodes("/test2", rev, 0, 0, Integer.MAX_VALUE, null);
+        assertEquals("{\":childNodeCount\":0}", test2);
         mk.dispose();
     }
     
