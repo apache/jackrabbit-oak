35a7f014668fec3b1752acd8a4f23e7bba7b2ac4
Alexandru Parvulescu <alexparvulescu@apache.org>
2013-04-04 07:02:53 +0000

    OAK-740 Malformed solr delete query
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1464298 13f79535-47bb-0310-9956-ffa450edef68

--- oak-solr-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/solr/index/SolrIndexUpdate.java
+++ oak-solr-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/solr/index/SolrIndexUpdate.java
@@ -152,6 +152,7 @@ public class SolrIndexUpdate implements Closeable {
         if (!path.startsWith("/")) {
             path = "/" + path;
         }
+        path = path.replace("/", "\\/");
         solrServer.deleteByQuery(new StringBuilder(configuration.getPathField())
                 .append(':').append(path).append("*").toString());
     }
