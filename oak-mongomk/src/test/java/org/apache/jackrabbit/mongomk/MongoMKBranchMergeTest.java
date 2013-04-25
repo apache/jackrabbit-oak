3ceb4e4b815eeb2f2ec9e7f242955fae7f639dc9
Marcel Reutegger <mreutegg@apache.org>
2013-04-25 08:32:03 +0000

    OAK-619 Lock-free MongoMK implementation - Improved collision detection - More conflict tests with branches (some ignored because conflict detection is not yet complete)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1475669 13f79535-47bb-0310-9956-ffa450edef68

--- oak-mongomk/src/test/java/org/apache/jackrabbit/mongomk/MongoMKBranchMergeTest.java
+++ oak-mongomk/src/test/java/org/apache/jackrabbit/mongomk/MongoMKBranchMergeTest.java
@@ -279,6 +279,27 @@ public class MongoMKBranchMergeTest extends BaseMongoMKTest {
     }
 
     @Test
+    public void propertyConflictWithMergedBranch() {
+        addNodes(null, "/trunk");
+        String rev = setProp(null, "/trunk/prop1", "value1");
+        assertPropExists(null, "/trunk", "prop1");
+
+        String branchRev = mk.branch(null);
+
+        branchRev = setProp(branchRev, "/trunk/prop1", "value1a");
+        assertPropValue(branchRev, "/trunk", "prop1", "value1a");
+
+        mk.merge(branchRev, "");
+        assertPropValue(null, "/trunk", "prop1", "value1a");
+        try {
+            setProp(rev, "/trunk/prop1", "value1b");
+            fail("Expected: Concurrent modification exception");
+        } catch (Exception expected) {
+            // expected
+        }
+    }
+
+    @Test
     public void oneBranchChangedPropertiesWithConflict() {
         addNodes(null, "/trunk");
         setProp(null, "/trunk/prop1", "value1");
