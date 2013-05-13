d60f94897be8d6568471d0c44a982d20a84f7129
Marcel Reutegger <mreutegg@apache.org>
2013-05-13 13:21:12 +0000

    OAK-815: Wrong type for empty multi-valued property
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1481838 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/version/VersionableTest.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.jcr.version;
+
+import javax.jcr.Node;
+import javax.jcr.PropertyType;
+import javax.jcr.RepositoryException;
+import javax.jcr.version.VersionManager;
+
+import org.apache.jackrabbit.test.AbstractJCRTest;
+
+/**
+ * <code>VersionableTest</code> contains tests for method relevant to
+ * versionable nodes.
+ */
+public class VersionableTest extends AbstractJCRTest {
+
+    public void testGetTypeOfPredecessors() throws RepositoryException {
+        Node node = testRootNode.addNode(nodeName1, testNodeType);
+        node.addMixin(mixVersionable);
+        superuser.save();
+        VersionManager vMgr = superuser.getWorkspace().getVersionManager();
+        vMgr.checkin(node.getPath());
+        assertEquals(PropertyType.nameFromValue(PropertyType.REFERENCE),
+                PropertyType.nameFromValue(node.getProperty(jcrPredecessors).getType()));
+    }
+}
