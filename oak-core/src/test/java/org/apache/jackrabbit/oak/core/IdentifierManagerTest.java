20cb31e73a8ddd9559e851bba066bc6c04f020b2
Angela Schreiber <angela@apache.org>
2013-05-15 07:32:14 +0000

    minor improvement: move IdentifierManagerTest from plugins/identifier to core package (consistency)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1482708 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/core/IdentifierManagerTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/core/IdentifierManagerTest.java
@@ -16,11 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.jackrabbit.oak.plugins.identifier;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
+package org.apache.jackrabbit.oak.core;
 
 import org.apache.jackrabbit.JcrConstants;
 import org.apache.jackrabbit.oak.Oak;
@@ -29,12 +25,15 @@ import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.api.Root;
 import org.apache.jackrabbit.oak.api.Tree;
 import org.apache.jackrabbit.oak.api.Type;
-import org.apache.jackrabbit.oak.core.IdentifierManager;
 import org.apache.jackrabbit.oak.plugins.memory.PropertyStates;
 import org.apache.jackrabbit.oak.plugins.nodetype.write.InitialContent;
 import org.apache.jackrabbit.oak.spi.security.OpenSecurityProvider;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
 public class IdentifierManagerTest {
     private static final String UUID_Y = IdentifierManager.generateUUID();
