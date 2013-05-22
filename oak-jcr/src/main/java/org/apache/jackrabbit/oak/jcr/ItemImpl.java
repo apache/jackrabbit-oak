f38e2de8438590d44f3ba91e6cffbcec795dcbc7
Jukka Zitting <jukka@apache.org>
2013-05-22 07:14:35 +0000

    OAK-510: Multivalued properties with array size 0 forget their type
    Default to string when no other type hints are available
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1485097 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/ItemImpl.java
+++ oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/ItemImpl.java
@@ -430,8 +430,12 @@ abstract class ItemImpl<T extends ItemDelegate> implements Item {
         Value[] nonNullValues = compact(values);
         int targetType = getType(definition, type);
         if (nonNullValues.length == 0) {
+            if (targetType == PropertyType.UNDEFINED) {
+                // default to string when no other type hints are available
+                targetType = PropertyType.STRING;
+            }
             return MemoryPropertyBuilder
-                    .array(Type.fromTag(type, false), name)
+                    .array(Type.fromTag(targetType, false), name)
                     .getPropertyState();
         } else if (targetType == type) {
             return PropertyStates.createProperty(name, Arrays.asList(nonNullValues));
