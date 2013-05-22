29eaa099d793d57ad9a61a2c57b1e288e566ddf0
Jukka Zitting <jukka@apache.org>
2013-05-22 06:36:09 +0000

    OAK-510: Multivalued properties with array size 0 forget their type
    Remove test that explicitly expects the wrong behavior
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1485084 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/RepositoryTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/RepositoryTest.java
@@ -986,26 +986,6 @@ public class RepositoryTest extends AbstractRepositoryTest {
     }
 
     @Test
-    public void testEmptyMultiValuedPropertyType() throws RepositoryException {
-        Node parentNode = getNode(TEST_PATH);
-        Value[] values = new Value[0];
-
-        parentNode.setProperty("multi empty", values, PropertyType.BOOLEAN);
-        parentNode.getSession().save();
-
-        Session session2 = createAnonymousSession();
-        try {
-            Property property = session2.getProperty(TEST_PATH + "/multi empty");
-            assertTrue(property.isMultiple());
-            assertEquals(PropertyType.STRING, property.getType());
-            Value[] values2 = property.getValues();
-            assertEquals(0, values2.length);
-        } finally {
-            session2.logout();
-        }
-    }
-
-    @Test
     public void addMultiValuedStringWithNull() throws RepositoryException {
         Node parentNode = getNode(TEST_PATH);
         Value[] values = new Value[3];
