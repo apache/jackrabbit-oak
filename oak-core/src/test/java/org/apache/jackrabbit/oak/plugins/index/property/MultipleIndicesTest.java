995616b0c89c1c3b0b9d317ba7430232fd06e55b
Thomas Mueller <thomasm@apache.org>
2013-05-27 10:01:05 +0000

    OAK-841 Indexing empty string fails with "there's already a child node with name ':index'" (additional test case)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1486555 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/property/MultipleIndicesTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/property/MultipleIndicesTest.java
@@ -103,11 +103,18 @@ public class MultipleIndicesTest extends AbstractQueryTest {
         t.addChild("node-2").setProperty("pid", "");
         root.commit();
 
+        t = root.getTree("/");
+        t.addChild("node-3").setProperty("pid", ":");
+        root.commit();
+
         setTravesalFallback(false);
         assertQuery("select [jcr:path] from [nt:base] where [pid] = 'value'",
                 ImmutableList.of("/node-1"));
         assertQuery("select [jcr:path] from [nt:base] where [pid] = ''",
                 ImmutableList.of("/node-2"));
+        assertQuery("select [jcr:path] from [nt:base] where [pid] = ':'",
+                ImmutableList.of("/node-3"));
+        
         setTravesalFallback(true);
     }
 }
