dc7f2d0ecd96725146ec3b1a3c4c52317be9906b
Jukka Zitting <jukka@apache.org>
2013-06-13 11:24:02 +0000

    OAK-867: Oak whiteboard
    Initial interface and basic implementations
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1492626 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/jackrabbit/oak/spi/whiteboard/DefaultWhiteboard.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.spi.whiteboard;
+
+import java.util.Dictionary;
+
+public class DefaultWhiteboard implements Whiteboard {
+
+    @Override
+    public <T> Registration register(
+            final Class<T> type, final T service,
+            final Dictionary<?, ?> properties) {
+        registered(type, service, properties);
+        return new Registration() {
+            @Override
+            public void unregister() {
+                unregistered(type, service, properties);
+            }
+        };
+    }
+
+    //---------------------------------------------------------< protected >--
+
+    protected void registered(
+            Class<?> type, Object service, Dictionary<?, ?> properties) {
+    }
+
+    protected void unregistered(
+            Class<?> type, Object service, Dictionary<?, ?> properties) {
+    }
+
+}
