5f066e4696cdeb2bd17732e6d0673f136770411e
Michael Duerig <mduerig@apache.org>
2013-06-14 19:24:59 +0000

    OAK-781: Clarify / fix effects of MISSING_NODE as base state of NodeBuilder Disallow removing root node. Avoid NPE
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1493222 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/memory/MemoryNodeBuilder.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/memory/MemoryNodeBuilder.java
@@ -269,7 +269,7 @@ public class MemoryNodeBuilder implements NodeBuilder {
 
     @Override
     public boolean remove() {
-        if (exists()) {
+        if (!isRoot() && exists()) {
             head().getMutableNodeState();  // Make sure the removed node is connected
             parent.head().getMutableNodeState().removeChildNode(name);
             return true;
