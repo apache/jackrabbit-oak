0c605a7d27f027ca7b14d03279cfeaf4d96edcb3
Angela Schreiber <angela@apache.org>
2013-06-26 12:50:11 +0000

    OAK-873 : Wrong readStatus resolution when using glob restriction and write permission
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1496908 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/authorization/ReadTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/authorization/ReadTest.java
@@ -378,4 +378,37 @@ public class ReadTest extends AbstractEvaluationTest {
             superuser.save();
         }
     }
+
+    @Test
+    public void testGlobRestriction4()throws Exception{
+        Node a = superuser.getNode(path).addNode("a");
+        allow(path, readPrivileges);
+        deny(path, readPrivileges, createGlobRestriction("*/anotherpath"));
+
+        String aPath = a.getPath();
+        assertTrue(testSession.nodeExists(aPath));
+        Node n = testSession.getNode(aPath);
+
+        Node test = testSession.getNode(path);
+        assertTrue(test.hasNode("a"));
+        Node n2 = test.getNode("a");
+        assertTrue(n.isSame(n2));
+    }
+
+    @Test
+    public void testGlobRestriction5()throws Exception{
+        Node a = superuser.getNode(path).addNode("a");
+        allow(path, readPrivileges);
+        deny(path, readPrivileges, createGlobRestriction("*/anotherpath"));
+        allow(a.getPath(), repWritePrivileges);
+
+        String aPath = a.getPath();
+        assertTrue(testSession.nodeExists(aPath));
+        Node n = testSession.getNode(aPath);
+
+        Node test = testSession.getNode(path);
+        assertTrue(test.hasNode("a"));
+        Node n2 = test.getNode("a");
+        assertTrue(n.isSame(n2));
+    }
 }
