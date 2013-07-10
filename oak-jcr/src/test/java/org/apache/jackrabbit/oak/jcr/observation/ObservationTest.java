4b8a283652ba5941d7a6171aa63ecee9cec7234a
Michael Duerig <mduerig@apache.org>
2013-07-10 12:35:11 +0000

    OAK-144 Implement observation Correctly offset id when path filter is set
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1501751 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/observation/ObservationTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/observation/ObservationTest.java
@@ -155,6 +155,32 @@ public class ObservationTest extends AbstractRepositoryTest {
     }
 
     @Test
+    public void identifier() throws RepositoryException, InterruptedException, ExecutionException {
+        ExpectationListener listener = new ExpectationListener();
+        observationManager.addEventListener(listener, NODE_ADDED, TEST_PATH, true, null, null, false);
+        try {
+            Node n = getNode(TEST_PATH);
+            listener.expect(new Expectation("Has correct id") {
+                @Override
+                public boolean onEvent(Event event) throws Exception {
+                    return (TEST_PATH + "/newNode").equals(event.getIdentifier());
+                }
+            });
+
+            n.addNode("newNode");
+            getAdminSession().save();
+
+            List<Expectation> missing = listener.getMissing(2, TimeUnit.SECONDS);
+            assertTrue("Missing events: " + missing, missing.isEmpty());
+            List<Event> unexpected = listener.getUnexpected();
+            assertTrue("Unexpected events: " + unexpected, unexpected.isEmpty());
+        }
+        finally {
+            observationManager.removeEventListener(listener);
+        }
+    }
+
+    @Test
     public void observationOnRootNode() throws Exception {
         ExpectationListener listener = new ExpectationListener();
         observationManager.addEventListener(listener, PROPERTY_ADDED, "/", true, null, null, false);
