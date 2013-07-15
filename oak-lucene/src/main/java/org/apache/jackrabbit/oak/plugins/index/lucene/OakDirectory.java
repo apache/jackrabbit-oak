9be779031c4edb89e5468244225244253110fa62
Jukka Zitting <jukka@apache.org>
2013-07-15 11:22:17 +0000

    OAK-895: Random access for Lucene index binaries
    Increase the blob size to more than the MongoMK inline limit
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1503177 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/OakDirectory.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/OakDirectory.java
@@ -112,7 +112,7 @@ class OakDirectory extends Directory {
         // do nothing
     }
 
-    private static final int BLOB_SIZE = 4092;
+    private static final int BLOB_SIZE = 10 * 1024; // > MongoMK inline limit
 
     private static class OakIndexFile {
 
