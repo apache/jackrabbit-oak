3d6ffb5962755cb0a40e1062d1720c3373555a5a
Angela Schreiber <angela@apache.org>
2013-07-25 16:49:10 +0000

    OAK-226: adding test case
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1507061 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/authorization/ReadPropertyTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/authorization/ReadPropertyTest.java
@@ -69,12 +69,11 @@ public class ReadPropertyTest extends AbstractEvaluationTest {
             assertTrue(testSession.itemExists(pPath));
             assertTrue(testSession.propertyExists(pPath));
             Property p = testSession.getProperty(pPath);
-            assertEquals(pPath, p.getPath());
         }
     }
 
     @Test
-    public void testReadPropertyParent() throws Exception {
+    public void testGetParent() throws Exception {
         deny(path, privilegesFromName(PrivilegeConstants.REP_READ_NODES));
 
         List<String> propertyPaths = new ArrayList<String>();
@@ -90,6 +89,22 @@ public class ReadPropertyTest extends AbstractEvaluationTest {
             } catch (AccessDeniedException e) {
                 // success
             }
+        }
+    }
+
+    @Test
+    public void testGetPath() throws Exception {
+        deny(path, privilegesFromName(PrivilegeConstants.JCR_READ));
+        allow(path, privilegesFromName(PrivilegeConstants.REP_READ_PROPERTIES));
+
+        List<String> propertyPaths = new ArrayList<String>();
+        propertyPaths.add(childPPath);
+        propertyPaths.add(childchildPPath);
+        propertyPaths.add(path + "/jcr:primaryType");
+
+        for (String pPath : propertyPaths) {
+            Property p = testSession.getProperty(pPath);
+            assertEquals(pPath, p.getPath());
         }
     }
 }
