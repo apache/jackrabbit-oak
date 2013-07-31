49ebb5bbc86895f342b28cf339d48ad49bffbe02
Angela Schreiber <angela@apache.org>
2013-07-31 09:12:28 +0000

    OAK-90 : Implement Principal Management
    - add tests for everyone group
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1508780 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/security/principal/PrincipalProviderImplTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/security/principal/PrincipalProviderImplTest.java
@@ -18,6 +18,7 @@ package org.apache.jackrabbit.oak.security.principal;
 
 import java.security.Principal;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -25,6 +26,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.google.common.collect.ImmutableSet;
 import org.apache.jackrabbit.api.security.principal.PrincipalManager;
 import org.apache.jackrabbit.api.security.user.Group;
 import org.apache.jackrabbit.api.security.user.User;
@@ -86,6 +88,38 @@ public class PrincipalProviderImplTest extends AbstractSecurityTest {
 
             Principal ep = principalProvider.getPrincipal(EveryonePrincipal.NAME);
             assertFalse(ep instanceof EveryonePrincipal);
+        } finally {
+            if (everyoneGroup != null) {
+                everyoneGroup.remove();
+                root.commit();
+            }
+        }
+    }
+
+    @Test
+    public void testEveryoneMembers() throws Exception {
+        Principal everyone = principalProvider.getPrincipal(EveryonePrincipal.NAME);
+        assertTrue(everyone instanceof EveryonePrincipal);
+
+        Group everyoneGroup = null;
+        try {
+            UserManager userMgr = getUserManager(root);
+            everyoneGroup = userMgr.createGroup(EveryonePrincipal.NAME);
+            root.commit();
+
+            Principal ep = principalProvider.getPrincipal(EveryonePrincipal.NAME);
+            Set<? extends Principal> everyoneMembers = ImmutableSet.copyOf(Collections.list(((java.security.acl.Group) ep).members()));
+
+            Iterator<? extends Principal> all = principalProvider.findPrincipals(PrincipalManager.SEARCH_TYPE_ALL);
+            while (all.hasNext()) {
+                Principal p = all.next();
+                if (everyone.equals(p)) {
+                    assertFalse(everyoneMembers.contains(p));
+                } else {
+                    assertTrue(everyoneMembers.contains(p));
+                }
+            }
+
         } finally {
             if (everyoneGroup != null) {
                 everyoneGroup.remove();
