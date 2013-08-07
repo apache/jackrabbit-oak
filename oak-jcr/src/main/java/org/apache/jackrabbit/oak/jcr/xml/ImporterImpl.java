942783e57b81ae97c767c270c0b932b1ef6a8e3f
Michael Dürig <mduerig@apache.org>
2013-08-07 13:31:42 +0000

    OAK-945: UserImporter should not cast UserManager to UserManagerImpl
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1511301 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/xml/ImporterImpl.java
+++ oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/xml/ImporterImpl.java
@@ -16,11 +16,14 @@
  */
 package org.apache.jackrabbit.oak.jcr.xml;
 
+import static org.apache.jackrabbit.oak.plugins.nodetype.NodeTypeConstants.NODE_TYPES_PATH;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
 import java.util.UUID;
+
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
 import javax.jcr.ImportUUIDBehavior;
@@ -60,8 +63,6 @@ import org.apache.jackrabbit.oak.spi.xml.ReferenceChangeTracker;
 import org.apache.jackrabbit.oak.util.TreeUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static org.apache.jackrabbit.oak.plugins.nodetype.NodeTypeConstants.NODE_TYPES_PATH;
 
 public class ImporterImpl implements Importer {
     private static final Logger log = LoggerFactory.getLogger(ImporterImpl.class);
@@ -150,7 +151,8 @@ public class ImporterImpl implements Importer {
 
         pItemImporters.clear();
         for (ProtectedItemImporter importer : sessionContext.getProtectedItemImporters()) {
-            if (importer.init(sessionContext.getSession(), root, sessionContext, isWorkspaceImport, uuidBehavior, refTracker)) {
+            // FIXME this passes the session scoped name path mapper also for workspace imports
+            if (importer.init(sessionContext.getSession(), root, sessionContext, isWorkspaceImport, uuidBehavior, refTracker, sessionContext.getSecurityProvider())) {
                 pItemImporters.add(importer);
             }
         }
