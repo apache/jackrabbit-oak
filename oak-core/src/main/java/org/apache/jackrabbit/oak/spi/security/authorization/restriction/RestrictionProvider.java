184dda60328323d0057329692898d63291c914b3
Angela Schreiber <angela@apache.org>
2013-08-07 16:49:08 +0000

    OAK-51 : Access Control Management (restriction related javadoc and minor improvement)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1511390 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/spi/security/authorization/restriction/RestrictionProvider.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/security/authorization/restriction/RestrictionProvider.java
@@ -26,30 +26,107 @@ import javax.jcr.security.AccessControlException;
 import org.apache.jackrabbit.oak.api.Tree;
 
 /**
- * RestrictionProvider... TODO
+ * Interface to manage the supported restrictions present with a given access
+ * control and permission management implementation.
+ *
+ * @since OAK 1.0
  */
 public interface RestrictionProvider {
 
+    /**
+     * Returns the restriction definitions supported by this provider implementation
+     * at the specified path.
+     *
+     * @param oakPath The path of the access controlled tree. A {@code null}
+     * path indicates that the supported restrictions for repository level
+     * policies should be returned.
+     * @return The set of supported restrictions at the given path.
+     */
     @Nonnull
     Set<RestrictionDefinition> getSupportedRestrictions(@Nullable String oakPath);
 
+    /**
+     * Creates a new single valued restriction for the specified parameters.
+     *
+     * @param oakPath The path of the access controlled tree or {@code null} if
+     * the target policies applies to the repository level.
+     * @param oakName The name of the restriction.
+     * @param value The value of the restriction.
+     * @return A new restriction instance.
+     * @throws AccessControlException If no matching restriction definition
+     * exists for the specified parameters.
+     * @throws RepositoryException If another error occurs.
+     */
     @Nonnull
     Restriction createRestriction(@Nullable String oakPath,
                                   @Nonnull String oakName,
-                                  @Nonnull Value value) throws RepositoryException;
+                                  @Nonnull Value value) throws AccessControlException, RepositoryException;
 
+    /**
+     * Creates a new multi valued restriction for the specified parameters.
+     *
+     * @param oakPath The path of the access controlled tree or {@code null} if
+     * the target policies applies to the repository level.
+     * @param oakName The name of the restriction.
+     * @param values The values of the restriction.
+     * @return A new restriction instance.
+     * @throws AccessControlException If no matching restriction definition
+     * exists for the specified parameters.
+     * @throws RepositoryException If another error occurs.
+     */
     @Nonnull
     Restriction createRestriction(@Nullable String oakPath,
                                   @Nonnull String oakName,
-                                  @Nonnull Value... values) throws RepositoryException;
+                                  @Nonnull Value... values) throws AccessControlException, RepositoryException;
 
+    /**
+     * Read the valid restrictions stored in the specified ACE tree.
+     *
+     * @param oakPath The path of the access controlled tree or {@code null} if
+     * the target policies applies to the repository level.
+     * @param aceTree The tree corresponding to an ACE that may contain
+     * restrictions.
+     * @return The valid restrictions stored with the specified tree or an
+     * empty set.
+     */
     @Nonnull
     Set<Restriction> readRestrictions(@Nullable String oakPath, @Nonnull Tree aceTree);
 
-    void writeRestrictions(String oakPath, Tree aceTree, Set<Restriction> restrictions) throws AccessControlException;
+    /**
+     * Writes the given restrictions to the specified ACE tree. Note, that this
+     * method does not validate the specified restrictions (see also
+     * {@link #validateRestrictions(String, org.apache.jackrabbit.oak.api.Tree)}).
+     *
+     * @param oakPath The path of the access controlled tree or {@code null} if
+     * the target policies applies to the repository level.
+     * @param aceTree The tree corresponding to an ACE that will have the
+     * specified restrictions added.
+     * @throws RepositoryException If an error occurs while writing the
+     * restrictions.
+     */
+    void writeRestrictions(String oakPath, Tree aceTree, Set<Restriction> restrictions) throws RepositoryException;
 
-    void validateRestrictions(@Nullable String oakPath, @Nonnull Tree aceTree) throws AccessControlException;
+    /**
+     * Validate the restrictions present with the specified ACE tree.
+     *
+     * @param oakPath The path of the access controlled tree or {@code null} if
+     * the target policies applies to the repository level.
+     * @param aceTree The tree corresponding to an ACE.
+     * @throws AccessControlException If any invalid restrictions are detected.
+     * @throws RepositoryException If another error occurs.
+     */
+    void validateRestrictions(@Nullable String oakPath, @Nonnull Tree aceTree) throws AccessControlException, RepositoryException;
 
+    /**
+     * Creates the {@link RestrictionPattern} for the restriction information
+     * stored with specified tree.
+     *
+     * @param oakPath The path of the access controlled tree or {@code null} if
+     * the target policies applies to the repository level.
+     * @param tree The tree holding the restriction information.
+     * @return A new {@link RestrictionPattern} representing the restriction
+     * information present with the given tree.
+     */
     @Nonnull
     RestrictionPattern getPattern(@Nullable String oakPath, @Nonnull Tree tree);
 }
