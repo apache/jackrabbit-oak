6e89091295877f17a3d0fe26d950ceaed0ebf3f9
Michael Duerig <mduerig@apache.org>
2013-08-16 11:23:47 +0000

    OAK-962: RootFuzzIT fails on MongoMK with seed -1091889749 @Ignored test case
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1514656 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/RepositoryTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/RepositoryTest.java
@@ -1595,6 +1595,20 @@ public class RepositoryTest extends AbstractRepositoryTest {
     }
 
     @Test
+    @Ignore("OAK-962")  // FIXME OAK-962
+    public void oak962() throws RepositoryException {
+        Session session = getAdminSession();
+        Node root = session.getRootNode().addNode("root");
+        root.addNode("N3");
+        root.addNode("N6").addNode("N7");
+        session.save();
+        session.move("/root/N6/N7", "/root/N3/N12");
+        root.getNode("N3").getNode("N12").remove();
+        root.getNode("N6").remove();
+        session.save();
+    }
+
+    @Test
     public void moveReferenceable() throws RepositoryException {
         Session session = getAdminSession();
 
