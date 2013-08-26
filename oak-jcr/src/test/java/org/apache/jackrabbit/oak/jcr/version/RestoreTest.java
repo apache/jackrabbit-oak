84a554a9437fd49151303ebb597876fb0fa7ed4c
Marcel Reutegger <mreutegg@apache.org>
2013-08-26 12:15:06 +0000

    OAK-977: Version restore may include jcr:frozenXXX properties
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1517515 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/version/RestoreTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/version/RestoreTest.java
@@ -21,6 +21,7 @@ import javax.jcr.RepositoryException;
 import javax.jcr.version.Version;
 import javax.jcr.version.VersionManager;
 
+import org.apache.jackrabbit.JcrConstants;
 import org.apache.jackrabbit.test.AbstractJCRTest;
 
 /**
@@ -45,5 +46,27 @@ public class RestoreTest extends AbstractJCRTest {
         assertTrue(superuser.nodeExists(path));
         n = superuser.getNode(path);
         assertEquals("Property not restored", "a", n.getProperty("prop").getString());
+    }
+
+    public void testRestoreReferenceableChild() throws RepositoryException {
+        VersionManager vMgr = superuser.getWorkspace().getVersionManager();
+        Node n = testRootNode.addNode(nodeName1, "test:versionable");
+        n.addMixin(mixVersionable);
+        n.setProperty("prop", "a");
+        Node child = n.addNode("test:copyOnParentVersion", ntUnstructured);
+        child.addMixin(mixReferenceable);
+        child.setProperty("prop", "a");
+        superuser.save();
+        String path = n.getPath();
+        Version v = vMgr.checkpoint(path); // 1.0
+        n.setProperty("prop", "b");
+        child.setProperty("prop", "b");
+        superuser.save();
+        vMgr.checkpoint(path); // 1.1
+        vMgr.restore(v, true);
+        assertEquals("Property not restored", "a", n.getProperty("prop").getString());
+        assertEquals("Property not restored", "a", child.getProperty("prop").getString());
+        assertFalse("Restored node must not have jcr:frozenPrimaryType property",
+                child.hasProperty(JcrConstants.JCR_FROZENPRIMARYTYPE));
     }
 }
