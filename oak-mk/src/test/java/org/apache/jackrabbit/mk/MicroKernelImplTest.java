029b3f63eae3b12e26fd3972c3e0bafa8dad9899
Michael Duerig <mduerig@apache.org>
2013-08-28 08:53:55 +0000

    OAK-979: MicroKernel.diff() returns incomplete JSON when moves are involved (H2) remove debugging output
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1518109 13f79535-47bb-0310-9956-ffa450edef68

--- oak-mk/src/test/java/org/apache/jackrabbit/mk/MicroKernelImplTest.java
+++ oak-mk/src/test/java/org/apache/jackrabbit/mk/MicroKernelImplTest.java
@@ -57,7 +57,6 @@ public class MicroKernelImplTest {
         String base = mk.commit("", "+\"/a\":{\"b\":{}}", mk.getHeadRevision(), null);
         String head = mk.commit("", "+\"/a/c\":{}+\"/a/d\":{}-\"/a/b\"", base, null);
         String diff = mk.diff(base, head, "/a", 0);
-        System.out.println(diff);
         assertTrue(diff.contains("c"));
         assertTrue(diff.contains("d"));
     }
