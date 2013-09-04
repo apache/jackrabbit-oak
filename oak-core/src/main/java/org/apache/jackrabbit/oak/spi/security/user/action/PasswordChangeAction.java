83f692e598d4b7aaf10d869f8eb1705472a3cc5b
Angela Schreiber <angela@apache.org>
2013-09-04 09:52:20 +0000

    OAK-50: user mgt
    - add PasswordChangeAction
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1519963 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/security/user/action/PasswordChangeAction.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.spi.security.user.action;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+import javax.jcr.RepositoryException;
+import javax.jcr.nodetype.ConstraintViolationException;
+
+import org.apache.jackrabbit.api.security.user.User;
+import org.apache.jackrabbit.oak.api.Root;
+import org.apache.jackrabbit.oak.namepath.NamePathMapper;
+import org.apache.jackrabbit.oak.spi.security.ConfigurationParameters;
+import org.apache.jackrabbit.oak.spi.security.SecurityProvider;
+import org.apache.jackrabbit.oak.spi.security.user.UserConstants;
+import org.apache.jackrabbit.oak.spi.security.user.util.PasswordUtil;
+import org.apache.jackrabbit.oak.util.TreeUtil;
+
+/**
+ * {@code PasswordChangeAction} asserts that the upon
+ * {@link #onPasswordChange(org.apache.jackrabbit.api.security.user.User, String,
+ * org.apache.jackrabbit.oak.api.Root, org.apache.jackrabbit.oak.namepath.NamePathMapper)}
+ * a different, non-null password is specified.
+ *
+ * @see org.apache.jackrabbit.api.security.user.User#changePassword(String)
+ * @see org.apache.jackrabbit.api.security.user.User#changePassword(String, String)
+ */
+public class PasswordChangeAction extends AbstractAuthorizableAction {
+
+    @Override
+    protected void init(SecurityProvider securityProvider, ConfigurationParameters config) {
+        // nothing to do
+    }
+
+    //-------------------------------------------------< AuthorizableAction >---
+    @Override
+    public void onPasswordChange(User user, String newPassword, Root root, NamePathMapper namePathMapper) throws RepositoryException {
+        if (newPassword == null) {
+            throw new ConstraintViolationException("Expected a new password that is not null.");
+        }
+        String pwHash = getPasswordHash(root, user);
+        if (PasswordUtil.isSame(pwHash, newPassword)) {
+            throw new ConstraintViolationException("New password is identical to the old password.");
+        }
+    }
+
+    //------------------------------------------------------------< private >---
+    @CheckForNull
+    private String getPasswordHash(@Nonnull Root root, @Nonnull User user) throws RepositoryException {
+        return TreeUtil.getString(root.getTree(user.getPath()), UserConstants.REP_PASSWORD);
+    }
+}
