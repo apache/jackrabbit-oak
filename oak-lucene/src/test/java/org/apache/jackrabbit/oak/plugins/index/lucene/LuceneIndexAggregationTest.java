7e96aa8380fea573e2074b4f962aaeb34b68aa2c
Alexandru Parvulescu <alexparvulescu@apache.org>
2013-09-09 15:09:46 +0000

    OAK-828 Full-text support for index aggregates  - added fix to prevent duplication of aggregation results
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1521152 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexAggregationTest.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexAggregationTest.java
@@ -278,4 +278,24 @@ public class LuceneIndexAggregationTest extends AbstractQueryTest {
 
     }
 
+    @Test
+    public void testPreventDoubleAggregation() throws Exception {
+        Tree file = root.getTree("/").addChild("myFile");
+        file.setProperty(JCR_PRIMARYTYPE, NT_FILE, Type.NAME);
+        file.setProperty("jcr:title", "fox");
+
+        Tree resource = file.addChild(JCR_CONTENT);
+        resource.setProperty(JCR_PRIMARYTYPE, "nt:resource", Type.NAME);
+        resource.setProperty("jcr:lastModified", Calendar.getInstance());
+        resource.setProperty("jcr:encoding", "UTF-8");
+        resource.setProperty("jcr:mimeType", "text/plain");
+        resource.setProperty(binaryProperty(JCR_DATA,
+                "the quick brown fox jumps over the lazy dog."));
+        root.commit();
+
+        String matchChildSimple = "//element(*, nt:file)[( jcr:contains(., 'fox') )]";
+        assertQuery(matchChildSimple, "xpath",
+                ImmutableList.of("/myFile"));
+    }
+
 }
