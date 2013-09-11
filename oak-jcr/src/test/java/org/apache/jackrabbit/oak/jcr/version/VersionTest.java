92617d3cbf6199a9f613d0f8cf648c3d9304edc1
Marcel Reutegger <mreutegg@apache.org>
2013-09-11 06:30:58 +0000

    OAK-999: Version creates frozenNode children with orignial NoteType instead of frozenNode
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1521744 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/version/VersionTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/version/VersionTest.java
@@ -72,4 +72,16 @@ public class VersionTest extends AbstractJCRTest {
         assertTrue(rows.hasNext());
         assertTrue(rows.nextRow().getNode() instanceof Version);
     }
+
+    public void testFrozenNode() throws RepositoryException {
+        Node n = testRootNode.addNode(nodeName1, testNodeType);
+        n.addMixin(mixVersionable);
+        Node child = n.addNode(nodeName2, ntUnstructured);
+        superuser.save();
+        VersionManager vMgr = superuser.getWorkspace().getVersionManager();
+        vMgr.checkpoint(n.getPath());
+        Version v = vMgr.getBaseVersion(n.getPath());
+        Node frozenChild = v.getFrozenNode().getNode(child.getName());
+        assertEquals(ntFrozenNode, frozenChild.getPrimaryNodeType().getName());
+    }
 }
