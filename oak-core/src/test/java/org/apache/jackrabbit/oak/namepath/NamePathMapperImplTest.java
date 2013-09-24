575831b797c001ee96cf02648ef1117bb543a212
Michael Duerig <mduerig@apache.org>
2013-09-24 08:51:03 +0000

    JCR-3562: Adding a child node named {foo fails but bar} works add test case from JCR-3562
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1525817 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/namepath/NamePathMapperImplTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/namepath/NamePathMapperImplTest.java
@@ -214,4 +214,13 @@ public class NamePathMapperImplTest {
         assertFalse(JcrPathParser.validate("/foo/../.."));
     }
 
+    @Test
+    public void testBracketsInNodeName() throws Exception {
+        String[] childNames = { "{A}", "B}", "{C", "(D)", "E)", "(F", };
+
+        for (String name : childNames) {
+            assertEquals(name, npMapper.getOakName(name));
+        }
+    }
+
 }
