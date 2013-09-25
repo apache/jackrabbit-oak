e6f04fc8d56bae0e9ad7bea9452fe4c7aa1b1c2a
Jukka Zitting <jukka@apache.org>
2013-09-25 18:23:05 +0000

    OAK-1031: SegmentMK: Fewer segment lookups
    Improve the Record base class in preparation for broader changes.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1526253 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/segment/Record.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/segment/Record.java
@@ -16,16 +16,59 @@
  */
 package org.apache.jackrabbit.oak.plugins.segment;
 
+/**
+ * Record within a segment.
+ */
 class Record {
 
+    /**
+     * Identifier of this record.
+     */
     private final RecordId id;
 
     protected Record(RecordId id) {
         this.id = id;
     }
 
+    /**
+     * Returns the identifier of this record.
+     *
+     * @return record identifier
+     */
     public RecordId getRecordId() {
         return id;
+    }
+
+    /**
+     * Returns the segment offset of this record.
+     *
+     * @return segment offset of this record
+     */
+    protected int getOffset() {
+        return id.getOffset();
+    }
+
+    /**
+     * Returns the segment offset of the given byte position in this record.
+     *
+     * @param position byte position within this record
+     * @return segment offset of the given byte position
+     */
+    protected int getOffset(int position) {
+        return getOffset() + position;
+    }
+
+    /**
+     * Returns the segment offset of a byte position in this record.
+     * The position is calculated from the given number of raw bytes and
+     * record identifiers.
+     *
+     * @param bytes number of raw bytes before the position
+     * @param ids number of record identifiers before the position
+     * @return segment offset of the specified byte position
+     */
+    protected int getOffset(int bytes, int ids) {
+        return getOffset(bytes + ids * Segment.RECORD_ID_BYTES);
     }
 
 }
