9f510b8bbd266b22d41aade5c589613cecf33582
Alexandru Parvulescu <alexparvulescu@apache.org>
2013-09-27 08:55:58 +0000

    OAK-1022 Add a custom Oak Lucene analizer
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1526836 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/OakAnalyzer.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/OakAnalyzer.java
@@ -16,23 +16,16 @@
  */
 package org.apache.jackrabbit.oak.plugins.index.lucene;
 
-import java.io.IOException;
 import java.io.Reader;
 
-import org.apache.jackrabbit.oak.plugins.index.lucene.util.OakWordTokenFilter;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
-import org.apache.lucene.analysis.standard.StandardFilter;
-import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.core.WhitespaceTokenizer;
+import org.apache.lucene.analysis.miscellaneous.WordDelimiterFilter;
 import org.apache.lucene.util.Version;
 
 public class OakAnalyzer extends Analyzer {
-
-    /** Default maximum allowed token length */
-    public static final int DEFAULT_MAX_TOKEN_LENGTH = 255;
-
-    private int maxTokenLength = DEFAULT_MAX_TOKEN_LENGTH;
 
     private final Version matchVersion;
 
@@ -47,36 +40,16 @@ public class OakAnalyzer extends Analyzer {
         this.matchVersion = matchVersion;
     }
 
-    /**
-     * Set maximum allowed token length. If a token is seen that exceeds this
-     * length then it is discarded. This setting only takes effect the next time
-     * tokenStream or tokenStream is called.
-     */
-    public void setMaxTokenLength(int length) {
-        maxTokenLength = length;
-    }
-
-    /**
-     * @see #setMaxTokenLength
-     */
-    public int getMaxTokenLength() {
-        return maxTokenLength;
-    }
-
     @Override
     protected TokenStreamComponents createComponents(final String fieldName,
             final Reader reader) {
-        final StandardTokenizer src = new StandardTokenizer(matchVersion, reader);
-        src.setMaxTokenLength(maxTokenLength);
-        TokenStream tok = new StandardFilter(matchVersion, src);
-        tok = new LowerCaseFilter(matchVersion, tok);
-        tok = new OakWordTokenFilter(matchVersion, tok);
-        return new TokenStreamComponents(src, tok) {
-            @Override
-            protected void setReader(final Reader reader) throws IOException {
-                src.setMaxTokenLength(OakAnalyzer.this.maxTokenLength);
-                super.setReader(reader);
-            }
-        };
+        WhitespaceTokenizer src = new WhitespaceTokenizer(matchVersion, reader);
+        TokenStream tok = new LowerCaseFilter(matchVersion, src);
+        tok = new WordDelimiterFilter(tok,
+                WordDelimiterFilter.GENERATE_WORD_PARTS
+                        | WordDelimiterFilter.STEM_ENGLISH_POSSESSIVE
+                        | WordDelimiterFilter.GENERATE_NUMBER_PARTS, null);
+
+        return new TokenStreamComponents(src, tok);
     }
 }
