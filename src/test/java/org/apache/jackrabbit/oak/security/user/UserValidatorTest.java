d4c793cd4e22f40115915f8dc3d4247d98a92a30
Angela Schreiber <angela@apache.org>
2013-09-30 15:31:56 +0000

    OAK-1054 : Folder containing an admin user should not be removed
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1527612 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/jackrabbit/oak/security/user/UserValidatorTest.java
+++ src/test/java/org/apache/jackrabbit/oak/security/user/UserValidatorTest.java
@@ -209,6 +209,27 @@ public class UserValidatorTest extends AbstractSecurityTest implements UserConst
     }
 
     @Test
+    public void testRemoveAdminUserFolder() throws Exception {
+        try {
+            String adminId = getConfig().getConfigValue(PARAM_ADMIN_ID, DEFAULT_ADMIN_ID);
+            UserManager userMgr = getUserManager(root);
+            Authorizable admin = userMgr.getAuthorizable(adminId);
+            if (admin == null) {
+                admin = userMgr.createUser(adminId, adminId);
+                root.commit();
+            }
+
+            root.getTree(admin.getPath()).getParent().remove();
+            root.commit();
+            fail("Admin user cannot be removed");
+        } catch (CommitFailedException e) {
+            // success
+        } finally {
+            root.refresh();
+        }
+    }
+
+    @Test
     public void testDisableAdminUser() throws Exception {
         try {
             String adminId = getConfig().getConfigValue(PARAM_ADMIN_ID, DEFAULT_ADMIN_ID);
