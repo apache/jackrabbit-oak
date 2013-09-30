3c9ac51b53eac44036633f4b44b8b242fac7398c
Angela Schreiber <angela@apache.org>
2013-09-30 15:31:56 +0000

    OAK-1054 : Folder containing an admin user should not be removed
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1527612 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/security/user/UserValidator.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/security/user/UserValidator.java
@@ -56,7 +56,7 @@ class UserValidator extends DefaultValidator implements UserConstants {
         this.parentAfter = parentAfter;
         this.provider = provider;
 
-        authorizableType = UserUtil.getType(parentAfter);
+        authorizableType = (parentAfter == null) ? null : UserUtil.getType(parentAfter);
     }
 
     //----------------------------------------------------------< Validator >---
@@ -149,12 +149,17 @@ class UserValidator extends DefaultValidator implements UserConstants {
 
     @Override
     public Validator childNodeDeleted(String name, NodeState before) throws CommitFailedException {
-        Tree node = parentBefore.getChild(name);
-        if (isAdminUser(node)) {
-            String msg = "The admin user cannot be removed.";
-            throw constraintViolation(27, msg);
+        Tree tree = parentBefore.getChild(name);
+        AuthorizableType type = UserUtil.getType(tree);
+        if (type == AuthorizableType.USER || type == AuthorizableType.GROUP) {
+            if (isAdminUser(tree)) {
+                String msg = "The admin user cannot be removed.";
+                throw constraintViolation(27, msg);
+            }
+            return null;
+        } else {
+            return new VisibleValidator(new UserValidator(tree, null, provider), true, true);
         }
-        return null;
     }
 
     //------------------------------------------------------------< private >---
