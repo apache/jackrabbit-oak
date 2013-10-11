d56de1b60587ebb6f09e8f2dd35fa97e39a1ecb7
Angela Schreiber <angela@apache.org>
2013-10-11 07:28:30 +0000

    OAK-527: testing accessibility of version information if versionable node is removed
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1531206 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/authorization/VersionManagementTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/authorization/VersionManagementTest.java
@@ -295,6 +295,42 @@ public class VersionManagementTest extends AbstractEvaluationTest {
      * @since oak
      */
     @Test
+    public void testAccessVersionHistoryVersionableNodeRemoved() throws Exception {
+        Node n = createVersionableNode(superuser.getNode(path));
+        allow(n.getPath(), versionPrivileges);
+
+        n.checkin();
+        n.checkout();
+
+        String versionablePath = n.getPath();
+        VersionHistory vh = n.getVersionHistory();
+        String vhPath = vh.getPath();
+        String vhUUID = vh.getIdentifier();
+
+        // remove the versionable node
+        n.remove();
+        superuser.save();
+
+        testSession.refresh(false);
+        assertTrue(testSession.nodeExists(path));
+        assertFalse(testSession.nodeExists(versionablePath));
+
+        // accessing the version history directly should still succeed as
+        // read permission is still granted on the tree defined by the parent.
+        VersionHistory history = (VersionHistory) testSession.getNode(vhPath);
+        history = (VersionHistory) testSession.getNodeByIdentifier(vhUUID);
+        history = (VersionHistory) testSession.getNodeByUUID(vhUUID);
+
+        // revoking read permission on the parent node -> version history
+        // must no longer be accessible
+        modify(path, Privilege.JCR_READ, false);
+        assertFalse(testSession.nodeExists(vhPath));
+    }
+
+    /**
+     * @since oak
+     */
+    @Test
     public void testAddVersionLabel() throws Exception {
         Node n = createVersionableNode(superuser.getNode(path));
         allow(n.getPath(), versionPrivileges);
