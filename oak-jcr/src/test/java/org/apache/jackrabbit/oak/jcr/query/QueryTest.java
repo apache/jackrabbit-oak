d7f0f180356798d32e8d08d276f256b75fb060b9
Alexandru Parvulescu <alexparvulescu@apache.org>
2013-10-14 13:02:24 +0000

    OAK-1093 IllegalArgumentException on Row.getValues()
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1531880 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/query/QueryTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/query/QueryTest.java
@@ -349,4 +349,25 @@ public class QueryTest extends AbstractRepositoryTest {
         assertEquals("", getPaths(q));
     }
 
+    /**
+     * OAK-1093
+     */
+    @Test
+    public void getValuesOnMvp() throws RepositoryException {
+        Session session = getAdminSession();
+        Node hello = session.getRootNode().addNode("hello");
+        hello.setProperty("id", "1");
+        hello.setProperty("properties", new String[] { "p1", "p2" });
+        session.save();
+
+        QueryManager qm = session.getWorkspace().getQueryManager();
+        Query q = qm.createQuery("select properties from [nt:base] where id = 1",
+                Query.JCR_SQL2);
+
+        QueryResult r = q.execute();
+        RowIterator it = r.getRows();
+        assertTrue(it.hasNext());
+        Row row = it.nextRow();
+        assertEquals("p1 p2", row.getValues()[0].getString());
+    }
 }
