2cf70c9ebe8027a2e95bbd7c85226d5822cb7310
Jukka Zitting <jukka@apache.org>
2013-10-18 20:24:44 +0000

    OAK-987: Implement the MicroKernel API
    Various JSON/P cleanups
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1533617 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/kernel/JsopDiff.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/kernel/JsopDiff.java
@@ -16,10 +16,7 @@
  */
 package org.apache.jackrabbit.oak.kernel;
 
-import java.io.IOException;
-
 import org.apache.jackrabbit.mk.json.JsopBuilder;
-import org.apache.jackrabbit.oak.api.Blob;
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.commons.PathUtils;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
@@ -30,40 +27,29 @@ import org.apache.jackrabbit.oak.spi.state.NodeStateDiff;
  */
 public class JsopDiff implements NodeStateDiff {
 
-    private final KernelNodeStore store;
-
     private final JsopBuilder jsop;
+
+    private final BlobSerializer blobs;
 
     protected final String path;
 
-    JsopDiff(KernelNodeStore store, JsopBuilder jsop, String path) {
-        this.store = store;
+    private JsopDiff(JsopBuilder jsop, String path, BlobSerializer blobs) {
         this.jsop = jsop;
         this.path = path;
+        this.blobs = blobs;
     }
 
-    JsopDiff(KernelNodeStore store) {
-        this(store, new JsopBuilder(), "/");
+
+    JsopDiff(BlobSerializer blobs) {
+        this(new JsopBuilder(), "/", blobs);
     }
 
-    /**
-     * Create the jsop diff between {@code before} and {@code after} and
-     * stores binaries to {@code store}.
-     *
-     * @param store   node store for storing binaries.
-     * @param before  before node state
-     * @param after   after node state
-     * @param path    base path
-     * @param jsop    builder to feed in the diff
-     */
-    public static void diffToJsop(
-            KernelNodeStore store, NodeState before, NodeState after,
-            String path, JsopBuilder jsop) {
-        after.compareAgainstBaseState(before, new JsopDiff(store, jsop, path));
+    JsopDiff() {
+        this(new BlobSerializer());
     }
 
     /**
-     * Create the jsop diff between {@code before} and {@code after} for
+     * Create the JSOP diff between {@code before} and {@code after} for
      * debugging purposes.
      * <p>
      * This method does not store binaries but returns them inlined
@@ -75,33 +61,9 @@ public class JsopDiff implements NodeStateDiff {
      * @return  jsop diff between {@code before} and {@code after}
      */
     public static String diffToJsop(NodeState before, NodeState after) {
-        class ToStringDiff extends JsopDiff {
-            public ToStringDiff() {
-                super(null);
-            }
-
-            public ToStringDiff(JsopBuilder jsop, String path) {
-                super(null, jsop, path);
-            }
-
-            @Override
-            protected String writeBlob(Blob blob) {
-                return "Blob{" + blob + '}';
-            }
-
-            @Override
-            protected JsopDiff createChildDiff(JsopBuilder jsop, String path) {
-                return new ToStringDiff(jsop, path);
-            }
-        }
-
-        JsopDiff diff = new ToStringDiff();
+        JsopDiff diff = new JsopDiff();
         after.compareAgainstBaseState(before, diff);
         return diff.toString();
-    }
-
-    protected JsopDiff createChildDiff(JsopBuilder jsop, String path) {
-        return new JsopDiff(store, jsop, path);
     }
 
     //-----------------------------------------------------< NodeStateDiff >--
@@ -109,14 +71,14 @@ public class JsopDiff implements NodeStateDiff {
     @Override
     public boolean propertyAdded(PropertyState after) {
         jsop.tag('^').key(buildPath(after.getName()));
-        new DiffJsonSerializer().serialize(after);
+        new JsonSerializer(jsop, blobs).serialize(after);
         return true;
     }
 
     @Override
     public boolean propertyChanged(PropertyState before, PropertyState after) {
         jsop.tag('^').key(buildPath(after.getName()));
-        new DiffJsonSerializer().serialize(after);
+        new JsonSerializer(jsop, blobs).serialize(after);
         return true;
     }
 
@@ -129,7 +91,7 @@ public class JsopDiff implements NodeStateDiff {
     @Override
     public boolean childNodeAdded(String name, NodeState after) {
         jsop.tag('+').key(buildPath(name));
-        new DiffJsonSerializer().serialize(after);
+        new JsonSerializer(jsop, blobs).serialize(after);
         return true;
     }
 
@@ -141,8 +103,8 @@ public class JsopDiff implements NodeStateDiff {
 
     @Override
     public boolean childNodeChanged(String name, NodeState before, NodeState after) {
-        String path = buildPath(name);
-        after.compareAgainstBaseState(before, createChildDiff(jsop, path));
+        after.compareAgainstBaseState(
+                before, new JsopDiff(jsop, buildPath(name), blobs));
         return true;
     }
 
@@ -157,52 +119,6 @@ public class JsopDiff implements NodeStateDiff {
 
     protected String buildPath(String name) {
         return PathUtils.concat(path, name);
-    }
-
-    /**
-     * Make sure {@code blob} is persisted and return the id of
-     * the persisted blob.
-     *
-     * @param blob  blob to persist
-     * @return  id of the persisted blob
-     */
-    protected String writeBlob(Blob blob) {
-        KernelBlob kernelBlob;
-        if (blob instanceof KernelBlob) {
-            kernelBlob = (KernelBlob) blob;
-        } else {
-            try {
-                kernelBlob = store.createBlob(blob.getNewStream());
-            } catch (IOException e) {
-                throw new IllegalStateException(e);
-            }
-        }
-        return kernelBlob.getBinaryID();
-    }
-
-    private class DiffJsonSerializer extends JsonSerializer {
-
-        DiffJsonSerializer() {
-            super(jsop);
-        }
-
-        @Override
-        protected JsonSerializer getChildSerializer() {
-            return new DiffJsonSerializer();
-        }
-
-        /**
-         * Make sure {@code blob} is persisted and return the id of
-         * the persisted blob.
-         *
-         * @param blob  blob to persist
-         * @return  id of the persisted blob
-         */
-        @Override
-        protected String getBlobId(Blob blob) {
-            return writeBlob(blob);
-        }
-
     }
 
 }
