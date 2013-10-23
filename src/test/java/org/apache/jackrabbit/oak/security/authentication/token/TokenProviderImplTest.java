522b26005372cafcff7104d4a2b82655c7ceb38b
Angela Schreiber <angela@apache.org>
2013-10-23 18:02:03 +0000

    OAK-91 : authentication (wip)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1535084 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/jackrabbit/oak/security/authentication/token/TokenProviderImplTest.java
+++ src/test/java/org/apache/jackrabbit/oak/security/authentication/token/TokenProviderImplTest.java
@@ -250,6 +250,30 @@ public class TokenProviderImplTest extends AbstractTokenTest {
     }
 
     @Test
+    public void testGetTokenInfoFromInvalidLocation4() throws Exception {
+        TokenInfo info = tokenProvider.createToken(userId, Collections.<String, Object>emptyMap());
+        Tree tokenTree = getTokenTree(info);
+
+        assertNotNull(tokenProvider.getTokenInfo(info.getToken()));
+
+        TokenInfo info2 = null;
+        try {
+            Tree adminTree = root.getTree(getUserManager(root).getAuthorizable(adminSession.getAuthInfo().getUserID()).getPath());
+            NodeUtil node = new NodeUtil(adminTree).getOrAddChild(".tokens", "nt:unstructured");
+            assertTrue(root.move(tokenTree.getPath(), node.getTree().getPath() + "/" + tokenTree.getName()));
+            root.commit();
+
+            info2 = tokenProvider.getTokenInfo(info.getToken());
+            assertNotNull(info2);
+            assertFalse(info2.matches(new TokenCredentials(info.getToken())));
+        } finally {
+            Tree t = getTokenTree(info2);
+            t.remove();
+            root.commit();
+        }
+    }
+
+    @Test
     public void testGetTokenInfo() throws Exception {
         String token = tokenProvider.createToken(userId, Collections.<String, Object>emptyMap()).getToken();
         TokenInfo info = tokenProvider.getTokenInfo(token);
