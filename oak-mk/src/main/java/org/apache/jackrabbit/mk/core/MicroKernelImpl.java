d3298697889f030eb62c5d3f9b51f2cd8702ef87
Marcel Reutegger <mreutegg@apache.org>
2013-10-29 08:19:06 +0000

    OAK-1122: Empty branch commit returns head revision on trunk
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1536604 13f79535-47bb-0310-9956-ffa450edef68

--- oak-mk/src/main/java/org/apache/jackrabbit/mk/core/MicroKernelImpl.java
+++ oak-mk/src/main/java/org/apache/jackrabbit/mk/core/MicroKernelImpl.java
@@ -411,7 +411,7 @@ public class MicroKernelImpl implements MicroKernel {
             throw new IllegalArgumentException("absolute path expected: " + path);
         }
         if (jsonDiff == null || jsonDiff.length() == 0) {
-            return getHeadRevision();
+            return revisionId != null ? revisionId : getHeadRevision();
         }
 
         Id revId = revisionId == null ? getHeadRevisionId() : Id.fromString(revisionId);
