78bda2dc4b26368332dc5dc7f1c39d64a12ca296
Thomas Mueller <thomasm@apache.org>
2013-10-30 13:37:45 +0000

    OAK-1128 Conditions of the form "(x=1 or x=2) or y=3" are not processed correctly
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1537082 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/query/QueryTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/query/QueryTest.java
@@ -372,7 +372,7 @@ public class QueryTest extends AbstractRepositoryTest {
     }
 
     @Test
-    public void testOak1096() throws RepositoryException, InterruptedException {
+    public void testOak1096() throws RepositoryException {
         Session writer = createAdminSession();
         Session reader = createAdminSession();
         try {
@@ -394,4 +394,23 @@ public class QueryTest extends AbstractRepositoryTest {
             }
         }
     }
+    
+    @Test
+    public void testOak1128() throws RepositoryException {
+        Session session = createAdminSession();
+        Node p = session.getRootNode().addNode("etc");
+        p.addNode("p1");
+        Node r = p.addNode("p2").addNode("r", "nt:unstructured");
+        r.setProperty("nt:resourceType", "test");
+        session.save();
+        Query q = session.getWorkspace().getQueryManager().createQuery(
+                "/jcr:root/etc//*["+
+                        "(@jcr:primaryType = 'a'  or @jcr:primaryType = 'b') "+
+                        "or @nt:resourceType = 'test']", "xpath");
+        QueryResult qr = q.execute();
+        NodeIterator ni = qr.getNodes();
+        Node n = ni.nextNode();
+        assertEquals("/etc/p2/r", n.getPath());
+    }
+    
 }
