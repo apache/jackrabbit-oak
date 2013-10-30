d6f2ebfc502186722d9ebe12ab7ceda093932964
Marcel Reutegger <mreutegg@apache.org>
2013-10-30 13:05:58 +0000

    OAK-1129: Repeated MongoMK.rebase() always adds new revision
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1537072 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/mongomk/MongoMKRebaseTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/mongomk/MongoMKRebaseTest.java
@@ -358,4 +358,14 @@ public class MongoMKRebaseTest extends BaseMongoMKTest {
         assertTrue(trunkNode.contains("\"q\":43"));
     }
 
+    @Test
+    public void rebaseMultiple() {
+        String branch = mk.branch(null);
+        branch = mk.commit("/", "+\"x\":{}", branch, null);
+        String head = mk.commit("/", "+\"y\":{}", null, null);
+        branch = mk.rebase(branch, head);
+        assertEquals("repeated rebase with same head must not create new branch revision",
+                branch, mk.rebase(branch, head));
+    }
+
 }
