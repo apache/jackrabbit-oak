574c9832c731f89b7c343639a1e21e5b4a2c6c80
Angela Schreiber <angela@apache.org>
2013-11-04 14:17:47 +0000

    OAK-64: privilege mgt
    - remove unused code
    - change signature of getInstance(PrivilegeBits) to allow for specifying multiple base privileges
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1538618 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/spi/security/privilege/PrivilegeBitsTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/spi/security/privilege/PrivilegeBitsTest.java
@@ -476,6 +476,21 @@ public class PrivilegeBitsTest extends AbstractSecurityTest implements Privilege
     }
 
     @Test
+    public void testGetInstanceFromBase() {
+        PrivilegeBits pb = PrivilegeBits.getInstance(READ_NODES_PRIVILEGE_BITS);
+        pb.add(PrivilegeBits.BUILT_IN.get(PrivilegeConstants.JCR_READ_ACCESS_CONTROL));
+        pb.add(PrivilegeBits.BUILT_IN.get(PrivilegeConstants.JCR_NODE_TYPE_MANAGEMENT));
+
+        PrivilegeBits pb2 = PrivilegeBits.getInstance(READ_NODES_PRIVILEGE_BITS,
+                PrivilegeBits.BUILT_IN.get(PrivilegeConstants.JCR_READ_ACCESS_CONTROL),
+                PrivilegeBits.BUILT_IN.get(PrivilegeConstants.JCR_NODE_TYPE_MANAGEMENT));
+
+        assertNotSame(pb, pb2);
+        assertEquivalent(pb, pb2);
+        assertSame(pb.unmodifiable(), pb2.unmodifiable());
+    }
+
+    @Test
     public void testGetInstanceFromPropertyState() {
         for (long l : LONGS) {
             PropertyState property = createPropertyState(l);
