455a6efa4cfa12a770b1250ac6d115834219dfe6
Angela Schreiber <angela@apache.org>
2013-11-13 13:55:59 +0000

    OAK-1177 : Node#isModified returns true if node has non-readable child
    -> delegate evaluation to NodeBuilder#isModified()
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1541521 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/authorization/ItemModifiedTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/authorization/ItemModifiedTest.java
@@ -26,7 +26,6 @@ import org.junit.Test;
 /**
  * Tests for <a href="https://issues.apache.org/jira/browse/OAK-1177">OAK-1177</a>
  */
-@Ignore("OAK-1177") // FIXME: OAK-1177
 public class ItemModifiedTest extends AbstractEvaluationTest {
 
     @Ignore("OAK-1177") // FIXME: OAK-1177
