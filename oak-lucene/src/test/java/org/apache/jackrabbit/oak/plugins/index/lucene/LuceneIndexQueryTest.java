c4bb7ed9313eaa3259cf85053a3dc716a1bc9be2
Alexandru Parvulescu <alexparvulescu@apache.org>
2013-11-20 21:15:02 +0000

    OAK-1208 Lucene Index should ignore property existence checks
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1543951 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexQueryTest.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexQueryTest.java
@@ -200,4 +200,30 @@ public class LuceneIndexQueryTest extends AbstractQueryTest {
 
     }
 
+    /**
+     * OAK-1208 property existence constraints break queries
+     */
+    @Test
+    public void testOAK1208() throws Exception {
+        Tree t = root.getTree("/").addChild("containsWithMultipleOr");
+        Tree one = t.addChild("one");
+        one.setProperty("p", "dam/smartcollection");
+        one.setProperty("t", "media");
+
+        Tree two = t.addChild("two");
+        two.setProperty("p", "dam/collection");
+        two.setProperty("t", "media");
+
+        Tree three = t.addChild("three");
+        three.setProperty("p", "dam/hits");
+        three.setProperty("t", "media");
+
+        root.commit();
+
+        StringBuffer stmt = new StringBuffer();
+        stmt.append("//*[jcr:contains(., 'media') and (@p = 'dam/smartcollection' or @p = 'dam/collection') ]");
+        assertQuery(stmt.toString(), "xpath",
+                ImmutableList.of(one.getPath(), two.getPath()));
+    }
+
 }
