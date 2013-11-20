fc0459095a9268a7369ecbf7db195250936f696f
Michael Dürig <mduerig@apache.org>
2013-11-20 17:06:38 +0000

    OAK-1163: Observation events should respect permissions First draft for AC filtering of observation events
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1543881 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/jackrabbit/oak/plugins/observation/filter/ACFilter.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.jackrabbit.oak.plugins.observation.filter;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import javax.annotation.Nonnull;
+
+import org.apache.jackrabbit.oak.api.PropertyState;
+import org.apache.jackrabbit.oak.commons.PathUtils;
+import org.apache.jackrabbit.oak.core.ImmutableTree;
+import org.apache.jackrabbit.oak.plugins.observation.filter.EventGenerator.Filter;
+import org.apache.jackrabbit.oak.spi.security.authorization.permission.TreePermission;
+import org.apache.jackrabbit.oak.spi.state.NodeState;
+
+/**
+ * {@code EventTypeFilter} filters based on the access rights of the observing session.
+ */
+public class ACFilter implements Filter {
+    private final ImmutableTree beforeTree;
+    private final ImmutableTree afterTree;
+    private final TreePermission treePermission;
+
+    /**
+     * Create a new {@code Filter} instance that includes an event when the
+     * observing session has sufficient permissions to read the associated item.
+     *
+     * @param beforeTree  before state of the node being filtered
+     * @param afterTree   after state of the node being filtered
+     * @param treePermission  tree permission for the node being filtered
+     */
+    public ACFilter(@Nonnull ImmutableTree beforeTree, @Nonnull ImmutableTree afterTree,
+            @Nonnull TreePermission treePermission) {
+        this.beforeTree = checkNotNull(beforeTree);
+        this.afterTree = checkNotNull(afterTree);
+        this.treePermission = checkNotNull(treePermission);
+    }
+
+    @Override
+    public boolean includeAdd(PropertyState after) {
+        return treePermission.canRead(after);
+    }
+
+    @Override
+    public boolean includeChange(PropertyState before, PropertyState after) {
+        return treePermission.canRead(after);
+    }
+
+    @Override
+    public boolean includeDelete(PropertyState before) {
+        return treePermission.canRead(before);
+    }
+
+    @Override
+    public boolean includeAdd(String name, NodeState after) {
+        return treePermission.getChildPermission(name, after).canRead();
+    }
+
+    @Override
+    public boolean includeChange(String name, NodeState before, NodeState after) {
+        return treePermission.getChildPermission(name, after).canRead();
+    }
+
+    @Override
+    public boolean includeDelete(String name, NodeState before) {
+        return treePermission.getChildPermission(name, before).canRead();
+    }
+
+    @Override
+    public boolean includeMove(String sourcePath, String destPath, NodeState moved) {
+        return treePermission.getChildPermission(PathUtils.getName(destPath), moved).canRead();
+    }
+
+    @Override
+    public Filter create(String name, NodeState before, NodeState after) {
+        return new ACFilter(beforeTree.getChild(name), afterTree.getChild(name),
+                treePermission.getChildPermission(name, after));
+    }
+}
