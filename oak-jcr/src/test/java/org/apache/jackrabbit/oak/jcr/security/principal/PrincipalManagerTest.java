ed613463ef7cf10f6e9eef0df180e72b63d00652
Michael Duerig <mduerig@apache.org>
2013-11-25 09:05:30 +0000

    OAK-482 Group members stored in a rep:members tree println for diagnosing build failure on Buildbot
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1545181 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/principal/PrincipalManagerTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/principal/PrincipalManagerTest.java
@@ -174,21 +174,28 @@ public class PrincipalManagerTest extends AbstractJCRTest {
         }
     }
 
+    // FIXME remove println statements put here to diagnose why the build fails on Buildbot
     @Test
-    public void testMembers() {
-        PrincipalIterator it = principalMgr.getPrincipals(PrincipalManager.SEARCH_TYPE_ALL);
-        while (it.hasNext()) {
-            Principal p = it.nextPrincipal();
-            if (p.equals(principalMgr.getEveryone())) {
-                continue;
-            }
-            if (isGroup(p)) {
-                Enumeration<? extends Principal> en = ((java.security.acl.Group) p).members();
-                while (en.hasMoreElements()) {
-                    Principal memb = en.nextElement();
-                    assertTrue(principalMgr.hasPrincipal(memb.getName()));
+    public void testMembers() throws Throwable {
+        try {
+            PrincipalIterator it = principalMgr.getPrincipals(PrincipalManager.SEARCH_TYPE_ALL);
+            while (it.hasNext()) {
+                Principal p = it.nextPrincipal();
+                System.out.println(p);
+                if (p.equals(principalMgr.getEveryone())) {
+                    continue;
+                }
+                if (isGroup(p)) {
+                    Enumeration<? extends Principal> en = ((java.security.acl.Group) p).members();
+                    while (en.hasMoreElements()) {
+                        Principal memb = en.nextElement();
+                        assertTrue(principalMgr.hasPrincipal(memb.getName()));
+                    }
                 }
             }
+        } catch (Throwable t) {
+            t.printStackTrace();
+            throw t;
         }
     }
 
