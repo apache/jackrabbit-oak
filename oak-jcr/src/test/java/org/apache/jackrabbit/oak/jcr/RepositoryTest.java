ab3b2a51284c4ab406b130cf4a85c365d69af823
Tobias Bocanegra <tripod@apache.org>
2013-11-26 10:25:11 +0000

    OAK-1216 Path parsing must support SNS indexes, irrespective of SNS support
    - check for SNS paths and throw PathNotFoundException
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1545598 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/RepositoryTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/RepositoryTest.java
@@ -43,6 +43,7 @@ import javax.jcr.GuestCredentials;
 import javax.jcr.InvalidItemStateException;
 import javax.jcr.Item;
 import javax.jcr.ItemExistsException;
+import javax.jcr.ItemNotFoundException;
 import javax.jcr.NamespaceException;
 import javax.jcr.NamespaceRegistry;
 import javax.jcr.NoSuchWorkspaceException;
@@ -268,6 +269,36 @@ public class RepositoryTest extends AbstractRepositoryTest {
         assertNotNull(node);
         assertEquals("foo", node.getName());
         assertEquals("/foo", node.getPath());
+    }
+
+    /**
+     * Test SNS 1-based indexed path.
+     * JCR 2.0, Chapter 22.2:
+     * <em>
+     *     A name in a content repository path that does not explicitly specify an index implies an index of 1.
+     *     For example, /a/b/c is equivalent to /a[1]/b[1]/c[1].
+     * </em>
+     *
+     * @throws RepositoryException
+     */
+    @Test
+    public void getNodeSNS() throws RepositoryException {
+        Node node = getNode("/foo[1]");
+        assertNotNull(node);
+        assertEquals("foo", node.getName());
+        assertEquals("/foo", node.getPath());
+
+        node.addNode("bar");
+        Node bar = getNode("/foo[1]/bar[1]");
+        assertEquals("/foo/bar", bar.getPath());
+
+        try {
+            getNode("/foo[1]/bar[2]");
+            fail("retrieving wrong SNS index should throw PathNotFoundException");
+        } catch (PathNotFoundException e) {
+            // expected.
+        }
+
     }
 
     @Test(expected = RepositoryException.class)
