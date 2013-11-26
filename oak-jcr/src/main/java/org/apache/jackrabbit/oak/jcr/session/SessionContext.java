e403e00341a854773a4aba39952ceddb9674d075
Tobias Bocanegra <tripod@apache.org>
2013-11-26 10:25:11 +0000

    OAK-1216 Path parsing must support SNS indexes, irrespective of SNS support
    - check for SNS paths and throw PathNotFoundException
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1545598 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/session/SessionContext.java
+++ oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/session/SessionContext.java
@@ -328,7 +328,13 @@ public class SessionContext implements NamePathMapper {
         if (oakPath != null) {
             return oakPath;
         } else {
-            throw new RepositoryException("Invalid name or path: " + jcrPath);
+            // check if the path is an SNS path with an index > 1 and throw a PathNotFoundException instead (see OAK-1216)
+            if (getOakPathKeepIndex(jcrPath) != null) {
+                throw new PathNotFoundException(jcrPath);
+            } else {
+                throw new RepositoryException("Invalid name or path: " + jcrPath);
+            }
+
         }
     }
 
