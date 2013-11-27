afc3143b6b36199f408ef31385ba2f41ee305f1a
Tobias Bocanegra <tripod@apache.org>
2013-11-27 23:45:05 +0000

    OAK-1234 Node.getReferences() also returns weak references on same node
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1546234 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/ReferencesTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/ReferencesTest.java
@@ -144,6 +144,22 @@ public class ReferencesTest extends AbstractJCRTest {
         checkReferences("refs", ref.getWeakReferences(), n0.getPath() + "/weak_reference");
     }
 
+    public void testMixedReferencesOnSameNode3() throws RepositoryException {
+        Node ref = testRootNode.addNode(nodeName2, testNodeType);
+        ref.addMixin(mixReferenceable);
+        superuser.save();
+
+        Node n0 = testRootNode.addNode(nodeName1, testNodeType);
+        n0.setProperty("strong_reference", ref);
+        n0.setProperty("mv_strong_reference", new Value[]{superuser.getValueFactory().createValue(ref, false)});
+        n0.setProperty("weak_reference", superuser.getValueFactory().createValue(ref, true));
+        n0.setProperty("mv_weak_reference", new Value[]{superuser.getValueFactory().createValue(ref, true)});
+        superuser.save();
+
+        checkReferences("refs", ref.getReferences(), n0.getPath() + "/strong_reference", n0.getPath() + "/mv_strong_reference");
+        checkReferences("refs", ref.getWeakReferences(), n0.getPath() + "/weak_reference", n0.getPath() + "/mv_weak_reference");
+    }
+
     // OAK-1195 Unable to move referenced mode
     public void testMovedReferences() throws RepositoryException {
         Node ref = testRootNode.addNode(nodeName2, testNodeType);
