8e25529a87a04cbbce62038e6b8b313fcbfe5f02
Thomas Mueller <thomasm@apache.org>
2013-11-27 12:59:55 +0000

    OAK-28 Query implementation (additional test)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1546020 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/query/QueryTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/query/QueryTest.java
@@ -73,6 +73,35 @@ public class QueryTest extends AbstractRepositoryTest {
         assertFalse(it.hasNext());
     }
 
+    @Test
+    public void doubleQuote() throws RepositoryException {
+        Session session = getAdminSession();
+        Node hello = session.getRootNode().addNode("hello");
+        hello.setProperty("x", 1);
+        Node world = hello.addNode("world");
+        world.setProperty("x", 2);
+        session.save();
+        QueryManager qm = session.getWorkspace().getQueryManager();
+        Query q;
+        q = qm.createQuery(
+                "SELECT * FROM [nt:base] AS s WHERE ISDESCENDANTNODE(s,[/hello])", 
+                Query.JCR_SQL2);
+        assertEquals("/hello/world", getPaths(q));
+        q = qm.createQuery(
+                "SELECT * FROM [nt:base] AS s WHERE ISDESCENDANTNODE(s,\"/hello\")", 
+                Query.JCR_SQL2);
+        assertEquals("/hello/world", getPaths(q));
+        try {
+            q = qm.createQuery(
+                    "SELECT * FROM [nt:base] AS s WHERE ISDESCENDANTNODE(s,[\"/hello\"])", 
+                    Query.JCR_SQL2);
+            getPaths(q);
+            fail();
+        } catch (InvalidQueryException e) {
+            // expected: absolute path
+        }
+    }
+
     @SuppressWarnings("deprecation")
     @Test
     public void or() throws RepositoryException {
