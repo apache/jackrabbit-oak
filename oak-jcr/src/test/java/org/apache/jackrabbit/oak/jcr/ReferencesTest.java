cd63b8a32d17f0ac170273f6035e7c0e5979dba7
Tobias Bocanegra <tripod@apache.org>
2013-12-03 15:13:13 +0000

    OAK-1257 Adding a new reference to an existing MV property deletes the back references of the old ones
    - adding test case
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1547424 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/ReferencesTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/ReferencesTest.java
@@ -245,6 +245,45 @@ public class ReferencesTest extends AbstractJCRTest {
         checkReferences("refs", ref1.getReferences(), n.getPath() + "/myref");
     }
 
+    // OAK-1257
+    public void testMVReferencesAltering() throws RepositoryException {
+        Node ref0 = testRootNode.addNode(nodeName2, testNodeType);
+        ref0.addMixin(mixReferenceable);
+        Node ref1 = testRootNode.addNode(nodeName3, testNodeType);
+        ref1.addMixin(mixReferenceable);
+        Node ref2 = testRootNode.addNode(nodeName4, testNodeType);
+        ref2.addMixin(mixReferenceable);
+        superuser.save();
+
+        Node n = testRootNode.addNode(nodeName1, testNodeType);
+        Value[] vs = new Value[]{
+                superuser.getValueFactory().createValue(ref0),
+                superuser.getValueFactory().createValue(ref1)
+        };
+        n.setProperty("myref", vs);
+        superuser.save();
+
+        assertEquals("ref0", ref0.getIdentifier(), n.getProperty("myref").getValues()[0].getString());
+        assertEquals("ref1", ref1.getIdentifier(), n.getProperty("myref").getValues()[1].getString());
+        checkReferences("refs", ref0.getReferences(), n.getPath() + "/myref");
+        checkReferences("refs", ref1.getReferences(), n.getPath() + "/myref");
+
+        vs = new Value[]{
+                superuser.getValueFactory().createValue(ref0),
+                superuser.getValueFactory().createValue(ref1),
+                superuser.getValueFactory().createValue(ref2)
+        };
+        n.setProperty("myref", vs);
+        superuser.save();
+
+        assertEquals("ref0", ref0.getIdentifier(), n.getProperty("myref").getValues()[0].getString());
+        assertEquals("ref1", ref1.getIdentifier(), n.getProperty("myref").getValues()[1].getString());
+        assertEquals("ref2", ref2.getIdentifier(), n.getProperty("myref").getValues()[2].getString());
+        checkReferences("refs", ref0.getReferences(), n.getPath() + "/myref");
+        checkReferences("refs", ref1.getReferences(), n.getPath() + "/myref");
+        checkReferences("refs", ref2.getReferences(), n.getPath() + "/myref");
+    }
+
     public void testVersionReferencesVH() throws RepositoryException {
         Node n = testRootNode.addNode(nodeName1, testNodeType);
         n.addMixin(mixVersionable);
