5d16fd1307c43091b390c609ca5122ef7b25579e
Alexandru Parvulescu <alexparvulescu@apache.org>
2013-12-03 16:11:57 +0000

    OAK-1250 Guard against invalid/missing checkpoints
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1547453 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/jackrabbit/oak/plugins/segment/SegmentNodeStore.java
+++ src/main/java/org/apache/jackrabbit/oak/plugins/segment/SegmentNodeStore.java
@@ -178,17 +178,45 @@ public class SegmentNodeStore implements NodeStore, Observable {
     @Override @Nonnull
     public synchronized String checkpoint(long lifetime) {
         checkArgument(lifetime > 0);
-        // TODO: Guard the checkpoint from garbage collection
-        return head.getRecordId().toString();
+        String name = UUID.randomUUID().toString();
+
+        // try 5 times
+        for (int i = 0; i < 5; i++) {
+            if (commitSemaphore.tryAcquire()) {
+                try {
+                    refreshHead();
+
+                    SegmentNodeState ns = head;
+                    RecordId ri = head.getRecordId();
+
+                    SegmentRootBuilder builder = ns.builder();
+                    NodeBuilder cp = builder.child(name);
+                    cp.setProperty("timestamp", System.currentTimeMillis()
+                            + lifetime);
+                    cp.setChildNode(ROOT, ns.getChildNode(ROOT));
+
+                    if (journal.setHead(ri, builder.getNodeState()
+                            .getRecordId())) {
+                        refreshHead();
+                        return name;
+                    }
+
+                } finally {
+                    commitSemaphore.release();
+                }
+            }
+        }
+
+        return name;
     }
 
     @Override @CheckForNull
     public synchronized NodeState retrieve(@Nonnull String checkpoint) {
-        // TODO: Verify validity of the checkpoint
-        RecordId id = RecordId.fromString(checkNotNull(checkpoint));
-        SegmentNodeState root =
-                new SegmentNodeState(store.getWriter().getDummySegment(), id);
-        return root.getChildNode(ROOT);
+        NodeState cp = head.getChildNode(checkpoint).getChildNode(ROOT);
+        if (cp.exists()) {
+            return cp;
+        }
+        return null;
     }
 
     private class Commit {
