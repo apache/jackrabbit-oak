90242d63b4709335e371d5a140e7b1a3bbbf37c6
Julian Reschke <reschke@apache.org>
2013-12-10 15:23:57 +0000

    OAK-1271 - Fix incorrect test assumption
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1549863 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/mongomk/DocumentSplitTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/mongomk/DocumentSplitTest.java
@@ -93,7 +93,8 @@ public class DocumentSplitTest extends BaseMongoMKTest {
         // one remaining in the local deleted map
         assertEquals(1, deleted.size());
         for (Revision rev : revisions) {
-            assertTrue("document should contain revision " + rev, doc.containsRevision(rev));
+            assertTrue("document should contain revision (or have revision in commit root path):" + rev, doc.containsRevision(rev)
+                    || doc.getCommitRootPath(rev) != null);
             assertTrue(doc.isCommitted(rev));
         }
         Node node = ns.getNode("/foo", Revision.fromString(head));
