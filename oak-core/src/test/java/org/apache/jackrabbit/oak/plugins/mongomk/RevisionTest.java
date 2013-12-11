0a21f1128a1a824d1834636255f23928ada40c52
Marcel Reutegger <mreutegg@apache.org>
2013-12-11 14:13:33 +0000

    OAK-1274: Wrong comparison for old Revisions
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1550144 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/mongomk/RevisionTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/mongomk/RevisionTest.java
@@ -116,12 +116,12 @@ public class RevisionTest {
         assertEquals(r2.compareRevisionTime(r1), comp.compare(r2, r1));
         assertEquals(r1.compareRevisionTime(r1), comp.compare(r1, r1));
     }
-    
+
     @Test
     public void revisionComparatorCluster() {
-        
+
         RevisionComparator comp = new RevisionComparator(0);
-        
+
         Revision r1c1 = new Revision(0x110, 0, 1);
         Revision r2c1 = new Revision(0x120, 0, 1);
         Revision r3c1 = new Revision(0x130, 0, 1);
@@ -134,27 +134,27 @@ public class RevisionTest {
         assertEquals(-1, comp.compare(r2c1, r2c2));
         assertEquals(-1, comp.compare(r3c1, r3c2));
 
-        // now we declare r1+r2 of c1 to be after r1+r2 of c2
+        // now we declare r2+r3 of c1 to be after r2+r3 of c2
         comp.add(r2c1, new Revision(0x20, 0, 0));
         comp.add(r2c2, new Revision(0x10, 0, 0));
 
         assertEquals(
-                "1:\n r120-0-1:r20-0-0\n" + 
+                "1:\n r120-0-1:r20-0-0\n" +
                 "2:\n r200-0-2:r10-0-0\n", comp.toString());
 
         assertEquals(1, comp.compare(r1c1, r1c2));
         assertEquals(1, comp.compare(r2c1, r2c2));
         // both r3cx are still "in the future"
         assertEquals(-1, comp.compare(r3c1, r3c2));
-        
+
         // now we declare r3 of c1 to be before r3 of c2
-        // (with the same range timestamp, 
+        // (with the same range timestamp,
         // the revision timestamps are compared)
         comp.add(r3c1, new Revision(0x30, 0, 0));
         comp.add(r3c2, new Revision(0x30, 0, 0));
 
         assertEquals(
-                "1:\n r120-0-1:r20-0-0 r130-0-1:r30-0-0\n" + 
+                "1:\n r120-0-1:r20-0-0 r130-0-1:r30-0-0\n" +
                 "2:\n r200-0-2:r10-0-0 r300-0-2:r30-0-0\n", comp.toString());
 
         assertEquals(1, comp.compare(r1c1, r1c2));
@@ -164,28 +164,31 @@ public class RevisionTest {
         assertEquals(-1, comp.compare(r1c2, r1c1));
         assertEquals(-1, comp.compare(r2c2, r2c1));
         assertEquals(1, comp.compare(r3c2, r3c1));
-        
+
         // get rid of old timestamps
         comp.purge(0x10);
         assertEquals(
-                "1:\n r120-0-1:r20-0-0 r130-0-1:r30-0-0\n" + 
+                "1:\n r120-0-1:r20-0-0 r130-0-1:r30-0-0\n" +
                 "2:\n r300-0-2:r30-0-0\n", comp.toString());
         comp.purge(0x20);
         assertEquals(
-                "1:\n r130-0-1:r30-0-0\n" + 
+                "1:\n r130-0-1:r30-0-0\n" +
                 "2:\n r300-0-2:r30-0-0\n", comp.toString());
-        
+
         // update an entry
         comp.add(new Revision(0x301, 1, 2), new Revision(0x30, 0, 0));
         assertEquals(
-                "1:\n r130-0-1:r30-0-0\n" + 
+                "1:\n r130-0-1:r30-0-0\n" +
                 "2:\n r301-1-2:r30-0-0\n", comp.toString());
-        
+
         comp.purge(0x30);
         assertEquals("", comp.toString());
 
     }
 
+    /**
+     * OAK-1274
+     */
     @Ignore
     @Test
     public void clusterCompare() {
