8c432cd8cbd12261b5fe91261cd0208670186d4e
Tobias Bocanegra <tripod@apache.org>
2013-12-19 22:52:58 +0000

    Adding tests for OAK-1299 and OAK-1300
    - OAK-1299 Removing user from group also invalidates other members memberships
    - OAK-1300 Removing a reference from a mv property destroys the reverse lookup from the remaining
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1552450 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/ReferencesTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/ReferencesTest.java
@@ -245,7 +245,7 @@ public class ReferencesTest extends AbstractJCRTest {
     }
 
     // OAK-1257
-    public void testMVReferencesAltering() throws RepositoryException {
+    public void testMVAddAltering() throws RepositoryException {
         Node ref0 = testRootNode.addNode(nodeName2, testNodeType);
         ref0.addMixin(mixReferenceable);
         Node ref1 = testRootNode.addNode(nodeName3, testNodeType);
@@ -279,6 +279,46 @@ public class ReferencesTest extends AbstractJCRTest {
         assertEquals("ref1", ref1.getIdentifier(), n.getProperty("myref").getValues()[1].getString());
         assertEquals("ref2", ref2.getIdentifier(), n.getProperty("myref").getValues()[2].getString());
         checkReferences("refs", ref0.getReferences(), n.getPath() + "/myref");
+        checkReferences("refs", ref1.getReferences(), n.getPath() + "/myref");
+        checkReferences("refs", ref2.getReferences(), n.getPath() + "/myref");
+    }
+
+    // OAK-1300
+    public void testMVRemoveReferences() throws RepositoryException {
+        Node ref0 = testRootNode.addNode(nodeName2, testNodeType);
+        ref0.addMixin(mixReferenceable);
+        Node ref1 = testRootNode.addNode(nodeName3, testNodeType);
+        ref1.addMixin(mixReferenceable);
+        Node ref2 = testRootNode.addNode(nodeName4, testNodeType);
+        ref2.addMixin(mixReferenceable);
+        superuser.save();
+
+        Node n = testRootNode.addNode(nodeName1, testNodeType);
+        Value[] vs = new Value[]{
+                superuser.getValueFactory().createValue(ref0),
+                superuser.getValueFactory().createValue(ref1),
+                superuser.getValueFactory().createValue(ref2)
+        };
+        n.setProperty("myref", vs);
+        superuser.save();
+
+        assertEquals("ref0", ref0.getIdentifier(), n.getProperty("myref").getValues()[0].getString());
+        assertEquals("ref1", ref1.getIdentifier(), n.getProperty("myref").getValues()[1].getString());
+        assertEquals("ref2", ref2.getIdentifier(), n.getProperty("myref").getValues()[2].getString());
+        checkReferences("refs", ref0.getReferences(), n.getPath() + "/myref");
+        checkReferences("refs", ref1.getReferences(), n.getPath() + "/myref");
+        checkReferences("refs", ref2.getReferences(), n.getPath() + "/myref");
+
+        vs = new Value[]{
+                superuser.getValueFactory().createValue(ref1),
+                superuser.getValueFactory().createValue(ref2)
+        };
+        n.setProperty("myref", vs);
+        superuser.save();
+
+        assertEquals("ref1", ref1.getIdentifier(), n.getProperty("myref").getValues()[0].getString());
+        assertEquals("ref2", ref2.getIdentifier(), n.getProperty("myref").getValues()[1].getString());
+        checkReferences("refs", ref0.getReferences());
         checkReferences("refs", ref1.getReferences(), n.getPath() + "/myref");
         checkReferences("refs", ref2.getReferences(), n.getPath() + "/myref");
     }
