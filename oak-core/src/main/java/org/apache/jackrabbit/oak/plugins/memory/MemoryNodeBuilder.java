73cc24425e00e105e82446bff4cc618b0eac19f6
Michael Duerig <mduerig@apache.org>
2013-12-19 10:12:10 +0000

    OAK-1297: MoveDetector does not detect moved nodes that have been moved in an earlier commit already Properly annotate moved node in the face of moves from earlier commits
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1552279 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/memory/MemoryNodeBuilder.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/memory/MemoryNodeBuilder.java
@@ -374,9 +374,7 @@ public class MemoryNodeBuilder implements NodeBuilder {
         PropertyState base = builder.getBaseState().getProperty(MoveDetector.SOURCE_PATH);
         PropertyState head = builder.getNodeState().getProperty(MoveDetector.SOURCE_PATH);
         if (Objects.equal(base, head)) {
-            if (!builder.hasProperty(MoveDetector.SOURCE_PATH)) {
-                builder.setProperty(MoveDetector.SOURCE_PATH, path);
-            }
+            builder.setProperty(MoveDetector.SOURCE_PATH, path);
         }
     }
 
