13d1057c114fd8c31f6db4b3fc7e38c1ce1a2f5d
Marcel Reutegger <mreutegg@apache.org>
2014-01-06 15:39:27 +0000

    OAK-1305: Use non-orderable child nodes in FlatTreeWithAceForSamePrincipalTest - use distinct test root node - use non-orderable child nodes (nt:folder) - convert tabs to spaces
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1555865 13f79535-47bb-0310-9956-ffa450edef68

--- oak-run/src/main/java/org/apache/jackrabbit/oak/benchmark/FlatTreeWithAceForSamePrincipalTest.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/benchmark/FlatTreeWithAceForSamePrincipalTest.java
@@ -17,6 +17,7 @@
 package org.apache.jackrabbit.oak.benchmark;
 
 import java.security.Principal;
+
 import javax.jcr.Node;
 import javax.jcr.Session;
 import javax.jcr.SimpleCredentials;
@@ -30,62 +31,60 @@ import org.apache.jackrabbit.oak.spi.security.privilege.PrivilegeConstants;
 
 public class FlatTreeWithAceForSamePrincipalTest extends AbstractTest {
 
-    private static final String TEST_USER_ID = "test";
+    private static final String TEST_USER_ID = "test" + TEST_ID;
+    private static final String ROOT_NODE_NAME = "test" + TEST_ID;
+    private static final String ROOT_PATH = "/" + ROOT_NODE_NAME;
 
     private UserManager userManager;
 
     private Session admin;
-	private Session reader;
+    private Session reader;
 
-	@Override
-	protected void beforeSuite() throws Exception {
+    @Override
+    protected void beforeSuite() throws Exception {
 
-		long start = System.currentTimeMillis();
+        long start = System.currentTimeMillis();
 
-		admin = loginWriter();
-		userManager = ((JackrabbitSession) admin).getUserManager();
-		Principal userPrincipal = userManager.createUser(TEST_USER_ID, TEST_USER_ID).getPrincipal();
+        admin = loginWriter();
+        userManager = ((JackrabbitSession) admin).getUserManager();
+        Principal userPrincipal = userManager.createUser(TEST_USER_ID, TEST_USER_ID).getPrincipal();
 
-		AccessControlManager acm = admin.getAccessControlManager();
-		JackrabbitAccessControlList acl = AccessControlUtils.getAccessControlList(acm, "/");
-		acl.addEntry(userPrincipal, AccessControlUtils.privilegesFromNames(acm, PrivilegeConstants.JCR_READ), true);
-		acm.setPolicy("/", acl);
+        AccessControlManager acm = admin.getAccessControlManager();
+        JackrabbitAccessControlList acl = AccessControlUtils.getAccessControlList(acm, "/");
+        acl.addEntry(userPrincipal, AccessControlUtils.privilegesFromNames(acm, PrivilegeConstants.JCR_READ), true);
+        acm.setPolicy("/", acl);
 
-		Node a = admin.getRootNode().addNode("a");
-		for (int i = 1; i < 10000; i++) {
-			a.addNode("node" + i);
-			acl = AccessControlUtils.getAccessControlList(acm, "/a/node"+i);
-			acl.addEntry(userPrincipal, AccessControlUtils.privilegesFromNames(acm, PrivilegeConstants.JCR_READ), true);
-			acm.setPolicy("/a/node"+i, acl);
-		}
-
-		admin.save();
-		reader = login(new SimpleCredentials(TEST_USER_ID, TEST_USER_ID.toCharArray()));
-
-		long end = System.currentTimeMillis();
-		System.out.println("setup time "+(end - start));
-	}
-
-	@Override
-	protected void runTest() throws Exception {
-		Node n = reader.getNode("/a");
- 		for (int i = 1; i < 10000; i++) {
-			n.getNode("node" + i);
-		}
-	}
-
-	protected void afterTest() throws Exception {
-		 
-	}
-
-	protected void afterSuite() throws Exception {
-        Node root = admin.getRootNode();
-        if (root.hasNode("a")) {
-            root.getNode("a").remove();
+        Node a = admin.getRootNode().addNode(ROOT_NODE_NAME, "nt:folder");
+        for (int i = 1; i < 10000; i++) {
+            a.addNode("node" + i, "nt:folder");
+            acl = AccessControlUtils.getAccessControlList(acm, ROOT_PATH + "/node" + i);
+            acl.addEntry(userPrincipal, AccessControlUtils.privilegesFromNames(acm, PrivilegeConstants.JCR_READ), true);
+            acm.setPolicy(ROOT_PATH + "/node" + i, acl);
         }
-		if (userManager != null) {
+
+        admin.save();
+        reader = login(new SimpleCredentials(TEST_USER_ID, TEST_USER_ID.toCharArray()));
+
+        long end = System.currentTimeMillis();
+        System.out.println("setup time " + (end - start));
+    }
+
+    @Override
+    protected void runTest() throws Exception {
+        Node n = reader.getNode(ROOT_PATH);
+        for (int i = 1; i < 10000; i++) {
+            n.getNode("node" + i);
+        }
+    }
+
+    protected void afterSuite() throws Exception {
+        Node root = admin.getRootNode();
+        if (root.hasNode(ROOT_NODE_NAME)) {
+            root.getNode(ROOT_NODE_NAME).remove();
+        }
+        if (userManager != null) {
             userManager.getAuthorizable(TEST_USER_ID).remove();
         }
         admin.save();
-	}
+    }
 }
