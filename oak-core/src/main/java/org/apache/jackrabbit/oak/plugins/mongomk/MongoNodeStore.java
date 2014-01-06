25a704390e971959794a42c8a09d97f49311b3c3
Marcel Reutegger <mreutegg@apache.org>
2014-01-06 13:43:14 +0000

    OAK-1254: Parallel execution of SimpleSearchTest fails with MongoMK - publish current head revision first to revision comparator - enable tests
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1555752 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/mongomk/MongoNodeStore.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/mongomk/MongoNodeStore.java
@@ -1135,8 +1135,15 @@ public final class MongoNodeStore
             Revision r = e.getValue();
             Revision last = lastKnownRevision.get(machineId);
             if (last == null || r.compareRevisionTime(last) > 0) {
-                lastKnownRevision.put(machineId, r);
+                if (!hasNewRevisions) {
+                    // publish our revision once before any foreign revision
+
+                    // the latest revisions of the current cluster node
+                    // happened before the latest revisions of other cluster nodes
+                    revisionComparator.add(Revision.newRevision(clusterId), headSeen);
+                }
                 hasNewRevisions = true;
+                lastKnownRevision.put(machineId, r);
                 revisionComparator.add(r, otherSeen);
             }
         }
@@ -1144,11 +1151,6 @@ public final class MongoNodeStore
             store.invalidateCache();
             // TODO only invalidate affected items
             docChildrenCache.invalidateAll();
-            // add a new revision, so that changes are visible
-            Revision r = Revision.newRevision(clusterId);
-            // the latest revisions of the current cluster node
-            // happened before the latest revisions of other cluster nodes
-            revisionComparator.add(r, headSeen);
             // the head revision is after other revisions
             setHeadRevision(Revision.newRevision(clusterId));
         }
