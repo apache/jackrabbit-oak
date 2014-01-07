c1537f58a66bb20bf9a644aca271a73ddf65e73a
Thomas Mueller <thomasm@apache.org>
2014-01-07 15:35:21 +0000

    OAK-1308 XPath queries with ISO9075 escaped properties don't work
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1556247 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/query/QueryTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/query/QueryTest.java
@@ -427,6 +427,31 @@ public class QueryTest extends AbstractRepositoryTest {
         assertEquals("p1 p2", row.getValues()[0].getString());
     }
 
+    @SuppressWarnings("deprecation")
+    @Test
+    public void xpathEscapeTest() throws RepositoryException {
+        Session writer = createAdminSession();
+        Session reader = createAdminSession();
+        try {
+            Node rootNode = writer.getRootNode();
+            Node node = rootNode.addNode("test", "nt:unstructured");
+            node.addNode(".tokens");
+            writer.save();
+
+            QueryManager qm = reader.getWorkspace().getQueryManager();
+            Query q = qm.createQuery("/jcr:root//*[_x002e_tokens/@jcr:primaryType]", Query.XPATH);
+            NodeIterator res = q.execute().getNodes();
+            assertEquals(1, res.getSize());
+        } finally {
+            if (reader != null) {
+                reader.logout();
+            }
+            if (writer != null) {
+                writer.logout();
+            }
+        }
+    }
+
     @Test
     public void testOak1096() throws RepositoryException {
         Session writer = createAdminSession();
