10be3d284ea81d8db24d1570f4b35476bb2597fb
Angela Schreiber <angela@apache.org>
2014-01-10 10:20:58 +0000

    OAK-1268 : Add support for composite authorization setup
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1557071 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/security/authorization/accesscontrol/PolicyOwner.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.spi.security.authorization.accesscontrol;
+
+import javax.jcr.security.AccessControlPolicy;
+
+/**
+ * Interface to improve pluggability of the {@link javax.jcr.security.AccessControlManager},
+ * namely the interaction of multiple managers within a
+ * single repository. It provides a single method {@link #defines(String, javax.jcr.security.AccessControlPolicy)}
+ * that allows to termine the responsible manager upon
+ * {@link javax.jcr.security.AccessControlManager#setPolicy(String, javax.jcr.security.AccessControlPolicy) setPolicy}
+ * and
+ * {@link javax.jcr.security.AccessControlManager#removePolicy(String, javax.jcr.security.AccessControlPolicy) removePolicy}.
+ *
+ * @see org.apache.jackrabbit.oak.spi.security.authorization.CompositeAuthorizationConfiguration
+ */
+public interface PolicyOwner {
+
+    /**
+     * Determines if the implementing {@code AccessManager} defines the specified
+     * {@code acceessControlPolicy} at the given {@code absPath}. If this method
+     * returns {@code true} it is expected that the given policy is valid to be
+     * {@link javax.jcr.security.AccessControlManager#setPolicy(String, javax.jcr.security.AccessControlPolicy) set}
+     * or {@link javax.jcr.security.AccessControlManager#removePolicy(String, javax.jcr.security.AccessControlPolicy) removed}
+     * with the manager.
+     *
+     * @param absPath An absolute path.
+     * @param accessControlPolicy The access control policy to be tested.
+     * @return {@code true} If the {@code AccessControlManager} implementing this
+     * interface can handle the specified {@code accessControlPolicy} at the given {@code path}.
+     */
+    boolean defines(String absPath, AccessControlPolicy accessControlPolicy);
+}
