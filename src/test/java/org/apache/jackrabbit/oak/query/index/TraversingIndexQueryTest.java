ad3f41e00bba82acb38924434c916c3de7dc5eba
Alexandru Parvulescu <alexparvulescu@apache.org>
2014-01-13 13:10:44 +0000

    OAK-1301 Path conditions not respected in XPath query  - fix + test for the initial query
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-core@1557703 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/jackrabbit/oak/query/index/TraversingIndexQueryTest.java
+++ src/test/java/org/apache/jackrabbit/oak/query/index/TraversingIndexQueryTest.java
@@ -156,4 +156,52 @@ public class TraversingIndexQueryTest extends AbstractQueryTest {
                 "//*[(jcr:like(child/@prop, 'aaa%') and jcr:like(child/@prop, 'bbb%') and jcr:like(child/@prop, 'ccc%'))]",
                 "xpath", ImmutableList.of("/content/two"));
     }
+
+    @Test
+    public void testOak1301() throws Exception {
+        Tree t1 = root.getTree("/").addChild("home").addChild("users")
+                .addChild("testing").addChild("socialgraph_test_user_4");
+        t1.setProperty("jcr:primaryType", "rep:User");
+        t1.setProperty("rep:authorizableId", "socialgraph_test_user_4");
+
+        Tree s = t1.addChild("social");
+        s.setProperty("jcr:primaryType", "sling:Folder");
+
+        Tree r = s.addChild("relationships");
+        r.setProperty("jcr:primaryType", "sling:Folder");
+
+        Tree f = r.addChild("friend");
+        f.setProperty("jcr:primaryType", "sling:Folder");
+
+        Tree sg = f.addChild("socialgraph_test_group");
+        sg.setProperty("jcr:primaryType", "nt:unstructured");
+        sg.setProperty("id", "socialgraph_test_group");
+
+        Tree t2 = root.getTree("/").addChild("home").addChild("groups")
+                .addChild("testing").addChild("socialgraph_test_group");
+        root.commit();
+
+        // select [jcr:path], [jcr:score], * from [nt:base] as a where [id/*] =
+        // 'socialgraph_test_group' and isdescendantnode(a, '/home') /* xpath:
+        // /jcr:root/home//*[id='socialgraph_test_group'] */
+        assertQuery(
+                "/jcr:root/home//*[id='socialgraph_test_group']",
+                "xpath",
+                ImmutableList
+                        .of("/home/users/testing/socialgraph_test_user_4/social/relationships/friend/socialgraph_test_group"));
+
+        // sql2 select c.[jcr:path] as [jcr:path], c.[jcr:score] as [jcr:score],
+        // c.* from [nt:base] as a inner join [nt:base] as b on ischildnode(b,
+        // a) inner join [nt:base] as c on isdescendantnode(c, b) where name(a)
+        // = 'social' and isdescendantnode(a, '/home') and name(b) =
+        // 'relationships' and c.[id/*] = 'socialgraph_test_group' /* xpath:
+        // /jcr:root/home//social/relationships//*[id='socialgraph_test_group']
+        // */
+        assertQuery(
+                "/jcr:root/home//social/relationships//*[id='socialgraph_test_group']",
+                "xpath",
+                ImmutableList
+                        .of("/home/users/testing/socialgraph_test_user_4/social/relationships/friend/socialgraph_test_group"));
+
+    }
 }
