3c85f3b8b370e7481aba958ec4642b7543b0adec
Angela Schreiber <angela@apache.org>
2014-01-14 09:34:35 +0000

    OAK-710 : PermissionValidator: Proper permission evaluation for moving/renaming nodes (wip)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1557993 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/authorization/SessionMoveTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/authorization/SessionMoveTest.java
@@ -280,29 +280,28 @@ public class SessionMoveTest extends AbstractMoveTest {
         testSession.save();
     }
 
-    // FIXME: adding replacement is not detected by node state diff.
-//    @Test
-//    public void testMoveAndAddReplacementAtSource() throws Exception {
-//        allow(path, privilegesFromNames(new String[]{
-//                Privilege.JCR_REMOVE_CHILD_NODES, Privilege.JCR_ADD_CHILD_NODES
-//        }));
-//        allow(siblingPath, privilegesFromNames(new String[] {
-//                PrivilegeConstants.JCR_ADD_CHILD_NODES, PrivilegeConstants.JCR_NODE_TYPE_MANAGEMENT
-//        }));
-//
-//        testSession.move(nodePath3, siblingDestPath);
-//
-//        Node sourceParent = testSession.getNode(childNPath);
-//        Node replacement = sourceParent.addNode(Text.getName(nodePath3));
-//        replacement.setProperty("movedProp", "val");
-//
-//        try {
-//            testSession.save();
-//            fail("Missing ADD_NODE and ADD_PROPERTY permission on source parent.");
-//        } catch (AccessDeniedException e) {
-//            // success
-//        }
-//    }
+    @Test
+    public void testMoveAndAddReplacementAtSource() throws Exception {
+        allow(path, privilegesFromNames(new String[]{
+                Privilege.JCR_REMOVE_CHILD_NODES, Privilege.JCR_ADD_CHILD_NODES
+        }));
+        allow(siblingPath, privilegesFromNames(new String[] {
+                PrivilegeConstants.JCR_ADD_CHILD_NODES, PrivilegeConstants.JCR_NODE_TYPE_MANAGEMENT
+        }));
+
+        testSession.move(nodePath3, siblingDestPath);
+
+        Node sourceParent = testSession.getNode(childNPath);
+        Node replacement = sourceParent.addNode(Text.getName(nodePath3));
+        replacement.setProperty("movedProp", "val");
+
+        try {
+            testSession.save();
+            fail("Missing ADD_NODE and ADD_PROPERTY permission on source parent.");
+        } catch (AccessDeniedException e) {
+            // success
+        }
+    }
 
     @Test
     public void testMoveAndAddReplacementAtSource2() throws Exception {
