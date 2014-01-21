b0fef4f668f0ccf2aa04f0e6899a7e9ba5989c48
Marcel Reutegger <mreutegg@apache.org>
2014-01-21 17:09:27 +0000

    OAK-1346: Restore of OPV copy child node may fail
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1560102 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/version/VersionTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/version/VersionTest.java
@@ -86,17 +86,19 @@ public class VersionTest extends AbstractJCRTest {
         assertEquals(ntFrozenNode, frozenChild.getPrimaryNodeType().getName());
     }
 
-    // OAK-1009
+    // OAK-1009 & OAK-1346
     public void testFrozenUUID() throws RepositoryException {
         Node n = testRootNode.addNode(nodeName1, testNodeType);
         n.addMixin(mixVersionable);
-        Node child = n.addNode(nodeName2, ntUnstructured);
+        Node child = n.addNode(nodeName2, "nt:folder");
         superuser.save();
         VersionManager vMgr = superuser.getWorkspace().getVersionManager();
+        Version v = vMgr.checkpoint(n.getPath());
         vMgr.checkpoint(n.getPath());
-        Version v = vMgr.getBaseVersion(n.getPath());
-        Node frozenChild = v.getFrozenNode().getNode(child.getName());
+        Version baseVersion = vMgr.getBaseVersion(n.getPath());
+        Node frozenChild = baseVersion.getFrozenNode().getNode(child.getName());
         assertEquals(child.getIdentifier(),
                 frozenChild.getProperty(Property.JCR_FROZEN_UUID).getString());
+        vMgr.restore(v, true);
     }
 }
