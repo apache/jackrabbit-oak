b7e549a97309584ad53a61cd82427c37d60240ac
Michael Dürig <mduerig@apache.org>
2014-01-29 10:28:28 +0000

    typo
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1562396 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/spi/state/MoveDetector.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/state/MoveDetector.java
@@ -45,7 +45,7 @@ import org.apache.jackrabbit.oak.spi.commit.Validator;
  *     add operation on the move target.</li>
  *     <li>Removing a moved node is not reported as a move operation but as a remove operation
  *     from the source of the move.</li>
- *     <li>Moving a child node of a transiently moved node is not reported from the original
+ *     <li>Moving a child node of a transiently moved node is reported from the original
  *     source node, not from the intermediate one.</li>
  *     <li>Moving a node back and forth to its original location is not reported at all.</li>
  * </ul>
