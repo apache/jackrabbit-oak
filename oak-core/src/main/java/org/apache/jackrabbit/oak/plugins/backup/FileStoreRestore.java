2e7d81779cebce598c0619d0b1aac105e1a5efc3
Alexandru Parvulescu <alexparvulescu@apache.org>
2014-02-03 08:49:46 +0000

    OAK-1380 Restore from backup utility class
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1563802 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/backup/FileStoreRestore.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.jackrabbit.oak.plugins.backup;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.jackrabbit.oak.api.CommitFailedException;
+import org.apache.jackrabbit.oak.plugins.segment.Journal;
+import org.apache.jackrabbit.oak.plugins.segment.SegmentNodeState;
+import org.apache.jackrabbit.oak.plugins.segment.file.FileStore;
+import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
+import org.apache.jackrabbit.oak.spi.state.ApplyDiff;
+import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
+import org.apache.jackrabbit.oak.spi.state.NodeState;
+import org.apache.jackrabbit.oak.spi.state.NodeStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FileStoreRestore {
+
+    private static final Logger log = LoggerFactory
+            .getLogger(FileStoreRestore.class);
+
+    static int MAX_FILE_SIZE = 256;
+
+    private static final String JOURNAL_FILE_NAME = "journal.log";
+
+    public static void restore(File source, NodeStore store)
+            throws IOException, CommitFailedException {
+        // 1. verify that this is an actual filestore
+        if (!validFileStore(source)) {
+            throw new IOException("Folder " + source
+                    + " is not a valid FileStore directory");
+        }
+
+        // 2. init filestore
+        FileStore restore = new FileStore(source, MAX_FILE_SIZE, false);
+        try {
+            Journal journal = restore.getJournal("root");
+            SegmentNodeState state = new SegmentNodeState(restore.getWriter()
+                    .getDummySegment(), journal.getHead());
+            restore(state.getChildNode("root"), store);
+        } finally {
+            restore.close();
+        }
+    }
+
+    public static void restore(NodeState source, NodeStore store)
+            throws CommitFailedException {
+        long s = System.currentTimeMillis();
+        NodeState after = store.getRoot();
+        NodeBuilder builder = after.builder();
+        source.compareAgainstBaseState(after, new ApplyDiff(builder));
+        store.merge(builder, EmptyHook.INSTANCE, null);
+        log.debug("Restore finished in {} ms.", System.currentTimeMillis() - s);
+    }
+
+    private static boolean validFileStore(File source) {
+        if (source == null || !source.isDirectory()) {
+            return false;
+        }
+        for (String f : source.list()) {
+            if (JOURNAL_FILE_NAME.equals(f)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
