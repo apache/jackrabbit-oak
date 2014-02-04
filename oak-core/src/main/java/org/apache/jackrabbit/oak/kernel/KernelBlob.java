a282eeefffed02e21a97145876ec7873238385be
Michael Dürig <mduerig@apache.org>
2014-02-04 13:42:02 +0000

    OAK-834: Efficient copying of binaries across repositories Implement getReference() for KernelBlob
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1564297 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/kernel/KernelBlob.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/kernel/KernelBlob.java
@@ -65,6 +65,11 @@ public class KernelBlob extends AbstractBlob {
         return kernel.getLength(binaryID);
     }
 
+    @Override
+    public String getReference() {
+        return binaryID;
+    }
+
     /**
      * This implementation delegates back to the underlying {@code Microkernel}
      * if other is also of type {@code KernelBlob}.
