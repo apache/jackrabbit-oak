a30c5b51c465701f8669a3771c73f8c74bd8368d
Angela Schreiber <angela@apache.org>
2014-02-06 17:40:48 +0000

    OAK-1400 : Move PropertyBuilder from o.a.j.o.util to o.a.j.o.plugins.memory (move test cases as well)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1565372 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/memory/PropertyBuilderTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/memory/PropertyBuilderTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.oak.util;
+package org.apache.jackrabbit.oak.plugins.memory;
 
 import java.util.Arrays;
 
