284f7143550b910ebc56d66add23ccd2b3692c83
Angela Schreiber <angela@apache.org>
2014-02-27 18:46:14 +0000

    remove superfluous todo
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1572675 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/spi/commit/MoveTrackerTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/spi/commit/MoveTrackerTest.java
@@ -27,9 +27,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-/**
- * MoveTrackerTest... TODO
- */
 public class MoveTrackerTest {
 
     @Test
