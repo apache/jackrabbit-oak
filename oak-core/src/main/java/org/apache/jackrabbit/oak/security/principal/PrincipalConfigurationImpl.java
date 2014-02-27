3473c163688e8119e1752bcc0ba4c301557a6077
Tobias Bocanegra <tripod@apache.org>
2014-02-27 18:37:34 +0000

    Revert "OAK-1474: revert changes made rev. 1572401"
    This reverts commit 061bdb5fef8040cbc39b1040cd8190f169a8dfb3.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1572672 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/security/principal/PrincipalConfigurationImpl.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/security/principal/PrincipalConfigurationImpl.java
@@ -16,14 +16,18 @@
  */
 package org.apache.jackrabbit.oak.security.principal;
 
+import java.util.Dictionary;
+
 import javax.annotation.Nonnull;
 
+import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.jackrabbit.api.security.principal.PrincipalManager;
 import org.apache.jackrabbit.oak.api.Root;
 import org.apache.jackrabbit.oak.namepath.NamePathMapper;
 import org.apache.jackrabbit.oak.spi.security.ConfigurationBase;
+import org.apache.jackrabbit.oak.spi.security.ConfigurationParameters;
 import org.apache.jackrabbit.oak.spi.security.SecurityConfiguration;
 import org.apache.jackrabbit.oak.spi.security.SecurityProvider;
 import org.apache.jackrabbit.oak.spi.security.principal.PrincipalConfiguration;
@@ -43,8 +47,14 @@ public class PrincipalConfigurationImpl extends ConfigurationBase implements Pri
     }
 
     public PrincipalConfigurationImpl(SecurityProvider securityProvider) {
-        super(securityProvider);
+        super(securityProvider, securityProvider.getParameters(NAME));
     }
+
+    @Activate
+    private void activate(Dictionary<String, Object> properties) {
+        setParameters(ConfigurationParameters.of(properties));
+    }
+
 
     //---------------------------------------------< PrincipalConfiguration >---
     @Nonnull
