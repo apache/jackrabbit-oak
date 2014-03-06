9391f658c32f199c6db48f5d8d635a97b54cd950
Thomas Mueller <thomasm@apache.org>
2014-03-06 10:26:06 +0000

    OAK-1263 Optimize oak index to support 'fast ORDER BY' queries
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1574837 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/IndexUtils.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/IndexUtils.java
@@ -107,8 +107,29 @@ public class IndexUtils {
                                              boolean unique,
                                              @Nonnull String[] propertyNames,
                                              @Nullable String[] declaringNodeTypeNames) throws RepositoryException {
+
+        createIndexDefinition(indexNode, indexDefName, unique, propertyNames, declaringNodeTypeNames, PropertyIndexEditorProvider.TYPE);
+    }
+
+    /**
+     * Create a new property index definition below the given {@code indexNode} of the provided {@code propertyIndexType}.
+     * 
+     * @param indexNode
+     * @param indexDefName
+     * @param unique
+     * @param propertyNames
+     * @param declaringNodeTypeNames
+     * @param propertyIndexType
+     * @throws RepositoryException
+     */
+    public static void createIndexDefinition(@Nonnull NodeUtil indexNode, 
+                                             @Nonnull String indexDefName, 
+                                             boolean unique, 
+                                             @Nonnull String[] propertyNames, 
+                                             @Nullable String[] declaringNodeTypeNames, 
+                                             @Nonnull String propertyIndexType) throws RepositoryException {
         NodeUtil entry = indexNode.getOrAddChild(indexDefName, INDEX_DEFINITIONS_NODE_TYPE);
-        entry.setString(TYPE_PROPERTY_NAME, PropertyIndexEditorProvider.TYPE);
+        entry.setString(TYPE_PROPERTY_NAME, propertyIndexType);
         entry.setBoolean(REINDEX_PROPERTY_NAME, true);
         if (unique) {
             entry.setBoolean(UNIQUE_PROPERTY_NAME, true);
