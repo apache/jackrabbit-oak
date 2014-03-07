fd8a54da360af3106c5b6c2b8a6935fcf5b75f60
Tobias Bocanegra <tripod@apache.org>
2014-03-07 02:25:51 +0000

    @trivial token login module must refresh in case another login module just created the user
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1575125 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/security/authentication/token/TokenLoginModule.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/security/authentication/token/TokenLoginModule.java
@@ -159,6 +159,7 @@ public final class TokenLoginModule extends AbstractLoginModule {
         if (tokenProvider != null && sharedState.containsKey(SHARED_KEY_CREDENTIALS)) {
             Credentials shared = getSharedCredentials();
             if (shared != null && tokenProvider.doCreateToken(shared)) {
+                getRoot().refresh(); // refresh root, in case the external login module created users
                 TokenInfo ti = tokenProvider.createToken(shared);
                 if (ti != null) {
                     TokenCredentials tc = new TokenCredentials(ti.getToken());
