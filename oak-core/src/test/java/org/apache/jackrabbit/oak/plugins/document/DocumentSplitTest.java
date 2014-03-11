279bb3ce8c25c54afadd19c5cbb9acda13e4bc4f
Marcel Reutegger <mreutegg@apache.org>
2014-03-11 17:41:01 +0000

    OAK-1429: Slow event listeners do not scale as expected
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1576432 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/DocumentSplitTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/DocumentSplitTest.java
@@ -277,6 +277,27 @@ public class DocumentSplitTest extends BaseDocumentMKTest {
         }
     }
 
+    @Test
+    public void commitRootInPrevious() {
+        DocumentStore store = mk.getDocumentStore();
+        DocumentNodeStore ns = mk.getNodeStore();
+        mk.commit("/", "+\"test\":{\"node\":{}}", null, null);
+        mk.commit("/test", "+\"foo\":{}+\"bar\":{}", null, null);
+        mk.commit("/test", "^\"foo/prop\":0^\"bar/prop\":0", null, null);
+        NodeDocument doc = store.find(NODES, Utils.getIdFromPath("/test/foo"));
+        assertNotNull(doc);
+        String rev = null;
+        for (int i = 0; i < NodeDocument.NUM_REVS_THRESHOLD; i++) {
+            rev = mk.commit("/test/foo", "^\"prop\":" + i, null, null);
+        }
+        ns.runBackgroundOperations();
+        doc = store.find(NODES, Utils.getIdFromPath("/test/foo"));
+        assertNotNull(doc);
+        DocumentNodeState node = doc.getNodeAtRevision(ns,
+                Revision.fromString(rev), null);
+        assertNotNull(node);
+    }
+
     private void syncMKs(List<DocumentMK> mks, int idx) {
         mks.get(idx).runBackgroundOperations();
         for (int i = 0; i < mks.size(); i++) {
