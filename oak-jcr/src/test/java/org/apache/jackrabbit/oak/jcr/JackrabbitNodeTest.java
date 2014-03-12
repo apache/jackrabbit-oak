cf1f04d57772d32d7565eaaeac45c4bfd8d79e82
Julian Reschke <reschke@apache.org>
2014-03-12 15:03:25 +0000

    OAK-1491 - add additional diagnostics for occasionally failing test
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1576773 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/JackrabbitNodeTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/JackrabbitNodeTest.java
@@ -104,15 +104,41 @@ public class JackrabbitNodeTest extends AbstractJCRTest {
             superuser.save();
 
             boolean foundMove = false;
+            StringBuffer diags = new StringBuffer();
             for (Event event : result.getEvents(5000)) {
+                if (diags.length() != 0) {
+                    diags.append(", ");
+                }
+                diags.append("type " + event.getType() + " " + event.getDate() + "ms " + event.getPath());
                 if (Event.NODE_MOVED == event.getType()) {
                     foundMove = true;
                     break;
                 }
             }
+            if (diags.length() == 0) {
+                diags.append("none");
+            }
 
             if (!foundMove) {
-                fail("Expected NODE_MOVED event upon renaming a node.");
+                // force another event, wait some more
+                testRootNode.addNode(name + "XYZ");
+                superuser.save();
+
+                StringBuffer addDiags = new StringBuffer();
+                for (Event event : result.getEvents(60000)) {
+                    if (addDiags.length() != 0) {
+                        addDiags.append(", ");
+                    }
+                    addDiags.append("type " + event.getType() + " " + event.getDate() + "ms " + event.getPath());
+                }
+
+                if (addDiags.length() > 0) {
+                    diags.append("; next event after additional addNode/save operation: " + addDiags);
+                }
+            }
+
+            if (!foundMove) {
+                fail("Expected NODE_MOVED event upon renaming a node (received: " + diags + ")");
             }
         } finally {
             mgr.removeEventListener(result);
