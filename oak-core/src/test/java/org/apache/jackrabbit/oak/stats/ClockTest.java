93ba842a8c600db6f4f3b285fab46ec15b288ee5
Julian Reschke <reschke@apache.org>
2014-03-13 12:34:13 +0000

    OAK-1535: improve test to report actual drift on failure
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1577136 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/stats/ClockTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/stats/ClockTest.java
@@ -29,6 +29,7 @@ public class ClockTest {
     public void testClockDrift() throws InterruptedException {
         ScheduledExecutorService executor =
                 Executors.newSingleThreadScheduledExecutor();
+        final long limit = 20;
         try {
             Clock[] clocks = new Clock[] {
                     Clock.SIMPLE,
@@ -38,14 +39,14 @@ public class ClockTest {
 
             for (Clock clock : clocks) {
                 long drift = clock.getTime() - System.currentTimeMillis();
-                assertTrue(Math.abs(drift) < 20); // Windows can have 15ms gaps
+                assertTrue("unexpected drift: " + Math.abs(drift) + " (limit " + limit +")", Math.abs(drift) < limit); // Windows can have 15ms gaps
             }
 
             Thread.sleep(100);
 
             for (Clock clock : clocks) {
                 long drift = clock.getTime() - System.currentTimeMillis();
-                assertTrue(Math.abs(drift) < 20);
+                assertTrue("unexpected drift: " + Math.abs(drift) + " (limit " + limit +")", Math.abs(drift) < limit);
             }
         } finally {
             executor.shutdown();
