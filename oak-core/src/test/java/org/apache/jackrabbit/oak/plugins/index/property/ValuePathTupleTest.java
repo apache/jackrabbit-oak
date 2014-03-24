ccf15bb834343d0fad7b09d701b7d2866d91449f
Thomas Mueller <thomasm@apache.org>
2014-03-24 14:14:50 +0000

    OAK-1561 Implement optimised range queries
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1580862 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/property/ValuePathTupleTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/property/ValuePathTupleTest.java
@@ -140,13 +140,81 @@ public class ValuePathTupleTest {
             new ValuePathTuple("e", "foobar"),
             new ValuePathTuple("f", "foobar"));
         Iterator<ValuePathTuple> filtered = Iterables.filter(data,
-            new ValuePathTuple.LessThanPredicate("e",true)).iterator();
+                new ValuePathTuple.LessThanPredicate("e", true)).iterator();
         assertTrue(filtered.hasNext());
         assertEquals("a", filtered.next().getValue());
         assertEquals("b", filtered.next().getValue());
         assertEquals("c", filtered.next().getValue());
         assertEquals("d", filtered.next().getValue());
         assertEquals("e", filtered.next().getValue());
+        assertFalse(filtered.hasNext());
+    }
+    
+    @Test
+    public void betweenNoIncludes() {
+        List<ValuePathTuple> data = ImmutableList.of(
+            new ValuePathTuple("a", "foobar"),
+            new ValuePathTuple("b", "foobar"), 
+            new ValuePathTuple("c", "foobar"),
+            new ValuePathTuple("d", "foobar"), 
+            new ValuePathTuple("e", "foobar"),
+            new ValuePathTuple("f", "foobar"));
+        Iterator<ValuePathTuple> filtered = Iterables.filter(data,
+            new ValuePathTuple.BetweenPredicate("b", "d", false, false)).iterator();
+        assertTrue(filtered.hasNext());
+        assertEquals("c", filtered.next().getValue());
+        assertFalse(filtered.hasNext());
+    }
+
+    @Test
+    public void betweenIncludeStart() {
+        List<ValuePathTuple> data = ImmutableList.of(
+            new ValuePathTuple("a", "foobar"),
+            new ValuePathTuple("b", "foobar"), 
+            new ValuePathTuple("c", "foobar"),
+            new ValuePathTuple("d", "foobar"), 
+            new ValuePathTuple("e", "foobar"),
+            new ValuePathTuple("f", "foobar"));
+        Iterator<ValuePathTuple> filtered = Iterables.filter(data,
+            new ValuePathTuple.BetweenPredicate("b", "d", true, false)).iterator();
+        assertTrue(filtered.hasNext());
+        assertEquals("b", filtered.next().getValue());
+        assertEquals("c", filtered.next().getValue());
+        assertFalse(filtered.hasNext());
+    }
+
+    @Test
+    public void betweenIncludeEnd() {
+        List<ValuePathTuple> data = ImmutableList.of(
+            new ValuePathTuple("a", "foobar"),
+            new ValuePathTuple("b", "foobar"), 
+            new ValuePathTuple("c", "foobar"),
+            new ValuePathTuple("d", "foobar"), 
+            new ValuePathTuple("e", "foobar"),
+            new ValuePathTuple("f", "foobar"));
+        Iterator<ValuePathTuple> filtered = Iterables.filter(data,
+            new ValuePathTuple.BetweenPredicate("b", "d", false, true)).iterator();
+        assertTrue(filtered.hasNext());
+        assertEquals("c", filtered.next().getValue());
+        assertEquals("d", filtered.next().getValue());
+        assertFalse(filtered.hasNext());
+    }
+
+    @Test
+    public void betweenIncludeBoth() {
+        List<ValuePathTuple> data = ImmutableList.of(
+            new ValuePathTuple("a", "foobar"),
+            new ValuePathTuple("b", "foobar"), 
+            new ValuePathTuple("c", "foobar"),
+            new ValuePathTuple("d", "foobar"), 
+            new ValuePathTuple("e", "foobar"),
+            new ValuePathTuple("f", "foobar"));
+        Iterator<ValuePathTuple> filtered = Iterables.filter(data,
+            new ValuePathTuple.BetweenPredicate("b", "d", true, true)).iterator();
+        assertTrue(filtered.hasNext());
+        assertEquals("b", filtered.next().getValue());
+        assertEquals("c", filtered.next().getValue());
+        assertEquals("d", filtered.next().getValue());
         assertFalse(filtered.hasNext());
     }
 }
