970b10b0ff5bab8df27d71cd5177ce35949f2e3d
Jukka Zitting <jukka@apache.org>
2014-03-25 03:24:50 +0000

    OAK-1594: Build fails on Java 8
    Make SimpleTest.escapePropertyName() independent of property ordering
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1581202 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/SimpleTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/SimpleTest.java
@@ -231,9 +231,13 @@ public class SimpleTest {
     public void escapePropertyName() {
         DocumentMK mk = createMK();
         String rev = mk.commit(
-                "/", "+\"test\":{\"name.first\": \"Hello\", \"_id\": \"a\", \"$x\": \"1\"}", null, null);
-        String test = mk.getNodes("/test", rev, 0, 0, Integer.MAX_VALUE, null);
-        assertEquals("{\"$x\":\"1\",\"_id\":\"a\",\"name.first\":\"Hello\",\":childNodeCount\":0}", test);
+                "/", "+\"test1\":{\"name.first\": \"Hello\"} +\"test2\":{\"_id\": \"a\"} +\"test3\":{\"$x\": \"1\"}", null, null);
+        String test1 = mk.getNodes("/test1", rev, 0, 0, Integer.MAX_VALUE, null);
+        assertEquals("{\"name.first\":\"Hello\",\":childNodeCount\":0}", test1);
+        String test2 = mk.getNodes("/test2", rev, 0, 0, Integer.MAX_VALUE, null);
+        assertEquals("{\"_id\":\"a\",\":childNodeCount\":0}", test2);
+        String test3 = mk.getNodes("/test3", rev, 0, 0, Integer.MAX_VALUE, null);
+        assertEquals("{\"$x\":\"1\",\":childNodeCount\":0}", test3);
         mk.dispose();
     }
 
