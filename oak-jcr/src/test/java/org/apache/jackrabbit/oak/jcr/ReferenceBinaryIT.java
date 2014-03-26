75467ac9640aae21372653d85da15b588c09cdce
Chetan Mehrotra <chetanm@apache.org>
2014-03-26 07:58:05 +0000

    OAK-1604 - Support for signed references in Blob (WIP)
    Port the org.apache.jackrabbit.core.value.ReferenceBinaryTest to Oak. The test is run with two fixture for SegmentStore and DocumentNodeStore backed by FileDataStore
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1581720 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/ReferenceBinaryIT.java
@@ -0,0 +1,169 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.jackrabbit.oak.jcr;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+
+import javax.jcr.Binary;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+
+import com.google.common.collect.Lists;
+import com.google.common.io.BaseEncoding;
+import org.apache.jackrabbit.api.JackrabbitRepository;
+import org.apache.jackrabbit.api.ReferenceBinary;
+import org.apache.jackrabbit.commons.jackrabbit.SimpleReferenceBinary;
+import org.apache.jackrabbit.core.data.RandomInputStream;
+import org.apache.jackrabbit.oak.plugins.blob.datastore.DataStoreBlobStore;
+import org.apache.jackrabbit.oak.plugins.blob.datastore.OakFileDataStore;
+import org.apache.jackrabbit.oak.plugins.segment.SegmentStore;
+import org.apache.jackrabbit.oak.plugins.segment.file.FileStore;
+import org.apache.jackrabbit.oak.spi.blob.BlobStore;
+import org.apache.jackrabbit.oak.spi.state.NodeStore;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.apache.jackrabbit.oak.jcr.NodeStoreFixture.DocumentFixture;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(Parameterized.class)
+public class ReferenceBinaryIT {
+
+    //Taken from org.apache.jackrabbit.oak.plugins.segment.Segment
+    //As SegmentStore inlines binary content with size less then MEDIUM_LIMIT
+    static final int SMALL_LIMIT = 1 << 7;
+    static final int MEDIUM_LIMIT = (1 << (16 - 2)) + SMALL_LIMIT;
+
+    private static final int STREAM_LENGTH = MEDIUM_LIMIT + 1000;
+
+    private final NodeStoreFixture fixture;
+
+    private NodeStore nodeStore;
+    private Repository repository;
+
+    public ReferenceBinaryIT(NodeStoreFixture fixture) {
+        this.fixture = fixture;
+    }
+
+    @Before
+    public void setup() throws RepositoryException {
+        nodeStore = fixture.createNodeStore();
+        repository  = new Jcr(nodeStore).createRepository();
+    }
+
+    /**
+     * Taken from org.apache.jackrabbit.core.value.ReferenceBinaryTest
+     * @throws Exception
+     */
+    @Test
+    public void testReferenceBinaryExchangeWithSharedRepository() throws Exception {
+        Session firstSession = createAdminSession();
+
+        // create a binary
+        Binary b = firstSession.getValueFactory().createBinary(new RandomInputStream(1, STREAM_LENGTH));
+
+        ReferenceBinary referenceBinary = null;
+        if (b instanceof ReferenceBinary) {
+            referenceBinary = (ReferenceBinary) b;
+        }
+
+        assertNotNull(referenceBinary);
+
+        assertNotNull(referenceBinary.getReference());
+
+        // in the current test the message is exchanged via repository which is shared as well
+        // put the reference message value in a property on a node
+        String newNode = "sample_" + System.nanoTime();
+        firstSession.getRootNode().addNode(newNode).setProperty("reference", referenceBinary.getReference());
+
+        // save the first session
+        firstSession.save();
+
+        // get a second session over the same repository / ds
+        Session secondSession = repository.login(new SimpleCredentials("admin", "admin".toCharArray()));
+
+        // read the binary referenced by the referencing binary
+        String reference = secondSession.getRootNode().getNode(newNode).getProperty("reference").getString();
+
+        ReferenceBinary ref = new SimpleReferenceBinary(reference);
+
+        assertEquals(b, secondSession.getValueFactory().createValue(ref).getBinary());
+
+        safeLogout(firstSession);
+        safeLogout(secondSession);
+
+    }
+
+    @After
+    public void tearDown() {
+        if (repository instanceof JackrabbitRepository) {
+            ((JackrabbitRepository) repository).shutdown();
+        }
+        fixture.dispose(nodeStore);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> fixtures() throws IOException {
+        File file = new File(new File("target"), "tar." + System.nanoTime());
+        SegmentStore segmentStore = new FileStore(createBlobStore(), file, 266, true);
+
+        List<Object[]> fixtures = Lists.newArrayList();
+        NodeStoreFixture.SegmentFixture segmentFixture = new NodeStoreFixture.SegmentFixture(segmentStore);
+        if (segmentFixture.isAvailable()) {
+            fixtures.add(new Object[] {segmentFixture});
+        }
+        DocumentFixture documentFixture = new DocumentFixture(DocumentFixture.DEFAULT_URI, false, createBlobStore());
+        if (documentFixture.isAvailable()) {
+            fixtures.add(new Object[]{documentFixture});
+        }
+        return fixtures;
+    }
+
+    private static BlobStore createBlobStore(){
+        File file = new File(new File("target"), "datastore." + System.nanoTime());
+        OakFileDataStore fds = new OakFileDataStore();
+        byte[] key = new byte[256];
+        new Random().nextBytes(key);
+        fds.setSigningKey(BaseEncoding.base64().encode(key));
+        fds.setMinRecordLength(4092);
+        fds.init(file.getAbsolutePath());
+        return new DataStoreBlobStore(fds);
+    }
+
+    private Session createAdminSession() throws RepositoryException {
+        return repository.login(new SimpleCredentials("admin", "admin".toCharArray()));
+    }
+
+    private static void safeLogout(Session session) {
+        try {
+            session.logout();
+        } catch (Exception ignore) {}
+    }
+}
