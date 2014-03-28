807c0cfa1f6e251a59fdc58d227fabdc325065bb
Tommaso Teofili <tommaso@apache.org>
2014-03-28 11:17:09 +0000

    OAK-1637 - more configurable SolrIndexInitializer
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1582702 13f79535-47bb-0310-9956-ffa450edef68

--- oak-solr-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/solr/util/package-info.java
+++ oak-solr-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/solr/util/package-info.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@Version("0.18")
+@Version("0.19")
 @Export(optional = "provide:=true")
 package org.apache.jackrabbit.oak.plugins.index.solr.util;
 
