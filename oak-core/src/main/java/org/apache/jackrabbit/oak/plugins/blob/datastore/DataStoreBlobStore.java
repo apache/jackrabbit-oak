c91bfa543be9e0ff9e2f255f3e19080e8067e025
Chetan Mehrotra <chetanm@apache.org>
2014-04-01 10:43:55 +0000

    OAK-1655 - DataStoreBlobStore does not take into maxLastModifiedTime when fetching all chunks
    If the maxLastModifiedTime <= 0then all files would be returned
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1583603 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/blob/datastore/DataStoreBlobStore.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/blob/datastore/DataStoreBlobStore.java
@@ -267,7 +267,8 @@ public class DataStoreBlobStore implements DataStore, BlobStore, GarbageCollecta
             public boolean apply(DataIdentifier input) {
                 try {
                     DataRecord dr = delegate.getRecord(input);
-                    if(dr != null && dr.getLastModified() < maxLastModifiedTime){
+                    if(dr != null && (maxLastModifiedTime <=0
+                            || dr.getLastModified() < maxLastModifiedTime)){
                         return true;
                     }
                 } catch (DataStoreException e) {
