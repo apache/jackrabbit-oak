ce81b317cf1e5115eb61ca401042efa29294b8d4
Alexandru Parvulescu <alexparvulescu@apache.org>
2014-04-01 20:10:17 +0000

    OAK-1654 Composite index aggregates
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1583771 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/aggregate/AggregateIndex.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/aggregate/AggregateIndex.java
@@ -16,15 +16,15 @@
  */
 package org.apache.jackrabbit.oak.plugins.index.aggregate;
 
-import java.util.ArrayList;
+
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.jackrabbit.oak.api.PropertyValue;
-import org.apache.jackrabbit.oak.commons.PathUtils;
 import org.apache.jackrabbit.oak.query.fulltext.FullTextAnd;
 import org.apache.jackrabbit.oak.query.fulltext.FullTextExpression;
 import org.apache.jackrabbit.oak.query.fulltext.FullTextOr;
@@ -32,14 +32,17 @@ import org.apache.jackrabbit.oak.query.fulltext.FullTextTerm;
 import org.apache.jackrabbit.oak.query.fulltext.FullTextVisitor;
 import org.apache.jackrabbit.oak.query.index.FilterImpl;
 import org.apache.jackrabbit.oak.spi.query.Cursor;
+import org.apache.jackrabbit.oak.spi.query.Cursors;
 import org.apache.jackrabbit.oak.spi.query.Cursors.AbstractCursor;
 import org.apache.jackrabbit.oak.spi.query.Filter;
 import org.apache.jackrabbit.oak.spi.query.IndexRow;
 import org.apache.jackrabbit.oak.spi.query.QueryIndex.FulltextQueryIndex;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
 
+import com.google.common.base.Function;
 import com.google.common.base.Predicates;
 import com.google.common.collect.Iterators;
+import com.google.common.collect.Lists;
 
 /**
  * A virtual full-text that can aggregate nodes based on aggregate definitions.
@@ -58,7 +61,13 @@ public class AggregateIndex implements FulltextQueryIndex {
         if (baseIndex == null) {
             return Double.POSITIVE_INFINITY;
         }
-        return baseIndex.getCost(filter, rootState) - 0.05;
+        double localCost = Double.POSITIVE_INFINITY;
+        FullTextExpression e = filter.getFullTextConstraint();
+        if (e != null && hasCompositeExpression(e)) {
+            localCost = flattenCost(e, filter, baseIndex, rootState);
+        }
+        double baseCost = baseIndex.getCost(filter, rootState);
+        return Math.min(localCost, baseCost) - 0.05;
     }
 
     @Override
@@ -67,68 +76,170 @@ public class AggregateIndex implements FulltextQueryIndex {
         if (baseIndex.getNodeAggregator() == null) {
             return baseIndex.query(filter, rootState);
         }
-        return new AggregationCursor(baseIndex.query(
-                newAggregationFilter(filter), rootState),
-                baseIndex.getNodeAggregator(), rootState);
+        return newCursor(filter, baseIndex, rootState);
     }
 
-    private static Filter newAggregationFilter(Filter filter) {
-        FilterImpl f = new FilterImpl(filter);
-        // disables node type checks for now
-        f.setMatchesAllTypes(true);
-
-        // TODO OAK-828
-        // FullTextExpression constraint = filter.getFullTextConstraint();
-        // constraint = getFlatConstraint(constraint);
-        // f.setFullTextConstraint(constraint);
-
-        return f;
-    }
-
-    static FullTextExpression getFlatConstraint(
-            FullTextExpression constraint) {
-        if (constraint == null) {
-            return null;
+    private static Cursor newCursor(Filter f, FulltextQueryIndex index,
+            NodeState state) {
+        FullTextExpression e = f.getFullTextConstraint();
+        if (hasCompositeExpression(e)) {
+            Cursor c = flatten(e, f, index, state);
+            if (c != null) {
+                return c;
+            }
         }
-        final AtomicReference<FullTextExpression> result = new AtomicReference<FullTextExpression>();
-        constraint.accept(new FullTextVisitor() {
-            
+        return new AggregationCursor(index.query(newAggregationFilter(f, null),
+                state), index.getNodeAggregator(), state);
+    }
+
+    private static boolean hasCompositeExpression(FullTextExpression ft) {
+        if (ft == null) {
+            return false;
+        }
+        final AtomicReference<Boolean> composite = new AtomicReference<Boolean>();
+        composite.set(false);
+
+        ft.accept(new FullTextVisitor() {
+
             @Override
             public boolean visit(FullTextTerm term) {
-                String p = term.getPropertyName();
-                if (p != null) {
-                    if (PathUtils.getDepth(p) > 1) {
-                        // remove indirection
-                        String name = PathUtils.getName(p);
-                        term = new FullTextTerm(name, term);
-                    }
-                }
-                result.set(term);
                 return true;
             }
 
             @Override
             public boolean visit(FullTextAnd and) {
-                ArrayList<FullTextExpression> list = new ArrayList<FullTextExpression>();
-                for (FullTextExpression e : and.list) {
-                    list.add(getFlatConstraint(e));
-                }
-                result.set(new FullTextAnd(list));
+                composite.set(true);
                 return true;
             }
 
             @Override
             public boolean visit(FullTextOr or) {
-                ArrayList<FullTextExpression> list = new ArrayList<FullTextExpression>();
-                for (FullTextExpression e : or.list) {
-                    list.add(getFlatConstraint(e));
-                }
-                result.set(new FullTextOr(list));
+                composite.set(true);
                 return true;
             }
-            
+        });
+        return composite.get() && !hasNegativeContains(ft);
+    }
+
+    private static boolean hasNegativeContains(FullTextExpression ft) {
+        if (ft == null) {
+            return false;
+        }
+        final AtomicReference<Boolean> hasNegative = new AtomicReference<Boolean>();
+        hasNegative.set(false);
+
+        ft.accept(new FullTextVisitor.FullTextVisitorBase() {
+
+            @Override
+            public boolean visit(FullTextTerm term) {
+                if (term.isNot()) {
+                    hasNegative.set(true);
+                }
+                return true;
+            }
+
+        });
+        return hasNegative.get();
+    }
+
+    private static Cursor flatten(FullTextExpression constraint,
+            final Filter filter, final FulltextQueryIndex index,
+            final NodeState state) {
+        if (constraint == null) {
+            return null;
+        }
+        final AtomicReference<Cursor> result = new AtomicReference<Cursor>();
+        constraint.accept(new FullTextVisitor() {
+
+            @Override
+            public boolean visit(FullTextTerm term) {
+                result.set(filterToCursor(newAggregationFilter(filter, term),
+                        index, state));
+                return true;
+            }
+
+            @Override
+            public boolean visit(FullTextAnd and) {
+                Iterator<FullTextExpression> iterator = and.list.iterator();
+                Cursor c = flatten(iterator.next(), filter, index, state);
+                while (iterator.hasNext()) {
+                    FullTextExpression input = iterator.next();
+                    Cursor newC = flatten(input, filter, index, state);
+                    c = Cursors.newIntersectionCursor(c, newC,
+                            filter.getQueryEngineSettings());
+                }
+                result.set(c);
+                return true;
+            }
+
+            @Override
+            public boolean visit(FullTextOr or) {
+                List<Cursor> cursors = Lists.transform(or.list,
+                        new Function<FullTextExpression, Cursor>() {
+                            @Override
+                            public Cursor apply(FullTextExpression input) {
+                                return flatten(input, filter, index, state);
+                            }
+                        });
+                result.set(Cursors.newConcatCursor(cursors,
+                        filter.getQueryEngineSettings()));
+                return true;
+            }
         });
         return result.get();
+    }
+
+    private static double flattenCost(FullTextExpression constraint,
+            final Filter filter, final FulltextQueryIndex index,
+            final NodeState state) {
+        if (constraint == null) {
+            return Double.POSITIVE_INFINITY;
+        }
+        final AtomicReference<Double> result = new AtomicReference<Double>();
+        result.set(0d);
+        constraint.accept(new FullTextVisitor() {
+
+            @Override
+            public boolean visit(FullTextTerm term) {
+                result.set(result.get() + index.getCost(newAggregationFilter(filter, term), state));
+                return true;
+            }
+
+            @Override
+            public boolean visit(FullTextAnd and) {
+                for (FullTextExpression input : and.list) {
+                    double d = flattenCost(input, filter, index, state);
+                    result.set(result.get() + d);
+                }
+                return true;
+            }
+
+            @Override
+            public boolean visit(FullTextOr or) {
+                for (FullTextExpression input : or.list) {
+                    double d = flattenCost(input, filter, index, state);
+                    result.set(result.get() + d);
+                }
+                return true;
+            }
+        });
+        return result.get();
+    }
+
+    private static Cursor filterToCursor(Filter f, FulltextQueryIndex index,
+            NodeState state) {
+        return new AggregationCursor(index.query(f, state),
+                index.getNodeAggregator(), state);
+    }
+
+    private static Filter newAggregationFilter(Filter filter, FullTextExpression exp) {
+        FilterImpl f = new FilterImpl(filter);
+        // disables node type checks for now
+        f.setMatchesAllTypes(true);
+        if (exp != null) {
+            f.setFullTextConstraint(exp);
+        }
+        return f;
     }
 
     @Override
