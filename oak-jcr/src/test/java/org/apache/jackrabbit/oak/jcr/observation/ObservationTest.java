fa7446a99a432858e0c8aa7940602575fdaa36d7
Michael Dürig <mduerig@apache.org>
2014-04-02 14:32:01 +0000

    OAK-1661: JCR Event Info should contain NodeType for Events Type Node-Deleted pass primary and mixin types of added and removed nodes through the event's info map
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1584053 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/observation/ObservationTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/observation/ObservationTest.java
@@ -28,6 +28,9 @@ import static javax.jcr.observation.Event.PERSIST;
 import static javax.jcr.observation.Event.PROPERTY_ADDED;
 import static javax.jcr.observation.Event.PROPERTY_CHANGED;
 import static javax.jcr.observation.Event.PROPERTY_REMOVED;
+import static org.apache.jackrabbit.JcrConstants.JCR_MIXINTYPES;
+import static org.apache.jackrabbit.JcrConstants.JCR_PRIMARYTYPE;
+import static org.apache.jackrabbit.JcrConstants.NT_UNSTRUCTURED;
 import static org.apache.jackrabbit.oak.api.Type.STRING;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -149,6 +152,65 @@ public class ObservationTest extends AbstractRepositoryTest {
             missing = listener.getMissing(TIME_OUT, TimeUnit.SECONDS);
             assertTrue("Missing events: " + missing, missing.isEmpty());
             unexpected = listener.getUnexpected();
+            assertTrue("Unexpected events: " + unexpected, unexpected.isEmpty());
+        }
+        finally {
+            observationManager.removeEventListener(listener);
+        }
+    }
+
+    @Test
+    public void infoMap() throws RepositoryException, ExecutionException, InterruptedException {
+        ExpectationListener listener = new ExpectationListener();
+        observationManager.addEventListener(listener, NODE_ADDED, "/", true, null, null, false);
+        try {
+            Node n = getNode(TEST_PATH);
+            n.addNode("n1", "oak:Unstructured");
+            n.addNode("n2");
+            n.getNode("n2").addMixin(TEST_TYPE);
+
+            listener.expect(new Expectation("infoMap for n1") {
+                @Override
+                public boolean onEvent(Event event) throws Exception {
+                    if (event.getPath().endsWith("n1")) {
+                        Map<?, ?> info = event.getInfo();
+                        return info != null &&
+                                "oak:Unstructured".equals(info.get(JCR_PRIMARYTYPE));
+                    } else {
+                        return false;
+                    }
+                }
+            });
+
+            listener.expect(new Expectation("infoMap for n2") {
+                @Override
+                public boolean onEvent(Event event) throws Exception {
+                    if (event.getPath().endsWith("n2")) {
+                        Map<?, ?> info = event.getInfo();
+                        if (info == null) {
+                            return false;
+                        }
+                        Object mixinTypes = info.get(JCR_MIXINTYPES);
+                        if (!(mixinTypes instanceof String[])) {
+                            return false;
+                        }
+
+                        Object primaryType = info.get(JCR_PRIMARYTYPE);
+                        String[] mixins = (String[]) mixinTypes;
+                        return NT_UNSTRUCTURED.equals(primaryType) &&
+                                mixins.length == 1 &&
+                                TEST_TYPE.equals(mixins[0]);
+                    } else {
+                        return false;
+                    }
+                }
+            });
+
+            getAdminSession().save();
+
+            List<Expectation> missing = listener.getMissing(TIME_OUT, TimeUnit.SECONDS);
+            assertTrue("Missing events: " + missing, missing.isEmpty());
+            List<Event> unexpected = listener.getUnexpected();
             assertTrue("Unexpected events: " + unexpected, unexpected.isEmpty());
         }
         finally {
