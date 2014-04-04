078f7bd21be3258e610b22015b79c1ea4367db29
Michael Duerig <mduerig@apache.org>
2014-04-04 16:11:40 +0000

    OAK-1674: Node isNew() is false in case the node is removed and added in same commit @Ignored test cases
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1584781 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/core/MutableTreeTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/core/MutableTreeTest.java
@@ -45,6 +45,7 @@ import org.apache.jackrabbit.oak.plugins.memory.StringBasedBlob;
 import org.apache.jackrabbit.oak.plugins.tree.TreeConstants;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -310,6 +311,18 @@ public class MutableTreeTest extends OakBaseTest {
 
         assertFalse(x.exists());
         assertFalse(y.exists());
+    }
+
+    @Ignore("OAK-1674")  // FIXME OAK-1674
+    @Test
+    public void isNew() throws CommitFailedException {
+        Tree tree = root.getTree("/");
+        tree.addChild("c");
+        root.commit();
+
+        tree.getChild("c").remove();
+        Tree c = tree.addChild("c");
+        assertEquals(Status.NEW, c.getStatus());
     }
 
     @Test
