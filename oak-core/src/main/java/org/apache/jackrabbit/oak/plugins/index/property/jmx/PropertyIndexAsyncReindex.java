50e1382b211e19ab3e7afb6f03cccf000bba7795
Alexandru Parvulescu <alexparvulescu@apache.org>
2014-04-08 10:35:18 +0000

    OAK-1684 Non-blocking reindexing doesn't save the initial checkpoint
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1585680 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/property/jmx/PropertyIndexAsyncReindex.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jackrabbit.oak.plugins.index.property.jmx;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static java.lang.System.nanoTime;
+import static org.apache.jackrabbit.oak.management.ManagementOperation.done;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+
+import javax.annotation.Nonnull;
+import javax.management.openmbean.CompositeData;
+
+import org.apache.jackrabbit.oak.management.ManagementOperation;
+import org.apache.jackrabbit.oak.plugins.index.AsyncIndexUpdate;
+
+/**
+ * Default implementation of {@link PropertyIndexAsyncReindexMBean} based on a
+ * {@code Runnable}.
+ */
+public class PropertyIndexAsyncReindex implements
+        PropertyIndexAsyncReindexMBean {
+
+    public static final String OP_NAME = "Property index asynchronous reindex";
+
+    private final AsyncIndexUpdate async;
+    private final Executor executor;
+
+    private ManagementOperation arOp = done(OP_NAME, 0);
+
+    /**
+     * @param gc
+     *            Revision garbage collector
+     * @param executor
+     *            executor for running the garbage collection task
+     */
+    public PropertyIndexAsyncReindex(@Nonnull AsyncIndexUpdate async,
+            @Nonnull Executor executor) {
+        this.async = checkNotNull(async);
+        this.executor = checkNotNull(executor);
+    }
+
+    @Nonnull
+    @Override
+    public CompositeData startPropertyIndexAsyncReindex() {
+        if (arOp.isDone()) {
+            arOp = new ManagementOperation(OP_NAME, new Callable<Long>() {
+                @Override
+                public Long call() throws Exception {
+                    long t0 = nanoTime();
+                    boolean done = false;
+                    while (!done) {
+                        async.run();
+                        done = async.isFinished();
+                    }
+                    return nanoTime() - t0;
+                }
+            });
+            executor.execute(arOp);
+        }
+        return getPropertyIndexAsyncReindexStatus();
+    }
+
+    @Nonnull
+    @Override
+    public CompositeData getPropertyIndexAsyncReindexStatus() {
+        return arOp.getStatus().toCompositeData();
+    }
+}
