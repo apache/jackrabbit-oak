a0f5698af8917b2d7941e07a877dd7c6831612fd
Marcel Reutegger <mreutegg@apache.org =  mreutegg = Marcel Reutegger mreutegg@apache.org@apache.org>
2014-04-15 11:49:52 +0000

    OAK-1732: Cluster node lease not renewed in time
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1587538 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/ClusterTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/ClusterTest.java
@@ -131,7 +131,7 @@ public class ClusterTest {
         assertEquals(1, c1.getId());
         c1.setLeaseTime(1);
         // this will quickly expire
-        c1.renewLease(1);
+        c1.renewLease();
         Thread.sleep(10);
         c2 = ClusterNodeInfo.getInstance(store, "m1", null);
         assertEquals(1, c2.getId());
