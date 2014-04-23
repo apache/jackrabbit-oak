d46133d53645ce398c541016626ff852013b8c27
Thomas Mueller <thomasm@apache.org>
2014-04-23 08:11:57 +0000

    OAK-1755 Search fails if the property to be searched on is of different types in different nodes
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1589352 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/query/QueryTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/query/QueryTest.java
@@ -34,6 +34,7 @@ import java.util.Set;
 
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
+import javax.jcr.PropertyType;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.ValueFactory;
@@ -59,6 +60,23 @@ public class QueryTest extends AbstractRepositoryTest {
 
     public QueryTest(NodeStoreFixture fixture) {
         super(fixture);
+    }
+    
+    @Test
+    public void date() throws Exception {
+        Session session = getAdminSession();
+        Node t1 = session.getRootNode().addNode("t1");
+        t1.setProperty("x", "22.06.07");
+        Node t2 = session.getRootNode().addNode("t2");
+        t2.setProperty("x", "2007-06-22T01:02:03.000Z", PropertyType.DATE);
+        session.save();
+        
+        String query = "//*[x='a' or x='b']";
+        QueryResult r = session.getWorkspace().
+                getQueryManager().createQuery(
+                query, "xpath").execute();
+        NodeIterator it = r.getNodes();
+        assertFalse(it.hasNext());
     }
     
     @Test
