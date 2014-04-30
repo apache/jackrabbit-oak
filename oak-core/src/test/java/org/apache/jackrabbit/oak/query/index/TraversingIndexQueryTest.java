bd058aeb397df83cabce25324be913348e53136f
Alexandru Parvulescu <alexparvulescu@apache.org>
2014-04-30 10:05:19 +0000

    OAK-1739 Incorrect handling of multivalued comparisons in queries  - added a new test for relative properties fixed by this issue
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1591229 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/query/index/TraversingIndexQueryTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/query/index/TraversingIndexQueryTest.java
@@ -13,6 +13,8 @@
  */
 package org.apache.jackrabbit.oak.query.index;
 
+import static org.apache.jackrabbit.JcrConstants.JCR_PRIMARYTYPE;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -233,6 +235,69 @@ public class TraversingIndexQueryTest extends AbstractQueryTest {
                 ImmutableList.of("/content"));
         assertQuery("//*[(*/@prop > 200)]", "xpath", new ArrayList<String>());
         assertQuery("//*[(*/@prop > 1000)]", "xpath", new ArrayList<String>());
+    }
+
+    @Test
+    public void testRelativeProperties2() throws Exception {
+        Tree t = root.getTree("/").addChild("content").addChild("nodes");
+        Tree a = t.addChild("a");
+        a.setProperty(JCR_PRIMARYTYPE, "nt:unstructured", Type.NAME);
+        Tree b = a.addChild("b");
+        b.setProperty(JCR_PRIMARYTYPE, "nt:unstructured", Type.NAME);
+        Tree c = b.addChild("c");
+        c.setProperty(JCR_PRIMARYTYPE, "nt:unstructured", Type.NAME);
+
+        Tree d1 = c.addChild("d1");
+        d1.setProperty("prop", 10);
+        d1.setProperty(JCR_PRIMARYTYPE, "nt:unstructured", Type.NAME);
+        Tree d2 = c.addChild("d2");
+        d2.setProperty("prop", 20);
+        d2.setProperty(JCR_PRIMARYTYPE, "nt:unstructured", Type.NAME);
+        Tree d3 = c.addChild("d3");
+        d3.setProperty("prop", 30);
+        d3.setProperty(JCR_PRIMARYTYPE, "nt:unstructured", Type.NAME);
+        root.commit();
+
+        assertQuery("/jcr:root/content/nodes//*[(*/*/*/@prop >= 9)]", "xpath",
+                ImmutableList.of("/content/nodes/a", "/content/nodes/a/b",
+                        "/content/nodes/a/b/c"));
+
+        assertQuery(
+                "/jcr:root/content/nodes//element(*, nt:unstructured)[(*/*/*/@prop >= 9)]",
+                "xpath", ImmutableList.of("/content/nodes/a",
+                        "/content/nodes/a/b", "/content/nodes/a/b/c"));
+
+        assertQuery("/jcr:root/content/nodes//*[(*/*/*/@prop >= 10)]", "xpath",
+                ImmutableList.of("/content/nodes/a", "/content/nodes/a/b",
+                        "/content/nodes/a/b/c"));
+        assertQuery(
+                "/jcr:root/content/nodes//element(*, nt:unstructured)[(*/*/*/@prop >= 10)]",
+                "xpath", ImmutableList.of("/content/nodes/a",
+                        "/content/nodes/a/b", "/content/nodes/a/b/c"));
+
+        assertQuery("/jcr:root/content/nodes//*[(*/*/*/@prop >= 15)]", "xpath",
+                ImmutableList.of("/content/nodes/a", "/content/nodes/a/b",
+                        "/content/nodes/a/b/c"));
+        assertQuery(
+                "/jcr:root/content/nodes//element(*, nt:unstructured)[(*/*/*/@prop >= 15)]",
+                "xpath", ImmutableList.of("/content/nodes/a",
+                        "/content/nodes/a/b", "/content/nodes/a/b/c"));
+
+        assertQuery("/jcr:root/content/nodes//*[(*/*/*/@prop >= 20)]", "xpath",
+                ImmutableList.of("/content/nodes/a", "/content/nodes/a/b",
+                        "/content/nodes/a/b/c"));
+        assertQuery(
+                "/jcr:root/content/nodes//element(*, nt:unstructured)[(*/*/*/@prop >= 20)]",
+                "xpath", ImmutableList.of("/content/nodes/a",
+                        "/content/nodes/a/b", "/content/nodes/a/b/c"));
+
+        assertQuery("/jcr:root/content/nodes//*[(*/*/*/@prop >= 30)]", "xpath",
+                ImmutableList.of("/content/nodes/a", "/content/nodes/a/b",
+                        "/content/nodes/a/b/c"));
+        assertQuery(
+                "/jcr:root/content/nodes//element(*, nt:unstructured)[(*/*/*/@prop >= 30)]",
+                "xpath", ImmutableList.of("/content/nodes/a",
+                        "/content/nodes/a/b", "/content/nodes/a/b/c"));
     }
 
     @Test
