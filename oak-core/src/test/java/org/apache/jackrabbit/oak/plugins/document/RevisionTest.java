077efee59b63d5272650d1cf4f5b2ad1ef23014a
Thomas Mueller <thomasm@apache.org>
2014-05-08 12:29:52 +0000

    OAK-1807 ConstraintViolationException seen with multiple Oak/Mongo with ConcurrentCreateNodesTest
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1593250 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/RevisionTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/RevisionTest.java
@@ -17,6 +17,8 @@
 package org.apache.jackrabbit.oak.plugins.document;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
@@ -35,8 +37,6 @@ import com.google.common.collect.Queues;
 import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.Uninterruptibles;
 import org.apache.jackrabbit.oak.plugins.document.Revision.RevisionComparator;
-import org.apache.jackrabbit.oak.stats.Clock;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -297,7 +297,44 @@ public class RevisionTest {
         assertEquals(new Revision(0x30, 0, 0), comp.getRevisionSeen(r21));
     }
 
-    @Ignore("OAK-1807")
+    @Test
+    public void uniqueRevision2() throws Exception {
+        List<Thread> threads = new ArrayList<Thread>();
+        final AtomicBoolean stop = new AtomicBoolean();
+        final Set<Revision> set = Collections
+                .synchronizedSet(new HashSet<Revision>());
+        final Revision[] duplicate = new Revision[1];
+        for (int i = 0; i < 20; i++) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Revision[] last = new Revision[1024];
+                    while (!stop.get()) {
+                        for (Revision r : last) {
+                            set.remove(r);
+                        }
+                        for (int i = 0; i < last.length; i++) {
+                            last[i] = Revision.newRevision(1);
+                        }
+                        for (Revision r : last) {
+                            if (!set.add(r)) {
+                                duplicate[0] = r;
+                            }
+                        }
+                    }
+                }
+            });
+            thread.start();
+            threads.add(thread);
+        }
+        Thread.sleep(200);
+        stop.set(true);
+        for (Thread t : threads) {
+            t.join();
+        }
+        assertNull("Duplicate revision", duplicate[0]);
+    }
+
     @Test
     public void uniqueRevision() throws Exception {
         //Revision.setClock(new Clock.Virtual());
