7171577d6072bd69a436cc45bcfddd08fcfbd01d
Chetan Mehrotra <chetanm@apache.org>
2014-05-13 09:17:10 +0000

    OAK-1645 - Route find queries to Mongo secondary in MongoDocumentStore
    The find and query method now explicitly define the read preference to be used. The preference is determined by type of Collection , maxCacheAge and modified time of parent. if preference is secondary then actual preference used is picked from DB level settings
    
    Also exposed a setting 'maxReplicationLagMillis' which determines the max possible lag in replication beyond which its safe to route the read queries to secondaries
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1594161 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/mongo/ReadPreferenceIT.java
@@ -0,0 +1,152 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.jackrabbit.oak.plugins.document.mongo;
+
+import java.util.concurrent.TimeUnit;
+
+import com.mongodb.ReadPreference;
+import org.apache.jackrabbit.oak.plugins.document.DocumentMK;
+import org.apache.jackrabbit.oak.plugins.document.DocumentNodeStore;
+import org.apache.jackrabbit.oak.plugins.document.MongoUtils;
+import org.apache.jackrabbit.oak.plugins.document.Revision;
+import org.apache.jackrabbit.oak.plugins.document.util.MongoConnection;
+import org.apache.jackrabbit.oak.plugins.document.util.Utils;
+import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
+import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
+import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
+import org.apache.jackrabbit.oak.spi.state.NodeStore;
+import org.apache.jackrabbit.oak.stats.Clock;
+import org.junit.After;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.apache.jackrabbit.oak.plugins.document.Collection.NODES;
+import static org.apache.jackrabbit.oak.plugins.document.Collection.SETTINGS;
+import static org.apache.jackrabbit.oak.plugins.document.mongo.MongoDocumentStore.DocumentReadPreference;
+import static org.junit.Assert.assertEquals;
+
+public class ReadPreferenceIT {
+
+    private DocumentNodeStore documentNodeStore;
+    private NodeStore nodeStore;
+
+    private MongoDocumentStore mongoDS;
+
+    private Clock clock;
+
+    private long replicationLag;
+
+    @BeforeClass
+    public static void checkMongoDbAvailable() {
+        Assume.assumeNotNull(MongoUtils.getConnection());
+    }
+
+    @Before
+    public void prepareStores() throws Exception {
+        clock = new Clock.Virtual();
+        replicationLag = TimeUnit.SECONDS.toMillis(10);
+        MongoConnection mc = MongoUtils.getConnection();
+        documentNodeStore = new DocumentMK.Builder()
+                .clock(clock)
+                .setMaxReplicationLag(replicationLag, TimeUnit.HOURS)
+                .setMongoDB(mc.getDB())
+                .setClusterId(1)
+                .getNodeStore();
+        mongoDS = (MongoDocumentStore) documentNodeStore.getDocumentStore();
+        nodeStore = documentNodeStore;
+    }
+
+    @After
+    public void clearDB() {
+        MongoUtils.dropCollections(mongoDS.getDBCollection(NODES).getDB());
+    }
+
+    @Test
+    public void testPreferenceConversion() throws Exception{
+        //For cacheAge < replicationLag result should be primary
+        assertEquals(DocumentReadPreference.PRIMARY, mongoDS.getReadPreference(0));
+        assertEquals(DocumentReadPreference.PRIMARY,
+                mongoDS.getReadPreference((int) (replicationLag - 100)));
+
+        //For Integer.MAX_VALUE it should be secondary as caller intends that value is stable
+        assertEquals(DocumentReadPreference.PREFER_SECONDARY,
+                mongoDS.getReadPreference(Integer.MAX_VALUE));
+
+        //For all other cases depends on age
+        assertEquals(DocumentReadPreference.PREFER_SECONDARY_IF_OLD_ENOUGH,
+                mongoDS.getReadPreference(-1));
+        assertEquals(DocumentReadPreference.PREFER_SECONDARY_IF_OLD_ENOUGH,
+                mongoDS.getReadPreference((int) (replicationLag + 100)));
+    }
+
+    @Test
+    public void testMongoReadPreferencesDefault() throws Exception{
+        assertEquals(ReadPreference.primary(),
+                mongoDS.getMongoReadPreference(NODES,"foo", DocumentReadPreference.PRIMARY));
+
+        assertEquals(ReadPreference.primaryPreferred(),
+                mongoDS.getMongoReadPreference(NODES,"foo", DocumentReadPreference.PREFER_PRIMARY));
+
+        //By default Mongo read preference is primary
+        assertEquals(ReadPreference.primary(),
+                mongoDS.getMongoReadPreference(NODES,"foo", DocumentReadPreference.PREFER_SECONDARY));
+
+        //Change the default and assert again
+        mongoDS.getDBCollection(NODES).getDB().setReadPreference(ReadPreference.secondary());
+        assertEquals(ReadPreference.secondary(),
+                mongoDS.getMongoReadPreference(NODES,"foo", DocumentReadPreference.PREFER_SECONDARY));
+
+        //for case where parent age cannot be determined the preference should be primaryPreferred
+        assertEquals(ReadPreference.primaryPreferred(),
+                mongoDS.getMongoReadPreference(NODES,"foo", DocumentReadPreference.PREFER_SECONDARY_IF_OLD_ENOUGH));
+
+        //For collection other than NODES always primary
+        assertEquals(ReadPreference.primary(),
+                mongoDS.getMongoReadPreference(SETTINGS,"foo", DocumentReadPreference.PREFER_SECONDARY_IF_OLD_ENOUGH));
+
+    }
+
+    @Test
+    public void testMongoReadPreferencesWithAge() throws Exception{
+        //Change the default
+        ReadPreference testPref = ReadPreference.secondary();
+        mongoDS.getDBCollection(NODES).getDB().setReadPreference(testPref);
+
+        NodeBuilder b1 = nodeStore.getRoot().builder();
+        b1.child("x").child("y");
+        nodeStore.merge(b1, EmptyHook.INSTANCE, CommitInfo.EMPTY);
+
+        String id = Utils.getIdFromPath("/x/y");
+        mongoDS.invalidateCache(NODES,id);
+
+        //For modifiedTime < replicationLag primary should be preferred
+        assertEquals(ReadPreference.primaryPreferred(),
+                mongoDS.getMongoReadPreference(NODES,id, DocumentReadPreference.PREFER_SECONDARY_IF_OLD_ENOUGH));
+
+        //Going into future to make parent /x old enough
+        clock.waitUntil(Revision.getCurrentTimestamp() + replicationLag);
+
+        //For old modified nodes secondaries should be preferred
+        assertEquals(testPref,
+                mongoDS.getMongoReadPreference(NODES,id, DocumentReadPreference.PREFER_SECONDARY_IF_OLD_ENOUGH));
+    }
+}
