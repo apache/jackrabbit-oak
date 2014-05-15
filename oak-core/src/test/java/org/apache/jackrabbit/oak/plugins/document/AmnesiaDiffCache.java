918aad84327dda91af2a97b940fd169c0e5e4f3e
Marcel Reutegger <mreutegg@apache.org>
2014-05-15 12:18:37 +0000

    OAK-1822: NodeDocument _modified may go back in time
    Additional test showing the diff issue when _modified is set back in time
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1594888 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/AmnesiaDiffCache.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.plugins.document;
+
+import javax.annotation.Nonnull;
+
+/**
+ * A diff cache implementation, which immediately forgets the diff.
+ */
+class AmnesiaDiffCache implements DiffCache {
+
+    static final DiffCache INSTANCE = new AmnesiaDiffCache();
+
+    private AmnesiaDiffCache() {
+        super();
+    }
+
+    @Override
+    public String getChanges(@Nonnull Revision from,
+                             @Nonnull Revision to,
+                             @Nonnull String path) {
+        return null;
+    }
+
+    @Nonnull
+    @Override
+    public Entry newEntry(@Nonnull Revision from, @Nonnull Revision to) {
+        return new Entry() {
+            @Override
+            public void append(@Nonnull String path, @Nonnull String changes) {
+            }
+
+            @Override
+            public void done() {
+            }
+        };
+    }
+}
