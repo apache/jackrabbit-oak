f11b8930056bd6503abd40403bee667fe0cba738
Marcel Reutegger <mreutegg@apache.org>
2014-05-22 16:14:44 +0000

    OAK-1727: Cross foreign cluster revision comparison may be wrong
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1596910 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/RevisionTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/RevisionTest.java
@@ -221,6 +221,35 @@ public class RevisionTest {
         assertTrue(comp.compare(r1c2, r2c1) < 0);
     }
 
+    // OAK-1727
+    @Test
+    public void clusterCompare2() {
+        RevisionComparator comp = new RevisionComparator(1);
+
+        comp.add(Revision.fromString("r3-0-1"), Revision.fromString("r1-1-0"));
+
+        Revision r1 = Revision.fromString("r1-0-2");
+        Revision r2 = Revision.fromString("r4-0-2");
+
+        // cluster sync
+        Revision c1sync = Revision.fromString("r5-0-1");
+        comp.add(c1sync,  Revision.fromString("r2-0-0"));
+        Revision c2sync = Revision.fromString("r4-1-2");
+        comp.add(c2sync,  Revision.fromString("r2-1-0"));
+        Revision c3sync = Revision.fromString("r2-0-3");
+        comp.add(c3sync,  Revision.fromString("r2-1-0"));
+
+        assertTrue(comp.compare(r1, r2) < 0);
+        assertTrue(comp.compare(r2, c2sync) < 0);
+        // same seen-at revision, but clusterId 2 < 3
+        assertTrue(comp.compare(c2sync, c3sync) < 0);
+
+        // this means, c3sync must be after r1 and r2
+        // because: r1 < r2 < c2sync < c3sync
+        assertTrue(comp.compare(r1, c3sync) < 0);
+        assertTrue(comp.compare(r2, c3sync) < 0);
+    }
+
     @Test
     public void revisionSeen() {
         RevisionComparator comp = new RevisionComparator(1);
