cf97ca456ab2d817cfadb307ce2914a46ed48e90
Alexandru Parvulescu <alexparvulescu@apache.org>
2014-06-09 14:57:41 +0000

    OAK-1858 Segment Explorer  - properly close the store on exit  - added option to run compaction, run the diff between 2 node states, and to trace the node store references to a specific file (wip)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1601403 13f79535-47bb-0310-9956-ffa450edef68

--- oak-run/src/main/java/org/apache/jackrabbit/oak/explorer/Explorer.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/explorer/Explorer.java
@@ -21,12 +21,21 @@ package org.apache.jackrabbit.oak.explorer;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
 import javax.swing.UIManager;
@@ -77,9 +86,15 @@ public class Explorer {
         }
     }
 
-    private void createAndShowGUI(String path, FileStore store) {
-        JFrame frame = new JFrame("Explore " + path);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private void createAndShowGUI(final String path, final FileStore store) {
+        final JFrame frame = new JFrame("Explore " + path);
+        frame.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                store.close();
+                System.exit(0);
+            }
+        });
 
         JPanel content = new JPanel(new GridBagLayout());
 
@@ -88,7 +103,7 @@ public class Explorer {
         log.setLineWrap(true);
         log.setEditable(false);
 
-        NodeStoreTree treePanel = new NodeStoreTree(store, log);
+        final NodeStoreTree treePanel = new NodeStoreTree(store, log);
 
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.BOTH;
@@ -99,9 +114,71 @@ public class Explorer {
                 new JScrollPane(treePanel), new JScrollPane(log));
         splitPane.setDividerLocation(0.3);
         content.add(new JScrollPane(splitPane), c);
-
         frame.getContentPane().add(content);
 
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setMargin(new Insets(2, 2, 2, 2));
+
+        JMenuItem menuCompaction = new JMenuItem("Tar Compaction");
+        menuCompaction.setMnemonic(KeyEvent.VK_C);
+        menuCompaction.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ev) {
+                int run = JOptionPane.showConfirmDialog(frame,
+                        "Run compaction on the tar files", "Tar Compaction",
+                        JOptionPane.WARNING_MESSAGE, JOptionPane.YES_NO_OPTION);
+                if (run == JOptionPane.YES_OPTION) {
+                    treePanel.compact();
+                }
+            }
+        });
+
+        JMenuItem menuRefs = new JMenuItem("Tar File Refs");
+        menuRefs.setMnemonic(KeyEvent.VK_R);
+        menuRefs.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ev) {
+                List<String> tarFiles = new ArrayList<String>();
+                for (File f : new File(path).listFiles()) {
+                    if (f.getName().endsWith(".tar")) {
+                        tarFiles.add(f.getName());
+                    }
+                }
+
+                String s = (String) JOptionPane.showInputDialog(frame,
+                        "Choose a tar file", "Tar File References",
+                        JOptionPane.PLAIN_MESSAGE, null, tarFiles.toArray(),
+                        tarFiles.get(0));
+                if (s != null) {
+                    treePanel.printDependenciesToFile(s);
+                    return;
+                }
+            }
+        });
+
+        JMenuItem menuDiff = new JMenuItem("SegmentNodeState diff");
+        menuDiff.setMnemonic(KeyEvent.VK_D);
+        menuDiff.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ev) {
+                String s = (String) JOptionPane.showInputDialog(frame,
+                        "SegmentNodeState diff\nUsage: <recordId> <recordId> [<path>]",
+                        "SegmentNodeState diff", JOptionPane.PLAIN_MESSAGE);
+                if (s != null) {
+                    treePanel.printDiff(s);
+                    return;
+                }
+            }
+        });
+
+        menuBar.add(menuCompaction);
+        menuBar.add(new JSeparator(JSeparator.VERTICAL));
+        menuBar.add(menuRefs);
+        menuBar.add(new JSeparator(JSeparator.VERTICAL));
+        menuBar.add(menuDiff);
+        menuBar.add(new JSeparator(JSeparator.VERTICAL));
+
+        frame.setJMenuBar(menuBar);
         frame.pack();
         frame.setSize(960, 720);
         frame.setLocationRelativeTo(null);
