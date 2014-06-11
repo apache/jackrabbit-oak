9f354827d0c62cc02b8c4878d3a493c5da45b64c
Chetan Mehrotra <chetanm@apache.org>
2014-06-11 08:58:27 +0000

    OAK-1862 - Checkpoints release method
    Implementing release support for DocumentNodeStore
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1601833 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/CheckpointsTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/CheckpointsTest.java
@@ -125,4 +125,26 @@ public class CheckpointsTest {
         assertNull(store.getCheckpoints().getOldestRevisionToKeep());
     }
 
+    @Test
+    public void checkpointRemove() throws Exception{
+        long et1 = 1000, et2 = et1 + 1000;
+        String cp1 = store.checkpoint(et1);
+        Revision r1 = Revision.fromString(cp1);
+
+        //Do some commit to change headRevision
+        NodeBuilder b2 = store.getRoot().builder();
+        b2.child("x");
+        store.merge(b2, EmptyHook.INSTANCE, CommitInfo.EMPTY);
+
+        String cp2 = store.checkpoint(et2);
+        Revision r2 = Revision.fromString(cp2);
+
+        assertEquals(2, store.getCheckpoints().size());
+        assertEquals(r2, store.getCheckpoints().getOldestRevisionToKeep());
+
+        store.release(cp2);
+
+        assertEquals(1, store.getCheckpoints().size());
+        assertEquals(r1, store.getCheckpoints().getOldestRevisionToKeep());
+    }
 }
