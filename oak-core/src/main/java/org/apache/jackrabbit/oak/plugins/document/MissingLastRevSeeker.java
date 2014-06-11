93de8b9c3641d71c3090b0367496db8f4df8ca13
Chetan Mehrotra <chetanm@apache.org>
2014-06-11 06:26:36 +0000

    OAK-1883 - Merged revision 1601814
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1601817 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/MissingLastRevSeeker.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/MissingLastRevSeeker.java
@@ -94,6 +94,7 @@ public class MissingLastRevSeeker {
     public void releaseRecoveryLock(int clusterId){
         UpdateOp update = new UpdateOp(Integer.toString(clusterId), true);
         update.set(ClusterNodeInfo.REV_RECOVERY_LOCK, null);
+        update.set(ClusterNodeInfo.STATE, null);
         store.createOrUpdate(Collection.CLUSTER_NODES, update);
     }
 
