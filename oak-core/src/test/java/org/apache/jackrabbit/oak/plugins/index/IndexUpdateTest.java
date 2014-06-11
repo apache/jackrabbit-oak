d95c70c5af5119ae21b7d46172057060c42de3f3
Alexandru Parvulescu <alexparvulescu@apache.org>
2014-06-11 15:02:12 +0000

    OAK-1874 merged revision 1601922
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1601923 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/IndexUpdateTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/IndexUpdateTest.java
@@ -172,6 +172,39 @@ public class IndexUpdateTest {
         assertEquals(ImmutableSet.of("testRoot"), find(lookup, "foo", "abc"));
     }
 
+    /**
+     * Auto Reindex Test
+     * <ul>
+     * <li>Add some content</li>
+     * <li>Add an index definition without a reindex flag (see OAK-1874)</li>
+     * <li>Search & verify</li>
+     * </ul>
+     */
+    @Test
+    public void testReindexAuto() throws Exception {
+        builder.child("testRoot").setProperty("foo", "abc");
+        NodeState before = builder.getNodeState();
+
+        createIndexDefinition(builder.child(INDEX_DEFINITIONS_NAME),
+                "rootIndex", false, false, ImmutableSet.of("foo"), null);
+
+        NodeState after = builder.getNodeState();
+
+        NodeState indexed = HOOK.processCommit(before, after, CommitInfo.EMPTY);
+
+        // first check that the index content nodes exist
+        NodeState ns = checkPathExists(indexed, INDEX_DEFINITIONS_NAME,
+                "rootIndex");
+        checkPathExists(ns, INDEX_CONTENT_NODE_NAME);
+        PropertyState ps = ns.getProperty(REINDEX_PROPERTY_NAME);
+        assertNotNull(ps);
+        assertFalse(ps.getValue(Type.BOOLEAN));
+
+        // next, lookup
+        PropertyIndexLookup lookup = new PropertyIndexLookup(indexed);
+        assertEquals(ImmutableSet.of("testRoot"), find(lookup, "foo", "abc"));
+    }
+
     @Test
     public void testIndexDefinitions() throws Exception {
         createIndexDefinition(builder.child(INDEX_DEFINITIONS_NAME),
