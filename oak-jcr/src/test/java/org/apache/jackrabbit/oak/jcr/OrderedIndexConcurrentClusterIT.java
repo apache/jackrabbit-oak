6aeb06efcf9d2ddfb0a4b2d24eaaf15857be614d
Marcel Reutegger <mreutegg@apache.org>
2014-06-16 06:54:04 +0000

    OAK-1892: OrderedIndexConcurrentClusterIT takes too long
    Disable test for now.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1602809 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/OrderedIndexConcurrentClusterIT.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/OrderedIndexConcurrentClusterIT.java
@@ -44,6 +44,7 @@ import org.junit.After;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -211,6 +212,7 @@ public class OrderedIndexConcurrentClusterIT {
         }
     }
 
+    @Ignore("OAK-1892")
     @Test
     public void deleteConcurrently() throws Exception {
         final int loop = LOOP;
