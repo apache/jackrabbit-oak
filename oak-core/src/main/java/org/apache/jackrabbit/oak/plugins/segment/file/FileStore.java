bba20585c1bf5318043b1f1c3029e0781090012e
Jukka Zitting <jukka@apache.org>
2014-06-16 05:44:40 +0000

    OAK-1804: TarMK compaction
    Drop the write number -based compaction threshold as it is troublesome
    with large repositories where the size of the repository could exceed
    the number of tar files used by compaction.
    
    Move JVM gc to the point where it's most needed, i.e. just before cleanup.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1602800 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/segment/file/FileStore.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/segment/file/FileStore.java
@@ -131,12 +131,6 @@ public class FileStore implements SegmentStore {
     private final AtomicBoolean compactNeeded = new AtomicBoolean(false);
 
     /**
-     * The number of new files created at which a compaction should be
-     * triggered.
-     */
-    private int compactThreshold = 10;
-
-    /**
      * List of old tar file generations that are waiting to be removed.
      */
     private final LinkedList<File> toBeRemoved = newLinkedList();
@@ -363,6 +357,10 @@ public class FileStore implements SegmentStore {
 
                     if (cleanup) {
                         long start = System.nanoTime();
+
+                        // Suggest to the JVM that now would be a good time
+                        // to clear stale weak references in the SegmentTracker
+                        System.gc();
 
                         Set<UUID> ids = newHashSet();
                         for (SegmentId id : tracker.getReferencedSegmentIds()) {
@@ -591,9 +589,6 @@ public class FileStore implements SegmentStore {
                         directory,
                         String.format(FILE_NAME_FORMAT, writeNumber, "a"));
                 writer = new TarWriter(writeFile);
-                if (writeNumber % compactThreshold == 0) {
-                    compactNeeded.set(true);
-                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -616,7 +611,6 @@ public class FileStore implements SegmentStore {
 
     @Override
     public void gc() {
-        System.gc();
         compactNeeded.set(true);
     }
 
