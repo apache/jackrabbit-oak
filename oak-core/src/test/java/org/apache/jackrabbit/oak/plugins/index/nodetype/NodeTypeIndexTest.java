599b4e61db9715d3e9173211d5cd9a61a8e15909
Thomas Mueller <thomasm@apache.org>
2014-06-23 20:24:54 +0000

    OAK-1902 NodeTypeIndex is not conversative enough about its cost
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1604927 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/nodetype/NodeTypeIndexTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/nodetype/NodeTypeIndexTest.java
@@ -72,6 +72,9 @@ public class NodeTypeIndexTest {
 
         // remove "rep:security" as it interferes with tests
         root.getChildNode("rep:security").remove();
+        
+        // remove "entryCount", so the node type index cost is not fixed
+        root.getChildNode("oak:index").getChildNode("nodetype").removeProperty("entryCount");
 
         addFolder(root, "folder-1");
         addFolder(root, "folder-2");
