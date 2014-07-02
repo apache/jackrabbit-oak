08fcadf2d85d5e29c2b2083548cddfb03bf03026
Julian Reschke <reschke@apache.org>
2014-07-02 12:58:29 +0000

    OAK-1914 - adjust JDBC connection URI for Oracle 12
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1607355 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/DocumentStoreFixture.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/DocumentStoreFixture.java
@@ -39,7 +39,7 @@ public abstract class DocumentStoreFixture {
     public static final DocumentStoreFixture RDB_PG = new RDBFixture("RDB-Postgres", "jdbc:postgresql:oak", "postgres", "geheim");
     public static final DocumentStoreFixture RDB_DB2 = new RDBFixture("RDB-DB2", "jdbc:db2://localhost:50000/OAK", "oak", "geheim");
     public static final DocumentStoreFixture RDB_MYSQL = new RDBFixture("RDB-MySQL", "jdbc:mysql://localhost:3306/oak", "root", "geheim");
-    public static final DocumentStoreFixture RDB_ORACLE = new RDBFixture("RDB-Oracle", "jdbc:oracle:thin:@//localhost:1521/XE", "oak", "geheim");
+    public static final DocumentStoreFixture RDB_ORACLE = new RDBFixture("RDB-Oracle", "jdbc:oracle:thin:@localhost:1521:orcl", "system", "geheim");
     public static final DocumentStoreFixture MONGO = new MongoFixture("mongodb://localhost:27017/oak");
 
     public abstract String getName();
