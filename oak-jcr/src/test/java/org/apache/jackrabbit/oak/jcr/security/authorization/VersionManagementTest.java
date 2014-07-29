7f8262982f4db7d8446e8db72678222ebc39fb27
Michael Dürig <mduerig@apache.org>
2014-07-29 16:04:43 +0000

    OAK-1998: Accessible tree below a non-accessible parent are HiddenTree Only return HiddenTree instances for actually hidden trees, not for inaccessible trees
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1614397 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/authorization/VersionManagementTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/authorization/VersionManagementTest.java
@@ -30,7 +30,6 @@ import javax.jcr.version.VersionManager;
 
 import org.apache.jackrabbit.test.NotExecutableException;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -181,39 +180,6 @@ public class VersionManagementTest extends AbstractEvaluationTest {
             // success
         } finally {
             // revert privilege modification (manually remove the ACE added)
-            for (AccessControlEntry entry : acl.getAccessControlEntries()) {
-                if (entry.getPrincipal().equals(testUser.getPrincipal())) {
-                    acl.removeAccessControlEntry(entry);
-                }
-            }
-            acMgr.setPolicy(SYSTEM, acl);
-            superuser.save();
-        }
-    }
-
-    /**
-     * @since oak
-     */
-    @Test
-    public void testAccessVersionContentWithoutStoreAccess() throws Exception {
-        Node n = createVersionableNode(superuser.getNode(path));
-        Version v = n.checkin();
-        VersionHistory vh = n.getVersionHistory();
-        n.checkout();
-        Version v2 = n.checkin();
-        n.checkout();
-
-        testSession.refresh(false);
-        assertFalse(testAcMgr.hasPrivileges(n.getPath(), versionPrivileges));
-        AccessControlList acl = deny(SYSTEM, privilegesFromName(Privilege.JCR_READ));
-
-        try {
-            // version information must still be accessible
-            assertTrue(testSession.nodeExists(v.getPath()));
-            assertTrue(testSession.nodeExists(v2.getPath()));
-            assertTrue(testSession.nodeExists(vh.getPath()));
-
-        } finally {
             for (AccessControlEntry entry : acl.getAccessControlEntries()) {
                 if (entry.getPrincipal().equals(testUser.getPrincipal())) {
                     acl.removeAccessControlEntry(entry);
