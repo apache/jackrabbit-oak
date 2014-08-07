f041313c3cd5efca2f8e01db0778546071b81d76
Alexandru Parvulescu <alexparvulescu@apache.org>
2014-08-07 10:27:20 +0000

    OAK-2017 Add checkpoint management to oak-run  - added check that the provided repo path is a valid store for debug, compact and checkpoints
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1616463 13f79535-47bb-0310-9956-ffa450edef68

--- oak-run/src/main/java/org/apache/jackrabbit/oak/run/Main.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/run/Main.java
@@ -297,6 +297,9 @@ public class Main {
         if (args.length != 1) {
             System.err.println("usage: compact <path>");
             System.exit(1);
+        } else if (!isValidFileStore(args[0])) {
+            System.err.println("Invalid FileStore directory " + args[0]);
+            System.exit(1);
         } else {
             File directory = new File(args[0]);
             System.out.println("Compacting " + directory);
@@ -326,6 +329,10 @@ public class Main {
         if (args.length == 0) {
             System.err
                     .println("usage: checkpoints <path> [list|rm-all|rm <checkpoint>]");
+            System.exit(1);
+        }
+        if (!isValidFileStore(args[0])) {
+            System.err.println("Invalid FileStore directory " + args[0]);
             System.exit(1);
         }
 
@@ -403,6 +410,9 @@ public class Main {
     private static void debug(String[] args) throws IOException {
         if (args.length == 0) {
             System.err.println("usage: debug <path> [id...]");
+            System.exit(1);
+        } else if (!isValidFileStore(args[0])) {
+            System.err.println("Invalid FileStore directory " + args[0]);
             System.exit(1);
         } else {
             // TODO: enable debug information for other node store implementations
@@ -524,6 +534,25 @@ public class Main {
                 store.close();
             }
         }
+    }
+
+    /**
+     * Checks if the provided directory is a valid FileStore
+     * 
+     * @return true if the provided directory is a valid FileStore
+     */
+    private static boolean isValidFileStore(String path) {
+        File store = new File(path);
+        if (store == null || !store.isDirectory()) {
+            return false;
+        }
+        // for now the only check is the existence of the journal file
+        for (String f : store.list()) {
+            if ("journal.log".equals(f)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static void upgrade(String[] args) throws Exception {
