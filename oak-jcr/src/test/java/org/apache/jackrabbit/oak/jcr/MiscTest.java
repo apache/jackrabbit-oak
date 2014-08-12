06b2767d0cef2c3ff8893067563ee917af4738cd
Tobias Bocanegra <tripod@apache.org>
2014-08-12 15:44:04 +0000

    OAK-2024 Add test case
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1617510 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/MiscTest.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.jcr;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.Property;
+import javax.jcr.PropertyIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.apache.jackrabbit.JcrConstants;
+import org.apache.jackrabbit.commons.jackrabbit.authorization.AccessControlUtils;
+import org.apache.jackrabbit.test.AbstractJCRTest;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class MiscTest extends AbstractJCRTest {
+
+    @Ignore("OAK-2024")
+    @Test
+    public void testTraverseIndex() throws Exception {
+        AccessControlUtils.denyAllToEveryone(superuser, "/oak:index");
+        superuser.save();
+        Node index = superuser.getNode("/oak:index");
+        traverse(index);
+    }
+
+
+    private void traverse(Node node) throws RepositoryException {
+        System.out.println(node.getPath());
+        PropertyIterator iter = node.getProperties();
+        while (iter.hasNext()) {
+            Property p = iter.nextProperty();
+            System.out.println(p.getPath());
+            p.getDefinition();
+        }
+        NodeIterator niter = node.getNodes();
+        while (niter.hasNext()) {
+            traverse(niter.nextNode());
+        }
+    }
+}
