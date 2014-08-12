004db804f06b91c5e361c5abd718092dd86a4e3a
Thomas Mueller <thomasm@apache.org>
2014-08-12 07:41:19 +0000

    OAK-2021 XPath queries with certain combinations of "or" conditions don't use an index OAK-2022 XPath queries with "order by" are never converted to "union"
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1617430 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/query/SQL2ParserTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/query/SQL2ParserTest.java
@@ -59,7 +59,7 @@ public class SQL2ParserTest {
     public void testUnwrappedOr() throws ParseException {
         String q = new XPathToSQL2Converter()
                 .convert("/jcr:root/home//test/* [@type='t1' or @type='t2' or @type='t3']");
-        String token = "and (b.[type] = 't1' or b.[type] = 't2' or b.[type] = 't3')";
+        String token = "and b.[type] in('t1', 't2', 't3')";
         assertTrue(q.contains(token));
     }
     
