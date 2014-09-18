bc5faa763f50a42377b9c1d076be23d247b62a33
Manfred Baedke <baedke@apache.org>
2014-09-18 12:52:30 +0000

    OAK-1915: TarMK failover 2.0
    Added metatype descriptor.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1625963 13f79535-47bb-0310-9956-ffa450edef68

--- oak-tarmk-failover/src/main/java/org/apache/jackrabbit/oak/plugins/segment/failover/store/FailoverStoreService.java
+++ oak-tarmk-failover/src/main/java/org/apache/jackrabbit/oak/plugins/segment/failover/store/FailoverStoreService.java
@@ -45,8 +45,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-@Property(name = "org.apache.sling.installer.configuration.persist", label="Distribute config", description = "Should be always disabled to avoid storing the configuration in the repository", boolValue = false)
-@Component(policy = ConfigurationPolicy.REQUIRE)
+@Property(name = "org.apache.sling.installer.configuration.persist", label="Persist configuration", description = "Must be always disabled to avoid storing the configuration in the repository", boolValue = false)
+@Component(metatype = true, policy = ConfigurationPolicy.REQUIRE)
 public class FailoverStoreService {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
@@ -55,31 +55,30 @@ public class FailoverStoreService {
     private static final String MODE_SLAVE = "slave";
 
     public static final String MODE_DEFAULT = MODE_MASTER;
-    @Property(name = "Mode", description = "TarMK Cold Standby mode (master or slave)",
-            options = {
+    @Property(options = {
             @PropertyOption(name = MODE_MASTER, value = MODE_MASTER),
             @PropertyOption(name = MODE_SLAVE, value = MODE_SLAVE) },
             value = MODE_DEFAULT)
     public static final String MODE = "mode";
 
     public static final int PORT_DEFAULT = 8023;
-    @Property(name = "Port", description = "TCP/IP port to use", intValue = PORT_DEFAULT)
+    @Property(intValue = PORT_DEFAULT)
     public static final String PORT = "port";
 
     public static final String MASTER_HOST_DEFAULT = "127.0.0.1";
-    @Property(name = "Master Host", description = "Master host (slave mode only)", value = MASTER_HOST_DEFAULT)
+    @Property(value = MASTER_HOST_DEFAULT)
     public static final String MASTER_HOST = "master.host";
 
     public static final int INTERVAL_DEFAULT = 5;
-    @Property(name = "Sync interval (seconds)", description = "Sync interval in seconds (slave mode only)", intValue = INTERVAL_DEFAULT)
+    @Property(intValue = INTERVAL_DEFAULT)
     public static final String INTERVAL = "interval";
 
-    public static final String ALLOWED_CLIENT_IP_RANGES_DEFAULT = null;
-    @Property(name = "Allowed IP-Ranges", description = "Accept incoming requests for these host names and IP-ranges only (master mode only)")
+    public static final String[] ALLOWED_CLIENT_IP_RANGES_DEFAULT = new String[] {};
+    @Property(cardinality = Integer.MAX_VALUE)
     public static final String ALLOWED_CLIENT_IP_RANGES = "master.allowed-client-ip-ranges";
 
     public static final boolean SECURE_DEFAULT = false;
-    @Property(name = "Secure", description = "Use secure connections", boolValue = SECURE_DEFAULT)
+    @Property(boolValue = SECURE_DEFAULT)
     public static final String SECURE = "secure";
 
     @Reference(policy = STATIC, policyOption = GREEDY)
@@ -128,12 +127,11 @@ public class FailoverStoreService {
     private void bootstrapMaster(ComponentContext context) throws CertificateException, SSLException {
         Dictionary<?, ?> props = context.getProperties();
         int port = PropertiesUtil.toInteger(props.get(PORT), PORT_DEFAULT);
-        String ipRanges = PropertiesUtil.toString(props.get(ALLOWED_CLIENT_IP_RANGES), ALLOWED_CLIENT_IP_RANGES_DEFAULT);
-        String[] ranges = ipRanges == null ? null : ipRanges.split(",");
+        String[] ranges = PropertiesUtil.toStringArray(props.get(ALLOWED_CLIENT_IP_RANGES), ALLOWED_CLIENT_IP_RANGES_DEFAULT);
         boolean secure = PropertiesUtil.toBoolean(props.get(SECURE), SECURE_DEFAULT);
         master = new FailoverServer(port, segmentStore, ranges, secure);
         master.start();
-        log.info("started failover master on port {} with allowed ip ranges {}.", port, ipRanges);
+        log.info("started failover master on port {} with allowed ip ranges {}.", port, ranges);
     }
 
     private void bootstrapSlave(ComponentContext context) throws SSLException {
