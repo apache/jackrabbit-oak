c7669f314119e22653dba6f01a4dc2ef87863242
Chetan Mehrotra <chetanm@apache.org>
2014-09-23 15:18:58 +0000

    OAK-2117 - Reindex removes all nodes under index defenition node
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1627052 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/IndexUpdate.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/IndexUpdate.java
@@ -45,6 +45,7 @@ import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.spi.commit.Editor;
 import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
+import org.apache.jackrabbit.oak.spi.state.NodeStateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -170,7 +171,9 @@ public class IndexUpdate implements Editor {
                         // as we don't know the index content node name
                         // beforehand, we'll remove all child nodes
                         for (String rm : definition.getChildNodeNames()) {
-                            definition.getChildNode(rm).remove();
+                            if (NodeStateUtils.isHidden(rm)) {
+                                definition.getChildNode(rm).remove();
+                            }
                         }
                         reindex.put(concat(getPath(), INDEX_DEFINITIONS_NAME, name), editor);
                     }
