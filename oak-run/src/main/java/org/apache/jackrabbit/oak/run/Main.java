1a610bdde16737993ace941fd3fd0a03a0150360
Alexandru Parvulescu <alexparvulescu@apache.org>
2014-10-17 13:17:48 +0000

    OAK-1915 TarMK Cold Standby  - removed fragments introduced by the merge
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1632568 13f79535-47bb-0310-9956-ffa450edef68

--- oak-run/src/main/java/org/apache/jackrabbit/oak/run/Main.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/run/Main.java
@@ -56,7 +56,6 @@ import org.apache.jackrabbit.oak.http.OakServlet;
 import org.apache.jackrabbit.oak.jcr.Jcr;
 import org.apache.jackrabbit.oak.plugins.backup.FileStoreBackup;
 import org.apache.jackrabbit.oak.plugins.backup.FileStoreRestore;
-import org.apache.jackrabbit.oak.plugins.document.Collection;
 import org.apache.jackrabbit.oak.plugins.document.DocumentMK;
 import org.apache.jackrabbit.oak.plugins.document.DocumentNodeStore;
 import org.apache.jackrabbit.oak.plugins.segment.RecordId;
@@ -323,83 +322,6 @@ public class Main {
                 failoverServer.close();
             }
         }
-    }
-
-    public static NodeStore bootstrapNodeStore(String[] args, Closer closer,
-            String h) throws IOException {
-        //TODO add support for other NodeStore flags
-        OptionParser parser = new OptionParser();
-        OptionSpec<Integer> clusterId = parser
-                .accepts("clusterId", "MongoMK clusterId").withRequiredArg()
-                .ofType(Integer.class).defaultsTo(0);
-        OptionSpec<?> help = parser.acceptsAll(asList("h", "?", "help"),
-                "show help").forHelp();
-        OptionSpec<String> nonOption = parser
-                .nonOptions(h);
-
-        OptionSet options = parser.parse(args);
-        List<String> nonOptions = nonOption.values(options);
-
-        if (options.has(help)) {
-            parser.printHelpOn(System.out);
-            System.exit(0);
-        }
-
-        if (nonOptions.isEmpty()) {
-            parser.printHelpOn(System.err);
-            System.exit(1);
-        }
-
-        String src = nonOptions.get(0);
-        if (src.startsWith(MongoURI.MONGODB_PREFIX)) {
-            MongoClientURI uri = new MongoClientURI(src);
-            if (uri.getDatabase() == null) {
-                System.err.println("Database missing in MongoDB URI: "
-                        + uri.getURI());
-                System.exit(1);
-            }
-            MongoConnection mongo = new MongoConnection(uri.getURI());
-            closer.register(asCloseable(mongo));
-            DocumentNodeStore store = new DocumentMK.Builder()
-                    .setMongoDB(mongo.getDB())
-                    .setClusterId(clusterId.value(options)).getNodeStore();
-            closer.register(asCloseable(store));
-            return store;
-        } else {
-            FileStore fs = new FileStore(new File(src), 256, false);
-            closer.register(asCloseable(fs));
-            return new SegmentNodeStore(fs);
-        }
-    }
-
-    private static Closeable asCloseable(final FileStore fs) {
-        return new Closeable() {
-
-            @Override
-            public void close() throws IOException {
-                fs.close();
-            }
-        };
-    }
-
-    private static Closeable asCloseable(final DocumentNodeStore dns) {
-        return new Closeable() {
-
-            @Override
-            public void close() throws IOException {
-                dns.dispose();
-            }
-        };
-    }
-
-    private static Closeable asCloseable(final MongoConnection con) {
-        return new Closeable() {
-
-            @Override
-            public void close() throws IOException {
-                con.close();
-            }
-        };
     }
 
     private static void compact(String[] args) throws IOException {
