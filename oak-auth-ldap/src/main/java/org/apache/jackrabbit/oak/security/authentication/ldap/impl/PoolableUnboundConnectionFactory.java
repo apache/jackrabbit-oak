8108196a626734d1d26ad9ab9ba99a6cf0533dc6
Tobias Bocanegra <tripod@apache.org>
2014-10-21 17:16:59 +0000

    OAK-2213 The unbound connection pool does not verify if the connection is still alive
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1633415 13f79535-47bb-0310-9956-ffa450edef68

--- oak-auth-ldap/src/main/java/org/apache/jackrabbit/oak/security/authentication/ldap/impl/PoolableUnboundConnectionFactory.java
+++ oak-auth-ldap/src/main/java/org/apache/jackrabbit/oak/security/authentication/ldap/impl/PoolableUnboundConnectionFactory.java
@@ -19,16 +19,25 @@ package org.apache.jackrabbit.oak.security.authentication.ldap.impl;
 
 
 import org.apache.commons.pool.PoolableObjectFactory;
+import org.apache.directory.api.ldap.model.constants.SchemaConstants;
 import org.apache.directory.api.ldap.model.exception.LdapException;
+import org.apache.directory.api.ldap.model.name.Dn;
 import org.apache.directory.ldap.client.api.LdapConnection;
 import org.apache.directory.ldap.client.api.LdapConnectionConfig;
 import org.apache.directory.ldap.client.api.LdapNetworkConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
  * A factory for creating unbound LdapConnection objects managed by LdapConnectionPool.
  */
 public class PoolableUnboundConnectionFactory implements PoolableObjectFactory<LdapConnection> {
+
+    /**
+     * default logger
+     */
+    private static final Logger log = LoggerFactory.getLogger(PoolableUnboundConnectionFactory.class);
 
     /**
      * configuration object for the connection
@@ -49,6 +58,7 @@ public class PoolableUnboundConnectionFactory implements PoolableObjectFactory<L
      * {@inheritDoc}
      */
     public void activateObject(LdapConnection connection) throws Exception {
+        log.debug("activate connection: {}", connection);
     }
 
 
@@ -56,6 +66,7 @@ public class PoolableUnboundConnectionFactory implements PoolableObjectFactory<L
      * {@inheritDoc}
      */
     public void destroyObject(LdapConnection connection) throws Exception {
+        log.debug("destroy connection: {}", connection);
         connection.close();
     }
 
@@ -68,6 +79,7 @@ public class PoolableUnboundConnectionFactory implements PoolableObjectFactory<L
                 ? new TlsGuardingConnection(config)
                 : new LdapNetworkConnection(config);
         connection.connect();
+        log.debug("creating new connection: {}", connection);
         return connection;
     }
 
@@ -76,6 +88,7 @@ public class PoolableUnboundConnectionFactory implements PoolableObjectFactory<L
      * {@inheritDoc}
      */
     public void passivateObject(LdapConnection connection) throws Exception {
+        log.debug("passivate connection: {}", connection);
     }
 
 
@@ -83,7 +96,16 @@ public class PoolableUnboundConnectionFactory implements PoolableObjectFactory<L
      * {@inheritDoc}
      */
     public boolean validateObject(LdapConnection connection) {
-        return connection.isConnected();
+        boolean valid = false;
+        if (connection.isConnected()) {
+            try {
+                valid = connection.lookup(Dn.ROOT_DSE, SchemaConstants.NO_ATTRIBUTE) != null;
+            } catch (LdapException le) {
+                log.debug("error during connection validation: {}", le.toString());
+            }
+        }
+        log.debug("validating connection {}: {}", connection, valid);
+        return valid;
     }
 
     /**
