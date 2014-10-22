ed52a852c63f318ab05078295943945c2c157170
Alexandru Parvulescu <alexparvulescu@apache.org>
2014-10-22 19:35:18 +0000

    OAK-2227 Increase package export on org.apache.jackrabbit.oak.plugins.segment  - merged r1633687
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1633691 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/segment/file/package-info.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/segment/file/package-info.java
@@ -14,9 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@Version("1.1.0")
+@Version("1.0.0")
 @Export(optional = "provide:=true")
-package org.apache.jackrabbit.oak.plugins.segment;
+package org.apache.jackrabbit.oak.plugins.segment.file;
 
 import aQute.bnd.annotation.Export;
 import aQute.bnd.annotation.Version;
