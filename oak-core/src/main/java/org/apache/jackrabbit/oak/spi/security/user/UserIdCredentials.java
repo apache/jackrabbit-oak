24c19d6de79b1bf9bdae338ce8439172d018a37a
Angela Schreiber <angela@apache.org>
2014-10-28 12:02:57 +0000

    OAK-2233 : Add UserIdCredentials to Oak user management extension
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1634853 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/security/user/UserIdCredentials.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.spi.security.user;
+
+import javax.annotation.Nonnull;
+import javax.jcr.Credentials;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Credentials implementation that only contains a {@code userId} but no password.
+ * It can be used for {@link org.apache.jackrabbit.api.security.user.User#getCredentials()},
+ * where the corresponding user doesn't have a password set.
+ */
+public final class UserIdCredentials implements Credentials {
+
+    private final String userId;
+
+    public UserIdCredentials(@Nonnull String userId) {
+        this.userId = userId;
+    }
+
+    @Nonnull
+    public String getUserId() {
+        return userId;
+    }
+
+}
