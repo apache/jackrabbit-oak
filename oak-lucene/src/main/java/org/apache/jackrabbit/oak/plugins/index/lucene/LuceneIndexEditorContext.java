29d3d8f1384f78e35039c8cb8503db67ae37496c
Chetan Mehrotra <chetanm@apache.org>
2014-10-28 12:53:10 +0000

    OAK-2235 - Lucene index not created if no node is indexed
    By default IndexWriter is lazily created but if logic detects its a reindex mode then it would initialize write and that would create an empty index dir
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1634864 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexEditorContext.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexEditorContext.java
@@ -101,6 +101,8 @@ public class LuceneIndexEditorContext {
 
     private final IndexUpdateCallback updateCallback;
 
+    private boolean reindex;
+
     LuceneIndexEditorContext(NodeBuilder definition, Analyzer analyzer, IndexUpdateCallback updateCallback) {
         this.definitionBuilder = definition;
         this.definition = new IndexDefinition(definitionBuilder);
@@ -132,6 +134,14 @@ public class LuceneIndexEditorContext {
      * close writer if it's not null
      */
     void closeWriter() throws IOException {
+        //If reindex or fresh index and write is null on close
+        //it indicates that the index is empty. In such a case trigger
+        //creation of write such that an empty Lucene index state is persisted
+        //in directory
+        if (reindex && writer == null){
+            getWriter();
+        }
+
         if (writer != null) {
             writer.close();
 
@@ -142,6 +152,10 @@ public class LuceneIndexEditorContext {
             status.setProperty("lastUpdated", ISO8601.format(Calendar.getInstance()), Type.DATE);
             status.setProperty("indexedNodes",indexedNodes);
         }
+    }
+
+    public void enableReindexMode(){
+        reindex = true;
     }
 
     public long incIndexedNodes() {
