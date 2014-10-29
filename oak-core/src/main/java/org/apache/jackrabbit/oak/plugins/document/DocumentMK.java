ceaf1a41c0cc7c518dd5043efcc5a3fbf873c715
Thomas Mueller <thomasm@apache.org>
2014-10-29 15:35:03 +0000

    OAK-2191 Persistent cache for the DocumentNodeStore (OSGi config option)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1635178 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentMK.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentMK.java
@@ -66,7 +66,7 @@ public class DocumentMK implements MicroKernel {
     /**
      * The path where the persistent cache is stored.
      */
-    static final String PERSISTENT_CACHE = 
+    static final String DEFAULT_PERSISTENT_CACHE_URI = 
             System.getProperty("oak.documentMK.persCache");
 
     /**
@@ -508,6 +508,7 @@ public class DocumentMK implements MicroKernel {
         private boolean disableBranches;
         private Clock clock = Clock.SIMPLE;
         private Executor executor;
+        private String persistentCacheURI = DEFAULT_PERSISTENT_CACHE_URI;
         private PersistentCache persistentCache;
 
         public Builder() {
@@ -565,6 +566,16 @@ public class DocumentMK implements MicroKernel {
             if(this.blobStore == null) {
                 this.blobStore = new RDBBlobStore(ds);
             }
+            return this;
+        }
+        
+        /**
+         * Sets the persistent cache option.
+         *
+         * @return this
+         */
+        public Builder setPersistentCache(String persistentCache) {
+            this.persistentCacheURI = persistentCache;
             return this;
         }
 
@@ -846,12 +857,12 @@ public class DocumentMK implements MicroKernel {
         }
         
         private PersistentCache getPersistentCache() {
-            if (PERSISTENT_CACHE == null) {
+            if (persistentCacheURI == null) {
                 return null;
             }
             if (persistentCache == null) {
                 try {
-                    persistentCache = new PersistentCache(PERSISTENT_CACHE);
+                    persistentCache = new PersistentCache(persistentCacheURI);
                 } catch (Throwable e) {
                     LOG.warn("Persistent cache not available; please disable the configuration", e);
                     throw new IllegalArgumentException(e);
@@ -862,7 +873,7 @@ public class DocumentMK implements MicroKernel {
         
         private <K extends CacheValue, V extends CacheValue> Cache<K, V> buildCache(
                 long maxWeight) {
-            if (LIRS_CACHE || PERSISTENT_CACHE != null) {
+            if (LIRS_CACHE || persistentCacheURI != null) {
                 return CacheLIRS.newBuilder().
                         weigher(weigher).
                         averageWeight(2000).
