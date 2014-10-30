284d7dd046e994582d5021b9f90566ac940d2e58
Angela Schreiber <angela@apache.org>
2014-10-30 10:54:37 +0000

    OAK-2245 : UserImporter should always set the rep:authorizableID
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1635464 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/user/UserImportPwExpiryTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/user/UserImportPwExpiryTest.java
@@ -37,7 +37,8 @@ import static org.junit.Assert.assertTrue;
  * Testing user import with default {@link org.apache.jackrabbit.oak.spi.xml.ImportBehavior}
  * and pw-expiry content
  *
- * @see OAK-1922
+ * @see <a href="https://issues.apache.org/jira/browse/OAK-1922">OAK-1922</a>
+ * @see <a href="https://issues.apache.org/jira/browse/OAK-1943">OAK-1943</a>
  */
 public class UserImportPwExpiryTest extends AbstractImportTest {
 
@@ -88,6 +89,40 @@ public class UserImportPwExpiryTest extends AbstractImportTest {
 
         doImport(USERPATH, xml);
 
+        Authorizable authorizable = userMgr.getAuthorizable("x");
+        Node userNode = adminSession.getNode(authorizable.getPath());
+        assertTrue(userNode.hasNode(UserConstants.REP_PWD));
+        Node pwdNode = userNode.getNode(UserConstants.REP_PWD);
+        assertTrue(pwdNode.getDefinition().isProtected());
+        assertTrue(pwdNode.hasProperty(UserConstants.REP_PASSWORD_LAST_MODIFIED));
+        assertTrue(pwdNode.getProperty(UserConstants.REP_PASSWORD_LAST_MODIFIED).getDefinition().isProtected());
+    }
+
+    /**
+     * @since Oak 1.1
+     */
+    @Test
+    public void testImportUserCreatesPasswordLastModified2() throws Exception {
+        // import user without rep:pwd child node defined
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<sv:node sv:name=\"x\" xmlns:mix=\"http://www.jcp.org/jcr/mix/1.0\" xmlns:nt=\"http://www.jcp.org/jcr/nt/1.0\" xmlns:fn_old=\"http://www.w3.org/2004/10/xpath-functions\" xmlns:fn=\"http://www.w3.org/2005/xpath-functions\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" xmlns:sv=\"http://www.jcp.org/jcr/sv/1.0\" xmlns:rep=\"internal\" xmlns:jcr=\"http://www.jcp.org/jcr/1.0\">" +
+                "   <sv:property sv:name=\"jcr:primaryType\" sv:type=\"Name\">" +
+                "      <sv:value>rep:User</sv:value>" +
+                "   </sv:property>" +
+                "   <sv:property sv:name=\"jcr:uuid\" sv:type=\"String\">" +
+                "      <sv:value>9dd4e461-268c-3034-b5c8-564e155c67a6</sv:value>" +
+                "   </sv:property>" +
+                "   <sv:property sv:name=\"rep:password\" sv:type=\"String\">" +
+                "      <sv:value>pw</sv:value>" +
+                "   </sv:property>" +
+                "   <sv:property sv:name=\"rep:principalName\" sv:type=\"String\">" +
+                "      <sv:value>xPrincipal</sv:value>" +
+                "   </sv:property>" +
+                "</sv:node>";
+
+        doImport(USERPATH, xml);
+
+        // verify that the pwd node has still been created
         Authorizable authorizable = userMgr.getAuthorizable("x");
         Node userNode = adminSession.getNode(authorizable.getPath());
         assertTrue(userNode.hasNode(UserConstants.REP_PWD));
