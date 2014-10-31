8420d33da99b7e7d9615566cc1727e241feeb0f5
Chetan Mehrotra <chetanm@apache.org>
2014-10-31 06:11:43 +0000

    OAK-2005 - Use separate Lucene index for performing property related queries
    Using the misspelled name as rev 1613037 is yet not merged to branch
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1635680 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/LucenePropertyIndexTest.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/LucenePropertyIndexTest.java
@@ -71,7 +71,7 @@ public class LucenePropertyIndexTest extends AbstractQueryTest {
 
     @Override
     protected void createTestIndexNode() throws Exception {
-        setTraversalEnabled(false);
+        setTravesalEnabled(false);
     }
 
     @Override
