1f85a926224ee7804cd71dfeebdc61fa2d4da907
Michael Duerig <mduerig@apache.org>
2014-11-10 13:45:49 +0000

    OAK-2031: MemoryNodeBuilder#set only works on root builders - merged r1621124
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1637854 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/memory/MemoryNodeBuilder.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/memory/MemoryNodeBuilder.java
@@ -16,6 +16,12 @@
  */
 package org.apache.jackrabbit.oak.plugins.memory;
 
+import static com.google.common.base.Objects.toStringHelper;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+import static org.apache.jackrabbit.oak.plugins.memory.EmptyNodeState.EMPTY_NODE;
+import static org.apache.jackrabbit.oak.spi.state.AbstractNodeState.checkValidName;
+
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -24,7 +30,6 @@ import javax.annotation.Nonnull;
 
 import com.google.common.base.Objects;
 import com.google.common.io.ByteStreams;
-
 import org.apache.jackrabbit.oak.api.Blob;
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.api.Type;
@@ -33,12 +38,6 @@ import org.apache.jackrabbit.oak.spi.state.EqualsDiff;
 import org.apache.jackrabbit.oak.spi.state.MoveDetector;
 import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
-
-import static com.google.common.base.Objects.toStringHelper;
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Preconditions.checkState;
-import static org.apache.jackrabbit.oak.plugins.memory.EmptyNodeState.EMPTY_NODE;
-import static org.apache.jackrabbit.oak.spi.state.AbstractNodeState.checkValidName;
 
 /**
  * In-memory node state builder.
@@ -236,12 +235,15 @@ public class MemoryNodeBuilder implements NodeBuilder {
      * Replaces the current state of this builder with the given node state.
      * The base state remains unchanged.
      *
-     * @param newHead new head state
+     * @param newState new state
      */
-    protected void set(NodeState newHead) {
-        checkState(parent == null);
-        // updating the base revision forces all sub-builders to refresh
-        baseRevision = rootHead.setState(newHead);
+    protected void set(NodeState newState) {
+        if (parent == null) {
+            // updating the base revision forces all sub-builders to refresh
+            baseRevision = rootHead.setState(newState);
+        } else {
+            parent.setChildNode(name, newState);
+        }
     }
 
     //--------------------------------------------------------< NodeBuilder >---
