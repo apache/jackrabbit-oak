7455b4c42236eb238f06c95d0d7fc28a98cc0204
Michael Dürig <mduerig@apache.org>
2014-11-10 14:17:38 +0000

    OAK-1916: NodeStoreKernel doesn't handle array properties correctly - merged r1606079
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1637872 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/kernel/NodeStoreKernel.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/kernel/NodeStoreKernel.java
@@ -30,6 +30,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.zip.CheckedInputStream;
+import java.util.zip.Checksum;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
