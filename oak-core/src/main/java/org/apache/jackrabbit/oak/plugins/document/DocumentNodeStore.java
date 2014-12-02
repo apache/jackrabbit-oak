4b37764386fc1ee27ba398b6800a94a814597e9f
Marcel Reutegger <mreutegg@apache.org>
2014-12-02 08:28:20 +0000

    OAK-2311: Released checkpoint can still be retrieved
    - Fix for DocumentNodeStore
    - Removed MONGO_MK fixture because the MicroKernel does not have methods for checkpoint release and info. 
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1642816 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStore.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStore.java
@@ -1383,7 +1383,12 @@ public final class DocumentNodeStore
     @CheckForNull
     @Override
     public NodeState retrieve(@Nonnull String checkpoint) {
-        return getRoot(Revision.fromString(checkpoint));
+        Revision r = Revision.fromString(checkpoint);
+        if (checkpoints.getCheckpoints().containsKey(r)) {
+            return getRoot(r);
+        } else {
+            return null;
+        }
     }
 
     @Override
