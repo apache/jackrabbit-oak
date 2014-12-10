d0f6715d6749c07a5fd339dfee9afd2b7d5cb606
Marcel Reutegger <mreutegg@apache.org>
2014-12-10 13:45:27 +0000

    OAK-2336: NodeDocument.getNodeAtRevision() may read too many revisions
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1644407 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/NodeDocument.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/NodeDocument.java
@@ -775,7 +775,7 @@ public final class NodeDocument extends Document implements CachedNodeDocument{
             // check if there may be more recent values in a previous document
             if (value != null && !getPreviousRanges().isEmpty()) {
                 Revision newest = getLocalMap(key).firstKey();
-                if (!value.revision.equals(newest)) {
+                if (isRevisionNewer(nodeStore, newest, value.revision)) {
                     // not reading the most recent value, we may need to
                     // consider previous documents as well
                     Revision newestPrev = getPreviousRanges().firstKey();
