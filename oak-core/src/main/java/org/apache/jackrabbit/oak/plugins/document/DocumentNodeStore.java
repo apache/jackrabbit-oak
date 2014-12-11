105bd1c8043d38fce9e3aa35fc3f7d6fb3465d9c
Marcel Reutegger <mreutegg@apache.org>
2014-12-11 14:29:01 +0000

    OAK-2345: Diff reads too many nodes
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1644650 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStore.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStore.java
@@ -1532,6 +1532,13 @@ public final class DocumentNodeStore
             Revision last = lastKnownRevision.get(machineId);
             if (last == null || r.compareRevisionTime(last) > 0) {
                 lastKnownRevision.put(machineId, r);
+                // OAK-2345
+                // only consider as external change if
+                // - the revision changed for the machineId
+                // or
+                // - the revision is within the time frame we remember revisions
+                if (last != null
+                        || r.getTimestamp() > revisionPurgeMillis())
                 externalChanges.put(r, otherSeen);
             }
         }
@@ -1562,7 +1569,17 @@ public final class DocumentNodeStore
                 backgroundOperationLock.writeLock().unlock();
             }
         }
-        revisionComparator.purge(Revision.getCurrentTimestamp() - REMEMBER_REVISION_ORDER_MILLIS);
+        revisionComparator.purge(revisionPurgeMillis());
+    }
+
+    /**
+     * Returns the time in milliseconds when revisions can be purged from the
+     * revision comparator.
+     *
+     * @return time in milliseconds.
+     */
+    private static long revisionPurgeMillis() {
+        return Revision.getCurrentTimestamp() - REMEMBER_REVISION_ORDER_MILLIS;
     }
 
     private void backgroundSplit() {
