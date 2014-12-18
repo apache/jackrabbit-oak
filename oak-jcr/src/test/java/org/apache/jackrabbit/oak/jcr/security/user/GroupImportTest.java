7cbc00b076af2ee67d4d572ebf7edd77ce1fe2d6
Tobias Bocanegra <tripod@apache.org>
2014-12-18 02:02:48 +0000

    OAK-2367 Importing a group with random node name and members fails.
    - adding test case
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1646372 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/user/GroupImportTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/security/user/GroupImportTest.java
@@ -29,6 +29,7 @@ import org.apache.jackrabbit.api.security.user.Authorizable;
 import org.apache.jackrabbit.api.security.user.Group;
 import org.apache.jackrabbit.api.security.user.User;
 import org.apache.jackrabbit.oak.spi.security.user.UserConstants;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -257,6 +258,41 @@ public class GroupImportTest extends AbstractImportTest {
                 "       <sv:property sv:name=\"jcr:uuid\" sv:type=\"String\"><sv:value>0120a4f9-196a-3f9e-b9f5-23f31f914da7</sv:value></sv:property>" +
                 "       <sv:property sv:name=\"rep:principalName\" sv:type=\"String\"><sv:value>g1</sv:value></sv:property>" +
                 "       <sv:property sv:name=\"rep:members\" sv:type=\"WeakReference\"><sv:value>" + uuid + "</sv:value></sv:property>" +
+                "   </sv:node>" +
+                "</sv:node>";
+
+        doImport(getTargetPath(), xml);
+
+        Group g1 = (Group) userMgr.getAuthorizable("g1");
+        assertNotNull(g1);
+
+        // getWeakReferences only works upon save.
+        adminSession.save();
+
+        assertTrue(g1.isMember(admin));
+
+        boolean found = false;
+        for (Iterator<Group> it = admin.declaredMemberOf(); it.hasNext() && !found; ) {
+            found = "g1".equals(it.next().getID());
+        }
+        assertTrue(found);
+    }
+
+    @Test
+    @Ignore("OAK-2367")
+    public void testImportMembersWithIdDifferentFromNodeName() throws Exception {
+        Authorizable admin = checkNotNull(userMgr.getAuthorizable(UserConstants.DEFAULT_ADMIN_ID));
+        String uuid = adminSession.getNode(admin.getPath()).getUUID();
+        // deliberately put the 'rep:members' before the 'rep:authorizableId' to cover OAK-2367
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+                "<sv:node sv:name=\"gFolder\" xmlns:mix=\"http://www.jcp.org/jcr/mix/1.0\" xmlns:nt=\"http://www.jcp.org/jcr/nt/1.0\" xmlns:fn_old=\"http://www.w3.org/2004/10/xpath-functions\" xmlns:fn=\"http://www.w3.org/2005/xpath-functions\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" xmlns:sv=\"http://www.jcp.org/jcr/sv/1.0\" xmlns:rep=\"internal\" xmlns:jcr=\"http://www.jcp.org/jcr/1.0\">" +
+                "   <sv:property sv:name=\"jcr:primaryType\" sv:type=\"Name\"><sv:value>rep:AuthorizableFolder</sv:value></sv:property>" +
+                "   <sv:node sv:name=\"g_different\">" +
+                "       <sv:property sv:name=\"jcr:primaryType\" sv:type=\"Name\"><sv:value>rep:Group</sv:value></sv:property>" +
+                "       <sv:property sv:name=\"jcr:uuid\" sv:type=\"String\"><sv:value>0120a4f9-196a-3f9e-b9f5-23f31f914da7</sv:value></sv:property>" +
+                "       <sv:property sv:name=\"rep:principalName\" sv:type=\"String\"><sv:value>g1</sv:value></sv:property>" +
+                "       <sv:property sv:name=\"rep:members\" sv:type=\"WeakReference\"><sv:value>" + uuid + "</sv:value></sv:property>" +
+                "       <sv:property sv:name=\"rep:authorizableId\" sv:type=\"String\"><sv:value>g1</sv:value></sv:property>" +
                 "   </sv:node>" +
                 "</sv:node>";
 
