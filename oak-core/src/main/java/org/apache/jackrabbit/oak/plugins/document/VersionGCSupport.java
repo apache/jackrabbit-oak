cd01ea9683e0d786f795e642f950ff447118773f
Marcel Reutegger <mreutegg@apache.org>
2015-01-08 09:43:41 +0000

    OAK-1791: Clean up dangling references to previous documents
    Work in progress
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1650239 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/VersionGCSupport.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/VersionGCSupport.java
@@ -22,13 +22,27 @@ package org.apache.jackrabbit.oak.plugins.document;
 import java.util.List;
 import java.util.Set;
 
+import javax.annotation.CheckForNull;
+
+import com.google.common.base.Function;
 import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
+import org.apache.jackrabbit.oak.plugins.document.VersionGarbageCollector.VersionGCStats;
+import org.apache.jackrabbit.oak.plugins.document.util.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Iterables.transform;
+import static org.apache.jackrabbit.oak.plugins.document.Collection.NODES;
 import static org.apache.jackrabbit.oak.plugins.document.NodeDocument.SplitDocType;
+import static org.apache.jackrabbit.oak.plugins.document.util.Utils.getAllDocuments;
 
 public class VersionGCSupport {
+
+    private static final Logger LOG = LoggerFactory.getLogger(VersionGCSupport.class);
+
     private final DocumentStore store;
 
     public VersionGCSupport(DocumentStore store) {
@@ -36,7 +50,7 @@ public class VersionGCSupport {
     }
 
     public Iterable<NodeDocument> getPossiblyDeletedDocs(final long lastModifiedTime) {
-        return Iterables.filter(getAllDocuments(), new Predicate<NodeDocument>() {
+        return filter(getAllDocuments(store), new Predicate<NodeDocument>() {
             @Override
             public boolean apply(NodeDocument input) {
                 return input.wasDeletedOnce() && !input.hasBeenModifiedSince(lastModifiedTime);
@@ -44,21 +58,123 @@ public class VersionGCSupport {
         });
     }
 
-    public int deleteSplitDocuments(Set<SplitDocType> gcTypes, long oldestRevTimeStamp) {
-        List<String> docsToDelete = Lists.newArrayList();
-        for(NodeDocument doc : getAllDocuments()){
-            SplitDocType splitType = doc.getSplitDocType();
-            if(gcTypes.contains(splitType) && doc.hasAllRevisionLessThan(oldestRevTimeStamp)){
-                docsToDelete.add(doc.getId());
-            }
-        }
-        store.remove(Collection.NODES, docsToDelete);
-        return docsToDelete.size();
+    public void deleteSplitDocuments(Set<SplitDocType> gcTypes,
+                                     long oldestRevTimeStamp,
+                                     VersionGCStats stats) {
+        stats.splitDocGCCount += createCleanUp(gcTypes, oldestRevTimeStamp, stats)
+                // .disconnect() TODO: work in progress
+                .deleteSplitDocuments();
     }
 
-    private List<NodeDocument> getAllDocuments() {
-        //Fetch all documents.
-        return store.query(Collection.NODES,NodeDocument.MIN_ID_VALUE,
-                NodeDocument.MAX_ID_VALUE, Integer.MAX_VALUE);
+    protected SplitDocumentCleanUp createCleanUp(Set<SplitDocType> gcTypes,
+                                                 long oldestRevTimeStamp,
+                                                 VersionGCStats stats) {
+        return new SplitDocumentCleanUp(gcTypes, oldestRevTimeStamp, stats);
+    }
+
+    protected class SplitDocumentCleanUp {
+
+        protected final Set<SplitDocType> gcTypes;
+        protected final long oldestRevTimeStamp;
+        protected final VersionGCStats stats;
+
+        protected SplitDocumentCleanUp(Set<SplitDocType> gcTypes,
+                                       long oldestRevTimeStamp,
+                                       VersionGCStats stats) {
+            this.gcTypes = gcTypes;
+            this.oldestRevTimeStamp = oldestRevTimeStamp;
+            this.stats = stats;
+        }
+
+        protected SplitDocumentCleanUp disconnect() {
+            for (NodeDocument splitDoc : identifyGarbage()) {
+                disconnect(splitDoc);
+            }
+            return this;
+        }
+
+        protected int deleteSplitDocuments() {
+            List<String> docsToDelete = Lists.newArrayList(
+                    transform(identifyGarbage(), new Function<NodeDocument, String>() {
+                        @Override
+                        public String apply(NodeDocument input) {
+                            return input.getId();
+                        }
+                    }));
+            store.remove(NODES, docsToDelete);
+            return docsToDelete.size();
+        }
+
+        protected Iterable<NodeDocument> identifyGarbage() {
+            return filter(getAllDocuments(store), new Predicate<NodeDocument>() {
+                @Override
+                public boolean apply(NodeDocument doc) {
+                    SplitDocType splitType = doc.getSplitDocType();
+                    return gcTypes.contains(splitType)
+                            && doc.hasAllRevisionLessThan(oldestRevTimeStamp);
+                }
+            });
+        }
+
+        protected void disconnect(NodeDocument splitDoc) {
+            String splitId = splitDoc.getId();
+            String mainId = Utils.getIdFromPath(splitDoc.getMainPath());
+            NodeDocument doc = store.find(NODES, mainId);
+            if (doc == null) {
+                LOG.warn("Main document {} already removed. Split document is {}",
+                        mainId, splitId);
+                return;
+            }
+            int slashIdx = splitId.lastIndexOf('/');
+            int height = Integer.parseInt(splitId.substring(slashIdx + 1));
+            Revision rev = Revision.fromString(
+                    splitId.substring(splitId.lastIndexOf('/', slashIdx - 1) + 1, slashIdx));
+            doc = findReferencingDoc(doc, rev, height);
+            if (doc == null) {
+                LOG.warn("Split document {} not referenced anymore. Main document is {}",
+                        splitId, mainId);
+                return;
+            }
+            // remove reference
+            UpdateOp update = new UpdateOp(doc.getId(), false);
+            NodeDocument.removePrevious(update, rev);
+            NodeDocument old = store.findAndUpdate(NODES, update);
+            if (old != null
+                    && old.getSplitDocType() == SplitDocType.INTERMEDIATE
+                    && old.getPreviousRanges().size() == 1
+                    && old.getPreviousRanges().containsKey(rev)) {
+                // this was the last reference on an intermediate split doc
+                disconnect(old);
+                store.remove(NODES, old.getId());
+                stats.intermediateSplitDocGCCount++;
+            }
+        }
+
+        @CheckForNull
+        protected NodeDocument findReferencingDoc(NodeDocument current,
+                                                  Revision revision,
+                                                  int height) {
+            for (Range range : current.getPreviousRanges().values()) {
+                if (range.getHeight() == height && range.high.equals(revision)) {
+                    return current;
+                } else if (range.includes(revision)) {
+                    String prevId = Utils.getPreviousIdFor(
+                            current.getMainPath(), range.high, range.height);
+                    NodeDocument prev = store.find(NODES, prevId);
+                    if (prev == null) {
+                        LOG.warn("Split document {} does not exist anymore. Main document is {}",
+                                prevId, Utils.getIdFromPath(current.getMainPath()));
+                        continue;
+                    }
+                    // recurse into the split hierarchy
+                    NodeDocument doc = findReferencingDoc(prev, revision, height);
+                    if (doc != null) {
+                        return doc;
+                    }
+                }
+
+            }
+            return null;
+        }
     }
 }
