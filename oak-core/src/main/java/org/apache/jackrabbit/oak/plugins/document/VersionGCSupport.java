3677490a7b3ed7ea6e52a94159ea4f919acfb917
Marcel Reutegger <mreutegg@apache.org>
2015-01-13 10:35:46 +0000

    OAK-1791: Clean up dangling references to previous documents
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1651323 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/VersionGCSupport.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/VersionGCSupport.java
@@ -19,29 +19,17 @@
 
 package org.apache.jackrabbit.oak.plugins.document;
 
-import java.util.List;
 import java.util.Set;
 
-import javax.annotation.CheckForNull;
-
-import com.google.common.base.Function;
 import com.google.common.base.Predicate;
-import com.google.common.collect.Lists;
 
 import org.apache.jackrabbit.oak.plugins.document.VersionGarbageCollector.VersionGCStats;
-import org.apache.jackrabbit.oak.plugins.document.util.Utils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static com.google.common.collect.Iterables.filter;
-import static com.google.common.collect.Iterables.transform;
-import static org.apache.jackrabbit.oak.plugins.document.Collection.NODES;
 import static org.apache.jackrabbit.oak.plugins.document.NodeDocument.SplitDocType;
 import static org.apache.jackrabbit.oak.plugins.document.util.Utils.getAllDocuments;
 
 public class VersionGCSupport {
-
-    private static final Logger LOG = LoggerFactory.getLogger(VersionGCSupport.class);
 
     private final DocumentStore store;
 
@@ -62,119 +50,25 @@ public class VersionGCSupport {
                                      long oldestRevTimeStamp,
                                      VersionGCStats stats) {
         stats.splitDocGCCount += createCleanUp(gcTypes, oldestRevTimeStamp, stats)
-                // .disconnect() TODO: work in progress
+                .disconnect()
                 .deleteSplitDocuments();
     }
 
     protected SplitDocumentCleanUp createCleanUp(Set<SplitDocType> gcTypes,
                                                  long oldestRevTimeStamp,
                                                  VersionGCStats stats) {
-        return new SplitDocumentCleanUp(gcTypes, oldestRevTimeStamp, stats);
+        return new SplitDocumentCleanUp(store, stats,
+                identifyGarbage(gcTypes, oldestRevTimeStamp));
     }
 
-    protected class SplitDocumentCleanUp {
-
-        protected final Set<SplitDocType> gcTypes;
-        protected final long oldestRevTimeStamp;
-        protected final VersionGCStats stats;
-
-        protected SplitDocumentCleanUp(Set<SplitDocType> gcTypes,
-                                       long oldestRevTimeStamp,
-                                       VersionGCStats stats) {
-            this.gcTypes = gcTypes;
-            this.oldestRevTimeStamp = oldestRevTimeStamp;
-            this.stats = stats;
-        }
-
-        protected SplitDocumentCleanUp disconnect() {
-            for (NodeDocument splitDoc : identifyGarbage()) {
-                disconnect(splitDoc);
+    protected Iterable<NodeDocument> identifyGarbage(final Set<SplitDocType> gcTypes,
+                                                     final long oldestRevTimeStamp) {
+        return filter(getAllDocuments(store), new Predicate<NodeDocument>() {
+            @Override
+            public boolean apply(NodeDocument doc) {
+                return gcTypes.contains(doc.getSplitDocType())
+                        && doc.hasAllRevisionLessThan(oldestRevTimeStamp);
             }
-            return this;
-        }
-
-        protected int deleteSplitDocuments() {
-            List<String> docsToDelete = Lists.newArrayList(
-                    transform(identifyGarbage(), new Function<NodeDocument, String>() {
-                        @Override
-                        public String apply(NodeDocument input) {
-                            return input.getId();
-                        }
-                    }));
-            store.remove(NODES, docsToDelete);
-            return docsToDelete.size();
-        }
-
-        protected Iterable<NodeDocument> identifyGarbage() {
-            return filter(getAllDocuments(store), new Predicate<NodeDocument>() {
-                @Override
-                public boolean apply(NodeDocument doc) {
-                    SplitDocType splitType = doc.getSplitDocType();
-                    return gcTypes.contains(splitType)
-                            && doc.hasAllRevisionLessThan(oldestRevTimeStamp);
-                }
-            });
-        }
-
-        protected void disconnect(NodeDocument splitDoc) {
-            String splitId = splitDoc.getId();
-            String mainId = Utils.getIdFromPath(splitDoc.getMainPath());
-            NodeDocument doc = store.find(NODES, mainId);
-            if (doc == null) {
-                LOG.warn("Main document {} already removed. Split document is {}",
-                        mainId, splitId);
-                return;
-            }
-            int slashIdx = splitId.lastIndexOf('/');
-            int height = Integer.parseInt(splitId.substring(slashIdx + 1));
-            Revision rev = Revision.fromString(
-                    splitId.substring(splitId.lastIndexOf('/', slashIdx - 1) + 1, slashIdx));
-            doc = findReferencingDoc(doc, rev, height);
-            if (doc == null) {
-                LOG.warn("Split document {} not referenced anymore. Main document is {}",
-                        splitId, mainId);
-                return;
-            }
-            // remove reference
-            UpdateOp update = new UpdateOp(doc.getId(), false);
-            NodeDocument.removePrevious(update, rev);
-            NodeDocument old = store.findAndUpdate(NODES, update);
-            if (old != null
-                    && old.getSplitDocType() == SplitDocType.INTERMEDIATE
-                    && old.getPreviousRanges().size() == 1
-                    && old.getPreviousRanges().containsKey(rev)) {
-                // this was the last reference on an intermediate split doc
-                disconnect(old);
-                store.remove(NODES, old.getId());
-                stats.intermediateSplitDocGCCount++;
-            }
-        }
-
-        @CheckForNull
-        protected NodeDocument findReferencingDoc(NodeDocument current,
-                                                  Revision revision,
-                                                  int height) {
-            for (Range range : current.getPreviousRanges().values()) {
-                if (range.getHeight() == height && range.high.equals(revision)) {
-                    return current;
-                } else if (range.includes(revision)) {
-                    String prevId = Utils.getPreviousIdFor(
-                            current.getMainPath(), range.high, range.height);
-                    NodeDocument prev = store.find(NODES, prevId);
-                    if (prev == null) {
-                        LOG.warn("Split document {} does not exist anymore. Main document is {}",
-                                prevId, Utils.getIdFromPath(current.getMainPath()));
-                        continue;
-                    }
-                    // recurse into the split hierarchy
-                    NodeDocument doc = findReferencingDoc(prev, revision, height);
-                    if (doc != null) {
-                        return doc;
-                    }
-                }
-
-            }
-            return null;
-        }
+        });
     }
 }
