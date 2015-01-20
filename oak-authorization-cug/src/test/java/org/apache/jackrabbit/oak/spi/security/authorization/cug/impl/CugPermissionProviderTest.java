6336477dab07a4ac0860f72479b83aa8ba77a9ce
Angela Schreiber <angela@apache.org>
2015-01-20 16:14:50 +0000

    OAK-1268
    Add support for composite authorization setup (wip)
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1653280 13f79535-47bb-0310-9956-ffa450edef68

--- oak-authorization-cug/src/test/java/org/apache/jackrabbit/oak/spi/security/authorization/cug/impl/CugPermissionProviderTest.java
+++ oak-authorization-cug/src/test/java/org/apache/jackrabbit/oak/spi/security/authorization/cug/impl/CugPermissionProviderTest.java
@@ -16,7 +16,63 @@
  */
 package org.apache.jackrabbit.oak.spi.security.authorization.cug.impl;
 
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Set;
+
+import org.apache.jackrabbit.oak.spi.security.authorization.permission.ControlFlag;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+
 public class CugPermissionProviderTest extends AbstractCugTest {
 
-    // TODO
+    private CugPermissionProvider ppSufficient;
+    private CugPermissionProvider ppRequisite;
+
+    @Before
+    @Override
+    public void before() throws Exception {
+        super.before();
+
+        Set<Principal> principals = Collections.emptySet(); // TODO
+        String[] supportedPaths = new String[0]; // TODO
+
+        ppSufficient = new CugPermissionProvider(root, principals, supportedPaths, ControlFlag.SUFFICIENT, CugContext.INSTANCE);
+        ppRequisite = new CugPermissionProvider(root, principals, supportedPaths, ControlFlag.REQUISITE, CugContext.INSTANCE);
+    }
+
+    @Test
+    public void testGetFlag() {
+        assertSame(ControlFlag.SUFFICIENT, ppSufficient.getFlag());
+        assertSame(ControlFlag.REQUISITE, ppRequisite.getFlag());
+    }
+
+    @Test
+    public void testHandlesPath() {
+        // TODO
+    }
+
+    @Test
+    public void testHandlesTree() {
+        // TODO
+    }
+
+    @Test
+    public void testHandlesRepositoryPermissions() {
+        assertFalse(ppSufficient.handlesRepositoryPermissions());
+        assertFalse(ppRequisite.handlesRepositoryPermissions());
+    }
+
+    @Test
+    public void getPrivileges() {
+        // TODO
+    }
+
+    @Test
+    public void hasPrivileges() {
+        // TODO
+    }
 }
