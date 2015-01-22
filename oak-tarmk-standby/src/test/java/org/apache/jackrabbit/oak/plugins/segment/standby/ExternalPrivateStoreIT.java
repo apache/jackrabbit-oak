957f0913b8e610a13ee49bea34e9b2168c5f4602
Alexandru Parvulescu <alexparvulescu@apache.org>
2015-01-22 10:31:16 +0000

    OAK-2372 TARMK Cold Standby improve test cases for FSDS mirroring  - merged r1646684, r1651652, r1652024, r1652035, r1652058, r1652127
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1653803 13f79535-47bb-0310-9956-ffa450edef68

--- oak-tarmk-standby/src/test/java/org/apache/jackrabbit/oak/plugins/segment/standby/ExternalPrivateStoreIT.java
+++ oak-tarmk-standby/src/test/java/org/apache/jackrabbit/oak/plugins/segment/standby/ExternalPrivateStoreIT.java
@@ -19,46 +19,20 @@
 package org.apache.jackrabbit.oak.plugins.segment.standby;
 
 import static org.apache.jackrabbit.oak.plugins.segment.SegmentTestUtils.createTmpTargetDir;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.jackrabbit.core.data.FileDataStore;
-import org.apache.jackrabbit.oak.api.Blob;
-import org.apache.jackrabbit.oak.api.CommitFailedException;
-import org.apache.jackrabbit.oak.api.PropertyState;
-import org.apache.jackrabbit.oak.api.Type;
-import org.apache.jackrabbit.oak.plugins.blob.datastore.DataStoreBlobStore;
-import org.apache.jackrabbit.oak.plugins.segment.SegmentNodeStore;
 import org.apache.jackrabbit.oak.plugins.segment.file.FileStore;
-import org.apache.jackrabbit.oak.plugins.segment.standby.client.StandbyClient;
-import org.apache.jackrabbit.oak.plugins.segment.standby.server.StandbyServer;
-import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
-import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
-import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
-import org.apache.jackrabbit.oak.spi.state.NodeStore;
+
 import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
-import com.google.common.io.ByteStreams;
 
-public class ExternalPrivateStoreIT extends TestBase {
+public class ExternalPrivateStoreIT extends DataStoreTestBase {
 
     private File primaryStore;
     private File secondaryStore;
-
-    @Before
-    public void setUp() throws Exception {
-        setUpServerAndClient();
-    }
 
     @After
     public void after() {
@@ -72,81 +46,14 @@ public class ExternalPrivateStoreIT extends TestBase {
 
     @Override
     protected FileStore setupPrimary(File d) throws IOException {
-        primaryStore = getPrimaryStoreDir();
-        FileDataStore fds = new FileDataStore();
-        fds.setMinRecordLength(4092);
-        fds.init(primaryStore.getAbsolutePath());
-        DataStoreBlobStore blobStore = new DataStoreBlobStore(fds);
-        return new FileStore(blobStore, d, 1, false);
-    }
-
-    private static File getPrimaryStoreDir() throws IOException {
-        return createTmpTargetDir("ExternalStoreITPrimary");
+        primaryStore = createTmpTargetDir("ExternalStoreITPrimary");
+        return setupFileDataStore(d, primaryStore.getAbsolutePath());
     }
 
     @Override
     protected FileStore setupSecondary(File d) throws IOException {
-        secondaryStore = getSecondaryStoreDir();
-        FileDataStore fds = new FileDataStore();
-        fds.setMinRecordLength(4092);
-        fds.init(secondaryStore.getAbsolutePath());
-        DataStoreBlobStore blobStore = new DataStoreBlobStore(fds);
-        return new FileStore(blobStore, d, 1, false);
-    }
-
-    private static File getSecondaryStoreDir() throws IOException {
-        return createTmpTargetDir("ExternalStoreITSecondary");
-    }
-
-    @Test
-    public void testSync() throws Exception {
-        final int mb = 1 * 1024 * 1024;
-        final int blobSize = 5 * mb;
-        FileStore primary = getPrimary();
-        FileStore secondary = getSecondary();
-
-        NodeStore store = new SegmentNodeStore(primary);
-        final StandbyServer server = new StandbyServer(getPort(), primary);
-        server.start();
-        byte[] data = addTestContent(store, "server", blobSize);
-
-        StandbyClient cl = new StandbyClient("127.0.0.1", getPort(), secondary);
-        cl.run();
-
-        try {
-            assertEquals(primary.getHead(), secondary.getHead());
-        } finally {
-            server.close();
-            cl.close();
-        }
-
-        assertTrue(primary.size() < mb);
-        assertTrue(secondary.size() < mb);
-
-        PropertyState ps = secondary.getHead().getChildNode("root")
-                .getChildNode("server").getProperty("testBlob");
-        assertNotNull(ps);
-        assertEquals(Type.BINARY.tag(), ps.getType().tag());
-        Blob b = ps.getValue(Type.BINARY);
-        assertEquals(blobSize, b.length());
-        byte[] testData = new byte[blobSize];
-        ByteStreams.readFully(b.getNewStream(), testData);
-        assertArrayEquals(data, testData);
-    }
-
-    private static byte[] addTestContent(NodeStore store, String child, int size)
-            throws CommitFailedException, IOException {
-        NodeBuilder builder = store.getRoot().builder();
-        builder.child(child).setProperty("ts", System.currentTimeMillis());
-
-        byte[] data = new byte[size];
-        new Random().nextBytes(data);
-        Blob blob = store.createBlob(new ByteArrayInputStream(data));
-
-        builder.child(child).setProperty("testBlob", blob);
-
-        store.merge(builder, EmptyHook.INSTANCE, CommitInfo.EMPTY);
-        return data;
+        secondaryStore = createTmpTargetDir("ExternalStoreITSecondary");
+        return setupFileDataStore(d, secondaryStore.getAbsolutePath());
     }
 
 }
