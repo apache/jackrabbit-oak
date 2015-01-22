1a21951a7beae16964db35e7ab9a0c618ed168ba
Chetan Mehrotra <chetanm@apache.org>
2015-01-22 14:07:46 +0000

    OAK-2439 - IndexPlanner returning plan for queries involving jcr:score
    Merging 1653849
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1653865 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/IndexPlanner.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/IndexPlanner.java
@@ -35,6 +35,7 @@ import org.apache.jackrabbit.oak.query.fulltext.FullTextExpression;
 import org.apache.jackrabbit.oak.query.fulltext.FullTextTerm;
 import org.apache.jackrabbit.oak.query.fulltext.FullTextVisitor;
 import org.apache.jackrabbit.oak.spi.query.Filter;
+import org.apache.jackrabbit.oak.spi.query.QueryIndex;
 import org.apache.lucene.index.IndexReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,6 +43,7 @@ import org.slf4j.LoggerFactory;
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Lists.newArrayListWithCapacity;
 import static com.google.common.collect.Maps.newHashMap;
+import static org.apache.jackrabbit.JcrConstants.JCR_SCORE;
 import static org.apache.jackrabbit.oak.commons.PathUtils.getAncestorPath;
 import static org.apache.jackrabbit.oak.commons.PathUtils.getDepth;
 import static org.apache.jackrabbit.oak.commons.PathUtils.getParentPath;
@@ -155,7 +157,8 @@ class IndexPlanner {
         //Fulltext expression can also be like jcr:contains(jcr:content/metadata/@format, 'image')
 
         List<OrderEntry> sortOrder = createSortOrder(indexingRule);
-        if (!indexedProps.isEmpty() || !sortOrder.isEmpty() || ft != null || evalPathRestrictions) {
+        boolean canSort = canHandleSorting(sortOrder);
+        if (!indexedProps.isEmpty() || canSort || ft != null || evalPathRestrictions) {
             //TODO Need a way to have better cost estimate to indicate that
             //this index can evaluate more propertyRestrictions natively (if more props are indexed)
             //For now we reduce cost per entry
@@ -188,6 +191,20 @@ class IndexPlanner {
         //by NodeType index
 
         return null;
+    }
+
+    private boolean canHandleSorting(List<OrderEntry> sortOrder) {
+        if (sortOrder.isEmpty()){
+            return false;
+        }
+
+        //If jcr:score is the only sort order then opt out
+        if (sortOrder.size() == 1
+                && JCR_SCORE.equals(sortOrder.get(0).getPropertyName())){
+            return false;
+        }
+
+        return true;
     }
 
     private boolean canEvalAllFullText(final IndexingRule indexingRule, FullTextExpression ft) {
