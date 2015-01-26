f1a2e282af86138c74ad45aba2e122684d18da20
Marcel Reutegger <mreutegg@apache.org>
2015-01-26 12:35:30 +0000

    OAK-2442: NoSuchElementException thrown by NodeDocument
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1654778 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/NodeDocument.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/NodeDocument.java
@@ -802,8 +802,13 @@ public final class NodeDocument extends Document implements CachedNodeDocument{
             if (!Utils.isPropertyName(key)) {
                 continue;
             }
+            // ignore when local map is empty (OAK-2442)
+            SortedMap<Revision, String> local = getLocalMap(key);
+            if (local.isEmpty()) {
+                continue;
+            }
             // first check local map, which contains most recent values
-            Value value = getLatestValue(nodeStore, getLocalMap(key),
+            Value value = getLatestValue(nodeStore, local,
                     min, readRevision, validRevisions, lastRevs);
 
             // check if there may be more recent values in a previous document
