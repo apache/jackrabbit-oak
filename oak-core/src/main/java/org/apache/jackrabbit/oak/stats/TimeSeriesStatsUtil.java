9a56cf8fa11867d513a83977b6db4b36e4b62ae2
Michael Duerig <mduerig@apache.org>
2015-03-03 16:29:43 +0000

    OAK-2552: Implement MBean monitoring garbage collection Merged revision 1663730
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1663734 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/stats/TimeSeriesStatsUtil.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.jackrabbit.oak.stats;
+
+import javax.annotation.Nonnull;
+import javax.management.openmbean.ArrayType;
+import javax.management.openmbean.CompositeData;
+import javax.management.openmbean.CompositeDataSupport;
+import javax.management.openmbean.CompositeType;
+import javax.management.openmbean.OpenDataException;
+import javax.management.openmbean.OpenType;
+import javax.management.openmbean.SimpleType;
+
+import org.apache.jackrabbit.api.stats.TimeSeries;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Utility class for retrieving {@link javax.management.openmbean.CompositeData} for
+ * {@link org.apache.jackrabbit.api.stats.TimeSeries}.
+ */
+public final class TimeSeriesStatsUtil {
+    public static final String[] ITEM_NAMES = new String[] {"per second", "per minute", "per hour", "per week"};
+
+    private static final Logger LOG = LoggerFactory.getLogger(TimeSeriesStatsUtil.class);
+
+    private TimeSeriesStatsUtil() {
+    }
+
+    @Nonnull
+    public static CompositeData asCompositeData(TimeSeries timeSeries, String name) {
+        try {
+            long[][] values = new long[][] {timeSeries.getValuePerSecond(), timeSeries.getValuePerMinute(),
+                timeSeries.getValuePerHour(), timeSeries.getValuePerWeek()};
+            return new CompositeDataSupport(getCompositeType(name), ITEM_NAMES, values);
+        } catch (Exception e) {
+            String msg = "Error creating CompositeData instance from TimeSeries";
+            LOG.error(msg, e);
+            throw new IllegalArgumentException(msg, e);
+        }
+    }
+
+    private static CompositeType getCompositeType(String name) throws OpenDataException {
+        ArrayType<int[]> longArrayType = new ArrayType<int[]>(SimpleType.LONG, true);
+        OpenType<?>[] itemTypes = new OpenType[] {longArrayType, longArrayType, longArrayType, longArrayType};
+        return new CompositeType(name, name + " time series", ITEM_NAMES, ITEM_NAMES, itemTypes);
+    }
+}
