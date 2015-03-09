5cf689f80768aa4fe95f0c69b446b9dcf22998e0
Tommaso Teofili <tommaso@apache.org>
2015-03-09 14:49:46 +0000

    OAK-2594 - backported OAK-2490 to branch 1.0
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1665253 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/query/index/FilterImpl.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/query/index/FilterImpl.java
@@ -28,6 +28,7 @@ import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.jcr.PropertyType;
+import javax.jcr.Session;
 
 import org.apache.jackrabbit.oak.api.PropertyValue;
 import org.apache.jackrabbit.oak.commons.PathUtils;
@@ -38,6 +39,7 @@ import org.apache.jackrabbit.oak.query.ast.Operator;
 import org.apache.jackrabbit.oak.query.ast.SelectorImpl;
 import org.apache.jackrabbit.oak.query.fulltext.FullTextExpression;
 import org.apache.jackrabbit.oak.spi.query.Filter;
+import org.apache.jackrabbit.oak.spi.security.authorization.permission.PermissionProvider;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
@@ -574,6 +576,12 @@ public class FilterImpl implements Filter {
     @Override
     public QueryEngineSettings getQueryEngineSettings() {
         return settings;
+    }
+
+    @Override
+    public boolean isAccessible(String path) {
+        PermissionProvider permissionProvider = selector.getQuery().getExecutionContext().getPermissionProvider();
+        return permissionProvider != null && permissionProvider.isGranted(path, Session.ACTION_READ);
     }
 
 }
