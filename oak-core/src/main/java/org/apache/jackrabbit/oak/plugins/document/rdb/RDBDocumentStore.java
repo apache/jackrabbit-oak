503eeef8b5d955524a2721dd24910e12cf0962ca
Julian Reschke <reschke@apache.org>
2015-03-13 13:18:48 +0000

    OAK-2628 - RDB: convenience tool for dumping table creation statements (ported to 1.0)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1666439 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBDocumentStore.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBDocumentStore.java
@@ -360,7 +360,7 @@ public class RDBDocumentStore implements DocumentStore {
     /**
      * Defines variation in the capabilities of different RDBs.
      */
-    enum DB {
+    protected enum DB {
         DEFAULT("default") {
         },
 
