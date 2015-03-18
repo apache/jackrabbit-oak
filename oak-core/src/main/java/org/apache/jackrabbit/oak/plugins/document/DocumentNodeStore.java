162a128b40b7735fa1efe422e609c860675f531e
Marcel Reutegger <mreutegg@apache.org>
2015-03-18 13:23:18 +0000

    OAK-2642: DocumentNodeStore.dispose() may leave repository in an inconsistent state
    Merged revisions 1667293 and 1667498 from trunk
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1667543 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStore.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStore.java
@@ -460,42 +460,51 @@ public final class DocumentNodeStore
     }
 
     public void dispose() {
-        runBackgroundOperations();
-        if (!isDisposed.getAndSet(true)) {
-            synchronized (isDisposed) {
-                isDisposed.notifyAll();
-            }
+        if (isDisposed.getAndSet(true)) {
+            // only dispose once
+            return;
+        }
+        // notify background threads waiting on isDisposed
+        synchronized (isDisposed) {
+            isDisposed.notifyAll();
+        }
+        try {
+            backgroundThread.join();
+        } catch (InterruptedException e) {
+            // ignore
+        }
+
+        // do a final round of background operations after
+        // the background thread stopped
+        internalRunBackgroundOperations();
+
+        if (leaseUpdateThread != null) {
             try {
-                backgroundThread.join();
+                leaseUpdateThread.join();
             } catch (InterruptedException e) {
                 // ignore
             }
-            if (leaseUpdateThread != null) {
-                try {
-                    leaseUpdateThread.join();
-                } catch (InterruptedException e) {
-                    // ignore
-                }
-            }
-            if (clusterNodeInfo != null) {
-                clusterNodeInfo.dispose();
-            }
-            store.dispose();
-            unsavedLastRevisions.close();
+        }
 
-            LOG.info("Disposed DocumentNodeStore with clusterNodeId: {}", clusterId);
+        // now mark this cluster node as inactive by
+        // disposing the clusterNodeInfo
+        if (clusterNodeInfo != null) {
+            clusterNodeInfo.dispose();
+        }
+        store.dispose();
+        unsavedLastRevisions.close();
 
-            if (blobStore instanceof Closeable) {
-                try {
-                    ((Closeable) blobStore).close();
-                } catch (IOException ex) {
-                    LOG.debug("Error closing blob store " + blobStore, ex);
-                }
+        if (blobStore instanceof Closeable) {
+            try {
+                ((Closeable) blobStore).close();
+            } catch (IOException ex) {
+                LOG.debug("Error closing blob store " + blobStore, ex);
             }
         }
         if (persistentCache != null) {
             persistentCache.close();
         }
+        LOG.info("Disposed DocumentNodeStore with clusterNodeId: {}", clusterId);
     }
 
     Revision setHeadRevision(@Nonnull Revision newHead) {
@@ -547,6 +556,7 @@ public final class DocumentNodeStore
             base = headRevision;
         }
         backgroundOperationLock.readLock().lock();
+        checkOpen();
         boolean success = false;
         Commit c;
         try {
@@ -576,6 +586,7 @@ public final class DocumentNodeStore
             base = headRevision;
         }
         backgroundOperationLock.readLock().lock();
+        checkOpen();
         boolean success = false;
         MergeCommit c;
         try {
@@ -1469,48 +1480,49 @@ public final class DocumentNodeStore
 
     //----------------------< background operations >---------------------------
 
-    public synchronized void runBackgroundOperations() {
+    public void runBackgroundOperations() {
         if (isDisposed.get()) {
-            return;
-        }
-        if (simpleRevisionCounter != null) {
-            // only when using timestamp
             return;
         }
         if (!ENABLE_BACKGROUND_OPS) {
             return;
         }
         try {
-            long start = clock.getTime();
-            long time = start;
-            // clean orphaned branches and collisions
-            cleanOrphanedBranches();
-            cleanCollisions();
-            long cleanTime = clock.getTime() - time;
-            time = clock.getTime();
-            // split documents (does not create new revisions)
-            backgroundSplit();
-            long splitTime = clock.getTime() - time;
-            time = clock.getTime();
-            // write back pending updates to _lastRev
-            backgroundWrite();
-            long writeTime = clock.getTime() - time;
-            time = clock.getTime();
-            // pull in changes from other cluster nodes
-            BackgroundReadStats readStats = backgroundRead(true);
-            long readTime = clock.getTime() - time;
-            String msg = "Background operations stats (clean:{}, split:{}, write:{}, read:{} {})";
-            if (clock.getTime() - start > TimeUnit.SECONDS.toMillis(10)) {
-                // log as info if it took more than 10 seconds
-                LOG.info(msg, cleanTime, splitTime, writeTime, readTime, readStats);
-            } else {
-                LOG.debug(msg, cleanTime, splitTime, writeTime, readTime, readStats);
-            }
+            internalRunBackgroundOperations();
         } catch (RuntimeException e) {
             if (isDisposed.get()) {
+                LOG.warn("Background operation failed: " + e.toString(), e);
                 return;
             }
             throw e;
+        }
+    }
+
+    private synchronized void internalRunBackgroundOperations() {
+        long start = clock.getTime();
+        long time = start;
+        // clean orphaned branches and collisions
+        cleanOrphanedBranches();
+        cleanCollisions();
+        long cleanTime = clock.getTime() - time;
+        time = clock.getTime();
+        // split documents (does not create new revisions)
+        backgroundSplit();
+        long splitTime = clock.getTime() - time;
+        time = clock.getTime();
+        // write back pending updates to _lastRev
+        backgroundWrite();
+        long writeTime = clock.getTime() - time;
+        time = clock.getTime();
+        // pull in changes from other cluster nodes
+        BackgroundReadStats readStats = backgroundRead(true);
+        long readTime = clock.getTime() - time;
+        String msg = "Background operations stats (clean:{}, split:{}, write:{}, read:{} {})";
+        if (clock.getTime() - start > TimeUnit.SECONDS.toMillis(10)) {
+            // log as info if it took more than 10 seconds
+            LOG.info(msg, cleanTime, splitTime, writeTime, readTime, readStats);
+        } else {
+            LOG.debug(msg, cleanTime, splitTime, writeTime, readTime, readStats);
         }
     }
 
@@ -1738,6 +1750,19 @@ public final class DocumentNodeStore
     }
 
     //-----------------------------< internal >---------------------------------
+
+    /**
+     * Checks if this store is still open and throws an
+     * {@link IllegalStateException} if it is already disposed (or a dispose
+     * is in progress).
+     *
+     * @throws IllegalStateException if this store is disposed.
+     */
+    private void checkOpen() throws IllegalStateException {
+        if (isDisposed.get()) {
+            throw new IllegalStateException("This DocumentNodeStore is disposed");
+        }
+    }
 
     /**
      * Search for presence of child node as denoted by path in the children cache of parent
--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStoreTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStoreTest.java
@@ -42,9 +42,12 @@ import java.util.Map;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.annotation.CheckForNull;
@@ -1134,6 +1137,94 @@ public class DocumentNodeStoreTest {
         assertTrue("Existing children should be reported as such", existingChild.exists());
 
         store.dispose();
+    }
+
+    // OAK-2642
+    @Test
+    public void dispose() throws CommitFailedException, InterruptedException {
+        final BlockingQueue<String> updates = new ArrayBlockingQueue<String>(1);
+        MemoryDocumentStore docStore = new MemoryDocumentStore() {
+            @Override
+            public <T extends Document> void update(Collection<T> collection,
+                                                    List<String> keys,
+                                                    UpdateOp updateOp) {
+                for (String k : keys) {
+                    try {
+                        updates.put(k);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                super.update(collection, keys, updateOp);
+            }
+        };
+        final DocumentNodeStore store = new DocumentMK.Builder()
+                .setClusterId(1).setAsyncDelay(0)
+                .setDocumentStore(docStore).getNodeStore();
+        updates.clear();
+
+        NodeBuilder builder = store.getRoot().builder();
+        builder.child("test").child("node");
+        merge(store, builder);
+
+        builder = store.getRoot().builder();
+        builder.child("test").child("node").child("child-1");
+        merge(store, builder);
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                store.dispose();
+            }
+        });
+        t.start();
+
+        String p = updates.take();
+        assertEquals("2:/test/node", p);
+        // background ops in dispose is still in progress
+        assertTrue(t.isAlive());
+        // wait until next update comes in
+        for (;;) {
+            if (updates.peek() != null) {
+                break;
+            }
+        }
+
+        // add child-2 while dispose is in progress
+        try {
+            builder = store.getRoot().builder();
+            builder.child("test").child("node").child("child-2");
+            merge(store, builder);
+            fail("Merge must fail with CommitFailedException");
+        } catch (CommitFailedException e) {
+            // expected
+        }
+
+        // drain updates until dispose finished
+        while (t.isAlive()) {
+            updates.poll(10, TimeUnit.MILLISECONDS);
+        }
+
+        // start new store with clusterId 2
+        DocumentNodeStore store2 = new DocumentMK.Builder()
+                .setClusterId(2).setAsyncDelay(0)
+                .setDocumentStore(docStore).getNodeStore();
+
+        // perform recovery if needed
+        LastRevRecoveryAgent agent = new LastRevRecoveryAgent(store2);
+        if (agent.isRecoveryNeeded()) {
+            agent.recover(1);
+        }
+
+        builder = store2.getRoot().builder();
+        NodeBuilder test = builder.getChildNode("test");
+        assertTrue(test.exists());
+        NodeBuilder node = test.getChildNode("node");
+        assertTrue(node.exists());
+        if (!node.hasChildNode("child-2")) {
+            node.child("child-2");
+            merge(store2, builder);
+        }
     }
 
     private static void assertNoPreviousDocs(Set<String> ids) {
