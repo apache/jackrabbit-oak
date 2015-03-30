33b3a0044889c92d8c18acbade73efd19e605a8e
Marcel Reutegger <mreutegg@apache.org>
2015-03-30 14:34:21 +0000

    OAK-2371: Helper class for CI configurations
    Merged revision 1646726 from trunk
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1670112 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-commons/src/test/java/org/apache/jackrabbit/oak/commons/CIHelper.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.jackrabbit.oak.commons;
+
+import static com.google.common.base.Objects.equal;
+import static java.lang.Boolean.parseBoolean;
+import static java.lang.System.getenv;
+
+/**
+ * Utility class for ITs to determine the environment running in.
+ */
+public final class CIHelper {
+    private CIHelper() { }
+
+    /**
+     * @return  {@code true} iff running on
+     * http://ci.apache.org/builders/oak-trunk-win7
+     */
+    public static boolean buildBotTrunkWin7() {
+        String user = getenv("USERDOMAIN");
+        return user != null && user.startsWith("bb-win7");
+    }
+
+    /**
+     * @return  {@code true} iff running on
+     * http://ci.apache.org/builders/oak-trunk
+     */
+    public static boolean buildBotTrunkLinux() {
+        String user = getenv("USER");
+        return user != null && user.startsWith("buildslave3");
+    }
+
+    /**
+     * @return  {@code true} iff running on
+     * https://travis-ci.org/
+     */
+    public static boolean travis() {
+        return parseBoolean(getenv("TRAVIS"));
+    }
+
+    /**
+     * @return  {@code true} iff running on with {@code PROFILE=pedantic}
+     */
+    public static boolean travisPedantic() {
+        return equal(getenv("PROFILE"), "pedantic");
+    }
+
+    /**
+     * @return  {@code true} iff running on with {@code PROFILE=unittesting}
+     */
+    public static boolean travisUnitTesting() {
+        return equal(getenv("PROFILE"), "unittesting");
+    }
+
+    /**
+     * @return  {@code true} iff running on with {@code PROFILE=integrationTesting}
+     */
+    public static boolean travisIntegrationTesting() {
+        return equal(getenv("PROFILE"), "integrationTesting");
+    }
+
+}
