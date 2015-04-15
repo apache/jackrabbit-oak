ecf654528f15017fd3268433e22d4507d2ebbdbb
Angela Schreiber <angela@apache.org>
2015-04-15 09:14:52 +0000

    OAK-2674 : Fix FindBug Issues (null-deref in oak-auth-external)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1673688 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/spi/security/ConfigurationParameters.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/security/ConfigurationParameters.java
@@ -494,6 +494,35 @@ public final class ConfigurationParameters implements Map<String, Object> {
             return current < 0 ? null : new Milliseconds(current + 1);
         }
 
+        @Nonnull
+        public static Milliseconds of(@Nullable String str, @Nonnull Milliseconds defaultValue) {
+            if (str == null) {
+                return defaultValue;
+            }
+            Matcher m = pattern.matcher(str);
+            long current = -1;
+            while (m.find()) {
+                String number = m.group(1);
+                String decimal = m.group(2);
+                if (decimal != null) {
+                    number += decimal;
+                }
+                String unit = m.group(3);
+                double value = Double.valueOf(number);
+                if ("s".equals(unit)) {
+                    value *= 1000.0;
+                } else if ("m".equals(unit)) {
+                    value *= 60 * 1000.0;
+                } else if ("h".equals(unit)) {
+                    value *= 60 * 60 * 1000.0;
+                } else if ("d".equals(unit)) {
+                    value *= 24 * 60 * 60 * 1000.0;
+                }
+                current += value;
+            }
+            return current < 0 ? defaultValue : new Milliseconds(current + 1);
+        }
+
         @Override
         public boolean equals(Object o) {
             return this == o || !(o == null || getClass() != o.getClass()) && value == ((Milliseconds) o).value;
