ac3039ef1a3d2f04ca73bc893f30ba8e3214696d
Angela Schreiber <angela@apache.org>
2015-04-22 10:18:37 +0000

    unsued import
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1675313 13f79535-47bb-0310-9956-ffa450edef68

--- oak-run/src/main/java/org/apache/jackrabbit/oak/benchmark/AbstractLoginTest.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/benchmark/AbstractLoginTest.java
@@ -31,7 +31,6 @@ import com.google.common.collect.ImmutableMap;
 import org.apache.jackrabbit.api.JackrabbitSession;
 import org.apache.jackrabbit.api.security.authentication.token.TokenCredentials;
 import org.apache.jackrabbit.api.security.user.Authorizable;
-import org.apache.jackrabbit.api.security.user.User;
 import org.apache.jackrabbit.commons.jackrabbit.authorization.AccessControlUtils;
 import org.apache.jackrabbit.oak.Oak;
 import org.apache.jackrabbit.oak.fixture.JcrCreator;
