38670e11ed5682b49b7a4b37203aadcd89e1de44
Marcel Reutegger <mreutegg@apache.org>
2015-05-05 12:47:14 +0000

    OAK-2685: Track root state revision when reading the tree
    Commit missing part of the patch
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1677806 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/TieredDiffCache.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.plugins.document;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.google.common.collect.Iterables;
+
+import org.apache.jackrabbit.oak.cache.CacheStats;
+
+/**
+ * Implements a tiered diff cache which consists of a {@link LocalDiffCache} and
+ * a {@link MemoryDiffCache}.
+ */
+class TieredDiffCache implements DiffCache {
+
+    private final LocalDiffCache localCache;
+    private final MemoryDiffCache memoryCache;
+
+    TieredDiffCache(DocumentMK.Builder builder) {
+        this.localCache = new LocalDiffCache(builder);
+        this.memoryCache = new MemoryDiffCache(builder);
+    }
+
+    @Override
+    public String getChanges(@Nonnull Revision from,
+                             @Nonnull Revision to,
+                             @Nonnull String path,
+                             @Nullable Loader loader) {
+        // check local first without loader
+        String changes = localCache.getChanges(from, to, path, null);
+        if (changes != null) {
+            return changes;
+        }
+        return memoryCache.getChanges(from, to, path, loader);
+    }
+
+    /**
+     * Creates a new entry in the {@link LocalDiffCache} only!
+     *
+     * @param from the from revision.
+     * @param to the to revision.
+     * @return the new entry.
+     */
+    @Nonnull
+    @Override
+    public Entry newEntry(@Nonnull Revision from, @Nonnull Revision to) {
+        return localCache.newEntry(from, to);
+    }
+
+    @Nonnull
+    @Override
+    public Iterable<CacheStats> getStats() {
+        return Iterables.concat(localCache.getStats(), memoryCache.getStats());
+    }
+}
