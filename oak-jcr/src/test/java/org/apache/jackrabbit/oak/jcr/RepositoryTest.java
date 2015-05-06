fe7c4e84e4cf129d80ee1a14029cd34356d6f4f4
Michael Duerig <mduerig@apache.org>
2015-05-06 09:13:19 +0000

    OAK-2824: Backport OAK-2238 to branches Merged revision 1641346
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1677952 13f79535-47bb-0310-9956-ffa450edef68

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/RepositoryTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/RepositoryTest.java
@@ -20,6 +20,8 @@ package org.apache.jackrabbit.oak.jcr;
 
 import static java.util.Arrays.asList;
 import static javax.jcr.ImportUUIDBehavior.IMPORT_UUID_CREATE_NEW;
+import static javax.jcr.Repository.OPTION_NODE_AND_PROPERTY_WITH_SAME_NAME_SUPPORTED;
+import static org.apache.jackrabbit.JcrConstants.JCR_UUID;
 import static org.apache.jackrabbit.commons.JcrUtils.getChildNodes;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -641,6 +643,29 @@ public class RepositoryTest extends AbstractRepositoryTest {
         Node parentNode = getNode(TEST_PATH);
         addProperty(parentNode, "string", getAdminSession().getValueFactory().createValue("string value"));
     }
+
+    @Test
+    public void testGetItemReturnsNodeBeforeProperty() throws RepositoryException {
+        String snnpSupported = getRepository().getDescriptor(OPTION_NODE_AND_PROPERTY_WITH_SAME_NAME_SUPPORTED);
+        assumeTrue(Boolean.valueOf(snnpSupported));
+
+        String newNodeName = "getItemTest";
+        String nodeAndPropertyName = "subnode";
+
+        Session session = getAdminSession();
+        Node root = session.getRootNode();
+        assertFalse(root.hasNode(newNodeName));
+        Node newNode = root.addNode(newNodeName, NodeTypeConstants.NT_OAK_UNSTRUCTURED);
+
+        newNode.setProperty(nodeAndPropertyName, "prop value");
+        newNode.addNode(nodeAndPropertyName);
+        session.save();
+
+        Item item = session.getItem("/" + newNodeName + "/" + nodeAndPropertyName);
+
+        assertTrue("should retrieve Node before property", item.isNode());
+    }
+
 
     @Test
     public void addMultiValuedString() throws RepositoryException {
