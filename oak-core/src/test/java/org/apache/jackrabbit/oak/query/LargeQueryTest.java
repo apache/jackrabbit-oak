831049848303bc502ebd3a458a7d20e006caa5fa
Thomas Mueller <thomasm@apache.org>
2015-05-19 13:56:55 +0000

    OAK-2873 Performance problems with many "or" conditions
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1680294 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/query/LargeQueryTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/query/LargeQueryTest.java
@@ -21,12 +21,13 @@ import static org.junit.Assert.assertEquals;
 import java.text.ParseException;
 
 import org.apache.jackrabbit.oak.query.xpath.XPathToSQL2Converter;
+import org.h2.util.Profiler;
 import org.junit.Test;
 
 public class LargeQueryTest {
 
     @Test
-    public void test() throws ParseException {
+    public void testSimpleOr() throws ParseException {
         StringBuilder buff = new StringBuilder("//*[");
         StringBuilder buff2 = new StringBuilder(
                 "select [jcr:path], [jcr:score], * from [nt:base] as a where [x] in(");
@@ -46,4 +47,46 @@ public class LargeQueryTest {
         buff2.append(" /* xpath: ").append(xpath).append(" */");
         assertEquals(buff2.toString(), sql2);
     }
+    
+    @Test
+    public void testCombinedOr() throws ParseException {
+        StringBuilder buff = new StringBuilder("//*[");
+        StringBuilder buff2 = new StringBuilder(
+                "select [jcr:path], [jcr:score], * from [nt:base] as a where [x] in(");
+        int step = 111;
+        for (int i = 0; i < 5000; i++) {
+            if (i % step == 2) {
+                if (i > 0) {
+                    buff.append(" or ");
+                }
+                buff.append("@x>").append(i);
+                buff2.append(") union select [jcr:path], [jcr:score], * from [nt:base] as a " + 
+                        "where [x] > ").append(i);
+                buff2.append(" union select [jcr:path], [jcr:score], * from [nt:base] as a " + 
+                        "where [x] in(");
+            } else {
+                if (i > 0) {
+                    buff.append(" or ");
+                }
+                buff.append("@x=").append(i);
+                if (i > 0 && i % step != 3) {
+                    buff2.append(", ");
+                }
+                buff2.append(i);
+            }
+        }
+        buff.append("]");
+        buff2.append(")");
+        String xpath = buff.toString();
+        XPathToSQL2Converter conv = new XPathToSQL2Converter();
+        long start = System.currentTimeMillis();
+        Profiler prof = new Profiler().startCollecting();
+        String sql2 = conv.convert(xpath);
+        long time = System.currentTimeMillis() - start;
+        System.out.println("time: " + time);
+        System.out.println(prof.getTop(5));
+        buff2.append(" /* xpath: ").append(xpath).append(" */");
+        assertEquals(buff2.toString(), sql2);
+    }
+    
 }
