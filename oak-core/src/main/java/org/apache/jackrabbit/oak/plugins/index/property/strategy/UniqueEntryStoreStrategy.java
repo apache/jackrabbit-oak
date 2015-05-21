7c783cb5baabc52addb34809f28a231c62f7a56c
Thomas Mueller <thomasm@apache.org>
2015-05-21 07:17:42 +0000

    OAK-2663 Unique property index can trigger OOM during upgrade of large repository
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1680748 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/property/strategy/UniqueEntryStoreStrategy.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/property/strategy/UniqueEntryStoreStrategy.java
@@ -89,7 +89,9 @@ public class UniqueEntryStoreStrategy implements IndexStoreStrategy {
             PropertyState s = k.getProperty("entry");
             for (int i = 0; i < s.count(); i++) {
                 String r = s.getValue(Type.STRING, i);
-                list.add(r);
+                if (!list.contains(r)) {
+                    list.add(r);
+                }
             }
         }
         PropertyState s2 = MultiStringPropertyState.stringProperty("entry", list);
@@ -139,6 +141,11 @@ public class UniqueEntryStoreStrategy implements IndexStoreStrategy {
                 return list.iterator();
             }
         };
+    }
+
+    @Override
+    public boolean exists(NodeBuilder index, String key) {
+        return index.hasChildNode(key);
     }
 
     @Override
