7770095e1572b6b3b096aeb151c6eb959fb5589a
angela <anchela@adobe.com>
2015-06-09 09:05:31 +0200

    WIP

--- oak-exercise/src/test/java/org/apache/jackrabbit/oak/security/authentication/L4_UserIDTest.java
+++ oak-exercise/src/test/java/org/apache/jackrabbit/oak/security/authentication/L4_UserIDTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.oak.security.user;
+package org.apache.jackrabbit.oak.security.authentication;
 
 import java.util.Collections;
 import javax.jcr.Credentials;
