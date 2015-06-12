3d3707fad6be68bccf84511f8e868e7122c0871c
Julian Reschke <reschke@apache.org>
2015-06-12 12:11:12 +0000

    OAK-2979: JournalTest.cleanupTest - fix occasional test failures by adding a Thread.sleep
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1685075 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/JournalTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/JournalTest.java
@@ -188,6 +188,7 @@ public class JournalTest {
         ns1.renewClusterIdLease();
         JournalGarbageCollector gc = new JournalGarbageCollector(ns1);
         // first clean up
+        Thread.sleep(100); // OAK-2979 : wait 100ms before doing the cleanup
         gc.gc(1, TimeUnit.MILLISECONDS);
         Thread.sleep(100); // sleep just quickly
         assertEquals(0, gc.gc(1, TimeUnit.DAYS));
