cc11a367c89cb81a2030dc5b37256bd6b46d7022
Julian Reschke <reschke@apache.org>
2015-06-17 12:40:38 +0000

    OAK-1266: DataSourceWrapper for RDBDocumentStore tests: add logging of parameters and support for batches
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1686003 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBPreparedStatementWrapper.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBPreparedStatementWrapper.java
@@ -38,6 +38,8 @@ import java.sql.SQLWarning;
 import java.sql.SQLXML;
 import java.sql.Time;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
@@ -46,6 +48,7 @@ public class RDBPreparedStatementWrapper implements PreparedStatement {
     private final RDBDataSourceWrapper datasource;
     private final PreparedStatement statement;
     private ResultSet resultSet = null;
+    private List<Object> parameters = new ArrayList<Object>();
 
     public RDBPreparedStatementWrapper(RDBDataSourceWrapper datasource, PreparedStatement statement) {
         this.datasource = datasource;
@@ -53,7 +56,13 @@ public class RDBPreparedStatementWrapper implements PreparedStatement {
     }
 
     public void addBatch() throws SQLException {
+        long start = System.nanoTime();
         statement.addBatch();
+        List<RDBLogEntry> l = datasource.getLog();
+        if (l != null) {
+            l.add(new RDBLogEntry(start, "statement.addBatch() with parameters: " + parameters));
+        }
+        parameters.clear();
     }
 
     public void addBatch(String sql) throws SQLException {
@@ -70,6 +79,7 @@ public class RDBPreparedStatementWrapper implements PreparedStatement {
 
     public void clearParameters() throws SQLException {
         statement.clearParameters();
+        parameters.clear();
     }
 
     public void clearWarnings() throws SQLException {
@@ -110,7 +120,25 @@ public class RDBPreparedStatementWrapper implements PreparedStatement {
     }
 
     public int[] executeBatch() throws SQLException {
-        return statement.executeBatch();
+        long start = System.nanoTime();
+        int results[] = {};
+        SQLException x = null;
+        try {
+            results = statement.executeBatch();
+            return results;
+        } catch (SQLException ex) {
+            x = ex;
+            throw ex;
+        } finally {
+            List<RDBLogEntry> l = datasource.getLog();
+            if (l != null) {
+                String message = "statement.executeBatch() -> " + Arrays.toString(results);
+                if (x != null) {
+                    message += " -> " + x.getMessage();
+                }
+                l.add(new RDBLogEntry(start, message));
+            }
+        }
     }
 
     public ResultSet executeQuery() throws SQLException {
@@ -125,7 +153,7 @@ public class RDBPreparedStatementWrapper implements PreparedStatement {
         } finally {
             List<RDBLogEntry> l = datasource.getLog();
             if (l != null) {
-                String message = "statement.executeQuery()";
+                String message = "statement.executeQuery() with parameters: " + parameters;
                 if (x != null) {
                     message += " -> " + x.getMessage();
                 }
@@ -151,7 +179,7 @@ public class RDBPreparedStatementWrapper implements PreparedStatement {
         } finally {
             List<RDBLogEntry> l = datasource.getLog();
             if (l != null) {
-                String message = "statement.executeUpdate()";
+                String message = "statement.executeUpdate() with parameters: " + parameters;
                 if (result != null) {
                     message += " -> " + result;
                 }
@@ -267,82 +295,102 @@ public class RDBPreparedStatementWrapper implements PreparedStatement {
 
     public void setArray(int parameterIndex, Array x) throws SQLException {
         statement.setArray(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setAsciiStream(int parameterIndex, InputStream x, int length) throws SQLException {
         statement.setAsciiStream(parameterIndex, x, length);
+        record(parameterIndex, x);
     }
 
     public void setAsciiStream(int parameterIndex, InputStream x, long length) throws SQLException {
         statement.setAsciiStream(parameterIndex, x, length);
+        record(parameterIndex, x);
     }
 
     public void setAsciiStream(int parameterIndex, InputStream x) throws SQLException {
         statement.setAsciiStream(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setBigDecimal(int parameterIndex, BigDecimal x) throws SQLException {
         statement.setBigDecimal(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setBinaryStream(int parameterIndex, InputStream x, int length) throws SQLException {
         statement.setBinaryStream(parameterIndex, x, length);
+        record(parameterIndex, x);
     }
 
     public void setBinaryStream(int parameterIndex, InputStream x, long length) throws SQLException {
         statement.setBinaryStream(parameterIndex, x, length);
+        record(parameterIndex, x);
     }
 
     public void setBinaryStream(int parameterIndex, InputStream x) throws SQLException {
         statement.setBinaryStream(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setBlob(int parameterIndex, Blob x) throws SQLException {
         statement.setBlob(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setBlob(int parameterIndex, InputStream inputStream, long length) throws SQLException {
         statement.setBlob(parameterIndex, inputStream, length);
+        record(parameterIndex, inputStream);
     }
 
     public void setBlob(int parameterIndex, InputStream inputStream) throws SQLException {
         statement.setBlob(parameterIndex, inputStream);
+        record(parameterIndex, inputStream);
     }
 
     public void setBoolean(int parameterIndex, boolean x) throws SQLException {
         statement.setBoolean(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setByte(int parameterIndex, byte x) throws SQLException {
         statement.setByte(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setBytes(int parameterIndex, byte[] x) throws SQLException {
         statement.setBytes(parameterIndex, x);
+        record(parameterIndex, Arrays.asList(x));
     }
 
     public void setCharacterStream(int parameterIndex, Reader reader, int length) throws SQLException {
         statement.setCharacterStream(parameterIndex, reader, length);
+        record(parameterIndex, reader);
     }
 
     public void setCharacterStream(int parameterIndex, Reader reader, long length) throws SQLException {
         statement.setCharacterStream(parameterIndex, reader, length);
+        record(parameterIndex, reader);
     }
 
     public void setCharacterStream(int parameterIndex, Reader reader) throws SQLException {
         statement.setCharacterStream(parameterIndex, reader);
+        record(parameterIndex, reader);
     }
 
     public void setClob(int parameterIndex, Clob x) throws SQLException {
         statement.setClob(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setClob(int parameterIndex, Reader reader, long length) throws SQLException {
         statement.setClob(parameterIndex, reader, length);
+        record(parameterIndex, reader);
     }
 
     public void setClob(int parameterIndex, Reader reader) throws SQLException {
         statement.setClob(parameterIndex, reader);
+        record(parameterIndex, reader);
     }
 
     public void setCursorName(String name) throws SQLException {
@@ -351,14 +399,17 @@ public class RDBPreparedStatementWrapper implements PreparedStatement {
 
     public void setDate(int parameterIndex, Date x, Calendar cal) throws SQLException {
         statement.setDate(parameterIndex, x, cal);
+        record(parameterIndex, x);
     }
 
     public void setDate(int parameterIndex, Date x) throws SQLException {
         statement.setDate(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setDouble(int parameterIndex, double x) throws SQLException {
         statement.setDouble(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setEscapeProcessing(boolean enable) throws SQLException {
@@ -375,14 +426,17 @@ public class RDBPreparedStatementWrapper implements PreparedStatement {
 
     public void setFloat(int parameterIndex, float x) throws SQLException {
         statement.setFloat(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setInt(int parameterIndex, int x) throws SQLException {
         statement.setInt(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setLong(int parameterIndex, long x) throws SQLException {
         statement.setLong(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setMaxFieldSize(int max) throws SQLException {
@@ -395,46 +449,57 @@ public class RDBPreparedStatementWrapper implements PreparedStatement {
 
     public void setNCharacterStream(int parameterIndex, Reader value, long length) throws SQLException {
         statement.setNCharacterStream(parameterIndex, value, length);
+        record(parameterIndex, value);
     }
 
     public void setNCharacterStream(int parameterIndex, Reader value) throws SQLException {
         statement.setNCharacterStream(parameterIndex, value);
+        record(parameterIndex, value);
     }
 
     public void setNClob(int parameterIndex, NClob value) throws SQLException {
         statement.setNClob(parameterIndex, value);
+        record(parameterIndex, value);
     }
 
     public void setNClob(int parameterIndex, Reader reader, long length) throws SQLException {
         statement.setNClob(parameterIndex, reader, length);
+        record(parameterIndex, reader);
     }
 
     public void setNClob(int parameterIndex, Reader reader) throws SQLException {
         statement.setNClob(parameterIndex, reader);
+        record(parameterIndex, reader);
     }
 
     public void setNString(int parameterIndex, String value) throws SQLException {
         statement.setNString(parameterIndex, value);
+        record(parameterIndex, value);
     }
 
     public void setNull(int parameterIndex, int sqlType, String typeName) throws SQLException {
         statement.setNull(parameterIndex, sqlType, typeName);
+        record(parameterIndex, "(null)");
     }
 
     public void setNull(int parameterIndex, int sqlType) throws SQLException {
         statement.setNull(parameterIndex, sqlType);
+        record(parameterIndex, "(null)");
     }
 
     public void setObject(int parameterIndex, Object x, int targetSqlType, int scaleOrLength) throws SQLException {
         statement.setObject(parameterIndex, x, targetSqlType, scaleOrLength);
+        record(parameterIndex, x);
     }
 
     public void setObject(int parameterIndex, Object x, int targetSqlType) throws SQLException {
         statement.setObject(parameterIndex, x, targetSqlType);
+        record(parameterIndex, x);
     }
 
     public void setObject(int parameterIndex, Object x) throws SQLException {
         statement.setObject(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setPoolable(boolean poolable) throws SQLException {
@@ -447,6 +512,7 @@ public class RDBPreparedStatementWrapper implements PreparedStatement {
 
     public void setRef(int parameterIndex, Ref x) throws SQLException {
         statement.setRef(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setRowId(int parameterIndex, RowId x) throws SQLException {
@@ -455,41 +521,60 @@ public class RDBPreparedStatementWrapper implements PreparedStatement {
 
     public void setSQLXML(int parameterIndex, SQLXML xmlObject) throws SQLException {
         statement.setSQLXML(parameterIndex, xmlObject);
+        record(parameterIndex, xmlObject);
     }
 
     public void setShort(int parameterIndex, short x) throws SQLException {
         statement.setShort(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setString(int parameterIndex, String x) throws SQLException {
         statement.setString(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setTime(int parameterIndex, Time x, Calendar cal) throws SQLException {
         statement.setTime(parameterIndex, x, cal);
+        record(parameterIndex, x);
     }
 
     public void setTime(int parameterIndex, Time x) throws SQLException {
         statement.setTime(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setTimestamp(int parameterIndex, Timestamp x, Calendar cal) throws SQLException {
         statement.setTimestamp(parameterIndex, x, cal);
+        record(parameterIndex, x);
     }
 
     public void setTimestamp(int parameterIndex, Timestamp x) throws SQLException {
         statement.setTimestamp(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setURL(int parameterIndex, URL x) throws SQLException {
         statement.setURL(parameterIndex, x);
+        record(parameterIndex, x);
     }
 
     public void setUnicodeStream(int parameterIndex, InputStream x, int length) throws SQLException {
         statement.setUnicodeStream(parameterIndex, x, length);
+        record(parameterIndex, x);
     }
 
     public <T> T unwrap(Class<T> iface) throws SQLException {
         return statement.unwrap(iface);
+    }
+
+    private void record(int index, Object x) {
+        List<RDBLogEntry> l = datasource.getLog();
+        if (l != null) {
+            while (parameters.size() < index) {
+                parameters.add("UNSET");
+            }
+            parameters.set(index - 1, x);
+        }
     }
 }
