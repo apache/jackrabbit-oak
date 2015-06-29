9966212d1768215a6bc22825fc2d94ac7cfa2c77
Chetan Mehrotra <chetanm@apache.org>
2015-06-29 06:03:47 +0000

    OAK-2401 - SegmentNodeStoreService prone to deadlocks
    reduce the scope of synchronized code block
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1688090 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/segment/SegmentNodeStoreService.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/segment/SegmentNodeStoreService.java
@@ -229,7 +229,7 @@ public class SegmentNodeStoreService extends ProxyNodeStore
 
     private FileStore store;
 
-    private SegmentNodeStore delegate;
+    private volatile SegmentNodeStore delegate;
 
     private ObserverTracker observerTracker;
 
@@ -265,7 +265,7 @@ public class SegmentNodeStoreService extends ProxyNodeStore
     public static final String PROP_BLOB_GC_MAX_AGE = "blobGcMaxAgeInSecs";
 
     @Override
-    protected synchronized SegmentNodeStore getNodeStore() {
+    protected SegmentNodeStore getNodeStore() {
         checkState(delegate != null, "service must be activated when used");
         return delegate;
     }
@@ -445,19 +445,22 @@ public class SegmentNodeStoreService extends ProxyNodeStore
     }
 
     @Deactivate
-    public synchronized void deactivate() {
+    public void deactivate() {
         unregisterNodeStore();
 
-        if (observerTracker != null) {
-            observerTracker.stop();
-        }
-        if (gcMonitor != null) {
-            gcMonitor.stop();
-        }
-        delegate = null;
-        if (store != null) {
-            store.close();
-            store = null;
+        synchronized (this) {
+            if (observerTracker != null) {
+                observerTracker.stop();
+            }
+            if (gcMonitor != null) {
+                gcMonitor.stop();
+            }
+            delegate = null;
+
+            if (store != null) {
+                store.close();
+                store = null;
+            }
         }
     }
 
