fd6902d709947e090d3597899e2d345d639356e4
Chetan Mehrotra <chetanm@apache.org>
2015-07-14 09:48:58 +0000

    OAK-2892 - Speed up lucene indexing post migration by pre extracting the text content from binaries
    Merging 1690247,1690635,1690674
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.2@1690888 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/fulltext/PreExtractedTextProvider.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.jackrabbit.oak.plugins.index.fulltext;
+
+import java.io.IOException;
+
+import javax.annotation.CheckForNull;
+
+import aQute.bnd.annotation.ConsumerType;
+import org.apache.jackrabbit.oak.api.Blob;
+
+@ConsumerType
+public interface PreExtractedTextProvider {
+
+    /**
+     * Get pre extracted text for given blob at given path
+     *
+     * @param propertyPath path of the binary property
+     * @param blob binary property value
+     *
+     * @return pre extracted text or null if no
+     * pre extracted text found for given blob
+     */
+    @CheckForNull
+    ExtractedText getText(String propertyPath, Blob blob) throws IOException;
+}
