0f1db0e0797343be0c7b1299ad975de43fd10540
Angela Schreiber <angela@apache.org>
2015-07-14 13:49:05 +0000

    minor improvement: remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1690933 13f79535-47bb-0310-9956-ffa450edef68

--- oak-auth-ldap/src/test/java/org/apache/jackrabbit/oak/security/authentication/ldap/LdapLoginTestBase.java
+++ oak-auth-ldap/src/test/java/org/apache/jackrabbit/oak/security/authentication/ldap/LdapLoginTestBase.java
@@ -46,7 +46,6 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
