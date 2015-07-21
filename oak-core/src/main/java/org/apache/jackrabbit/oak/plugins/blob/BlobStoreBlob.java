958edcd21291eebbf71c177e0c2282e5e1de795c
Manfred Baedke <baedke@apache.org>
2015-07-21 17:28:14 +0000

    OAK-3131: DocumentNodeStore doesn't serialize blobs that have been created in a different store
    BlobSerializer will now also check the origin store.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1692177 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/blob/BlobStoreBlob.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/blob/BlobStoreBlob.java
@@ -74,6 +74,10 @@ public class BlobStoreBlob implements Blob {
         return blobId;
     }
 
+    public BlobStore getBlobStore() {
+        return blobStore;
+    }
+
     //------------------------------------------------------------< Object >--
 
     @Override
