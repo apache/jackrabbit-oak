b81508e43e366654306887f68163f40fbd64603b
Chetan Mehrotra <chetanm@apache.org>
2015-07-28 08:09:41 +0000

    OAK-3146 - ExternalLoginModuleFactory should inject SyncManager and ExternalIdentityProviderManager
    Reverting 1692998
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1693021 13f79535-47bb-0310-9956-ffa450edef68

--- oak-auth-external/src/main/java/org/apache/jackrabbit/oak/spi/security/authentication/external/impl/ExternalLoginModule.java
+++ oak-auth-external/src/main/java/org/apache/jackrabbit/oak/spi/security/authentication/external/impl/ExternalLoginModule.java
@@ -76,10 +76,6 @@ public class ExternalLoginModule extends AbstractLoginModule {
      */
     public static final String PARAM_SYNC_HANDLER_NAME = "sync.handlerName";
 
-    private ExternalIdentityProviderManager idpManager;
-
-    private SyncManager syncManager;
-
     /**
      * internal configuration when invoked from a factory rather than jaas
      */
@@ -140,13 +136,11 @@ public class ExternalLoginModule extends AbstractLoginModule {
         if (idpName.isEmpty()) {
             log.error("External login module needs IPD name. Will not be used for login.");
         } else {
-            if (idpManager == null) {
-                idpManager = WhiteboardUtils.getService(whiteboard, ExternalIdentityProviderManager.class);
-            }
-            if (idpManager == null) {
+            ExternalIdentityProviderManager idpMgr = WhiteboardUtils.getService(whiteboard, ExternalIdentityProviderManager.class);
+            if (idpMgr == null) {
                 log.error("External login module needs IDPManager. Will not be used for login.");
             } else {
-                idp = idpManager.getProvider(idpName);
+                idp = idpMgr.getProvider(idpName);
                 if (idp == null) {
                     log.error("No IDP found with name {}. Will not be used for login.", idpName);
                 }
@@ -157,13 +151,11 @@ public class ExternalLoginModule extends AbstractLoginModule {
         if (syncHandlerName.isEmpty()) {
             log.error("External login module needs SyncHandler name. Will not be used for login.");
         } else {
-            if (syncManager == null) {
-                syncManager = WhiteboardUtils.getService(whiteboard, SyncManager.class);
-            }
-            if (syncManager == null) {
+            SyncManager syncMgr = WhiteboardUtils.getService(whiteboard, SyncManager.class);
+            if (syncMgr == null) {
                 log.error("External login module needs SyncManager. Will not be used for login.");
             } else {
-                syncHandler = syncManager.getSyncHandler(syncHandlerName);
+                syncHandler = syncMgr.getSyncHandler(syncHandlerName);
                 if (syncHandler == null) {
                     log.error("No SyncHandler found with name {}. Will not be used for login.", syncHandlerName);
                 }
@@ -412,14 +404,5 @@ public class ExternalLoginModule extends AbstractLoginModule {
         // TODO: maybe delegate getSupportedCredentials to IDP
         Class scClass = SimpleCredentials.class;
         return Collections.singleton(scClass);
-    }
-
-
-    public void setSyncManager(SyncManager syncManager) {
-        this.syncManager = syncManager;
-    }
-
-    public void setIdpManager(ExternalIdentityProviderManager idpManager) {
-        this.idpManager = idpManager;
     }
 }
