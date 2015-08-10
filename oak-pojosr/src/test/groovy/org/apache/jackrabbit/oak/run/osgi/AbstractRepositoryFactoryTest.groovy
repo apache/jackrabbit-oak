42a14ff5254bda548ed274096253fec39d57887e
Chetan Mehrotra <chetanm@apache.org>
2015-08-10 11:49:53 +0000

    OAK-3202 - Restrict the bundles pickedup by PojoSR while running the testcase
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1695033 13f79535-47bb-0310-9956-ffa450edef68

--- oak-pojosr/src/test/groovy/org/apache/jackrabbit/oak/run/osgi/AbstractRepositoryFactoryTest.groovy
+++ oak-pojosr/src/test/groovy/org/apache/jackrabbit/oak/run/osgi/AbstractRepositoryFactoryTest.groovy
@@ -35,6 +35,7 @@ import java.util.concurrent.TimeUnit
 
 import static org.apache.jackrabbit.oak.run.osgi.OakOSGiRepositoryFactory.REPOSITORY_HOME
 import static org.apache.jackrabbit.oak.run.osgi.OakOSGiRepositoryFactory.REPOSITORY_STARTUP_TIMEOUT
+import static org.apache.jackrabbit.oak.run.osgi.OakOSGiRepositoryFactory.REPOSITORY_BUNDLE_FILTER
 
 abstract class AbstractRepositoryFactoryTest{
     static final int SVC_WAIT_TIME = Integer.getInteger("pojosr.waitTime", 10)
@@ -51,7 +52,14 @@ abstract class AbstractRepositoryFactoryTest{
         workDir = tmpFolder.getRoot();
         config = [
                 (REPOSITORY_HOME): workDir.absolutePath,
-                (REPOSITORY_STARTUP_TIMEOUT) : 2
+                (REPOSITORY_STARTUP_TIMEOUT) : 2,
+                (REPOSITORY_BUNDLE_FILTER) : '''
+                            (|
+                            (Bundle-SymbolicName=org.apache.jackrabbit*)
+                            (Bundle-SymbolicName=org.apache.sling*)
+                            (Bundle-SymbolicName=org.apache.felix*)
+                            (Bundle-SymbolicName=org.apache.aries*)
+                            )''',
         ]
     }
 
