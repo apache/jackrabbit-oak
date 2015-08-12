2ec6635f37c05138310380503fc26f4cd7d45905
Alexandru Parvulescu <alexparvulescu@apache.org>
2015-08-12 14:15:35 +0000

    OAK-2545 SegmentMk IT tests are too intensive  - merged r1695521
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.2@1695531 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/segment/file/LargeNumberOfPropertiesTestIT.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/segment/file/LargeNumberOfPropertiesTestIT.java
@@ -18,31 +18,47 @@
  */
 package org.apache.jackrabbit.oak.plugins.segment.file;
 
+import static org.apache.commons.io.FileUtils.deleteDirectory;
+import static org.apache.jackrabbit.oak.plugins.segment.file.FileStore.newFileStore;
+import static org.junit.Assume.assumeTrue;
+
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.jackrabbit.oak.plugins.segment.SegmentNodeStore;
 import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
 import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
 import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * <p>Tests verifying if the repository gets corrupted or not: {@code OAK-2481 IllegalStateException in TarMk with large number of properties}</p>
+ *
+ * <p>These tests are disabled by default due to their long running time. On the
+ * command line specify {@code -DLargeNumberOfPropertiesTestIT=true} to enable
+ * them.</p>
+ *
+ *<p>If you only want to run this test:<br>
+ * {@code mvn verify -Dsurefire.skip.ut=true -PintegrationTesting -Dit.test=LargeNumberOfPropertiesTestIT -DLargeNumberOfPropertiesTestIT=true}
+ * </p>
+ */
 public class LargeNumberOfPropertiesTestIT {
 
     private static final Logger LOG = LoggerFactory
-            .getLogger(SegmentReferenceLimitTestIT.class);
+            .getLogger(LargeNumberOfPropertiesTestIT.class);
+    private static final boolean ENABLED = Boolean
+            .getBoolean(LargeNumberOfPropertiesTestIT.class.getSimpleName());
 
     private File directory;
 
     @Before
     public void setUp() throws IOException {
-        directory = File.createTempFile("LargeNumberOfPropertiesTestIT", "dir",
+        assumeTrue(ENABLED);
+        directory = File.createTempFile(getClass().getSimpleName(), "dir",
                 new File("target"));
         directory.delete();
         directory.mkdir();
@@ -51,24 +67,18 @@ public class LargeNumberOfPropertiesTestIT {
     @After
     public void cleanDir() {
         try {
-            FileUtils.deleteDirectory(directory);
+            if (directory != null) {
+                deleteDirectory(directory);
+            }
         } catch (IOException e) {
             LOG.error("Error cleaning directory", e);
         }
     }
 
-    /**
-     * OAK-2481 IllegalStateException in TarMk with large number of properties
-     *
-     * TODO Test is currently ignored because of how memory intensive it is
-     *
-     * @see <a
-     *      href="https://issues.apache.org/jira/browse/OAK-2481">OAK-2481</a>
-     */
     @Test
-    @Ignore
     public void corruption() throws Exception {
-        FileStore fileStore = new FileStore(directory, 5, 0, false);
+        FileStore fileStore = newFileStore(directory).withMaxFileSize(5)
+                .withNoCache().withMemoryMapping(true).create();
         SegmentNodeStore nodeStore = new SegmentNodeStore(fileStore);
 
         NodeBuilder root = nodeStore.getRoot().builder();
@@ -78,7 +88,7 @@ public class LargeNumberOfPropertiesTestIT {
             // i=26 hits the hard limit for the number of properties a node can
             // have (262144)
             for (int i = 0; i < 25; i++) {
-                System.out.println(i);
+                LOG.debug("run {}/24", i);
                 for (int j = 0; j < 10000; j++) {
                     c.setProperty("int-" + i + "-" + j, i);
                 }
