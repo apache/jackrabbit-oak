34c81afb5f0a1dfcdac91fd2f98b6d9969dc915c
Marcel Reutegger <mreutegg@apache.org>
2015-08-25 08:11:05 +0000

    OAK-3282: DataStoreBlobStore cache ignores key for memory calculation
    Merge revision 1697589 from trunk
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1697598 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/blob/datastore/DataStoreBlobStore.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/blob/datastore/DataStoreBlobStore.java
@@ -31,6 +31,7 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.jcr.RepositoryException;
 
@@ -49,6 +50,7 @@ import org.apache.jackrabbit.core.data.DataStore;
 import org.apache.jackrabbit.core.data.DataStoreException;
 import org.apache.jackrabbit.core.data.MultiDataStoreAware;
 import org.apache.jackrabbit.oak.cache.CacheLIRS;
+import org.apache.jackrabbit.oak.commons.StringUtils;
 import org.apache.jackrabbit.oak.spi.blob.BlobStore;
 import org.apache.jackrabbit.oak.spi.blob.GarbageCollectableBlobStore;
 import org.slf4j.Logger;
@@ -107,8 +109,8 @@ public class DataStoreBlobStore implements DataStore, BlobStore, GarbageCollecta
                 .maximumWeight(cacheSizeInMB * FileUtils.ONE_MB)
                 .weigher(new Weigher<String, byte[]>() {
                     @Override
-                    public int weigh(String key, byte[] value) {
-                        return value.length;
+                    public int weigh(@Nonnull String key, @Nonnull byte[] value) {
+                        return StringUtils.estimateMemoryUsage(key) + value.length;
                     }
                 })
                 .build();
