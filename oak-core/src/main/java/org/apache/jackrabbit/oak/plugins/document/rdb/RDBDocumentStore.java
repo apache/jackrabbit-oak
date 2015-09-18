41423245239cbafbc8a516ff56cdb946640e874d
Julian Reschke <reschke@apache.org>
2015-09-18 13:59:35 +0000

    OAK-3422: RDBDocumentStore: improve index diagnostics
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1703858 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBDocumentStore.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBDocumentStore.java
@@ -616,7 +616,7 @@ public class RDBDocumentStore implements DocumentStore {
         }
     }
 
-    private String asQualifiedDbName(String one, String two) {
+    private static String asQualifiedDbName(String one, String two) {
         if (one == null && two == null) {
             return null;
         }
@@ -624,6 +624,21 @@ public class RDBDocumentStore implements DocumentStore {
             one = one == null ? "" : one.trim();
             two = two == null ? "" : two.trim();
             return one.isEmpty() ? two : one + "." + two;
+        }
+    }
+
+    private static String indexTypeAsString(int type) {
+        switch (type) {
+            case DatabaseMetaData.tableIndexClustered:
+                return "clustered";
+            case DatabaseMetaData.tableIndexHashed:
+                return "hashed";
+            case DatabaseMetaData.tableIndexStatistic:
+                return "statistic";
+            case DatabaseMetaData.tableIndexOther:
+                return "other";
+            default:
+                return "indexType=" + type;
         }
     }
 
@@ -654,7 +669,7 @@ public class RDBDocumentStore implements DocumentStore {
                         info.put("fields", new TreeMap<Integer, String>());
                     }
                     info.put("nonunique", rs.getBoolean(4));
-                    info.put("type", rs.getInt(7));
+                    info.put("type", indexTypeAsString(rs.getInt(7)));
                     String inSchema = rs.getString(2);
                     inSchema = inSchema == null ? "" : inSchema.trim();
                     // skip indices on tables in other schemas in case we have that information
@@ -685,6 +700,7 @@ public class RDBDocumentStore implements DocumentStore {
                         sb.append(field);
                     }
                     sb.append(")");
+                    sb.append(" ").append(index.getValue().get("type"));
                 }
             }
             if (sb.length() != 0) {
