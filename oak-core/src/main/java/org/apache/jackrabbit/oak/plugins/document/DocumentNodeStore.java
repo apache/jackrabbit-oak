44b091980393b3ae56e3751847ba9fb30beeb4ff
Marcel Reutegger <mreutegg@apache.org>
2015-09-23 08:41:12 +0000

    OAK-3433: Background update may create journal entry with incorrect id
    Merged revisions 1704629,1704655 from trunk
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.2@1704781 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStore.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStore.java
@@ -1626,7 +1626,7 @@ public final class DocumentNodeStore
         runBackgroundReadOperations();
     }
 
-    private void runBackgroundUpdateOperations() {
+    void runBackgroundUpdateOperations() {
         if (isDisposed.get()) {
             return;
         }
@@ -1667,7 +1667,7 @@ public final class DocumentNodeStore
 
     //----------------------< background read operations >----------------------
 
-    private void runBackgroundReadOperations() {
+    void runBackgroundReadOperations() {
         if (isDisposed.get()) {
             return;
         }
@@ -2007,9 +2007,9 @@ public final class DocumentNodeStore
     BackgroundWriteStats backgroundWrite() {
         return unsavedLastRevisions.persist(this, new UnsavedModifications.Snapshot() {
             @Override
-            public void acquiring() {
+            public void acquiring(Revision mostRecent) {
                 if (store.create(JOURNAL,
-                        singletonList(changes.asUpdateOp(getHeadRevision())))) {
+                        singletonList(changes.asUpdateOp(mostRecent)))) {
                     changes = JOURNAL.newDocument(getDocumentStore());
                 }
             }
