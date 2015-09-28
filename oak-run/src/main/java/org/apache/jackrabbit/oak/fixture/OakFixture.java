87438e3be691c56ca38a52f6c5eef39e5953e460
Julian Reschke <reschke@apache.org>
2015-09-28 13:34:40 +0000

    OAK-3417: oak-run OakFixture fix incorrect clusterIDs (ported to 1.2)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.2@1705691 13f79535-47bb-0310-9956-ffa450edef68

--- oak-run/src/main/java/org/apache/jackrabbit/oak/fixture/OakFixture.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/fixture/OakFixture.java
@@ -195,7 +195,7 @@ public abstract class OakFixture {
                             setMongoDB(mongo.getDB()).
                             memoryCacheSize(cacheSize).
                             setPersistentCache("target/persistentCache,time").
-                            setClusterId(i).
+                            setClusterId(i + 1).
                             setLogging(false);
                     setupBlobStore(mkBuilder);
                     kernels[i] = mkBuilder.open();
@@ -291,7 +291,7 @@ public abstract class OakFixture {
                     DataSource ds = RDBDataSourceFactory.forJdbcUrl(jdbcuri, jdbcuser, jdbcpasswd);
                     DocumentMK.Builder mkBuilder = new DocumentMK.Builder()
                             .setRDBConnection(ds, getOptions(dropDBAfterTest, tablePrefix)).memoryCacheSize(cacheSize)
-                            .setClusterId(i).setLogging(false);
+                            .setClusterId(i + 1).setLogging(false);
                     if (blobStore != null) {
                         mkBuilder.setBlobStore(blobStore);
                     }
