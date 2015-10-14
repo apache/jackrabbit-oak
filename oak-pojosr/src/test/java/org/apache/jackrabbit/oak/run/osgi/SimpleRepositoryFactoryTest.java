78abeed95d94f329c5754c623a08a5a1e09e0b8d
Chetan Mehrotra <chetanm@apache.org>
2015-10-14 11:08:33 +0000

    OAK-2714 - Test failures on Jenkins
    Merging 1696522
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.2@1708582 13f79535-47bb-0310-9956-ffa450edef68

--- oak-pojosr/src/test/java/org/apache/jackrabbit/oak/run/osgi/SimpleRepositoryFactoryTest.java
+++ oak-pojosr/src/test/java/org/apache/jackrabbit/oak/run/osgi/SimpleRepositoryFactoryTest.java
@@ -28,6 +28,7 @@ import javax.jcr.Repository;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.jackrabbit.api.JackrabbitRepository;
 import org.apache.jackrabbit.commons.JcrUtils;
 import org.junit.Before;
 import org.junit.Test;
@@ -55,6 +56,7 @@ public class SimpleRepositoryFactoryTest {
 
         Repository repository = JcrUtils.getRepository(config);
         assertNotNull(repository);
+        ((JackrabbitRepository)repository).shutdown();
     }
 
 
