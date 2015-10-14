a3f21d465666f0e66d06f8ba7a568c0561deb6c0
Chetan Mehrotra <chetanm@apache.org>
2015-10-14 05:56:40 +0000

    OAK-3513 - Session save going through despite repository being shutdown causing reindex flag to reset
    Merging 1708546
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1708547 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/Oak.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/Oak.java
@@ -22,6 +22,7 @@ import static org.apache.jackrabbit.oak.spi.whiteboard.WhiteboardUtils.registerM
 import static org.apache.jackrabbit.oak.spi.whiteboard.WhiteboardUtils.registerObserver;
 import static org.apache.jackrabbit.oak.spi.whiteboard.WhiteboardUtils.scheduleWithFixedDelay;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -50,6 +51,7 @@ import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.io.Closer;
 
+import org.apache.jackrabbit.oak.api.CommitFailedException;
 import org.apache.jackrabbit.oak.api.ContentRepository;
 import org.apache.jackrabbit.oak.api.ContentSession;
 import org.apache.jackrabbit.oak.api.Root;
@@ -495,6 +497,7 @@ public class Oak {
     }
 
     public ContentRepository createContentRepository() {
+        final RepoStateCheckHook repoStateCheckHook = new RepoStateCheckHook();
         final List<Registration> regs = Lists.newArrayList();
         regs.add(whiteboard.register(Executor.class, executor, Collections.emptyMap()));
 
@@ -503,6 +506,7 @@ public class Oak {
 
         QueryIndexProvider indexProvider = CompositeQueryIndexProvider.compose(queryIndexProviders);
 
+        commitHooks.add(repoStateCheckHook);
         List<CommitHook> initHooks = new ArrayList<CommitHook>(commitHooks);
         initHooks.add(new EditorHook(CompositeEditorProvider
                 .compose(editorProviders)));
@@ -565,6 +569,7 @@ public class Oak {
             @Override
             public void close() throws IOException {
                 super.close();
+                repoStateCheckHook.close();
                 new CompositeRegistration(regs).unregister();
             }
         };
@@ -616,6 +621,29 @@ public class Oak {
      */
     public Root createRoot() {
         return createContentSession().getLatestRoot();
+    }
+
+    /**
+     * CommitHook to ensure that commit only go through till repository is not
+     * closed. Once repository is closed the commits would be failed
+     */
+    private static class RepoStateCheckHook implements CommitHook, Closeable {
+        private volatile boolean closed;
+
+        @Nonnull
+        @Override
+        public NodeState processCommit(NodeState before, NodeState after, CommitInfo info) throws CommitFailedException {
+            if (closed){
+                throw new CommitFailedException(
+                        CommitFailedException.OAK, 2, "ContentRepository closed");
+            }
+            return after;
+        }
+
+        @Override
+        public void close() throws IOException {
+            this.closed = true;
+        }
     }
 
 }
