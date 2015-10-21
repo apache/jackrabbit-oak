5aa10a158e20dc4b156d802bbed5cadd0dd50eb0
Alexandru Parvulescu <alexparvulescu@apache.org>
2015-10-21 10:17:43 +0000

    OAK-3531 Oak Explorer: add segment GC roots report
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1709789 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/plugins/segment/FileStoreHelper.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.jackrabbit.oak.plugins.segment;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
+import static java.util.Collections.reverseOrder;
+import static java.util.Collections.sort;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.UUID;
+
+import org.apache.jackrabbit.oak.plugins.segment.file.FileStore;
+
+public class FileStoreHelper {
+
+    public final static String newline = "\n";
+
+    private FileStoreHelper() {
+    }
+
+    public static List<String> getTarFiles(FileStore store) {
+        List<String> files = newArrayList();
+        for (String p : store.getTarReaderIndex().keySet()) {
+            files.add(new File(p).getName());
+        }
+        sort(files, reverseOrder());
+        return files;
+    }
+
+    public static void getGcRoots(FileStore store, UUID uuidIn,
+            Map<UUID, Set<Entry<UUID, String>>> links) throws IOException {
+        Deque<UUID> todos = new ArrayDeque<UUID>();
+        todos.add(uuidIn);
+        while (!todos.isEmpty()) {
+            UUID uuid = todos.remove();
+            for (String f : getTarFiles(store)) {
+                Map<UUID, List<UUID>> graph = store.getTarGraph(f);
+                for (Entry<UUID, List<UUID>> g : graph.entrySet()) {
+                    if (g.getValue() != null && g.getValue().contains(uuid)) {
+                        UUID uuidP = g.getKey();
+                        if (!todos.contains(uuidP)) {
+                            todos.add(uuidP);
+                            Set<Entry<UUID, String>> deps = links.get(uuid);
+                            if (deps == null) {
+                                deps = newHashSet();
+                                links.put(uuid, deps);
+                            }
+                            deps.add(new SimpleImmutableEntry<UUID, String>(
+                                    uuidP, f));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void printGcRoots(StringBuilder sb,
+            Map<UUID, Set<Entry<UUID, String>>> links, UUID uuid, String space,
+            String inc) {
+        Set<Entry<UUID, String>> roots = links.remove(uuid);
+        if (roots == null || roots.isEmpty()) {
+            return;
+        }
+        // TODO is sorting by file name needed?
+        for (Entry<UUID, String> r : roots) {
+            sb.append(space + r.getKey() + "[" + r.getValue() + "]");
+            sb.append(newline);
+            printGcRoots(sb, links, r.getKey(), space + inc, inc);
+        }
+    }
+
+}
