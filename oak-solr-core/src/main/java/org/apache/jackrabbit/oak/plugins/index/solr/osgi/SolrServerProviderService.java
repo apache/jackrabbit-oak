2b106bb5a96eb52bdece7e56fdab2b9ae748b5d8
Tommaso Teofili <tommaso@apache.org>
2015-11-16 10:41:33 +0000

    OAK-3639 - fixed "updated" configuration provider method name
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1714542 13f79535-47bb-0310-9956-ffa450edef68

--- oak-solr-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/solr/osgi/SolrServerProviderService.java
+++ oak-solr-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/solr/osgi/SolrServerProviderService.java
@@ -49,7 +49,7 @@ import org.slf4j.LoggerFactory;
                 policy = ReferencePolicy.DYNAMIC,
                 bind = "bindSolrServerConfigurationProvider",
                 unbind = "unbindSolrServerConfigurationProvider",
-                updated = "updateSolrServerConfigurationProvider"
+                updated = "updatedSolrServerConfigurationProvider"
         )
 })
 @Service(SolrServerProvider.class)
