779e3518bd5b569aed656020d8229828fbb0ba51
Julian Reschke <reschke@apache.org>
2015-12-03 16:09:55 +0000

    OAK-3722: RDBDataSourceFactory: restore ability to close the connection pool (ported to 1.2)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.2@1717794 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBDataSourceFactory.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBDataSourceFactory.java
@@ -16,9 +16,17 @@
  */
 package org.apache.jackrabbit.oak.plugins.document.rdb;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
 
 import javax.sql.DataSource;
 
@@ -64,14 +72,13 @@ public class RDBDataSourceFactory {
             String classname = "org.apache.tomcat.jdbc.pool.DataSource";
             try {
                 Class<?> dsclazz = Class.forName(classname);
-                DataSource ds = (DataSource)dsclazz.newInstance();
-                dsclazz.getMethod("setDriverClassName",  String.class).invoke(ds, d.getClass().getName());
-                dsclazz.getMethod("setUsername",  String.class).invoke(ds, username);
-                dsclazz.getMethod("setPassword",  String.class).invoke(ds, passwd);
-                dsclazz.getMethod("setUrl",  String.class).invoke(ds, url);
-                return ds;
-            }
-            catch (Exception ex) {
+                DataSource ds = (DataSource) dsclazz.newInstance();
+                dsclazz.getMethod("setDriverClassName", String.class).invoke(ds, d.getClass().getName());
+                dsclazz.getMethod("setUsername", String.class).invoke(ds, username);
+                dsclazz.getMethod("setPassword", String.class).invoke(ds, passwd);
+                dsclazz.getMethod("setUrl", String.class).invoke(ds, url);
+                return new CloseableDataSource(ds);
+            } catch (Exception ex) {
                 String message = "trying to create datasource " + classname;
                 LOG.info(message, ex);
                 throw new DocumentStoreException(message, ex);
@@ -85,5 +92,86 @@ public class RDBDataSourceFactory {
 
     public static DataSource forJdbcUrl(String url, String username, String passwd) {
         return forJdbcUrl(url, username, passwd, null);
+    }
+
+    /**
+     * A {@link Closeable} {@link DataSource} based on a generic {@link Source}
+     * .
+     */
+    private static class CloseableDataSource implements DataSource, Closeable {
+
+        private DataSource ds;
+
+        public CloseableDataSource(DataSource ds) {
+            this.ds = ds;
+        }
+
+        @Override
+        public PrintWriter getLogWriter() throws SQLException {
+            return this.ds.getLogWriter();
+        }
+
+        @Override
+        public int getLoginTimeout() throws SQLException {
+            return this.ds.getLoginTimeout();
+        }
+
+        @Override
+        public void setLogWriter(PrintWriter pw) throws SQLException {
+            this.ds.setLogWriter(pw);
+        }
+
+        @Override
+        public void setLoginTimeout(int t) throws SQLException {
+            this.ds.setLoginTimeout(t);
+        }
+
+        @Override
+        public boolean isWrapperFor(Class<?> c) throws SQLException {
+            return this.ds.isWrapperFor(c);
+        }
+
+        @Override
+        public <T> T unwrap(Class<T> c) throws SQLException {
+            return this.ds.unwrap(c);
+        }
+
+        @Override
+        public void close() throws IOException {
+            Class<?> dsclazz = ds.getClass();
+            try {
+                Method clmethod = dsclazz.getMethod("close");
+                clmethod.invoke(ds);
+            } catch (NoSuchMethodException e) {
+                LOG.debug("Class " + dsclazz + " does not have close() method");
+            } catch (IllegalArgumentException e) {
+                LOG.debug("Class " + dsclazz + " does not have close() method");
+            } catch (InvocationTargetException e) {
+                throw new IOException("trying to close datasource", e);
+            } catch (IllegalAccessException e) {
+                throw new IOException("trying to close datasource", e);
+            }
+        }
+
+        @Override
+        public Connection getConnection() throws SQLException {
+            return this.ds.getConnection();
+        }
+
+        @Override
+        public Connection getConnection(String user, String passwd) throws SQLException {
+            return this.ds.getConnection(user, passwd);
+        }
+
+        // needed in Java 7...
+        @SuppressWarnings("unused")
+        public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+            throw new SQLFeatureNotSupportedException();
+        }
+
+        @Override
+        public String toString() {
+            return this.getClass().getName() + " wrapping a " + this.ds.toString();
+        }
     }
 }
