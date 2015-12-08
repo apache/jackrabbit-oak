a2cdab4301872a5c64f55b4daca410d7813c913a
Alexandru Parvulescu <alexparvulescu@apache.org>
2015-12-08 09:44:00 +0000

    OAK-3436 Prevent missing checkpoint due to unstable topology from causing complete reindexing  reverted fix as it breaks temp checkpoints
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1718541 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/AsyncIndexUpdateLeaseTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/AsyncIndexUpdateLeaseTest.java
@@ -39,6 +39,7 @@ import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
 import org.apache.jackrabbit.oak.spi.state.NodeStore;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableSet;
@@ -79,6 +80,7 @@ public class AsyncIndexUpdateLeaseTest extends OakBaseTest {
     }
 
     @Test
+    @Ignore("OAK-3436")
     public void testPrePrepare() throws Exception {
         // take care of initial reindex before
         new AsyncIndexUpdate(name, store, provider).run();
