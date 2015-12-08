7c82956604a8b4447c577d8ed637691c501dfaa0
Francesco Mari <frm@apache.org>
2015-12-08 13:54:59 +0000

    OAK-3741 - Let the name of the result type of AbstractCheckpointMBean be equal to the name of the concrete subclass
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1718610 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/util/package-info.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/util/package-info.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@Version("1.3.1")
+@Version("2.0.0")
 @Export(optional = "provide:=true")
 package org.apache.jackrabbit.oak.util;
 
