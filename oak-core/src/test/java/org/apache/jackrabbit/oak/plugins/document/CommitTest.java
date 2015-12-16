a246d1e9a1ed50459479dd30a0d5c1fef33d1809
Marcel Reutegger <mreutegg@apache.org>
2015-12-16 12:40:39 +0000

    JCR-3733: Sometimes hierarchy conflict between concurrent add/delete isn't detected
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1720350 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/CommitTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/CommitTest.java
@@ -19,11 +19,19 @@
 
 package org.apache.jackrabbit.oak.plugins.document;
 
+import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
+import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
+import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
+import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CommitTest {
+
+    @Rule
+    public DocumentMKBuilderProvider builderProvider = new DocumentMKBuilderProvider();
 
     @Test
     public void testModifiedTime(){
@@ -32,5 +40,41 @@ public class CommitTest {
         assertEquals(10, NodeDocument.getModifiedInSecs(12000));
         assertEquals(15, NodeDocument.getModifiedInSecs(15000));
         assertEquals(15, NodeDocument.getModifiedInSecs(15006));
+    }
+
+    // OAK-3733
+    @Ignore("OAK-3733")
+    @Test
+    public void detectConflict() throws Exception {
+        // prepare node store
+        DocumentNodeStore ns = builderProvider.newBuilder().getNodeStore();
+        NodeBuilder b = ns.getRoot().builder();
+        b.child("foo");
+        b.child("bar");
+        ns.merge(b, EmptyHook.INSTANCE, CommitInfo.EMPTY);
+        b = ns.getRoot().builder();
+        b.child("foo").remove();
+        ns.merge(b, EmptyHook.INSTANCE, CommitInfo.EMPTY);
+
+        // this commit should fail
+        Commit c = ns.newCommit(ns.getHeadRevision(), null);
+        try {
+            c.addNode(new DocumentNodeState(ns, "/foo/baz", c.getRevision()));
+            UpdateOp op = c.getUpdateOperationForNode("/bar");
+            op.setMapEntry("p", c.getRevision(), "v");
+            try {
+                c.apply();
+                ns.done(c, false, null);
+            } catch (DocumentStoreException e) {
+                // expected
+            }
+        } finally {
+            ns.canceled(c);
+        }
+
+        // try to re-create /foo
+        b = ns.getRoot().builder();
+        b.child("foo");
+        ns.merge(b, EmptyHook.INSTANCE, CommitInfo.EMPTY);
     }
 }
