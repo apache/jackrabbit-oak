b2614b49ca46bb0c49fdc20cad4aa560a44445e9
Marcel Reutegger <mreutegg@apache.org>
2016-01-27 11:07:39 +0000

    OAK-3932: DocumentStore.getIfCached() must not return NodeDocument.NULL
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1726993 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/BasicDocumentStoreTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/BasicDocumentStoreTest.java
@@ -911,6 +911,14 @@ public class BasicDocumentStoreTest extends AbstractDocumentStoreTest {
         assertNull(ds.getIfCached(Collection.NODES, id));
     }
 
+    // OAK-3932
+    @Test
+    public void getIfCachedNonExistingDocument() throws Exception {
+        String id = Utils.getIdFromPath("/foo");
+        assertNull(ds.find(Collection.NODES, id));
+        assertNull(ds.getIfCached(Collection.NODES, id));
+    }
+
     private UpdateOp newDocument(String path, long modified) {
         String id = Utils.getIdFromPath(path);
         UpdateOp op = new UpdateOp(id, true);
