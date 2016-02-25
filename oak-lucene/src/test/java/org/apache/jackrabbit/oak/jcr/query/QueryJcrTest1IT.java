192f749d94d18fa639d9f6d83f0f92b4dcf7467f
Marcel Reutegger <mreutegg@apache.org>
2016-02-25 09:58:24 +0000

    OAK-4061: QueryJcrTestIT times out on travis
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1732271 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/jcr/query/QueryJcrTest1IT.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/jcr/query/QueryJcrTest1IT.java
@@ -16,41 +16,28 @@
  */
 package org.apache.jackrabbit.oak.jcr.query;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-import org.apache.jackrabbit.core.query.ChildAxisQueryTest;
-import org.apache.jackrabbit.core.query.DerefTest;
 import org.apache.jackrabbit.core.query.ExcerptTest;
-import org.apache.jackrabbit.core.query.FnNameQueryTest;
 import org.apache.jackrabbit.core.query.FulltextQueryTest;
 import org.apache.jackrabbit.core.query.FulltextSQL2QueryTest;
 import org.apache.jackrabbit.core.query.JoinTest;
 import org.apache.jackrabbit.core.query.LimitAndOffsetTest;
 import org.apache.jackrabbit.core.query.MixinTest;
 import org.apache.jackrabbit.core.query.OrderByTest;
-import org.apache.jackrabbit.core.query.ParentNodeTest;
 import org.apache.jackrabbit.core.query.PathQueryNodeTest;
 import org.apache.jackrabbit.core.query.QueryResultTest;
 import org.apache.jackrabbit.core.query.SQL2NodeLocalNameTest;
 import org.apache.jackrabbit.core.query.SQL2OffsetLimitTest;
 import org.apache.jackrabbit.core.query.SQL2OrderByTest;
 import org.apache.jackrabbit.core.query.SQL2OuterJoinTest;
-import org.apache.jackrabbit.core.query.SQL2PathEscapingTest;
-import org.apache.jackrabbit.core.query.SQL2QueryResultTest;
 import org.apache.jackrabbit.core.query.SQLTest;
-import org.apache.jackrabbit.core.query.SelectClauseTest;
-import org.apache.jackrabbit.core.query.ShareableNodeTest;
-import org.apache.jackrabbit.core.query.SimilarQueryTest;
-import org.apache.jackrabbit.core.query.SimpleQueryTest;
 import org.apache.jackrabbit.core.query.SkipDeletedNodesTest;
-import org.apache.jackrabbit.core.query.UpperLowerCaseQueryTest;
-import org.apache.jackrabbit.core.query.VersionStoreQueryTest;
-import org.apache.jackrabbit.core.query.XPathAxisTest;
 import org.apache.jackrabbit.test.ConcurrentTestSuite;
 
-public class QueryJcrTestIT extends TestCase {
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class QueryJcrTest1IT extends TestCase {
 
     public static Test suite() {
         TestSuite suite = new ConcurrentTestSuite(
@@ -70,19 +57,6 @@ public class QueryJcrTestIT extends TestCase {
         suite.addTestSuite(OrderByTest.class);
         suite.addTestSuite(ExcerptTest.class);
         suite.addTestSuite(QueryResultTest.class);
-        suite.addTestSuite(ParentNodeTest.class);
-        suite.addTestSuite(SimilarQueryTest.class);
-        suite.addTestSuite(DerefTest.class);
-        suite.addTestSuite(XPathAxisTest.class);
-        suite.addTestSuite(SQL2QueryResultTest.class);
-        suite.addTestSuite(SimpleQueryTest.class);
-        suite.addTestSuite(FnNameQueryTest.class);
-        suite.addTestSuite(UpperLowerCaseQueryTest.class);
-        suite.addTestSuite(SQL2PathEscapingTest.class);
-        suite.addTestSuite(ChildAxisQueryTest.class);
-        suite.addTestSuite(SelectClauseTest.class);
-        suite.addTestSuite(ShareableNodeTest.class);
-        suite.addTestSuite(VersionStoreQueryTest.class);
         return suite;
     }
 }
