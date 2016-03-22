58f755b59cde8c8e3542c5c0b10ccdc6e20e633a
Tommaso Teofili <tommaso@apache.org>
2016-03-22 10:23:43 +0000

    OAK-4126 - avoid loops when suggest/spellcheck query throws exception
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1736176 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndex.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndex.java
@@ -390,6 +390,7 @@ public class LuceneIndex implements AdvanceFulltextQueryIndex {
                         }
                     } else if (luceneRequestFacade.getLuceneRequest() instanceof SpellcheckHelper.SpellcheckQuery) {
                         SpellcheckHelper.SpellcheckQuery spellcheckQuery = (SpellcheckHelper.SpellcheckQuery) luceneRequestFacade.getLuceneRequest();
+                        noDocs = true;
                         SuggestWord[] suggestWords = SpellcheckHelper.getSpellcheck(spellcheckQuery);
 
                         // ACL filter spellchecks
@@ -410,9 +411,9 @@ public class LuceneIndex implements AdvanceFulltextQueryIndex {
                         }
 
                         queue.add(new LuceneResultRow(suggestedWords));
-                        noDocs = true;
                     } else if (luceneRequestFacade.getLuceneRequest() instanceof SuggestHelper.SuggestQuery) {
                         SuggestHelper.SuggestQuery suggestQuery = (SuggestHelper.SuggestQuery) luceneRequestFacade.getLuceneRequest();
+                        noDocs = true;
                         List<Lookup.LookupResult> lookupResults = SuggestHelper.getSuggestions(indexNode.getLookup(), suggestQuery);
 
                         // ACL filter suggestions
@@ -433,7 +434,6 @@ public class LuceneIndex implements AdvanceFulltextQueryIndex {
                         }
 
                         queue.add(new LuceneResultRow(suggestedWords));
-                        noDocs = true;
                     }
                 } catch (IOException e) {
                     LOG.warn("query via {} failed.", LuceneIndex.this, e);
