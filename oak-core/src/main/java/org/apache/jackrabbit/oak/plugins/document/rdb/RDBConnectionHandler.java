b90407aff83eb6d1878efd06a91c95f2c6bc3a7d
Julian Reschke <reschke@apache.org>
2016-04-01 12:05:47 +0000

    OAK-4156: RDBConnectionHandler: add logging when getting the connection takes long (ported to 1.2)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.2@1737352 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBConnectionHandler.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBConnectionHandler.java
@@ -19,9 +19,7 @@ package org.apache.jackrabbit.oak.plugins.document.rdb;
 import java.io.Closeable;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
@@ -59,7 +57,7 @@ public class RDBConnectionHandler implements Closeable {
      * Obtain a {@link Connection} suitable for read-only operations.
      */
     public @Nonnull Connection getROConnection() throws SQLException {
-        Connection c = getDataSource().getConnection();
+        Connection c = getConnection();
         c.setAutoCommit(false);
         setReadOnly(c, true);
         return c;
@@ -69,7 +67,7 @@ public class RDBConnectionHandler implements Closeable {
      * Obtain a {@link Connection} suitable for read-write operations.
      */
     public @Nonnull Connection getRWConnection() throws SQLException {
-        Connection c = getDataSource().getConnection();
+        Connection c = getConnection();
         c.setAutoCommit(false);
         setReadOnly(c, false);
         return c;
@@ -141,6 +139,19 @@ public class RDBConnectionHandler implements Closeable {
                     + (System.currentTimeMillis() - this.closedTime) + "ms ago)");
         }
         return result;
+    }
+
+    @Nonnull
+    private Connection getConnection() throws IllegalStateException, SQLException {
+        long ts = System.currentTimeMillis();
+        Connection c = getDataSource().getConnection();
+        if (LOG.isDebugEnabled()) {
+            long elapsed = System.currentTimeMillis() - ts;
+            if (elapsed >= 100) {
+                LOG.debug("Obtaining a new connection from " + this.ds + " took " + elapsed + "ms");
+            }
+        }
+        return c;
     }
 
     // workaround for broken connection wrappers
