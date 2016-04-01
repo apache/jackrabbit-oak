43bf724a10a12802e2bb126915d0d976b187943a
Stefan Egli <stefanegli@apache.org>
2016-04-01 10:26:00 +0000

    OAK-4153 : further refinement of previous fix: calculation of beforeChildRemoved based on reusing already existing comparison, removed unnecessary and confusing else case, and put beforeChild to the start of this all to help improve readability - ie this is OAK-4153-3.patch
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-segment@1737342 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/jackrabbit/oak/plugins/segment/SegmentNodeState.java
+++ src/main/java/org/apache/jackrabbit/oak/plugins/segment/SegmentNodeState.java
@@ -555,33 +555,25 @@ public class SegmentNodeState extends Record implements NodeState {
                 }
             }
         } else if (beforeChildName != Template.MANY_CHILD_NODES) {
-            boolean beforeChildStillExists = false;
+            boolean beforeChildRemoved = true;
+            NodeState beforeChild =
+                    beforeTemplate.getChildNode(beforeChildName, beforeId);
             for (ChildNodeEntry entry
                     : afterTemplate.getChildNodeEntries(afterId)) {
                 String childName = entry.getName();
-                beforeChildStillExists |= childName.equals(beforeChildName);
                 NodeState afterChild = entry.getNodeState();
                 if (beforeChildName.equals(childName)) {
-                    NodeState beforeChild =
-                            beforeTemplate.getChildNode(beforeChildName, beforeId);
-                    if (beforeChild.exists()) {
-                        if (!fastEquals(afterChild, beforeChild)
-                                && !diff.childNodeChanged(
-                                        childName, beforeChild, afterChild)) {
-                            return false;
-                        }
-                    } else {
-                        if (!diff.childNodeAdded(childName, afterChild)) {
-                            return false;
-                        }
+                    beforeChildRemoved = false;
+                    if (!fastEquals(afterChild, beforeChild)
+                            && !diff.childNodeChanged(
+                                    childName, beforeChild, afterChild)) {
+                        return false;
                     }
                 } else if (!diff.childNodeAdded(childName, afterChild)) {
                     return false;
                 }
             }
-            if (!beforeChildStillExists) {
-                NodeState beforeChild =
-                        beforeTemplate.getChildNode(beforeChildName, beforeId);
+            if (beforeChildRemoved) {
                 if (!diff.childNodeDeleted(beforeChildName, beforeChild)) {
                     return false;
                 }
