84b7df81964a6da3da8f3fc56e9907281d0228bf
Vikas Saurabh <catholicon@apache.org>
2016-04-12 16:07:52 +0000

    OAK-4182: oak-run->console should have a read-only mode to connect to document stores (mongo, rdb, etc)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1738825 13f79535-47bb-0310-9956-ffa450edef68

--- oak-run/src/main/java/org/apache/jackrabbit/oak/console/Console.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/console/Console.java
@@ -54,6 +54,7 @@ public class Console {
                 .withRequiredArg().ofType(Integer.class).defaultsTo(0);
         OptionSpec quiet = parser.accepts("quiet", "be less chatty");
         OptionSpec shell = parser.accepts("shell", "run the shell after executing files");
+        OptionSpec readOnly = parser.accepts("read-only", "connect to repository in read-only mode");
         OptionSpec help = parser.acceptsAll(asList("h", "?", "help"), "show help").forHelp();
 
         // RDB specific options
@@ -83,16 +84,22 @@ public class Console {
                 System.exit(1);
             }
             MongoConnection mongo = new MongoConnection(uri.getURI());
-            DocumentNodeStore store = new DocumentMK.Builder().
+
+            DocumentMK.Builder builder = new DocumentMK.Builder().
                     setMongoDB(mongo.getDB()).
-                    setClusterId(clusterId.value(options)).getNodeStore();
+                    setClusterId(clusterId.value(options));
+            if (options.has(readOnly)) {
+                builder.setReadOnlyMode();
+            }
+            DocumentNodeStore store = builder.getNodeStore();
             fixture = new MongoFixture(store);
         } else if (nonOptions.get(0).startsWith("jdbc")) {
             DataSource ds = RDBDataSourceFactory.forJdbcUrl(nonOptions.get(0), rdbjdbcuser.value(options),
                     rdbjdbcpasswd.value(options));
-            DocumentNodeStore store = new DocumentMK.Builder().
+            DocumentMK.Builder builder = new DocumentMK.Builder().
                     setRDBConnection(ds).
-                    setClusterId(clusterId.value(options)).getNodeStore();
+                    setClusterId(clusterId.value(options));
+            DocumentNodeStore store = builder.getNodeStore();
             fixture = new MongoFixture(store);
         } else {
             fixture = new SegmentFixture(FileStore.builder(new File(nonOptions.get(0))).withMaxFileSize(256).build());
