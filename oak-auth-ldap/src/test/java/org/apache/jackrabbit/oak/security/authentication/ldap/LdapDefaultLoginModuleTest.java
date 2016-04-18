74e1c1471d7b361365602d4d65bb7b84af2f5601
Angela Schreiber <angela@apache.org>
2016-04-18 13:32:01 +0000

    minor improvement: fix javadoc
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1739759 13f79535-47bb-0310-9956-ffa450edef68

--- oak-auth-ldap/src/test/java/org/apache/jackrabbit/oak/security/authentication/ldap/LdapDefaultLoginModuleTest.java
+++ oak-auth-ldap/src/test/java/org/apache/jackrabbit/oak/security/authentication/ldap/LdapDefaultLoginModuleTest.java
@@ -65,7 +65,7 @@ public class LdapDefaultLoginModuleTest extends LdapLoginTestBase {
     /**
      * Login with {@link javax.jcr.GuestCredentials} must succeed and result in
      * an guest session as the SUFFICIENT
-     * {@link org.apache.jackrabbit.oak.spi.security.authentication.LoginModuleImpl}
+     * {@link org.apache.jackrabbit.oak.security.authentication.user.LoginModuleImpl}
      * handles the guest login (in contrast to the ExternalLoginModule).
      *
      * @throws Exception
