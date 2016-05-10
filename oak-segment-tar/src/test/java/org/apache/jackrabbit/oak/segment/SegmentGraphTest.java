baf889485029f4f6492caee0064ce3ba43154560
Michael Dürig <mduerig@apache.org>
2016-05-10 13:35:36 +0000

    OAK-4288: TarReader.calculateForwardReferences only used by oak-run graph tool Fix SegmentGraphTest (adapt to new segment format and generalise)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1743179 13f79535-47bb-0310-9956-ffa450edef68

--- oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/SegmentGraphTest.java
+++ oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/SegmentGraphTest.java
@@ -22,91 +22,103 @@ package org.apache.jackrabbit.oak.segment;
 import static com.google.common.collect.Maps.newHashMap;
 import static com.google.common.collect.Sets.newHashSet;
 import static java.util.Collections.singleton;
+import static org.apache.jackrabbit.oak.plugins.memory.EmptyNodeState.EMPTY_NODE;
+import static org.apache.jackrabbit.oak.plugins.memory.PropertyStates.createProperty;
 import static org.apache.jackrabbit.oak.segment.SegmentGraph.createRegExpFilter;
 import static org.apache.jackrabbit.oak.segment.SegmentGraph.parseSegmentGraph;
+import static org.apache.jackrabbit.oak.segment.SegmentVersion.LATEST_VERSION;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.UUID;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multiset;
-import org.apache.commons.io.IOUtils;
 import org.apache.jackrabbit.oak.segment.SegmentGraph.Graph;
 import org.apache.jackrabbit.oak.segment.file.FileStore;
 import org.apache.jackrabbit.oak.segment.file.FileStore.ReadOnlyStore;
+import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-@Ignore
-// FIXME OAK-4288: TarReader.calculateForwardReferences only used by oak-run graph tool
-// Fix test SegmentGraphTest
 public class SegmentGraphTest {
-    private final Set<UUID> segments = newHashSet(
-        UUID.fromString("5be0c2ea-b6ba-4f80-acad-657a20f920b6"),
-        UUID.fromString("fdaca71e-f71e-4f19-abf5-144e8c85f9e3"),
-        UUID.fromString("53be3b93-87fa-487f-a2fc-7c17e639c231"),
-        UUID.fromString("2eae0bc2-d3dd-4ba4-a765-70c38073437d"),
-        UUID.fromString("ab61b8c9-222c-4119-a73b-5f61c0bc4741"),
-        UUID.fromString("38c42dde-5928-4cc3-a483-37185d6971e4")
-    );
-
-    private final Map<UUID, Set<UUID>> references = ImmutableMap.<UUID, Set<UUID>>of(
-        UUID.fromString("5be0c2ea-b6ba-4f80-acad-657a20f920b6"),
-            newHashSet(UUID.fromString("2eae0bc2-d3dd-4ba4-a765-70c38073437d")),
-        UUID.fromString("fdaca71e-f71e-4f19-abf5-144e8c85f9e3"),
-            newHashSet(UUID.fromString("ab61b8c9-222c-4119-a73b-5f61c0bc4741")),
-        UUID.fromString("2eae0bc2-d3dd-4ba4-a765-70c38073437d"),
-            newHashSet(UUID.fromString("2fdaca71e-f71e-4f19-abf5-144e8c85f9e3"),
-                       UUID.fromString("ab61b8c9-222c-4119-a73b-5f61c0bc4741"))
-    );
-
-    private final Set<UUID> filteredSegments = newHashSet(
-        UUID.fromString("fdaca71e-f71e-4f19-abf5-144e8c85f9e3"),
-        UUID.fromString("2eae0bc2-d3dd-4ba4-a765-70c38073437d"),
-        UUID.fromString("ab61b8c9-222c-4119-a73b-5f61c0bc4741")
-    );
-
-    private final Map<UUID, Set<UUID>> filteredReferences = ImmutableMap.<UUID, Set<UUID>>of(
-        UUID.fromString("fdaca71e-f71e-4f19-abf5-144e8c85f9e3"),
-            newHashSet(UUID.fromString("ab61b8c9-222c-4119-a73b-5f61c0bc4741")),
-        UUID.fromString("2eae0bc2-d3dd-4ba4-a765-70c38073437d"),
-            newHashSet(UUID.fromString("2fdaca71e-f71e-4f19-abf5-144e8c85f9e3"),
-                       UUID.fromString("ab61b8c9-222c-4119-a73b-5f61c0bc4741"))
-    );
-
-    private final Set<String> gcGenerations = newHashSet("0", "1");
-    private final Map<String, Set<String>> gcReferences = ImmutableMap.of(
-        "0", singleton("0"),
-        "1", singleton("0")
-    );
+    private final Set<UUID> segments = newHashSet();
+    private final Map<UUID, Set<UUID>> references = newHashMap();
+    private final Set<UUID> filteredSegments = newHashSet();
+    private final Map<UUID, Set<UUID>> filteredReferences = newHashMap();
 
     @Rule
     public TemporaryFolder storeFolder = new TemporaryFolder();
 
     private File getStoreFolder() {
-        return storeFolder.getRoot();
+         return storeFolder.getRoot();
     }
 
     @Before
     public void setup() throws IOException {
-        System.out.println(getStoreFolder());
-        unzip(SegmentGraphTest.class.getResourceAsStream("file-store.zip"), getStoreFolder());
+        FileStore store = FileStore.builder(getStoreFolder()).build();
+        try {
+            SegmentNodeState root = store.getHead();
+            segments.add(getSegmentId(root));
+
+            SegmentWriter w1 = new SegmentWriter(store, LATEST_VERSION,
+                    new SegmentBufferWriter(store, LATEST_VERSION, "writer1"));
+
+            SegmentWriter w2 = new SegmentWriter(store, LATEST_VERSION,
+                    new SegmentBufferWriter(store, LATEST_VERSION, "writer2"));
+
+            SegmentWriter w3 = new SegmentWriter(store, LATEST_VERSION,
+                    new SegmentBufferWriter(store, LATEST_VERSION, "writer3", 0));
+
+            SegmentPropertyState p1 = w1.writeProperty(createProperty("p1", "v1"));
+            segments.add(getSegmentId(p1));
+
+            SegmentPropertyState p2 = w2.writeProperty(createProperty("p2", "v2"));
+            segments.add(getSegmentId(p2));
+            filteredSegments.add(getSegmentId(p2));
+
+            NodeBuilder builder = EMPTY_NODE.builder();
+            builder.setProperty(p1);
+            builder.setProperty(p2);
+
+            SegmentNodeState n1 = w3.writeNode(builder.getNodeState());
+            segments.add(getSegmentId(n1));
+            filteredSegments.add(getSegmentId(n1));
+            addReference(references, getSegmentId(n1), getSegmentId(p1));
+            addReference(references, getSegmentId(n1), getSegmentId(p2));
+            addReference(filteredReferences, getSegmentId(n1), getSegmentId(p2));
+
+            store.setHead(root, n1);
+
+            w1.flush();
+            w2.flush();
+            w3.flush();
+        } finally {
+            store.close();
+        }
     }
+
+    private static void addReference(Map<UUID, Set<UUID>> references, UUID from, UUID to) {
+        Set<UUID> tos = references.get(from);
+        if (tos == null) {
+            tos = newHashSet();
+            references.put(from, tos);
+        }
+        tos.add(to);
+    }
+
+    private static UUID getSegmentId(SegmentPropertyState p1) {return p1.getSegment().getSegmentId().asUUID();}
+
+    private static UUID getSegmentId(SegmentNodeState root) {return root.getSegment().getSegmentId().asUUID();}
 
     @Test
     public void testSegmentGraph() throws IOException {
@@ -128,7 +140,7 @@ public class SegmentGraphTest {
     public void testSegmentGraphWithFilter() throws IOException {
         ReadOnlyStore store = FileStore.builder(getStoreFolder()).buildReadOnly();
         try {
-            Predicate<UUID> filter = createRegExpFilter(".*testWriter.*", store.getTracker());
+            Predicate<UUID> filter = createRegExpFilter(".*(writer2|writer3).*", store.getTracker());
             Graph<UUID> segmentGraph = parseSegmentGraph(store, filter);
             assertEquals(filteredSegments, newHashSet(segmentGraph.vertices()));
             Map<UUID, Set<UUID>> map = newHashMap();
@@ -143,33 +155,23 @@ public class SegmentGraphTest {
 
     @Test
     public void testGCGraph() throws IOException {
+        // TODO Improve test coverage to non trivial cases with more than a single generation
+        // This is quite tricky as there is no easy way to construct a file store with
+        // a segment graphs having edges between generations (OAK-3348)
         ReadOnlyStore store = FileStore.builder(getStoreFolder()).buildReadOnly();
         try {
             Graph<String> gcGraph = SegmentGraph.parseGCGraph(store);
-            assertEquals(gcGenerations, newHashSet(gcGraph.vertices()));
+            assertEquals(ImmutableSet.of("0"), newHashSet(gcGraph.vertices()));
             Map<String, Set<String>> map = newHashMap();
             for (Entry<String, Multiset<String>> entry : gcGraph.edges()) {
                 map.put(entry.getKey(), entry.getValue().elementSet());
             }
-            assertEquals(gcReferences, map);
+            assertEquals(ImmutableMap.of(
+                "0", singleton("0")
+            ), map);
         } finally {
             store.close();
         }
     }
 
-    private static void unzip(InputStream is, File target) throws IOException {
-        ZipInputStream zis = new ZipInputStream(is);
-        try {
-            for (ZipEntry entry = zis.getNextEntry(); entry != null; entry = zis.getNextEntry()) {
-                OutputStream out = new FileOutputStream(new File(target, entry.getName()));
-                try {
-                    IOUtils.copy(zis, out);
-                } finally {
-                    out.close();
-                }
-            }
-        } finally {
-            zis.close();
-        }
-    }
 }
