6aaf660cb9aa59c9ca60c7fdf10620a7a3d11120
Angela Schreiber <angela@apache.org>
2016-06-06 09:12:47 +0000

    minor improvement: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1746978 13f79535-47bb-0310-9956-ffa450edef68

--- oak-auth-external/src/test/java/org/apache/jackrabbit/oak/spi/security/authentication/external/impl/principal/ExternalGroupPrincipalProviderTest.java
+++ oak-auth-external/src/test/java/org/apache/jackrabbit/oak/spi/security/authentication/external/impl/principal/ExternalGroupPrincipalProviderTest.java
@@ -50,7 +50,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class ExternalGroupPrincipalProviderTest extends AbstractPrincipalTest {
 
