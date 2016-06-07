1d709ebd07ac48895dd4bd9019cf688431e45cd1
Alexandru Parvulescu <alexparvulescu@apache.org>
2016-06-07 17:45:26 +0000

    OAK-4083 Simplify concurrency when loading data from the primary  - merged r1733615
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.2@1747284 13f79535-47bb-0310-9956-ffa450edef68

--- oak-tarmk-standby/src/main/java/org/apache/jackrabbit/oak/plugins/segment/standby/client/StandbyClientHandler.java
+++ oak-tarmk-standby/src/main/java/org/apache/jackrabbit/oak/plugins/segment/standby/client/StandbyClientHandler.java
@@ -21,14 +21,11 @@ package org.apache.jackrabbit.oak.plugins.segment.standby.client;
 import static org.apache.jackrabbit.oak.plugins.segment.standby.codec.Messages.newGetHeadReq;
 
 import java.io.Closeable;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.util.concurrent.DefaultEventExecutorGroup;
-import io.netty.util.concurrent.EventExecutorGroup;
 import org.apache.jackrabbit.oak.plugins.segment.RecordId;
 import org.apache.jackrabbit.oak.plugins.segment.standby.codec.RecordIdDecoder;
 import org.apache.jackrabbit.oak.plugins.segment.standby.codec.ReplyDecoder;
@@ -37,8 +34,7 @@ import org.apache.jackrabbit.oak.plugins.segment.standby.store.StandbyStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class StandbyClientHandler extends SimpleChannelInboundHandler<RecordId>
-        implements Closeable {
+public class StandbyClientHandler extends SimpleChannelInboundHandler<RecordId> implements Closeable {
 
     private static final Logger log = LoggerFactory
             .getLogger(StandbyClientHandler.class);
@@ -49,11 +45,7 @@ public class StandbyClientHandler extends SimpleChannelInboundHandler<RecordId>
     private final int readTimeoutMs;
     private final boolean autoClean;
 
-    private EventExecutorGroup loaderExecutor;
-
-    public StandbyClientHandler(final StandbyStore store,
-            CommunicationObserver observer, AtomicBoolean running,
-            int readTimeoutMs, boolean autoClean) {
+    public StandbyClientHandler(final StandbyStore store, CommunicationObserver observer, AtomicBoolean running, int readTimeoutMs, boolean autoClean) {
         this.store = store;
         this.observer = observer;
         this.running = running;
@@ -69,8 +61,7 @@ public class StandbyClientHandler extends SimpleChannelInboundHandler<RecordId>
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, RecordId msg)
-            throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, RecordId msg) throws Exception {
         setHead(ctx, msg);
     };
 
@@ -80,7 +71,6 @@ public class StandbyClientHandler extends SimpleChannelInboundHandler<RecordId>
     }
 
     synchronized void setHead(ChannelHandlerContext ctx, RecordId head) {
-
         if (store.getHead().getRecordId().equals(head)) {
             // all sync'ed up
             log.debug("no changes on sync.");
@@ -93,28 +83,19 @@ public class StandbyClientHandler extends SimpleChannelInboundHandler<RecordId>
         ctx.pipeline().remove(this);
         ctx.pipeline().addLast(new ReplyDecoder(store));
 
-        loaderExecutor = new DefaultEventExecutorGroup(4);
-        SegmentLoaderHandler h2 = new SegmentLoaderHandler(store, head,
-                loaderExecutor, this.observer.getID(), running, readTimeoutMs,
-                autoClean);
-        ctx.pipeline().addLast(loaderExecutor, h2);
-
-        h2.channelActive(ctx);
-        log.debug("updating current head finished");
+        ctx.pipeline().addLast(new SegmentLoaderHandler(store, head, this.observer.getID(), running, readTimeoutMs, autoClean));
+        ctx.pipeline().fireUserEventTriggered("sync");
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
-            throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         log.error("Exception caught, closing channel.", cause);
         close();
     }
 
     @Override
-    public synchronized void close() {
-        if (loaderExecutor != null && !loaderExecutor.isShuttingDown()) {
-            loaderExecutor.shutdownGracefully(0, 1, TimeUnit.SECONDS)
-                    .syncUninterruptibly();
-        }
+    public void close() {
+        // This handler doesn't own resources to release
     }
+
 }
