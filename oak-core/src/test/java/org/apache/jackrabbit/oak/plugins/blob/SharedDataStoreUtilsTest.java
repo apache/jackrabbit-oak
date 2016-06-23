cbb88a0745660a5e1dc1407f32d7fcb5aff48b1a
Amit Jain <amitj@apache.org>
2016-06-23 05:21:28 +0000

    OAK-4430: DataStoreBlobStore#getAllChunkIds fetches DataRecord when not needed Merge r1747342 from trunk
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.4@1749821 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/blob/SharedDataStoreUtilsTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/blob/SharedDataStoreUtilsTest.java
@@ -18,37 +18,53 @@
  */
 package org.apache.jackrabbit.oak.plugins.blob;
 
+import static com.google.common.collect.Sets.newHashSet;
+import static junit.framework.Assert.assertEquals;
+import static org.apache.jackrabbit.oak.plugins.blob.datastore.DataStoreUtils.cleanup;
+import static org.apache.jackrabbit.oak.plugins.blob.datastore.DataStoreUtils.getBlobStore;
 import static org.hamcrest.CoreMatchers.instanceOf;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Random;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
+import javax.annotation.Nullable;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Iterables;
 import junit.framework.Assert;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.jackrabbit.core.data.DataRecord;
+import org.apache.jackrabbit.oak.plugins.blob.datastore.DataStoreBlobStore;
 import org.apache.jackrabbit.oak.plugins.blob.datastore.SharedDataStoreUtils;
 import org.apache.jackrabbit.oak.plugins.blob.datastore.SharedDataStoreUtils.SharedStoreRecordType;
 import org.apache.jackrabbit.oak.plugins.blob.datastore.DataStoreUtils;
 import org.junit.After;
 import org.junit.Assume;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Test for SharedDataUtils to test addition, retrieval and deletion of root records.
  */
 public class SharedDataStoreUtilsTest {
-    SharedDataStore dataStore;
+    private static final Logger log = LoggerFactory.getLogger(SharedDataStoreUtilsTest.class);
+
+    DataStoreBlobStore dataStore;
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
         try {
-            Assume.assumeThat(DataStoreUtils.getBlobStore(), instanceOf(SharedDataStore.class));
+            Assume.assumeThat(getBlobStore(), instanceOf(SharedDataStore.class));
         } catch (Exception e) {
             Assume.assumeNoException(e);
         }
@@ -56,7 +72,7 @@ public class SharedDataStoreUtilsTest {
 
     @Test
     public void test() throws Exception {
-        dataStore = DataStoreUtils.getBlobStore();
+        dataStore = getBlobStore();
         String repoId1 = UUID.randomUUID().toString();
         String repoId2 = UUID.randomUUID().toString();
 
@@ -72,7 +88,7 @@ public class SharedDataStoreUtilsTest {
         dataStore.addMetadataRecord(new ByteArrayInputStream(new byte[0]),
                                        SharedStoreRecordType.MARKED_START_MARKER.getNameFromId(repoId1));
         DataRecord markerRec1 = dataStore.getMetadataRecord(SharedStoreRecordType.MARKED_START_MARKER.getNameFromId(repoId1));
-        Assert.assertEquals(
+        assertEquals(
                SharedStoreRecordType.MARKED_START_MARKER.getIdFromName(markerRec1.getIdentifier().toString()),
                repoId1);
         long lastModifiedMarkerRec1 = markerRec1.getLastModified();
@@ -89,14 +105,14 @@ public class SharedDataStoreUtilsTest {
         DataRecord rec2 = dataStore.getMetadataRecord(SharedStoreRecordType.REFERENCES.getNameFromId(repoId2));
         long lastModifiedRec2 = rec2.getLastModified();
 
-        Assert.assertEquals(SharedStoreRecordType.REPOSITORY.getIdFromName(repo1.getIdentifier().toString()), repoId1);
-        Assert.assertEquals(
+        assertEquals(SharedStoreRecordType.REPOSITORY.getIdFromName(repo1.getIdentifier().toString()), repoId1);
+        assertEquals(
                 SharedStoreRecordType.REPOSITORY.getIdFromName(repo2.getIdentifier().toString()),
                 repoId2);
-        Assert.assertEquals(
+        assertEquals(
                 SharedStoreRecordType.REFERENCES.getIdFromName(rec1.getIdentifier().toString()),
                 repoId1);
-        Assert.assertEquals(
+        assertEquals(
                 SharedStoreRecordType.REFERENCES.getIdFromName(rec2.getIdentifier().toString()),
                 repoId2);
 
@@ -108,7 +124,7 @@ public class SharedDataStoreUtilsTest {
         // Since, we don't care about which file specifically but only the earliest timestamped record
         // Earliest time should be the min timestamp from the 2 reference files
         long minRefTime = (lastModifiedRec1 <= lastModifiedRec2 ? lastModifiedRec1 : lastModifiedRec2);
-        Assert.assertEquals(
+        assertEquals(
                SharedDataStoreUtils.getEarliestRecord(
                         dataStore.getAllMetadataRecords(SharedStoreRecordType.REFERENCES.getType())).getLastModified(), 
                         minRefTime);
@@ -129,13 +145,59 @@ public class SharedDataStoreUtilsTest {
         Assert.assertTrue(dataStore.getAllMetadataRecords(SharedStoreRecordType.MARKED_START_MARKER.getType()).isEmpty());
     
         // Repository ids should still be available
-        Assert.assertEquals(2,
+        assertEquals(2,
             dataStore.getAllMetadataRecords(SharedStoreRecordType.REPOSITORY.getType()).size());
+    }
+
+    @Test
+    public void testGetAllChunkIds() throws Exception {
+        dataStore = getBlobStore();
+        int number = 1010;
+        Set<String> added = newHashSet();
+        for (int i = 0; i < number; i++) {
+            String rec = dataStore.writeBlob(randomStream(i, 16516));
+            added.add(rec);
+        }
+
+        Set<String> retrieved = newHashSet(dataStore.getAllChunkIds(0));
+        assertEquals(added, retrieved);
+    }
+
+    @Test
+    public void testGetAllRecords() throws Exception {
+        dataStore = getBlobStore();
+        int number = 1010;
+        Set<String> added = newHashSet();
+        for (int i = 0; i < number; i++) {
+            String rec = dataStore.addRecord(randomStream(i, 16516))
+                .getIdentifier().toString();
+            added.add(rec);
+        }
+
+        Set<String> retrieved = newHashSet(Iterables.transform(newHashSet(dataStore.getAllRecords()),
+            new Function<DataRecord, String>() {
+                @Nullable @Override public String apply(@Nullable DataRecord input) {
+                    return input.getIdentifier().toString();
+                }
+            }));
+        assertEquals(added, retrieved);
+    }
+
+    static InputStream randomStream(int seed, int size) {
+        Random r = new Random(seed);
+        byte[] data = new byte[size];
+        r.nextBytes(data);
+        return new ByteArrayInputStream(data);
     }
 
     @After
     public void close() throws IOException {
         FileUtils.cleanDirectory(new File(DataStoreUtils.getHomeDir()));
+        try {
+            cleanup(dataStore.getDataStore(), new Date());
+        } catch (Exception e) {
+            log.error("Error closing data store", e);
+        }
     }
 }
 
