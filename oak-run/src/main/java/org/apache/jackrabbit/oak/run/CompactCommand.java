dc0e4fb6cb91800379848d0f8ca389778451ecb1
Alexandru Parvulescu <alexparvulescu@apache.org>
2016-06-24 11:46:26 +0000

    OAK-4505 Offline compaction clearer output values
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1750076 13f79535-47bb-0310-9956-ffa450edef68

--- oak-run/src/main/java/org/apache/jackrabbit/oak/run/CompactCommand.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/run/CompactCommand.java
@@ -17,16 +17,21 @@
 
 package org.apache.jackrabbit.oak.run;
 
+import static com.google.common.collect.Sets.difference;
+import static com.google.common.collect.Sets.newHashSet;
+
 import java.io.File;
-import java.util.Arrays;
+import java.util.Date;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-import com.google.common.base.Stopwatch;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
 import org.apache.commons.io.FileUtils;
 import org.apache.jackrabbit.oak.commons.IOUtils;
+
+import com.google.common.base.Stopwatch;
 
 class CompactCommand implements Command {
 
@@ -37,7 +42,7 @@ class CompactCommand implements Command {
                 "Path to segment store (required)").ofType(String.class);
         OptionSpec<Void> forceFlag = parser.accepts(
                 "force", "Force compaction and ignore non matching segment version");
-        OptionSpec segmentTar = parser.accepts("segment-tar", "Use oak-segment-tar instead of oak-segment");
+        OptionSpec<?> segmentTar = parser.accepts("segment-tar", "Use oak-segment-tar instead of oak-segment");
         OptionSet options = parser.parse(args);
 
         String path = directoryArg.value(options);
@@ -50,23 +55,60 @@ class CompactCommand implements Command {
         File directory = new File(path);
         boolean force = options.has(forceFlag);
 
+        boolean success = false;
+        Set<String> beforeLs = newHashSet();
+        Set<String> afterLs = newHashSet();
         Stopwatch watch = Stopwatch.createStarted();
 
-        if (options.has(segmentTar)) {
-            SegmentTarUtils.compact(directory, force);
-        } else {
-            SegmentUtils.compact(directory, force);
-        }
-
-        watch.stop();
-        System.out.println("    after  "
-                + Arrays.toString(directory.list()));
-        long sizeAfter = FileUtils.sizeOfDirectory(directory);
+        System.out.println("Compacting " + directory);
+        System.out.println("    before ");
+        beforeLs.addAll(list(directory));
+        long sizeBefore = FileUtils.sizeOfDirectory(directory);
         System.out.println("    size "
-                + IOUtils.humanReadableByteCount(sizeAfter) + " (" + sizeAfter
+                + IOUtils.humanReadableByteCount(sizeBefore) + " (" + sizeBefore
                 + " bytes)");
-        System.out.println("    duration  " + watch.toString() + " ("
-                + watch.elapsed(TimeUnit.SECONDS) + "s).");
+        System.out.println("    -> compacting");
+
+        try {
+            if (options.has(segmentTar)) {
+                SegmentTarUtils.compact(directory, force);
+            } else {
+                SegmentUtils.compact(directory, force);
+            }
+            success = true;
+        } catch (Throwable e) {
+            System.out.println("Compaction failure stack trace:");
+            e.printStackTrace(System.out);
+        } finally {
+            watch.stop();
+            if (success) {
+                System.out.println("    after ");
+                afterLs.addAll(list(directory));
+                long sizeAfter = FileUtils.sizeOfDirectory(directory);
+                System.out.println("    size "
+                        + IOUtils.humanReadableByteCount(sizeAfter) + " ("
+                        + sizeAfter + " bytes)");
+                System.out.println("    removed files " + difference(beforeLs, afterLs));
+                System.out.println("    added files " + difference(afterLs, beforeLs));
+                System.out.println("Compaction succeed in " + watch.toString()
+                        + " (" + watch.elapsed(TimeUnit.SECONDS) + "s).");
+            } else {
+                System.out.println("Compaction failed in " + watch.toString()
+                        + " (" + watch.elapsed(TimeUnit.SECONDS) + "s).");
+                System.exit(1);
+            }
+        }
+    }
+
+    private static Set<String> list(File directory) {
+        Set<String> files = newHashSet();
+        for (File f : directory.listFiles()) {
+            String d = new Date(f.lastModified()).toString();
+            String n = f.getName();
+            System.out.println("        " + d + ", " + n);
+            files.add(n);
+        }
+        return files;
     }
 
 }
