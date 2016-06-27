9ed7830e8dfcc299b8a7507816d4e795440c2d9c
Alexandru Parvulescu <alexparvulescu@apache.org>
2016-06-27 14:59:14 +0000

    OAK-4505 Offline compaction clearer output values  - merged r1750076, r1750077
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.4@1750356 13f79535-47bb-0310-9956-ffa450edef68

--- oak-run/src/main/java/org/apache/jackrabbit/oak/run/CompactCommand.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/run/CompactCommand.java
@@ -19,13 +19,17 @@ package org.apache.jackrabbit.oak.run;
 
 import static org.apache.jackrabbit.oak.plugins.segment.FileStoreHelper.openFileStore;
 
+import static com.google.common.collect.Sets.difference;
+import static com.google.common.collect.Sets.newHashSet;
+
 import java.io.File;
 import java.io.RandomAccessFile;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
-import com.google.common.base.Stopwatch;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
@@ -34,6 +38,8 @@ import org.apache.jackrabbit.oak.commons.IOUtils;
 import org.apache.jackrabbit.oak.plugins.segment.compaction.CompactionStrategy;
 import org.apache.jackrabbit.oak.plugins.segment.file.FileStore;
 import org.apache.jackrabbit.oak.plugins.segment.file.JournalReader;
+
+import com.google.common.base.Stopwatch;
 
 class CompactCommand implements Command {
 
@@ -52,82 +58,114 @@ class CompactCommand implements Command {
             parser.printHelpOn(System.err);
             System.exit(-1);
         }
-        Stopwatch watch = Stopwatch.createStarted();
-        FileStore store = openFileStore(path, options.has(forceFlag));
+
         File directory = new File(path);
-        try {
-            boolean persistCM = Boolean.getBoolean("tar.PersistCompactionMap");
-            System.out.println("Compacting " + directory);
-            System.out.println("    before " + Arrays.toString(directory.list()));
-            long sizeBefore = FileUtils.sizeOfDirectory(directory);
-            System.out.println("    size "
-                    + IOUtils.humanReadableByteCount(sizeBefore) + " (" + sizeBefore
-                    + " bytes)");
 
-            System.out.println("    -> compacting");
+        boolean success = false;
+        Set<String> beforeLs = newHashSet();
+        Set<String> afterLs = newHashSet();
+        Stopwatch watch = Stopwatch.createStarted();
 
-            CompactionStrategy compactionStrategy = new CompactionStrategy(
-                    false, CompactionStrategy.CLONE_BINARIES_DEFAULT,
-                    CompactionStrategy.CleanupType.CLEAN_ALL, 0,
-                    CompactionStrategy.MEMORY_THRESHOLD_DEFAULT) {
-                @Override
-                public boolean compacted(Callable<Boolean> setHead)
-                        throws Exception {
-                    // oak-run is doing compaction single-threaded
-                    // hence no guarding needed - go straight ahead
-                    // and call setHead
-                    return setHead.call();
-                }
-            };
-            compactionStrategy.setOfflineCompaction(true);
-            compactionStrategy.setPersistCompactionMap(persistCM);
-            store.setCompactionStrategy(compactionStrategy);
-            store.compact();
-        } finally {
-            store.close();
-        }
-
-        System.out.println("    -> cleaning up");
-        store = openFileStore(path, false);
-        try {
-            for (File file : store.cleanup()) {
-                if (!file.exists() || file.delete()) {
-                    System.out.println("    -> removed old file " + file.getName());
-                } else {
-                    System.out.println("    -> failed to remove old file " + file.getName());
-                }
-            }
-
-            String head;
-            File journal = new File(directory, "journal.log");
-            JournalReader journalReader = new JournalReader(journal);
-            try {
-                head = journalReader.iterator().next() + " root\n";
-            } finally {
-                journalReader.close();
-            }
-
-            RandomAccessFile journalFile = new RandomAccessFile(journal, "rw");
-            try {
-                System.out.println("    -> writing new " + journal.getName() + ": " + head);
-                journalFile.setLength(0);
-                journalFile.writeBytes(head);
-                journalFile.getChannel().force(false);
-            } finally {
-                journalFile.close();
-            }
-        } finally {
-            store.close();
-        }
-        watch.stop();
-        System.out.println("    after  "
-                + Arrays.toString(directory.list()));
-        long sizeAfter = FileUtils.sizeOfDirectory(directory);
+        System.out.println("Compacting " + directory);
+        System.out.println("    before ");
+        beforeLs.addAll(list(directory));
+        long sizeBefore = FileUtils.sizeOfDirectory(directory);
         System.out.println("    size "
-                + IOUtils.humanReadableByteCount(sizeAfter) + " (" + sizeAfter
+                + IOUtils.humanReadableByteCount(sizeBefore) + " (" + sizeBefore
                 + " bytes)");
-        System.out.println("    duration  " + watch.toString() + " ("
-                + watch.elapsed(TimeUnit.SECONDS) + "s).");
+        System.out.println("    -> compacting");
+
+        try {
+            FileStore store = openFileStore(path, options.has(forceFlag));
+            boolean persistCM = Boolean.getBoolean("tar.PersistCompactionMap");
+            try {
+                CompactionStrategy compactionStrategy = new CompactionStrategy(
+                        false, CompactionStrategy.CLONE_BINARIES_DEFAULT,
+                        CompactionStrategy.CleanupType.CLEAN_ALL, 0,
+                        CompactionStrategy.MEMORY_THRESHOLD_DEFAULT) {
+                    @Override
+                    public boolean compacted(Callable<Boolean> setHead)
+                            throws Exception {
+                        // oak-run is doing compaction single-threaded
+                        // hence no guarding needed - go straight ahead
+                        // and call setHead
+                        return setHead.call();
+                    }
+                };
+                compactionStrategy.setOfflineCompaction(true);
+                compactionStrategy.setPersistCompactionMap(persistCM);
+                store.setCompactionStrategy(compactionStrategy);
+                store.compact();
+            } finally {
+                store.close();
+            }
+
+            System.out.println("    -> cleaning up");
+            store = openFileStore(path, false);
+            try {
+                for (File file : store.cleanup()) {
+                    if (!file.exists() || file.delete()) {
+                        System.out.println("    -> removed old file " + file.getName());
+                    } else {
+                        System.out.println("    -> failed to remove old file " + file.getName());
+                    }
+                }
+
+                String head;
+                File journal = new File(directory, "journal.log");
+                JournalReader journalReader = new JournalReader(journal);
+                try {
+                    head = journalReader.iterator().next() + " root\n";
+                } finally {
+                    journalReader.close();
+                }
+
+                RandomAccessFile journalFile = new RandomAccessFile(journal, "rw");
+                try {
+                    System.out.println("    -> writing new " + journal.getName() + ": " + head);
+                    journalFile.setLength(0);
+                    journalFile.writeBytes(head);
+                    journalFile.getChannel().force(false);
+                } finally {
+                    journalFile.close();
+                }
+            } finally {
+                store.close();
+            }
+            success = true;
+        } catch (Throwable e) {
+            System.out.println("Compaction failure stack trace:");
+            e.printStackTrace(System.out);
+        } finally {
+            watch.stop();
+            if (success) {
+                System.out.println("    after ");
+                afterLs.addAll(list(directory));
+                long sizeAfter = FileUtils.sizeOfDirectory(directory);
+                System.out.println("    size "
+                        + IOUtils.humanReadableByteCount(sizeAfter) + " ("
+                        + sizeAfter + " bytes)");
+                System.out.println("    removed files " + difference(beforeLs, afterLs));
+                System.out.println("    added files " + difference(afterLs, beforeLs));
+                System.out.println("Compaction succeeded in " + watch.toString()
+                        + " (" + watch.elapsed(TimeUnit.SECONDS) + "s).");
+            } else {
+                System.out.println("Compaction failed in " + watch.toString()
+                        + " (" + watch.elapsed(TimeUnit.SECONDS) + "s).");
+                System.exit(1);
+            }
+        }
+    }
+
+    private static Set<String> list(File directory) {
+        Set<String> files = newHashSet();
+        for (File f : directory.listFiles()) {
+            String d = new Date(f.lastModified()).toString();
+            String n = f.getName();
+            System.out.println("        " + d + ", " + n);
+            files.add(n);
+        }
+        return files;
     }
 
 }
