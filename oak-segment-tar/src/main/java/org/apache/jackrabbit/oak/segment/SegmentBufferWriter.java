511eadaf0076af9ea41c613ea00139648c8b565a
Francesco Mari <frm@apache.org>
2016-06-30 14:06:31 +0000

    OAK-4525 - Ensure that unreferenced node records are root records in a segment
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1750801 13f79535-47bb-0310-9956-ffa450edef68

--- oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/SegmentBufferWriter.java
+++ oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/SegmentBufferWriter.java
@@ -216,8 +216,24 @@ public class SegmentBufferWriter implements WriteOperationHandler {
      * @param recordId the record id
      */
     public void writeRecordId(RecordId recordId) {
+        writeRecordId(recordId, true);
+    }
+
+    /**
+     * Write a record ID. Optionally, mark this record ID as being a reference.
+     * If a record ID is marked as a reference, the referenced record can't be a
+     * root record in this segment.
+     *
+     * @param recordId  the record ID.
+     * @param reference {@code true} if this record ID is a reference, {@code
+     *                  false} otherwise.
+     */
+    public void writeRecordId(RecordId recordId, boolean reference) {
         checkNotNull(recordId);
-        roots.remove(recordId);
+
+        if (reference) {
+            roots.remove(recordId);
+        }
 
         int offset = recordId.getOffset();
         checkState(0 <= offset && offset < MAX_SEGMENT_SIZE);
