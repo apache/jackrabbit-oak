e498662615cb6c377f9747ea28b8668a5f65ac8d
Tommaso Teofili <tommaso@apache.org>
2016-06-30 14:50:25 +0000

    OAK-4502 - relative indexes should still filter ACLs for spellchecks/suggestions on the full path
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1750809 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/LucenePropertyIndex.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/LucenePropertyIndex.java
@@ -464,7 +464,11 @@ public class LucenePropertyIndex implements AdvancedQueryIndex, QueryIndex, Nati
                             if (topDocs.totalHits > 0) {
                                 for (ScoreDoc doc : topDocs.scoreDocs) {
                                     Document retrievedDoc = searcher.doc(doc.doc);
-                                    if (filter.isAccessible(retrievedDoc.get(FieldNames.PATH))) {
+                                    String prefix = filter.getPath();
+                                    if (prefix.length() == 1) {
+                                        prefix = "";
+                                    }
+                                    if (filter.isAccessible(prefix + retrievedDoc.get(FieldNames.PATH))) {
                                         queue.add(new LuceneResultRow(suggestion.string));
                                         break;
                                     }
@@ -492,7 +496,11 @@ public class LucenePropertyIndex implements AdvancedQueryIndex, QueryIndex, Nati
                             if (topDocs.totalHits > 0) {
                                 for (ScoreDoc doc : topDocs.scoreDocs) {
                                     Document retrievedDoc = searcher.doc(doc.doc);
-                                    if (filter.isAccessible(retrievedDoc.get(FieldNames.PATH))) {
+                                    String prefix = filter.getPath();
+                                    if (prefix.length() == 1) {
+                                        prefix = "";
+                                    }
+                                    if (filter.isAccessible(prefix + retrievedDoc.get(FieldNames.PATH))) {
                                         queue.add(new LuceneResultRow(suggestion.key.toString(), suggestion.value));
                                         break;
                                     }
