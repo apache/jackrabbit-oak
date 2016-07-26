b6444689b58fcc068625315427b8258b985b430c
Francesco Mari <frm@apache.org>
2016-07-26 08:38:41 +0000

    OAK-4596 - Don't flush a segment buffer if it contains only the segment info record
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1754084 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/SegmentBufferWriterTest.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jackrabbit.oak.segment;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.apache.jackrabbit.oak.segment.SegmentWriterBuilder.segmentWriterBuilder;
+import static org.apache.jackrabbit.oak.segment.file.FileStoreBuilder.fileStoreBuilder;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.jackrabbit.oak.segment.file.FileStore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class SegmentBufferWriterTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder(new File("target"));
+
+    private FileStore openFileStore() throws Exception {
+        return fileStoreBuilder(folder.getRoot()).build();
+    }
+
+    @Test
+    public void nonDirtyBuffersShouldNotBeFlushed() throws Exception {
+        List<SegmentId> before;
+
+        try (FileStore store = openFileStore()) {
+            before = newArrayList(store.getSegmentIds());
+            segmentWriterBuilder("t").build(store).flush();
+        }
+
+        List<SegmentId> after;
+
+        try (FileStore store = openFileStore()) {
+            after = newArrayList(store.getSegmentIds());
+        }
+
+        assertEquals(before, after);
+    }
+
+    @Test
+    public void dirtyBuffersShouldBeFlushed() throws Exception {
+        List<SegmentId> before;
+
+        try (FileStore store = openFileStore()) {
+            before = newArrayList(store.getSegmentIds());
+            SegmentWriter writer = segmentWriterBuilder("t").build(store);
+            writer.writeString("test");
+            writer.flush();
+        }
+
+        List<SegmentId> after;
+
+        try (FileStore store = openFileStore()) {
+            after = newArrayList(store.getSegmentIds());
+        }
+
+        assertNotEquals(before, after);
+    }
+
+}
