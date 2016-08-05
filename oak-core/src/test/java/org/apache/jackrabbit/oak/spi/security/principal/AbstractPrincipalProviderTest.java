96e8d09657f6413b000e9e2d9ba6f392d1c26dc9
Angela Schreiber <angela@apache.org>
2016-08-05 15:59:09 +0000

    minor improvement: additional testing for oak principal management
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1755360 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/spi/security/principal/AbstractPrincipalProviderTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/spi/security/principal/AbstractPrincipalProviderTest.java
@@ -37,17 +37,61 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public abstract class AbstractPrincipalProviderTest extends AbstractSecurityTest {
 
     protected PrincipalProvider principalProvider;
 
+    private Principal userPrincipal;
+    private Principal nonExisting;
+
+    protected  String groupId;
+    protected Group testGroup;
+
+    protected String groupId2;
+    protected Group testGroup2;
+
     @Override
     public void before() throws Exception {
         super.before();
 
+        userPrincipal = getTestUser().getPrincipal();
+        nonExisting = new PrincipalImpl("nonExisting");
+
+        groupId = "testGroup" + UUID.randomUUID();
+        testGroup = getUserManager(root).createGroup(groupId);
+        testGroup.addMember(getTestUser());
+
+        groupId2 = "testGroup2" + UUID.randomUUID();
+        testGroup2 = getUserManager(root).createGroup(groupId2);
+        testGroup.addMember(testGroup2);
+
+        root.commit();
+
         principalProvider = createPrincipalProvider();
+        assertNull(principalProvider.getPrincipal(nonExisting.getName()));
+    }
+
+    @Override
+    public void after() throws Exception {
+        try {
+            root.refresh();
+            Group gr = getUserManager(root).getAuthorizable(groupId, Group.class);
+            if (gr != null) {
+                gr.remove();
+                root.commit();
+            }
+
+            gr = getUserManager(root).getAuthorizable(groupId2, Group.class);
+            if (gr != null) {
+                gr.remove();
+                root.commit();
+            }
+        } finally {
+            super.after();
+        }
     }
 
     protected abstract PrincipalProvider createPrincipalProvider();
@@ -68,11 +112,14 @@ public abstract class AbstractPrincipalProviderTest extends AbstractSecurityTest
 
     @Test
     public void testUserPrincipal() throws Exception {
-        User user = getTestUser();
-
-        Principal principal = principalProvider.getPrincipal(user.getPrincipal().getName());
+        Principal principal = principalProvider.getPrincipal(userPrincipal.getName());
 
         assertNotNull(principal);
+    }
+
+    @Test
+    public void testNonExistingPrincipal() throws Exception {
+        assertNull(principalProvider.getPrincipal(nonExisting.getName()));
     }
 
     @Test
@@ -115,19 +162,10 @@ public abstract class AbstractPrincipalProviderTest extends AbstractSecurityTest
 
     @Test
     public void testGroupPrincipal() throws Exception {
-        Group group = getUserManager(root).createGroup("testGroup" + UUID.randomUUID());
-        root.commit();
+        Principal principal = principalProvider.getPrincipal(testGroup.getPrincipal().getName());
 
-        try {
-            Principal principal = principalProvider.getPrincipal(group.getPrincipal().getName());
-
-            assertNotNull(principal);
-            assertTrue(principal instanceof java.security.acl.Group);
-
-        } finally {
-            group.remove();
-            root.commit();
-        }
+        assertNotNull(principal);
+        assertTrue(principal instanceof java.security.acl.Group);
     }
 
     @Test
@@ -149,6 +187,45 @@ public abstract class AbstractPrincipalProviderTest extends AbstractSecurityTest
                 root.commit();
             }
         }
+    }
+
+    @Test
+    public void testGetGroupMembership() throws Exception {
+        Set<java.security.acl.Group> grPrincipals  = principalProvider.getGroupMembership(userPrincipal);
+        assertEquals(2, grPrincipals.size());
+        assertTrue(grPrincipals.contains(EveryonePrincipal.getInstance()));
+        assertTrue(grPrincipals.contains(testGroup.getPrincipal()));
+    }
+
+    @Test
+    public void tstGetGroupMembershipNonExisting() {
+        Set<java.security.acl.Group> grPrincipals = principalProvider.getGroupMembership(nonExisting);
+        assertNotNull(grPrincipals);
+        assertTrue(grPrincipals.isEmpty());
+    }
+
+    @Test
+    public void testGetGroupMembershipEveryonePrincipal() {
+        Set<java.security.acl.Group> grPrincipals = principalProvider.getGroupMembership(EveryonePrincipal.getInstance());
+        assertNotNull(grPrincipals);
+        assertTrue(grPrincipals.isEmpty());
+    }
+
+    @Test
+    public void testGetGroupMembershipGroupPrincipal() throws Exception {
+        Set<java.security.acl.Group> grPrincipals = principalProvider.getGroupMembership(testGroup.getPrincipal());
+        assertNotNull(grPrincipals);
+        assertEquals(1, grPrincipals.size());
+        assertTrue(grPrincipals.contains(EveryonePrincipal.getInstance()));
+    }
+
+    @Test
+    public void testGetGroupMembershipGroupPrincipal2() throws Exception {
+        Set<java.security.acl.Group> grPrincipals = principalProvider.getGroupMembership(testGroup2.getPrincipal());
+        assertNotNull(grPrincipals);
+        assertEquals(2, grPrincipals.size());
+        assertTrue(grPrincipals.contains(testGroup.getPrincipal()));
+        assertTrue(grPrincipals.contains(EveryonePrincipal.getInstance()));
     }
 
     @Test
@@ -181,30 +258,18 @@ public abstract class AbstractPrincipalProviderTest extends AbstractSecurityTest
 
     @Test
     public void testFindGroupPrincipal() throws Exception {
-        Group testGroup = null;
-        try {
-            UserManager userMgr = getUserManager(root);
-            testGroup = userMgr.createGroup("TestGroup");
-            root.commit();
+        String principalName = testGroup.getPrincipal().getName();
+        assertNotNull(principalProvider.getPrincipal(principalName));
 
-            String principalName = testGroup.getPrincipal().getName();
-            assertNotNull(principalProvider.getPrincipal(principalName));
+        List<String> nameHints = new ArrayList<String>();
+        nameHints.add("testGroup");
+        nameHints.add("test");
+        nameHints.add("Group");
+        nameHints.add("stGr");
 
-            List<String> nameHints = new ArrayList<String>();
-            nameHints.add("TestGroup");
-            nameHints.add("Test");
-            nameHints.add("Group");
-            nameHints.add("stGr");
-
-            assertResult(principalProvider, nameHints, principalName, PrincipalManager.SEARCH_TYPE_GROUP, true);
-            assertResult(principalProvider, nameHints, principalName, PrincipalManager.SEARCH_TYPE_ALL, true);
-            assertResult(principalProvider, nameHints, principalName, PrincipalManager.SEARCH_TYPE_NOT_GROUP, false);
-        } finally {
-            if (testGroup != null) {
-                testGroup.remove();
-                root.commit();
-            }
-        }
+        assertResult(principalProvider, nameHints, principalName, PrincipalManager.SEARCH_TYPE_GROUP, true);
+        assertResult(principalProvider, nameHints, principalName, PrincipalManager.SEARCH_TYPE_ALL, true);
+        assertResult(principalProvider, nameHints, principalName, PrincipalManager.SEARCH_TYPE_NOT_GROUP, false);
     }
 
     @Test
