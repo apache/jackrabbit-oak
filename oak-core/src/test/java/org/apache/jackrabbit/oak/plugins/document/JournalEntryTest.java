ad595e82631df09c7bcf51b2481e69f1de436da0
Manfred Baedke <baedke@apache.org>
2016-08-24 18:08:10 +0000

    OAK-4682: ConcurrentModificationException in JournalEntry.TreeNode
    Merging fix into branch 1.4.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.4@1757557 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/JournalEntryTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/JournalEntryTest.java
@@ -213,4 +213,32 @@ public class JournalEntryTest {
             assertTrue(loaderCalled.get());
         }
     }
+
+    // OAK-4682
+    @Test
+    public void concurrentModification() throws Exception {
+        DocumentNodeStore store = new DocumentMK.Builder().getNodeStore();
+        try {
+            final JournalEntry entry = store.getCurrentJournalEntry();
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 100000; i++) {
+                        entry.modified("/node-" + i);
+                    }
+                }
+            });
+            t.start();
+            StringSort sort = JournalEntry.newSorter();
+            try {
+                entry.addTo(sort);
+            } finally {
+                sort.close();
+            }
+            t.join();
+        } finally {
+            store.dispose();
+        }
+    }
+
 }
