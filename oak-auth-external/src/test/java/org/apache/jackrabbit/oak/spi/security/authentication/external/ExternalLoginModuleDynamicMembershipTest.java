98bd67fc7f4f7b8e102a3868028f337973661367
Dominique Jäggi <dj@apache.org>
2016-08-25 14:50:04 +0000

    OAK-4679 - Backport OAK-4119, OAK-4101, OAK-4087 and OAK-4344 - fixing test failures on windows
    (EDIT by reschke 2017-11-03: this apparently includes the change for OAK-4397)
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.4@1757697 13f79535-47bb-0310-9956-ffa450edef68

--- oak-auth-external/src/test/java/org/apache/jackrabbit/oak/spi/security/authentication/external/ExternalLoginModuleDynamicMembershipTest.java
+++ oak-auth-external/src/test/java/org/apache/jackrabbit/oak/spi/security/authentication/external/ExternalLoginModuleDynamicMembershipTest.java
@@ -23,6 +23,7 @@ import javax.jcr.SimpleCredentials;
 import javax.jcr.Value;
 
 import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.User;
 import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.jackrabbit.oak.api.Root;
 import org.apache.jackrabbit.oak.spi.security.authentication.external.impl.ExternalIdentityConstants;
@@ -165,17 +166,19 @@ public class ExternalLoginModuleDynamicMembershipTest extends ExternalLoginModul
             // synchronized users with full membership sync.
             Root systemRoot = getSystemRoot();
             UserManager userManager = getUserManager(systemRoot);
-            Authorizable a = userManager.getAuthorizable(USER_ID);
-            a.removeProperty(ExternalIdentityConstants.REP_EXTERNAL_PRINCIPAL_NAMES);
+            User user = userManager.getAuthorizable(USER_ID, User.class);
+            user.removeProperty(ExternalIdentityConstants.REP_EXTERNAL_PRINCIPAL_NAMES);
             systemRoot.commit();
+
+            waitUntilExpired(user, systemRoot, syncConfig.user().getExpirationTime());
 
             // login again
             login(new SimpleCredentials(USER_ID, new char[0])).close();
 
             systemRoot.refresh();
-            a = userManager.getAuthorizable(USER_ID);
-            assertTrue(a.hasProperty(ExternalIdentityConstants.REP_LAST_SYNCED));
-            assertFalse(a.hasProperty(ExternalIdentityConstants.REP_EXTERNAL_PRINCIPAL_NAMES));
+            user = userManager.getAuthorizable(USER_ID, User.class);
+            assertTrue(user.hasProperty(ExternalIdentityConstants.REP_LAST_SYNCED));
+            assertFalse(user.hasProperty(ExternalIdentityConstants.REP_EXTERNAL_PRINCIPAL_NAMES));
 
             for (ExternalIdentityRef ref : idp.getUser(USER_ID).getDeclaredGroups()) {
                 assertNotNull(userManager.getAuthorizable(ref.getId()));
