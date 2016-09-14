928ac853a106ebf0a9145716ce5e96f1bc719987
Amit Jain <amitj@apache.org>
2016-09-14 09:03:33 +0000

    OAK-4712: Publish S3DataStore stats in JMX MBean
    Modified patch from Matt Ryan
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1760661 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/blob/datastore/S3DataStoreService.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/blob/datastore/S3DataStoreService.java
@@ -19,18 +19,25 @@
 
 package org.apache.jackrabbit.oak.plugins.blob.datastore;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.Map;
 import java.util.Properties;
 
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.ConfigurationPolicy;
 import org.apache.jackrabbit.core.data.DataStore;
-import org.apache.jackrabbit.oak.blob.cloud.aws.s3.S3DataStore;
+import org.apache.jackrabbit.core.data.DataStoreException;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.ComponentContext;
 
 @Component(policy = ConfigurationPolicy.REQUIRE, name = S3DataStoreService.NAME)
 public class S3DataStoreService extends AbstractDataStoreService{
     public static final String NAME = "org.apache.jackrabbit.oak.plugins.blob.datastore.S3DataStore";
+    private static final String DESCRIPTION = "oak.datastore.description";
+
+    private ServiceRegistration delegateReg;
 
     @Override
     protected DataStore createDataStore(ComponentContext context, Map<String, Object> config) {
@@ -40,7 +47,24 @@ public class S3DataStoreService extends AbstractDataStoreService{
         properties.putAll(config);
 
         dataStore.setProperties(properties);
+
+        Dictionary<String, Object> props = new Hashtable<String, Object>();
+        props.put(Constants.SERVICE_PID, dataStore.getClass().getName());
+        props.put(DESCRIPTION, getDescription());
+
+        delegateReg = context.getBundleContext().registerService(new String[] {
+            SharedS3DataStore.class.getName(),
+            SharedS3DataStore.class.getName()
+        }, dataStore , props);
+
         return dataStore;
+    }
+
+    protected void deactivate() throws DataStoreException {
+        if (delegateReg != null) {
+            delegateReg.unregister();
+        }
+        super.deactivate();
     }
 
     @Override
