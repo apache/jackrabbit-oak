818ba7443d359597c7f893a3281f5d519be4f1c1
Tomasz Rekawek <tomekr@apache.org>
2016-09-19 10:04:08 +0200

    Added MULTIPLEXED_MEM fixture.

--- oak-it/src/test/java/org/apache/jackrabbit/oak/plugins/multiplex/MultiplexingMemoryFixture.java
+++ oak-it/src/test/java/org/apache/jackrabbit/oak/plugins/multiplex/MultiplexingMemoryFixture.java
@@ -18,33 +18,25 @@
  */
 package org.apache.jackrabbit.oak.plugins.multiplex;
 
-import java.io.IOException;
-
 import org.apache.jackrabbit.oak.fixture.NodeStoreFixture;
-import org.apache.jackrabbit.oak.plugins.segment.SegmentNodeStore;
-import org.apache.jackrabbit.oak.plugins.segment.memory.MemoryStore;
+import org.apache.jackrabbit.oak.plugins.memory.MemoryNodeStore;
 import org.apache.jackrabbit.oak.spi.mount.MountInfoProvider;
 import org.apache.jackrabbit.oak.spi.state.NodeStore;
 
-public class MultiplexingSegmentFixture extends NodeStoreFixture {
+public class MultiplexingMemoryFixture extends NodeStoreFixture {
 
     private static final String MOUNT_PATH = "/tmp";
     
     @Override
     public NodeStore createNodeStore() {
-        
-        try {
-            MountInfoProvider mip = new SimpleMountInfoProvider.Builder()
-                    .mount("temp", MOUNT_PATH)
-                    .build();
-            
-            NodeStore globalStore = SegmentNodeStore.builder(new MemoryStore()).build();
-            NodeStore tempMount = SegmentNodeStore.builder(new MemoryStore()).build();
-            
-            return new MultiplexingNodeStore.Builder(mip, globalStore).addMount("temp", tempMount).build();
-        } catch (IOException e) {
-            throw new RuntimeException();
-        }
+        MountInfoProvider mip = new SimpleMountInfoProvider.Builder()
+                .mount("temp", MOUNT_PATH)
+                .build();
+
+        NodeStore globalStore = new MemoryNodeStore();
+        NodeStore tempMount = new MemoryNodeStore();
+
+        return new MultiplexingNodeStore.Builder(mip, globalStore).addMount("temp", tempMount).build();
     }
     
     @Override
