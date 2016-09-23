7d3f02e84abc5e6adc1c55d6d154743f330d6164
Amit Jain <amitj@apache.org>
2016-09-23 08:26:32 +0000

    OAK-4848: Improve oak-blob-cloud tests
    @trivial Remove unused import
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1762026 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/blob/datastore/AbstractDataStoreTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/blob/datastore/AbstractDataStoreTest.java
@@ -26,7 +26,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Properties;
 import java.util.Random;
 
 import javax.jcr.RepositoryException;
