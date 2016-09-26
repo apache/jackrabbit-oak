3e84bb7fddf2d4ff3d5d4c5889803b7a2e17baa0
Tomasz Rekawek <tomekr@apache.org>
2016-09-26 11:29:33 +0200

    Updated the package info.

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/multiplex/package-info.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/multiplex/package-info.java
@@ -10,7 +10,7 @@
  *       NodeStore should be aware of the specific implementation being used.</li>
  *   <li>Persistence-agnosticity. The multiplexing support should be applicable to any conformant
  *       NodeStore implementation.</li>
- *   <li>Neglibigle performance impact. Multiplexing should not add a significat performance overhead.</li> 
+ *   <li>Negligible performance impact. Multiplexing should not add a significat performance overhead.</li>
  * </ol>
  * </p>
  * 
@@ -19,7 +19,7 @@
  * <p>The main entry point is the {@link org.apache.jackrabbit.oak.plugins.multiplex.MultiplexingNodeStore},
  * which wraps one or more NodeStore instances. Also of interest are the {@link org.apache.jackrabbit.oak.plugins.multiplex.MultiplexingNodeState} and {@link org.apache.jackrabbit.oak.plugins.multiplex.MultiplexingNodeBuilder}.</p>
  * 
- * <p> These classes maintain internal mappings of the 'native' objects.For instance, if the 
+ * <p> These classes maintain internal mappings of the 'native' objects. For instance, if the
  * multiplexing NodeStore holds two MemoryNodeStore instances, then a call to {@linkplain org.apache.jackrabbit.oak.spi.state.NodeStore#getRoot()} 
  * will return a multiplexing NodeState backed by two MemoryNodeState instances. Similarly, a call to {@linkplain org.apache.jackrabbit.oak.spi.state.NodeState#builder()} will return a multiplexing
  * NodeBuilder backed by two MemoryNodeState instances.</p>
@@ -29,26 +29,7 @@
  * 
  * <h2>Open items</h2>
  * 
- * <p>1. Need to support clients which expect that the NodeState is an instance or subclass
- * of MemoryNodeState</p>
- * 
- * <p>Currently there are some initialisers in oak that unconditionally pass a NodeState
- * to a MemoryNodeStore:
- * 
- * <ol>
- *   <li>{@link org.apache.jackrabbit.oak.plugins.nodetype.write.InitialContent}</li>
- *   <li>{@link org.apache.jackrabbit.oak.security.privilege.PrivilegeInitializer}</li>
- *   <li>{@link org.apache.jackrabbit.oak.security.user.UserInitializer}</li>
- * </ol>
- * 
- * Since the multiplexing classes don't inherit from the memory-based classes this fails at 
- * runtime. The compromise that was reached was to add a new {@link org.apache.jackrabbit.oak.spi.state.HasNativeNodeBuilder} interface, implemented by the 
- * multiplexing NodeStore, which allows exposing the native node builder, which is a MemoryNodeBuilder
- * instance or subclass. This is not ideal though, as it forces (some) users to know about the
- * multiplexing specifics.
- * </p>
- * 
- * <p>2. Brute-force support for oak:mount nodes.</p>
+ * <p>1. Brute-force support for oak:mount nodes.</p>
  *  
  *  <p>The {@link org.apache.jackrabbit.oak.spi.mount.Mount#getPathFragmentName()} method defines
  *  a name pattern that can be used by mounted stores to contribute to a patch which is not
@@ -57,7 +38,8 @@
  *  is found.</p>
  *  
  *  <p>The current implementation naively queries all stores whenever the child node list is prepared.
- *  This is obviously correct but painfully slow.</p> 
+ *  This is obviously correct but painfully slow.
+ *  {@link org.apache.jackrabbit.oak.plugins.multiplex.MountedNodeStore#hasChildren(java.lang.String, java.lang.String)}</p>
  */
 package org.apache.jackrabbit.oak.plugins.multiplex;
 
