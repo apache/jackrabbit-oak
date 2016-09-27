a6ab9339c24b7f89f5cf598d0823239b719e857b
Amit Jain <amitj@apache.org>
2016-09-27 09:22:26 +0000

    OAK-4838: Move S3 classes to oak-blob-cloud module
    Refactor packages for common classes
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1762416 13f79535-47bb-0310-9956-ffa450edef68

--- oak-blob-cloud/src/test/java/org/apache/jackrabbit/oak/blob/cloud/aws/s3/TestS3Ds.java
+++ oak-blob-cloud/src/test/java/org/apache/jackrabbit/oak/blob/cloud/aws/s3/TestS3Ds.java
@@ -23,6 +23,7 @@ import javax.jcr.RepositoryException;
 
 import org.apache.jackrabbit.core.data.CachingDataStore;
 import org.apache.jackrabbit.oak.blob.cloud.S3DataStoreUtils;
+import org.apache.jackrabbit.oak.blob.cloud.s3.S3Constants;
 import org.apache.jackrabbit.oak.plugins.blob.datastore.AbstractDataStoreTest;
 import org.junit.After;
 import org.junit.Before;
