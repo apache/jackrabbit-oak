f55207449910f31cbb90c853bdc1c6d63c0df044
Alexandru Parvulescu <alexparvulescu@apache.org>
2016-10-05 18:01:14 +0000

    OAK-4896 OSGi config for the size based estimation is ignored
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk/oak-segment-tar@1763473 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/jackrabbit/oak/segment/SegmentNodeStoreService.java
+++ src/main/java/org/apache/jackrabbit/oak/segment/SegmentNodeStoreService.java
@@ -520,7 +520,7 @@ public class SegmentNodeStoreService extends ProxyNodeStore
         int forceTimeout = toInteger(property(COMPACTION_FORCE_TIMEOUT), FORCE_TIMEOUT_DEFAULT);
 
         byte gainThreshold = getGainThreshold();
-        long sizeDeltaEstimation = toLong(COMPACTION_SIZE_DELTA_ESTIMATION, SIZE_DELTA_ESTIMATION_DEFAULT);
+        long sizeDeltaEstimation = toLong(property(COMPACTION_SIZE_DELTA_ESTIMATION), SIZE_DELTA_ESTIMATION_DEFAULT);
 
         return new SegmentGCOptions(pauseCompaction, gainThreshold, retryCount, forceTimeout)
                 .setGcSizeDeltaEstimation(sizeDeltaEstimation);
