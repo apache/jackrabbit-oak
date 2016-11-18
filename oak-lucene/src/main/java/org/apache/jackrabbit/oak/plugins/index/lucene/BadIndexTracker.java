54b45cf2dadeb34d2dafd4362c9a9ac63b67dff3
Chetan Mehrotra <chetanm@apache.org>
2016-11-18 12:25:15 +0000

    OAK-4836 - Avoid excessive logging in case of corrupt index or mis-configured index defnition
    Adding BadIndexTracker which tracks bad indexes and controls access to them
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1770371 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/BadIndexTracker.java
@@ -0,0 +1,158 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.jackrabbit.oak.plugins.index.lucene;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Stopwatch;
+import com.google.common.base.Throwables;
+import com.google.common.base.Ticker;
+import com.google.common.collect.Maps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class BadIndexTracker {
+    /**
+     * Time interval in millis after which a bad index would be accessed again
+     * to check if it has been fixed
+     */
+    private static final long DEFAULT_RECHECK_INTERVAL = TimeUnit.MINUTES.toMillis(15);
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final Map<String, BadIndexInfo> badIndexes = Maps.newConcurrentMap();
+    private final long recheckIntervalMillis;
+    private Ticker ticker = Ticker.systemTicker();
+    private int indexerCycleCount;
+
+    public BadIndexTracker() {
+        this(DEFAULT_RECHECK_INTERVAL);
+    }
+
+    public BadIndexTracker(long recheckIntervalMillis) {
+        this.recheckIntervalMillis = recheckIntervalMillis;
+        log.info("Bad Index recheck interval set to {} seconds",
+                TimeUnit.MILLISECONDS.toSeconds(recheckIntervalMillis));
+    }
+
+    public void markGoodIndexes(Set<String> updatedIndexPaths) {
+        indexerCycleCount++;
+        for (String indexPath : updatedIndexPaths) {
+            markGoodIndex(indexPath);
+        }
+    }
+
+    public void markGoodIndex(String indexPath) {
+        BadIndexInfo info = badIndexes.remove(indexPath);
+        if (info != null) {
+            log.info("Index [{}] which was not working {} is found to be healthy again",
+                    indexPath, info.getStats());
+        }
+    }
+
+    public void markBadIndex(String path, Throwable e) {
+        BadIndexInfo badIndex = badIndexes.get(path);
+        if (badIndex == null) {
+            badIndexes.put(path, new BadIndexInfo(path, e));
+            log.error("Could not access the Lucene index at [{}]", path, e);
+        } else {
+            badIndex.failedAccess(e);
+            log.error("Could not access the Lucene index at [{}] . {}",
+                    path, badIndex.getStats(), e);
+        }
+    }
+
+    public boolean isIgnoredBadIndex(String path) {
+        BadIndexInfo badIdx = badIndexes.get(path);
+        if (badIdx == null) {
+            return false;
+        }
+        return !badIdx.tryAgain();
+    }
+
+    public Set<String> getIndexPaths() {
+        return badIndexes.keySet();
+    }
+
+    public long getRecheckIntervalMillis() {
+        return recheckIntervalMillis;
+    }
+
+    BadIndexInfo getInfo(String indexPath){
+        return badIndexes.get(indexPath);
+    }
+
+    void setTicker(Ticker ticker) {
+        this.ticker = ticker;
+    }
+
+    class BadIndexInfo {
+        final String path;
+        final int lastIndexerCycleCount = indexerCycleCount;
+        private final Stopwatch created = Stopwatch.createStarted(ticker);
+        private final Stopwatch watch = Stopwatch.createStarted(ticker);
+        private String exception;
+        private int accessCount;
+        private int failedAccessCount;
+
+        public BadIndexInfo(String path, Throwable e) {
+            this.path = path;
+            this.exception = Throwables.getStackTraceAsString(e);
+        }
+
+        public boolean tryAgain() {
+            accessCount++;
+
+            if (watch.elapsed(TimeUnit.MILLISECONDS) > recheckIntervalMillis) {
+                watch.reset().start();
+                return true;
+            }
+
+            if (log.isDebugEnabled()) {
+                log.debug("Ignoring index [{}] which is not working correctly {}", path, getStats());
+            }
+            return false;
+        }
+
+        public String getStats() {
+            return String.format("since %s ,%d indexing cycles, accessed %d times",
+                    created, getCycleCount(), accessCount);
+        }
+
+        public int getFailedAccessCount() {
+            return failedAccessCount;
+        }
+
+        public int getAccessCount() {
+            return accessCount;
+        }
+
+        private int getCycleCount() {
+            return indexerCycleCount - lastIndexerCycleCount;
+        }
+
+        public void failedAccess(Throwable e) {
+            failedAccessCount++;
+            exception = Throwables.getStackTraceAsString(e);
+        }
+    }
+
+}
--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/IndexTrackerTest.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/IndexTrackerTest.java
@@ -19,19 +19,31 @@
 
 package org.apache.jackrabbit.oak.plugins.index.lucene;
 
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
 import com.google.common.collect.ImmutableSet;
+import org.apache.jackrabbit.oak.api.Type;
+import org.apache.jackrabbit.oak.commons.PathUtils;
 import org.apache.jackrabbit.oak.plugins.index.IndexUpdateProvider;
+import org.apache.jackrabbit.oak.plugins.memory.ArrayBasedBlob;
+import org.apache.jackrabbit.oak.plugins.memory.PropertyStates;
 import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
 import org.apache.jackrabbit.oak.spi.commit.EditorHook;
 import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
+import org.apache.jackrabbit.oak.plugins.index.lucene.BadIndexTracker.BadIndexInfo;
 import org.junit.Test;
 
 import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.INDEX_DEFINITIONS_NAME;
 import static org.apache.jackrabbit.oak.plugins.index.lucene.util.LuceneIndexHelper.newLucenePropertyIndexDefinition;
 import static org.apache.jackrabbit.oak.plugins.nodetype.write.InitialContent.INITIAL_CONTENT;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+@SuppressWarnings("UnusedAssignment")
 public class IndexTrackerTest {
     private static final EditorHook HOOK = new EditorHook(
             new IndexUpdateProvider(
@@ -67,6 +79,101 @@ public class IndexTrackerTest {
         tracker.update(indexed);
         //Post refresh size should be 0 as all are closed
         assertEquals(0, tracker.getIndexNodePaths().size());
+    }
+
+    @Test
+    public void badIndexAccess() throws Exception{
+        createIndex("foo");
+
+        //1. Create and populate index
+        NodeState before = builder.getNodeState();
+        builder.setProperty("foo", "bar");
+        NodeState after = builder.getNodeState();
+
+        NodeState indexed = HOOK.processCommit(before, after, CommitInfo.EMPTY);
+        tracker.update(indexed);
+
+        IndexNode indexNode = tracker.acquireIndexNode("/oak:index/foo");
+        indexNode.release();
+
+        assertTrue(tracker.getBadIndexTracker().getIndexPaths().isEmpty());
+
+        //2. Corrupt the index
+        builder = indexed.builder();
+        indexed = corruptIndex("/oak:index/foo");
+
+        //3. Recreate the tracker as we cannot push corrupt index in existing tracker
+        //As diffAndUpdate would fail and existing IndexNode would not be changed
+        tracker = new IndexTracker();
+        tracker.update(indexed);
+
+        VirtualTicker ticker = new VirtualTicker();
+        tracker.getBadIndexTracker().setTicker(ticker);
+
+        indexNode = tracker.acquireIndexNode("/oak:index/foo");
+
+        //Index must be corrupted hence it must be null
+        assertNull(indexNode);
+        assertTrue(tracker.getBadIndexTracker().getIndexPaths().contains("/oak:index/foo"));
+
+        BadIndexInfo badIdxInfo = tracker.getBadIndexTracker().getInfo("/oak:index/foo");
+        assertNotNull(badIdxInfo);
+        assertEquals(0, badIdxInfo.getAccessCount());
+
+        //Try to access again
+        indexNode = tracker.acquireIndexNode("/oak:index/foo");
+        assertEquals(1, badIdxInfo.getAccessCount());
+        assertEquals(0, badIdxInfo.getFailedAccessCount());
+
+        indexNode = tracker.acquireIndexNode("/oak:index/foo");
+        assertEquals(2, badIdxInfo.getAccessCount());
+        assertEquals(0, badIdxInfo.getFailedAccessCount());
+
+        //5. Move clock forward
+        ticker.addTime(tracker.getBadIndexTracker().getRecheckIntervalMillis() + 1, TimeUnit.MILLISECONDS);
+
+        //Now index access must be attempted again
+        indexNode = tracker.acquireIndexNode("/oak:index/foo");
+        assertEquals(3, badIdxInfo.getAccessCount());
+        assertEquals(1, badIdxInfo.getFailedAccessCount());
+
+        //6. Now lets reindex to fix the corruption
+        builder = indexed.builder();
+        before = indexed;
+        after = reindex("/oak:index/foo");
+
+        indexed = HOOK.processCommit(before, after, CommitInfo.EMPTY);
+        tracker.update(indexed);
+
+        //7. Now indexNode should be accessible
+        indexNode = tracker.acquireIndexNode("/oak:index/foo");
+        assertNotNull(indexNode);
+
+        //And this index would not be considered bad
+        badIdxInfo = tracker.getBadIndexTracker().getInfo("/oak:index/foo");
+        assertNull(badIdxInfo);
+    }
+
+    private NodeState corruptIndex(String indexPath) {
+        NodeBuilder dir = TestUtil.child(builder, PathUtils.concat(indexPath, ":data"));
+        for (String name : dir.getChildNodeNames()){
+            if (!"segments.gen".equals(name)){
+                dir.getChildNode(name).setProperty(PropertyStates.createProperty("jcr:data", Collections
+                        .singletonList(new ArrayBasedBlob("foo".getBytes())), Type.BINARIES));
+            }
+        }
+        return builder.getNodeState();
+    }
+
+    private NodeState reindex(String indexPath){
+        NodeBuilder dir = TestUtil.child(builder, indexPath);
+        dir.setProperty("reindex", true);
+        return builder.getNodeState();
+    }
+
+    private void createIndex(String propName){
+        NodeBuilder index = builder.child(INDEX_DEFINITIONS_NAME);
+        newLucenePropertyIndexDefinition(index, propName, ImmutableSet.of(propName), null);
     }
 
 }
