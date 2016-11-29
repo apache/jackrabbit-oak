1e2d4d50f67a1752eaf3d8dd38ffe4deb8684e24
Thomas Mueller <thomasm@apache.org>
2016-11-29 14:29:28 +0000

    OAK-5185 Improved JMX descriptions for the query engine
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1771911 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/api/jmx/QueryEngineSettingsMBean.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/api/jmx/QueryEngineSettingsMBean.java
@@ -16,6 +16,8 @@
  */
 package org.apache.jackrabbit.oak.api.jmx;
 
+import org.apache.jackrabbit.oak.commons.jmx.Description;
+
 import aQute.bnd.annotation.ProviderType;
 
 @ProviderType
@@ -29,6 +31,8 @@ public interface QueryEngineSettingsMBean {
      * 
      * @return the limit
      */
+    @Description("Get the limit on how many nodes a query may read at most into memory, for " +
+            "\"order by\" and \"distinct\" queries. If this limit is exceeded, the query throws an exception.")    
     long getLimitInMemory();
     
     /**
@@ -45,6 +49,9 @@ public interface QueryEngineSettingsMBean {
      * 
      * @return the limit
      */
+    @Description("Get the limit on how many nodes a query may read at most (raw read " +
+            "operations, including skipped nodes). If this limit is exceeded, the " +
+            "query throws an exception.")    
     long getLimitReads();
     
     /**
@@ -60,6 +67,8 @@ public interface QueryEngineSettingsMBean {
      * 
      * @return true if they fail
      */
+    @Description("Whether queries that don't use an index will fail (throw an exception). " +
+            "The default is false.")    
     boolean getFailTraversal();
 
     /**
@@ -70,10 +79,11 @@ public interface QueryEngineSettingsMBean {
     void setFailTraversal(boolean failTraversal);
 
     /**
-     * Whether the query result size should return an estimation (or -1 if disabled) for large queries
+     * Whether the query result size should return an estimation for large queries.
      *
      * @return true if enabled
      */
+    @Description("Whether the query result size should return an estimation for large queries.")    
     boolean isFastQuerySize();
 
     void setFastQuerySize(boolean fastQuerySize);
