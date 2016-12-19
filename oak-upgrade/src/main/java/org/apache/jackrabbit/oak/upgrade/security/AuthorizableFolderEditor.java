eba742d0880aedc1c728c3b7c66a4ce987be0de1
Tomasz Rekawek <tomekr@apache.org>
2016-12-19 10:30:38 +0000

    @trivial updated javadoc syntax in oak-upgrade
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.4@1775050 13f79535-47bb-0310-9956-ffa450edef68

--- oak-upgrade/src/main/java/org/apache/jackrabbit/oak/upgrade/security/AuthorizableFolderEditor.java
+++ oak-upgrade/src/main/java/org/apache/jackrabbit/oak/upgrade/security/AuthorizableFolderEditor.java
@@ -36,7 +36,7 @@ import static org.apache.jackrabbit.oak.spi.security.user.UserConstants.NT_REP_A
  * There are occasions where in old JR2 repositories not all ancestors on
  * the users path are of type {@code rep:AuthorizableFolder}, thus leading
  * to exceptions after repository upgrade.
- * <br/>
+ * <br>
  * In order to avoid such situations, this hook verifies that all nodes on
  * the users and groups paths are of type {@code rep:AuthorizableFolder} and
  * fixes the node-type if it is incorrect.
