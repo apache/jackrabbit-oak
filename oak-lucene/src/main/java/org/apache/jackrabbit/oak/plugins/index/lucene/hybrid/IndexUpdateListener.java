b76051488305e3a4b95f32f5263b1b5d904590ec
Chetan Mehrotra <chetanm@apache.org>
2017-01-11 05:21:39 +0000

    OAK-4808 - Index external changes as part of NRT indexing
    Make the classes package scoped
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1778243 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/hybrid/IndexUpdateListener.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/hybrid/IndexUpdateListener.java
@@ -19,7 +19,7 @@
 
 package org.apache.jackrabbit.oak.plugins.index.lucene.hybrid;
 
-public interface IndexUpdateListener extends ReaderRefreshPolicy {
+interface IndexUpdateListener extends ReaderRefreshPolicy {
 
     void updated();
 }
