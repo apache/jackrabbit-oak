4bcec8ccc79e156129a07115c2eca91ce4972ecc
Chetan Mehrotra <chetanm@apache.org>
2017-01-16 06:49:05 +0000

    OAK-5418 - Test failure: TomcatIT.testTomcat()
    Use random port for starting Tomcat to avoid conflicts in CI setups
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1778968 13f79535-47bb-0310-9956-ffa450edef68

--- oak-examples/webapp/src/test/java/org/apache/jackrabbit/j2ee/TomcatIT.java
+++ oak-examples/webapp/src/test/java/org/apache/jackrabbit/j2ee/TomcatIT.java
@@ -70,7 +70,7 @@ public class TomcatIT extends TestCase {
         File repoDir = new File("target", "repository");
         FileUtils.deleteQuietly(repoDir);
 
-        url = new URL("http://localhost:12856/");
+        url = new URL("http://localhost:"+getPort()+"/");
 
         tomcat = new Tomcat();
         tomcat.setSilent(true);
@@ -127,6 +127,10 @@ public class TomcatIT extends TestCase {
         client.close();
 
         tomcat.stop();
+    }
+
+    private static int getPort() {
+        return Integer.getInteger("tomcat.http.port", 12856);
     }
 
 }
