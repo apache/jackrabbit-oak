82217d9f02840eb6e658d6198b23a8b33a6a420c
Julian Reschke <reschke@apache.org>
2017-01-19 15:50:30 +0000

    OAK-3683: BasicDocumentStore.testInterestingStrings failure on MongoDB with Java 8
    remove unused imports
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1779484 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/BasicDocumentStoreTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/BasicDocumentStoreTest.java
@@ -16,14 +16,12 @@
  */
 package org.apache.jackrabbit.oak.plugins.document;
 
-import static com.google.common.base.StandardSystemProperty.JAVA_SPECIFICATION_VERSION;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.junit.Assume.assumeFalse;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
