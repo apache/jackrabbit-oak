226ad47aa8e06492729e33d63872946f8861e08c
Andrei Dulceanu <adulceanu@apache.org>
2017-02-22 11:24:47 +0000

    OAK-5600 - Test coverage for CheckCommand Added (disabled) test for checking an invalid repository
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1784012 13f79535-47bb-0310-9956-ffa450edef68

--- oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/file/tooling/CheckValidRepositoryTest.java
+++ oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/file/tooling/CheckValidRepositoryTest.java
@@ -18,66 +18,22 @@
  */
 package org.apache.jackrabbit.oak.segment.file.tooling;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Random;
 import java.util.Set;
 
-import org.apache.jackrabbit.oak.segment.SegmentNodeStore;
-import org.apache.jackrabbit.oak.segment.SegmentNodeStoreBuilders;
-import org.apache.jackrabbit.oak.segment.file.FileStore;
-import org.apache.jackrabbit.oak.segment.file.FileStoreBuilder;
 import org.apache.jackrabbit.oak.segment.tool.Check;
-import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
-import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
-import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Lists;
 
 /**
- * Tests for {@link CheckCommand}
+ * Tests for {@link CheckCommand} assuming a valid repository.
  */
-public class CheckValidRepositoryTest {
-    private static final Logger log = LoggerFactory.getLogger(CheckValidRepositoryTest.class);
-    
-    @Rule
-    public final TemporaryFolder temporaryFolder = new TemporaryFolder(new File("target"));
+public class CheckValidRepositoryTest extends CheckRepositoryTestBase {
 
-    @Before
-    public void setup() throws Exception {
-        FileStore fileStore = FileStoreBuilder.fileStoreBuilder(temporaryFolder.getRoot())
-                .withMaxFileSize(256)
-                .withSegmentCacheSize(64)
-                .build();
-        
-        SegmentNodeStore nodeStore = SegmentNodeStoreBuilders.builder(fileStore).build();
-        NodeBuilder builder = nodeStore.getRoot().builder();
-        
-        addChildWithBlobProperties(nodeStore, builder, "a", 5);
-        addChildWithBlobProperties(nodeStore, builder, "b", 10);
-        addChildWithBlobProperties(nodeStore, builder, "c", 15);
-        
-        addChildWithProperties(nodeStore, builder, "d", 5);
-        addChildWithProperties(nodeStore, builder, "e", 5);
-        addChildWithProperties(nodeStore, builder, "f", 5);
-        
-        nodeStore.merge(builder, EmptyHook.INSTANCE, CommitInfo.EMPTY);
-        fileStore.close();
-    }
-    
     @Test
     public void testSuccessfulFullCheckWithBinaryTraversal() throws Exception {
         StringWriter strOut = new StringWriter();
@@ -265,38 +221,5 @@ public class CheckValidRepositoryTest {
         
         assertExpectedOutput(strOut.toString(), Lists.newArrayList("Broken revision", "Checked 2 nodes and 10 properties", "No good revision found"));
         assertExpectedOutput(strErr.toString(), Lists.newArrayList("Invalid path: /g"));
-    }
-    
-    private static void assertExpectedOutput(String message, List<String> assertMessages) {
-        log.info("Assert message: {}", assertMessages);
-        log.info("Message logged: {}", message);
-
-        
-        for (String msg : assertMessages) {
-            Assert.assertTrue(message.contains(msg));
-        }
-    }
-    
-    private static void addChildWithBlobProperties(SegmentNodeStore nodeStore, NodeBuilder builder, String childName,
-            int propCount) throws IOException {
-        NodeBuilder child = builder.child(childName);
-        for (int i = 0; i < propCount; i++) {
-            child.setProperty(childName + i, nodeStore.createBlob(randomStream(i, 2000)));
-        }
-    }
-
-    private static void addChildWithProperties(SegmentNodeStore nodeStore, NodeBuilder builder, String childName,
-            int propCount) throws IOException {
-        NodeBuilder child = builder.child(childName);
-        for (int i = 0; i < propCount; i++) {
-            child.setProperty(childName + i, childName + i);
-        }
-    }
-    
-    private static InputStream randomStream(int seed, int size) {
-        Random r = new Random(seed);
-        byte[] data = new byte[size];
-        r.nextBytes(data);
-        return new ByteArrayInputStream(data);
     }
 }
