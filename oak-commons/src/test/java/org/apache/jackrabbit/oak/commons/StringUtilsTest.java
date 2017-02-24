ebea8a9817849c178510a3586136227fd1fc0445
Michael Dürig <mduerig@apache.org>
2017-02-24 15:46:30 +0000

    OAK-5357: StringUtils conversion functions can throw NullPointerException Make the NPE explicit and add test coverage
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1784288 13f79535-47bb-0310-9956-ffa450edef68

--- oak-commons/src/test/java/org/apache/jackrabbit/oak/commons/StringUtilsTest.java
+++ oak-commons/src/test/java/org/apache/jackrabbit/oak/commons/StringUtilsTest.java
@@ -16,34 +16,57 @@
  */
 package org.apache.jackrabbit.oak.commons;
 
-import com.google.common.collect.Maps;
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.util.Map;
+
+import com.google.common.collect.Maps;
+import org.junit.Test;
 
 /**
  * Test the string utilities.
  */
-public class StringUtilsTest extends TestCase {
+public class StringUtilsTest {
 
-    public void testHex() {
-        assertEquals("0123", StringUtils.convertBytesToHex(new byte[]{(byte) 0x01, (byte) 0x23}));
-        assertEquals("89bd", StringUtils.convertBytesToHex(new byte[]{(byte) 0x89, (byte) 0xbd}));
-        assertEquals("face", StringUtils.convertBytesToHex(new byte[]{(byte) 0xfa, (byte) 0xce}));
-        IOUtilsTest.assertEquals(new byte[]{(byte) 0xfa, (byte) 0xce}, StringUtils.convertHexToBytes("face"));
-        IOUtilsTest.assertEquals(new byte[]{(byte) 0xfa, (byte) 0xce}, StringUtils.convertHexToBytes("fAcE"));
-        IOUtilsTest.assertEquals(new byte[]{(byte) 0xfa, (byte) 0xce}, StringUtils.convertHexToBytes("FaCe"));
-        IOUtilsTest.assertEquals(new byte[]{(byte) 0x09, (byte) 0xaf}, StringUtils.convertHexToBytes("09af"));
+    @Test
+    public void testBytesToHex() {
+        assertEquals("0123", StringUtils.convertBytesToHex(new byte[] {(byte) 0x01, (byte) 0x23}));
+        assertEquals("89bd", StringUtils.convertBytesToHex(new byte[] {(byte) 0x89, (byte) 0xbd}));
+        assertEquals("face", StringUtils.convertBytesToHex(new byte[] {(byte) 0xfa, (byte) 0xce}));
+        assertEquals("", StringUtils.convertBytesToHex(new byte[] {}));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullToHex() {
+        StringUtils.convertBytesToHex(null);
+    }
+
+    @Test
+    public void testHexToBytes() {
+        IOUtilsTest.assertEquals(new byte[] {(byte) 0xfa, (byte) 0xce}, StringUtils.convertHexToBytes("face"));
+        IOUtilsTest.assertEquals(new byte[] {(byte) 0xfa, (byte) 0xce}, StringUtils.convertHexToBytes("fAcE"));
+        IOUtilsTest.assertEquals(new byte[] {(byte) 0xfa, (byte) 0xce}, StringUtils.convertHexToBytes("FaCe"));
+        IOUtilsTest.assertEquals(new byte[] {(byte) 0x09, (byte) 0xaf}, StringUtils.convertHexToBytes("09af"));
+        IOUtilsTest.assertEquals(new byte[] {}, StringUtils.convertHexToBytes(""));
+    }
+
+    @Test
+    public void testInvalidHexToBytes() {
         for (String s : new String[]{"120", "1/", "9:", "fast", "a`", "ag", "0@", "aG"}) {
             try {
                 StringUtils.convertHexToBytes(s);
                 fail();
-            } catch (IllegalArgumentException e) {
-                // expected
-            }
+            } catch (IllegalArgumentException expected) { }
         }
     }
 
+    @Test(expected = NullPointerException.class)
+    public void testNullToBytes() {
+        StringUtils.convertHexToBytes(null);
+    }
+
+    @Test
     public void testEstimateMemoryUsage() {
         final Map<String, Integer> testStrings = Maps.newHashMap();
         testStrings.put(null, 0);
