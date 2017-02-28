68701cef24da824280a45e476aff280479dd94cd
Angela Schreiber <angela@apache.org>
2017-02-28 14:31:55 +0000

    OAK-5793 : Improve coverage for security code in oak-core (wip)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1784743 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/spi/security/principal/CompositePrincipalProviderTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/spi/security/principal/CompositePrincipalProviderTest.java
@@ -16,16 +16,38 @@
  */
 package org.apache.jackrabbit.oak.spi.security.principal;
 
+import java.security.Principal;
+import java.security.acl.Group;
+import java.util.Iterator;
+import javax.annotation.Nonnull;
+
+import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
-import org.apache.jackrabbit.oak.AbstractSecurityTest;
-import org.apache.jackrabbit.oak.namepath.NamePathMapper;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import org.apache.jackrabbit.api.security.principal.PrincipalManager;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
-public class CompositePrincipalProviderTest extends AbstractSecurityTest{
+public class CompositePrincipalProviderTest {
+
+    private final TestPrincipalProvider pp1 = new TestPrincipalProvider();
+    private final TestPrincipalProvider pp2 = new TestPrincipalProvider("p1", "p2");
+    private final PrincipalProvider cpp = CompositePrincipalProvider.of(ImmutableList.<PrincipalProvider>of(pp1, pp2));
+
+    private Iterable<Principal> testPrincipals() {
+        return Iterables.concat(pp1.getTestPrincipals(), pp2.getTestPrincipals());
+    }
+
+    private static void assertIterator(@Nonnull Iterable<? extends Principal> expected, @Nonnull Iterator<? extends Principal> result) {
+        assertEquals(ImmutableSet.copyOf(expected), ImmutableSet.copyOf(result));
+    }
 
     @Test
     public void testOfEmptyList() {
@@ -34,16 +56,83 @@ public class CompositePrincipalProviderTest extends AbstractSecurityTest{
 
     @Test
     public void testOfSingletonList() {
-        PrincipalProvider pp = getSecurityProvider().getConfiguration(PrincipalConfiguration.class).getPrincipalProvider(root, NamePathMapper.DEFAULT);
+        PrincipalProvider pp = new TestPrincipalProvider(true);
         assertSame(pp, CompositePrincipalProvider.of(ImmutableList.of(pp)));
     }
 
     @Test
     public void testOfList() {
-        PrincipalProvider pp = getSecurityProvider().getConfiguration(PrincipalConfiguration.class).getPrincipalProvider(root, NamePathMapper.DEFAULT);
+        assertNotSame(pp1, cpp);
+        assertNotSame(pp2, cpp);
+        assertTrue(cpp instanceof CompositePrincipalProvider);
+    }
 
-        PrincipalProvider of = CompositePrincipalProvider.of(ImmutableList.of(pp, pp));
-        assertNotSame(pp, of);
-        assertTrue(of instanceof CompositePrincipalProvider);
+    @Test
+    public void getPrincipalUnknown() {
+        assertNull(cpp.getPrincipal(TestPrincipalProvider.UNKNOWN.getName()));
+    }
+
+    @Test
+    public void getPrincipal() {
+        for (Principal principal : testPrincipals()) {
+            assertEquals(principal, cpp.getPrincipal(principal.getName()));
+        }
+    }
+
+    @Test
+    public void getGroupMembership() {
+        for (Principal principal : testPrincipals()) {
+            boolean atleastEveryone = cpp.getGroupMembership(principal).contains(EveryonePrincipal.getInstance());
+            assertTrue("All principals (except everyone) must be member of the everyone group. Violation: "+principal.getName(), atleastEveryone);
+        }
+    }
+
+    @Test
+    public void getGroupMembershipUnknown() {
+        assertTrue(cpp.getGroupMembership(TestPrincipalProvider.UNKNOWN).isEmpty());
+    }
+
+    @Test
+    public void testGetPrincipalsByUnknownId() {
+        assertTrue(cpp.getPrincipals(TestPrincipalProvider.getIDFromPrincipal(TestPrincipalProvider.UNKNOWN)).isEmpty());
+    }
+
+    @Test
+    public void findPrincipalsUnknown() {
+        assertFalse(cpp.findPrincipals(TestPrincipalProvider.UNKNOWN.getName(), PrincipalManager.SEARCH_TYPE_ALL).hasNext());
+        assertFalse(cpp.findPrincipals(TestPrincipalProvider.UNKNOWN.getName(), PrincipalManager.SEARCH_TYPE_NOT_GROUP).hasNext());
+        assertFalse(cpp.findPrincipals(TestPrincipalProvider.UNKNOWN.getName(), PrincipalManager.SEARCH_TYPE_GROUP).hasNext());
+    }
+
+    @Test
+    public void findPrincipalsByTypeGroup() {
+        Iterable<? extends Principal> expected = Iterables.concat(ImmutableSet.of(EveryonePrincipal.getInstance()), Iterables.filter(testPrincipals(), new Predicate<Principal>() {
+            @Override
+            public boolean apply(Principal input) {
+                return input instanceof Group;
+            }
+        }));
+
+        Iterator<? extends Principal> result = cpp.findPrincipals(PrincipalManager.SEARCH_TYPE_GROUP);
+        assertIterator(expected, result);
+    }
+
+    @Test
+    public void findPrincipalsByTypeNotGroup() {
+        Iterable<? extends Principal> expected = Iterables.filter(testPrincipals(), new Predicate<Principal>() {
+            @Override
+            public boolean apply(Principal input) {
+                return !(input instanceof Group);
+            }
+        });
+
+        Iterator<? extends Principal> result = cpp.findPrincipals(PrincipalManager.SEARCH_TYPE_NOT_GROUP);
+        assertIterator(expected, result);
+    }
+
+    @Test
+    public void findPrincipalsByTypeAll() {
+        Iterator<? extends Principal> result = cpp.findPrincipals(PrincipalManager.SEARCH_TYPE_ALL);
+        assertIterator(Iterables.concat(ImmutableSet.of(EveryonePrincipal.getInstance()), testPrincipals()), result);
     }
 }
