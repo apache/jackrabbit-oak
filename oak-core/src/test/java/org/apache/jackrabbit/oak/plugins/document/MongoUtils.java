f0d0a8b95965c9ab6a48f272cd8a468ed4c34ac7
Marcel Reutegger <mreutegg@apache.org>
2017-03-01 16:21:56 +0000

    OAK-5870: Reduce serverSelectionTimeoutMS for tests on MongoDB
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1784985 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/MongoUtils.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/MongoUtils.java
@@ -37,7 +37,8 @@ public class MongoUtils {
             System.getProperty("mongo.db", "MongoMKDB");
 
     public static final String URL =
-            System.getProperty("mongo.url", "mongodb://" + HOST + ":" + PORT + "/" + DB + "?connectTimeoutMS=3000");
+            System.getProperty("mongo.url", "mongodb://" + HOST + ":" + PORT + "/" + DB +
+                    "?connectTimeoutMS=3000&serverSelectionTimeoutMS=3000");
 
     protected static Exception exception;
 
