4f5f245a8d31a10e7f76fad64b8484216e9e49c0
Angela Schreiber <angela@apache.org>
2017-04-05 08:27:57 +0000

    OAK-5882 : Improve coverage for oak.security code in oak-core (wip)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1790189 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/security/user/UserImporterMembershipAbortTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/security/user/UserImporterMembershipAbortTest.java
@@ -19,8 +19,17 @@ package org.apache.jackrabbit.oak.security.user;
 import javax.jcr.nodetype.ConstraintViolationException;
 
 import com.google.common.collect.ImmutableList;
+import org.apache.jackrabbit.oak.api.PropertyState;
+import org.apache.jackrabbit.oak.api.Tree;
+import org.apache.jackrabbit.oak.api.Type;
+import org.apache.jackrabbit.oak.spi.security.principal.EveryonePrincipal;
 import org.apache.jackrabbit.oak.spi.xml.ImportBehavior;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 public class UserImporterMembershipAbortTest extends UserImporterMembershipIgnoreTest {
 
@@ -38,6 +47,28 @@ public class UserImporterMembershipAbortTest extends UserImporterMembershipIgnor
     @Test(expected = ConstraintViolationException.class)
     public void testMixedMembers() throws Exception {
         importer.startChildInfo(createNodeInfo("memberRef", NT_REP_MEMBER_REFERENCES), ImmutableList.of(createPropInfo(REP_MEMBERS, unknownContentId, knownMemberContentId)));
+        importer.processReferences();
+    }
+
+    @Test(expected = ConstraintViolationException.class)
+    public void testAddSameAsMember() throws Exception {
+        String contentId = userProvider.getContentID(groupTree);
+
+        // NOTE: reversed over of import compared to 'testNewMembers'
+        assertTrue(importer.handlePropInfo(groupTree, createPropInfo(REP_MEMBERS, contentId), mockPropertyDefinition(NT_REP_MEMBER_REFERENCES, true)));
+        importer.processReferences();
+    }
+
+    @Test(expected = ConstraintViolationException.class)
+    public void testNewMembersToEveryone() throws Exception {
+        groupTree.setProperty(REP_MEMBERS, ImmutableList.of(knownMemberContentId), Type.STRINGS);
+        groupTree.setProperty(REP_PRINCIPAL_NAME, EveryonePrincipal.NAME);
+
+        Tree userTree = createUserTree();
+        String contentId = userProvider.getContentID(userTree);
+
+        assertTrue(importer.handlePropInfo(userTree, createPropInfo(REP_AUTHORIZABLE_ID, TEST_USER_ID), mockPropertyDefinition(NT_REP_AUTHORIZABLE, false)));
+        assertTrue(importer.handlePropInfo(groupTree, createPropInfo(REP_MEMBERS, contentId), mockPropertyDefinition(NT_REP_MEMBER_REFERENCES, true)));
         importer.processReferences();
     }
 }
