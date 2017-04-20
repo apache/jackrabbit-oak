c6cef922b36c1a530d0b71cc4f9de795a90fdd4c
Angela Schreiber <angela@apache.org>
2017-04-20 15:05:18 +0000

    OAK-6092 Find a new home for ApproximateCounter
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1792070 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/counter/ApproximateCounterTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/counter/ApproximateCounterTest.java
@@ -14,13 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.oak.util;
+package org.apache.jackrabbit.oak.plugins.index.counter;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Random;
 
+import org.apache.jackrabbit.oak.plugins.index.counter.ApproximateCounter;
 import org.junit.Test;
 
 public class ApproximateCounterTest {
