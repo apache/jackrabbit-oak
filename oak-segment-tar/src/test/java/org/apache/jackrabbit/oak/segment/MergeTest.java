0ce46611bbea51ef49048b1653eb25e27f249cb0
Andrei Dulceanu <adulceanu@apache.org>
2017-04-27 09:55:46 +0000

    OAK-6074 - Simplify merge logic in LockBasedScheduler
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1792857 13f79535-47bb-0310-9956-ffa450edef68

--- oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/MergeTest.java
+++ oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/MergeTest.java
@@ -20,23 +20,15 @@ package org.apache.jackrabbit.oak.segment;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.IOException;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import javax.annotation.Nonnull;
 
 import org.apache.jackrabbit.oak.api.CommitFailedException;
 import org.apache.jackrabbit.oak.segment.memory.MemoryStore;
-import org.apache.jackrabbit.oak.spi.commit.CommitHook;
 import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
 import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
 import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
-import org.apache.jackrabbit.oak.spi.state.NodeState;
 import org.apache.jackrabbit.oak.spi.state.NodeStore;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class MergeTest {
@@ -85,60 +77,5 @@ public class MergeTest {
 
         assertTrue(store.getRoot().hasProperty("foo"));
         assertTrue(store.getRoot().hasProperty("bar"));
-    }
-
-    @Test
-    @Ignore("OAK-4122")
-    public void testPessimisticMerge() throws Exception {
-        final SegmentNodeStore store = SegmentNodeStoreBuilders.builder(new MemoryStore()).build();
-        final Semaphore semaphore = new Semaphore(0);
-        final AtomicBoolean running = new AtomicBoolean(true);
-
-        Thread background = new Thread() {
-            @Override
-            public void run() {
-                for (int i = 0; running.get(); i++) {
-                    try {
-                        NodeBuilder a = store.getRoot().builder();
-                        a.setProperty("foo", "abc" + i);
-                        store.merge(a, EmptyHook.INSTANCE, CommitInfo.EMPTY);
-                        semaphore.release();
-                    } catch (CommitFailedException e) {
-                        fail();
-                    }
-                }
-            }
-        };
-        background.start();
-
-        // wait for the first commit
-        semaphore.acquire();
-
-        assertTrue(store.getRoot().hasProperty("foo"));
-        assertFalse(store.getRoot().hasProperty("bar"));
-
-        NodeBuilder b = store.getRoot().builder();
-        b.setProperty("bar", "xyz");
-        
-        // FIXME OAK-4122
-        //  store.setMaximumBackoff(100);
-        store.merge(b, new CommitHook() {
-            @Override @Nonnull
-            public NodeState processCommit(
-                    NodeState before, NodeState after, CommitInfo info) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    fail();
-                }
-                return after;
-            }
-        }, CommitInfo.EMPTY);
-
-        assertTrue(store.getRoot().hasProperty("foo"));
-        assertTrue(store.getRoot().hasProperty("bar"));
-
-        running.set(false);
-        background.join();
     }
 }
