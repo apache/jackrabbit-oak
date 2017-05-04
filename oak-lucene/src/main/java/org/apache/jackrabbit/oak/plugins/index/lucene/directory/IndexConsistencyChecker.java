5738ff7be16efee31148be0e50b03187f05bf434
Chetan Mehrotra <chetanm@apache.org>
2017-05-04 06:42:22 +0000

    OAK-5558 - Consistency checker for Lucene indexes
    Implemented support for testing based on CheckIndex utility from Lucene
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1793741 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/directory/IndexConsistencyChecker.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/directory/IndexConsistencyChecker.java
@@ -19,7 +19,11 @@
 
 package org.apache.jackrabbit.oak.plugins.index.lucene.directory;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,22 +32,37 @@ import javax.jcr.PropertyType;
 import com.google.common.base.Stopwatch;
 import com.google.common.io.ByteStreams;
 import com.google.common.io.CountingInputStream;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.jackrabbit.oak.api.Blob;
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.api.Root;
 import org.apache.jackrabbit.oak.api.Tree;
 import org.apache.jackrabbit.oak.api.Type;
+import org.apache.jackrabbit.oak.commons.PathUtils;
+import org.apache.jackrabbit.oak.plugins.index.lucene.IndexDefinition;
 import org.apache.jackrabbit.oak.plugins.index.lucene.LuceneIndexConstants;
+import org.apache.jackrabbit.oak.plugins.index.lucene.OakDirectory;
+import org.apache.jackrabbit.oak.plugins.index.lucene.writer.MultiplexersLucene;
 import org.apache.jackrabbit.oak.plugins.tree.RootFactory;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
+import org.apache.jackrabbit.oak.spi.state.NodeStateUtils;
+import org.apache.jackrabbit.oak.spi.state.ReadOnlyBuilder;
+import org.apache.lucene.index.CheckIndex;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.IOContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class IndexConsistencyChecker {
     private final Logger log = LoggerFactory.getLogger(getClass());
     private final NodeState rootState;
     private final String indexPath;
+    private final File workDirRoot;
 
     public enum Level {
         /**
@@ -52,7 +71,7 @@ public class IndexConsistencyChecker {
          */
         BLOBS_ONLY,
         /**
-         * Performs full check via {@link org.apache.lucene.index.CheckIndex}. This
+         * Performs full check via {@code org.apache.lucene.index.CheckIndex}. This
          * reads whole index and hence can take time
          */
         FULL
@@ -72,17 +91,59 @@ public class IndexConsistencyChecker {
 
         public long binaryPropSize;
 
-        public List<String> invalidBlobIds = new ArrayList<>();
+        public List<FileSizeStatus> invalidBlobIds = new ArrayList<>();
 
-        public List<String> msgs = new ArrayList<>();
+        public List<String> missingBlobIds = new ArrayList<>();
+
+        public List<DirectoryStatus> dirStatus = new ArrayList<>();
     }
 
-    public IndexConsistencyChecker(NodeState rootState, String indexPath) {
-        this.rootState = rootState;
-        this.indexPath = indexPath;
+    public static class DirectoryStatus {
+        public final String dirName;
+
+        public final List<String> missingFiles = new ArrayList<>();
+
+        public final List<FileSizeStatus> filesWithSizeMismatch = new ArrayList<>();
+
+        public boolean clean;
+
+        public long size;
+
+        public CheckIndex.Status status;
+
+        public long numDocs;
+
+        public DirectoryStatus(String dirName) {
+            this.dirName = dirName;
+        }
     }
 
-    public Result check(Level level){
+    public static class FileSizeStatus {
+        public final String name;
+
+        public final long actualSize;
+
+        public final long expectedSize;
+
+        public FileSizeStatus(String name, long actualSize, long expectedSize) {
+            this.name = name;
+            this.actualSize = actualSize;
+            this.expectedSize = expectedSize;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s => expected %d, actual %d", name, expectedSize, actualSize);
+        }
+    }
+
+    public IndexConsistencyChecker(NodeState rootState, String indexPath, File workDirRoot) {
+        this.rootState = checkNotNull(rootState);
+        this.indexPath = checkNotNull(indexPath);
+        this.workDirRoot = checkNotNull(workDirRoot);
+    }
+
+    public Result check(Level level) throws IOException {
         Stopwatch watch = Stopwatch.createStarted();
         Result result = new Result();
         result.indexPath = indexPath;
@@ -90,10 +151,9 @@ public class IndexConsistencyChecker {
 
         log.debug("[{}] Starting check", indexPath);
 
-        switch (level){
-            case BLOBS_ONLY :
-                checkBlobs(result);
-                break;
+        checkBlobs(result);
+        if (level == Level.FULL && result.clean){
+            checkIndex(result);
         }
 
         if (result.clean){
@@ -104,6 +164,82 @@ public class IndexConsistencyChecker {
 
         return result;
     }
+
+    private void checkIndex(Result result) throws IOException {
+        NodeState idx = NodeStateUtils.getNode(rootState, indexPath);
+        IndexDefinition defn = IndexDefinition.newBuilder(rootState, idx, indexPath).build();
+        File workDir = createWorkDir(workDirRoot, PathUtils.getName(indexPath));
+
+        for (String dirName : idx.getChildNodeNames()){
+            //TODO Check for SuggestionDirectory Pending
+            if (NodeStateUtils.isHidden(dirName) && MultiplexersLucene.isIndexDirName(dirName)){
+                DirectoryStatus dirStatus = new DirectoryStatus(dirName);
+                result.dirStatus.add(dirStatus);
+                log.warn("[{}] Checking directory {}", indexPath, dirName);
+                try {
+                    checkIndexDirectory(dirStatus, idx, defn, workDir, dirName);
+                } catch (IOException e){
+                    dirStatus.clean = false;
+                    log.warn("[{}][{}] Error occurred while performing directory check", indexPath, dirName, e);
+                }
+
+                if (!dirStatus.clean){
+                    result.clean = false;
+                }
+            }
+        }
+    }
+
+    private void checkIndexDirectory(DirectoryStatus dirStatus, NodeState idx, IndexDefinition defn,
+                                     File workDir, String dirName) throws IOException {
+        File idxDir = createWorkDir(workDir, dirName);
+        Directory sourceDir = new OakDirectory(new ReadOnlyBuilder(idx), dirName, defn, true);
+        Directory targetDir = FSDirectory.open(idxDir);
+
+        boolean clean = true;
+        for (String file : sourceDir.listAll()) {
+            log.debug("[{}][{}] Checking {}", indexPath, dirName, file);
+            try {
+                sourceDir.copy(targetDir, file, file, IOContext.DEFAULT);
+            } catch (FileNotFoundException ignore){
+                dirStatus.missingFiles.add(file);
+                clean = false;
+                log.warn("[{}][{}] File {} missing", indexPath, dirName, file);
+            }
+
+            if (targetDir.fileLength(file) != sourceDir.fileLength(file)){
+                FileSizeStatus fileStatus = new FileSizeStatus(file, targetDir.fileLength(file), sourceDir.fileLength(file));
+                dirStatus.filesWithSizeMismatch.add(fileStatus);
+                clean = false;
+                log.warn("[{}][{}] File size mismatch {}", indexPath, dirName, fileStatus);
+            } else {
+                dirStatus.size += sourceDir.fileLength(file);
+                log.debug("[{}][{}] File {} is consistent", indexPath, dirName, file);
+            }
+        }
+
+        if (clean){
+            log.debug("[{}][{}] Directory content found to be consistent. Proceeding to IndexCheck", indexPath, dirName);
+            CheckIndex ci = new CheckIndex(targetDir);
+
+            if (log.isDebugEnabled()) {
+                ci.setInfoStream(new LoggingPrintStream(log), log.isTraceEnabled());
+            }
+
+            dirStatus.status = ci.checkIndex();
+            dirStatus.clean = dirStatus.status.clean;
+            log.debug("[{}][{}] IndexCheck was successful. Proceeding to open DirectoryReader", indexPath, dirName);
+        }
+
+        if (dirStatus.clean){
+            DirectoryReader dirReader = DirectoryReader.open(targetDir);
+            dirStatus.numDocs = dirReader.numDocs();
+            log.debug("[{}][{}] DirectoryReader can be opened", indexPath, dirName);
+            dirReader.close();
+        }
+    }
+
+    //~---------------------------------------< Blob Validation >
 
     private void checkBlobs(Result result) {
         Root root = RootFactory.createReadOnlyRoot(rootState);
@@ -148,8 +284,7 @@ public class IndexConsistencyChecker {
             if (cis.getCount() != blob.length()){
                 String msg = String.format("Invalid blob %s. Length mismatch - expected ${%d} -> found ${%d}",
                         blobPath, blob.length(), cis.getCount());
-                result.msgs.add(msg);
-                result.invalidBlobIds.add(id);
+                result.invalidBlobIds.add(new FileSizeStatus(blobPath, cis.getCount(), blob.length()));
                 log.warn("[{}] {}", indexPath, msg);
                 result.clean = false;
                 result.blobSizeMismatch = true;
@@ -157,10 +292,45 @@ public class IndexConsistencyChecker {
             result.binaryPropSize += cis.getCount();
         } catch (Exception e) {
             log.warn("[{}] Error occurred reading blob at {}", indexPath, blobPath, e);
-            result.invalidBlobIds.add(id);
+            result.missingBlobIds.add(id);
             result.clean = false;
             result.missingBlobs = true;
         }
     }
 
+    //~-----------------------------------------------< utility >
+
+    private static File createWorkDir(File parent, String name) throws IOException {
+        String fsSafeName = IndexRootDirectory.getFSSafeName(name);
+        File dir = new File(parent, fsSafeName);
+        FileUtils.forceMkdir(dir);
+        FileUtils.cleanDirectory(dir);
+        return dir;
+    }
+
+    /**
+     * Adapter to pipe info messages from lucene into log messages.
+     */
+    private static final class LoggingPrintStream extends PrintStream {
+
+        /** Buffer print calls until a newline is written */
+        private final StringBuffer buffer = new StringBuffer();
+
+        private final Logger log;
+
+        public LoggingPrintStream(Logger log) {
+            super(ByteStreams.nullOutputStream());
+            this.log = log;
+        }
+
+        public void print(String s) {
+            buffer.append(s);
+        }
+
+        public void println(String s) {
+            buffer.append(s);
+            log.debug(buffer.toString());
+            buffer.setLength(0);
+        }
+    }
 }
