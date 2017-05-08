3f464bb230967dfe47d402f36139335e4a311b29
Julian Reschke <reschke@apache.org>
2017-05-08 12:13:55 +0000

    OAK-5667: RDBDocumentStore: remove unneeded support for DBs without support for CASE statements in SELECT (ported to 1.0)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.0@1794348 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBDocumentStoreDB.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBDocumentStoreDB.java
@@ -67,11 +67,6 @@ public enum RDBDocumentStoreDB {
         public String checkVersion(DatabaseMetaData md) throws SQLException {
             return RDBJDBCTools.versionCheck(md, 10, 11, description);
         }
-
-        @Override
-        public boolean allowsCaseInSelect() {
-            return false;
-        }
     },
 
     POSTGRES("PostgreSQL") {
@@ -401,13 +396,6 @@ public enum RDBDocumentStoreDB {
      */
     public String checkVersion(DatabaseMetaData md) throws SQLException {
         return "Unknown database type: " + md.getDatabaseProductName();
-    }
-
-    /**
-     * Allows case in select. Default true.
-     */
-    public boolean allowsCaseInSelect() {
-        return true;
     }
 
     /**
