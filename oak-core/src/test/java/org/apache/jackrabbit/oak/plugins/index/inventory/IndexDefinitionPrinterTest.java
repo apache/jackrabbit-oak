a32c9f35748b2f4596583f5c4c62f67d58df2168
Chetan Mehrotra <chetanm@apache.org>
2017-05-12 05:20:40 +0000

    OAK-6080 - Index report service
    Printer to dump index definitions
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1794923 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/inventory/IndexDefinitionPrinterTest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.jackrabbit.oak.plugins.index.inventory;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import com.google.common.collect.Lists;
+import org.apache.felix.inventory.Format;
+import org.apache.jackrabbit.oak.plugins.index.IndexPathService;
+import org.apache.jackrabbit.oak.plugins.index.inventory.IndexDefinitionPrinter;
+import org.apache.jackrabbit.oak.plugins.memory.MemoryNodeStore;
+import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
+import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
+import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
+import org.apache.jackrabbit.oak.spi.state.NodeStore;
+import org.json.simple.JSONValue;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class IndexDefinitionPrinterTest {
+    private NodeStore store = new MemoryNodeStore();
+    private IndexPathService pathService = mock(IndexPathService.class);
+    private IndexDefinitionPrinter printer = new IndexDefinitionPrinter(store, pathService);
+
+    @Test
+    public void printer() throws Exception{
+        NodeBuilder builder = store.getRoot().builder();
+        builder.child("a").setProperty("foo", "bar");
+        builder.child("b").child("c").setProperty("foo", "bar");
+
+        store.merge(builder, EmptyHook.INSTANCE, CommitInfo.EMPTY);
+
+        when(pathService.getIndexPaths()).thenReturn(Lists.newArrayList("/a", "/b"));
+
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        printer.print(pw, Format.JSON, false);
+
+        pw.flush();
+
+        String json = sw.toString();
+
+        //If there is any error in rendered json
+        //exception would fail the test
+        JSONValue.parseWithException(json);
+    }
+}
