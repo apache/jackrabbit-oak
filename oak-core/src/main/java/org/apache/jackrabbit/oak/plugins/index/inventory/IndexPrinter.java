5d372ca90e00897b9f37d83891177fb86444e86c
Chetan Mehrotra <chetanm@apache.org>
2017-05-12 05:20:24 +0000

    OAK-6215 - Dump index stats and index definitions
    Printer to dump index stats
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1794922 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/inventory/IndexPrinter.java
@@ -0,0 +1,145 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.jackrabbit.oak.plugins.index.inventory;
+
+import java.io.PrintWriter;
+import java.util.List;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ListMultimap;
+import org.apache.felix.inventory.Format;
+import org.apache.felix.inventory.InventoryPrinter;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.jackrabbit.oak.api.jmx.IndexStatsMBean;
+import org.apache.jackrabbit.oak.commons.IOUtils;
+import org.apache.jackrabbit.oak.plugins.index.AsyncIndexInfo;
+import org.apache.jackrabbit.oak.plugins.index.AsyncIndexInfoService;
+import org.apache.jackrabbit.oak.plugins.index.IndexInfo;
+import org.apache.jackrabbit.oak.plugins.index.IndexInfoService;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+@Component
+@Service
+@Properties({
+        @Property(name = "felix.inventory.printer.name", value = "oak-index-stats"),
+        @Property(name = "felix.inventory.printer.title", value = "Oak Index Stats"),
+        @Property(name = "felix.inventory.printer.format", value = { "TEXT"})
+})
+public class IndexPrinter implements InventoryPrinter {
+
+    @Reference
+    private IndexInfoService indexInfoService;
+
+    @Reference
+    private AsyncIndexInfoService asyncIndexInfoService;
+
+    public IndexPrinter() {
+    }
+
+    public IndexPrinter(IndexInfoService indexInfoService, AsyncIndexInfoService asyncIndexInfoService) {
+        this.indexInfoService = checkNotNull(indexInfoService);
+        this.asyncIndexInfoService = checkNotNull(asyncIndexInfoService);
+    }
+
+    @Override
+    public void print(PrintWriter pw, Format format, boolean isZip) {
+        //TODO Highlight if failing
+        printAsyncIndexInfo(pw);
+        printIndexInfo(pw);
+    }
+
+    private void printAsyncIndexInfo(PrintWriter pw) {
+        List<String> asyncLanes = ImmutableList.copyOf(asyncIndexInfoService.getAsyncLanes());
+        String title = "Async Indexers State";
+        printTitle(pw, title);
+        pw.printf("Number of async indexer lanes : %d%n", asyncLanes.size());
+        pw.println();
+        for (String lane : asyncLanes) {
+            pw.println(lane);
+            AsyncIndexInfo info = asyncIndexInfoService.getInfo(lane);
+            if (info != null) {
+                        pw.printf("    Last Indexed To      : %tc%n", info.getLastIndexedTo());
+                IndexStatsMBean stats = info.getStatsMBean();
+                if (stats != null) {
+                        pw.printf("    Status              : %s%n", stats.getStatus());
+                        pw.printf("    Failing             : %s%n", stats.isFailing());
+                        pw.printf("    Paused              : %s%n", stats.isPaused());
+                    if (stats.isFailing()) {
+                        pw.printf("    Failing Since       : %s%n", stats.getFailingSince());
+                        pw.printf("    Latest Error        : %s%n", stats.getLatestError());
+                    }
+                }
+                pw.println();
+            }
+        }
+    }
+
+    private static void printTitle(PrintWriter pw, String title) {
+        pw.println(title);
+        pw.println(Strings.repeat("=", title.length()));
+    }
+
+    private void printIndexInfo(PrintWriter pw) {
+        ListMultimap<String, IndexInfo> infos = ArrayListMultimap.create();
+        for (IndexInfo info : indexInfoService.getAllIndexInfo()) {
+            infos.put(info.getType(), info);
+        }
+
+        pw.printf("Total number of indexes : %d%n", infos.size());
+        for (String type : infos.keySet()){
+            List<IndexInfo> typedInfo = infos.get(type);
+            String title = String.format("%s(%d)", type, typedInfo.size());
+            printTitle(pw, title);
+            pw.println();
+            for (IndexInfo info : typedInfo){
+                printIndexInfo(pw, info);
+            }
+        }
+    }
+
+    private static void printIndexInfo(PrintWriter pw, IndexInfo info) {
+        pw.println(info.getIndexPath());
+        pw.printf("    Type                    : %s%n", info.getType());
+        if (info.getAsyncLaneName() != null) {
+            pw.printf("    async                   : true%n");
+            pw.printf("    async name              : %s%n", info.getAsyncLaneName());
+        }
+
+        if (info.getIndexedUpToTime() > 0){
+            pw.printf("    Last Indexed Upto       : %tc%n", info.getIndexedUpToTime());
+        }
+
+        if (info.getSizeInBytes() > 0){
+            pw.printf("    Size                    : %s%n", IOUtils.humanReadableByteCount(info.getSizeInBytes()));
+        }
+
+        if (info.getEstimatedEntryCount() > 0){
+            pw.printf("    Estimated entry count   : %d%n", info.getEstimatedEntryCount());
+        }
+        pw.println();
+    }
+}
