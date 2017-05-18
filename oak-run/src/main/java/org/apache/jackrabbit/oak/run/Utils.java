8aa46c75e02642c09d5907d59671a45f36d83987
Amit Jain <amitj@apache.org>
2017-05-18 07:23:17 +0000

    OAK-6233: Typed properties not handled properly in the initialization of DataStore in oak-run
    - Explicitly copying properties to a Map instance as objects
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1795488 13f79535-47bb-0310-9956-ffa450edef68

--- oak-run/src/main/java/org/apache/jackrabbit/oak/run/Utils.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/run/Utils.java
@@ -29,6 +29,7 @@ import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.annotation.CheckForNull;
@@ -250,7 +251,7 @@ class Utils {
             delegate = new OakFileDataStore();
             String cfgPath = fdsConfig.value(options);
             Properties props = loadAndTransformProps(cfgPath);
-            populate(delegate, Maps.fromProperties(props), true);
+            populate(delegate, asMap(props), true);
             delegate.init(null);
         }
         DataStoreBlobStore blobStore = new DataStoreBlobStore(delegate);
@@ -313,5 +314,13 @@ class Utils {
             props.put(key, dict.get(key));
         }
         return props;
+    }
+
+    private static Map<String, ?> asMap(Properties props) {
+        Map<String, Object> map = Maps.newHashMap();
+        for (Object key : props.keySet()) {
+            map.put((String)key, props.get(key));
+        }
+        return map;
     }
 }
