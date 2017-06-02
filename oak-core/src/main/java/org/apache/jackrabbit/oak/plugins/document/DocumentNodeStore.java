36128393fb056aeea08a8759c7e5b8bebe743dec
Tomasz Rekawek <tomekr@apache.org>
2017-06-02 10:05:06 +0000

    OAK-6294: The "missing" node cache value breaks the DocumentNodeStore#applyChanges
    Reverted the work-around patch until we know the root cause.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1797365 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStore.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStore.java
@@ -1387,9 +1387,6 @@ public final class DocumentNodeStore
                 String p = PathUtils.getAncestorPath(path, depth - i);
                 PathRev key = new PathRev(p, beforeState.getLastRevision());
                 beforeState = nodeCache.getIfPresent(key);
-                if (beforeState == missing) {
-                    beforeState = null;
-                }
             }
             DocumentNodeState.Children children = null;
             if (beforeState != null) {
