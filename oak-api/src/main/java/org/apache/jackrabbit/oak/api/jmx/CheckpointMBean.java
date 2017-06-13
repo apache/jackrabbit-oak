35dae9b10b0e0edbce4dd1f3e9200d3e3d7a4b28
Vikas Saurabh <catholicon@apache.org>
2017-06-13 04:35:44 +0000

    OAK-6337: Decide major version bump of o.a.j.o.api.jmx
    Temporarily adding deprecated method which was removed earlier and hence
    required major version bump in package.
    Whatever we eventually decide - (1) OK to have major version bump OR (2) configure baseline with stable release .... This additional method should go away
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1798559 13f79535-47bb-0310-9956-ffa450edef68

--- oak-api/src/main/java/org/apache/jackrabbit/oak/api/jmx/CheckpointMBean.java
+++ oak-api/src/main/java/org/apache/jackrabbit/oak/api/jmx/CheckpointMBean.java
@@ -19,6 +19,7 @@
 
 package org.apache.jackrabbit.oak.api.jmx;
 
+import javax.management.openmbean.CompositeData;
 import javax.management.openmbean.TabularData;
 
 import aQute.bnd.annotation.ProviderType;
@@ -48,6 +49,12 @@ public interface CheckpointMBean {
      * @return creation date of oldest checkpoint.
      */
     Date getOldestCheckpointCreationDate();
+
+    /**
+     * @return creation information about oldest checkpoint.
+     */
+    @Deprecated
+    CompositeData getOldestCheckpointCreationTime();
 
     /**
      * Create a new checkpoint with the given {@code lifetime}.
