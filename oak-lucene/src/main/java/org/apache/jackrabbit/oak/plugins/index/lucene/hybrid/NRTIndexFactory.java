ccb6e666defdb5e7c41a3b37ed1a1c33585d98ca
Chetan Mehrotra <chetanm@apache.org>
2017-07-28 06:03:44 +0000

    OAK-6500 - NRTIndex leaks file handles due to unclosed IndexReader
    Refactor code to allow customizing creation of NRTCachingDirectory.
    Later it would be used to force use of SimpleDirectory for simulating
    file handle leakage
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1803247 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/hybrid/NRTIndexFactory.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/hybrid/NRTIndexFactory.java
@@ -20,6 +20,7 @@
 package org.apache.jackrabbit.oak.plugins.index.lucene.hybrid;
 
 import java.io.Closeable;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -32,6 +33,9 @@ import org.apache.jackrabbit.oak.plugins.index.lucene.IndexCopier;
 import org.apache.jackrabbit.oak.plugins.index.lucene.IndexDefinition;
 import org.apache.jackrabbit.oak.stats.Clock;
 import org.apache.jackrabbit.oak.stats.StatisticsProvider;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.NRTCachingDirectory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -52,6 +56,7 @@ public class NRTIndexFactory implements Closeable{
     private final Clock clock;
     private final long refreshDeltaInSecs;
     private final StatisticsProvider statisticsProvider;
+    private NRTDirectoryFactory directoryFactory = DefaultNRTDirFactory.INSTANCE;
 
     public NRTIndexFactory(IndexCopier indexCopier, StatisticsProvider statisticsProvider) {
         this(indexCopier, Clock.SIMPLE, REFRESH_DELTA_IN_SECS, statisticsProvider);
@@ -75,7 +80,7 @@ public class NRTIndexFactory implements Closeable{
         }
         String indexPath = definition.getIndexPath();
         NRTIndex current = new NRTIndex(definition, indexCopier, getRefreshPolicy(definition),
-                getPrevious(indexPath), statisticsProvider);
+                getPrevious(indexPath), statisticsProvider, directoryFactory);
         indexes.put(indexPath, current);
         closeLast(indexPath);
         return current;
@@ -91,6 +96,10 @@ public class NRTIndexFactory implements Closeable{
 
     List<NRTIndex> getIndexes(String path){
         return indexes.get(path);
+    }
+
+    public void setDirectoryFactory(NRTDirectoryFactory directoryFactory) {
+        this.directoryFactory = directoryFactory;
     }
 
     private void closeLast(String indexPath) {
@@ -121,5 +130,16 @@ public class NRTIndexFactory implements Closeable{
             //return new RefreshOnReadPolicy(clock, TimeUnit.SECONDS, refreshDeltaInSecs);
         }
         return new TimedRefreshPolicy(clock, TimeUnit.SECONDS, refreshDeltaInSecs);
+    }
+
+    private enum DefaultNRTDirFactory implements NRTDirectoryFactory {
+        INSTANCE;
+
+        @Override
+        public Directory createNRTDir(IndexDefinition definition, File indexDir) throws IOException {
+            Directory fsdir = FSDirectory.open(indexDir);
+            //TODO make these configurable
+            return new NRTCachingDirectory(fsdir, 1, 1);
+        }
     }
 }
