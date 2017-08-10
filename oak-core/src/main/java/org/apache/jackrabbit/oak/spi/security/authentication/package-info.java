6f852901771627b2c60b8396379f98e867bb9bb0
Alex Deparvu <stillalex@apache.org>
2017-08-10 11:02:56 +0000

    OAK-6539 Decrease version export for org.apache.jackrabbit.oak.spi.security.authentication
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1804663 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/spi/security/authentication/package-info.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/security/authentication/package-info.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@Version("1.3.0")
+@Version("1.2.0")
 package org.apache.jackrabbit.oak.spi.security.authentication;
 
 import org.osgi.annotation.versioning.Version;
