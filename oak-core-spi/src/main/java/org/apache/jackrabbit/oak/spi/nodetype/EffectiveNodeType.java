2ad88ac1d53c0b26014b56b1cffed3b1a3412187
Chetan Mehrotra <chetanm@apache.org>
2017-09-22 10:21:33 +0000

    OAK-6690 Refactor constants classes NodeTypeConstants, NamespaceConstants, VersionConstants
    Fix the license headers
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1809295 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core-spi/src/main/java/org/apache/jackrabbit/oak/spi/nodetype/EffectiveNodeType.java
+++ oak-core-spi/src/main/java/org/apache/jackrabbit/oak/spi/nodetype/EffectiveNodeType.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.jackrabbit.oak.spi.nodetype;
 
 import javax.annotation.Nonnull;
@@ -10,9 +26,6 @@ import javax.jcr.nodetype.PropertyDefinition;
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.api.Tree;
 
-/**
- * Created by angela on 20/09/17.
- */
 public interface EffectiveNodeType {
     boolean includesNodeType(String nodeTypeName);
 
