40c27e2266ceaa7383989ebc11540ba00ba4f7b3
Chetan Mehrotra <chetanm@apache.org>
2017-10-13 09:10:47 +0000

    OAK-6113 - update spring to 1.5.x release
    Update to Spring 1.5.7 release
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1812086 13f79535-47bb-0310-9956-ffa450edef68

--- oak-examples/standalone/src/test/java/org/apache/jackrabbit/oak/standalone/RepositoryBootIT.java
+++ oak-examples/standalone/src/test/java/org/apache/jackrabbit/oak/standalone/RepositoryBootIT.java
@@ -26,12 +26,14 @@ import javax.jcr.SimpleCredentials;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration({OakServer.class, TestConfig.class})
+@SpringBootTest
+@ContextConfiguration(classes = {OakServer.class, TestConfig.class})
 @TestPropertySource(properties = {"repo.home=target/testrepo"})
 public class RepositoryBootIT {
 
