9b1bdf032d7a6ae59193572815843a70839e8deb
Julian Reschke <reschke@apache.org>
2017-10-20 14:26:55 +0000

    OAK-6852: RDBDocumentStore conditional remove: check condition properly
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1812750 13f79535-47bb-0310-9956-ffa450edef68

--- oak-store-document/src/main/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBDocumentStoreJDBC.java
+++ oak-store-document/src/main/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBDocumentStoreJDBC.java
@@ -207,7 +207,7 @@ public class RDBDocumentStoreJDBC {
                 throw new DocumentStoreException("Unsupported number of conditions in : " + entry.getValue().entrySet());
             }
             Entry<Key, Condition> c = entry.getValue().entrySet().iterator().next();
-            if (!c.getKey().getName().equals(MODIFIED)) {
+            if (!c.getKey().getName().equals(MODIFIED) || c.getValue().type != Condition.Type.EQUALS) {
                 throw new DocumentStoreException("Unsupported condition: " + c);
             }
         }
