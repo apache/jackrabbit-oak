f075b960029db11446a7a920606a8f5a4c6ab56c
Matt Ryan <matt@mvryan.org>
2017-10-31 10:24:25 -0600

    Adds unit test to test registering multiple data store providers.

--- oak-blob-plugins/src/test/java/org/apache/jackrabbit/oak/plugins/blob/datastore/FileDataStoreFactoryTest.java
+++ oak-blob-plugins/src/test/java/org/apache/jackrabbit/oak/plugins/blob/datastore/FileDataStoreFactoryTest.java
@@ -1,6 +1,7 @@
 package org.apache.jackrabbit.oak.plugins.blob.datastore;
 
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import org.apache.jackrabbit.core.data.DataStore;
 import org.apache.jackrabbit.oak.spi.blob.DataStoreProvider;
 import org.apache.jackrabbit.oak.stats.StatisticsProvider;
@@ -12,8 +13,11 @@ import org.osgi.service.component.ComponentContext;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Set;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNull;
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
@@ -78,5 +82,29 @@ public class FileDataStoreFactoryTest {
 
         activateService(factory, "local");
         assertNull(context.getService(DataStoreProvider.class));
+    }
+
+    @Test
+    public void testActivateMultiple() throws IOException {
+        Set<String> roles = Sets.newHashSet("local1", "local2", "cloud1");
+        for (String role : roles) {
+            activateService(role);
+        }
+        DataStoreProvider[] dsps = context.getServices(DataStoreProvider.class, null);
+        assertEquals(3, dsps.length);
+        for (DataStoreProvider dsp : dsps) {
+            assertTrue(roles.contains(dsp.getRole()));
+        }
+
+        for (String role : roles) {
+            dsps = context.getServices(DataStoreProvider.class, String.format("(role=%s)", role));
+            assertEquals(1, dsps.length);
+            DataStoreProvider dsp = dsps[0];
+            assertEquals(role, dsp.getRole());
+        }
+
+        // Invalid role
+        dsps = context.getServices(DataStoreProvider.class, "(role=notarole)");
+        assertEquals(0, dsps.length);
     }
 }
