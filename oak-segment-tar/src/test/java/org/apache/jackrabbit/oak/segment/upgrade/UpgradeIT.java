589d3c90faf5fe7cd3e555e22ac074ac14b22782
Michael Dürig <mduerig@apache.org>
2017-11-06 16:20:29 +0000

    OAK-6894: testcase temporarily disabled Enable test again
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1814426 13f79535-47bb-0310-9956-ffa450edef68

--- oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/upgrade/UpgradeIT.java
+++ oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/upgrade/UpgradeIT.java
@@ -43,7 +43,6 @@ import org.apache.jackrabbit.oak.segment.file.tar.IOMonitorAdapter;
 import org.apache.jackrabbit.oak.segment.file.tar.TarFiles;
 import org.apache.jackrabbit.oak.segment.tool.Compact;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -93,7 +92,6 @@ public class UpgradeIT {
     }
 
     @Test
-    @Ignore("OAK-6894")
     public void offRCUpgradesSegments() throws IOException, InvalidFileStoreVersionException {
         checkSegmentVersion(V_12);
         checkStoreVersion(1);
