2f267cede648740afaa1b92b2d5722315efc05a2
Davide Giannella <davide@apache.org>
2017-11-07 11:55:57 +0000

    Apache Jackrabbit Oak 1.7.11
    wrong javadocs
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1814491 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/IndexStatistics.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/IndexStatistics.java
@@ -103,10 +103,10 @@ public class IndexStatistics {
     /**
      * @param field Index field for which number of indexed documents are to be return
      * @return number of indexed documents (without subtracting potentially deleted ones)
-     *         for the given {@code field}.<br/>
-     *         -1: if index codec doesn't store doc-count-for-field statistics, OR <br/>
-     *             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;reader threw an exception while reading fields, OR <br/>
-     *             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;exception thrown while reading count for the field, OR <br/>
+     *         for the given {@code field}.<br>
+     *         -1: if index codec doesn't store doc-count-for-field statistics, OR <br>
+     *             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;reader threw an exception while reading fields, OR <br>
+     *             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;exception thrown while reading count for the field, OR <br>
      *             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;doc-count is asked for a non-property field.
      */
     public int getDocCountFor(String field) {
