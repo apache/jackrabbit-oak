8fe8436887ceb55649ac5915005aa402ecccd31b
Michael Dürig <mduerig@apache.org>
2017-11-14 11:15:19 +0000

    OAK-6931: Enable the -Dcache of offline compaction Pass the value of the cache system property to the compactor tool
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1815201 13f79535-47bb-0310-9956-ffa450edef68

--- oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/tool/Compact.java
+++ oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/tool/Compact.java
@@ -17,7 +17,9 @@
 
 package org.apache.jackrabbit.oak.segment.tool;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static org.apache.jackrabbit.oak.segment.SegmentCache.DEFAULT_SEGMENT_CACHE_MB;
 import static org.apache.jackrabbit.oak.segment.compaction.SegmentGCOptions.defaultGCOptions;
 import static org.apache.jackrabbit.oak.segment.file.FileStoreBuilder.fileStoreBuilder;
 
@@ -28,6 +30,7 @@ import java.io.RandomAccessFile;
 import javax.annotation.CheckForNull;
 import javax.annotation.Nullable;
 
+import org.apache.jackrabbit.oak.segment.SegmentCache;
 import org.apache.jackrabbit.oak.segment.file.FileStore;
 import org.apache.jackrabbit.oak.segment.file.FileStoreBuilder;
 import org.apache.jackrabbit.oak.segment.file.InvalidFileStoreVersionException;
@@ -59,6 +62,8 @@ public class Compact implements Runnable {
         private Boolean mmap;
 
         private boolean force;
+
+        private int segmentCacheSize = DEFAULT_SEGMENT_CACHE_MB;
 
         private Builder() {
             // Prevent external instantiation.
@@ -98,6 +103,19 @@ public class Compact implements Runnable {
         }
 
         /**
+         * The size of the segment cache in MB. The default of {@link SegmentCache#DEFAULT_SEGMENT_CACHE_MB}
+         * when this method is not invoked.
+         * @param segmentCacheSize   cache size in MB
+         * @return this builder
+         * @throws IllegalArgumentException  if {@code segmentCacheSize} is not a positive integer.
+         */
+        public Builder withSegmentCacheSize(int segmentCacheSize) {
+            checkArgument(segmentCacheSize > 0, "segmentCacheSize must be positive");
+            this.segmentCacheSize = segmentCacheSize;
+            return this;
+        }
+
+        /**
          * Create an executable version of the {@link Compact} command.
          *
          * @return an instance of {@link Runnable}.
@@ -114,11 +132,14 @@ public class Compact implements Runnable {
     @CheckForNull
     private final Boolean mmap;
 
+    private final int segmentCacheSize;
+
     private final boolean strictVersionCheck;
 
     private Compact(Builder builder) {
         this.path = builder.path;
         this.mmap = builder.mmap;
+        this.segmentCacheSize = builder.segmentCacheSize;
         this.strictVersionCheck = !builder.force;
     }
 
@@ -157,6 +178,7 @@ public class Compact implements Runnable {
     private FileStore newFileStore() throws IOException, InvalidFileStoreVersionException {
         FileStoreBuilder fileStoreBuilder = fileStoreBuilder(path.getAbsoluteFile())
                 .withStrictVersionCheck(strictVersionCheck)
+                .withSegmentCacheSize(segmentCacheSize)
                 .withGCOptions(defaultGCOptions()
                     .setOffline()
                     .setGCLogInterval(logAt));
