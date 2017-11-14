c6ca930bb552ff9ec78dd1f7d92b763e81201b97
Angela Schreiber <angela@apache.org>
2017-11-14 13:42:02 +0000

    OAK-6934 : Add test coverage of plugins.tree package (wip)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1815216 13f79535-47bb-0310-9956-ffa450edef68

--- oak-security-spi/src/test/java/org/apache/jackrabbit/oak/plugins/tree/AbstractTreeTest.java
+++ oak-security-spi/src/test/java/org/apache/jackrabbit/oak/plugins/tree/AbstractTreeTest.java
@@ -24,43 +24,49 @@ import org.apache.jackrabbit.oak.api.Tree;
 import org.apache.jackrabbit.oak.api.Type;
 import org.apache.jackrabbit.oak.commons.PathUtils;
 import org.apache.jackrabbit.oak.plugins.memory.PropertyStates;
+import org.apache.jackrabbit.oak.spi.nodetype.NodeTypeConstants;
 import org.junit.Before;
 import org.mockito.Mockito;
 
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
 
 public class AbstractTreeTest {
 
+    static final String CHILD_PATH = "/z/child";
+    static final String STRING_VALUE = "value";
+    static final long LONG_VALUE = 1;
+
     Tree rootTree;
     Tree nonExisting;
+    Tree z;
     Tree child;
     Root root;
 
     @Before
     public void before() throws Exception {
 
-        rootTree = mockTree("/", null, true);
+        rootTree = mockTree("/", null, true, NodeTypeConstants.NT_REP_ROOT);
         when(rootTree.hasProperty("p")).thenReturn(true);
-        when(rootTree.getProperty("p")).thenReturn(PropertyStates.createProperty("p", 1));
+        when(rootTree.getProperty("p")).thenReturn(PropertyStates.createProperty("p", LONG_VALUE));
 
-        nonExisting = mockTree("/nonExisting", rootTree, false);
+        nonExisting = mockTree("/nonExisting", rootTree, false, NodeTypeConstants.NT_OAK_UNSTRUCTURED);
 
         Tree x = mockTree("/x", rootTree, true);
-        Tree subTree = mockTree("/z", rootTree, true);
-        child = mockTree("/z/child", subTree, true);
+        z = mockTree("/z", rootTree, true, NodeTypeConstants.NT_OAK_UNSTRUCTURED);
+        child = mockTree("/z/child", z, true, NodeTypeConstants.NT_OAK_UNSTRUCTURED);
         when(child.hasProperty("p")).thenReturn(true);
-        when(child.getProperty("p")).thenReturn(PropertyStates.createProperty("p", "value"));
+        when(child.getProperty("p")).thenReturn(PropertyStates.createProperty("p", STRING_VALUE));
         when(child.hasProperty("pp")).thenReturn(true);
         PropertyState pp = PropertyStates.createProperty("pp", Lists.newArrayList("v1", "v2"), Type.STRINGS);
         when(child.getProperty("pp")).thenReturn(pp);
         when(child.hasProperty(JcrConstants.JCR_PRIMARYTYPE)).thenReturn(true);
-        when(child.getProperty(JcrConstants.JCR_PRIMARYTYPE)).thenReturn(PropertyStates.createProperty(JcrConstants.JCR_PRIMARYTYPE, JcrConstants.NT_UNSTRUCTURED, Type.NAME));
         when(child.hasProperty(JcrConstants.JCR_MIXINTYPES)).thenReturn(true);
         PropertyState mixinNames = PropertyStates.createProperty(JcrConstants.JCR_MIXINTYPES, Lists.newArrayList(JcrConstants.MIX_LOCKABLE, JcrConstants.MIX_VERSIONABLE), Type.NAMES);
         when(child.getProperty(JcrConstants.JCR_MIXINTYPES)).thenReturn(mixinNames);
 
-        when(subTree.getChild("child")).thenReturn(child);
-        when(rootTree.getChild("z")).thenReturn(subTree);
+        when(z.getChild("child")).thenReturn(child);
+        when(rootTree.getChild("z")).thenReturn(z);
         when(rootTree.getChild("x")).thenReturn(x);
         when(rootTree.getChild("nonExisting")).thenReturn(nonExisting);
 
@@ -74,7 +80,23 @@ public class AbstractTreeTest {
     }
 
     public Tree mockTree(String path, Tree parent, boolean exists) {
-        Tree t = Mockito.mock(Tree.class);
+        return mockTree(path, parent, exists, new Class[0]);
+    }
+
+    public Tree mockTree(String path, Tree parent, boolean exists, String primaryType) {
+        Tree t = mockTree(path, parent, exists, new Class[0]);
+        when(t.getProperty(JcrConstants.JCR_PRIMARYTYPE)).thenReturn(PropertyStates.createProperty(JcrConstants.JCR_PRIMARYTYPE, primaryType, Type.NAME));
+        return t;
+    }
+
+
+    public Tree mockTree(String path, Tree parent, boolean exists, Class<?>... interfaces) {
+        Tree t;
+        if (interfaces.length > 0) {
+            t = Mockito.mock(Tree.class, withSettings().extraInterfaces(interfaces));
+        } else {
+            t = Mockito.mock(Tree.class);
+        }
         when(t.getPath()).thenReturn(path);
         when(t.getName()).thenReturn(PathUtils.getName(path));
 
