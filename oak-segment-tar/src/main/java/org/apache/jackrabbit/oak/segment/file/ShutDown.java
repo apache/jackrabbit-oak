9ac823e49ec0fb86f6d8d9e03f3a269e6c689baf
Michael Dürig <mduerig@apache.org>
2017-12-07 11:27:30 +0000

    OAK-6884: TarMK disk space check is not synchronized with FileStore opened state Avoid scheduling further disk space checks once a shutdown has been initiated
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1817357 13f79535-47bb-0310-9956-ffa450edef68

--- oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/file/ShutDown.java
+++ oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/file/ShutDown.java
@@ -20,20 +20,42 @@ package org.apache.jackrabbit.oak.segment.file;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+/**
+ * An instance of this class encapsulates the shutdown logic of the {@link FileStore}.
+ * <p>
+ * A shutdown is initiated by calling {@link #shutDown} and completed at the point
+ * where the returned {@link ShutDownCloser} has been {@link ShutDownCloser#close() closed}.
+ *
+ * Code that needs to protect itself from running after a shutdown has been initiated can
+ * use the {@link #keepAlive()}, {@link #tryKeepAlive()} and {@link #isShutDown}:
+ *
+ * <pre>
+     try (ShutDownCloser ignored = shutDown.keepAlive()) {
+        // protected code here
+     }
+ * </pre>
+ */
 class ShutDown {
 
+    /**
+     * An {@link AutoCloseable} whose {@link #close()} doesn't throw an exception.
+     */
     interface ShutDownCloser extends AutoCloseable {
-
+        @Override
         void close();
-
     }
 
-    private volatile boolean shutDownRequested;
+    private volatile boolean isShutDown;
 
     private boolean shutDown;
 
     private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
+    /**
+     * Keep the store from being shut down until the returned {@link ShutDownCloser}
+     * is {@link ShutDownCloser#close() closed}.
+     * @throws IllegalStateException if the store is already {@link #isShutDown() shut down}.
+     */
     ShutDownCloser keepAlive() {
         lock.readLock().lock();
 
@@ -45,8 +67,23 @@ class ShutDown {
         return () -> lock.readLock().unlock();
     }
 
+    /**
+     * Try to keep the store from being shut down until the returned {@link ShutDownCloser}
+     * is {@link ShutDownCloser#close() closed}. Callers of this method need to call
+     * {@link #isShutDown()} before proceeding.
+     */
+    ShutDownCloser tryKeepAlive() {
+        lock.readLock().lock();
+        return () -> lock.readLock().unlock();
+    }
+
+    /**
+     * Initiate a shutdown of the store. The shutdown is complete once the returned
+     * {@link ShutDownCloser} is {@link ShutDownCloser#close() closed}.
+     * @return
+     */
     ShutDownCloser shutDown() {
-        shutDownRequested = true;
+        isShutDown = true;
         lock.writeLock().lock();
 
         if (shutDown) {
@@ -60,8 +97,11 @@ class ShutDown {
         };
     }
 
-    boolean shutDownRequested() {
-        return shutDownRequested;
+    /**
+     * @return  {@code true} iff the store is shut down.
+     */
+    boolean isShutDown() {
+        return isShutDown;
     }
 
 }
