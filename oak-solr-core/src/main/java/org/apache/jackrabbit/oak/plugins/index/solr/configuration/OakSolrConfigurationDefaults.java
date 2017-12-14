c8ce48130fd0956f06a7473350efefb1eedec194
Tommaso Teofili <tommaso@apache.org>
2017-12-14 13:02:09 +0000

    OAK-4318 - minor improvements for CloudSolrServer initialization, fixed schema
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1818124 13f79535-47bb-0310-9956-ffa450edef68

--- oak-solr-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/solr/configuration/OakSolrConfigurationDefaults.java
+++ oak-solr-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/solr/configuration/OakSolrConfigurationDefaults.java
@@ -30,7 +30,7 @@ public class OakSolrConfigurationDefaults {
     public static final String DESC_FIELD_NAME = "path_des";
     public static final String ANC_FIELD_NAME = "path_anc";
     public static final String CATCHALL_FIELD = "catch_all";
-    public static final int ROWS = Integer.MAX_VALUE;
+    public static final int ROWS = 10;
     public static final boolean PROPERTY_RESTRICTIONS = false;
     public static final boolean PATH_RESTRICTIONS = false;
     public static final boolean PRIMARY_TYPES = false;
