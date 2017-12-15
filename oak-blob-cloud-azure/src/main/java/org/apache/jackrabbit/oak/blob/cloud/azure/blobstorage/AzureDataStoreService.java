0be961a9cf18340ead3ea080a61e523c5d7d6113
Amit Jain <amitj@apache.org>
2017-12-15 05:26:39 +0000

    OAK-6047: incorrect metatype annotations
    Merge r1790502 from trunk
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.6@1818234 13f79535-47bb-0310-9956-ffa450edef68

--- oak-blob-cloud-azure/src/main/java/org/apache/jackrabbit/oak/blob/cloud/azure/blobstorage/AzureDataStoreService.java
+++ oak-blob-cloud-azure/src/main/java/org/apache/jackrabbit/oak/blob/cloud/azure/blobstorage/AzureDataStoreService.java
@@ -22,7 +22,7 @@ package org.apache.jackrabbit.oak.blob.cloud.azure.blobstorage;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.ConfigurationPolicy;
 
-@Component(policy = ConfigurationPolicy.REQUIRE, name = AzureDataStoreService.NAME, metatype = true)
+@Component(policy = ConfigurationPolicy.REQUIRE, name = AzureDataStoreService.NAME)
 public class AzureDataStoreService extends AbstractAzureDataStoreService {
     public static final String NAME = "org.apache.jackrabbit.oak.plugins.blob.datastore.AzureDataStore";
 }
