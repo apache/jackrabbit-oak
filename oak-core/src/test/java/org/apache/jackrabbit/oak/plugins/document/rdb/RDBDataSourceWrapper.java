626565c74a04e2d751829e1bbfc317435c583b6a
Julian Reschke <reschke@apache.org>
2017-12-16 10:58:56 +0000

    OAK-7068: RDBBlobStore may wrap SQLExceptions into RuntimeExceptions (ported to 1.4)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.4@1818379 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBDataSourceWrapper.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBDataSourceWrapper.java
@@ -55,6 +55,7 @@ public class RDBDataSourceWrapper implements DataSource, Closeable {
     private final DataSource ds;
     private boolean batchResultPrecise = true;
     private String temporaryCommitException = null;
+    private String temporaryUpdateException = null;
 
     // Logging
 
@@ -103,6 +104,14 @@ public class RDBDataSourceWrapper implements DataSource, Closeable {
 
     public String getTemporaryCommitException() {
         return this.temporaryCommitException;
+    }
+
+    public void setTemporaryUpdateException(String exmsg) {
+        this.temporaryUpdateException = exmsg;
+    }
+
+    public String getTemporaryUpdateException() {
+        return this.temporaryUpdateException;
     }
 
     // DataSource
