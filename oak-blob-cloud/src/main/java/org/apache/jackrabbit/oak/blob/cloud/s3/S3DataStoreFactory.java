09c8f07cbad633e90a7c6d43a14efa82b084108a
Matt Ryan <matt@mvryan.org>
2017-12-18 12:03:00 -0700

    Adds S3DataStoreFactory, for use with CompositeDataStore.
    Also modifies AbstractS3DataStoreService to make registration
    static, and uses closer to close everything (same pattern as
    for FileDataStoreFactory).

--- oak-blob-cloud/src/main/java/org/apache/jackrabbit/oak/blob/cloud/s3/S3DataStoreFactory.java
+++ oak-blob-cloud/src/main/java/org/apache/jackrabbit/oak/blob/cloud/s3/S3DataStoreFactory.java
@@ -1,4 +1,23 @@
 package org.apache.jackrabbit.oak.blob.cloud.s3;
 
-public class S3DataStoreFactory {
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.ConfigurationPolicy;
+import org.apache.jackrabbit.core.data.DataStore;
+import org.apache.jackrabbit.oak.plugins.blob.datastore.AbstractDataStoreFactory;
+import org.osgi.service.component.ComponentContext;
+
+import java.util.Map;
+
+@Component(policy = ConfigurationPolicy.REQUIRE,
+        name = "org.apache.jackrabbit.oak.plugins.blob.datastore.S3DataStoreFactory",
+        configurationFactory = true,
+        label = "Apache Jackrabbit Oak S3DataStore Factory",
+        description = "Factory allowing configuration of multiple instances of S3DataStores, " +
+                "for use with CompositeDataStore.")
+public class S3DataStoreFactory extends AbstractDataStoreFactory {
+    @Override
+    protected DataStore createDataStore(ComponentContext context, Map<String, Object> config) {
+        DataStore ds = S3DataStoreService.createS3DataStore(context, config, getStatisticsProvider(), getDescription(), closer);
+        return ds;
+    }
 }
