fd65a20d06cd2239adeaf4be74ef34ebfd329089
Marcel Reutegger <mreutegg@apache.org>
2018-02-22 09:01:08 +0000

    OAK-6392: Partial lastRev update with branches disabled
    Merge revision 1799861 from trunk and enable test
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.2@1825037 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStore.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStore.java
@@ -666,7 +666,13 @@ public final class DocumentNodeStore
             }
         } else {
             // branch commit
-            c.applyToCache(c.getBaseRevision(), isBranch);
+            try {
+                c.applyToCache(c.getBaseRevision(), isBranch);
+            } finally {
+                if (isDisableBranches()) {
+                    backgroundOperationLock.readLock().unlock();
+                }
+            }
         }
     }
 
@@ -676,6 +682,17 @@ public final class DocumentNodeStore
                 commitQueue.canceled(c.getRevision());
             } finally {
                 backgroundOperationLock.readLock().unlock();
+            }
+        } else {
+            try {
+                Branch b = branches.getBranch(c.getBaseRevision());
+                if (b != null) {
+                    b.removeCommit(c.getRevision().asBranchRevision());
+                }
+            } finally {
+                if (isDisableBranches()) {
+                    backgroundOperationLock.readLock().unlock();
+                }
             }
         }
     }
@@ -2021,7 +2038,16 @@ public final class DocumentNodeStore
                 "base must be a branch revision: " + base);
 
         checkOpen();
-        return new Commit(this, newRevision(), base, branch);
+        Commit c = new Commit(this, newRevision(), base, branch);
+        if (isDisableBranches()) {
+            // Regular branch commits do not need to acquire the background
+            // operation lock because the head is not updated and no pending
+            // lastRev updates are done on trunk. When branches are disabled,
+            // a branch commit becomes a pseudo trunk commit and the lock
+            // must be acquired.
+            backgroundOperationLock.readLock().lock();
+        }
+        return c;
     }
 
     /**
