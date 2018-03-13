93a35e701442652c6c141583ce4890893a8ea063
Tomasz Rekawek <tomekr@apache.org>
2018-03-13 19:29:29 +0000

    OAK-7335: oak-upgrade long name filter should consider the path length
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.8@1826668 13f79535-47bb-0310-9956-ffa450edef68

--- oak-store-document/src/main/java/org/apache/jackrabbit/oak/plugins/document/util/Utils.java
+++ oak-store-document/src/main/java/org/apache/jackrabbit/oak/plugins/document/util/Utils.java
@@ -76,14 +76,14 @@ public class Utils {
      * possibly be too large to be used for the primary key for the document
      * store.
      */
-    static final int PATH_SHORT = Integer.getInteger("oak.pathShort", 165);
+    public static final int PATH_SHORT = Integer.getInteger("oak.pathShort", 165);
 
     /**
      * The maximum length of the parent path, in bytes. If the parent path is
      * longer, then the id of a document is no longer the path, but the hash of
      * the parent, and then the node name.
      */
-    static final int PATH_LONG = Integer.getInteger("oak.pathLong", 350);
+    public static final int PATH_LONG = Integer.getInteger("oak.pathLong", 350);
 
     /**
      * The maximum size a node name, in bytes. This is only a problem for long path.
