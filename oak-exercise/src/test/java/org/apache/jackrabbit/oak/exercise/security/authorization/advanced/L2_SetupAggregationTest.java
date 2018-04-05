655ecf776cc6eaf9dd9855e00e355dcc861658ec
Angela Schreiber <angela@apache.org>
2018-04-05 15:33:48 +0000

    OAK-5122 : Exercise for Custom Authorization Models (wip)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1828446 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-exercise/src/test/java/org/apache/jackrabbit/oak/exercise/security/authorization/advanced/L2_SetupAggregationTest.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.exercise.security.authorization.advanced;
+
+/**
+ * <pre>
+ * Module: Advanced Authorization Topics
+ * =============================================================================
+ *
+ * Title: Aggregating Multiple Authorization Models : Setup
+ * -----------------------------------------------------------------------------
+ *
+ * Goal:
+ * Learn how to deploy authorization models and setup Oak authorization with more
+ * than one models.
+ *
+ * Note, that this section only focuses on OSGi-based Oak setup scenarios.
+ *
+ * Exercises:
+ * TODO
+ *
+ * </pre>
+ */
+public class L2_SetupAggregationTest {
+
+
+}
