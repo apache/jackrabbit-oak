c254a442f515a586cb3a3a21384c1d17b85bc57f
Tomasz Rekawek <tomekr@apache.org>
2018-04-11 12:47:07 +0000

    OAK-7404: ReadOnlyFileStore doesn't use custom persistence
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1828900 13f79535-47bb-0310-9956-ffa450edef68

--- oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/file/ReadOnlyFileStore.java
+++ oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/file/ReadOnlyFileStore.java
@@ -72,6 +72,7 @@ public class ReadOnlyFileStore extends AbstractFileStore {
                 .withIOMonitor(ioMonitor)
                 .withMemoryMapping(memoryMapping)
                 .withReadOnly()
+                .withPersistence(builder.getPersistence())
                 .build();
 
         writer = defaultSegmentWriterBuilder("read-only").withoutCache().build(this);
