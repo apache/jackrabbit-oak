7760b84924b2db4309e4d7a6c433e31dc35dddbd
Julian Reschke <reschke@apache.org>
2018-04-27 16:58:49 +0000

    OAK-7458: JCR locking: add logging to determine callers
    Separate logging class. WARN once when a lock operations is done. TRACE all of them.
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1830365 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/lock/LockDeprecation.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.jcr.lock;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.apache.jackrabbit.oak.jcr.session.NodeImpl;
+import org.apache.jackrabbit.oak.jcr.session.SessionImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Support deprecation of JCR locking as per OAK-6421.
+ */
+public class LockDeprecation {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LockDeprecation.class);
+
+    private LockDeprecation() {
+    }
+
+    private static final int LOGDEPTH = 5;
+
+    private static AtomicBoolean NOTWARNEDYET = new AtomicBoolean(true);
+
+    private static Set<String> IGNOREDCLASSES = new HashSet<String>(Arrays.asList(
+            new String[] { Thread.class.getName(), java.lang.reflect.Method.class.getName(), LockDeprecation.class.getName(),
+                    LockManagerImpl.class.getName(), NodeImpl.class.getName(), SessionImpl.class.getName() }));
+
+    public static void logCall(String operation) {
+
+        boolean firstInvocation = NOTWARNEDYET.getAndSet(false);
+
+        if (firstInvocation) {
+            String explanation = "Support for JCR Locking is deprecated and will be disabled in a future version of Jackrabbit Oak (see OAK-6421 for further information)";
+            LOG.warn(explanation + " - " + createLogMessage(operation));
+        } else if (LOG.isTraceEnabled()) {
+            LOG.trace(createLogMessage(operation));
+        }
+    }
+
+    private static String createLogMessage(String operation) {
+        StackTraceElement elements[] = Thread.currentThread().getStackTrace();
+        StringBuilder b = new StringBuilder();
+        b.append("operation '" + operation + "' called from:");
+        int depth = 0;
+        for (StackTraceElement e : elements) {
+            if (depth < LOGDEPTH) {
+                String cn = e.getClassName();
+                if (!IGNOREDCLASSES.contains(cn) && !cn.startsWith("sun.reflect")) {
+                    b.append(" ").append(e.toString());
+                    depth += 1;
+                }
+            }
+        }
+        return b.toString();
+    }
+}
--- oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/lock/LockManagerImpl.java
+++ oak-jcr/src/main/java/org/apache/jackrabbit/oak/jcr/lock/LockManagerImpl.java
@@ -70,6 +70,7 @@ public class LockManagerImpl implements LockManager {
     @Override
     public void addLockToken(final String lockToken)
             throws RepositoryException {
+        LockDeprecation.logCall("addLockToken");
         try {
             delegate.performVoid(new LockOperation<Void>(sessionContext, lockToken, "addLockToken") {
                 @Override
@@ -89,6 +90,7 @@ public class LockManagerImpl implements LockManager {
 
     @Override
     public void removeLockToken(final String lockToken) throws RepositoryException {
+        LockDeprecation.logCall("removeLockToken");
         if (!delegate.perform(new SessionOperation<Boolean>("removeLockToken") {
             @Nonnull
             @Override
@@ -146,6 +148,7 @@ public class LockManagerImpl implements LockManager {
     @Override
     public Lock lock(String absPath, final boolean isDeep, final boolean isSessionScoped,
                      long timeoutHint, String ownerInfo) throws RepositoryException {
+        LockDeprecation.logCall("lock");
         return new LockImpl(sessionContext, delegate.perform(new LockOperation<NodeDelegate>(sessionContext, absPath, "lock") {
             @Nonnull
             @Override
@@ -169,6 +172,7 @@ public class LockManagerImpl implements LockManager {
 
     @Override
     public void unlock(String absPath) throws RepositoryException {
+        LockDeprecation.logCall("unlock");
         delegate.performVoid(new LockOperation<Void>(sessionContext, absPath, "unlock") {
             @Override
             protected void performVoid(@Nonnull NodeDelegate node)
