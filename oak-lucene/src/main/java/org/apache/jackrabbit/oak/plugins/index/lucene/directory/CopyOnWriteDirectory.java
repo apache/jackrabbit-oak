56e3aec6f5fe68cf4ca7441528c5237dc18dd205
Vikas Saurabh <catholicon@apache.org>
2018-07-02 15:46:13 +0000

    OAK-7598: Unnecessary WARN for locally absent segments.gen
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1834858 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/directory/CopyOnWriteDirectory.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/directory/CopyOnWriteDirectory.java
@@ -434,7 +434,7 @@ public class CopyOnWriteDirectory extends FilterDirectory {
                      log.warn("COWRemoteFileReference::file ({}) differs in length. local: {}; remote: {}, init-remote-length",
                              localFileLength, remoteFileLength, length);
                  }
-            } else if (IndexCopier.REMOTE_ONLY.contains(name)) {
+            } else if (!IndexCopier.REMOTE_ONLY.contains(name)) {
                 log.warn("COWRemoteFileReference::local file ({}) doesn't exist", name);
             }
 
