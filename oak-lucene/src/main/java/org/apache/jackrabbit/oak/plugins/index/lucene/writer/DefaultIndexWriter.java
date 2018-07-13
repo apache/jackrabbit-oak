0b803a3daeb8bca388ccc71317fc321795a379cb
Vikas Saurabh <catholicon@apache.org>
2018-07-13 00:00:32 +0000

    OAK-7630: Suggest directory update doesn't close AnalyzingInfixSuggester after building
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1835780 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/writer/DefaultIndexWriter.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/writer/DefaultIndexWriter.java
@@ -25,6 +25,7 @@ import java.util.List;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.io.Closer;
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.api.Type;
 import org.apache.jackrabbit.oak.commons.PerfLogger;
@@ -164,20 +165,23 @@ class DefaultIndexWriter implements LuceneIndexWriter {
      * @param analyzer the analyzer used to update the suggester
      */
     private boolean updateSuggester(Analyzer analyzer, Calendar currentTime) throws IOException {
+        final Closer closer = Closer.create();
+
         NodeBuilder suggesterStatus = definitionBuilder.child(suggestDirName);
-        DirectoryReader reader = DirectoryReader.open(writer, false);
+        DirectoryReader reader = closer.register(DirectoryReader.open(writer, false));
         final Directory suggestDirectory =
             directoryFactory.newInstance(definition, definitionBuilder, suggestDirName, false);
+        // updateSuggester would close the directory (directly or via lookup)
+        // closer.register(suggestDirectory);
         boolean updated = false;
         try {
-            SuggestHelper.updateSuggester(suggestDirectory, analyzer, reader);
+            SuggestHelper.updateSuggester(suggestDirectory, analyzer, reader, closer);
             suggesterStatus.setProperty("lastUpdated", ISO8601.format(currentTime), Type.DATE);
             updated = true;
         } catch (Throwable e) {
             log.warn("could not update suggester", e);
         } finally {
-            suggestDirectory.close();
-            reader.close();
+            closer.close();
         }
         return updated;
     }
