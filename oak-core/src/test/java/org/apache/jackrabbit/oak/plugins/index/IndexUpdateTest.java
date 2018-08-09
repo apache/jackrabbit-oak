200faaa7eddfc6f5d5516260a54079f885603d2e
Tomasz Rekawek <tomekr@apache.org>
2018-08-09 08:05:53 +0000

    OAK-7686: Partial migration doesn't update Lucene indexing data
    For now, we have essentially avoided setting reindex flag for
    async+nrt/sync indexes during a sync index run. Otoh, OAK-7696 would be
    the right fix for the original problem (admittedly more instrusive and
    discussion worthy)
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.8@1837695 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/IndexUpdateTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/IndexUpdateTest.java
@@ -29,6 +29,7 @@ import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.REINDEX_COU
 import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.REINDEX_PROPERTY_NAME;
 import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.TYPE_DISABLED;
 import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.TYPE_PROPERTY_NAME;
+import static org.apache.jackrabbit.oak.plugins.index.IndexUpdateCallback.NOOP;
 import static org.apache.jackrabbit.oak.plugins.index.IndexUtils.createIndexDefinition;
 import static org.apache.jackrabbit.oak.InitialContent.INITIAL_CONTENT;
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -49,6 +50,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import javax.annotation.Nonnull;
 
 import ch.qos.logback.classic.Level;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
 import org.apache.jackrabbit.oak.api.CommitFailedException;
 import org.apache.jackrabbit.oak.api.PropertyState;
@@ -520,6 +522,62 @@ public class IndexUpdateTest {
 
         try {
             hook.processCommit(before, after, CommitInfo.EMPTY);
+            fail("commit should fail on missing index provider");
+        } catch (CommitFailedException ex) {
+            // expected
+        }
+    }
+
+    /**
+     * OAK-7686: async def with nrt/sync def should fail on missing provider only when running in
+     * context of an async cycle
+     */
+    @Test
+    public void testMissingProviderWithAsyncDef() throws Exception {
+        final MissingIndexProviderStrategy mips = new MissingIndexProviderStrategy();
+        mips.setFailOnMissingIndexProvider(true);
+
+        // prepare different hooks for different types indexing cycles
+        EditorHook syncHook = new EditorHook((before, after, builder, info) ->
+                new IndexUpdate(emptyProvider(), null, after, builder, NOOP)
+                        .withMissingProviderStrategy(mips));
+        EditorHook asyncHook = new EditorHook((before, after, builder, info) ->
+                new IndexUpdate(emptyProvider(), "async-run", after, builder, NOOP)
+                        .withMissingProviderStrategy(mips));
+        EditorHook otherAsyncHook = new EditorHook((before, after, builder, info) ->
+                new IndexUpdate(emptyProvider(), "other-async-run", after, builder, NOOP)
+                        .withMissingProviderStrategy(mips));
+
+        builder = EmptyNodeState.EMPTY_NODE.builder();
+
+        // create async defs with nrt and sync mixed in
+        createIndexDefinition(builder.child(INDEX_DEFINITIONS_NAME),
+                "asyncIndex", true, false, ImmutableSet.of("foo"), null)
+                .setProperty(ASYNC_PROPERTY_NAME, ImmutableList.of("async-run"), Type.STRINGS)
+                .setProperty(REINDEX_PROPERTY_NAME, false);
+        createIndexDefinition(builder.child(INDEX_DEFINITIONS_NAME),
+                "nrtIndex", true, false, ImmutableSet.of("foo"), null)
+                .setProperty(ASYNC_PROPERTY_NAME, ImmutableList.of("async-run", "nrt"), Type.STRINGS)
+                .setProperty(REINDEX_PROPERTY_NAME, false);
+        createIndexDefinition(builder.child(INDEX_DEFINITIONS_NAME),
+                "asyncSyncIndex", true, false, ImmutableSet.of("foo"), null)
+                .setProperty(ASYNC_PROPERTY_NAME, ImmutableList.of("async-run", "sync"), Type.STRINGS)
+                .setProperty(REINDEX_PROPERTY_NAME, false);
+
+        // node states to run hook on
+        NodeState before = builder.getNodeState();
+        builder.child("testRoot").setProperty("foo", "abc");
+        NodeState after = builder.getNodeState();
+
+        // sync run should be ok with missing provider for an async def
+        syncHook.processCommit(before, after, CommitInfo.EMPTY);
+
+        // unrelated async run should be ok with missing provider
+        otherAsyncHook.processCommit(before, after, CommitInfo.EMPTY);
+
+        // async run matching the def async lane still should fail
+        try {
+            asyncHook.processCommit(before, after, CommitInfo.EMPTY);
             fail("commit should fail on missing index provider");
         } catch (CommitFailedException ex) {
             // expected
