d88256e4c0e3f0b4cc438b152fa119802fa50b91
Thomas Mueller <thomasm@apache.org>
2018-09-06 13:30:19 +0000

    OAK-7739: Use an index only if a certain node or property exists
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1840222 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/IndexDefinition.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/IndexDefinition.java
@@ -270,6 +270,8 @@ public final class IndexDefinition implements Aggregate.AggregateMapper {
 
     private final boolean syncPropertyIndexes;
 
+    private final String useIfExists;
+
     //~--------------------------------------------------------< Builder >
 
     public static Builder newBuilder(NodeState root, NodeState defn, String indexPath){
@@ -408,6 +410,7 @@ public final class IndexDefinition implements Aggregate.AggregateMapper {
         this.nrtIndexMode = supportsNRTIndexing(defn);
         this.syncIndexMode = supportsSyncIndexing(defn);
         this.syncPropertyIndexes = definedRules.stream().anyMatch(ir -> !ir.syncProps.isEmpty());
+        this.useIfExists = getOptionalValue(defn, IndexConstants.USE_IF_EXISTS, null);
     }
 
     public NodeState getDefinitionNodeState() {
@@ -1638,6 +1641,23 @@ public final class IndexDefinition implements Aggregate.AggregateMapper {
             mergePolicy = new TieredMergePolicy();
         }
         return mergePolicy;
+    }
+
+    public boolean isEnabled() {
+        if (useIfExists == null) {
+            return true;
+        }
+        NodeState nodeState = root;
+        for (String element : PathUtils.elements(useIfExists)) {
+            if (element.startsWith("@")) {
+                return nodeState.hasProperty(element.substring(1));
+            }
+            nodeState = nodeState.getChildNode(element);
+            if (!nodeState.exists()) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private static Set<String> getMultiProperty(NodeState definition, String propName){
