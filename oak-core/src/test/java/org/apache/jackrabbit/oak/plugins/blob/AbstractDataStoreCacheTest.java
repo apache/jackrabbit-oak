e40337f03eb999cdf2c23b4f2dccac7fbbf72418
Amit Jain <amitj@apache.org>
2018-10-18 04:48:18 +0000

    OAK-7798: Return stream for downloaded files rather than directly from backend
    Merge r1842677, r1843222, r1843231 from trunk
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.6@1844201 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/blob/AbstractDataStoreCacheTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/blob/AbstractDataStoreCacheTest.java
@@ -57,6 +57,7 @@ import org.apache.jackrabbit.core.data.DataIdentifier;
 import org.apache.jackrabbit.core.data.DataRecord;
 import org.apache.jackrabbit.core.data.DataStoreException;
 import org.apache.jackrabbit.core.data.util.NamedThreadFactory;
+import org.apache.jackrabbit.oak.commons.FileIOUtils;
 import org.apache.jackrabbit.oak.spi.blob.AbstractDataRecord;
 import org.apache.jackrabbit.oak.spi.blob.AbstractSharedBackend;
 import org.slf4j.Logger;
@@ -94,7 +95,7 @@ public class AbstractDataStoreCacheTest {
 
 
     static class TestCacheLoader<S, I> extends CacheLoader<String, FileInputStream> {
-        protected final File root;
+        protected File root;
 
         public TestCacheLoader(File dir) {
             this.root = new File(dir, "datastore");
@@ -149,6 +150,15 @@ public class AbstractDataStoreCacheTest {
 
         public TestErrorCacheLoader(File dir, long max) {
             super(dir);
+            this.max = max;
+        }
+
+        public TestErrorCacheLoader(File dir, long max, boolean override) {
+            super(dir);
+            if (override) {
+                this.root = dir;
+            }
+
             this.max = max;
         }
 
@@ -399,7 +409,7 @@ public class AbstractDataStoreCacheTest {
     }
 
     static File copyToFile(InputStream stream, File file) throws IOException {
-        FileUtils.copyInputStreamToFile(stream, file);
+        FileIOUtils.copyInputStreamToFile(stream, file);
         return file;
     }
 
