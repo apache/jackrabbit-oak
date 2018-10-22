b5071a61ff63cd8e899b916290cd2be9be788315
Michael Dürig <mduerig@apache.org>
2018-10-22 12:26:14 +0000

    OAK-7853: SegmentBufferWriter not flushed after OnRC Test case and added a fix that flushes SBW instances disposed after OnRC
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1844549 13f79535-47bb-0310-9956-ffa450edef68

--- oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/SegmentBufferWriterPool.java
+++ oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/SegmentBufferWriterPool.java
@@ -67,6 +67,11 @@ public class SegmentBufferWriterPool implements WriteOperationHandler {
      */
     private final Set<SegmentBufferWriter> disposed = newHashSet();
 
+    /**
+     * Retired writers that have not yet been flushed from a previous GC generation
+     */
+    private final Set<SegmentBufferWriter> disposedOldGen = newHashSet();
+
     @NotNull
     private final SegmentIdProvider idProvider;
 
@@ -114,6 +119,11 @@ public class SegmentBufferWriterPool implements WriteOperationHandler {
             // the list so they won't get re-used anymore.
             toFlush.addAll(writers.values());
             writers.clear();
+
+            // Collect all writers from old GC generations that
+            // have been disposed
+            toFlush.addAll(disposedOldGen);
+            disposedOldGen.clear();
 
             // Collect all borrowed writers, which we need to wait for.
             // Clear the list so they will get disposed once returned.
@@ -191,7 +201,7 @@ public class SegmentBufferWriterPool implements WriteOperationHandler {
                         gcGeneration.get()
                 );
             } else if (!writer.getGCGeneration().equals(gcGeneration.get())) {
-                disposed.add(writer);
+                disposedOldGen.add(writer);
                 writer = new SegmentBufferWriter(
                         idProvider,
                         reader,
