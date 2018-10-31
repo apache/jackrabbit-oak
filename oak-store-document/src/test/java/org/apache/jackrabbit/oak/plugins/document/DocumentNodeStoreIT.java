403f0952dccd667e315090d8d426bae2f8866cb1
Marcel Reutegger <mreutegg@apache.org>
2018-10-31 14:23:59 +0000

    OAK-7869: Commit queue stuck when input stream of blob blocks
    Surround switch to reading with readMonitor.enter()/leave()
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1845333 13f79535-47bb-0310-9956-ffa450edef68

--- oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStoreIT.java
+++ oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStoreIT.java
@@ -224,7 +224,14 @@ public class DocumentNodeStoreIT extends AbstractDocumentStoreTest {
 
                 private int readOrEnd() {
                     if (blocking.get()) {
-                        reading = true;
+                        if (!reading) {
+                            readMonitor.enter();
+                            try {
+                                reading = true;
+                            } finally {
+                                readMonitor.leave();
+                            }
+                        }
                         return 0;
                     } else {
                         return -1;
