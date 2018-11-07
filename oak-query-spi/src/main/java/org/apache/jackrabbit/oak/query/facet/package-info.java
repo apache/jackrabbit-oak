b08557f6de66d866720e4ba711acf199b0a4db51
Julian Reschke <reschke@apache.org>
2018-11-07 14:17:56 +0000

    OAK-7669: OAK-7511: get rid of JSR 305 dependency - use jetbrains nullability annotations instead - oak-query-spi
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.8@1846028 13f79535-47bb-0310-9956-ffa450edef68

--- oak-query-spi/src/main/java/org/apache/jackrabbit/oak/query/facet/package-info.java
+++ oak-query-spi/src/main/java/org/apache/jackrabbit/oak/query/facet/package-info.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@Version("1.0")
+@Version("1.0.1")
 package org.apache.jackrabbit.oak.query.facet;
 
 import org.osgi.annotation.versioning.Version;
