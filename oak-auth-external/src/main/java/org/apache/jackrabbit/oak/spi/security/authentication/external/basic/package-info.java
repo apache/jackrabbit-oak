54316dc1dbb53ef49afab97116717d7e2a890fff
Davide Giannella <davide@apache.org>
2018-12-20 12:55:52 +0000

    apache jackrabbit Oak 1.8.10
    baseline checks
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1849384 13f79535-47bb-0310-9956-ffa450edef68

--- oak-auth-external/src/main/java/org/apache/jackrabbit/oak/spi/security/authentication/external/basic/package-info.java
+++ oak-auth-external/src/main/java/org/apache/jackrabbit/oak/spi/security/authentication/external/basic/package-info.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@Version("1.3.1")
+@Version("1.3.2")
 package org.apache.jackrabbit.oak.spi.security.authentication.external.basic;
 
 import org.osgi.annotation.versioning.Version;
