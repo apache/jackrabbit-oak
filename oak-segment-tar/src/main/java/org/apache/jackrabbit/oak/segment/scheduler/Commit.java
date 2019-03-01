056762241561c80dd338cc59c144ab52cebcc653
Michael Dürig <mduerig@apache.org>
2019-03-01 07:20:30 +0000

    OAK-8071: Logging to detect commits carrying over from previous GC generation can block other threads from committing > Merged r1854515
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.8@1854560 13f79535-47bb-0310-9956-ffa450edef68

--- oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/scheduler/Commit.java
+++ oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/scheduler/Commit.java
@@ -23,12 +23,14 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import org.apache.jackrabbit.oak.api.CommitFailedException;
 import org.apache.jackrabbit.oak.segment.SegmentNodeBuilder;
 import org.apache.jackrabbit.oak.segment.SegmentNodeState;
+import org.apache.jackrabbit.oak.segment.file.tar.GCGeneration;
 import org.apache.jackrabbit.oak.spi.commit.CommitHook;
 import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
 import org.apache.jackrabbit.oak.spi.state.ConflictAnnotatingRebaseDiff;
 import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * A {@code Commit} instance represents a set of related changes, which when
@@ -41,6 +43,8 @@ public class Commit {
     private final CommitHook hook;
     private final CommitInfo info;
 
+    private volatile GCGeneration gcGeneration;
+
     public Commit(@NotNull NodeBuilder changes, @NotNull CommitHook hook, @NotNull CommitInfo info) {
         checkNotNull(changes);
         checkArgument(changes instanceof SegmentNodeBuilder);
@@ -48,6 +52,30 @@ public class Commit {
 
         this.hook = checkNotNull(hook);
         this.info = checkNotNull(info);
+    }
+
+    /**
+     * This method makes a best effort on getting the gc generation of the current commit.
+     * However it avoids causing a write ahead action by calling {@link NodeBuilder#getName(String)}
+     * on the changes in this commit.
+     *
+     * @return  the gc generation of this commit or {@code null} if not yet available.
+     */
+    @Nullable
+    public GCGeneration getGCGeneration() {
+        return gcGeneration;
+    }
+
+    @NotNull
+    private NodeState getBeforeState() {
+        return changes.getBaseState();
+    }
+
+    @NotNull
+    private SegmentNodeState getAfterState() {
+        SegmentNodeState after = changes.getNodeState();
+        gcGeneration = after.getGcGeneration();
+        return after;
     }
 
     /**
@@ -63,19 +91,19 @@ public class Commit {
      */
     public SegmentNodeState apply(SegmentNodeState base) throws CommitFailedException {
         SegmentNodeBuilder builder = base.builder();
-        if (SegmentNodeState.fastEquals(changes.getBaseState(), base.getChildNode(ROOT))) {
+        if (SegmentNodeState.fastEquals(getBeforeState(), base.getChildNode(ROOT))) {
             // use a shortcut when there are no external changes
-            NodeState before = changes.getBaseState();
-            NodeState after = changes.getNodeState();
+            NodeState before = getBeforeState();
+            NodeState after = getAfterState();
 
             builder.setChildNode(ROOT, hook.processCommit(before, after, info));
         } else {
             // there were some external changes, so do the full rebase
             ConflictAnnotatingRebaseDiff diff = new ConflictAnnotatingRebaseDiff(builder.child(ROOT));
-            changes.getNodeState().compareAgainstBaseState(changes.getBaseState(), diff);
+            getAfterState().compareAgainstBaseState(getBeforeState(), diff);
             // apply commit hooks on the rebased changes
             builder.setChildNode(ROOT, hook.processCommit(builder.getBaseState().getChildNode(ROOT),
-                    builder.getNodeState().getChildNode(ROOT), info));
+                                                          builder.getNodeState().getChildNode(ROOT), info));
         }
         return builder.getNodeState();
     }
@@ -94,11 +122,11 @@ public class Commit {
      * the same semantics as
      * {@link SegmentNodeState#fastEquals(NodeState, NodeState)}, which cannot
      * guarantee against false negatives.
-     * 
+     *
      * @return {@code true}, if the commit has changes.
      */
     public boolean hasChanges() {
-        return !SegmentNodeState.fastEquals(changes.getBaseState(), changes.getNodeState());
+        return !SegmentNodeState.fastEquals(getBeforeState(), getAfterState());
     }
 
     public CommitInfo info() {
