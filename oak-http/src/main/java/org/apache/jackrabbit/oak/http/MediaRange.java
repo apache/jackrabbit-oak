7fb4353eb40a488ab057c30e8732cfd82c72cf29
Julian Reschke <reschke@apache.org>
2019-04-26 07:10:46 +0000

    OAK-8135: HTTP service may not select correct media type if multiple are specified in Accept header field (ported to 1.10)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.10@1858178 13f79535-47bb-0310-9956-ffa450edef68

--- oak-http/src/main/java/org/apache/jackrabbit/oak/http/MediaRange.java
+++ oak-http/src/main/java/org/apache/jackrabbit/oak/http/MediaRange.java
@@ -28,6 +28,10 @@ public class MediaRange {
 
     private final double q;
 
+    private static final int QRESOLUTION = 1000;
+    private static double CORRECTIONFORSUBTYPEMATCH = 1f / (10 * QRESOLUTION);
+    private static double CORRECTIONFORTYPEANDSUBTYPEMATCH = 2f / (10 * QRESOLUTION);
+    
     public static MediaRange parse(String range, MediaTypeRegistry registry) {
         MediaType type = MediaType.parse(range);
         if (type == null) {
@@ -56,6 +60,20 @@ public class MediaRange {
         this.q = q;
     }
 
+    /**
+     * Matches the media range against the specified media type.
+     * <p>
+     * The "derived" quality value if computed from the specified q value (0 to
+     * 1) by subtracting 1/10000 in case the subtype in the range is "*", or
+     * 2/10000 if case both type and subtype are. This takes care of the
+     * precedence specified in RFC 7231, Section 5.3.2.
+     *
+     * @param type
+     *            type to match
+     * @param registry
+     *            media type registry
+     * @return {@code 0.0} for "no match", the derived quality value if match
+     */
     public double match(MediaType type, MediaTypeRegistry registry) {
         if (type.equals(this.type)) { // shortcut
             return q;
@@ -70,14 +88,16 @@ public class MediaRange {
             }
         }
 
-        if ("*/*".equals(this.type.toString())) {
-            return q;
-        } else if ("*".equals(this.type.getSubtype())
-                && type.getType().equals(this.type.getType())) {
-            return q;
+        if (q > 0.0 && "*/*".equals(this.type.toString())) {
+            return q - CORRECTIONFORTYPEANDSUBTYPEMATCH;
+        } else if (q > 0.0 && "*".equals(this.type.getSubtype()) && type.getType().equals(this.type.getType())) {
+            return q - CORRECTIONFORSUBTYPEMATCH;
         } else {
             return 0.0;
         }
     }
 
+    public String toString() {
+        return String.format("type=%s, q=%f", type, q);
+    }
 }
