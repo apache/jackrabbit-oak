ae79a44f8176b0bf80841a4530ff4b22477f020c
Julian Reschke <reschke@apache.org>
2019-05-06 06:55:11 +0000

    OAK-8296: DocumentNodeStoreBranchesTest uses javax.annotation.Nonnull (ported to 1.8)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.8@1858745 13f79535-47bb-0310-9956-ffa450edef68

--- oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStoreBranchesTest.java
+++ oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/DocumentNodeStoreBranchesTest.java
@@ -27,8 +27,6 @@ import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 
-import javax.annotation.Nonnull;
-
 import org.apache.jackrabbit.oak.api.CommitFailedException;
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.api.Type;
@@ -47,6 +45,7 @@ import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
 import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
 import org.apache.jackrabbit.oak.stats.Clock;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -162,7 +161,7 @@ public class DocumentNodeStoreBranchesTest {
 
         try {
             ns.merge(nb, new CommitHook() {
-                @Nonnull
+                @NotNull
                 @Override
                 public NodeState processCommit(NodeState before,
                                                NodeState after,
@@ -216,7 +215,7 @@ public class DocumentNodeStoreBranchesTest {
         nb.child("foo");
         try {
             ns.merge(nb, new CommitHook() {
-                @Nonnull
+                @NotNull
                 @Override
                 public NodeState processCommit(NodeState before,
                                                NodeState after,
@@ -265,7 +264,7 @@ public class DocumentNodeStoreBranchesTest {
         nb.child("foo");
         try {
             ns.merge(nb, new CommitHook() {
-                @Nonnull
+                @NotNull
                 @Override
                 public NodeState processCommit(NodeState before,
                                                NodeState after,
@@ -322,7 +321,7 @@ public class DocumentNodeStoreBranchesTest {
         nb.child("foo");
         try {
             ns.merge(nb, new CommitHook() {
-                @Nonnull
+                @NotNull
                 @Override
                 public NodeState processCommit(NodeState before,
                                                NodeState after,
