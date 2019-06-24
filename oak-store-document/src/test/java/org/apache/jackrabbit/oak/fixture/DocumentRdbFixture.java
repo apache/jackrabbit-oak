21d28d132a6531c9f1c98c1d87063704d22f6bc7
Julian Reschke <reschke@apache.org>
2019-06-24 13:58:01 +0000

    OAK-8371: Stop using deprecated DocumentMK.Builder in RDB tests (ported to 1.8)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.8@1862007 13f79535-47bb-0310-9956-ffa450edef68

--- oak-store-document/src/test/java/org/apache/jackrabbit/oak/fixture/DocumentRdbFixture.java
+++ oak-store-document/src/test/java/org/apache/jackrabbit/oak/fixture/DocumentRdbFixture.java
@@ -28,9 +28,9 @@ import java.util.concurrent.ConcurrentHashMap;
 import javax.sql.DataSource;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.jackrabbit.oak.plugins.document.DocumentMK;
 import org.apache.jackrabbit.oak.plugins.document.DocumentNodeStore;
 import org.apache.jackrabbit.oak.plugins.document.rdb.RDBDataSourceFactory;
+import org.apache.jackrabbit.oak.plugins.document.rdb.RDBDocumentNodeStoreBuilder;
 import org.apache.jackrabbit.oak.plugins.document.rdb.RDBOptions;
 import org.apache.jackrabbit.oak.spi.state.NodeStore;
 
@@ -55,8 +55,8 @@ public class DocumentRdbFixture extends NodeStoreFixture {
         this.jdbcUrl = pUrl.replace("{fname}", fname);
         DataSource ds = RDBDataSourceFactory.forJdbcUrl(jdbcUrl, pUser, pPasswd);
 
-        NodeStore result = new DocumentMK.Builder().setPersistentCache("target/persistentCache,time")
-                .setRDBConnection(ds, options).getNodeStore();
+        NodeStore result = new RDBDocumentNodeStoreBuilder().setPersistentCache("target/persistentCache,time")
+                .setRDBConnection(ds, options).build();
         this.dataSources.put(result, ds);
         return result;
     }
