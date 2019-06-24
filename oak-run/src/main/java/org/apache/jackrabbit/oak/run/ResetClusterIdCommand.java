9a809e09caea4d7436a895b2d298390982a81269
Julian Reschke <reschke@apache.org>
2019-06-24 15:03:32 +0000

    OAK-8349: RDBDocumentStore*: "reset clusterId tool" in oak-run (ported to 1.8)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.8@1862011 13f79535-47bb-0310-9956-ffa450edef68

--- oak-run/src/main/java/org/apache/jackrabbit/oak/run/ResetClusterIdCommand.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/run/ResetClusterIdCommand.java
@@ -19,7 +19,6 @@ package org.apache.jackrabbit.oak.run;
 import org.apache.jackrabbit.oak.api.CommitFailedException;
 import org.apache.jackrabbit.oak.api.Type;
 import org.apache.jackrabbit.oak.run.commons.Command;
-import org.apache.jackrabbit.oak.plugins.document.DocumentNodeStore;
 import org.apache.jackrabbit.oak.spi.cluster.ClusterRepositoryInfo;
 import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
 import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
@@ -27,14 +26,6 @@ import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
 import org.apache.jackrabbit.oak.spi.state.NodeStore;
 
 import com.google.common.io.Closer;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.MongoURI;
-
-import joptsimple.OptionParser;
-import joptsimple.OptionSet;
-
-import static org.apache.jackrabbit.oak.plugins.document.mongo.MongoDocumentNodeStoreBuilder.newMongoDocumentNodeStoreBuilder;
 
 /**
  * OFFLINE utility to delete the clusterId stored as hidden
@@ -54,13 +45,13 @@ class ResetClusterIdCommand implements Command {
         NodeBuilder builder = store.getRoot().builder();
         NodeBuilder clusterConfigNode = builder.getChildNode(
                 ClusterRepositoryInfo.CLUSTER_CONFIG_NODE);
-        
+
         if (!clusterConfigNode.exists()) {
             // if it doesn't exist, then there is no way to delete
             System.out.println("clusterId was never set or already deleted.");
             return;
         }
-        
+
         if (!clusterConfigNode.hasProperty(ClusterRepositoryInfo.CLUSTER_ID_PROP)) {
             // the config node exists, but the clusterId not 
             // so again, no way to delete
@@ -75,45 +66,24 @@ class ResetClusterIdCommand implements Command {
 
             System.out.println("clusterId deleted successfully. (old id was " + oldClusterId + ")");
         } catch (CommitFailedException e) {
-            System.err.println("Failed to delete clusterId due to exception: "+e.getMessage());
+            System.err.println("Failed to delete clusterId due to exception: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
     @Override
     public void execute(String... args) throws Exception {
-        OptionParser parser = new OptionParser();
-        OptionSet options = parser.parse(args);
-
-        if (options.nonOptionArguments().isEmpty()) {
-            System.out.println("usage: resetclusterid {<path>|<mongo-uri>}");
-            System.exit(1);
-        }
-
-        String source = options.nonOptionArguments().get(0).toString();
+        String help = "resetclusterid {<path>|<mongo-uri>|<jdbc-uri>}";
+        Utils.NodeStoreOptions opts = new Utils.NodeStoreOptions(help).parse(args);
 
         Closer closer = Closer.create();
         try {
-            NodeStore store;
-            if (args[0].startsWith(MongoURI.MONGODB_PREFIX)) {
-                MongoClientURI uri = new MongoClientURI(source);
-                MongoClient client = new MongoClient(uri);
-                final DocumentNodeStore dns = newMongoDocumentNodeStoreBuilder()
-                        .setMongoDB(client.getDB(uri.getDatabase()))
-                        .build();
-                closer.register(Utils.asCloseable(dns));
-                store = dns;
-            } else {
-                store = SegmentTarUtils.bootstrapNodeStore(source, closer);
-            }
-            
+            NodeStore store = Utils.bootstrapNodeStore(opts, closer);
             deleteClusterId(store);
         } catch (Throwable e) {
             throw closer.rethrow(e);
         } finally {
             closer.close();
         }
-    
     }
-    
 }
