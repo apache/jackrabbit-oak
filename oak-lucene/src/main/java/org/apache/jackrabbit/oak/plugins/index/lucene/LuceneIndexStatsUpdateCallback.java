51de0693a4edfac58f7e51d31c765ac9aca39d63
nitigup <nitigup@adobe.com>
2019-06-30 18:04:58 +0530

    debugging

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexStatsUpdateCallback.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexStatsUpdateCallback.java
@@ -68,14 +68,14 @@ public class LuceneIndexStatsUpdateCallback implements PropertyUpdateCallback {
             int docCount = Integer.parseInt(luceneIndexMBean.getDocCount(indexPath));
             HistogramStats docCountHistogram = statisticsProvider.getHistogram(indexPath + NO_DOCS, StatsOptions.METRICS_ONLY);
             docCountHistogram.update(docCount);
-            /*long mills = System.currentTimeMillis();
+            long mills = System.currentTimeMillis();
             long indexSize = Long.parseLong(luceneIndexMBean.getSize(indexPath));
             log.info("Time taken to calculate index mbean size :: " + String.valueOf(System.currentTimeMillis() - mills));
 
             HistogramStats indexSizeHistogram = statisticsProvider.getHistogram(indexPath + INDEX_SIZE, StatsOptions.METRICS_ONLY);
-            indexSizeHistogram.update(indexSize);*/
+            indexSizeHistogram.update(indexSize);
 
-            long milis = System.currentTimeMillis();
+            /*long milis = System.currentTimeMillis();
             long localIndexDirSize = indexCopier.getLocalIndexDirSize();
             log.info("Time taken to calculate index dir size :: " + String.valueOf(System.currentTimeMillis() - milis));
             CounterStats indexDirectorySizeStats = statisticsProvider.getCounterStats(LOCAL_INDEX_DIR_SIZE, StatsOptions.DEFAULT);
@@ -83,9 +83,9 @@ public class LuceneIndexStatsUpdateCallback implements PropertyUpdateCallback {
             if (deltaInSize != 0) {
                 indexDirectorySizeStats.inc(deltaInSize);
                 log.info("index directory size stats updated; size {} delta {}", localIndexDirSize, deltaInSize);
-            }
+            }*/
 
-           // log.debug("{} stats updated; docCount {}, size {}", indexPath, docCount, indexSize);
+            log.debug("{} stats updated; docCount {}, size {}", indexPath, docCount, indexSize);
 
         } catch (IOException e) {
             log.debug("could not update no_docs/index_size stats for index at {}", indexPath, e);
