26451859eda5c299640d2f643cdd38dbe98f93f1
Thomas Mueller <thomasm@apache.org>
2019-11-29 09:51:29 +0000

    OAK-8783 Merge index definitions (JsonObject part)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1870584 13f79535-47bb-0310-9956-ffa450edef68

--- oak-commons/src/main/java/org/apache/jackrabbit/oak/commons/json/JsonObject.java
+++ oak-commons/src/main/java/org/apache/jackrabbit/oak/commons/json/JsonObject.java
@@ -17,15 +17,82 @@
 package org.apache.jackrabbit.oak.commons.json;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * Simple JSON Object representation
+ * Simple JSON Object representation.
+ * 
+ * It optionally supports respecting the order of properties, and the order children.
  */
 public class JsonObject {
 
-    private Map<String, String> props = new HashMap<String, String>();
-    private Map<String, JsonObject> children = new HashMap<String, JsonObject>();
+    private Map<String, String> props;
+    private Map<String, JsonObject> children;
+
+    /**
+     * Create a Json object that doesn't respect the order.
+     */
+    public JsonObject() {
+        this(false);
+    }
+    
+    /**
+     * Create a Json object.
+     * 
+     * @param respectOrder whether the object should respect the order
+     */
+    public JsonObject(boolean respectOrder) {
+        props = map(respectOrder);
+        children = map(respectOrder);
+    }
+    
+    private static <K, V> Map<K, V> map(boolean respectOrder) {
+        return respectOrder ? new LinkedHashMap<K, V>() : new HashMap<K, V>();
+    }
+    
+    /**
+     * Build a Json object from a String.
+     * 
+     * @param json the json string
+     * @param respectOrder whether the object should respect the child order
+     * @return the json object
+     */
+    public static JsonObject fromJson(String json, boolean respectOrder) {
+        JsopTokenizer tokenizer = new JsopTokenizer(json);
+        tokenizer.read('{');
+        try {
+            return create(tokenizer, respectOrder);
+        } finally {
+            tokenizer.read(JsopReader.END);
+        }
+    }
+    
+    /**
+     * Reads a JSON object from the given tokenizer. The opening '{' of the
+     * object should already have been consumed from the tokenizer before
+     * this method is called.
+     *
+     * @param t tokenizer
+     * @param respectOrder whether the order should be respected
+     * @return JSON object
+     */
+    public static JsonObject create(JsopTokenizer t, boolean respectOrder) {
+        JsonObject obj = new JsonObject(respectOrder);
+        if (!t.matches('}')) {
+            do {
+                String key = t.readString();
+                t.read(':');
+                if (t.matches('{')) {
+                    obj.children.put(key, create(t, respectOrder));
+                } else {
+                    obj.props.put(key, t.readRawValue().trim());
+                }
+            } while (t.matches(','));
+            t.read('}');
+        }
+        return obj;        
+    }
 
     /**
      * Reads a JSON object from the given tokenizer. The opening '{' of the
@@ -36,32 +103,46 @@ public class JsonObject {
      * @return JSON object
      */
     public static JsonObject create(JsopTokenizer t) {
-        JsonObject obj = new JsonObject();
-        if (!t.matches('}')) {
-            do {
-                String key = t.readString();
-                t.read(':');
-                if (t.matches('{')) {
-                    obj.children.put(key, create(t));
-                } else {
-                    obj.props.put(key, t.readRawValue().trim());
-                }
-            } while (t.matches(','));
-            t.read('}');
-        }
-        return obj;
+        return create(t, false);
     }
 
+    /**
+     * Write the object to a builder.
+     * 
+     * @param buf the target
+     */
     public void toJson(JsopBuilder buf) {
         toJson(buf, this);
     }
 
+    /**
+     * Get the (mutable) map of properties.
+     * 
+     * @return the property map
+     */
     public Map<String, String> getProperties() {
         return props;
     }
 
+    /**
+     * Get the (mutable) map of children.
+     * 
+     * @return the children map
+     */
     public Map<String, JsonObject> getChildren() {
         return children;
+    }
+
+    /**
+     * Pretty-print the object.
+     * 
+     * @return the pretty-printed string representation
+     */
+    @Override
+    public String toString() {
+        JsopBuilder w = new JsopBuilder();
+        toJson(w);
+        return JsopBuilder.prettyPrint(w.toString());
     }
 
     private static void toJson(JsopBuilder buf, JsonObject obj) {
@@ -75,4 +156,5 @@ public class JsonObject {
         }
         buf.endObject();
     }
+
 }
