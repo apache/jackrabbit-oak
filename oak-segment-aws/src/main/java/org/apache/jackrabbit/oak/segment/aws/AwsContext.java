bccf7f7657c41f39f75ec97d8fc6e152a5ba4a3f
Alvaro Dias <alvarorahul@gmail.com>
2019-12-12 11:27:22 -0800

    OAK-8827 - AWS support for segment-tar Implement ManifestFile

--- oak-segment-aws/src/main/java/org/apache/jackrabbit/oak/segment/aws/AwsContext.java
+++ oak-segment-aws/src/main/java/org/apache/jackrabbit/oak/segment/aws/AwsContext.java
@@ -16,5 +16,81 @@
  */
 package org.apache.jackrabbit.oak.segment.aws;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class AwsContext {
+
+    private static final Logger log = LoggerFactory.getLogger(AwsContext.class);
+
+    private final AmazonS3 s3;
+    private final String bucketName;
+    private final String rootDirectory;
+
+    private AwsContext(AmazonS3 s3, String bucketName, String rootDirectory) {
+        this.s3 = s3;
+        this.bucketName = bucketName;
+        this.rootDirectory = rootDirectory.endsWith("/") ? rootDirectory : rootDirectory + "/";
+    }
+
+    /**
+     * Creates the context used to interact with AWS services.
+     * 
+     * @param s3               Client for accessing Amazon S3.
+     * @param bucketName       Name for the bucket that will store segments.
+     * @param rootDirectory    The root directory under which the segment store is
+     *                         setup.
+     * @return The context.
+     * @throws IOException
+     */
+    public static AwsContext create(AmazonS3 s3, String bucketName, String rootDirectory) throws IOException {
+        AwsContext awsContext = new AwsContext(s3, bucketName, rootDirectory);
+        try {
+            if (!s3.doesBucketExistV2(bucketName)) {
+                s3.createBucket(bucketName);
+            }
+        } catch(AmazonServiceException e) {
+            throw new IOException(e);
+        }
+
+        return awsContext;
+    }
+
+    public boolean doesObjectExist(String name) {
+        try {
+            return s3.doesObjectExist(bucketName, rootDirectory + name);
+        } catch (AmazonServiceException e) {
+            log.error("Can't check if the manifest exists", e);
+            return false;
+        }
+    }
+
+    public S3Object getObject(String name) throws IOException {
+        try {
+            GetObjectRequest request = new GetObjectRequest(bucketName, rootDirectory + name);
+            return s3.getObject(request);
+        } catch (AmazonServiceException e) {
+            throw new IOException(e);
+        }
+    }
+
+    public void putObject(String name, InputStream input) throws IOException {
+        try {
+            PutObjectRequest request = new PutObjectRequest(bucketName, rootDirectory + name, input,
+                    new ObjectMetadata());
+            s3.putObject(request);
+        } catch (AmazonServiceException e) {
+            throw new IOException(e);
+        }
+    }
 }
