e4d0576925ce795196dba11b4088353fa67905bd
Aravindo Wingeier <wingeier@adobe.com>
2020-01-24 16:16:10 +0100

    GRANITE-28713: Cleanup

--- oak-segment-azure/src/test/java/org/apache/jackrabbit/oak/segment/azure/CaseInsensitiveMapAccessTest.java
+++ oak-segment-azure/src/test/java/org/apache/jackrabbit/oak/segment/azure/CaseInsensitiveMapAccessTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.util.Collections;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CaseInsensitiveMapAccessTest {
 
