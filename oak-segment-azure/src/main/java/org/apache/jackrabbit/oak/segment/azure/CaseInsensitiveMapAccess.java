e1067f5be56f7cfc7a677ce34c51f2eadb9ec4cd
Aravindo Wingeier <wingeier@adobe.com>
2020-01-24 16:15:48 +0100

    GRANITE-28713: More javadoc

--- oak-segment-azure/src/main/java/org/apache/jackrabbit/oak/segment/azure/CaseInsensitiveMapAccess.java
+++ oak-segment-azure/src/main/java/org/apache/jackrabbit/oak/segment/azure/CaseInsensitiveMapAccess.java
@@ -5,10 +5,18 @@ import java.util.Map;
 import java.util.TreeMap;
 
 /**
- * Wrapper around the Map that allows accessing the map in a case-insensitive manner. The keys 'hello' and 'Hello'
- * refer to the same value.
+ * Wrapper around the map that allows accessing the map in a case-insensitive manner.
+ * For example, the keys 'hello' and 'Hello' access the same value.
  */
 public class CaseInsensitiveMapAccess {
+    /**
+     * Wrap a map that allows accessing the map in a case-insensitive manner.
+     * <p>
+     * Return an unmodifiable map to make it clear that changes are not reflected to the original map.
+     *
+     * @param map the map to convert
+     * @return an unmodifiable map with case-insensitive key access
+     */
     public static Map<String, String> convert(Map<String, String> map) {
         Map<String, String> caseInsensitiveMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
         if (map != null) {
