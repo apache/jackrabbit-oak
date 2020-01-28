9c081729edd987653bc1c958ec7dec7dd61c1445
Julian Reschke <reschke@apache.org>
2020-01-28 17:32:07 +0000

    OAK-8861: oak-core-spi: bump up logging for deprecated Guava based APIs to WARN
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1873265 13f79535-47bb-0310-9956-ffa450edef68

--- oak-core-spi/src/main/java/org/apache/jackrabbit/oak/spi/GuavaDeprecation.java
+++ oak-core-spi/src/main/java/org/apache/jackrabbit/oak/spi/GuavaDeprecation.java
@@ -27,7 +27,7 @@ public class GuavaDeprecation {
 
     private static final Logger LOG = LoggerFactory.getLogger(GuavaDeprecation.class);
 
-    private static final String DEFAULT = "info";
+    private static final String DEFAULT = "warn";
 
     private static final String TLOGLEVEL = System.getProperty(GuavaDeprecation.class + ".LOGLEVEL", DEFAULT);
 
