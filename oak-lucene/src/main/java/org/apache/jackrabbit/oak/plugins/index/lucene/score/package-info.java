9624e3c2a1fc7effc2551f7c52845279aac58e1e
fabriziofortino <fabrizio.fortino@gmail.com>
2020-02-11 13:16:06 +0100

    OAK-8659: deprecate lucene custom scorer package

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/score/package-info.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/score/package-info.java
@@ -14,6 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+ * @deprecated Unused package that exposes Lucene API directly - it will be removed in 1.28
+ */
+@Deprecated
 @Version("1.3.1")
 package org.apache.jackrabbit.oak.plugins.index.lucene.score;
 
