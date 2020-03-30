4422686882d839f73a294f8eb6cac83767fc83d4
Julian Reschke <reschke@apache.org>
2020-03-30 13:05:03 +0000

    OAK-8657: SimpleCredentialsSupport uses Guava API in exported API
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1875880 13f79535-47bb-0310-9956-ffa450edef68

--- oak-security-spi/src/main/java/org/apache/jackrabbit/oak/spi/security/authentication/credentials/package-info.java
+++ oak-security-spi/src/main/java/org/apache/jackrabbit/oak/spi/security/authentication/credentials/package-info.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@Version("2.1.1")
+@Version("3.0.0")
 package org.apache.jackrabbit.oak.spi.security.authentication.credentials;
 
 import org.osgi.annotation.versioning.Version;
