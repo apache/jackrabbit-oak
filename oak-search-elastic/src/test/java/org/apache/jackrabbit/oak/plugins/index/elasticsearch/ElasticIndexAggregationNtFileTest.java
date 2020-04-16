8ad62b8ac57f83dd19649f82845f6aed83a46f55
Mohit Kataria <mkataria@apache.org>
2020-04-16 12:49:38 +0000

    OAK-9006:Elastic Search: Support text search of a text file via aggregation
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1876601 13f79535-47bb-0310-9956-ffa450edef68

--- oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elasticsearch/ElasticIndexAggregationNtFileTest.java
+++ oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elasticsearch/ElasticIndexAggregationNtFileTest.java
@@ -96,7 +96,8 @@ public class ElasticIndexAggregationNtFileTest extends AbstractQueryTest {
         ElasticsearchConnection coordinate = new ElasticsearchConnection(
                 ElasticsearchConnection.DEFAULT_SCHEME,
                 elastic.getContainerIpAddress(),
-                elastic.getMappedPort(ElasticsearchConnection.DEFAULT_PORT));
+                elastic.getMappedPort(ElasticsearchConnection.DEFAULT_PORT),
+                "" + System.nanoTime());
 
         ElasticsearchIndexEditorProvider editorProvider = new ElasticsearchIndexEditorProvider(coordinate,
                 new ExtractedTextCache(10 * FileUtils.ONE_MB, 100));
