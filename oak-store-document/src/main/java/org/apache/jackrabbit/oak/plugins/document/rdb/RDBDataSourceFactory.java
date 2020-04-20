054c13733e4c19e8f6fa5e5d85de578d4cae0805
Julian Reschke <reschke@apache.org>
2020-04-20 10:25:42 +0000

    OAK-9021: use SystemPropertySupplier in RDBDataSourceFactory
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1876752 13f79535-47bb-0310-9956-ffa450edef68

--- oak-store-document/src/main/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBDataSourceFactory.java
+++ oak-store-document/src/main/java/org/apache/jackrabbit/oak/plugins/document/rdb/RDBDataSourceFactory.java
@@ -31,6 +31,7 @@ import java.util.logging.Logger;
 import javax.sql.DataSource;
 
 import org.apache.jackrabbit.oak.plugins.document.DocumentStoreException;
+import org.apache.jackrabbit.oak.plugins.document.util.SystemPropertySupplier;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -77,9 +78,10 @@ public class RDBDataSourceFactory {
                 dsclazz.getMethod("setUsername", String.class).invoke(ds, username);
                 dsclazz.getMethod("setPassword", String.class).invoke(ds, passwd);
                 dsclazz.getMethod("setUrl", String.class).invoke(ds, url);
-                String interceptors = System.getProperty(
-                        "org.apache.jackrabbit.oak.plugins.document.rdb.RDBDataSourceFactory.jdbcInterceptors",
-                        "SlowQueryReport(threshold=10000);ConnectionState;StatementCache");
+                String interceptors = SystemPropertySupplier
+                        .create("org.apache.jackrabbit.oak.plugins.document.rdb.RDBDataSourceFactory.jdbcInterceptors",
+                                "SlowQueryReport(threshold=10000);ConnectionState;StatementCache")
+                        .loggingTo(LOG).get();
                 if (!interceptors.isEmpty()) {
                     dsclazz.getMethod("setJdbcInterceptors", String.class).invoke(ds, interceptors);
                 }
