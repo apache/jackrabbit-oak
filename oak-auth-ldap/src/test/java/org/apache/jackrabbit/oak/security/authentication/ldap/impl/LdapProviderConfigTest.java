2d1188c0edeb301a8ad301e65e033f13b710803b
Manfred Baedke <baedke@apache.org>
2020-05-04 19:42:07 +0000

    OAK-9044: Improve unit test coverage of LdapProviderConfig
    Added tests.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1877360 13f79535-47bb-0310-9956-ffa450edef68

--- oak-auth-ldap/src/test/java/org/apache/jackrabbit/oak/security/authentication/ldap/impl/LdapProviderConfigTest.java
+++ oak-auth-ldap/src/test/java/org/apache/jackrabbit/oak/security/authentication/ldap/impl/LdapProviderConfigTest.java
@@ -19,6 +19,10 @@ package org.apache.jackrabbit.oak.security.authentication.ldap.impl;
 import org.apache.jackrabbit.oak.spi.security.ConfigurationParameters;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class LdapProviderConfigTest {
@@ -41,4 +45,159 @@ public class LdapProviderConfigTest {
         assertEquals(25, config.getSearchTimeout());
     }
 
+    @Test
+    public void testOfAllConfigurationParameters() {
+
+        Map<String, Object> params = new HashMap<>();
+
+        String testName = "testname";
+        String testLdapHost = "testhost.org";
+        int testLdapPort = LdapProviderConfig.PARAM_LDAP_PORT_DEFAULT + 1;
+        boolean testUseSsl = !LdapProviderConfig.PARAM_USE_SSL_DEFAULT;
+        boolean testUseTls = !LdapProviderConfig.PARAM_USE_TLS_DEFAULT;
+        boolean testNoCertCheck = !LdapProviderConfig.PARAM_NO_CERT_CHECK_DEFAULT;
+        String testBindDn = "cn=testBindDn";
+        String testBindPassword = "testPwd";
+        String testSearchTimeout = "1d 1h 1m 1s 1ms";
+        long testSearchTimeoutMs = 1 + 1000 * (1 + 60 * (1 + 60 * (1 + 24)));
+        boolean testUseUidForExtId = !LdapProviderConfig.PARAM_USE_UID_FOR_EXT_ID_DEFAULT;
+        String[] testCustomAttributes = new String[] {"a","b","c"};
+        String testGroupMemberAttribute = "testMemberAttr";
+
+        boolean testAdminPoolLookupOnValidate = !LdapProviderConfig.PARAM_ADMIN_POOL_LOOKUP_ON_VALIDATE_DEFAULT;
+        int testAdminPoolMaxActive = LdapProviderConfig.PARAM_ADMIN_POOL_MAX_ACTIVE_DEFAULT + 1;
+
+        boolean testUserPoolLookupOnValidate = !LdapProviderConfig.PARAM_USER_POOL_LOOKUP_ON_VALIDATE_DEFAULT;
+        int testUserPoolMaxActive = LdapProviderConfig.PARAM_USER_POOL_MAX_ACTIVE_DEFAULT + 2;
+
+        String testUserBaseDn = "ou=people,dc=org";
+        String[] testUserObjectClass = new String[] {"inetOrgPerson"};
+        String testUserIdAttribute = "foo";
+        String testUserExtraFilter = "(cn=*)";
+        boolean testUserMakeDnPath = !LdapProviderConfig.PARAM_USER_MAKE_DN_PATH_DEFAULT;
+
+        String testGroupBaseDn = "ou=groups,dc=org";
+        String[] testGroupObjectClass = new String[] {"posixGroup"};
+        String testGroupNameAttribute = "bar";
+        String testGroupExtraFilter = "(ou=*)";
+        boolean testGroupMakeDnPath = !LdapProviderConfig.PARAM_GROUP_MAKE_DN_PATH_DEFAULT;
+
+        params.put(LdapProviderConfig.PARAM_NAME, testName);
+        params.put(LdapProviderConfig.PARAM_LDAP_HOST, testLdapHost);
+        params.put(LdapProviderConfig.PARAM_LDAP_PORT, testLdapPort);
+        params.put(LdapProviderConfig.PARAM_USE_SSL, testUseSsl);
+        params.put(LdapProviderConfig.PARAM_USE_TLS, testUseTls);
+        params.put(LdapProviderConfig.PARAM_NO_CERT_CHECK, testNoCertCheck);
+        params.put(LdapProviderConfig.PARAM_BIND_DN, testBindDn);
+        params.put(LdapProviderConfig.PARAM_BIND_PASSWORD, testBindPassword);
+        params.put(LdapProviderConfig.PARAM_SEARCH_TIMEOUT, testSearchTimeout);
+        params.put(LdapProviderConfig.PARAM_USE_UID_FOR_EXT_ID, testUseUidForExtId);
+        params.put(LdapProviderConfig.PARAM_CUSTOM_ATTRIBUTES, testCustomAttributes);
+        params.put(LdapProviderConfig.PARAM_GROUP_MEMBER_ATTRIBUTE, testGroupMemberAttribute);
+
+        params.put(LdapProviderConfig.PARAM_ADMIN_POOL_LOOKUP_ON_VALIDATE, testAdminPoolLookupOnValidate);
+        params.put(LdapProviderConfig.PARAM_ADMIN_POOL_MAX_ACTIVE, testAdminPoolMaxActive);
+
+        params.put(LdapProviderConfig.PARAM_USER_POOL_LOOKUP_ON_VALIDATE, testUserPoolLookupOnValidate);
+        params.put(LdapProviderConfig.PARAM_USER_POOL_MAX_ACTIVE, testUserPoolMaxActive);
+
+        params.put(LdapProviderConfig.PARAM_USER_BASE_DN, testUserBaseDn);
+        params.put(LdapProviderConfig.PARAM_USER_OBJECTCLASS, testUserObjectClass);
+        params.put(LdapProviderConfig.PARAM_USER_ID_ATTRIBUTE, testUserIdAttribute);
+        params.put(LdapProviderConfig.PARAM_USER_EXTRA_FILTER, testUserExtraFilter);
+        params.put(LdapProviderConfig.PARAM_USER_MAKE_DN_PATH, testUserMakeDnPath);
+
+        params.put(LdapProviderConfig.PARAM_GROUP_BASE_DN, testGroupBaseDn);
+        params.put(LdapProviderConfig.PARAM_GROUP_OBJECTCLASS, testGroupObjectClass);
+        params.put(LdapProviderConfig.PARAM_GROUP_NAME_ATTRIBUTE, testGroupNameAttribute);
+        params.put(LdapProviderConfig.PARAM_GROUP_EXTRA_FILTER, testGroupExtraFilter);
+        params.put(LdapProviderConfig.PARAM_GROUP_MAKE_DN_PATH, testGroupMakeDnPath);
+
+        LdapProviderConfig config = LdapProviderConfig.of(ConfigurationParameters.of(params));
+        assertEquals(testName, config.getName());
+        assertEquals(testLdapHost, config.getHostname());
+        assertEquals(testLdapPort, config.getPort());
+        assertEquals(testUseSsl, config.useSSL());
+        assertEquals(testUseTls, config.useTLS());
+        assertEquals(testNoCertCheck, config.noCertCheck());
+        assertEquals(testBindDn, config.getBindDN());
+        assertEquals(testBindPassword, config.getBindPassword());
+        assertEquals(testSearchTimeoutMs, config.getSearchTimeout());
+        assertEquals(testUseUidForExtId, config.getUseUidForExtId());
+        assertArrayEquals(testCustomAttributes, config.getCustomAttributes());
+        assertEquals(testGroupMemberAttribute, config.getGroupMemberAttribute());
+
+        LdapProviderConfig.PoolConfig adminPoolConfig = config.getAdminPoolConfig();
+        assertEquals(testAdminPoolLookupOnValidate, adminPoolConfig.lookupOnValidate());
+        assertEquals(testAdminPoolMaxActive, adminPoolConfig.getMaxActive());
+
+        LdapProviderConfig.PoolConfig userPoolConfig = config.getUserPoolConfig();
+        assertEquals(testUserPoolLookupOnValidate, userPoolConfig.lookupOnValidate());
+        assertEquals(testUserPoolMaxActive, userPoolConfig.getMaxActive());
+
+        LdapProviderConfig.Identity userConfig = config.getUserConfig();
+        assertEquals(testUserBaseDn, userConfig.getBaseDN());
+        assertArrayEquals(testUserObjectClass, userConfig.getObjectClasses());
+        assertEquals(testUserIdAttribute, userConfig.getIdAttribute());
+        assertEquals(testUserExtraFilter, userConfig.getExtraFilter());
+        assertEquals(testUserMakeDnPath, userConfig.makeDnPath());
+
+        LdapProviderConfig.Identity groupConfig = config.getGroupConfig();
+        assertEquals(testGroupBaseDn, groupConfig.getBaseDN());
+        assertArrayEquals(testGroupObjectClass, groupConfig.getObjectClasses());
+        assertEquals(testGroupNameAttribute, groupConfig.getIdAttribute());
+        assertEquals(testGroupExtraFilter, groupConfig.getExtraFilter());
+        assertEquals(testGroupMakeDnPath, groupConfig.makeDnPath());
+    }
+
+    @Test
+    public void testGetMemberOfSearchFilter() {
+        Map<String, Object> params = new HashMap<>();
+        LdapProviderConfig config = LdapProviderConfig.of(ConfigurationParameters.of(params));
+        assertEquals("(&(" + LdapProviderConfig.PARAM_GROUP_MEMBER_ATTRIBUTE_DEFAULT + "=cn=bar)(objectclass=" + LdapProviderConfig.PARAM_GROUP_OBJECTCLASS_DEFAULT[0] + "))",
+                config.getMemberOfSearchFilter("cn=bar"));
+        params.put(LdapProviderConfig.PARAM_GROUP_MEMBER_ATTRIBUTE, "foo");
+        config = LdapProviderConfig.of(ConfigurationParameters.of(params));
+        assertEquals("(&(foo=cn=bar)(objectclass=" + LdapProviderConfig.PARAM_GROUP_OBJECTCLASS_DEFAULT[0] + "))",
+                config.getMemberOfSearchFilter("cn=bar"));
+        params.put(LdapProviderConfig.PARAM_GROUP_OBJECTCLASS, new String[] {"posixGroup"});
+        config = LdapProviderConfig.of(ConfigurationParameters.of(params));
+        assertEquals("(&(foo=cn=bar)(objectclass=posixGroup))",
+                config.getMemberOfSearchFilter("cn=bar"));
+        params.put(LdapProviderConfig.PARAM_GROUP_OBJECTCLASS, new String[] {"posixGroup", "groupOfUniqueNames"});
+        config = LdapProviderConfig.of(ConfigurationParameters.of(params));
+        assertEquals("(&(foo=cn=bar)(objectclass=posixGroup)(objectclass=groupOfUniqueNames))",
+                config.getMemberOfSearchFilter("cn=bar"));
+    }
+
+    @Test
+    public void testEncodeFilterValueNormal() {
+        //test a value that doesn't need escaping (see RFC4515 chapter 3)
+        StringBuilder builder = new StringBuilder();
+        for (int k = 1; k <= 0x27; k++) {
+            builder.append((char) k);
+        }
+        for (int k = 0x2b; k <= 0x5b; k++) {
+            builder.append((char) k);
+        }
+        for (int k = 0x5d; k <= 0x7f; k++) {
+            builder.append((char) k);
+        }
+        builder.append(Character.toChars(0x80));
+        builder.append(Character.toChars(0x7ff));
+        builder.append(Character.toChars(0x800));
+        builder.append(Character.toChars(0xffff));
+        builder.append(Character.toChars(0x10000));
+        builder.append(Character.toChars(0x10ffff));
+        String value = builder.toString();
+        assertEquals(value, LdapProviderConfig.encodeFilterValue(value));
+    }
+
+    @Test
+    public void testEncodeFilterValueEscaped() {
+        //test the encoding of character that need escaping (see RFC4515 chapter 3)
+        String value = "\u0000*()\\";
+        String encodedValue = "\\00\\2A\\28\\29\\5C";
+        assertEquals(encodedValue, LdapProviderConfig.encodeFilterValue(value));
+    }
 }
