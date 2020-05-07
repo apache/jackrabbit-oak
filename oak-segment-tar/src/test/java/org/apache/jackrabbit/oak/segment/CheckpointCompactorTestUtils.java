17ea60b0bfb0ca3670c99208170a774a6c99fdfc
Andrei Dulceanu <adulceanu@apache.org>
2020-05-07 10:53:45 +0000

    OAK-8832 - Offline Compaction fails while erroneously accessing external blob ((merged r1877063 into 1.10)
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/branches/1.10@1877469 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/CheckpointCompactorTestUtils.java
@@ -0,0 +1,126 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.jackrabbit.oak.segment;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.apache.jackrabbit.oak.plugins.memory.MultiBinaryPropertyState.binaryPropertyFromBlob;
+import static org.apache.jackrabbit.oak.segment.DefaultSegmentWriterBuilder.defaultSegmentWriterBuilder;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.jackrabbit.oak.api.Blob;
+import org.apache.jackrabbit.oak.api.CommitFailedException;
+import org.apache.jackrabbit.oak.segment.file.FileStore;
+import org.apache.jackrabbit.oak.segment.file.GCNodeWriteMonitor;
+import org.apache.jackrabbit.oak.segment.file.tar.GCGeneration;
+import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
+import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
+import org.apache.jackrabbit.oak.spi.gc.GCMonitor;
+import org.apache.jackrabbit.oak.spi.state.ChildNodeEntry;
+import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
+import org.apache.jackrabbit.oak.spi.state.NodeState;
+import org.apache.jackrabbit.oak.spi.state.NodeStore;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+
+public class CheckpointCompactorTestUtils {
+
+    private CheckpointCompactorTestUtils() {
+
+    }
+
+    public static void checkGeneration(NodeState node, GCGeneration gcGeneration) {
+        assertTrue(node instanceof SegmentNodeState);
+        assertEquals(gcGeneration, ((SegmentNodeState) node).getRecordId().getSegmentId().getGcGeneration());
+
+        for (ChildNodeEntry cne : node.getChildNodeEntries()) {
+            checkGeneration(cne.getNodeState(), gcGeneration);
+        }
+    }
+
+    public static NodeState getCheckpoint(NodeState superRoot, String name) {
+        NodeState checkpoint = superRoot
+                .getChildNode("checkpoints")
+                .getChildNode(name)
+                .getChildNode("root");
+        assertTrue(checkpoint.exists());
+        return checkpoint;
+    }
+
+    public static void assertSameStableId(NodeState node1, NodeState node2) {
+        assertTrue(node1 instanceof SegmentNodeState);
+        assertTrue(node2 instanceof SegmentNodeState);
+
+        assertEquals("Nodes should have the same stable ids",
+                ((SegmentNodeState) node1).getStableId(),
+                ((SegmentNodeState) node2).getStableId());
+    }
+
+    public static void assertSameRecord(NodeState node1, NodeState node2) {
+        assertTrue(node1 instanceof SegmentNodeState);
+        assertTrue(node2 instanceof SegmentNodeState);
+
+        assertEquals("Nodes should have been deduplicated",
+                ((SegmentNodeState) node1).getRecordId(),
+                ((SegmentNodeState) node2).getRecordId());
+    }
+
+    @NotNull
+    public static CheckpointCompactor createCompactor(@NotNull FileStore fileStore, @NotNull GCGeneration generation) {
+        SegmentWriter writer = defaultSegmentWriterBuilder("c")
+                .withGeneration(generation)
+                .build(fileStore);
+
+        return new CheckpointCompactor(
+                GCMonitor.EMPTY,
+                fileStore.getReader(),
+                writer,
+                fileStore.getBlobStore(),
+                GCNodeWriteMonitor.EMPTY);
+    }
+
+    public static void addTestContent(@NotNull String parent, @NotNull NodeStore nodeStore, int binPropertySize)
+            throws CommitFailedException, IOException {
+        NodeBuilder rootBuilder = nodeStore.getRoot().builder();
+        NodeBuilder parentBuilder = rootBuilder.child(parent);
+        parentBuilder.setChildNode("a").setChildNode("aa").setProperty("p", 42);
+        parentBuilder.getChildNode("a").setChildNode("bb").setChildNode("bbb");
+        parentBuilder.setChildNode("b").setProperty("bin", createBlob(nodeStore, binPropertySize));
+        parentBuilder.setChildNode("c").setProperty(binaryPropertyFromBlob("bins", createBlobs(nodeStore, 42, 43, 44)));
+        nodeStore.merge(rootBuilder, EmptyHook.INSTANCE, CommitInfo.EMPTY);
+    }
+
+    public static Blob createBlob(NodeStore nodeStore, int size) throws IOException {
+        byte[] data = new byte[size];
+        new Random().nextBytes(data);
+        return nodeStore.createBlob(new ByteArrayInputStream(data));
+    }
+
+    public static List<Blob> createBlobs(NodeStore nodeStore, int... sizes) throws IOException {
+        List<Blob> blobs = newArrayList();
+        for (int size : sizes) {
+            blobs.add(createBlob(nodeStore, size));
+        }
+        return blobs;
+    }
+}
