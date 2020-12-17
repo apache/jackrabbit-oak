e06b66d57fa29c0b12da86fedb98aedcf639e0db
Thomas Mueller <thomasm@apache.org>
2020-12-17 14:39:33 +0000

    OAK-9301 Automatically pick a merged index
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1884554 13f79535-47bb-0310-9956-ffa450edef68

--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/composite/blueGreen/CustomizedIndexTest.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/composite/blueGreen/CustomizedIndexTest.java
@@ -21,6 +21,8 @@ package org.apache.jackrabbit.oak.composite.blueGreen;
 import java.io.File;
 import java.io.IOException;
 
+import javax.jcr.Node;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -34,13 +36,13 @@ public class CustomizedIndexTest {
 
     @Rule
     public TemporaryFolder tempDir = new TemporaryFolder(new File("target"));
-    
+
     private File globalDir;
     private File libs1Dir;
     private File libs2Dir;
     private File datastoreDir;
     private File indexDir;
-    
+
     @Test
     public void test() throws Exception {
         createFolders();
@@ -51,73 +53,120 @@ public class CustomizedIndexTest {
         compositeLibs1();
         initLibs2();
         compositeLibs2();
+        compositeWithMergedIndex();
     }
-    
+
     private void initLibs1() throws Exception {
         Persistence p = Persistence.open(libs1Dir, config);
         p.session.getRootNode().addNode("libs").addNode("test").setProperty("foo", "a");
         p.session.save();
         IndexUtils.createIndex(p, "test-1", "foo", 10);
-        IndexUtils.assertQueryUsesIndexAndReturns(p, 
-                "/jcr:root//*[@foo]", 
-                "test-1", 
+        IndexUtils.assertQueryUsesIndexAndReturns(p,
+                "/jcr:root//*[@foo]",
+                "test-1",
                 "[/libs/test]");
         p.close();
     }
-    
+
     private void initGlobal() throws Exception {
         Persistence p = Persistence.open(globalDir, config);
         p.session.getRootNode().addNode("content").addNode("test").setProperty("foo", "a");
-        p.session.save(); 
+        p.session.save();
         p.close();
     }
-    
+
     private void compositeLibs1() throws Exception {
         Persistence p = Persistence.openComposite(globalDir, libs1Dir, config);
         IndexUtils.checkLibsIsReadOnly(p);
         IndexUtils.createIndex(p, "test-1", "foo", 10);
-        IndexUtils.assertQueryUsesIndexAndReturns(p, 
-                "/jcr:root//*[@foo] order by @jcr:path", 
-                "test-1", 
+        IndexUtils.assertQueryUsesIndexAndReturns(p,
+                "/jcr:root//*[@foo] order by @jcr:path",
+                "test-1",
                 "[/content/test, /libs/test]");
         p.close();
     }
-    
+
     private void compositeLibs2() throws Exception {
         Persistence p = Persistence.openComposite(globalDir, libs2Dir, config);
         IndexUtils.checkLibsIsReadOnly(p);
+
         IndexUtils.createIndex(p, "test-2", "foo", 20);
-        IndexUtils.assertQueryUsesIndexAndReturns(p, 
-                "/jcr:root//*[@foo] order by @jcr:path", 
-                "test-2", 
+
+        // the new index must be used in the new version (with libs2)
+        IndexUtils.assertQueryUsesIndexAndReturns(p,
+                "/jcr:root//*[@foo] order by @jcr:path",
+                "test-2",
                 "[/content/test, /libs/test2]");
         p.close();
-        // the new index must not be used in the old version (wiht libs1)
+
+        // the new index must not be used in the old version (with libs1)
         p = Persistence.openComposite(globalDir, libs1Dir, config);
-        IndexUtils.assertQueryUsesIndexAndReturns(p, 
-                "/jcr:root//*[@foo] order by @jcr:path", 
-                "test-1", 
+        IndexUtils.assertQueryUsesIndexAndReturns(p,
+                "/jcr:root//*[@foo] order by @jcr:path",
+                "test-1",
                 "[/content/test, /libs/test]");
         p.close();
     }
-    
+
+    private void compositeWithMergedIndex() throws Exception {
+        // a new _merged_ index must be used by the new version (with libs2)
+        // but it won't contain the libs part (as that's not indexed)
+        Persistence p = Persistence.openComposite(globalDir, libs2Dir, config);
+        Node n = IndexUtils.createIndex(p, "test-2-custom-1", "foo", 10);
+        n.setProperty("merges", new String[]{"/oak:index/test-2"});
+        n.getSession().save();
+        IndexUtils.assertQueryUsesIndexAndReturns(p,
+                "/jcr:root//*[@foo] order by @jcr:path",
+                "test-2-custom-1",
+                "[/content/test]");
+        // no let it point to a non-existing node: the index should be used
+        n.setProperty("merges", new String[]{"/oak:index/test-does-not-exist"});
+        n.getSession().save();
+        IndexUtils.assertQueryUsesIndexAndReturns(p,
+                "/jcr:root//*[@foo] order by @jcr:path",
+                "test-2-custom-1",
+                "[/content/test]");
+        p.close();
+
+        // the merged index is not used in the old version (with libs1)
+        // because the index test-2 is not active
+        p = Persistence.openComposite(globalDir, libs1Dir, config);
+        n = p.session.getNode("/oak:index/test-2-custom-1");
+        n.setProperty("merges", new String[]{"/oak:index/test-2"});
+        n.getSession().save();
+        IndexUtils.assertQueryUsesIndexAndReturns(p,
+                "/jcr:root//*[@foo] order by @jcr:path",
+                "test-1",
+                "[/content/test, /libs/test]");
+        // no let it point to a non-existing node: the index should be used
+        n.setProperty("merges", new String[]{"/oak:index/test-does-not-exist"});
+        n.getSession().save();
+        IndexUtils.assertQueryUsesIndexAndReturns(p,
+                "/jcr:root//*[@foo] order by @jcr:path",
+                "test-2-custom-1",
+                "[/content/test]");
+        p.close();
+
+
+    }
+
     private void initLibs2() throws Exception {
         Persistence p = Persistence.open(libs2Dir, config);
         p.session.getRootNode().addNode("libs").addNode("test2").setProperty("foo", "a");
         p.session.save();
         IndexUtils.createIndex(p, "test-1", "foo", 10);
-        IndexUtils.assertQueryUsesIndexAndReturns(p, 
-                "/jcr:root//*[@foo]", 
-                "test-1", 
+        IndexUtils.assertQueryUsesIndexAndReturns(p,
+                "/jcr:root//*[@foo]",
+                "test-1",
                 "[/libs/test2]");
         IndexUtils.createIndex(p, "test-2", "foo", 20);
-        IndexUtils.assertQueryUsesIndexAndReturns(p, 
-                "/jcr:root//*[@foo]", 
-                "test-2", 
+        IndexUtils.assertQueryUsesIndexAndReturns(p,
+                "/jcr:root//*[@foo]",
+                "test-2",
                 "[/libs/test2]");
-        p.close();        
+        p.close();
     }
-    
+
     private void createFolders() throws IOException {
         globalDir = tempDir.newFolder("global");
         libs1Dir = tempDir.newFolder("libs1");
@@ -125,5 +174,5 @@ public class CustomizedIndexTest {
         datastoreDir = tempDir.newFolder("datastore");
         indexDir = tempDir.newFolder("index");
     }
-    
+
 }
