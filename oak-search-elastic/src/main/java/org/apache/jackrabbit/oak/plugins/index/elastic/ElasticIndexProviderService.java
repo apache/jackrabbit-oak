d78888360826f75a11f03a2ed2e80a3e52be65c6
Amrit Verma <amrverma@apache.org>
2021-03-09 11:41:23 +0000

    OAK-9375: Remote elastic index deletion job incorrectly deletes indices
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/oak/trunk@1887362 13f79535-47bb-0310-9956-ffa450edef68

--- oak-search-elastic/src/main/java/org/apache/jackrabbit/oak/plugins/index/elastic/ElasticIndexProviderService.java
+++ oak-search-elastic/src/main/java/org/apache/jackrabbit/oak/plugins/index/elastic/ElasticIndexProviderService.java
@@ -110,11 +110,13 @@ public class ElasticIndexProviderService {
                 description = "Local file system path where text extraction cache stores/load entries to recover from timed out operation")
         String localTextExtractionDir();
 
-        @AttributeDefinition(name = "Remote index cleanup frequency", description = "Frequency (in seconds) of running remote index deletion scheduled task")
-        int remoteIndexCleanupFrequency() default 60;
+        @AttributeDefinition(name = "Remote index cleanup frequency", description = "Frequency (in seconds) of running remote index deletion scheduled task." +
+                "Set this to -1 to disable the task. Default is 1 hour.")
+        int remoteIndexCleanupFrequency() default 60*60;
 
-        @AttributeDefinition(name = "Remote index deletion threshold", description = "Time in seconds after which a remote index whose local index is not found gets deleted")
-        int remoteIndexDeletionThreshold() default 300;
+        @AttributeDefinition(name = "Remote index deletion threshold", description = "Time in seconds after which a remote index whose local index is not found gets deleted." +
+                "Default is 1 day.")
+        int remoteIndexDeletionThreshold() default 24*60*60;
     }
 
 
@@ -183,6 +185,9 @@ public class ElasticIndexProviderService {
         boolean reachable = elasticConnection.isAvailable();
         if (!reachable) {
             throw new IllegalArgumentException("Elastic server is not available - " + elasticConnection.toString());
+        }
+        if (contextConfig.remoteIndexCleanupFrequency() == -1) {
+            return;
         }
         ElasticIndexCleaner task = new ElasticIndexCleaner(elasticConnection, nodeStore, contextConfig.remoteIndexDeletionThreshold());
         oakRegs.add(scheduleWithFixedDelay(whiteboard, task, contextConfig.remoteIndexCleanupFrequency()));
