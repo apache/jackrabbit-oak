cd4eca8ef62ad8bff9f610da40a231f1c461c664
amrverma <amrverma@adobe.com>
2021-05-17 14:03:19 +0530

    trivial - removing unused import

--- oak-run-commons/src/main/java/org/apache/jackrabbit/oak/index/IndexOptions.java
+++ oak-run-commons/src/main/java/org/apache/jackrabbit/oak/index/IndexOptions.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 import com.google.common.base.Strings;
