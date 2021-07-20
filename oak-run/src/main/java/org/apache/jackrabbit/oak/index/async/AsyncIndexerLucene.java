6b3ca54e9577a7da28aedf272e4fb544893df634
Nitin Gupta <nitigup@Nitins-MacBook-Pro.local>
2021-07-20 19:39:33 +0530

    Handling failing async lane - needed to add counterIndexEditorProvider as well

--- oak-run/src/main/java/org/apache/jackrabbit/oak/index/async/AsyncIndexerLucene.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/index/async/AsyncIndexerLucene.java
@@ -4,7 +4,10 @@ import com.google.common.io.Closer;
 import org.apache.jackrabbit.oak.index.ExtendedIndexHelper;
 import org.apache.jackrabbit.oak.index.IndexCommand;
 import org.apache.jackrabbit.oak.plugins.index.AsyncIndexUpdate;
+import org.apache.jackrabbit.oak.plugins.index.CompositeIndexEditorProvider;
 import org.apache.jackrabbit.oak.plugins.index.IndexConstants;
+import org.apache.jackrabbit.oak.plugins.index.IndexEditorProvider;
+import org.apache.jackrabbit.oak.plugins.index.counter.NodeCounterEditorProvider;
 import org.apache.jackrabbit.oak.plugins.index.lucene.LuceneIndexEditorProvider;
 import org.apache.jackrabbit.oak.run.cli.NodeStoreFixture;
 import org.apache.jackrabbit.oak.stats.StatisticsProvider;
@@ -13,6 +16,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -44,8 +48,11 @@ public class AsyncIndexerLucene implements Closeable {
 
         for(String name : names) {
             log.info("Setting up Async executor for lane - " + name);
+
+            IndexEditorProvider composite = CompositeIndexEditorProvider
+                    .compose(Arrays.asList(new LuceneIndexEditorProvider(), new NodeCounterEditorProvider()));
             AsyncIndexUpdate task = new AsyncIndexUpdate(name, extendedIndexHelper.getNodeStore(),
-                    new LuceneIndexEditorProvider(), StatisticsProvider.NOOP, false);
+                    composite, StatisticsProvider.NOOP, false);
             // TODO : Handle closure for AsyncIndexUpdate - during command exit, problem is when to do it ? We want this to run in infinite loop
             // TODO : In oak, it gets closed with system bundle deactivation
             //close.register(task);
