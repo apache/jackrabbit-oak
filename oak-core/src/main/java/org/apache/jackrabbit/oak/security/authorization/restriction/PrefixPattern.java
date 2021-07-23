20818f2190d79aba4d460c7ff5634b5342ac3752
angela <anchela@adobe.com>
2021-07-23 14:47:28 +0200

    OAK-9512 : PrefixPattern.matches(String) always returns false OAK-9513 : PrefixPattern ignores empty namespace perfix

--- oak-core/src/main/java/org/apache/jackrabbit/oak/security/authorization/restriction/PrefixPattern.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/security/authorization/restriction/PrefixPattern.java
@@ -20,23 +20,28 @@ import java.util.Set;
 import com.google.common.collect.ImmutableSet;
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.api.Tree;
+import org.apache.jackrabbit.oak.commons.PathUtils;
 import org.apache.jackrabbit.oak.spi.security.authorization.restriction.RestrictionPattern;
 import org.apache.jackrabbit.util.Text;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * Implementation of the
+ * <p>Implementation of the
  * {@link org.apache.jackrabbit.oak.spi.security.authorization.restriction.RestrictionPattern}
  * interface that returns {@code true} if the name of the target property or tree
  * starts with any of the configured namespace prefixes.
+ * </p>s
+ * Note: an empty string prefix will match qualified item names defined with the 
+ * {@link javax.jcr.NamespaceRegistry#NAMESPACE_EMPTY empty namespace}. 
+ * See also sections 
+ * <a href="https://docs.adobe.com/content/docs/en/spec/jcr/2.0/3_Repository_Model.html#3.2.5.2%20Qualified%20Form">3.2.5.2 Qualified Form</a>
+ * and 
+ * <a href="https://docs.adobe.com/content/docs/en/spec/jcr/2.0/3_Repository_Model.html#3.2.5.3%20Qualified%20Form%20with%20the%20Empty%20Namespace">3.2.5.3 Qualified Form with the Empty Namespace</a>
+ * of the JCR v2.0 specification.
  */
 class PrefixPattern implements RestrictionPattern {
-
-    private static final Logger log = LoggerFactory.getLogger(PrefixPattern.class);
-
+    
     private final Set<String> prefixes;
 
     PrefixPattern(@NotNull Iterable<String> prefixes) {
@@ -46,26 +51,22 @@ class PrefixPattern implements RestrictionPattern {
     @Override
     public boolean matches(@NotNull Tree tree, @Nullable PropertyState property) {
         String name = (property != null) ? property.getName() : tree.getName();
-        String prefix = Text.getNamespacePrefix(name);
-        if (!prefix.isEmpty()) {
-            for (String p : prefixes) {
-                if (prefix.equals(p)) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return matchesPrefix(name);
     }
 
     @Override
     public boolean matches(@NotNull String path) {
-        log.debug("Unable to validate node type restriction.");
-        return false;
+        return matchesPrefix(PathUtils.getName(path));
+    }
+    
+    private boolean matchesPrefix(String name) {
+        String prefix = Text.getNamespacePrefix(name);
+        return prefixes.contains(prefix);
     }
 
     @Override
     public boolean matches() {
-        // node type pattern never matches for repository level permissions
+        // prefix pattern never matches for repository level permissions
         return false;
     }
 
