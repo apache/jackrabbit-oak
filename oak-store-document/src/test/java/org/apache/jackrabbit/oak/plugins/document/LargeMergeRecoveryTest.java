3a5fbaf705851cd58c04be7cd5d6649072627cc8
Stefan Egli <stefanegli@apache.org>
2021-08-16 18:15:49 +0200

    OAK-9535 : adding test which reproduces oversizes 'BsonMaximumSizeExceededException: Payload document size is larger than maximum of 16777216'

---
+++ oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/LargeMergeRecoveryTest.java
@@ -0,0 +1,99 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.jackrabbit.oak.plugins.document;
+
+import static org.apache.jackrabbit.oak.plugins.document.Collection.NODES;
+import static org.apache.jackrabbit.oak.plugins.document.TestUtils.disposeQuietly;
+import static org.apache.jackrabbit.oak.plugins.document.util.Utils.getIdFromPath;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
+import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
+import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
+import org.junit.Test;
+
+import com.google.common.collect.Iterables;
+
+public class LargeMergeRecoveryTest extends AbstractTwoNodeTest {
+
+    public LargeMergeRecoveryTest(DocumentStoreFixture fixture) {
+        super(fixture);
+    }
+
+    private static NodeDocument getDocument(DocumentNodeStore nodeStore,
+                                            String path) {
+        return nodeStore.getDocumentStore().find(NODES, getIdFromPath(path));
+    }
+
+    /**
+     * Reproduces OAK-9535
+     */
+    @Test
+    public void testLargeBranchMergeRecovery() throws Exception {
+        // 1. Create base structure /x/y
+        NodeBuilder b1 = ds1.getRoot().builder();
+        b1.child("x").child("y");
+        ds1.merge(b1, EmptyHook.INSTANCE, CommitInfo.EMPTY);
+        ds1.runBackgroundOperations();
+        ds2.runBackgroundOperations();
+
+        final String childPrefix = "childWithMediumLengthJavaContentRepositoryNodeNameInUTFdashEight-";
+
+        // 2. create a branch in C2
+        NodeBuilder b2 = ds2.getRoot().builder();
+        NodeBuilder test = b2.child("x").child("y");
+        System.out.println("Creating large branch merge, can take over a minute...");
+        for (int i = 0; i < DocumentMK.UPDATE_LIMIT * 3; i++) {
+            test.child(childPrefix + i);
+        }
+        System.out.println("Done.");
+
+        ds2.merge(b2, EmptyHook.INSTANCE, CommitInfo.EMPTY);
+
+        Revision zlastRev2 = ds2.getHeadRevision().getRevision(ds2.getClusterId());
+
+        long leaseTime = ds1.getClusterInfo().getLeaseTime();
+        ds1.runBackgroundOperations();
+
+        clock.waitUntil(clock.getTime() + leaseTime + 10);
+
+        // Renew the lease for C1
+        ds1.getClusterInfo().renewLease();
+
+        assertTrue(ds1.getLastRevRecoveryAgent().isRecoveryNeeded());
+
+        Iterable<Integer> cids = ds1.getLastRevRecoveryAgent()
+                .getRecoveryCandidateNodes();
+        assertEquals(1, Iterables.size(cids));
+        assertEquals(c2Id, Iterables.get(cids, 0).intValue());
+
+        System.out.println("RECOVER...");
+        ds1.getLastRevRecoveryAgent().recover(Iterables.get(cids, 0));
+        System.out.println("RECOVER DONE");
+
+        assertEquals(zlastRev2, getDocument(ds1, "/x/y").getLastRev().get(c2Id));
+        assertEquals(zlastRev2, getDocument(ds1, "/x").getLastRev().get(c2Id));
+        assertEquals(zlastRev2, getDocument(ds1, "/").getLastRev().get(c2Id));
+
+        // dispose ds2 quietly because it may now throw an exception
+        disposeQuietly(ds2);
+    }
+}
