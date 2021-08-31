aa28fa7db4fcd96df723403bfae9bcefc3d20c4b
Marcel Reutegger <marcel.reutegger@gmail.com>
2021-08-31 14:17:22 +0200

    OAK-9562: Missing _bin when node is recreated after revision GC
    Fix test on RDB

--- oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/BlobReferenceIteratorTest.java
+++ oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/BlobReferenceIteratorTest.java
@@ -100,7 +100,7 @@ public class BlobReferenceIteratorTest {
         return new DocumentMK.Builder()
                 .setLeaseCheck(false)
                 .clock(clock)
-                .setDocumentStore(fixture.createDocumentStore(clusterId))
+                .setDocumentStore(wrap(fixture.createDocumentStore(clusterId)))
                 .setClusterId(clusterId)
                 .setAsyncDelay(0)
                 .getNodeStore();
@@ -173,5 +173,21 @@ public class BlobReferenceIteratorTest {
         doc = store2.getDocumentStore().find(Collection.NODES, id, 0);
         assertNotNull(doc);
         assertTrue(doc.hasBinary());
+    }
+
+    private static DocumentStore wrap(DocumentStore ds) {
+        return new DocumentStoreTestWrapper(ds);
+    }
+
+    private static class DocumentStoreTestWrapper extends DocumentStoreWrapper {
+
+        private DocumentStoreTestWrapper(DocumentStore store) {
+            super(store);
+        }
+
+        @Override
+        public void dispose() {
+            // ignore
+        }
     }
 }
