3446052ad5f3316d75d4f05cc75e523dde9a1afd
stefan-egli <stefanegli@apache.org>
2021-09-14 14:26:09 +0200

    OAK-9568 : Support custom osgi LeaseFailureHandler (#364)
    * OAK-9535 related : trying to increase mvn memory
    
    * OAK-9568 : Support custom osgi LeaseFailureHandler
    
    * releng : avoid pushing Jenkinsfile changes
    
    * OAK-9568 : custom LeaseFailureHandler support test added
    
    * OAK-9568 : fixing failing test setups
    
    * OAK-9568 : moved LeaseFailureHandler to dedicated, exported, versioned package
    
    * OAK-9568 : LeaseFailureHandler added to bundle's Export-Package, plus IT added

--- oak-store-document/src/main/java/org/apache/jackrabbit/oak/plugins/document/spi/lease/LeaseFailureHandler.java
+++ oak-store-document/src/main/java/org/apache/jackrabbit/oak/plugins/document/spi/lease/LeaseFailureHandler.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.oak.plugins.document;
+package org.apache.jackrabbit.oak.plugins.document.spi.lease;
 
 /**
  * A LeaseFailureHandler can be provided to the DocumentMK.Builder 
