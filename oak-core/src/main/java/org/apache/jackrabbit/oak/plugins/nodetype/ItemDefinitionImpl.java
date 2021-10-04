7158934639e9c1e63414756a16f9de21d542b156
ankitaagar <ankiagarwa@gmail.com>
2021-10-04 23:46:12 +0530

    OAK-9591 - Implement hashcode() and equals() method in ItemDefinitionImpl

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/nodetype/ItemDefinitionImpl.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/nodetype/ItemDefinitionImpl.java
@@ -97,4 +97,29 @@ class ItemDefinitionImpl extends AbstractTypeDefinition
         return getName();
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ItemDefinitionImpl other = (ItemDefinitionImpl) obj;
+        if (type == null) {
+            if (other.type != null)
+                return false;
+        } else if (!type.equals(other.type))
+            return false;
+        return true;
+    }
+
 }
