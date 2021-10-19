dea191494ee48eadc453657141a25714afbc5f53
fabriziofortino <fabrizio.fortino@gmail.com>
2021-10-19 15:16:26 +0200

    OAK-9598: (minor) add missing license

--- oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elastic/ElasticReindexTest.java
+++ oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elastic/ElasticReindexTest.java
@@ -13,7 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package org.apache.jackrabbit.oak.plugins.index.elastic;
+ */
+package org.apache.jackrabbit.oak.plugins.index.elastic;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.jackrabbit.oak.Oak;
