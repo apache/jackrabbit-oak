82a33f5187ca4149849dc0e92475dccb89082684
Carlo Jelmini <jelmini@adobe.com>
2022-01-31 19:56:46 +0100

    OAK-9680 Add support for Azure SAS URIs to oak-segment-azure
    Allow using Shared Access Signature URI to connect to Azure Storage.

--- oak-segment-azure/src/test/java/org/apache/jackrabbit/oak/segment/azure/AzuriteDockerRule.java
+++ oak-segment-azure/src/test/java/org/apache/jackrabbit/oak/segment/azure/AzuriteDockerRule.java
@@ -20,10 +20,12 @@ import com.arakelian.docker.junit.DockerRule;
 import com.arakelian.docker.junit.model.ImmutableDockerConfig;
 import com.microsoft.azure.storage.CloudStorageAccount;
 import com.microsoft.azure.storage.StorageException;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
 import com.microsoft.azure.storage.blob.CloudBlobContainer;
 import com.spotify.docker.client.DefaultDockerClient;
 import com.spotify.docker.client.auth.FixedRegistryAuthSupplier;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.Assume;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
@@ -34,42 +36,57 @@ import java.security.InvalidKeyException;
 
 public class AzuriteDockerRule implements TestRule {
 
-    private static final String IMAGE = "trekawek/azurite";
+    private static final String IMAGE = "mcr.microsoft.com/azure-storage/azurite:3.15.0";
+    
+    public static final String ACCOUNT_KEY = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==";
+    public static final String ACCOUNT_NAME = "devstoreaccount1";
 
     private final DockerRule wrappedRule;
 
     public AzuriteDockerRule() {
         wrappedRule = new DockerRule(ImmutableDockerConfig.builder()
-                .image(IMAGE)
-                .name("oak-test-azurite")
-                .ports("10000")
-                .addStartedListener(container -> {
-                    container.waitForPort("10000/tcp");
-                    container.waitForLog("Azure Blob Storage Emulator listening on port 10000");
-                })
-                .addContainerConfigurer(builder -> builder.env("executable=blob"))
-                .alwaysRemoveContainer(true)
-                .build());
-
+            .image(IMAGE)
+            .name("oak-test-azurite")
+            .ports("10000")
+            .addStartedListener(container -> {
+                container.waitForPort("10000/tcp");
+                container.waitForLog("Azurite Blob service is successfully listening at http://0.0.0.0:10000");
+            })
+            .addContainerConfigurer(builder -> builder.env("executable=blob"))
+            .alwaysRemoveContainer(true)
+            .build());
     }
 
     public CloudBlobContainer getContainer(String name) throws URISyntaxException, StorageException, InvalidKeyException {
-        int mappedPort = getMappedPort();
-        CloudStorageAccount cloud = CloudStorageAccount.parse("DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:" + mappedPort + "/devstoreaccount1;");
-        CloudBlobContainer container = cloud.createCloudBlobClient().getContainerReference(name);
+        CloudStorageAccount cloud = getCloudStorageAccount();
+        CloudBlobClient cloudBlobClient = cloud.createCloudBlobClient();
+        CloudBlobContainer container = cloudBlobClient.getContainerReference(name);
         container.deleteIfExists();
         container.create();
         return container;
+    }
+
+    public CloudStorageAccount getCloudStorageAccount() throws URISyntaxException, InvalidKeyException {
+        String blobEndpoint = "BlobEndpoint=" + getBlobEndpoint();
+        String accountName = "AccountName=" + ACCOUNT_NAME;
+        String accountKey = "AccountKey=" + ACCOUNT_KEY;
+        return CloudStorageAccount.parse("DefaultEndpointsProtocol=http;" + ";" + accountName + ";" + accountKey + ";" + blobEndpoint);
+    }
+
+    @NotNull
+    public String getBlobEndpoint() {
+        int mappedPort = getMappedPort();
+        return "http://127.0.0.1:" + mappedPort + "/devstoreaccount1";
     }
 
     @Override
     public Statement apply(Statement statement, Description description) {
         try {
             DefaultDockerClient client = DefaultDockerClient.fromEnv()
-                    .connectTimeoutMillis(5000L)
-                    .readTimeoutMillis(20000L)
-                    .registryAuthSupplier(new FixedRegistryAuthSupplier())
-                    .build();
+                .connectTimeoutMillis(5000L)
+                .readTimeoutMillis(20000L)
+                .registryAuthSupplier(new FixedRegistryAuthSupplier())
+                .build();
             client.ping();
             client.pull(IMAGE);
             client.close();
