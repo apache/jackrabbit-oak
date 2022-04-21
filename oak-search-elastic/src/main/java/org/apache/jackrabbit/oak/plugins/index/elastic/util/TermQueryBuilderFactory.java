9f1e2d2563f813161b479153bca75d3155b63e14
fabriziofortino <fabrizio.fortino@gmail.com>
2022-04-21 21:33:43 +0200

    fixed range queries

--- oak-search-elastic/src/main/java/org/apache/jackrabbit/oak/plugins/index/elastic/util/TermQueryBuilderFactory.java
+++ oak-search-elastic/src/main/java/org/apache/jackrabbit/oak/plugins/index/elastic/util/TermQueryBuilderFactory.java
@@ -21,20 +21,11 @@ import org.apache.jackrabbit.oak.commons.PathUtils;
 import org.apache.jackrabbit.oak.plugins.index.search.FieldNames;
 import org.apache.jackrabbit.oak.plugins.index.search.spi.query.FulltextIndexPlanner;
 import org.apache.jackrabbit.oak.spi.query.Filter;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.PrefixQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.RangeQueryBuilder;
-import org.elasticsearch.index.query.TermQueryBuilder;
-import org.elasticsearch.index.query.WildcardQueryBuilder;
 import org.jetbrains.annotations.NotNull;
 
 import co.elastic.clients.elasticsearch._types.FieldValue;
 import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.Query;
-import co.elastic.clients.elasticsearch._types.query_dsl.RangeQuery;
-import co.elastic.clients.elasticsearch._types.query_dsl.TermQuery;
 import co.elastic.clients.json.JsonData;
 
 import java.util.List;
@@ -45,10 +36,6 @@ import static org.apache.jackrabbit.JcrConstants.JCR_MIXINTYPES;
 import static org.apache.jackrabbit.JcrConstants.JCR_PRIMARYTYPE;
 import static org.apache.jackrabbit.oak.plugins.index.search.FieldNames.PATH;
 import static org.apache.jackrabbit.oak.plugins.index.search.FieldNames.PATH_DEPTH;
-import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
-import static org.elasticsearch.index.query.QueryBuilders.prefixQuery;
-import static org.elasticsearch.index.query.QueryBuilders.termQuery;
-import static org.elasticsearch.index.query.QueryBuilders.wildcardQuery;
 
 public class TermQueryBuilderFactory {
     /**
@@ -126,33 +113,26 @@ public class TermQueryBuilderFactory {
                                 .stringValue(type))));
     }
     
-    //TODO Angela check newRangeQuery is doing what is expected. See original code bellow:
-//    private static <R> RangeQueryBuilder newRangeQuery(String field, R first, R last, boolean firstIncluding,
-//            boolean lastIncluding) {
-//        return QueryBuilders.rangeQuery(field).from(first).to(last).includeLower(firstIncluding)
-//                .includeUpper(lastIncluding);
-//    }
-    
     private static <R> Query newRangeQuery(String field, R first, R last, boolean firstIncluding,
             boolean lastIncluding) {
-        RangeQuery.Builder rqBuilder = new RangeQuery.Builder()
-                .field(field);
-        if(first!=null) {
-            if(firstIncluding) {
-                rqBuilder.lte(JsonData.of(first));
-            }else {
-                rqBuilder.lt(JsonData.of(first));
+
+        return Query.of(fn -> fn.range(fnr -> {
+            if (first != null) {
+                if (firstIncluding) {
+                    fnr.gte(JsonData.of(first));
+                } else {
+                    fnr.gt(JsonData.of(first));
+                }
             }
-        }
-        if(last!=null) {
-            if(lastIncluding) {
-                rqBuilder.gte(JsonData.of(last));
-            }else {
-                rqBuilder.gt(JsonData.of(last));
+            if (last != null) {
+                if (lastIncluding) {
+                    fnr.lte(JsonData.of(last));
+                } else {
+                    fnr.lt(JsonData.of(last));
+                }
             }
-        }
-        return Query.of(q->q
-                .range(rqBuilder.build()));
+            return fnr.field(field);
+        }));
     }
 
     private static <R> Query newInQuery(String field, List<R> values) {
@@ -164,7 +144,6 @@ public class TermQueryBuilderFactory {
                 .bool(bqBuilder.build()));
     }
 
-    // TODO Angela check newPropertyREstrictionQuery. It may fail because of the use of .toString.
     public static <R> Query newPropertyRestrictionQuery(String propertyName, Filter.PropertyRestriction pr,
             Function<PropertyValue, R> propToObj) {
 
@@ -173,10 +152,7 @@ public class TermQueryBuilderFactory {
         R not = pr.not != null ? propToObj.apply(pr.not) : null;
         if (pr.first != null && pr.first.equals(pr.last) && pr.firstIncluding && pr.lastIncluding) {
             // [property]=[value]
-            return Query.of(q->q
-                    .term(t->t
-                            .field(propertyName)
-                            .value(FieldValue.of(first.toString()))));
+            return Query.of(q -> q.term(t -> t.field(propertyName).value(FieldValue.of(first.toString()))));
         } else if (pr.first != null && pr.last != null) {
             return newRangeQuery(propertyName, first, last, pr.firstIncluding, pr.lastIncluding);
         } else if (pr.first != null) {
@@ -189,12 +165,13 @@ public class TermQueryBuilderFactory {
             return newInQuery(propertyName, pr.list.stream().map(propToObj).collect(Collectors.toList()));
         } else if (pr.isNot && pr.not != null) {
             // MUST_NOT [property]=[value]
-            return Query.of(q->q
-                    .bool(b->b
-                            .mustNot(mn->mn
-                                    .term(t->t
-                                            .field(propertyName)
-                                            .value(FieldValue.of(not.toString()))))));
+            return Query.of(q -> q.bool(b -> b
+                    .mustNot(mn -> mn
+                            .term(t -> t
+                                    .field(propertyName)
+                                    .value(FieldValue.of(not.toString()))))
+                    )
+            );
             // This helps with the NOT equal to condition for given property
         } else {
             return null;
