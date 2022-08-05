5ee6730f50171c657c40bbed986270a511c48229
Nitin Gupta <nitigup@adobe.com>
2022-08-05 10:23:45 +0530

    OAK-9858 | Making ES indexing sync(similar to lucene) for IndexQueryCommonTests that are based on text files

--- oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elastic/ElasticIndexQueryCommonTest.java
+++ oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elastic/ElasticIndexQueryCommonTest.java
@@ -93,7 +93,6 @@ public class ElasticIndexQueryCommonTest extends IndexQueryCommonTest {
     }
 
     @Override
-    @Ignore("OAK-9858")
     @Test
     public void sql2FullText() throws Exception {
         super.sql2FullText();
