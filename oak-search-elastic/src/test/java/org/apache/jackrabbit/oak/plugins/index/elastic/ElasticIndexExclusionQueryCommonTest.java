4764425d2b2dcfc4e3073371685b13a8d6fd7ad0
nit0906 <nitingupta0906@gmail.com>
2022-08-17 08:37:57 +0530

    OAK-9878 | Adding test for exclusion and inclusion properties in full… (#668)
    * OAK-9878 | Adding test for exclusion and inclusion properties in fulltext index + porting to common tests to be used by both lucene and elastic

--- oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elastic/ElasticIndexExclusionQueryCommonTest.java
+++ oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elastic/ElasticIndexExclusionQueryCommonTest.java
@@ -14,38 +14,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.jackrabbit.oak.plugins.index.lucene;
+package org.apache.jackrabbit.oak.plugins.index.elastic;
 
 import org.apache.jackrabbit.oak.InitialContentHelper;
 import org.apache.jackrabbit.oak.api.ContentRepository;
 import org.apache.jackrabbit.oak.plugins.index.IndexExclusionQueryCommonTest;
-import org.apache.jackrabbit.oak.plugins.index.LuceneIndexOptions;
 import org.apache.jackrabbit.oak.plugins.memory.MemoryNodeStore;
-import org.junit.Rule;
-import org.junit.rules.TemporaryFolder;
+import org.junit.ClassRule;
 
-import java.io.File;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+public class ElasticIndexExclusionQueryCommonTest extends IndexExclusionQueryCommonTest {
 
-/**
- * Tests the {@link LuceneIndexProvider} exclusion settings
- */
-public class LuceneIndexExclusionQueryCommonTest extends IndexExclusionQueryCommonTest {
-
-    private ExecutorService executorService = Executors.newFixedThreadPool(2);
-
-    @Rule
-    public TemporaryFolder temporaryFolder = new TemporaryFolder(new File("target"));
+    @ClassRule
+    public static final ElasticConnectionRule elasticRule = new ElasticConnectionRule(
+            ElasticTestUtils.ELASTIC_CONNECTION_STRING);
 
     @Override
     protected ContentRepository createRepository() {
-        indexOptions = new LuceneIndexOptions();
-        LuceneTestRepositoryBuilder luceneTestRepositoryBuilder = new LuceneTestRepositoryBuilder(executorService, temporaryFolder);
-        luceneTestRepositoryBuilder.setNodeStore(new MemoryNodeStore(InitialContentHelper.INITIAL_CONTENT));
-        repositoryOptionsUtil = luceneTestRepositoryBuilder.build();
+        indexOptions = new ElasticIndexOptions();
+        ElasticTestRepositoryBuilder builder = new ElasticTestRepositoryBuilder(elasticRule);
+        builder.setNodeStore(new MemoryNodeStore(InitialContentHelper.INITIAL_CONTENT));
+        repositoryOptionsUtil = builder.build();
 
         return repositoryOptionsUtil.getOak().createContentRepository();
     }
-
 }
