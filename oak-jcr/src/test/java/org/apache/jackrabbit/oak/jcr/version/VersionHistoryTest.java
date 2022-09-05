f8324ac8c7d582cf03d8b2b22689bc694d155074
Arun Kumar Ram <arunram@Aruns-MacBook-Pro.local>
2022-09-05 18:12:08 +0530

    OAK-9891:refactored test case

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/version/VersionHistoryTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/version/VersionHistoryTest.java
@@ -27,6 +27,9 @@ import javax.jcr.version.VersionManager;
 import org.apache.jackrabbit.JcrConstants;
 import org.apache.jackrabbit.test.AbstractJCRTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Contains {@link VersionHistory} related tests.
  */
@@ -175,26 +178,31 @@ public class VersionHistoryTest extends AbstractJCRTest {
 
         VersionHistory vhr = vm.getVersionHistory(n.getPath());
         // initialize versionName
-        String versionName= "";
+        String versionName = "";
         VersionIterator allversions = vhr.getAllVersions();
-        int count=0;
+        int count = 0;
         while (allversions.hasNext()) {
             Version version = allversions.nextVersion();
             if(count == 1) {
-                versionName=version.getName();
+                versionName = version.getName();
             }
             count++;
         }
-        vhr.addVersionLabel(versionName,"Label_" + versionName + "_0",false);
-        vhr.addVersionLabel(versionName,"Label_" + versionName + "_1",false);
-        vhr.addVersionLabel(versionName,"Label_" + versionName + "_2",false);
+        int versonLabelCount = 3;
+        List<String> versionLabels = new ArrayList<>();
+        for(int i = 0; i < versonLabelCount; i++) {
+            String labelName = "Label_" + versionName + "_" + i;
+            vhr.addVersionLabel(versionName, labelName,false);
+            versionLabels.add(labelName);
+        }
+
         superuser.save();
         vhr.removeVersion(versionName);
         superuser.save();
 
-        assertEquals("version label should not exist", false, vhr.hasVersionLabel("Label_" + versionName + "_0"));
-        assertEquals("version label should not exist", false, vhr.hasVersionLabel("Label_" + versionName + "_1"));
-        assertEquals("version label should not exist", false, vhr.hasVersionLabel("Label_" + versionName + "_2"));
+        for(String label : versionLabels) {
+            assertEquals("version label should not exist", false, vhr.hasVersionLabel(label));
+        }
 
     }
 
