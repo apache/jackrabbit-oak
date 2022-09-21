c4c1f53beeeb45491373fd4aac3b4fcc3046bd23
Nuno Santos <nsantos@adobe.com>
2022-09-21 11:32:42 +0200

    Dummy commit to trigger unit tests.

--- oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elastic/index/ElasticIndexHelperTest.java
+++ oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elastic/index/ElasticIndexHelperTest.java
@@ -65,7 +65,6 @@ public class ElasticIndexHelperTest {
         IndexDefinitionBuilder.IndexRule indexRuleB = builder.indexRule("typeB");
         indexRuleB.property("foo").type("Boolean");
         NodeState nodeState = builder.build();
-
         ElasticIndexDefinition definition =
                 new ElasticIndexDefinition(nodeState, nodeState, "path", "prefix");
 
