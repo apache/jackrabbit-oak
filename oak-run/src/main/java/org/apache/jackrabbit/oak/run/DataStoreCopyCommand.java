e4aec07fc9fb2b6c95128e38b46f0e4cae2d27f7
fabriziofortino <fabrizio.fortino@gmail.com>
2022-10-05 16:24:03 +0200

    OAK-9960: (oak-run) introduced datastore-copy command

---
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/run/DataStoreCopyCommand.java
@@ -0,0 +1,159 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.run;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+import org.apache.jackrabbit.oak.commons.IOUtils;
+import org.apache.jackrabbit.oak.run.commons.Command;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static com.google.common.base.StandardSystemProperty.FILE_SEPARATOR;
+
+/**
+ * Command to concurrently download blobs from an azure datastore using sas token authentication.
+ *
+ * Blobs are stored in a specific folder following the datastore structure format.
+ */
+public class DataStoreCopyCommand implements Command {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DataStoreCopyCommand.class);
+
+    private String sourceRepo;
+    private String includePath;
+    private File fileIncludePath;
+    private String sasToken;
+    private String outDir;
+    private int concurrency;
+
+    @Override
+    public void execute(String... args) throws Exception {
+        parseCommandLineParams(args);
+
+        List<String> ids;
+        if (fileIncludePath != null) {
+            if (fileIncludePath.isDirectory() || !fileIncludePath.exists()) {
+                throw new IllegalArgumentException("file-include-path must be a valid file");
+            }
+            ids = org.apache.commons.io.IOUtils.readLines(Files.newInputStream(fileIncludePath.toPath()), StandardCharsets.UTF_8);
+        } else {
+            ids = Arrays.stream(includePath.split(";")).collect(Collectors.toList());
+        }
+        if (ids.isEmpty()) {
+            throw new IllegalArgumentException("Blob ids must be specified");
+        }
+
+        try (Downloader downloader = new Downloader(concurrency)) {
+            long start = System.currentTimeMillis();
+            List<Downloader.ItemResponse> responses = downloader.download(ids.stream().map(id -> {
+                Downloader.Item item = new Downloader.Item();
+                item.source = sourceRepo + "/" + id;
+                if (sasToken != null) {
+                    item.source += "?" + sasToken;
+                }
+                // Rename the blob names to match expected datastore cache format (remove the "-" in the name)
+                String blobName = id.replaceAll("-", "");
+                if (id.length() < 6) {
+                    LOG.warn("Blob with name {} is less than 6 chars. Cannot create data folder structure. Storing in the root folder", blobName);
+                    item.destination = outDir + FILE_SEPARATOR.value() + blobName;
+                } else {
+                    item.destination = outDir + FILE_SEPARATOR.value()
+                            + blobName.substring(0, 2) + FILE_SEPARATOR.value() + blobName.substring(2, 4) + FILE_SEPARATOR.value()
+                            + blobName.substring(4, 6) + FILE_SEPARATOR.value() + blobName;
+                }
+                return item;
+            }).collect(Collectors.toList()));
+            long totalTime = System.currentTimeMillis() - start;
+
+            Map<Boolean, List<Downloader.ItemResponse>> partitioned =
+                    responses.stream().collect(Collectors.partitioningBy(ir -> ir.failed));
+
+            List<Downloader.ItemResponse> success = partitioned.get(false);
+            if (!success.isEmpty()) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("The following blobs were successfully downloaded:");
+                    success.forEach(s -> LOG.debug("{} [{}] downloaded in {} ms", s.item.source,
+                            IOUtils.humanReadableByteCount(s.size), s.time));
+                }
+
+                long totalBytes = success.stream().mapToLong(s -> s.size).sum();
+                if (totalTime > 60000) {
+                    LOG.info("Elapsed Time (Minutes): {}", TimeUnit.MILLISECONDS.toMinutes(totalTime));
+                } else {
+                    LOG.info("Elapsed Time (Seconds): {}", TimeUnit.MILLISECONDS.toSeconds(totalTime));
+                }
+                LOG.info("Number of File Transfers: {}", success.size());
+                LOG.info("TotalBytesTransferred: {}", totalBytes);
+                if (totalBytes > 10_000_000) {
+                    LOG.info("Speed (MB/sec): {}", (totalBytes / (1024 * 1024) / ((double) totalTime / 1000)));
+                } else {
+                    LOG.info("Speed (KB/sec): {}", (totalBytes / (1024) / ((double) totalTime / 1000)));
+                }
+            }
+
+            List<Downloader.ItemResponse> failures = partitioned.get(true);
+            if (!failures.isEmpty()) {
+                LOG.error("The following blobs threw an error:");
+                failures.forEach(f -> LOG.error(f.item.source, f.throwable));
+                throw new IllegalStateException("Errors while downloading blobs");
+            }
+        }
+
+    }
+
+    private void parseCommandLineParams(String... args) {
+        OptionParser parser = new OptionParser();
+
+        // options available for get-blobs only
+        OptionSpec<String> sourceRepoOpt = parser.accepts("source-repo", "The source repository url")
+                .withRequiredArg().ofType(String.class);
+        OptionSpec<String> includePathOpt = parser.accepts("include-path",
+                        "Include only these paths when copying (separated by semicolon)")
+                .withRequiredArg().ofType(String.class);
+        OptionSpec<File> fileIncludePathOpt = parser.accepts("file-include-path",
+                "Include only the paths specified in the file (separated by newline)").availableUnless(includePathOpt)
+                .withRequiredArg().ofType(File.class);
+        OptionSpec<String> sasTokenOpt = parser.accepts("sas-token", "The SAS token to access Azure Storage")
+                .withRequiredArg().ofType(String.class);
+        OptionSpec<String> outDirOpt = parser.accepts("out-dir",
+                "Path where to store the blobs (Optional). Otherwise, blobs will be stored in the current directory.")
+                .withRequiredArg().ofType(String.class).defaultsTo(System.getProperty("user.dir") + FILE_SEPARATOR.value() + "blobs");
+        OptionSpec<Integer> concurrencyOpt = parser.accepts("concurrency",
+                        "Max number of concurrent requests that can occur (the default value is equal to 16 multiplied by the number of cores)")
+                .withRequiredArg().ofType(Integer.class).defaultsTo(16 * Runtime.getRuntime().availableProcessors());
+
+        OptionSet optionSet = parser.parse(args);
+
+        this.sourceRepo = optionSet.valueOf(sourceRepoOpt);
+        this.includePath = optionSet.valueOf(includePathOpt);
+        this.fileIncludePath = optionSet.valueOf(fileIncludePathOpt);
+        this.sasToken = optionSet.valueOf(sasTokenOpt);
+        this.outDir = optionSet.valueOf(outDirOpt);
+        this.concurrency = optionSet.valueOf(concurrencyOpt);
+    }
+}
---
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/run/Downloader.java
@@ -0,0 +1,142 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.jackrabbit.oak.run;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+/**
+ * Generic concurrent file downloader which uses Java NIO channels to potentially leverage OS internal optimizations.
+ */
+public class Downloader implements Closeable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Downloader.class);
+
+    private final ExecutorService executorService;
+
+    public Downloader(int concurrency) {
+        LOG.info("Initializing Downloader with max number of concurrent requests={}", concurrency);
+        this.executorService = new ThreadPoolExecutor(
+                (int) Math.ceil(concurrency * .1), concurrency, 60L, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(),
+                new ThreadFactoryBuilder()
+                        .setNameFormat("downloader-%d")
+                        .setDaemon(true)
+                        .build()
+        );
+    }
+
+    public List<ItemResponse> download(List<Item> items) {
+        LOG.debug("Preparing to download {} items.\n{}", items.size(), items);
+        try {
+            return executorService
+                    .invokeAll(items.stream().map(DownloadWorker::new).collect(Collectors.toList()))
+                    .stream()
+                    .map(itemResponseFuture -> {
+                        try {
+                            return itemResponseFuture.get();
+                        } catch (InterruptedException | ExecutionException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .collect(Collectors.toList());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        executorService.shutdown();
+    }
+
+    public static class Item {
+        public String source;
+        public String destination;
+
+        @Override
+        public String toString() {
+            return "Item{" +
+                    "source='" + source + '\'' +
+                    ", destination='" + destination + '\'' +
+                    '}';
+        }
+    }
+
+    public static class ItemResponse {
+        public final Item item;
+        public boolean failed;
+        public long size;
+        public long time;
+        public Throwable throwable;
+
+        public ItemResponse(Item item) {
+            this.item = item;
+        }
+    }
+
+    private static class DownloadWorker implements Callable<ItemResponse> {
+
+        private final Item item;
+
+        DownloadWorker(Item item) {
+            this.item = item;
+        }
+
+        @Override
+        public ItemResponse call() {
+            ItemResponse response  = new ItemResponse(item);
+            long t0 = System.currentTimeMillis();
+            try {
+                URL sourceUrl = new URL(item.source);
+                File destinationFile = new File(item.destination);
+                destinationFile.getParentFile().mkdirs();
+                try (ReadableByteChannel byteChannel = Channels.newChannel(sourceUrl.openStream());
+                     FileOutputStream outputStream = new FileOutputStream(destinationFile)) {
+                    response.size = outputStream.getChannel()
+                            .transferFrom(byteChannel, 0, Long.MAX_VALUE);
+                }
+            } catch (Exception e) {
+                response.failed = true;
+                response.throwable = e;
+            } finally {
+                response.time = System.currentTimeMillis() - t0;
+            }
+            return response;
+        }
+    }
+
+}
