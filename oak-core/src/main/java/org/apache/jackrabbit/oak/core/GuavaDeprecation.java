4d744f146670a821c1735ca528a5b82055ee880c
Julian Reschke <reschke@apache.org>
2022-11-16 16:29:43 +0100

    OAK-10002: oak-core: bump up logging for deprecated Guava based APIs to INFO (#757)

--- oak-core/src/main/java/org/apache/jackrabbit/oak/core/GuavaDeprecation.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/core/GuavaDeprecation.java
@@ -25,7 +25,7 @@ public class GuavaDeprecation {
 
     private static final Logger LOG = LoggerFactory.getLogger(GuavaDeprecation.class);
 
-    private static final String DEFAULT = "debug";
+    private static final String DEFAULT = "info";
 
     private static final String TLOGLEVEL = System.getProperty(GuavaDeprecation.class + ".LOGLEVEL", DEFAULT);
 
