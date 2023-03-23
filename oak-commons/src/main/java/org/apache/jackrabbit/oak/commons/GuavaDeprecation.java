1e7f10444a0b6492a2b31afc31326da608e89751
Julian Reschke <reschke@apache.org>
2023-03-23 09:52:53 +0100

    OAK-10106: Guava deprecations: set minimal logging level to 'warn'

--- oak-commons/src/main/java/org/apache/jackrabbit/oak/commons/GuavaDeprecation.java
+++ oak-commons/src/main/java/org/apache/jackrabbit/oak/commons/GuavaDeprecation.java
@@ -38,7 +38,6 @@ public class GuavaDeprecation {
         switch (TLOGLEVEL.toLowerCase(Locale.ENGLISH)) {
             case "error":
             case "warn":
-            case "info":
                 t = TLOGLEVEL.toLowerCase(Locale.ENGLISH);
                 break;
             default:
