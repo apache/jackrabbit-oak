aa6508e6437ecd18ebf8bec60354aff55f3279a7
Julian Reschke <julian.reschke@gmx.de>
2023-04-13 08:16:46 +0100

    OAK-10186: restore import order

--- oak-it/src/test/java/org/apache/jackrabbit/oak/composite/CompositeNodeStoreTest.java
+++ oak-it/src/test/java/org/apache/jackrabbit/oak/composite/CompositeNodeStoreTest.java
@@ -18,9 +18,9 @@
  */
 package org.apache.jackrabbit.oak.composite;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static org.apache.jackrabbit.guava.common.base.Predicates.compose;
 import static org.apache.jackrabbit.guava.common.collect.Iterables.filter;
+import static com.google.common.collect.Lists.newArrayList;
 import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.INDEX_DEFINITIONS_NAME;
 import static org.apache.jackrabbit.oak.plugins.index.IndexUtils.createIndexDefinition;
 import static org.apache.jackrabbit.oak.spi.state.ChildNodeEntry.GET_NAME;
@@ -46,10 +46,12 @@ import java.util.concurrent.TimeUnit;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.jackrabbit.guava.common.base.Predicate;
-import org.apache.jackrabbit.guava.common.base.Predicates;
 import org.apache.jackrabbit.guava.common.collect.Iterables;
+import com.google.common.collect.ImmutableSet;
+import org.apache.jackrabbit.guava.common.base.Predicates;
+import com.google.common.collect.Lists;
+import org.apache.commons.io.FileUtils;
 import org.apache.jackrabbit.oak.api.Blob;
 import org.apache.jackrabbit.oak.api.CommitFailedException;
 import org.apache.jackrabbit.oak.api.Type;
@@ -85,9 +87,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
-
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
 
 @RunWith(Parameterized.class)
 public class CompositeNodeStoreTest {
