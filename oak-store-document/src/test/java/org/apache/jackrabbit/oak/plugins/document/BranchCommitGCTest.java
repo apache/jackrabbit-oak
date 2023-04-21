0b780f2d5cf23e940456f87daef2ac53f19edd25
Ankita Agarwal <ankiagarwa@gmail.com>
2023-04-21 11:39:35 +0530

    OAK-8646 - Tests for detecting changes from orphaned branch commits

--- oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/BranchCommitGCTest.java
+++ oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/BranchCommitGCTest.java
@@ -1,14 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.jackrabbit.oak.plugins.document;
 
 import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
 import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
 import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
 import org.apache.jackrabbit.oak.stats.Clock;
-import org.junit.*;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.Before;
+import org.junit.After;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static java.util.concurrent.TimeUnit.HOURS;
 import static org.apache.jackrabbit.oak.plugins.document.TestUtils.persistToBranch;
-import static org.junit.Assert.*;
 
 public class BranchCommitGCTest {
 
@@ -72,7 +93,7 @@ public class BranchCommitGCTest {
         b.child("test");
         persistToBranch(b);
 
-        // b must see 'a' and 'b'
+        // b must see 'foo' and 'test'
         assertTrue(b.hasChildNode("foo"));
         assertTrue(b.hasChildNode("test"));
 
