7ec92c5f96195320daf115f0c8abfa0cf07f840a
Carlo Jelmini <jelmini@adobe.com>
2023-05-11 16:27:16 +0200

    OAK-10237 Switch oak-blob to shaded Guava

--- oak-blob/src/test/java/org/apache/jackrabbit/oak/spi/blob/split/SplitBlobStoreTest.java
+++ oak-blob/src/test/java/org/apache/jackrabbit/oak/spi/blob/split/SplitBlobStoreTest.java
@@ -35,7 +35,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.google.common.io.Files;
+import org.apache.jackrabbit.guava.common.io.Files;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
