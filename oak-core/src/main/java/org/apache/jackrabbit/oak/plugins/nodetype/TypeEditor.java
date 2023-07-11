95948dc005ba28b2ffc4d52a3f4fc48474744906
Mark Adamcin <adamcin@adobe.com>
2023-07-11 08:15:21 -0700

    restored original variable names

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/nodetype/TypeEditor.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/nodetype/TypeEditor.java
@@ -289,15 +289,15 @@ public class TypeEditor extends DefaultEditor {
     public TypeEditor childNodeChanged(
             String name, NodeState before, NodeState after)
             throws CommitFailedException {
-        String childPrimary = after.getName(JCR_PRIMARYTYPE);
-        Iterable<String> childMixins = after.getNames(JCR_MIXINTYPES);
+        String primary = after.getName(JCR_PRIMARYTYPE);
+        Iterable<String> mixins = after.getNames(JCR_MIXINTYPES);
 
         final NodeBuilder childBuilder = builder.getChildNode(name);
-        if (childPrimary == null && effective != null) {
+        if (primary == null && effective != null) {
             // no primary type defined, find and apply a default type
-            childPrimary = effective.getDefaultType(name);
-            if (childPrimary != null) {
-                childBuilder.setProperty(JCR_PRIMARYTYPE, childPrimary, NAME);
+            primary = effective.getDefaultType(name);
+            if (primary != null) {
+                childBuilder.setProperty(JCR_PRIMARYTYPE, primary, NAME);
             } else {
                 constraintViolation(
                         4, "No default primary type available "
@@ -306,8 +306,8 @@ public class TypeEditor extends DefaultEditor {
         }
 
         // if node type didn't change no need to validate child node
-        boolean validate = primaryChanged(before, childPrimary) || mixinsChanged(before, childMixins);
-        TypeEditor editor = new TypeEditor(this, name, childPrimary, childMixins, childBuilder, validate);
+        boolean validate = primaryChanged(before, primary) || mixinsChanged(before, mixins);
+        TypeEditor editor = new TypeEditor(this, name, primary, mixins, childBuilder, validate);
         if (checkThisNode && validate && !effective.isValidChildNode(name, editor.getEffective())) {
             constraintViolation(
                     1, "No matching definition found for child node " + name
