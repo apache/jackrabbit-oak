cdfb9045334ee0cdd1beaea3834e905f2238e4ca
Steffen Van <svan@adobe.com>
2023-08-25 18:47:42 +0200

    refactor: updated to lucene 90 codec

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/util/CompressingCodec.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/util/CompressingCodec.java
@@ -18,15 +18,26 @@
 */
 package org.apache.jackrabbit.oak.plugins.index.lucene.util;
 
-import org.apache.lucene.codecs.*;
-import org.apache.lucene.codecs.compressing.CompressingStoredFieldsFormat;
-import org.apache.lucene.codecs.compressing.CompressingTermVectorsFormat;
+import org.apache.lucene.backward_codecs.lucene50.Lucene50TermVectorsFormat;
+import org.apache.lucene.backward_codecs.lucene70.Lucene70SegmentInfoFormat;
+import org.apache.lucene.backward_codecs.lucene90.Lucene90Codec;
+import org.apache.lucene.backward_codecs.lucene90.Lucene90FieldInfosFormat;
+import org.apache.lucene.codecs.DocValuesFormat;
+import org.apache.lucene.codecs.FieldInfosFormat;
+import org.apache.lucene.codecs.FilterCodec;
+import org.apache.lucene.codecs.LiveDocsFormat;
+import org.apache.lucene.codecs.NormsFormat;
+import org.apache.lucene.codecs.PostingsFormat;
+import org.apache.lucene.codecs.SegmentInfoFormat;
+import org.apache.lucene.codecs.StoredFieldsFormat;
+import org.apache.lucene.codecs.TermVectorsFormat;
 import org.apache.lucene.codecs.compressing.CompressionMode;
-import org.apache.lucene.codecs.lucene40.Lucene40LiveDocsFormat;
-import org.apache.lucene.codecs.lucene42.Lucene42NormsFormat;
-import org.apache.lucene.codecs.lucene46.Lucene46Codec;
-import org.apache.lucene.codecs.lucene46.Lucene46FieldInfosFormat;
-import org.apache.lucene.codecs.lucene46.Lucene46SegmentInfoFormat;
+import org.apache.lucene.codecs.lucene90.Lucene90LiveDocsFormat;
+import org.apache.lucene.codecs.lucene90.Lucene90NormsFormat;
+import org.apache.lucene.codecs.lucene90.Lucene90SegmentInfoFormat;
+import org.apache.lucene.codecs.lucene90.Lucene90TermVectorsFormat;
+import org.apache.lucene.codecs.lucene90.compressing.Lucene90CompressingStoredFieldsFormat;
+import org.apache.lucene.codecs.lucene94.Lucene94FieldInfosFormat;
 
 /**
  * Lucene Codec aimed to reduce index size as much as possible by enabling highest possible compression on term vectors and stored fields.
@@ -36,19 +47,20 @@ public class CompressingCodec extends FilterCodec {
     private static final int CHUNK_SIZE = 1024;
     private static final String SEGMENT_SUFFIX = "ctv";
 
-    private final TermVectorsFormat vectorsFormat = new CompressingTermVectorsFormat("Lucene41",
-            SEGMENT_SUFFIX, CompressionMode.HIGH_COMPRESSION, CHUNK_SIZE);
-    private final FieldInfosFormat fieldInfosFormat = new Lucene46FieldInfosFormat();
-    private final SegmentInfoFormat segmentInfosFormat = new Lucene46SegmentInfoFormat();
-    private final LiveDocsFormat liveDocsFormat = new Lucene40LiveDocsFormat();
-    private final PostingsFormat defaultFormat = PostingsFormat.forName("Lucene41");
-    private final DocValuesFormat defaultDVFormat = DocValuesFormat.forName("Lucene45");
-    private final NormsFormat normsFormat = new Lucene42NormsFormat();
-    private final StoredFieldsFormat fieldsFormat = new CompressingStoredFieldsFormat("Lucene41",
-            CompressionMode.HIGH_COMPRESSION, CHUNK_SIZE);
+    // TODO: Should we upgrade this to lucene 95? 
+    private final TermVectorsFormat vectorsFormat = new Lucene90TermVectorsFormat();
+    private final FieldInfosFormat fieldInfosFormat = new Lucene90FieldInfosFormat();
+    private final SegmentInfoFormat segmentInfosFormat = new Lucene90SegmentInfoFormat();
+//    private final LiveDocsFormat liveDocsFormat = new Lucene40LiveDocsFormat();
+    private final LiveDocsFormat liveDocsFormat = new Lucene90LiveDocsFormat();
+    private final PostingsFormat defaultFormat = PostingsFormat.forName("Lucene90");
+    private final DocValuesFormat defaultDVFormat = DocValuesFormat.forName("Lucene90");
+    private final NormsFormat normsFormat = new Lucene90NormsFormat();
+    private final StoredFieldsFormat fieldsFormat = new Lucene90CompressingStoredFieldsFormat("Lucene90", CompressionMode.FAST, CHUNK_SIZE, 1024, 1024);
+    
 
     public CompressingCodec() {
-        super("compressingCodec", new Lucene46Codec());
+        super("compressingCodec", new Lucene90Codec());
     }
 
     @Override
