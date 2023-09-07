3294f287d95ca66af8737b5d6e3d832661c81461
Nuno Santos <nsantos@adobe.com>
2023-09-07 14:03:49 +0200

    Simplify logic.

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/MetricsFormatterTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/MetricsFormatterTest.java
@@ -25,11 +25,11 @@ public class MetricsFormatterTest {
     @Test
     public void testAdd() {
         MetricsFormatter metricsFormatter = MetricsFormatter.newBuilder();
-        metricsFormatter.add("key", "value");
-        metricsFormatter.add("key", 1);
-        metricsFormatter.add("key", 1L);
-        metricsFormatter.add("key", true);
-        String result = metricsFormatter.build();
+        String result = metricsFormatter.add("key", "value")
+                .add("key", 1)
+                .add("key", 1L)
+                .add("key", true)
+                .build();
         assertEquals("{\"key\":\"value\",\"key\":1,\"key\":1,\"key\":true}", result);
         assertThrows(IllegalStateException.class, () -> metricsFormatter.add("key", "value"));
         assertEquals("{\"key\":\"value\",\"key\":1,\"key\":1,\"key\":true}", metricsFormatter.build());
