a4f2540ef1f036c6d7e041a4287624f5f64d518c
Mohit Kataria <mkataria@apache.org>
2023-10-06 21:37:43 +0530

    remove unused import

--- oak-commons/src/main/java/org/apache/jackrabbit/oak/commons/sort/ExternalSort.java
+++ oak-commons/src/main/java/org/apache/jackrabbit/oak/commons/sort/ExternalSort.java
@@ -40,7 +40,6 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import static java.util.function.Function.identity;
 
