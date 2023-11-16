edd58781cae622394bdcc6ea02de738abf3edeb1
Mohit Kataria <mkataria@apache.org>
2023-11-16 09:48:34 +0530

    OAK-10533: updated warn messages

--- oak-run-commons/src/main/java/org/apache/jackrabbit/oak/index/indexer/document/incrementalstore/MergeIncrementalFlatFileStore.java
+++ oak-run-commons/src/main/java/org/apache/jackrabbit/oak/index/indexer/document/incrementalstore/MergeIncrementalFlatFileStore.java
@@ -96,9 +96,12 @@ public class MergeIncrementalFlatFileStore implements MergeIncrementalStore {
         }
     }
 
-    /* this method is a little verbose but I think this is fine as we are not getting consistent
-     data from checkpoint diff and we need to handle cases differently.
-
+    /**
+     * Merges multiple index store files.
+     *
+     * This method is a little verbose, but I think this is fine
+     * as we are not getting consistent data from checkpoint diff
+     * and we need to handle cases differently.
      */
     private void mergeIndexStoreFiles() throws IOException {
         Map<String, IncrementalStoreOperand> enumMap = Arrays.stream(IncrementalStoreOperand.values())
@@ -129,7 +132,8 @@ public class MergeIncrementalFlatFileStore implements MergeIncrementalStore {
                         String operand = getOperand(incrementalFFSParts);
                         switch (enumMap.get(operand)) {
                             case ADD:
-                                log.warn("Expected operand {} or {} but got {} for incremental line {}. Merging will proceed as usual, but this needs to be looked into.",
+                                log.warn("Expected operand {} or {} but got {} for incremental line {}. " +
+                                                "Merging will proceed, but this is unexpected.",
                                         IncrementalStoreOperand.MODIFY, IncrementalStoreOperand.DELETE, getOperand(incrementalFFSParts), incrementalFFSLine);
                                 incrementalFFSLine = writeAndAdvance(writer, incrementalFFSBufferedReader,
                                         getFFSLineFromIncrementalFFSParts(incrementalFFSParts));
@@ -171,8 +175,9 @@ public class MergeIncrementalFlatFileStore implements MergeIncrementalStore {
                 // this case should not happen. But in case this happens we consider modify as addition of node
                 // this implies node is not present in older FFS and in checkpointdiff this came as modified instead of
                 // node addition.
-                log.warn("Expected operand {} but got {} for incremental line {}. Merging will proceed as usual, but this needs to be looked into.",
-                        IncrementalStoreOperand.ADD, getOperand(incrementalFFSParts), incrementalFFSLine);
+                log.warn("Expected operand {} but got {} for incremental line {}. " +
+                                "Merging will proceed, but this is unexpected.",
+                        IncrementalStoreOperand.ADD, operand, incrementalFFSLine);
                 incrementalFFSLine = writeAndAdvance(writer, incrementalFFSBufferedReader,
                         getFFSLineFromIncrementalFFSParts(incrementalFFSParts));
                 break;
@@ -181,11 +186,11 @@ public class MergeIncrementalFlatFileStore implements MergeIncrementalStore {
                 // but this node came as deletion of node for an already non-existing node.
                 // we just skip this node in this case.
                 log.warn("Expected operand {} but got {} for incremental line {}. Merging will proceed as usual, but this needs to be looked into.",
-                        IncrementalStoreOperand.ADD, getOperand(incrementalFFSParts), incrementalFFSLine);
+                        IncrementalStoreOperand.ADD, operand, incrementalFFSLine);
                 incrementalFFSLine = incrementalFFSBufferedReader.readLine();
                 break;
             default:
-                log.error("wrong operand in incremental ffs: operand:{}, line:{}", operand, incrementalFFSLine);
+                log.error("Wrong operand in incremental ffs: operand:{}, line:{}", operand, incrementalFFSLine);
                 throw new RuntimeException("wrong operand in incremental ffs: operand:" + operand + ", line:" + incrementalFFSLine);
         }
         return incrementalFFSLine;
