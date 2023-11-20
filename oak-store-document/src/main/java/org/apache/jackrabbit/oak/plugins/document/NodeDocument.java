84b9c5999a712b297b12e7ab51d6061d224109a8
stefan-egli <stefanegli@apache.org>
2023-11-20 16:52:50 +0100

    OAK-10542 : getLiveRevision must inspect split docs if local deleted … (#1198)
    * OAK-10542 : getLiveRevision must inspect split docs if local deleted does not contain most recent committed revision
    
    * OAK-10542 : TODO added about refactoring possibility
    
    * OAK-10542 : refactor code duplication
    
    * OAK-10542 : unignore the tests as OAK-10542 was merged in the meantime
    
    * OAK-10542 : method renamed for usability

--- oak-store-document/src/main/java/org/apache/jackrabbit/oak/plugins/document/NodeDocument.java
+++ oak-store-document/src/main/java/org/apache/jackrabbit/oak/plugins/document/NodeDocument.java
@@ -971,20 +971,7 @@ public final class NodeDocument extends Document {
                     readRevision, validRevisions, lastRevs);
 
             // check if there may be more recent values in a previous document
-            if (value != null
-                    && !getPreviousRanges().isEmpty()
-                    && !isMostRecentCommitted(local, value.revision, nodeStore)) {
-                // not reading the most recent value, we may need to
-                // consider previous documents as well
-                for (Revision prev : getPreviousRanges().keySet()) {
-                    if (prev.compareRevisionTimeThenClusterId(value.revision) > 0) {
-                        // a previous document has more recent changes
-                        // than value.revision
-                        value = null;
-                        break;
-                    }
-                }
-            }
+            value = requiresCompleteMapCheck(value, local, nodeStore) ? null : value;
 
             if (value == null && !getPreviousRanges().isEmpty()) {
                 // check revision history
@@ -1067,8 +1054,11 @@ public final class NodeDocument extends Document {
                                     RevisionVector readRevision,
                                     Map<Revision, String> validRevisions,
                                     LastRevs lastRevs) {
+        final SortedMap<Revision, String> local = getLocalDeleted();
         // check local deleted map first
-        Value value = getLatestValue(context, getLocalDeleted().entrySet(), readRevision, validRevisions, lastRevs);
+        Value value = getLatestValue(context, local.entrySet(), readRevision, validRevisions, lastRevs);
+        // check if there may be more recent values in a previous document
+        value = requiresCompleteMapCheck(value, local, context) ? null : value;
         if (value == null && !getPreviousRanges().isEmpty()) {
             // need to check complete map
             value = getLatestValue(context, getDeleted().entrySet(), readRevision, validRevisions, lastRevs);
@@ -2179,6 +2169,34 @@ public final class NodeDocument extends Document {
             }
         }
         return value;
+    }
+
+    /**
+     * Check if there may be more recent values in a previous document and thus a
+     * complete map check is required.
+     *
+     * @param localValue value as resolved from local value map
+     * @param local      local value map
+     * @param context    the revision context
+     * @return false if it is most recent, true otherwise
+     */
+    private boolean requiresCompleteMapCheck(@Nullable Value localValue,
+            @NotNull SortedMap<Revision, String> local,
+            @NotNull RevisionContext context) {
+        if (localValue != null
+                && !getPreviousRanges().isEmpty()
+                && !isMostRecentCommitted(local, localValue.revision, context)) {
+            // not reading the most recent value, we may need to
+            // consider previous documents as well
+            for (Revision prev : getPreviousRanges().keySet()) {
+                if (prev.compareRevisionTimeThenClusterId(localValue.revision) > 0) {
+                    // a previous document has more recent changes
+                    // than localValue.revision
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     /**
