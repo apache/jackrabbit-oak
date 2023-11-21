32712e75567fdf68719b3486a8c1b6da7391c66d
Fabrizio Fortino <fabrizio.fortino@gmail.com>
2023-11-21 10:42:59 +0100

    OAK-10558: disable flaky ElasticReliabilityTest

--- oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elastic/ElasticReliabilityTest.java
+++ oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elastic/ElasticReliabilityTest.java
@@ -22,6 +22,7 @@ import eu.rekawek.toxiproxy.model.ToxicDirection;
 import eu.rekawek.toxiproxy.model.toxic.LimitData;
 import org.apache.jackrabbit.oak.api.Tree;
 import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.ProvideSystemProperty;
@@ -85,6 +86,7 @@ public class ElasticReliabilityTest extends ElasticAbstractQueryTest {
     }
 
     @Test
+    @Ignore("OAK-10558")
     public void connectionCutOnQuery() throws Exception {
         setIndex("test1", createIndex("propa", "propb"));
 
