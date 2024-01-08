7c6066a974131330d65dfd71789c5d6c2a4aa2a1
Nuno Santos <nsantos@adobe.com>
2024-01-08 14:30:24 +0100

    OAK-10589 - Improve regex path filtering to also handle cases where excludedPaths are defined (#1258)

---
+++ oak-run-commons/src/main/java/org/apache/jackrabbit/oak/index/indexer/document/flatfile/pipelined/MongoRegexPathFilterFactory.java
@@ -0,0 +1,149 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.jackrabbit.oak.index.indexer.document.flatfile.pipelined;
+
+import org.apache.jackrabbit.oak.commons.PathUtils;
+import org.apache.jackrabbit.oak.spi.filter.PathFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.TreeSet;
+
+public class MongoRegexPathFilterFactory {
+
+    public static class MongoFilterPaths {
+        // Special value that means "download everything". This is used when regex path filtering is disabled or when
+        // the path filters would require downloading everything. When this is returned, it is preferable to do not
+        // use any filter in the Mongo query, even though using the values in this object as filters would also result
+        // in downloading everything
+        public static final MongoFilterPaths DOWNLOAD_ALL = new MongoFilterPaths(List.of("/"), List.of());
+
+        final List<String> included;
+        final List<String> excluded;
+
+        public MongoFilterPaths(List<String> included, List<String> excluded) {
+            this.included = included;
+            this.excluded = excluded;
+        }
+
+        @Override
+        public String toString() {
+            return "MongoFilterPaths{" +
+                    "included=" + included +
+                    ", excluded=" + excluded +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            MongoFilterPaths that = (MongoFilterPaths) o;
+            return Objects.equals(included, that.included) && Objects.equals(excluded, that.excluded);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(included, excluded);
+        }
+    }
+
+    private final static Logger LOG = LoggerFactory.getLogger(MongoRegexPathFilterFactory.class);
+
+    private final int maxNumberOfPaths;
+
+    public MongoRegexPathFilterFactory(int maxNumberOfPaths) {
+        this.maxNumberOfPaths = maxNumberOfPaths;
+    }
+
+    public MongoFilterPaths buildMongoFilter(List<PathFilter> pathFilters) {
+        if (pathFilters == null || pathFilters.isEmpty()) {
+            return MongoFilterPaths.DOWNLOAD_ALL;
+        }
+        // Merge and deduplicate the included and excluded paths of all filters.
+        // The included paths will also be further de-deuplicated, by removing paths that are children of other path in
+        // the list, that is, by keeping only the top level paths. To make it easy to de-duplicate, we sort the paths
+        // alphabetically.
+        // The list of excluded paths does not need to be sorted, but we do it anyway for consistency when logging the
+        // list of paths.
+        TreeSet<String> sortedCandidateIncludedPaths = new TreeSet<>();
+        TreeSet<String> candidateExcludedPaths = new TreeSet<>();
+        for (PathFilter pathFilter : pathFilters) {
+            sortedCandidateIncludedPaths.addAll(pathFilter.getIncludedPaths());
+            candidateExcludedPaths.addAll(pathFilter.getExcludedPaths());
+        }
+        // Keep only unique included paths. That is, if paths "/a/b" and "/a/b/c" are both in the list, keep only "/a/b"
+        List<String> finalIncludedPathsRoots = new ArrayList<>();
+        LOG.debug("Candidate included paths: {}, candidate excluded paths: {}", sortedCandidateIncludedPaths, candidateExcludedPaths);
+        // The quadratic algorithm below could easily be replaced by linear time algorithm, but it's not worth the added
+        // complexity because the list of paths should never be more than a few tens
+        // Idea for linear time algorithm: after adding a path to the includedPathsRoots, advance in the list of candidates
+        // skipping all paths that are children of the path just added. Since the list is sorted alphabetically, the children
+        // of a path will be next to it in the list
+        for (String candidateIncludedPath : sortedCandidateIncludedPaths) {
+            if (finalIncludedPathsRoots.stream().noneMatch(ancestor -> PathUtils.isAncestor(ancestor, candidateIncludedPath))) {
+                finalIncludedPathsRoots.add(candidateIncludedPath);
+            }
+        }
+
+        List<String> finalExcludedPaths = new ArrayList<>();
+        // Keep an excluded path only if it is not needed by any filter.
+        for (String candidateExcludedPath : candidateExcludedPaths) {
+            boolean notNeeded = pathFilters.stream().allMatch(pathFilter -> pathFilter.filter(candidateExcludedPath) == PathFilter.Result.EXCLUDE);
+            if (notNeeded) {
+                finalExcludedPaths.add(candidateExcludedPath);
+            }
+        }
+
+        // Do not filter if there are too many includedPaths. The official Mongo documentation recommends
+        // not using more than tens of values in the $in filter to avoid performance degradation.
+        // https://www.mongodb.com/docs/manual/reference/operator/query/in/
+        if (finalIncludedPathsRoots.size() > maxNumberOfPaths) {
+            LOG.info("Number of includedPaths ({}) exceed the maximum allowed ({}), downloading everything",
+                    finalIncludedPathsRoots.size(), maxNumberOfPaths);
+            return MongoFilterPaths.DOWNLOAD_ALL;
+        }
+
+        // Also check the number of excluded paths and disable filtering by them if there are too many.
+        // We do not need to revert to download the full repository in the case where there is a reasonable number of
+        // included paths but too many excluded paths. The excluded paths are always children of the included paths, so
+        // if we do not filter on excludedPaths, we simply download everything inside the included paths. This might be
+        // much less than the full repository.
+        if (finalExcludedPaths.size() > maxNumberOfPaths) {
+            LOG.info("Number of excludedPaths ({}) exceed the maximum allowed ({}), disabling filtering of excludedPaths",
+                    finalExcludedPaths.size(), maxNumberOfPaths);
+            finalExcludedPaths = List.of();
+        }
+
+        if (finalExcludedPaths.isEmpty()) {
+            // Only included paths
+            if (finalIncludedPathsRoots.contains("/")) {
+                return MongoFilterPaths.DOWNLOAD_ALL;
+            } else {
+                return new MongoFilterPaths(finalIncludedPathsRoots, List.of());
+            }
+        } else {
+            return new MongoFilterPaths(finalIncludedPathsRoots, finalExcludedPaths);
+        }
+    }
+
+}
--- oak-run-commons/src/test/java/org/apache/jackrabbit/oak/index/indexer/document/flatfile/pipelined/PipelinedMongoDownloadTaskTest.java
+++ oak-run-commons/src/test/java/org/apache/jackrabbit/oak/index/indexer/document/flatfile/pipelined/PipelinedMongoDownloadTaskTest.java
@@ -24,6 +24,7 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import org.apache.jackrabbit.oak.index.indexer.document.flatfile.pipelined.MongoRegexPathFilterFactory.MongoFilterPaths;
 import org.apache.jackrabbit.oak.plugins.document.Collection;
 import org.apache.jackrabbit.oak.plugins.document.DocumentStore;
 import org.apache.jackrabbit.oak.plugins.document.NodeDocument;
@@ -32,6 +33,7 @@ import org.apache.jackrabbit.oak.plugins.metric.MetricStatisticsProvider;
 import org.apache.jackrabbit.oak.spi.filter.PathFilter;
 import org.bson.BsonDocument;
 import org.bson.conversions.Bson;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -43,6 +45,7 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -53,12 +56,19 @@ import static org.mockito.Mockito.when;
 
 public class PipelinedMongoDownloadTaskTest {
 
+    private MongoRegexPathFilterFactory regexFilterBuilder;
+
     private NodeDocument newBasicDBObject(String id, long modified, DocumentStore docStore) {
         NodeDocument obj = Collection.NODES.newDocument(docStore);
         obj.put(NodeDocument.ID, "3:/content/dam/asset" + id);
         obj.put(NodeDocument.MODIFIED_IN_SECS, modified);
         obj.put(NodeDocumentCodec.SIZE_FIELD, 100);
         return obj;
+    }
+
+    @Before
+    public void setUp() {
+        this.regexFilterBuilder = new MongoRegexPathFilterFactory(PipelinedMongoDownloadTask.DEFAULT_OAK_INDEXER_PIPELINED_MONGO_REGEX_PATH_FILTERING_MAX_PATHS);
     }
 
     @Test
@@ -142,42 +152,139 @@ public class PipelinedMongoDownloadTaskTest {
 
     @Test
     public void ancestorsFilters() {
-        assertEquals(Set.of(), PipelinedMongoDownloadTask.getAncestors(Set.of()));
-        assertEquals(Set.of("/"), PipelinedMongoDownloadTask.getAncestors(Set.of("/")));
-        assertEquals(Set.of("/", "/a"), PipelinedMongoDownloadTask.getAncestors(Set.of("/a")));
-        assertEquals(Set.of("/", "/a", "/b", "/c", "/c/c1", "/c/c1/c2", "/c/c1/c2/c3", "/c/c1/c2/c4"),
-                PipelinedMongoDownloadTask.getAncestors(Set.of("/a", "/b", "/c/c1/c2/c3", "/c/c1/c2/c4"))
+        assertEquals(List.of(), PipelinedMongoDownloadTask.getAncestors(List.of()));
+        assertEquals(List.of("/"), PipelinedMongoDownloadTask.getAncestors(List.of("/")));
+        assertEquals(List.of("/", "/a"), PipelinedMongoDownloadTask.getAncestors(List.of("/a")));
+        assertEquals(List.of("/", "/a", "/b", "/c", "/c/c1", "/c/c1/c2", "/c/c1/c2/c3", "/c/c1/c2/c4"),
+                PipelinedMongoDownloadTask.getAncestors(List.of("/a", "/b", "/c/c1/c2/c3", "/c/c1/c2/c4"))
         );
     }
 
     @Test
-    public void regexFilters() {
-        assertEquals(Set.of(), PipelinedMongoDownloadTask.extractIncludedPaths(null));
+    public void regexFiltersIncludedPathsOnly() {
+        assertEquals(MongoFilterPaths.DOWNLOAD_ALL, regexFilterBuilder.buildMongoFilter(null));
 
-        assertEquals(Set.of(), PipelinedMongoDownloadTask.extractIncludedPaths(List.of()));
+        assertEquals(MongoFilterPaths.DOWNLOAD_ALL, regexFilterBuilder.buildMongoFilter(List.of()));
 
         List<PathFilter> singlePathFilter = List.of(
                 new PathFilter(List.of("/content/dam"), List.of())
         );
-        assertEquals(Set.of("/content/dam"), PipelinedMongoDownloadTask.extractIncludedPaths(singlePathFilter));
+        assertEquals(new MongoFilterPaths(List.of("/content/dam"), List.of()), regexFilterBuilder.buildMongoFilter(singlePathFilter));
 
         List<PathFilter> multipleIncludeFilters = createIncludedPathFilters("/content/dam", "/content/dam");
-        assertEquals(Set.of("/content/dam"), PipelinedMongoDownloadTask.extractIncludedPaths(multipleIncludeFilters));
+        assertEquals(new MongoFilterPaths(List.of("/content/dam"), List.of()), regexFilterBuilder.buildMongoFilter(multipleIncludeFilters));
 
-        List<PathFilter> includesRoot = List.of(
-                new PathFilter(List.of("/"), List.of())
-        );
-        assertEquals(Set.of(), PipelinedMongoDownloadTask.extractIncludedPaths(includesRoot));
+        List<PathFilter> includesRoot = createIncludedPathFilters("/", "/a/a1/a2");
+        assertEquals(MongoFilterPaths.DOWNLOAD_ALL, regexFilterBuilder.buildMongoFilter(includesRoot));
 
         List<PathFilter> multipleIncludeFiltersDifferent = createIncludedPathFilters("/a/a1", "/a/a1/a2");
-        assertEquals(Set.of("/a/a1"), PipelinedMongoDownloadTask.extractIncludedPaths(multipleIncludeFiltersDifferent));
+        assertEquals(new MongoFilterPaths(List.of("/a/a1"), List.of()), regexFilterBuilder.buildMongoFilter(multipleIncludeFiltersDifferent));
 
         List<PathFilter> multipleIncludeFiltersDifferent2 = createIncludedPathFilters("/a/a1/a2", "/a/a1", "/b", "/c", "/cc");
-        assertEquals(Set.of("/a/a1", "/b", "/c", "/cc"), PipelinedMongoDownloadTask.extractIncludedPaths(multipleIncludeFiltersDifferent2));
+        assertEquals(new MongoFilterPaths(List.of("/a/a1", "/b", "/c", "/cc"), List.of()), regexFilterBuilder.buildMongoFilter(multipleIncludeFiltersDifferent2));
+    }
 
-        List<PathFilter> withExcludeFilter = List.of(
-                new PathFilter(List.of("/"), List.of("/var"))
+    @Test
+    public void regexFiltersIncludedAndExcludedPaths() {
+        // Excludes is not empty
+
+        // Exclude paths is inside the included path tree
+        List<PathFilter> withExcludeFilter1 = List.of(
+                new PathFilter(List.of("/a"), List.of("/a/b"))
         );
-        assertEquals(Set.of(), PipelinedMongoDownloadTask.extractIncludedPaths(withExcludeFilter));
+        assertEquals(new MongoFilterPaths(List.of("/a"), List.of("/a/b")), regexFilterBuilder.buildMongoFilter(withExcludeFilter1));
+
+        // includedPath contains the root
+        List<PathFilter> withExcludeFilter2 = List.of(
+                new PathFilter(List.of("/"), List.of("/a"))
+        );
+        assertEquals(new MongoFilterPaths(List.of("/"), List.of("/a")), regexFilterBuilder.buildMongoFilter(withExcludeFilter2));
+
+        // One of the filters excludes a directory that is included by another filter. The path should still be included.
+        List<PathFilter> withExcludeFilter3_a = List.of(
+                new PathFilter(List.of("/"), List.of("/a")),
+                new PathFilter(List.of("/"), List.of())
+        );
+        assertEquals(MongoFilterPaths.DOWNLOAD_ALL, regexFilterBuilder.buildMongoFilter(withExcludeFilter3_a));
+
+        List<PathFilter> withExcludeFilter3_b = List.of(
+                new PathFilter(List.of("/a"), List.of("/a/a_excluded")),
+                new PathFilter(List.of("/a"), List.of())
+        );
+        assertEquals(new MongoFilterPaths(List.of("/a"), List.of()), regexFilterBuilder.buildMongoFilter(withExcludeFilter3_b));
+
+        List<PathFilter> withExcludeFilter4 = List.of(
+                new PathFilter(List.of("/"), List.of("/exc_a")),
+                new PathFilter(List.of("/"), List.of("/exc_b"))
+        );
+        assertEquals(MongoFilterPaths.DOWNLOAD_ALL, regexFilterBuilder.buildMongoFilter(withExcludeFilter4));
+
+        List<PathFilter> withExcludeFilter5 = List.of(
+                new PathFilter(List.of("/a"), List.of("/a/a_excluded")),
+                new PathFilter(List.of("/b"), List.of("/b/b_excluded"))
+        );
+        assertEquals(new MongoFilterPaths(List.of("/a", "/b"), List.of("/a/a_excluded", "/b/b_excluded")), regexFilterBuilder.buildMongoFilter(withExcludeFilter5));
+
+        List<PathFilter> withExcludeFilter6 = List.of(
+                new PathFilter(List.of("/"), List.of("/a", "/b", "/c")),
+                new PathFilter(List.of("/"), List.of("/b", "/c", "/d"))
+        );
+        assertEquals(new MongoFilterPaths(List.of("/"), List.of("/b", "/c")), regexFilterBuilder.buildMongoFilter(withExcludeFilter6));
+
+        List<PathFilter> withExcludeFilter7 = List.of(
+                new PathFilter(List.of("/a"), List.of("/a/b")),
+                new PathFilter(List.of("/a"), List.of("/a/b/c"))
+        );
+        assertEquals(new MongoFilterPaths(List.of("/"), List.of("/b", "/c")), regexFilterBuilder.buildMongoFilter(withExcludeFilter6));
+        assertEquals(new MongoFilterPaths(List.of("/a"), List.of("/a/b/c")), regexFilterBuilder.buildMongoFilter(withExcludeFilter7));
+
+        List<PathFilter> withExcludeFilter8 = List.of(
+                new PathFilter(
+                        List.of("/p1", "/p2", "/p3", "/p4", "/p5", "/p6", "/p7", "/p8", "/p9"),
+                        List.of("/p10", "/p5/p5_s1", "/p5/p5_s2/p5_s3", "/p11")
+                )
+        );
+        assertEquals(new MongoFilterPaths(
+                        List.of("/p1", "/p2", "/p3", "/p4", "/p5", "/p6", "/p7", "/p8", "/p9"),
+                        List.of("/p5/p5_s1", "/p5/p5_s2/p5_s3")),
+                regexFilterBuilder.buildMongoFilter(withExcludeFilter8));
+    }
+
+
+    @Test
+    public void regexFiltersLargeNumberOfIncludedPaths1() {
+        // Test a single path filter with many included paths.
+        var maximumPaths = IntStream.range(0, PipelinedMongoDownloadTask.DEFAULT_OAK_INDEXER_PIPELINED_MONGO_REGEX_PATH_FILTERING_MAX_PATHS)
+                .mapToObj(i -> "/p" + i)
+                .collect(Collectors.toList());
+        var pathFilter1 = List.of(new PathFilter(maximumPaths, List.of()));
+        assertEquals(new MongoFilterPaths(maximumPaths.stream().sorted().collect(Collectors.toList()), List.of()),
+                regexFilterBuilder.buildMongoFilter(pathFilter1));
+
+
+        var tooManyPaths = IntStream.range(0, PipelinedMongoDownloadTask.DEFAULT_OAK_INDEXER_PIPELINED_MONGO_REGEX_PATH_FILTERING_MAX_PATHS + 1)
+                .mapToObj(i -> "/p" + i)
+                .collect(Collectors.toList());
+        var pathFilter2 = List.of(new PathFilter(tooManyPaths, List.of()));
+        assertEquals(MongoFilterPaths.DOWNLOAD_ALL, regexFilterBuilder.buildMongoFilter(pathFilter2));
+    }
+
+    @Test
+    public void regexFiltersLargeNumberOfIncludedPaths2() {
+        // Test many path filters each with a single included paths.
+        var pathFilters = IntStream.range(0, PipelinedMongoDownloadTask.DEFAULT_OAK_INDEXER_PIPELINED_MONGO_REGEX_PATH_FILTERING_MAX_PATHS + 1)
+                .mapToObj(i -> new PathFilter(List.of("/p" + i), List.of()))
+                .collect(Collectors.toList());
+        assertEquals(MongoFilterPaths.DOWNLOAD_ALL, regexFilterBuilder.buildMongoFilter(pathFilters));
+    }
+
+    @Test
+    public void regexFiltersLargeNumberOfExcludedPaths() {
+        // When there are too many excluded paths, do not filter on excluded paths but still filter on included paths.
+        var excludedPaths = IntStream.range(0, PipelinedMongoDownloadTask.DEFAULT_OAK_INDEXER_PIPELINED_MONGO_REGEX_PATH_FILTERING_MAX_PATHS + 1)
+                .mapToObj(i -> "/parent/p" + i)
+                .collect(Collectors.toList());
+        var pathFilter = List.of(new PathFilter(List.of("/parent"), excludedPaths));
+        assertEquals(new MongoFilterPaths(List.of("/parent"), List.of()), regexFilterBuilder.buildMongoFilter(pathFilter));
     }
 }
