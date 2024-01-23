37276ab17de9dc4c7f4b99d715502e23311a5d03
Fabrizio Fortino <fabrizio.fortino@gmail.com>
2024-01-23 15:44:28 +0100

    OAK-10599: complete support for IndexAggregationCommonTest

--- oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elastic/ElasticIndexAggregationTest.java
+++ oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elastic/ElasticIndexAggregationTest.java
@@ -1,16 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.jackrabbit.oak.plugins.index.elastic;
 
+import org.apache.jackrabbit.oak.api.CommitFailedException;
 import org.apache.jackrabbit.oak.api.ContentRepository;
 import org.apache.jackrabbit.oak.plugins.index.IndexAggregationCommonTest;
-import org.apache.jackrabbit.oak.plugins.index.aggregate.SimpleNodeAggregator;
-import org.apache.jackrabbit.oak.spi.query.QueryIndex;
 import org.junit.ClassRule;
-
-import java.util.List;
-
-import static org.apache.jackrabbit.JcrConstants.JCR_CONTENT;
-import static org.apache.jackrabbit.JcrConstants.NT_FILE;
-import static org.apache.jackrabbit.JcrConstants.NT_FOLDER;
+import org.junit.Ignore;
+import org.junit.Test;
 
 public class ElasticIndexAggregationTest extends IndexAggregationCommonTest {
 
@@ -28,18 +39,10 @@ public class ElasticIndexAggregationTest extends IndexAggregationCommonTest {
         return repositoryOptionsUtil.getOak().createContentRepository();
     }
 
-    /**
-     * <code>
-     * <aggregate primaryType="nt:file">
-     * <include>jcr:content</include>
-     * <include>jcr:content/*</include>
-     * <include-property>jcr:content/jcr:lastModified</include-property>
-     * </aggregate>
-     * <code>
-     */
-    private static QueryIndex.NodeAggregator getNodeAggregator() {
-        return new SimpleNodeAggregator()
-                .newRuleWithName(NT_FILE, List.of(JCR_CONTENT, JCR_CONTENT + "/*"))
-                .newRuleWithName(NT_FOLDER, List.of("myFile", "subfolder/subsubfolder/file"));
+    @Override
+    @Test
+    @Ignore("OAK-10599: Elasticsearch does not support compatibility mode")
+    public void oak3371AggregateV1() throws CommitFailedException {
+        super.oak3371AggregateV1();
     }
 }
