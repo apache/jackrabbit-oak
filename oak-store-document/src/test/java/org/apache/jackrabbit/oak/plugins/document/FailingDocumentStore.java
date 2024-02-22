65ff438b04ce3e4509d980707551c2ad84c997a6
Stefan Egli <stefanegli@apache.org>
2024-02-22 15:12:47 +0100

    OAK-10659 : remove orphaned nodes/documents

--- oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/FailingDocumentStore.java
+++ oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/FailingDocumentStore.java
@@ -55,6 +55,8 @@ class FailingDocumentStore extends DocumentStoreWrapper {
 
     private boolean afterOp = false;
 
+    private boolean noDispose = false;
+
     class Fail {
 
         private Fail() {
@@ -309,4 +311,16 @@ class FailingDocumentStore extends DocumentStoreWrapper {
     private void reportRemainingOps(List<UpdateOp> remainingOps) {
         listeners.forEach(listener -> remainingOps.forEach(listener::failed));
     }
+
+    public void noDispose() {
+        noDispose = true;
+    }
+
+    @Override
+    public void dispose() {
+        if (!noDispose) {
+            super.dispose();
+        }
+    }
+
 }
