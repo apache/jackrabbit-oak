17673a661851aa3ebf444cc59969b6308873a84c
angela <anchela@adobe.com>
2024-06-12 12:46:09 +0200

    OAK-10451 : UserPrincipalProvider may cause many conflicts when under load (add missing licence header)

--- oak-core/src/test/java/org/apache/jackrabbit/oak/security/user/CachedPrincipalMembershipReaderTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/security/user/CachedPrincipalMembershipReaderTest.java
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.apache.jackrabbit.oak.security.user;
 
 import org.apache.jackrabbit.JcrConstants;
