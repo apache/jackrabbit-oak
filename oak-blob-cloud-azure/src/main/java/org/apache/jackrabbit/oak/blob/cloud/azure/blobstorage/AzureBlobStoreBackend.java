212836774f4c0de6fcf5f4073d4a722af2fc8791
Thomas Mueller <thomasm@apache.org>
2024-07-30 11:31:06 +0200

    Revert "OAK-10685: remove unused import of java.io.UnsupportedEncodingException"
    This reverts commit 36cfcfa08e66ef2aecc5cec3c08e6dd08db9038e.

--- oak-blob-cloud-azure/src/main/java/org/apache/jackrabbit/oak/blob/cloud/azure/blobstorage/AzureBlobStoreBackend.java
+++ oak-blob-cloud-azure/src/main/java/org/apache/jackrabbit/oak/blob/cloud/azure/blobstorage/AzureBlobStoreBackend.java
@@ -29,6 +29,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
