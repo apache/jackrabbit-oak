d49014d51868068552ab515eb229094cc29c4645
Thomas Mueller <thomasm@apache.org>
2024-07-30 11:30:14 +0200

    Revert "OAK-10965 - Make ConsoleIndexingReporter thread safe. (#1592)"
    This reverts commit 8b05caefb94252c49b62eb2499a705f3d4dd09f8.

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/ConsoleIndexingReporter.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/ConsoleIndexingReporter.java
@@ -26,19 +26,20 @@ import org.jetbrains.annotations.NotNull;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ConsoleIndexingReporter implements IndexingReporter {
+    // Print configuration in alphabetical order
+    private final Map<String, String> config = new TreeMap<>();
     // Print timings in the order they were added
     private final Map<String, String> timings = new LinkedHashMap<>();
-    // Print metrics and configuration in alphabetical order. They are sorted when printed.
-    private final Map<String, String> metrics = new HashMap<>();
-    private final Map<String, String> config = new HashMap<>();
+    // Print configuration in alphabetical order
+    private final Map<String, String> metrics = new TreeMap<>();
     private final List<String> envVariablesToLog;
     private List<String> indexes = List.of();
     private final List<String> informationStrings = new ArrayList<>();
@@ -54,24 +55,24 @@ public class ConsoleIndexingReporter implements IndexingReporter {
         this.envVariablesToLog = List.copyOf(envVariablesToLog);
     }
 
-    public synchronized void setIndexNames(@NotNull List<String> indexes) {
+    public void setIndexNames(@NotNull List<String> indexes) {
         this.indexes = List.copyOf(indexes);
     }
 
-    public synchronized void addConfig(String key, Object value) {
+    public void addConfig(String key, Object value) {
         config.put(key, value.toString());
     }
 
-    public synchronized void addTiming(String stage, String time) {
+    public void addTiming(String stage, String time) {
         timings.put(stage, time);
     }
 
-    public synchronized void addMetric(String name, long value) {
+    public void addMetric(String name, long value) {
         metrics.put(name, String.valueOf(value));
     }
 
     @Override
-    public synchronized void addInformation(String value) {
+    public void addInformation(String value) {
         informationStrings.add(value);
     }
 
@@ -80,12 +81,10 @@ public class ConsoleIndexingReporter implements IndexingReporter {
         if (value >= FileUtils.ONE_KB) {
             v += " (" + IOUtils.humanReadableByteCountBin(value) + ")";
         }
-        synchronized (this) {
-            metrics.put(name, v);
-        }
+        metrics.put(name, v);
     }
 
-    public synchronized String generateReport() {
+    public String generateReport() {
         return "Indexes: " + String.join(", ", indexes) + "\n" +
                 "Date: " + DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(ZonedDateTime.now()) + "\n" +
                 "OAK Version: " + OakVersion.getVersion() + "\n" +
