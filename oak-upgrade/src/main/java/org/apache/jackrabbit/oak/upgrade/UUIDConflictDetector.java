9ea2fd9f0b5a7607a3bbd77086564857b1812ea1
t-rana <tusharrana97@gmail.com>
2024-09-16 16:46:55 +0530

    multiple sort files fix

--- oak-upgrade/src/main/java/org/apache/jackrabbit/oak/upgrade/UUIDConflictDetector.java
+++ oak-upgrade/src/main/java/org/apache/jackrabbit/oak/upgrade/UUIDConflictDetector.java
@@ -14,6 +14,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
 import java.util.Arrays;
@@ -72,14 +73,19 @@ public class UUIDConflictDetector {
     }
 
     private File getSourceFileForPaths(Set<String> includePaths) throws IOException {
+        long startTime = System.currentTimeMillis();
+        log.info("starting fetching uuid nodes from source repository at: {}", startTime);
         File sourceFile = new File(dir, "source_uuids_" + getTimeStamp() + ".txt");
+        log.info("source file: {}", sourceFile.getName());
+        sourceFile.deleteOnExit();
         try (BufferedWriter writer = Files.newBufferedWriter(sourceFile.toPath())) {
             for (String path : includePaths) {
                 NodeState state = getNodeAtPath(sourceStore.getRoot(), path);
                 gatherUUIDs(state, path, writer);
             }
         }
-        return sortFile(sourceFile);
+        log.info("fetching uuid nodes completed from source repository in: {} ms", System.currentTimeMillis() - startTime);
+        return sortFile(sourceFile, "source");
     }
 
     private NodeState getNodeAtPath(NodeState node, String path) {
@@ -118,16 +124,33 @@ public class UUIDConflictDetector {
 
 
     private File gatherUUIDs(NodeState state, String prefix) throws IOException {
+        long startTime = System.currentTimeMillis();
+        log.info("starting fetching uuid nodes from {} repository at: {}", prefix, startTime);
         File file = new File(dir, prefix + "_uuids_" + getTimeStamp() + ".txt");
+        log.info("{} uuid file: {}", prefix, file.getName());
+        file.deleteOnExit();
         try (BufferedWriter writer = Files.newBufferedWriter(file.toPath())) {
             gatherUUIDs(state, "", writer);
         }
-        return sortFile(file);
+        log.info("fetching uuid nodes completed from {} repository in: {} ms", prefix, System.currentTimeMillis() - startTime);
+        return sortFile(file, prefix);
     }
 
-    private File sortFile(File file) throws IOException {
+    private File sortFile(File file, String prefix) throws IOException {
+        long startTime = System.currentTimeMillis();
+        log.info("sorting {} started at: {}", file.getName(), startTime);
         List<File> sortedFiles = ExternalSort.sortInBatch(file, Comparator.naturalOrder());
-        return sortedFiles.get(0);
+        log.info("sorting {} file completed in: {} ms", file.getName(), System.currentTimeMillis() - startTime);
+        File sortedFile = new File(dir, prefix + "_uuids_" + getTimeStamp() + ".txt");
+        log.info("sorted {} uuid file: {}", prefix, sortedFile.getName());
+
+        long mergeFileStartTime = System.currentTimeMillis();
+        log.info("merging sorted {} files started at: {} ms", prefix, mergeFileStartTime);
+        // Merge the sorted temporary files into the sortedFile
+        ExternalSort.mergeSortedFiles(sortedFiles, sortedFile);
+
+        log.info("merging sorted {} files completed in: {} ms", prefix, System.currentTimeMillis() - mergeFileStartTime);
+        return sortedFile;
     }
 
     private void gatherUUIDs(NodeState state, String path, BufferedWriter writer) throws IOException {
@@ -143,9 +166,13 @@ public class UUIDConflictDetector {
     }
 
     private void compareUUIDs(File sourceFile, File targetFile) throws IOException {
+        long startTime = System.currentTimeMillis();
+        log.info("started uuid conflict comparison in {}, {} files at: {}", sourceFile.getName(), targetFile.getName(), startTime);
+        Path uuidConflictFilePath = Paths.get(dir.getAbsolutePath(), "uuid_conflicts_" + getTimeStamp() + ".txt");
+        log.info("uuid conflict file: {}", uuidConflictFilePath.getFileName());
         try (BufferedReader sourceReader = Files.newBufferedReader(sourceFile.toPath());
              BufferedReader targetReader = Files.newBufferedReader(targetFile.toPath());
-             BufferedWriter conflictWriter = Files.newBufferedWriter(Paths.get(dir.getAbsolutePath(), "uuid_conflicts_" + getTimeStamp() + ".txt"))) {
+             BufferedWriter conflictWriter = Files.newBufferedWriter(uuidConflictFilePath)) {
 
             String sourceLine = sourceReader.readLine();
             String targetLine = targetReader.readLine();
@@ -175,6 +202,7 @@ public class UUIDConflictDetector {
                 }
             }
         }
+        log.info("uuid conflict comparison in {}, {} files completed in: {} ms", sourceFile.getName(), targetFile.getName(), System.currentTimeMillis() - startTime);
     }
 
     public long getTimeStamp() {
