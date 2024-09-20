68000c1faba7071eca362a112bb6851ea26d1bbb
Julian Reschke <reschke@apache.org>
2024-09-20 17:33:44 +0100

    OAK-11100: remove use of Guava transform/filter - oak-segment-tar

--- oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/upgrade/UpgradeIT.java
+++ oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/upgrade/UpgradeIT.java
@@ -15,10 +15,8 @@
  * limitations under the License.
  *
  */
-
 package org.apache.jackrabbit.oak.segment.upgrade;
 
-import static org.apache.jackrabbit.guava.common.collect.Iterables.transform;
 import static java.lang.String.format;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static org.apache.jackrabbit.oak.segment.SegmentVersion.V_12;
@@ -36,6 +34,7 @@ import java.io.IOException;
 
 import org.apache.commons.lang3.JavaVersion;
 import org.apache.commons.lang3.SystemUtils;
+import org.apache.jackrabbit.oak.commons.collections.CollectionUtils;
 import org.apache.jackrabbit.oak.segment.SegmentVersion;
 import org.apache.jackrabbit.oak.segment.data.SegmentData;
 import org.apache.jackrabbit.oak.segment.file.InvalidFileStoreVersionException;
@@ -151,10 +150,8 @@ public class UpgradeIT {
     }
 
     private static Iterable<SegmentData> getSegments(@NotNull TarFiles tarFiles) {
-        return transform(
-                tarFiles.getSegmentIds(),
-                uuid -> newSegmentData(tarFiles.readSegment(
-                    uuid.getMostSignificantBits(),
-                    uuid.getLeastSignificantBits())));
+        return () -> CollectionUtils.toStream(tarFiles.getSegmentIds())
+                .map(uuid -> newSegmentData(tarFiles.readSegment(uuid.getMostSignificantBits(), uuid.getLeastSignificantBits())))
+                .iterator();
     }
 }
