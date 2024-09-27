e8071b15b58c8736e6f7a5a8dbe8b33e9cb6390e
pirlogea <pirlogea@adobe.com>
2024-09-27 09:46:25 +0300

    OAK-11113 -- MissingLastRevSeekerTest fix flaky test

--- oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/CompressedDocumentPropertyStateTest.java
+++ oak-store-document/src/test/java/org/apache/jackrabbit/oak/plugins/document/CompressedDocumentPropertyStateTest.java
@@ -238,6 +238,11 @@ public class CompressedDocumentPropertyStateTest {
             if (nodeStore != null) {
                 nodeStore.dispose();
             }
+            try {
+                fixture.dispose();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
 
     }
