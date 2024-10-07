d469427f9c1ef97efb8d905a8e2a6a8fed444cdb
Julian Reschke <reschke@apache.org>
2024-10-07 16:30:27 +0100

    OAK-11169: MongoVersionGCSupport (oak-document-store) uses incorrect syntax for sort mongodb function (WIP)

--- oak-store-document/src/main/java/org/apache/jackrabbit/oak/plugins/document/mongo/MongoVersionGCSupport.java
+++ oak-store-document/src/main/java/org/apache/jackrabbit/oak/plugins/document/mongo/MongoVersionGCSupport.java
@@ -23,6 +23,7 @@ import static com.mongodb.client.model.Filters.exists;
 import static com.mongodb.client.model.Filters.gt;
 import static com.mongodb.client.model.Filters.or;
 import static com.mongodb.client.model.Projections.include;
+import static com.mongodb.client.model.Sorts.ascending;
 import static java.util.Optional.empty;
 import static java.util.Optional.ofNullable;
 import static org.apache.jackrabbit.guava.common.collect.Iterables.concat;
@@ -373,19 +374,19 @@ public class MongoVersionGCSupport extends VersionGCSupport {
      */
     @Override
     public Optional<NodeDocument> getOldestModifiedDoc(final Clock clock) {
-        final Bson sort = and(eq(MODIFIED_IN_SECS, 1), eq(ID, 1));
-
         // we need to add query condition to ignore `previous` documents which doesn't have this field
         final Bson query = exists(MODIFIED_IN_SECS);
+        // sort by MODIFIED_IN_SECS first, ID otherwise
+        final Bson sort = ascending(MODIFIED_IN_SECS, ID);
 
         FindIterable<BasicDBObject> limit = getNodeCollection().find(query).sort(sort).limit(1);
 
-        try(MongoCursor<BasicDBObject> cur = limit.iterator()) {
+        try (MongoCursor<BasicDBObject> cur = limit.iterator()) {
             return cur.hasNext() ? ofNullable(store.convertFromDBObject(NODES, cur.next())) : empty();
         } catch (Exception ex) {
             LOG.error("getOldestModifiedDoc() <- error while fetching data from Mongo", ex);
         }
-        LOG.info("No Modified Doc has been found, retuning empty");
+        LOG.info("No Modified Doc has been found, returning empty");
         return empty();
     }
 
