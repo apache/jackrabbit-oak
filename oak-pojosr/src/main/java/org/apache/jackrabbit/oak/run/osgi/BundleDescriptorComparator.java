2f31e56d42bdfa105b13e7eea1306c366544a51a
mbaedke <manfred.baedke@gmail.com>
2024-12-18 14:27:25 +0100

    Removed whitespace change

--- oak-pojosr/src/main/java/org/apache/jackrabbit/oak/run/osgi/BundleDescriptorComparator.java
+++ oak-pojosr/src/main/java/org/apache/jackrabbit/oak/run/osgi/BundleDescriptorComparator.java
@@ -54,7 +54,6 @@ class BundleDescriptorComparator implements Comparator<BundleDescriptor> {
         defaultLevels.put("org.apache.felix.fileinstall", 2);
 
         defaultLevels.put("org.apache.felix.scr", 10);
-        
         return Collections.unmodifiableMap(defaultLevels);
     }
 
