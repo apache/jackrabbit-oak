8bced3d084dabed215eae55d39dc0e07a1db6157
mbaedke <manfred.baedke@gmail.com>
2024-12-18 13:09:20 +0100

    e BundleDescriptorComparator.java

--- oak-pojosr/src/main/java/org/apache/jackrabbit/oak/run/osgi/BundleDescriptorComparator.java
+++ oak-pojosr/src/main/java/org/apache/jackrabbit/oak/run/osgi/BundleDescriptorComparator.java
@@ -53,6 +53,8 @@ class BundleDescriptorComparator implements Comparator<BundleDescriptor> {
         defaultLevels.put("org.apache.felix.configadmin", 2);
         defaultLevels.put("org.apache.felix.fileinstall", 2);
 
+        defaultLevels.put("org.apache.felix.scr", 10);
+        
         return Collections.unmodifiableMap(defaultLevels);
     }
 
