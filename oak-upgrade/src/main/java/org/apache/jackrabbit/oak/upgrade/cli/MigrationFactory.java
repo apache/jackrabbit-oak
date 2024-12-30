b0eda2adca824d386993b36814ba0148765166ce
Julian Reschke <reschke@apache.org>
2024-12-30 12:05:49 +0100

    OAK-11330: Remove usage of Guava ImmutableList (Builder)

--- oak-upgrade/src/main/java/org/apache/jackrabbit/oak/upgrade/cli/MigrationFactory.java
+++ oak-upgrade/src/main/java/org/apache/jackrabbit/oak/upgrade/cli/MigrationFactory.java
@@ -17,13 +17,14 @@
 package org.apache.jackrabbit.oak.upgrade.cli;
 
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.Collections;
 import java.util.List;
 import java.util.ServiceLoader;
 
 import javax.jcr.RepositoryException;
 
 import org.apache.jackrabbit.core.RepositoryContext;
+import org.apache.jackrabbit.oak.commons.collections.CollectionUtils;
 import org.apache.jackrabbit.oak.plugins.blob.datastore.DataStoreBlobStore;
 import org.apache.jackrabbit.oak.spi.blob.BlobStore;
 import org.apache.jackrabbit.oak.spi.commit.CommitHook;
@@ -35,7 +36,6 @@ import org.apache.jackrabbit.oak.upgrade.cli.parser.DatastoreArguments;
 import org.apache.jackrabbit.oak.upgrade.cli.parser.MigrationOptions;
 import org.apache.jackrabbit.oak.upgrade.cli.parser.StoreArguments;
 
-import org.apache.jackrabbit.guava.common.collect.ImmutableList;
 import org.apache.jackrabbit.guava.common.io.Closer;
 
 public class MigrationFactory {
@@ -123,9 +123,7 @@ public class MigrationFactory {
 
     private List<CommitHook> loacCommitHooks() {
         ServiceLoader<CommitHook> loader = ServiceLoader.load(CommitHook.class);
-        Iterator<CommitHook> iterator = loader.iterator();
-        ImmutableList.Builder<CommitHook> builder = ImmutableList.<CommitHook> builder().addAll(iterator);
-        return builder.build();
+        return Collections.unmodifiableList(CollectionUtils.toList(loader.iterator()));
     }
 
 }
