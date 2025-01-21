4039d375741a524ed899edd8213f8a4ac7f3e08d
Julian Reschke <julian.reschke@gmx.de>
2025-01-21 19:48:02 +0100

    OAK-11419: oak-store-document - persistent cache: remove unused UDP broadcaster

--- oak-store-document/src/main/java/org/apache/jackrabbit/oak/plugins/document/persistentCache/PersistentCache.java
+++ oak-store-document/src/main/java/org/apache/jackrabbit/oak/plugins/document/persistentCache/PersistentCache.java
@@ -36,7 +36,6 @@ import org.apache.jackrabbit.oak.plugins.document.persistentCache.async.CacheAct
 import org.apache.jackrabbit.oak.plugins.document.persistentCache.broadcast.Broadcaster;
 import org.apache.jackrabbit.oak.plugins.document.persistentCache.broadcast.InMemoryBroadcaster;
 import org.apache.jackrabbit.oak.plugins.document.persistentCache.broadcast.TCPBroadcaster;
-import org.apache.jackrabbit.oak.plugins.document.persistentCache.broadcast.UDPBroadcaster;
 import org.apache.jackrabbit.oak.stats.StatisticsProvider;
 import org.h2.mvstore.FileStore;
 import org.h2.mvstore.MVMap;
@@ -228,9 +227,6 @@ public class PersistentCache implements Broadcaster.Listener {
             return;
         } else if (broadcast.equals("inMemory")) {
             broadcaster = InMemoryBroadcaster.INSTANCE;
-        } else if (broadcast.startsWith("udp:")) {
-            String config = broadcast.substring("udp:".length(), broadcast.length());
-            broadcaster = new UDPBroadcaster(config);
         } else if (broadcast.startsWith("tcp:")) {
             String config = broadcast.substring("tcp:".length(), broadcast.length());
             broadcaster = new TCPBroadcaster(config);
