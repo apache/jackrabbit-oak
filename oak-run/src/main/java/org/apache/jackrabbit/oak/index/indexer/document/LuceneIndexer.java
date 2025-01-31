8ce32a17ade82ef7cb2073147b069fb0a8786e69
Nuno Santos <nsantos@adobe.com>
2025-01-31 09:28:52 +0100

    Use a single DefaultIndexWriterFactory in the LuceneIndexEditorProvider

--- oak-run/src/main/java/org/apache/jackrabbit/oak/index/indexer/document/LuceneIndexer.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/index/indexer/document/LuceneIndexer.java
@@ -158,4 +158,9 @@ public class LuceneIndexer implements NodeStateIndexer, FacetsConfigProvider {
         return facetsConfig;
     }
 
+
+    @Override
+    public String toString() {
+        return definition.toString();
+    }
 }
