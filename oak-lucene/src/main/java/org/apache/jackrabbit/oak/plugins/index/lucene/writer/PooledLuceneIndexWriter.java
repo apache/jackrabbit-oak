131779e60f1b8bd22e4b6d622ff096d8a7acebbc
Nuno Santos <nsantos@adobe.com>
2025-02-25 16:59:26 +0100

    Reduce logging when closing Lucene index writers.

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/writer/PooledLuceneIndexWriter.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/writer/PooledLuceneIndexWriter.java
@@ -41,6 +41,7 @@ public class PooledLuceneIndexWriter implements LuceneIndexWriter {
         this.writerPool = writerPool;
         this.delegateWriter = delegateWriter;
         this.indexName = indexName;
+        LOG.info("Created writer for index {}", indexName, new Throwable());
     }
 
     @Override
@@ -57,7 +58,7 @@ public class PooledLuceneIndexWriter implements LuceneIndexWriter {
 
     @Override
     public boolean close(long timestamp) throws IOException {
-        LOG.info("[{}] Shutting down PipelinedFullTextIndexWriter", indexName);
+        LOG.info("[{}] Shutting down writer", indexName);
         return writerPool.closeWriter(delegateWriter, timestamp);
     }
 
