a185bdf7315951c037b0c1faf7e4ca71a316b268
Julian Reschke <julian.reschke@gmx.de>
2025-03-08 08:33:47 +0100

    OAK-11571: commons: add Closer class (similar to Guava Closer) - tests

--- oak-commons/src/test/java/org/apache/jackrabbit/oak/commons/io/CloserTest.java
+++ oak-commons/src/test/java/org/apache/jackrabbit/oak/commons/io/CloserTest.java
@@ -18,7 +18,7 @@
  */
 package org.apache.jackrabbit.oak.commons.io;
 
-import com.google.common.io.Closer;
+import org.apache.jackrabbit.guava.common.io.Closer;
 import org.junit.Test;
 
 import java.io.Closeable;
