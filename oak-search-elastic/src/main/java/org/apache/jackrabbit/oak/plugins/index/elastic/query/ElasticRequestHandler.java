f5a21487165ecc20d129d27511d3ae3acf05505b
Mohit Kataria <tihom88@gmail.com>
2025-05-08 22:34:52 +0530

    OAK-11694: change todo to TODO

--- oak-search-elastic/src/main/java/org/apache/jackrabbit/oak/plugins/index/elastic/query/ElasticRequestHandler.java
+++ oak-search-elastic/src/main/java/org/apache/jackrabbit/oak/plugins/index/elastic/query/ElasticRequestHandler.java
@@ -244,7 +244,7 @@ public class ElasticRequestHandler {
     }
 
     private String generateFieldsForMLT() {
-        //todo with addition of :enricher status for inference. All documents will now have :enricher for inference enabled indexes.
+        //TODO with addition of :enricher status for inference. All documents will now have :enricher for inference enabled indexes.
         // as as result mlt is now returning all documents.
         // find a better way so that these fields can be easily managed.
         List<String> keys = elasticIndexDefinition.getPropertiesByName().entrySet().stream().map(Map.Entry::getKey).collect(Collectors.toList());
