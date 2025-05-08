60ec98f7be44285503b57928cac2aeb9a21e4a7c
Mohit Kataria <mkataria@apache.org>
2025-05-08 15:36:16 +0530

    OAK-11692: Add inference config in oak (#2269)
    * OAK-11693: Parse inference query and extract out inference config and text query
    
    * OAK-11693: Parse inference query and extract out inference config and text query
    
    * OAK-11692: Add inference config in oak.
    
    * OAK-11692: Added enricher status in inferenceConfig
    
    * OAK-11692: incorporated review comments
    
    * OAK-11693: Added test case
    
    * OAK-11692: incorporated review comments

---
+++ oak-query-spi/src/main/java/org/apache/jackrabbit/oak/spi/query/fulltext/InferenceQueryConfig.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.jackrabbit.oak.spi.query.fulltext;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class InferenceQueryConfig {
+    public static final String TYPE = "inferenceModelConfig";
+    @Nullable
+    private final String inferenceModelConfig;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public InferenceQueryConfig(@NotNull String queryConfig) {
+        if (queryConfig.isBlank()){
+            this.inferenceModelConfig = null;
+        } else if (queryConfig.equals("{}")) {
+            // in this case a default inferenceModelConfig will be used.
+            this.inferenceModelConfig = "";
+        } else {
+            try {
+                JsonNode jsonNode1 = objectMapper.readTree(queryConfig);
+                inferenceModelConfig = jsonNode1.get(TYPE).asText();
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("Error parsing inference query config: "+ queryConfig  + "error message: " + e.getMessage());
+            }
+        }
+    }
+
+    public @Nullable String getInferenceModelConfig() {
+        return inferenceModelConfig;
+    }
+}
---
+++ oak-search-elastic/src/main/java/org/apache/jackrabbit/oak/plugins/index/elastic/query/inference/InferenceConstants.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.jackrabbit.oak.plugins.index.elastic.query.inference;
+
+public interface InferenceConstants {
+    String ENABLED = "enabled";
+    String ENRICHER_CONFIG = "enricherConfig";
+    String TYPE = "type";
+    String DEFAULT_OAK_INDEX_INFERENCE_CONFIG_PATH = "/oak:index/:inferenceConfig";
+    String VECTOR_SPACES = ":vectorSpaces";
+    String VECTOR = "vector";
+    String ENRICH_NODE = ":enrich";
+    String DEFAULT_ENVIRONMENT_VARIABLE_PREFIX = "$";
+    String INFERENCE_ENVIRONMENT_VARIABLE_PREFIX = System.getProperty("org.apache.jackrabbit.oak.plugins.index.elastic.query.inference", DEFAULT_ENVIRONMENT_VARIABLE_PREFIX);
+    String DEFAULT_ENVIRONMENT_VARIABLE_VALUE = "";
+
+}
