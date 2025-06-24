55f1e2d1df2ac0ff95fb7a5ad657c48582855d10
Manfred Baedke <manfred.baedke@gmail.com>
2025-06-24 13:19:11 +0200

    OAK-11617: Provide oak-run commands to analyze and fix inconsistencies in the namespace registry
    Fixed NamespaceRegistryCommandTest.

--- oak-run/src/test/java/org/apache/jackrabbit/oak/run/NamespaceRegistryCommandTest.java
+++ oak-run/src/test/java/org/apache/jackrabbit/oak/run/NamespaceRegistryCommandTest.java
@@ -16,13 +16,21 @@
  */
 package org.apache.jackrabbit.oak.run;
 
-import org.apache.jackrabbit.oak.Oak;
-import org.apache.jackrabbit.oak.api.Root;
-import org.apache.jackrabbit.oak.api.Tree;
+import org.apache.jackrabbit.JcrConstants;
+import org.apache.jackrabbit.oak.InitialContent;
+import org.apache.jackrabbit.oak.api.CommitFailedException;
+import org.apache.jackrabbit.oak.plugins.document.DocumentNodeStore;
 import org.apache.jackrabbit.oak.plugins.document.MongoUtils;
 import org.apache.jackrabbit.oak.plugins.name.Namespaces;
+import org.apache.jackrabbit.oak.run.cli.NodeStoreFixture;
 import org.apache.jackrabbit.oak.run.cli.NodeStoreFixtureProvider;
-import org.apache.jackrabbit.oak.spi.security.OpenSecurityProvider;
+import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
+import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
+import org.apache.jackrabbit.oak.spi.namespace.NamespaceConstants;
+import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -35,11 +43,25 @@ import static org.junit.Assume.assumeTrue;
 
 public class NamespaceRegistryCommandTest {
 
-    private final NamespaceRegistryCommand cmd = new NamespaceRegistryCommand();
+    private static final NamespaceRegistryCommand cmd = new NamespaceRegistryCommand();
+    private static DocumentNodeStore store;
 
     @BeforeClass
-    public static void assumeMongoDB() {
+    public static void before() throws CommitFailedException {
         assumeTrue(MongoUtils.isAvailable());
+        try {
+            NodeStoreFixture fixture = NodeStoreFixtureProvider.create(cmd.getOptions(MongoUtils.URL, "--fix", "--read-write"));
+            store = (DocumentNodeStore) fixture.getStore();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        NodeBuilder rootBuilder = store.getRoot().builder();
+        new InitialContent().initialize(rootBuilder);
+        NodeBuilder system = rootBuilder.getChildNode(JcrConstants.JCR_SYSTEM);
+        NodeBuilder namespaces = system.getChildNode(NamespaceConstants.REP_NAMESPACES);
+        namespaces.remove();
+        Namespaces.setupNamespaces(rootBuilder.getChildNode(JcrConstants.JCR_SYSTEM));
+        store.merge(rootBuilder, EmptyHook.INSTANCE, CommitInfo.EMPTY);
     }
 
     @Test
@@ -54,25 +76,17 @@ public class NamespaceRegistryCommandTest {
 
     @Test
     public void breakAndFix() throws Exception {
-        Root root = getRoot(MongoUtils.URL, "--fix", "--read-write");
-        Tree namespacesTree = root.getTree(Namespaces.NAMESPACES_PATH);
-        assertTrue(namespacesTree.exists());
-        namespacesTree.setProperty("foo", "urn:foo");
-        root.commit();
-        testCmd(new String[] { MongoUtils.URL, "--analyse" }, new String[] { "This namespace registry model is inconsistent. The inconsistency can be fixed.", "The repaired registry would contain the following mappings:", "foo -> urn:foo" });
-        testCmd(new String[] { MongoUtils.URL, "--fix", "--read-write" }, new String[] { "This namespace registry model is consistent, containing the following mappings from prefixes to namespace uris:", "foo -> urn:foo" });
+        NodeBuilder rootBuilder = store.getRoot().builder();
+        NodeBuilder namespaces = rootBuilder.getChildNode(JcrConstants.JCR_SYSTEM).getChildNode(NamespaceConstants.REP_NAMESPACES);
+        namespaces.setProperty("foo", "urn:foo");
+        store.runBackgroundOperations();
+        store.merge(rootBuilder, EmptyHook.INSTANCE, CommitInfo.EMPTY);
     }
 
     @Test
     public void mappings() throws Exception {
         testCmd(new String[] { MongoUtils.URL, "--analyse" }, new String[] { "This namespace registry model is consistent"});
         testCmd(new String[] { MongoUtils.URL, "--fix", "--mappings",  "foo=urn:foo", "--read-write" }, new String[] { "This namespace registry model is consistent, containing the following mappings from prefixes to namespace uris:", "foo -> urn:foo" });
-    }
-
-    private Root getRoot(String... opts) throws Exception {
-        return new Oak(NodeStoreFixtureProvider.create(cmd.getOptions(opts)).getStore())
-                .with(new OpenSecurityProvider())
-                .createContentSession().getLatestRoot();
     }
 
     private void testCmd(String[] opts, String[] output) throws Exception {
