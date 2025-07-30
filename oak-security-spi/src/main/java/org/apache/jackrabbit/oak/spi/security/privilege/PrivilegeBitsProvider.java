629423a76759ff9cb477972207be1e5538671ee2
Rishabh Kumar <rishabhdaim1991@gmail.com>
2025-07-30 11:10:04 +0530

    OAK-11801 : removed Guava's fluent iterable with Apache's (#2408)

--- oak-security-spi/src/main/java/org/apache/jackrabbit/oak/spi/security/privilege/PrivilegeBitsProvider.java
+++ oak-security-spi/src/main/java/org/apache/jackrabbit/oak/spi/security/privilege/PrivilegeBitsProvider.java
@@ -28,7 +28,7 @@ import java.util.function.Function;
 import javax.jcr.security.AccessControlException;
 import javax.jcr.security.Privilege;
 
-import org.apache.jackrabbit.guava.common.collect.FluentIterable;
+import org.apache.commons.collections4.FluentIterable;
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.api.Root;
 import org.apache.jackrabbit.oak.api.Tree;
@@ -263,7 +263,7 @@ public final class PrivilegeBitsProvider implements PrivilegeConstants {
 
     @NotNull
     private Iterable<String> extractAggregatedPrivileges(@NotNull Iterable<String> privilegeNames) {
-        return FluentIterable.from(privilegeNames).transformAndConcat(new ExtractAggregatedPrivileges()::apply);
+        return IterableUtils.chainedIterable(FluentIterable.of(privilegeNames).transform(new ExtractAggregatedPrivileges()::apply));
     }
 
     @NotNull
