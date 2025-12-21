f3b89f804b7ea40edc11079898085fe0140bd80c
Nuno Santos <nsantos@adobe.com>
2025-12-21 11:42:05 +0100

    OAK-12044 - Optimize PathUtils.concat for the case where parent is the root directory. (#2669)

--- oak-commons/src/main/java/org/apache/jackrabbit/oak/commons/PathUtils.java
+++ oak-commons/src/main/java/org/apache/jackrabbit/oak/commons/PathUtils.java
@@ -317,8 +317,11 @@ public final class PathUtils {
         } else if (isAbsolutePath(subPath)) {
             throw new IllegalArgumentException("Cannot append absolute path " + subPath);
         }
-        String separator = denotesRootPath(parentPath) ? "" : "/";
-        return parentPath + separator + subPath;
+        if (denotesRootPath(parentPath)) {
+            return parentPath + subPath;
+        } else {
+            return parentPath + "/" + subPath;
+        }
     }
 
     /**
