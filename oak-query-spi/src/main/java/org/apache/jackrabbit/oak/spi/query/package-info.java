31e28a9648254b9413aa9f3c618f7411cd6a29fd
Thomas Mueller <mueller@adobe.com>
2026-02-03 09:10:39 +0100

    Revert "OAK-12057 - Select same plan with or without LIMIT option - version bump"
    This reverts commit 8a13d35b6e88ad959ec8d52a75a846f3bb02251e.

--- oak-query-spi/src/main/java/org/apache/jackrabbit/oak/spi/query/package-info.java
+++ oak-query-spi/src/main/java/org/apache/jackrabbit/oak/spi/query/package-info.java
@@ -18,7 +18,7 @@
 /**
  * This package contains oak query index related classes.
  */
-@Version("4.0.0")
+@Version("3.2.1")
 package org.apache.jackrabbit.oak.spi.query;
 
 import org.osgi.annotation.versioning.Version;
