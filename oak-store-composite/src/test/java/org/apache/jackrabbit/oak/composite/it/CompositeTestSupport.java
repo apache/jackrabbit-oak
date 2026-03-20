f8389dbe621cafb4d5d70e9eae7f6676b983eef3
rishabhdaim <rishabhdaim1991@gmail.com>
2026-03-20 15:00:48 +0530

    OAK-11946 : added to osgi composite tests

--- oak-store-composite/src/test/java/org/apache/jackrabbit/oak/composite/it/CompositeTestSupport.java
+++ oak-store-composite/src/test/java/org/apache/jackrabbit/oak/composite/it/CompositeTestSupport.java
@@ -100,7 +100,8 @@ public abstract class CompositeTestSupport extends TestSupport {
             mavenBundle().groupId("org.apache.commons").artifactId("commons-text").versionAsInProject(),
             mavenBundle().groupId("com.fasterxml.jackson.core").artifactId("jackson-core").versionAsInProject(),
             mavenBundle().groupId("com.fasterxml.jackson.core").artifactId("jackson-annotations").versionAsInProject(),
-            mavenBundle().groupId("com.fasterxml.jackson.core").artifactId("jackson-databind").versionAsInProject()
+            mavenBundle().groupId("com.fasterxml.jackson.core").artifactId("jackson-databind").versionAsInProject(),
+            mavenBundle().groupId("com.github.ben-manes.caffeine").artifactId("caffeine").versionAsInProject()
         );
     }
 
