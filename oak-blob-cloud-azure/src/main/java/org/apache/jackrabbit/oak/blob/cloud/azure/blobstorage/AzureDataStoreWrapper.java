c811da0ea61f012a2e19c7fe13a02b3c909f85a3
Julian Reschke <julian.reschke@gmx.de>
2026-08-14 13:42:25 +0100

    Revert "Apply suggestion from @jsedding"
    This reverts commit 59c4d445bf13c6f447dd07a5f117f1a5948d5caf.

--- oak-blob-cloud-azure/src/main/java/org/apache/jackrabbit/oak/blob/cloud/azure/blobstorage/AzureDataStoreWrapper.java
+++ oak-blob-cloud-azure/src/main/java/org/apache/jackrabbit/oak/blob/cloud/azure/blobstorage/AzureDataStoreWrapper.java
@@ -74,7 +74,7 @@ import java.util.Properties;
         configurationPid = AzureDataStoreWrapper.NAME,
         configurationPolicy = ConfigurationPolicy.REQUIRE
 )
-public class AzureDataStoreRegistrar extends AbstractDataStoreService {
+public class AzureDataStoreWrapper extends AbstractDataStoreService {
 
     private static final Logger log = LoggerFactory.getLogger(AzureDataStoreWrapper.class);
 
