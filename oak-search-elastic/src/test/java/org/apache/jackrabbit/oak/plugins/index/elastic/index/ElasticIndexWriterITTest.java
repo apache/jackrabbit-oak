33fdd3f4c194779a086ffeca1e039c9502c9b986
fabriziofortino <fabrizio.fortino@gmail.com>
2026-08-17 14:18:03 +0200

    OAK-12366: (toxiproxy) fix deprecation warnings

--- oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elastic/index/ElasticIndexWriterITTest.java
+++ oak-search-elastic/src/test/java/org/apache/jackrabbit/oak/plugins/index/elastic/index/ElasticIndexWriterITTest.java
@@ -35,11 +35,14 @@ import org.apache.jackrabbit.oak.spi.state.NodeState;
 import org.apache.jackrabbit.oak.spi.state.NodeStore;
 import org.apache.jackrabbit.oak.stats.StatisticsProvider;
 import org.jetbrains.annotations.NotNull;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.testcontainers.containers.ToxiproxyContainer;
+import org.testcontainers.toxiproxy.ToxiproxyContainer;
 import org.testcontainers.utility.DockerImageName;
+
+import java.io.IOException;
 
 import static org.apache.jackrabbit.oak.InitialContentHelper.INITIAL_CONTENT;
 
@@ -81,6 +84,12 @@ public class ElasticIndexWriterITTest {
         this.nodeStore = new MemoryNodeStore(INITIAL_CONTENT);
     }
 
+    @After
+    public void tearDown() throws IOException {
+        if (toxiproxy.isRunning()) {
+            toxiproxy.stop();
+        }
+    }
 
     @Test
     public void writerRecoversFromDisconnection() throws Exception {
