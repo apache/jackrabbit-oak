cd66863071906dcc9822e5b48e7bcc87560a0061
Nuno Santos <nsantos@adobe.com>
2026-09-24 15:15:02 +0200

    OAK-12413: Pre-size predictable ArrayList allocations in the compaction write path

--- oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/Template.java
+++ oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/Template.java
@@ -113,7 +113,7 @@ public class Template {
         requireNonNull(state);
         PropertyState primary = null;
         PropertyState mixins = null;
-        List<PropertyTemplate> templates = new ArrayList<>();
+        List<PropertyTemplate> templates = new ArrayList<>((int) state.getPropertyCount());
 
         for (PropertyState property : state.getProperties()) {
             String name = property.getName();
